8866c7da285fb70ed91628282623492c07753547
Jan-Philipp Willem <info@jwillem.de>
2015-08-06 23:58:02 +0200

    fix ujs issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require react_ujs
 //= require components
--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,7 +1,6 @@
-//= require_self
-//= require react_ujs
-
 // see https://github.com/rackt/react-router/issues/1067
 var React = require('react');
+var App = require('./components/App');
 
-App = require('./components/App');
+//= require_self
+//= require react_ujs
