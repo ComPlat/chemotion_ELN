682b852b15f74a3597f6098c7b6931abcec55625
Fernando D'Agostino <fd@ninjaconcept.com>
2015-09-07 12:27:00 +0200

    Update Sharing Modal. TODO: Checkedall

--- app/usecases/sharing/share_with_users.rb
+++ app/usecases/sharing/share_with_users.rb
@@ -8,14 +8,17 @@ module Usecases
       def execute!
         user_ids = @params.fetch(:user_ids, [])
         collection_attributes = @params.fetch(:collection_attributes, {})
-        # TODO reaction_ids,...
-        sample_ids = @params.fetch(:sample_ids, [])
+        
+        elements_filter = @params.fetch(:elements_filter, {})
+        sample_ids = elements_filter.fetch(:sample_ids, [])
+        reaction_ids = elements_filter.fetch(:reaction_ids, [])
 
         user_ids.each do |user_id|
           collection_attributes[:user_id] = user_id
           new_params = {
             collection_attributes: collection_attributes,
-            sample_ids: sample_ids
+            sample_ids: sample_ids,
+            reaction_ids: reaction_ids
           }
           Usecases::Sharing::ShareWithUser.new(new_params).execute!
         end
