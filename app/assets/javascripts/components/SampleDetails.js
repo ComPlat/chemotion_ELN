d3ad2de089e76c643475978fa5d75e6854a91147
Jan-Philipp Willem <info@jwillem.de>
2015-09-17 10:11:36 +0200

    replace dropdown with select2, add static solvents

--- app/assets/javascripts/components/SampleDetails.js
+++ app/assets/javascripts/components/SampleDetails.js
@@ -9,6 +9,7 @@ import UIStore from './stores/UIStore';
 
 import NumeralInputWithUnits from './NumeralInputWithUnits'
 import ElementCollectionLabels from './ElementCollectionLabels';
+import Select from 'react-select';
 
 import StructureEditorModal from './structure_editor/StructureEditorModal';
 
@@ -95,7 +96,6 @@ export default class SampleDetails extends React.Component {
     });
   }
 
-
   handlePurityChanged(e) {
     let sample = this.state.sample;
     sample.purity = this.refs.purityInput.getValue();
@@ -119,6 +119,7 @@ export default class SampleDetails extends React.Component {
       sample: sample
     });
   }
+
   handleBoilingPointChanged(e) {
     let sample = this.state.sample;
     sample.molecule.boiling_point = this.refs.boilingPointInput.getValue();
@@ -135,11 +136,11 @@ export default class SampleDetails extends React.Component {
     });
   }
 
-  handleSolventChanged(e) {
-    let sample = this.state.sample;
-    sample.solvent = this.refs.solventInput.getValue();
+  handleSolventChanged(event) {
+    let {sample} = this.state;
+    sample.solvent = event;
     this.setState({
-      sample: sample
+      sample
     });
   }
 
@@ -154,15 +155,15 @@ export default class SampleDetails extends React.Component {
   handleUnitChanged(unit, nextUnit, value) {
     console.log("ajax call with unit: " + unit + " nextUnit: " + nextUnit + " and value: " + value);
     let convertedValue = value;
-    if (unit && nextUnit && unit != nextUnit) {
-      switch (unit) {
+    if(unit && nextUnit && unit != nextUnit) {
+      switch(unit) {
         case 'g':
-          if (nextUnit == 'mol') {
+          if(nextUnit == 'mol') {
             convertedValue = value * 2;
           }
           break;
         case 'mol':
-          if (nextUnit == 'g') {
+          if(nextUnit == 'g') {
             convertedValue = value / 2;
           }
           break;
@@ -204,7 +205,7 @@ export default class SampleDetails extends React.Component {
   }
 
   _submitFunction() {
-    if (this.state.sample.id == '_new_') {
+    if(this.state.sample.id == '_new_') {
       this.createSample();
     } else {
       this.updateSample();
@@ -212,7 +213,7 @@ export default class SampleDetails extends React.Component {
   }
 
   _submitLabel() {
-    if (this.state.sample.id == '_new_') {
+    if(this.state.sample.id == '_new_') {
       return "Save Sample";
     } else {
       return "Update Sample";
@@ -228,7 +229,7 @@ export default class SampleDetails extends React.Component {
 
     let sample = this.state.sample || {}
     let sampleAmount = sample.amount_value && sample.amount_unit ? `(${sample.amount_value} ${sample.amount_unit})` : '';
-    let svgPath = sample.molecule && sample.molecule.molecule_svg_file ? `/images/molecules/${sample.molecule.molecule_svg_file}`  : '';
+    let svgPath = sample.molecule && sample.molecule.molecule_svg_file ? `/images/molecules/${sample.molecule.molecule_svg_file}` : '';
     let molfile = sample.molfile;
 
     let structureEditorButton = (
@@ -247,65 +248,67 @@ export default class SampleDetails extends React.Component {
           onSave={this.handleStructureEditorSave.bind(this)}
           onCancel={this.handleStructureEditorCancel.bind(this)}
           molfile={molfile}
-        />
+          />
         <Panel header="Sample Details" bsStyle='primary'>
-          <table width="100%" height="190px"><tr>
-            <td width="70%">
-              <h3>{sample.name}</h3>
-              <h4>{sampleAmount}</h4>
-              <ElementCollectionLabels element={sample} key={sample.id} />
-            </td>
-            <td width="30%">
-              <SVG key={sample.molecule && sample.molecule.id} src={svgPath} className="molecule-mid"/>
-            </td>
-          </tr></table>
+          <table width="100%" height="190px">
+            <tr>
+              <td width="70%">
+                <h3>{sample.name}</h3>
+                <h4>{sampleAmount}</h4>
+                <ElementCollectionLabels element={sample} key={sample.id}/>
+              </td>
+              <td width="30%">
+                <SVG key={sample.molecule && sample.molecule.id} src={svgPath} className="molecule-mid"/>
+              </td>
+            </tr>
+          </table>
           <ListGroup fill>
 
             <form>
               <ListGroupItem>
                 <Input type="text" label="Molecule" ref="moleculeInput"
-                  buttonAfter={structureEditorButton}
-                  value={sample.molecule && (sample.molecule.iupac_name || sample.molecule.sum_formular)}
-                />
+                       buttonAfter={structureEditorButton}
+                       value={sample.molecule && (sample.molecule.iupac_name || sample.molecule.sum_formular)}
+                  />
                 <table width="100%">
                   <tr>
                     <td width="50%" className="padding-right">
                       <Input type="text" label="InChI"
-                        value={sample.molecule && (sample.molecule.inchistring) }
-                        disabled
-                      />
+                             value={sample.molecule && (sample.molecule.inchistring) }
+                             disabled
+                        />
                     </td>
                     <td width="25%" className="padding-right">
                       <Input type="text" label="M. Weight"
-                        value={sample.molecule && (sample.molecule.molecular_weight) }
-                        disabled
-                      />
+                             value={sample.molecule && (sample.molecule.molecular_weight) }
+                             disabled
+                        />
                     </td>
                     <td width="25%">
                       <Input type="text" label="Density" ref="densityInput"
-                        value={sample.molecule && (sample.molecule.density) }
-                        onChange={(e) => this.handleDensityChanged(e)}
-                      />
+                             value={sample.molecule && (sample.molecule.density) }
+                             onChange={(e) => this.handleDensityChanged(e)}
+                        />
                     </td>
                   </tr>
                   <tr>
                     <td width="50%" className="padding-right">
                       <Input type="text" label="Formula"
-                        value={sample.molecule && (sample.molecule.sum_formular) }
-                        disabled
-                      />
+                             value={sample.molecule && (sample.molecule.sum_formular) }
+                             disabled
+                        />
                     </td>
                     <td width="25%" className="padding-right">
                       <Input type="text" label="Boiling Point" ref="boilingPointInput"
-                        value={sample.molecule && (sample.molecule.boiling_point) }
-                        onChange={(e) => this.handleBoilingPointChanged(e)}
-                      />
+                             value={sample.molecule && (sample.molecule.boiling_point) }
+                             onChange={(e) => this.handleBoilingPointChanged(e)}
+                        />
                     </td>
                     <td width="25%">
                       <Input type="text" label="Melting Point" ref="meltingPointInput"
-                       value={sample.molecule && (sample.molecule.melting_point) }
-                       onChange={(e) => this.handleMeltingPointChanged(e)}
-                      />
+                             value={sample.molecule && (sample.molecule.melting_point) }
+                             onChange={(e) => this.handleMeltingPointChanged(e)}
+                        />
                     </td>
                   </tr>
                 </table>
@@ -315,76 +318,77 @@ export default class SampleDetails extends React.Component {
                   <tr>
                     <td width="50%" className="padding-right" colSpan={2}>
                       <Input type="text" label="Name" ref="nameInput"
-                        placeholder={sample.name}
-                        value={sample.name}
-                        onChange={(e) => this.handleNameChanged(e)}
-                      />
+                             placeholder={sample.name}
+                             value={sample.name}
+                             onChange={(e) => this.handleNameChanged(e)}
+                        />
                     </td>
                     <td width="25%" className="padding-right">
                       <Input type="text" label="Impurities"
-                        ref="impuritiesInput"
-                        value={sample.impurities}
-                        onChange={(e) => this.handleImpuritiesChanged(e)}
-                      />
+                             ref="impuritiesInput"
+                             value={sample.impurities}
+                             onChange={(e) => this.handleImpuritiesChanged(e)}
+                        />
                     </td>
                     <td width="25%">
-                      <Input type='select' label='Solvent' ref="solventInput" onChange={(e) => this.handleSolventChanged(e)} value={sample.solvent}>
-                        <option value=''>-- Please Select --</option>
-                        <option value='Solvent1'>Solvent1</option>
-                        <option value='Solvent2'>Solvent2</option>
-                        <option value='Solvent3'>Solvent3</option>
-                      </Input>
+                      <label>Solvent</label>
+                      <Select ref='solventInput'
+                              name='solvents'
+                              multi={false}
+                              options={solvents}
+                              onChange={(e) => this.handleSolventChanged(e)}
+                              value={sample.solvent}/>
                     </td>
                   </tr>
                   <tr>
                     <td width="25%" className="padding-right">
                       <Input type="text" label="Weight"
-                        value={sample.weight}
-                        disabled
-                      />
+                             value={sample.weight}
+                             disabled
+                        />
                     </td>
                     <td width="25%" className="padding-right">
                       <Input type="text" label="Volume"
-                        value={sample.volume}
-                        disabled
-                      />
+                             value={sample.volume}
+                             disabled
+                        />
                     </td>
                     <td width="25%" className="padding-right">
                       <NumeralInputWithUnits
-                         key={sample.id}
-                         value={sample.amount_value}
-                         unit={sample.amount_unit || 'g'}
-                         label="Amount"
-                         units={['g', 'ml', 'mol']}
-                         numeralFormat='0,0.00'
-                         convertValueFromUnitToNextUnit={(unit, nextUnit, value) => this.handleUnitChanged(unit, nextUnit, value)}
-                         onChange={(amount) => this.handleAmountChanged(amount)}
-                      />
+                        key={sample.id}
+                        value={sample.amount_value}
+                        unit={sample.amount_unit || 'g'}
+                        label="Amount"
+                        units={['g', 'ml', 'mol']}
+                        numeralFormat='0,0.00'
+                        convertValueFromUnitToNextUnit={(unit, nextUnit, value) => this.handleUnitChanged(unit, nextUnit, value)}
+                        onChange={(amount) => this.handleAmountChanged(amount)}
+                        />
                     </td>
                     <td width="25%">
                       <Input type="text" label="Purity"
-                        ref="purityInput"
-                        value={sample.purity}
-                        onChange={(e) => this.handlePurityChanged(e)}
-                      />
+                             ref="purityInput"
+                             value={sample.purity}
+                             onChange={(e) => this.handlePurityChanged(e)}
+                        />
                     </td>
                   </tr>
                   <tr>
                     <td width="50%" colSpan={2} className="padding-right">
                       <Input type="textarea" label="Location"
-                        ref="locationInput"
-                        value={sample.location}
-                        onChange={(e) => this.handleLocationChanged(e)}
-                        rows={2}
-                      />
+                             ref="locationInput"
+                             value={sample.location}
+                             onChange={(e) => this.handleLocationChanged(e)}
+                             rows={2}
+                        />
                     </td>
                     <td width="50%" colSpan={2}>
                       <Input type="textarea" label="Description" ref="descriptionInput"
-                        placeholder={sample.description}
-                        value={sample.description}
-                        onChange={(e) => this.handleDescriptionChanged(e)}
-                        rows={2}
-                      />
+                             placeholder={sample.description}
+                             value={sample.description}
+                             onChange={(e) => this.handleDescriptionChanged(e)}
+                             rows={2}
+                        />
                     </td>
                   </tr>
                 </table>
@@ -393,7 +397,8 @@ export default class SampleDetails extends React.Component {
               <ListGroupItem>
                 <ButtonToolbar>
                   <Button bsStyle="primary" onClick={this.closeDetails.bind(this)}>Back</Button>
-                  <Button bsStyle="warning" onClick={this._submitFunction.bind(this)} disabled={!sampleIsValid}>{this._submitLabel()}</Button>
+                  <Button bsStyle="warning" onClick={this._submitFunction.bind(this)}
+                          disabled={!sampleIsValid}>{this._submitLabel()}</Button>
                 </ButtonToolbar>
               </ListGroupItem>
             </form>
@@ -403,3 +408,86 @@ export default class SampleDetails extends React.Component {
     )
   }
 }
+
+const solvents = [{
+  label: 'Aceton',
+  value: 'Aceton'
+}, {
+  label: 'Benzol',
+  value: 'Benzol'
+}, {
+  label: 'Butanol',
+  value: 'Butanol'
+}, {
+  label: 'Chloroform',
+  value: 'Chloroform'
+}, {
+  label: 'Cyclohexan',
+  value: 'Cyclohexan'
+}, {
+  label: 'Diethylether',
+  value: 'Diethylether'
+}, {
+  label: 'Dimethylsulfoxid (DMF)',
+  value: 'Dimethylsulfoxid'
+}, {
+  label: 'Dimethylformamid (DMF)',
+  value: 'Dimethylformamid'
+}, {
+  label: '1,4-Dioxan',
+  value: '1,4-Dioxan'
+}, {
+  label: 'Essigsäure',
+  value: 'Essigsäure'
+}, {
+  label: 'Ethanol',
+  value: 'Ethanol'
+}, {
+  label: 'Ethylacetat',
+  value: 'Ethylacetat'
+}, {
+  label: 'Isopropanol',
+  value: 'Isopropanol'
+}, {
+  label: 'Methanol',
+  value: 'Methanol'
+}, {
+  label: 'Methylenchlorid (DCM)',
+  value: 'Methylenchlorid'
+}, {
+  label: 'Methyl-tert-butylether (MTBE)',
+  value: 'Methyl-tert-butylether'
+}, {
+  label: 'n-Hexan',
+  value: 'n-Hexan'
+}, {
+  label: 'N-Methyl-2-pyrrolidon (NMP)',
+  value: 'N-Methyl-2-pyrrolidon'
+}, {
+  label: 'Pentan',
+  value: 'Pentan'
+}, {
+  label: 'Pyridin',
+  value: 'Pyridin'
+}, {
+  label: 'Tetrahydrofuran (THF)',
+  value: 'Tetrahydrofuran'
+}, {
+  label: 'Toluol',
+  value: 'Toluol'
+}, {
+  label: 'Wasser',
+  value: 'Wasser'
+}, {
+  label: 'CDCl3',
+  value: 'CDCl3'
+}, {
+  label: 'MeOD-d4',
+  value: 'MeOD-d4'
+}, {
+  label: 'C6D6',
+  value: 'C6D6'
+}, {
+  label: 'D2O',
+  value: 'D2O'
+}];
