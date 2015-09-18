2d90abfe51ead6cbd29be2b06fdbc810e67e6605
Jan-Philipp Willem <info@jwillem.de>
2015-09-18 16:57:32 +0200

    fix merge-issue

--- app/assets/javascripts/components/App.js
+++ app/assets/javascripts/components/App.js
@@ -164,73 +164,7 @@ class App extends React.Component {
   }
 }
 
-<<<<<<< HEAD
-import ElementStore from './stores/ElementStore';
-
-export default class Elements extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentElement: null
-    }
-  }
-
-  componentDidMount() {
-    ElementStore.listen(this.onChange.bind(this));
-  }
-
-  componentWillUnmount() {
-    ElementStore.unlisten(this.onChange.bind(this));
-  }
-
-  onChange(state) {
-    this.setState({
-      currentElement: state.currentElement
-    })
-  }
-
-  render() {
-    let width = this.state.currentElement ? "65%" : 0;
-    let elementDetails;
-
-    if(this.state.currentElement) {
-      //todo: switch component by element.type
-      switch(this.state.currentElement.type) {
-        case 'sample':
-          elementDetails = <SampleDetails sample={this.state.currentElement}/>;
-          break;
-        case 'reaction':
-          elementDetails = <ReactionDetails reaction={this.state.currentElement}/>;
-          break;
-        case 'wellplate':
-          elementDetails = <WellplateDetails wellplate={this.state.currentElement}/>;
-        default:
-      }
-    }
-
-    return (
-      <div id="elements">
-        <Table>
-          <tbody>
-          <tr valign="top" className="borderless">
-            <td className="borderless">
-              <List/>
-            </td>
-            <td className="borderless" width={width}>
-              {elementDetails}
-            </td>
-          </tr>
-          </tbody>
-        </Table>
-      </div>
-    )
-  }
-}
-
-$(document).ready(function () {
-=======
 $(document).ready(function() {
->>>>>>> implement dnd between samples and wellplate
   React.render(<App />, document.getElementById('app'));
   Aviator.dispatch();
 });
