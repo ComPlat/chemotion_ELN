d3f3b2f1b30f21f987e8c48cce90969414ded396
Florian Hübsch <fh@ninjaconcept.com>
2015-10-02 16:23:38 +0200

    Remove referenced elements from original owner if we take ownership.

--- app/api/chemotion/collection_api.rb
+++ app/api/chemotion/collection_api.rb
@@ -12,7 +12,25 @@ module Chemotion
           end
 
           post do
-            Collection.find(params[:id]).update(is_shared: false)
+            c = Collection.find(params[:id])
+            sample_ids = c.samples.pluck(:id)
+            reaction_ids = c.reactions.pluck(:id)
+            wellplate_ids = c.wellplates.pluck(:id)
+
+            owner = User.find(c.shared_by_id)
+            owner_collections = owner.collections
+            owner_sample_collections = owner_collections.includes(:samples).where('samples.id IN (?)', sample_ids).references(:samples)
+            owner_reaction_collections = owner_collections.includes(:reactions).where('reactions.id IN (?)', reaction_ids).references(:reacions)
+            owner_wellplate_collections = owner_collections.includes(:wellplates).where('wellplates.id IN (?)', wellplate_ids).references(:wellplates)
+
+            ActiveRecord::Base.transaction do
+              c.update(is_shared: false, parent: nil)
+
+              # delete all associations of former_owner to elements included in c
+              CollectionsSample.where('sample_id IN (?) AND collection_id IN (?)', sample_ids, owner_sample_collections.pluck(:id)).delete_all
+              CollectionsReaction.where('reaction_id IN (?) AND collection_id IN (?)', reaction_ids, owner_reaction_collections.pluck(:id)).delete_all
+              CollectionsWellplate.where('wellplate_id IN (?) AND collection_id IN (?)', wellplate_ids, owner_wellplate_collections.pluck(:id)).delete_all
+            end
           end
         end
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :collections
+  has_many :samples, through: :collections
+  has_many :reactions, through: :collections
+  has_many :wellplates, through: :collections
 
   def owns_collections?(collections)
     collections.pluck(:user_id).uniq == [id]
