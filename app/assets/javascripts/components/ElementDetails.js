10bd7e2fbee23f6d1e1779e74e5bd1b38eed53d5
Jan-Philipp Willem <info@jwillem.de>
2015-10-08 13:15:31 +0200

    add alternative elements-view

--- app/assets/javascripts/components/ElementDetails.js
+++ app/assets/javascripts/components/ElementDetails.js
@@ -1,46 +1,21 @@
 import React, {Component} from 'react';
-import ElementStore from './stores/ElementStore';
 import SampleDetails from './SampleDetails';
 import ReactionDetails from './ReactionDetails';
 import WellplateDetails from './WellplateDetails';
 import ScreenDetails from './ScreenDetails';
 
 export default class ElementDetails extends Component {
-  constructor(props) {
-    super(props);
-    const {currentElement} = props;
-    this.state = {
-      currentElement
-    };
-  }
-  componentDidMount() {
-    ElementStore.listen(state => this.handleOnChange(state));
-  }
-
-  componentWillUnmount() {
-    ElementStore.unlisten(state => this.handleOnChange(state));
-  }
-
-  handleOnChange(state) {
-    const {currentElement} = state;
-    this.setState({currentElement});
-  }
-
   render() {
-    const {currentElement} =  this.state;
-    if(currentElement) {
-      switch(currentElement.type) {
-        case 'sample':
-          return <SampleDetails sample={this.state.currentElement}/>;
-        case 'reaction':
-          return <ReactionDetails reaction={this.state.currentElement}/>;
-        case 'wellplate':
-          return <WellplateDetails wellplate={this.state.currentElement}/>;
-        case 'screen':
-          return <ScreenDetails screen={this.state.currentElement}/>;
-      }
-    } else {
-      return <div></div>;
+    const {currentElement} =  this.props;
+    switch (currentElement.type) {
+      case 'sample':
+        return <SampleDetails sample={currentElement}/>;
+      case 'reaction':
+        return <ReactionDetails reaction={currentElement}/>;
+      case 'wellplate':
+        return <WellplateDetails wellplate={currentElement}/>;
+      case 'screen':
+        return <ScreenDetails screen={currentElement}/>;
     }
   }
 }
--- app/assets/javascripts/components/Elements.js
+++ app/assets/javascripts/components/Elements.js
@@ -4,20 +4,51 @@ import {Col} from 'react-bootstrap';
 import HTML5Backend from 'react-dnd/modules/backends/HTML5';
 import List from './List';
 import ElementDetails from './ElementDetails';
-
+import ElementStore from './stores/ElementStore';
 
 class Elements extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentElement: null
+    };
+  }
+
+  componentDidMount() {
+    ElementStore.listen(state => this.handleOnChange(state));
+  }
+
+  componentWillUnmount() {
+    ElementStore.unlisten(state => this.handleOnChange(state));
+  }
+
+  handleOnChange(state) {
+    const {currentElement} = state;
+    this.setState({currentElement});
+  }
+
   render() {
-    return (
-      <div>
-        <Col md={4}>
-          <List/>
-        </Col>
-        <Col md={8}>
-          <ElementDetails/>
-        </Col>
-      </div>
-    )
+    const {currentElement} =  this.state;
+    if (currentElement) {
+      return (
+        <div>
+          <Col md={4}>
+            <List/>
+          </Col>
+          <Col md={8}>
+            <ElementDetails currentElement={currentElement}/>
+          </Col>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Col md={12}>
+            <List/>
+          </Col>
+        </div>
+      );
+    }
   }
 }
 
