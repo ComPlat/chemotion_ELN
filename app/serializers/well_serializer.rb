c2c37e9eba77fadbe4574d919f5783f4076fad9f
Jan-Philipp Willem <info@jwillem.de>
2015-10-08 10:38:13 +0200

    remove redundant readout declaration in well-serializer

--- app/serializers/well_serializer.rb
+++ app/serializers/well_serializer.rb
@@ -3,14 +3,6 @@ class WellSerializer < ActiveModel::Serializer
 
   has_one :sample
 
-  def readout
-    object.readout
-  end
-
-  def additive
-    object.additive
-  end
-
   def position
     #wrap position_x and y to position object
     {x: object.position_x, y: object.position_y}
