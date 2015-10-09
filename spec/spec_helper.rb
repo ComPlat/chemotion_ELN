2e455ae278e4a0c03474290a6907353fe45c0805
Florian Hübsch <fh@ninjaconcept.com>
2015-10-09 14:36:49 +0200

    Remove annoying spec filter.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,6 @@ require 'factory_girl_rails'
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
-  config.filter_run_including :focus => true
-  
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
