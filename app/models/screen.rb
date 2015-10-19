08ca1f9e914e608d38c5e35595b5d34e6246021f
Florian Hübsch <fh@ninjaconcept.com>
2015-10-19 17:46:07 +0200

    Dependent destroy screen associations, add screen model spec.

--- app/models/screen.rb
+++ app/models/screen.rb
@@ -17,7 +17,7 @@ class Screen < ActiveRecord::Base
   scope :by_requirements, ->(query) { where('requirements ILIKE ?', "%#{query}%") }
   scope :by_wellplate_ids, ->(ids) { joins(:wellplates).where('wellplates.id in (?)', ids) }
 
-  has_many :collections_screens
+  has_many :collections_screens, dependent: :destroy
   has_many :collections, through: :collections_screens
 
   has_many :screens_wellplates, dependent: :destroy
