39bfd3d7b4ea38c36fd2372863db88d63f254cc7
Fernando D'Agostino <fd@ninjaconcept.com>
2015-10-19 15:23:41 +0200

    Add update of the Samples when saving reaction or wellplate

--- app/assets/javascripts/components/stores/ElementStore.js
+++ app/assets/javascripts/components/stores/ElementStore.js
@@ -170,6 +170,7 @@ class ElementStore {
   handleUpdateWellplate(wellplate) {
     this.state.currentElement = wellplate;
     this.handleRefreshElements('wellplate');
+    this.handleRefreshElements('sample');
   }
 
   handleCreateWellplate(wellplate) {
@@ -210,6 +211,7 @@ class ElementStore {
   handleUpdateReaction(reaction) {
     this.state.currentElement = reaction;
     this.handleRefreshElements('reaction');
+    this.handleRefreshElements('sample');
   }
 
   handleCreateReaction(reaction) {
