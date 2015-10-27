7204ffc5ac1af733ab1c7773becac35552457256
Fernando D'Agostino <fd@ninjaconcept.com>
2015-10-27 16:31:10 +0100

    Add Analyses labels in the SampleDetails

--- app/serializers/sample_serializer.rb
+++ app/serializers/sample_serializer.rb
@@ -2,7 +2,8 @@ class SampleSerializer < ActiveModel::Serializer
   include Labeled
 
   attributes :id, :type, :name, :description, :created_at, :amount_value, :amount_unit, :molfile,
-             :purity, :solvent, :impurities, :location, :is_top_secret, :is_restricted, :external_label, :analyses
+             :purity, :solvent, :impurities, :location, :is_top_secret, :is_restricted, :external_label, :analyses,
+             :analysis_kinds
 
   has_one :molecule
 
@@ -20,6 +21,20 @@ class SampleSerializer < ActiveModel::Serializer
 
   def is_restricted
     false
+  end
+
+  def analysis_kinds
+    analyses = object.analyses
+    analyses.inject({confirmed: [], unconfirmed: [], other: []}) { |result, analysis|
+      if analysis["status"] == "Confirmed" 
+        result[:confirmed].push(analysis["kind"])
+      elsif analysis["status"] == "Unconfirmed" 
+        result[:unconfirmed].push(analysis["kind"])
+      else
+        result[:other].push(analysis["kind"])
+      end
+      result
+    }
   end
 
   class Level0 < ActiveModel::Serializer
