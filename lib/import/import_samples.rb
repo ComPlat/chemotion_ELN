020bfb7b01549e30302a977442623f94f48b0ddd
Fernando D'Agostino <fd@ninjaconcept.com>
2015-12-02 15:48:37 +0100

    Add ActiveRecord Base Transaction into import of samples from file

--- lib/import/import_samples.rb
+++ lib/import/import_samples.rb
@@ -2,14 +2,16 @@ require 'roo'
 class Import::ImportSamples
 
   def self.import_samples_from_file(file_path, collection_id)
-    xlsx = Roo::Spreadsheet.open(file_path)
-    rows = xlsx.parse(name: "Name", description: "Beschreibung", smiles: "Smiles")
-    rows.shift
-    rows.map do |row|
-      molfile = Chemotion::PubchemService.molfile_from_smiles URI::encode(row[:smiles], '[]/()+-.@#=\\')
-      molecule = Molecule.find_or_create_by_molfile(molfile)
-      sample = Sample.create(name: row[:name], description: row[:description], molecule: molecule)
-      CollectionsSample.create(collection_id: collection_id, sample_id: sample.id)
+    ActiveRecord::Base.transaction do
+      xlsx = Roo::Spreadsheet.open(file_path)
+      rows = xlsx.parse(name: "Name", description: "Beschreibung", smiles: "Smiles")
+      rows.shift
+      rows.map do |row|
+        molfile = Chemotion::PubchemService.molfile_from_smiles URI::encode(row[:smiles], '[]/()+-.@#=\\')
+        molecule = Molecule.find_or_create_by_molfile(molfile)
+        sample = Sample.create(name: row[:name], description: row[:description], molecule: molecule)
+        CollectionsSample.create(collection_id: collection_id, sample_id: sample.id)
+      end
     end
   end
 
