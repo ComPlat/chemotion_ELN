edf6ea2aa5b7794b1cec883e59d1f84595d86e63
Jan-Philipp Willem <info@jwillem.de>
2015-12-17 15:40:20 +0100

    fix reaction-svg arrow-label

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -80,7 +80,9 @@ class Reaction < ActiveRecord::Base
     inchikeys[:starting_materials] = starting_materials.includes(:molecule).pluck(:'molecules.inchikey')
     inchikeys[:reactants] = reactants.includes(:molecule).pluck(:'molecules.inchikey')
     inchikeys[:products] = products.includes(:molecule).pluck(:'molecules.inchikey')
-    label = [solvent, temperature].compact.join(', ')
+    label = [solvent, temperature]
+            .reject(&:blank?)
+            .join(", ")
     begin
       composer = SVG::ReactionComposer.new(inchikeys, label: label)
       self.reaction_svg_file = composer.compose_reaction_svg_and_save
