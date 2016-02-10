7b4ed2d00d05f983a521be4e833c58c9691af71d
PiTrem <pierre.tremouilhac@gmx.de>
2016-02-10 15:09:12 +0100

    dynamic aliasify configuration

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,54 @@ module Chemotion
 
     config.active_job.queue_adapter = :delayed_job
 
+
+    # Chemotion Plugin configuration
+
+    # list of registered plugins (from the plugins group of the Gemfile):
+    PLUGS = Bundler.load.current_dependencies.select{  |dep|  dep.groups.include?(:plugins)}.map(&:name)
+
+    # browserify: aliasifyConfig.js generation
+    # Aliasify is used to enable the import/export of modules from chemotion (app/asset or node_modules) to
+    # the plugin gems (and inversely).
+    # When browserify browses through the plugins files, "chemotion/node_modules" is
+    # not in the path (but plugin/node_modules is ). The aliases avoid the duplication of module installation.
+    # Normaly, modules at the top of myplugin/app/assets/javascripts are in the Node_path
+    # and can be imported directly (see
+    # https://github.com/browserify-rails/browserify-rails#support-for-rails-asset-directories-as-non-relative-module-sources )
+    # As this is debated matter, aliasify also ensure here the paths are correct.
+
+    config.before_configuration do
+      plugin_aliases,plugin_replacements="",""
+      PLUGS.each_with_index do |plugin,i|
+        plugin_path = File.join(Gem.loaded_specs[plugin].full_gem_path,"app","assets","javascripts",plugin).to_s
+        #plugin_replacements << %(    "_plugin#{i.to_s}/((\\/|\\\\w)+)": "#{plugin_path}/$1",\n)
+        plugin_aliases << %(    "#{plugin}":"#{plugin_path}",\n)
+      end
+      aliasify_config = %(module.exports = {
+      "aliases": {
+      #{plugin_aliases}
+      },
+      "replacements": {
+      "_components/((\\/|\\\\w)+)":  "#{Rails.root}"+"/app/assets/javascripts/components/$1",
+      "_node_modules/((\\/|\\\\w)+)":  "#{Rails.root}"+"/node_modules/$1",
+      #{plugin_replacements}
+      },
+      verbose: true
+      };
+      )
+      aliasify_file = File.join(Rails.root, 'aliasifyConfig.js')
+      File.write(aliasify_file,aliasify_config)
+
+      #For each registered plugin gem (group :plugins in Gemfile) browserify needs to find
+      #an aliasifyConfig.js file (and a package.json file ) in gem root directory.
+      #This create a sym link if no file exists.
+      PLUGS.each_with_index do |plugin,i|
+        plugin_path = File.join(Gem.loaded_specs[plugin].full_gem_path,".").to_s
+        `ln -s #{aliasify_file} #{plugin_path}` # unless File.exists?(aliasify_file)
+      end
+    end
     config.browserify_rails.commandline_options = "-t babelify -t aliasify "
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
