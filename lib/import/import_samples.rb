41ef10af899f6a55c50dcae3e2fbbfeed6e42025
Fernando D'Agostino <fd@ninjaconcept.com>
2016-02-22 11:05:06 +0100

    Fixes assignment of created_by when importing samples

--- lib/import/import_samples.rb
+++ lib/import/import_samples.rb
@@ -1,7 +1,7 @@
 require 'roo'
 class Import::ImportSamples
 
-  def self.import_samples_from_file(file_path, collection_id)
+  def self.import_samples_from_file(file_path, collection_id, current_user_id)
     ActiveRecord::Base.transaction do
       xlsx = Roo::Spreadsheet.open(file_path)
       rows = xlsx.parse(name: "Name", description: "Description", smiles: "Smiles", value: "Value")
@@ -14,7 +14,7 @@ class Import::ImportSamples
           raise "Import of Sample #{row[:name]}: Molecule is nil."
         end
 
-        sample = Sample.create(name: row[:name], description: row[:description], molecule: molecule, imported_readout: row[:value])
+        sample = Sample.create(name: row[:name], description: row[:description], molecule: molecule, imported_readout: row[:value], created_by: current_user_id)
         CollectionsSample.create(collection_id: collection_id, sample_id: sample.id)
       end
     end
