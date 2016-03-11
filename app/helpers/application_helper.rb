aa5fb7f192ce2aae4b05bc8b082127013356184f
PiTrem <pierre.tremouilhac@gmx.de>
2016-03-11 16:27:43 +0100

    resolve #134

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def plugin_list
+    Bundler.load.current_dependencies.select{  |dep|  dep.groups.include?(:plugins)}.map(&:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
 
   authenticated :user do
     root to: 'pages#welcome', as: :authenticated_root
+    get 'pages/settings', to: 'pages#settings'
   end
 
   mount API => '/'
-  mount Scifinding::Engine => 'scifi'
+
   root :to => redirect("/users/sign_in")
 
   get 'test', to: 'pages#test'
