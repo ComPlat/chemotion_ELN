0805d84bd276e9b3205c99e73ac4ef81c43cccbb
Serhii Kotov <s.kotovskij@gmail.com>
2016-04-13 16:03:31 +0200

    Reaction naming resolve #186

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :initials, :samples_count
+  attributes :id, :name, :initials, :samples_count, :reactions_count
 
   def samples_count
     object.samples.count
+  end
+
+  def reactions_count
+    object.reactions.count
   end
 end
