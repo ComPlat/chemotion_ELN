913100fff1c59705588fea488ad64644a850fb58
Serhii Kotov <s.kotovskij@gmail.com>
2016-04-22 13:48:43 +0200

    Empty formula hotfix

--- app/assets/javascripts/components/common/Formula.js
+++ app/assets/javascripts/components/common/Formula.js
@@ -4,16 +4,17 @@ var _ = require('lodash');
 
 export default class Formula extends React.Component {
   render() {
-
     let content = ''
 
-    let keys = this.props.formula.split(/([A-Za-z]{1}[a-z]{0,2})(\d*)/)
-    content = _.compact(keys).map(function(item) {
-      if((/\d+/).test(item))
-        return (<sub>{item}</sub>);
-      else
-        return item;
-    })
+    if(this.props.formula) {
+      let keys = this.props.formula.split(/([A-Za-z]{1}[a-z]{0,2})(\d*)/)
+      content = _.compact(keys).map(function(item) {
+        if((/\d+/).test(item))
+          return (<sub>{item}</sub>);
+        else
+          return item;
+      })
+    }
 
     return (
       <span>
