b1db21fb9c3a06358f52081cf40004eb7dad53ee
jasonych99 <jasonych99@gmail.com>
2016-05-20 09:26:05 +0200

    create Profile MVC

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,27 @@
 class PagesController < ApplicationController
   def welcome
   end
+
+  def profiles
+    current_user.has_profile
+    @profile = current_user.profile
+  end
+
+  def update_profiles
+    @profile = current_user.profile
+    @profile.assign_attributes(profile_param)
+
+    if @profile.save
+      flash["success"] = "Profile is successfully saved!"
+      redirect_to root_path
+    else
+      flash.now["danger"] = "Not saved! Please check input fields."
+      render "profile"
+    end
+  end
+
+  private
+  def profile_param
+    params.require(:profile).permit(:show_external_name)
+  end
 end
