2ce220bbf33646b91013d0363f3ff92dabccabeb
Christoph Ossner <christopher.ossner2@it.edu>
2016-05-24 12:50:12 +0200

    added column for shortcuts to users table
    added fild for shortcut on sign-up page
    
    added shortcut to devise fields, added validations for shortcut
    
    changed regExp for shortcut validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,10 @@ class User < ActiveRecord::Base
   has_many :samples_created, foreign_key: :created_by, class_name: 'Sample'
 
   validates_presence_of :first_name, :last_name, allow_blank: false
+  validates :shortcut, format: {with: /(\A[a-zA-Z]{1,3}\Z)|(\A[a-zA-Z][a-zA-Z0-9\-_][a-zA-Z]\Z)/,
+      message: "must be characters, numers, - or _. No leading trailing number, - or _."},
+    length: {in: 1..3},
+    uniqueness: true
 
   after_create :create_chemotion_public_collection, :create_all_collection, :has_profile
 
@@ -31,7 +35,9 @@ class User < ActiveRecord::Base
   end
 
   def initials
-    "#{first_name[0].capitalize}#{last_name[0].capitalize}"
+    # Originally initials were used, to have more flexibility a shortcut column was added
+    # to the users model. Use <tt>shortcut</tt> insted.
+    shortcut
   end
 
   def reset_counters
