8834c9ddc8ef59cd4507c7d22fe7148eb9689dbe
Christoph Ossner <christopher.ossner2@kit.edu>
2016-05-30 10:41:09 +0200

    refactor shortcut to name_abbreviation

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,10 @@ class User < ActiveRecord::Base
   has_many :samples_created, foreign_key: :created_by, class_name: 'Sample'
 
   validates_presence_of :first_name, :last_name, allow_blank: false
-  validates :shortcut, format: {with: /(\A[a-zA-Z]{1,3}\Z)|(\A[a-zA-Z][a-zA-Z0-9\-_][a-zA-Z]\Z)/,
-      message: "must be characters, numers, - or _. No leading trailing number, - or _."},
-    length: {in: 1..3},
-    uniqueness: true
+  validates :name_abbreviation, format: {with: /(\A[a-zA-Z]{1,3}\Z)|(\A[a-zA-Z][a-zA-Z0-9\-_][a-zA-Z]\Z)/,
+      message: "can be alphanumeric, middle '_' and '-' are allowed, but leading or trailing digit, '-' and '_' are not."},
+    length: {in: 1..3, message: "1 to 3 characters only"},
+    uniqueness:  {message: " has already been taken." }
 
   after_create :create_chemotion_public_collection, :create_all_collection, :has_profile
 
@@ -37,7 +37,7 @@ class User < ActiveRecord::Base
   def initials
     # Originally initials were used, to have more flexibility a shortcut column was added
     # to the users model. Use <tt>shortcut</tt> insted.
-    shortcut
+    name_abbreviation
   end
 
   def reset_counters
--- db/migrate/20160524102833_add_shortcut_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddShortcutToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :shortcut, :string, :limit => 3
-  end
-end
