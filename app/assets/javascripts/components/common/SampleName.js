7d7f3531d4e29fa1ed9baa7800171f96564e0254
jasonych99 <jasonych99@gmail.com>
2016-05-30 15:21:51 +0200

    always display sum_formula resolve #276

--- app/assets/javascripts/components/common/SampleName.js
+++ app/assets/javascripts/components/common/SampleName.js
@@ -7,18 +7,25 @@ export default class SampleName extends React.Component {
   render() {
     let {sample} = this.props;
 
-    let molecule_name = sample._molecule.iupac_name || (<Formula formula={sample._molecule.sum_formular}/>);
+    let molecule_name = sample._molecule.iupac_name
+    let sum_formular = (<Formula formula={sample._molecule.sum_formular}/>)
     if(sample.contains_residues) {
       let polymer_name = sample.polymer_type.charAt(0).toUpperCase()
                           + sample.polymer_type.slice(1);
       return (
-        <p>
-          {polymer_name.replace('_', '-') + ' - '}
-          {molecule_name}
-        </p>
+        <div>
+          <p>{polymer_name.replace('_', '-') + ' - '}</p>
+          <p>{sum_formular}</p>
+          <p>{molecule_name}</p>
+        </div>
       )
     } else {
-      return <p>{molecule_name}</p>
+      return (
+        <div>
+          <p>{sum_formular}</p>
+          <p>{molecule_name}</p>
+        </div>
+      )
     }
   }
 }
