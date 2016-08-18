8f115ba11fae6389da65b0dc6c626b388a0c0885
jasonych99 <jasonych99@gmail.com>
2016-08-18 11:05:44 +0200

    Rspec for lib/report/docx resolve #411

--- spec/factories/reactions.rb
+++ spec/factories/reactions.rb
@@ -1,8 +1,17 @@
 FactoryGirl.define do
   factory :reaction do
-    sequence(:name) { |i| "Reaction #{i}" }
     callback(:before_create) do |reaction|
       #reaction.collections << FactoryGirl.build(:collection) if reaction.collections.blank?
     end
+
+    sequence(:name) { |i| "Reaction #{i}" }
+    status "Successful"
+    solvent "Aceton"
+    description "I am description"
+    purification "{TLC,Distillation}"
+    rf_value 0.99
+    tlc_solvents "D2O"
+    tlc_description "I am tlc_description"
+    observation "I am observation"
   end
 end
