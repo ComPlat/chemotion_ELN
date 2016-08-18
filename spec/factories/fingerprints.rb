e8225aa43ea45ae8a533f375eaf23da71552eb29
An Nguyen <canlong8811@gmail.com>
2016-08-18 11:07:22 +0200

    RSpec fingerprint
    - Refactor fingerprint model
    - Add rspec for fingerprint
    - Edit an old migrate to stop populate a deleted column (molecule.num_set_bits), so that it does not take much time if migrate from the beginning

---
+++ spec/factories/fingerprints.rb
@@ -0,0 +1,22 @@
+FactoryGirl.define do
+  factory :fingerprint do
+    fp0 Fingerprint.generate_sample_fingerprint
+    fp1 Fingerprint.generate_sample_fingerprint
+    fp2 Fingerprint.generate_sample_fingerprint
+    fp3 Fingerprint.generate_sample_fingerprint
+    fp4 Fingerprint.generate_sample_fingerprint
+    fp5 Fingerprint.generate_sample_fingerprint
+    fp6 Fingerprint.generate_sample_fingerprint
+    fp7 Fingerprint.generate_sample_fingerprint
+    fp8 Fingerprint.generate_sample_fingerprint
+    fp9 Fingerprint.generate_sample_fingerprint
+    fp10 Fingerprint.generate_sample_fingerprint
+    fp11 Fingerprint.generate_sample_fingerprint
+    fp12 Fingerprint.generate_sample_fingerprint
+    fp13 Fingerprint.generate_sample_fingerprint
+    fp14 Fingerprint.generate_sample_fingerprint
+    fp15 Fingerprint.generate_sample_fingerprint
+
+    num_set_bits Random.rand(0..64)
+  end
+end
--- spec/models/fingerprint_spec.rb
+++ spec/models/fingerprint_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Fingerprint, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    let(:fingerprint) { build :fingerprint }
+
+    it 'possible to create valid fingerprint' do
+      expect(fingerprint.valid?).to be(true)
+    end
+
+    it 'invalid lenght of fingerprint is not 64' do
+      fingerprint.fp15 = "10"
+      expect(fingerprint.valid?).to be(false)
+
+      fingerprint.fp11 = "10101010101010101010101010101010101010101010101010101010101010101010"
+      expect(fingerprint.valid?).to be(false)
+    end
+
+    it 'invalid with fingerprint type is not binary' do
+      fingerprint.fp10 = nil
+      expect(fingerprint.valid?).to be(false)
+
+      expect {
+        fingerprint.fp10 = 8
+        fingerprint.valid?
+      }.to raise_error(ActiveRecord::StatementInvalid)
+    end
+
+    it 'invalid with num_set_bits type is not integer' do
+      fingerprint.num_set_bits = "test string"
+      expect(fingerprint.valid?).to be(false)
+    end
+
+    it 'invalid with num_set_bits is not in the range from 0 to 64' do
+      fingerprint.num_set_bits = 100
+      expect(fingerprint.valid?).to be(false)
+
+      fingerprint.num_set_bits = 64
+      expect(fingerprint.valid?).to be(false)
+
+      fingerprint.num_set_bits = nil
+      expect(fingerprint.valid?).to be(false)
+
+      fingerprint.num_set_bits = -1
+      expect(fingerprint.valid?).to be(false)
+
+      fingerprint.num_set_bits = 0
+      expect(fingerprint.valid?).to be(false)
+    end
+  end
+
+  describe 'deletion' do
+    let(:fingerprint) { create(:fingerprint) }
+    let(:sample)      { create(:sample, fingerprint_id: fingerprint.id) }
+
+    it 'fasle if associated sample existed' do
+      fingerprint.destroy
+      expect(fingerprint.destroyed?).to be(false)
+    end
+
+    it 'only soft deletes fingerprint' do
+      expect(Fingerprint.with_deleted).to eq [fingerprint]
+    end
+  end
 end
