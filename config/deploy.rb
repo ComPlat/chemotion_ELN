1e579ac52361bbca9778ac8c25e4ea411646568d
Serhii Kotov <s.kotovskij@gmail.com>
2016-09-22 15:26:57 +0200

    hotfix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rails_env,   "production"
 set :unicorn_env, "production"
 set :unicorn_rack_env, "production"
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-set :nvm_node, 'v4.4.3'
+set :nvm_node, 'v4.4.7'
 
 # Default value for :format is :pretty
 # set :format, :pretty
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
