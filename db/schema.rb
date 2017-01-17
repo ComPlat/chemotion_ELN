6028a9eba9b36ec8967fcafc8ebfbb66ee6f6323
jpotthoff <jan.potthoff@kit.eud>
2017-01-17 13:20:04 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,6 @@
 ActiveRecord::Schema.define(version: 20170201113437) do
 
 
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
