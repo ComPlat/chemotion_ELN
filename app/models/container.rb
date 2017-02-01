afe1f20b53f061810307c78a34b0e36f59bf663c
jpotthoff <jan.potthoff@kit.eud>
2017-02-01 13:21:31 +0100

    anestry_cache disabled

--- app/models/container.rb
+++ app/models/container.rb
@@ -1,8 +1,8 @@
 class Container < ActiveRecord::Base
   belongs_to :element, :polymorphic => true
   has_many :attachments
-  has_ancestry :cache_depth => true
-  
+  has_ancestry
+
   #accepts_nested_attributes_for :container
   scope :ordered, -> { order("name ASC") }
 
