5062eb5b6c0165e004f6cbc9eb18ddba7d8b1856
jasonych99 <jasonych99@gmail.com>
2017-02-07 16:54:12 +0100

    querl only when elements selected

--- app/assets/javascripts/components/actions/PermissionActions.js
+++ app/assets/javascripts/components/actions/PermissionActions.js
@@ -3,14 +3,25 @@ import PermissionsFetcher from '../fetchers/PermissionsFetcher';
 
 class PermissionActions {
   fetchPermissionStatus(params) {
-    return (dispatch) => { PermissionsFetcher.fetchPermissionStatus(params)
-      .then((result) => {
-        dispatch(result);
-      }).catch((errorMessage) => {
-        console.log(errorMessage);
-      });};
-  }
+    const { sample, reaction, screen, wellplate } = params.elements_filter;
+    const hasAll = sample.all || reaction.all || screen.all || wellplate.all;
+    const hasIncludes = [...sample.included_ids,
+                          ...sample.included_ids,
+                          ...sample.included_ids,
+                          ...sample.included_ids].length > 0;
+    const nonEmpty = hasAll || hasIncludes;
 
+    return (dispatch) => {
+      if(nonEmpty) {
+        PermissionsFetcher.fetchPermissionStatus(params)
+          .then((result) => {
+            dispatch(result);
+          }).catch((errorMessage) => {
+            console.log(errorMessage);
+          });
+      };
+    };
+  }
 }
 
 export default alt.createActions(PermissionActions);
