4a3a82780210aa513f03ce2beebc5157e33c052b
jasonych99 <jasonych99@gmail.com>
2017-02-15 11:01:12 +0100

    restore origin PermissionActions.js

--- app/assets/javascripts/components/actions/PermissionActions.js
+++ app/assets/javascripts/components/actions/PermissionActions.js
@@ -3,25 +3,14 @@ import PermissionsFetcher from '../fetchers/PermissionsFetcher';
 
 class PermissionActions {
   fetchPermissionStatus(params) {
-    const { sample, reaction, screen, wellplate } = params.elements_filter;
-    const hasAll = sample.all || reaction.all || screen.all || wellplate.all;
-    const hasIncludes = [...sample.included_ids,
-                          ...sample.included_ids,
-                          ...sample.included_ids,
-                          ...sample.included_ids].length > 0;
-    const nonEmpty = hasAll || hasIncludes;
-
-    return (dispatch) => {
-      if(nonEmpty) {
-        PermissionsFetcher.fetchPermissionStatus(params)
-          .then((result) => {
-            dispatch(result);
-          }).catch((errorMessage) => {
-            console.log(errorMessage);
-          });
-      };
-    };
+    return (dispatch) => { PermissionsFetcher.fetchPermissionStatus(params)
+      .then((result) => {
+        dispatch(result);
+      }).catch((errorMessage) => {
+        console.log(errorMessage);
+      });};
   }
+
 }
 
 export default alt.createActions(PermissionActions);
