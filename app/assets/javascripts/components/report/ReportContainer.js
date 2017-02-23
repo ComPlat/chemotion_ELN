3d922a9ce94ccde321eec5be15fe1f5b25df73dc
jasonych99 <jasonych99@gmail.com>
2017-02-23 13:18:09 +0100

    fix warning

--- app/assets/javascripts/components/report/ReportContainer.js
+++ app/assets/javascripts/components/report/ReportContainer.js
@@ -174,7 +174,7 @@ export default class ReportContainer extends Component {
 
   fileNameRule() {
     return (
-      <Tooltip>
+      <Tooltip id="file-name-rule" >
         <p>Max 40 characters.</p>
         <p>allowed: a to z, A to Z, 0 to 9, -, _</p>
       </Tooltip>
