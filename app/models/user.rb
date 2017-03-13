8680bb361ac8f59fd2f1cf43919c0e291cb02b9a
PiTrem <pierre.tremouilhac@gmx.de>
2017-03-13 11:27:09 +0100

    user devise: add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   acts_as_paranoid
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :profile, dependent: :destroy
