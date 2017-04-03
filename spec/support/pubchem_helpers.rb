efdc367831b4d74fc4c920d784ca1ee2f47c81ad
jasonych99 <jasonych99@gmail.com>
2017-04-03 16:44:21 +0200

    improve CAS rspec helpers

--- spec/support/pubchem_helpers.rb
+++ spec/support/pubchem_helpers.rb
@@ -13,4 +13,22 @@ module PubchemHelpers
     }
     response_hash.to_json.to_s
   end
+
+  def xref_from_inchikey
+    response_hash = {
+      InformationList: {
+        Information: [
+          {
+            CID: 123456789,
+            RN: [
+              "123-456-789",
+              "987-654-321",
+              "558440-22-5"
+            ]
+          }
+        ]
+      }
+    }
+    response_hash.to_json.to_s
+  end
 end
