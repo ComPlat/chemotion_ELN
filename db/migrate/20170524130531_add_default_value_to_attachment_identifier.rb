ba985ad6c9774eac1a9eae3bac7a729e2f22a407
PiTrem <pierre.tremouilhac@gmx.de>
2017-05-24 17:38:23 +0200

    upg attachment table

---
+++ db/migrate/20170524130531_add_default_value_to_attachment_identifier.rb
@@ -0,0 +1,30 @@
+class AddDefaultValueToAttachmentIdentifier < ActiveRecord::Migration
+  def self.up
+    change_column :attachments, :identifier, 'uuid USING uuid(attachments.identifier)', default: "uuid_generate_v4()", uniq: true, null: true
+    change_column :attachments, :version, :integer, default: 0, null: true
+    change_column :attachments, :checksum, :string, null: true
+    change_column :attachments, :storage, :string, limit: 20, null: true, default: 'tmp'
+    change_column :attachments, :filename, :string, null: true
+
+    add_column :attachments, :content_type, :string
+    add_column :attachments, :bucket, :string
+    add_column :attachments, :key, :string , limit: 500
+    add_column :attachments, :thumb, :boolean, default: false
+  end
+
+  def self.down
+    change_column :attachments, :identifier, 'varchar(255) USING concat(attachments.identifier)',  null: false
+    change_column :attachments, :version, :integer, null: false
+    change_column :attachments, :checksum, :string, null: false
+    change_column :attachments, :storage, :string, null: false
+    change_column :attachments, :filename, :string, null: false
+    change_column_default :attachments, :version, nil
+    change_column_default :attachments, :storage, nil
+    change_column_default :attachments, :identifier, nil
+
+    remove_column :attachments, :content_type
+    remove_column :attachments, :bucket
+    remove_column :attachments, :key
+    remove_column :attachments, :thumb
+  end
+end
