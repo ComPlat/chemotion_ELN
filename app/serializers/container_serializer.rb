b4ba806f86f33ee25ad57cc4c05c6dad6b65aa59
jasonych99 <jasonych99@gmail.com>
2017-06-08 09:24:45 +0200

    add sample-analyses preview to serializer

--- app/serializers/container_serializer.rb
+++ app/serializers/container_serializer.rb
@@ -1,9 +1,8 @@
 class ContainerSerializer < ActiveModel::Serializer
   attributes :id, :name, :container_type, :description, :extended_metadata,
-    :children, :code_log
+    :children, :code_log, :preview_img
 
   has_many :attachments, :serializer => AttachmentSerializer
-
 
   def extended_metadata
     extended_metadata = object.extended_metadata
@@ -38,13 +37,16 @@ class ContainerSerializer < ActiveModel::Serializer
   def json_tree(attachments, containers)
       containers.map do |container, subcontainers|
         current_attachments = attachments.select{|attach| attach.container_id == container.id}
-        {:id => container.id,
-          :name => container.name,
-          :container_type => container.container_type,
-          :description => container.description,
-          :extended_metadata => get_extended_metadata(container),
-          :attachments => current_attachments,
-          :children => json_tree(attachments, subcontainers).compact}
+        {
+          id: container.id,
+          name: container.name,
+          container_type: container.container_type,
+          description: container.description,
+          extended_metadata: get_extended_metadata(container),
+          attachments: current_attachments,
+          preview_img: preview_img(container),
+          children: json_tree(attachments, subcontainers).compact
+        }
       end
   end
 
@@ -59,5 +61,15 @@ class ContainerSerializer < ActiveModel::Serializer
     end
 
     extended_metadata
+  end
+
+  def preview_img(container = object)
+    first_child = container.children.length > 0 && container.children[0]
+    has_dataset = first_child && first_child.container_type == "dataset"
+    if has_dataset
+      attachment = first_child.attachments[0]
+      preview = Storage.new.read_thumbnail(attachment)
+      return preview || "not available"
+    end
   end
 end
