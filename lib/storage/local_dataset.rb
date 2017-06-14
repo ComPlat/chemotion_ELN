fe2210caa640d30d19dfae430fa8423a33599709
PiTrem <pierre.tremouilhac@gmx.de>
2017-06-14 15:12:38 +0200

    minor storage fixes minor container impr

--- lib/storage/local_dataset.rb
+++ lib/storage/local_dataset.rb
@@ -1,18 +1,18 @@
 require 'storage'
 
 class LocalDataset < Local
-  #store files  in subdirectories named after the parent container_id
+  # store files  in subdirectories named after the parent container_id
 
   private
 
   def set_key
     attachment.key = File.join(
-      attachment.container_id,
+      attachment.container_id.to_s,
       attachment.filename
     )
   end
 
   def set_bucket
+    attachment.bucket = nil
   end
-
 end
