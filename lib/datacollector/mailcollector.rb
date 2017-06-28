f7eb73f4ebc1403b5c98e08d9f9c0042daf84787
jpotthoff <jan.potthoff@kit.eud>
2017-06-28 15:21:18 +0200

    code cleaning, bugs fixed

--- lib/datacollector/mailcollector.rb
+++ lib/datacollector/mailcollector.rb
@@ -12,48 +12,15 @@ class Mailcollector
   end
 
   def execute
-
     begin
       imap = Net::IMAP.new(@server, @port, @ssl)
       response = imap.login(@mail_address, @password)
       if response['name'] == "OK"
+        puts "Logged in....."
         imap.select('INBOX')
-
         imap.search(['NOT', 'SEEN']).each do |message_id|
-          envelope = imap.fetch(message_id, "ENVELOPE")[0].attr["ENVELOPE"]
-
-          if envelope.cc[0].mailbox
-            helper = CollectorHelper.new(envelope.from[0].mailbox.to_s + "@" + envelope.from[0].host.to_s,
-              envelope.cc[0].mailbox + "@" + envelope.cc[0].host)
-          else
-            helper = CollectorHelper.new(envelope.from[0].mailbox.to_s + "@" + envelope.from[0].host.to_s)
-          end
-
-          if helper.sender_recipient_known?
-            raw_message = imap.fetch(message_id, 'RFC822').first.attr['RFC822']
-            message = Mail.read_from_string raw_message
-            if message.multipart?
-              begin
-                dataset = helper.prepare_dataset(message.subject)
-                message.attachments.each do |attachment|
-                  a = Attachment.new(
-                    filename: attachment.filename,
-                    file_data: attachment.decoded,
-                    created_by: helper.sender.id,
-                    created_for: helper.recipient.id,
-                    content_type: attachment.mime_type
-                  )
-                  a.save!
-                  a.update!(container_id: dataset.id)
-                  primary_store = Rails.configuration.storage.primary_store
-                  a.update!(storage: primary_store)
-                end
-              end
-            rescue Exception => e
-              puts "ERROR: Cannot handle mail " + e.message
-              raise e.message
-            end
-          end
+          puts "Handle new mail " + message_id.to_s
+          handle_new_mail(message_id, imap)
           imap.store(message_id, "+FLAGS", [:Deleted])
         end
         imap.close
@@ -61,14 +28,50 @@ class Mailcollector
         puts "ERROR: Cannot login " + @server
         raise
       end
-    rescue Exception => e
-      puts "ERROR: Cannot handle new data mails " + e.message
-      raise e.message
     ensure
       imap.logout
       imap.disconnect
     end
-
   end
 
+private
+  def handle_new_mail(message_id, imap)
+    envelope = imap.fetch(message_id, "ENVELOPE")[0].attr["ENVELOPE"]
+    helper = getHelper(envelope)
+    if helper.sender_recipient_known?
+      raw_message = imap.fetch(message_id, 'RFC822').first.attr['RFC822']
+      message = Mail.read_from_string raw_message
+      if message.multipart?
+        puts "Handle new message..."
+        handle_new_message(message, helper)
+      end
+    end
+  end
+
+  def handle_new_message(message, helper)
+    dataset = helper.prepare_dataset(message.subject)
+    message.attachments.each do |attachment|
+      puts "Store attachment..."
+      a = Attachment.new(
+        filename: attachment.filename,
+        file_data: attachment.decoded,
+        created_by: helper.sender.id,
+        created_for: helper.recipient.id,
+        content_type: attachment.mime_type
+      )
+      a.save!
+      a.update!(container_id: dataset.id)
+      primary_store = Rails.configuration.storage.primary_store
+      a.update!(storage: primary_store)
+    end
+  end
+
+  def getHelper(envelope)
+    if envelope.cc
+      helper = CollectorHelper.new(envelope.from[0].mailbox.to_s + "@" + envelope.from[0].host.to_s,
+        envelope.cc[0].mailbox + "@" + envelope.cc[0].host)
+    else
+      helper = CollectorHelper.new(envelope.from[0].mailbox.to_s + "@" + envelope.from[0].host.to_s)
+    end
+  end
 end
