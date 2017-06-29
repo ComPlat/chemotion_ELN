fe4d533b7a95ef9a79c6882c0d2229b47a14b318
PiTrem <pierre.tremouilhac@gmx.de>
2017-06-29 15:22:52 +0200

    added view for affiliations list and editing  - user can add new affiliation, delete former ones  - user can edit timestamps from to
    affiliation from to timestamp editing view
    
    add affiliation link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,21 @@
-# TODO Welcoming logged in user; maybe removed later on
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  before_action :fetch_affiliations, only: [:affiliations, :update_affiliations]
 
-  def welcome
-  end
+  def heme; end
+
+  def welcome; end
 
   def update_user
     @user = current_user
     @user.counters['reactions'] = params[:reactions_count].to_i
     @user.reaction_name_prefix = params[:reaction_name_prefix]
     if @user.save
-      flash["success"] = "User settings is successfully saved!"
+      flash['success'] = 'User settings is successfully saved!'
       redirect_to root_path
     else
-      flash.now["danger"] = "Not saved! Please check input fields."
-      render "user"
+      flash.now['danger'] = 'Not saved! Please check input fields.'
+      render 'user'
     end
   end
 
@@ -25,29 +26,87 @@ class PagesController < ApplicationController
 
   def update_profiles
     @profile = current_user.profile
-    @profile.assign_attributes(profile_param)
+    @profile.assign_attributes(profile_params)
 
     if @profile.save
-      flash["success"] = "Profile is successfully saved!"
+      flash['success'] = 'Profile is successfully saved!'
       redirect_to root_path
     else
-      flash.now["danger"] = "Not saved! Please check input fields."
-      render "profile"
+      flash.now['danger'] = 'Not saved! Please check input fields.'
+      render 'profile'
     end
   end
 
   def groups
-    @groups = (current_user.groups+current_user.administrated_accounts.where(type: 'Group')).uniq.map{|g| GroupSerializer.new(g).serializable_hash.deep_stringify_keys}
+    @groups = (
+      current_user.groups + current_user.administrated_accounts
+                                        .where(type: 'Group')
+    ).uniq.map do |g|
+      GroupSerializer.new(g).serializable_hash.deep_stringify_keys
+    end
     @new_group = Group.new
-    @users = Person.all.map{|u| UserSerializer.new(u).serializable_hash.deep_stringify_keys }
+    @users = Person.all.map do |u|
+      UserSerializer.new(u).serializable_hash.deep_stringify_keys
+    end
   end
 
-  def home
+  def affiliations
+    @new_aff = current_user.affiliations.build
+    @new_aff.organization = Swot::school_name(current_user.email)
+  end
 
+  def create_affiliation
+    @affiliation = current_user.affiliations.build(affiliation_params)
+    if current_user.save!
+      flash['success'] = 'New affiliation saved!'
+    else
+      flash.now['danger'] = 'Not saved! Please check input fields.'
+    end
+    redirect_to pages_affiliations_path
+  end
+
+  def update_affiliations
+    cu_user_affiliations = current_user.user_affiliations
+    affiliations_params[:affiliations].each do |affiliation|
+      if affiliation.delete(:_destroy).blank?
+        @affiliations.find(affiliation[:id]).update!(affiliation)
+      else
+        cu_user_affiliations.find_by(
+          affiliation_id: affiliation[:id]
+        ).destroy!
+      end
+    end
+    redirect_to pages_affiliations_path
   end
 
   private
-  def profile_param
+
+  def fetch_affiliations
+    @affiliations = current_user.affiliations.order(
+      to: :desc, from: :desc, created_at: :desc
+    )
+  end
+
+  def affiliation_params
+    params.require(:affiliation).permit(
+      :id, :_destroy,
+      :country, :organization, :department, :group,
+      :from, :to, :from_month, :to_month
+    )
+  end
+
+  def affiliations_params
+    params.permit(
+      :utf8, :_method, :authenticity_token, :commit,
+      affiliations: [
+        :id, :_destroy,
+        # :country, :organization, :department, :group,
+        :from, :to, :from_month, :to_month
+      ]
+    )
+  end
+
+  def profile_params
     params.require(:profile).permit(:show_external_name)
   end
 end
--- app/models/user_affiliation.rb
+++ app/models/user_affiliation.rb
@@ -1,4 +1,5 @@
 class UserAffiliation < ActiveRecord::Base
+  acts_as_paranoid
   belongs_to :user
   belongs_to :affiliation
 end
