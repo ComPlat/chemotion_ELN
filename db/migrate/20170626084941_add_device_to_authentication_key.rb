8bb84f7adf0211752094a6b96f24e6b131c8bbf7
jpotthoff <jan.potthoff@kit.eud>
2017-07-05 10:16:59 +0200

    migrations merged

--- db/migrate/20170626084941_add_device_to_authentication_key.rb
+++
@@ -1,5 +0,0 @@
-class AddDeviceToAuthenticationKey < ActiveRecord::Migration
-  def change
-    add_column :authentication_keys, :device_id, :integer
-  end
-end
--- db/migrate/20170628101506_add_ip_to_authentication_key.rb
+++
@@ -1,5 +0,0 @@
-class AddIpToAuthenticationKey < ActiveRecord::Migration
-  def change
-    add_column :authentication_keys, :ip, :inet
-  end
-end
