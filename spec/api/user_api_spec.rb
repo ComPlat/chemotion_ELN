935e047a9b8e731e7ff584c1cb743232d75461ca
PiTrem <pierre.tremouilhac@gmx.de>
2017-08-07 14:32:27 +0200

    upd spec + rubocop

--- spec/api/user_api_spec.rb
+++ spec/api/user_api_spec.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 describe Chemotion::UserAPI do
   let(:json_options) {
     {
-      only: [:id,:is_templates_moderator,:type, :reaction_name_prefix],
+      only: [:id, :is_templates_moderator, :type, :reaction_name_prefix],
       methods: [:name, :initials]
     }
   }
-  let(:srlzr){
-    {'samples_count' => 0, 'reactions_count' => 0}
+  let(:srlzr) {
+    { 'samples_count' => 0, 'reactions_count' => 0 }
   }
-  let(:layout){
+  let(:layout) {
     {
       'sample' => '1',
       'reaction' => '2',
@@ -24,116 +25,152 @@ describe Chemotion::UserAPI do
     let!(:p1)  { create(:person, first_name: 'Jane', last_name: 'Doe') }
     let!(:p2)  { create(:person, first_name: 'John', last_name: 'Doe') }
     let!(:p3)  { create(:person, first_name: 'Jin',  last_name: 'Doe') }
-    let!(:g1)  { create(:group)}
-    let!(:g2)  { create(:group, admins: [p1], users:[p1,p2])}
-    let!(:g3)  { create(:group, admins: [p1])}
-    let!(:g4)  { create(:group, admins: [p2], users:[p2,p3])}
+    let!(:g1)  { create(:group) }
+    let!(:g2)  { create(:group, admins: [p1], users: [p1, p2]) }
+    let!(:g3)  { create(:group, admins: [p1]) }
+    let!(:g4)  { create(:group, admins: [p2], users: [p2, p3]) }
 
     before do
-      allow_any_instance_of(WardenAuthentication).to receive(:current_user).and_return(p1)
+      allow_any_instance_of(WardenAuthentication).to receive(:current_user)
+        .and_return(p1)
     end
 
     describe 'GET /api/v1/users/' do
       before do
-        get "/api/v1/users/"
+        get '/api/v1/users/'
       end
-      it "Returns all users" do
-        expect(JSON.parse(response.body)['users'].collect{|e| [e['id'],e['initials']]}).to match_array(User.all.pluck(:id, :name_abbreviation))
+      it 'Returns all users' do
+        expect(
+          JSON.parse(response.body)['users'].collect do |e|
+            [e['id'], e['initials']]
+          end
+        ).to match_array(
+          User.where(type: %w(Person Group)).pluck(:id, :name_abbreviation)
+        )
 
-      #  expect(JSON.parse(response.body)['users']).to match_array([
-      #    p1.as_json(json_options).merge(srlzr),
-      #    p2.as_json(json_options).merge(srlzr),
-      #    p3.as_json(json_options).merge(srlzr),
-      #    g1.as_json(json_options).merge(srlzr).merge({users: g1.users.map{|s|
-      #      UserSerializer.new(s).serializable_hash.deep_stringify_keys}}),
-      #    g2.as_json(json_options).merge(srlzr).merge({users: g2.users.map{|s|
-      #      UserSerializer.new(s).serializable_hash.deep_stringify_keys}}),
-      #    g3.as_json(json_options).merge(srlzr).merge({users: g3.users.map{|s|
-      #      UserSerializer.new(s).serializable_hash.deep_stringify_keys}}),
-      #    g4.as_json(json_options).merge(srlzr).merge({users: g4.users.map{|s|
-      #      UserSerializer.new(s).serializable_hash.deep_stringify_keys}})
-      #  ])
+        # expect(
+        #   JSON.parse(response.body)['users']
+        # ).to match_array(
+        #   [
+        #     p1.as_json(json_options).merge(srlzr),
+        #     p2.as_json(json_options).merge(srlzr),
+        #     p3.as_json(json_options).merge(srlzr),
+        #     g1.as_json(json_options).merge(srlzr).merge(
+        #       users: g1.users.map do |s|
+        #         UserSerializer.new(s).serializable_hash.deep_stringify_keys
+        #       end
+        #     ),
+        #     g2.as_json(json_options).merge(srlzr).merge(
+        #       users: g2.users.map do |s|
+        #         UserSerializer.new(s).serializable_hash.deep_stringify_keys
+        #       end
+        #     ),
+        #     g3.as_json(json_options).merge(srlzr).merge(
+        #       users: g3.users.map do |s|
+        #         UserSerializer.new(s).serializable_hash.deep_stringify_keys
+        #       end
+        #     ),
+        #     g4.as_json(json_options).merge(srlzr).merge(
+        #       users: g4.users.map do |s|
+        #         UserSerializer.new(s).serializable_hash.deep_stringify_keys
+        #       end
+        #     )
+        #   ]
+        # )
       end
     end
 
     describe 'GET /api/v1/users/current' do
       before do
-        get "/api/v1/users/current"
+        get '/api/v1/users/current'
       end
-      it "Returns current user" do
-        expect(JSON.parse(response.body)['user']).to eq p1.as_json(json_options).merge(srlzr).merge("layout" => layout)
+      it 'Returns current user' do
+        expect(JSON.parse(response.body)['user']).to(
+          eq p1.as_json(json_options).merge(srlzr).merge('layout' => layout)
+        )
       end
     end
 
     describe 'POST /api/v1/groups/create' do
-      let(:params){
-        { "group_param" => {
-            "first_name" => "My",
-            "last_name" =>  "Fanclub",
-            "email" =>      "jane.s@fan.club",
-          #  "password" => "dummypassword",
-          #  "password_confirmation" => "dummypassword",
-            "name_abbreviation" => "JFC",
-            "users" => [p2.id]
+      let(:params) {
+        {
+          'group_param' => {
+            'first_name' => 'My', 'last_name' => 'Fanclub',
+            'email' => 'jane.s@fan.club',
+            'name_abbreviation' => 'JFC', 'users' => [p2.id]
           }
         }
       }
       before do
-        post "/api/v1/groups/create", params
+        post '/api/v1/groups/create', params
       end
-      it "Creates a group of persons" do
-        expect(Group.where(email:"jane.s@fan.club",last_name:"Fanclub" , first_name:"My", name_abbreviation: "JFC")).to_not be_empty
-        expect(Group.where(email:"jane.s@fan.club").last.users.pluck :id).to match_array [p1.id, p2.id]
-        expect(Group.where(email:"jane.s@fan.club").last.admins).to_not be_empty
-        expect(Group.where(email:"jane.s@fan.club").last.admins.first).to eq p1
-        expect(p1.administrated_accounts.where(email:"jane.s@fan.club")).not_to be_empty
+      it 'Creates a group of persons' do
+        expect(
+          Group.where(
+            email: 'jane.s@fan.club', last_name: 'Fanclub',
+            first_name: 'My', name_abbreviation: 'JFC'
+          )
+        ).to_not be_empty
+        expect(
+          Group.where(email: 'jane.s@fan.club').last.users.pluck(:id)
+        ).to match_array [p1.id, p2.id]
+        expect(
+          Group.where(email: 'jane.s@fan.club').last.admins
+        ).to_not be_empty
+        expect(
+          Group.where(email: 'jane.s@fan.club').last.admins.first
+        ).to eq p1
+        expect(
+          p1.administrated_accounts.where(email: 'jane.s@fan.club')
+        ).not_to be_empty
       end
     end
 
     describe 'PUT /api/v1/groups/upd as a group admin' do
-      let(:params){
+      let(:params) {
         {
-          "rm_users"  => [p1.id,p2.id],
-          "add_users" => [p3.id]
+          'rm_users'  => [p1.id, p2.id],
+          'add_users' => [p3.id]
         }
       }
       before do
         put "/api/v1/groups/upd/#{g2.id}", params
       end
-      it "Updates a group of persons" do
-        expect(Group.find(g2.id).users.pluck :id).to match_array [p3.id]
+      it 'Updates a group of persons' do
+        expect(Group.find(g2.id).users.pluck(:id)).to match_array [p3.id]
       end
     end
 
     describe 'PUT /api/v1/groups/upd as a group admin (delete)' do
-      let(:params){
+      let(:params) {
         {
-          "destroy_group" => 'true'
+          'destroy_group' => 'true'
         }
       }
       before do
         put "/api/v1/groups/upd/#{g3.id}", params
       end
-      it "Deletes a group of persons" do
+      it 'Deletes a group of persons' do
         expect(Group.where(id: [g3.id])).to be_empty
         expect(Group.count).to eq(3)
       end
     end
 
     describe 'PUT /api/v1/groups/upd as a non group admin' do
-      let(:params){
+      let(:params) {
         {
-          "rm_users"  => [p2.id,p3.id],
-          "add_users" => [p1.id]
+          'rm_users'  => [p2.id, p3.id],
+          'add_users' => [p1.id]
         }
       }
       before do
         put "/api/v1/groups/upd/#{g4.id}", params
       end
-      it "Does not update a group of persons" do
-        expect(Group.find(g4.id).users.pluck :id).to match_array([p2.id,p3.id])
+      it 'Does not update a group of persons' do
+        expect(
+          Group.find(g4.id).users.pluck(:id)
+        ).to match_array([p2.id, p3.id])
       end
     end
-
   end
 end
