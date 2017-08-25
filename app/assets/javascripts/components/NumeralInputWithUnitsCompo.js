0d9c8deee764a9bc97ea0fe2fca172085aaa3d2c
An Nguyen <qn5043@kit.edu>
2017-08-25 17:06:32 +0200

    Add molarity UI
    - Only molarity and density can be used for amount calculation
    - Add "block" property to NumeralInputWithUnitsCompo which disable
    editing. Double click to enable edit.
    - Double click to enable molarity/density and disable the other one.
    - Update amount calculation based on molarity/density

--- app/assets/javascripts/components/NumeralInputWithUnitsCompo.js
+++ app/assets/javascripts/components/NumeralInputWithUnitsCompo.js
@@ -5,23 +5,22 @@ import {metPreConv,metPrefSymbols} from './utils/metricPrefix';
 export default class NumeralInputWithUnitsCompo extends Component {
   constructor(props) {
     super(props);
-    let {value, unit,metricPrefix,precision} = props;
+
+    const { value, block, unit, metricPrefix, precision } = props;
     this.state ={
-      unit: unit,
-      value: value,
+      unit,
+      value,
+      block,
       metricPrefix: metricPrefix || "none",
       currentPrecision: precision,
       valueString:  0,
-      showString: false
+      showString: false,
     };
   }
 
   componentWillReceiveProps(nextProps) {
-    let {value, unit} = nextProps;
-    this.setState({
-      unit: unit,
-      value: value,
-    });
+    const { value, unit, block } = nextProps;
+    this.setState({ unit, value, block });
   }
 
   _handleValueChange(value) {
@@ -31,65 +30,73 @@ export default class NumeralInputWithUnitsCompo extends Component {
   }
 
   _handleInputValueChange(event) {
+    const inputField = event.target;
+    const caretPosition = $(inputField).caret();
+    let { value } = inputField;
+    let { valueString } = this.state;
+    const { metricPrefix } = this.state;
+    const lastChar = value[caretPosition - 1] || '';
 
-    let inputField = event.target;
-    let caretPosition = $(inputField).caret();
-    let {value} = inputField;
-    let {metricPrefix,valueString} = this.state;
-    let lastChar =  value[caretPosition-1] || "";
+    if (lastChar !== '' && !lastChar.match(/-|\d|\.|(,)/)) return false;
 
-    if(lastChar != "" && !lastChar.match(/-|\d|\.|(,)/)) return false;
+    const md = lastChar.match(/-|\d/);
+    const mc = lastChar.match(/\.|(,)/);
 
-    let md = lastChar.match(/-|\d/);
-    let mc = lastChar.match(/\.|(,)/);
-
-    if (mc && mc[1]){value = value.slice(0,caretPosition-1)+'.'+value.slice(caretPosition)}
-    else if (value == '0.' || value == '00'){
+    if (mc && mc[1]) {
+      value = `${value.slice(0, caretPosition - 1)}.${value.slice(caretPosition)}`;
+    } else if (value === '0.' || value === '00') {
       value = '0.0';
     }
 
     value = value.replace('--', '');
-    let matchMinus = value.match(/\d+(\-+)\d*/);
-    if(matchMinus && matchMinus[1])
-    value = value.replace(matchMinus[1], '');
+    const matchMinus = value.match(/\d+(\-+)\d*/);
+    if (matchMinus && matchMinus[1]) value = value.replace(matchMinus[1], '');
 
-    if (md||mc){ valueString=value }
+    if (md || mc) valueString = value;
 
-    let val = metPreConv(value, metricPrefix, "none");
+    const val = metPreConv(value, metricPrefix, 'none');
     this.setState({
-        value:  val,
-        showString: true,
-        valueString: valueString
-      }, () => {
-        this._onChangeCallback();
-        $(inputField).caret(caretPosition);
-      }
-    );
-
+      value: val,
+      showString: true,
+      valueString,
+    }, () => {
+      this._onChangeCallback();
+      $(inputField).caret(caretPosition);
+    });
   }
 
   _handleInputValueFocus(){
+    const { value, metricPrefix } = this.state;
+
     this.setState({
       currentPrecision: undefined,
       showString: true,
-      valueString: metPreConv(this.state.value,"none",this.state.metricPrefix) || 0,
-      }, () => {this._onChangeCallback();}
-    );
+      valueString: metPreConv(value, 'none', metricPrefix) || 0,
+    }, () => {this._onChangeCallback()});
   }
 
-  _handleInputValueBlur(){
-     this.setState({
-        currentPrecision: this.props.precision,
-        showString: false
-      }, () => {this._onChangeCallback();}
-  );
+  _handleInputValueBlur() {
+    this.setState({
+      currentPrecision: this.props.precision,
+      showString: false,
+    }, () => this._onChangeCallback());
+  }
+
+  handleInputDoubleClick(event) {
+    if (this.state.block) {
+      this.setState({
+        block: false,
+        value: 0,
+      });
+    }
   }
 
   _onChangeCallback() {
-    if(this.props.onChange) {
+    if (this.props.onChange) {
       this.props.onChange(this.state);
     }
   }
+
   togglePrefix(){
     let {metricPrefixes} = this.props
     let ind = metricPrefixes.indexOf(this.state.metricPrefix)
@@ -102,10 +109,13 @@ export default class NumeralInputWithUnitsCompo extends Component {
   }
 
   render() {
-    let { bsSize, bsStyle, disabled,label} = this.props;
-    let {unit,showString, value,metricPrefix,currentPrecision,valueString} = this.state;
+    const { bsSize, bsStyle, disabled, label} = this.props;
+    let {
+      unit, showString, value, metricPrefix,
+      currentPrecision, valueString, block,
+    } = this.state;
     let mp = metPrefSymbols[metricPrefix];
-    let val = ()=>{
+    let val = () => {
       const nanOrInfinity = isNaN(value) || !isFinite(value)
       if (!showString && nanOrInfinity){
         return 'n.d.'
@@ -113,6 +123,7 @@ export default class NumeralInputWithUnitsCompo extends Component {
         return  metPreConv(value,"none",metricPrefix).toPrecision(currentPrecision);
       } else {return valueString}
     };
+    let inputDisabled = disabled ? true : block;
 
     let prefixSwitch;
     // BsStyle-s for Input and buttonAfter have differences
@@ -136,10 +147,15 @@ export default class NumeralInputWithUnitsCompo extends Component {
       return (
         <div>
           {labelWrap}
-          <InputGroup>
-            <FormControl type='text'
-              bsClass='bs-form--compact form-control'
-              disabled={disabled} bsSize={bsSize} bsStyle={bsStyle}
+          <InputGroup
+            onDoubleClick={(event)=>this.handleInputDoubleClick(event)}
+          >
+            <FormControl
+              type="text"
+              bsClass="bs-form--compact form-control"
+              disabled={inputDisabled}
+              bsSize={bsSize}
+              bsStyle={bsStyle}
               value={val() || ''}
               onChange={(event) => this._handleInputValueChange(event)}
               onFocus={(event) => this._handleInputValueFocus(event)}
@@ -153,14 +169,20 @@ export default class NumeralInputWithUnitsCompo extends Component {
       return(
         <div>
           {labelWrap}
-          <FormControl type='text'
-            bsClass='bs-form--compact form-control'
-            disabled={disabled} bsSize={bsSize} bsStyle={bsStyle}
-            value={val() || ''}
-            onChange={(event) => this._handleInputValueChange(event)}
-            onFocus={(event) => this._handleInputValueFocus(event)}
-            onBlur={(event)=>this._handleInputValueBlur(event)}
-          />
+          <div onDoubleClick={(event)=>this.handleInputDoubleClick(event)}>
+            <FormControl
+              type="text"
+              bsClass="bs-form--compact form-control"
+              disabled={inputDisabled}
+              bsSize={bsSize}
+              bsStyle={bsStyle}
+              value={val() || ''}
+              onChange={(event) => this._handleInputValueChange(event)}
+              onFocus={(event) => this._handleInputValueFocus(event)}
+              onBlur={(event)=>this._handleInputValueBlur(event)}
+              onDoubleClick={(event)=>this.handleInputDoubleClick(event)}
+            />
+          </div>
         </div>
       );
     }
@@ -185,6 +207,7 @@ NumeralInputWithUnitsCompo.defaultProps = {
   value: 0,
   units: [],
   disabled: false,
-  bsSize: "small",
-  bsStyle: "default"
+  block: false,
+  bsSize: 'small',
+  bsStyle: 'default',
 };
--- app/assets/javascripts/components/SampleForm.js
+++ app/assets/javascripts/components/SampleForm.js
@@ -12,31 +12,38 @@ export default class SampleForm extends React.Component {
 
     this.state = {
       sample: props.sample,
+      molarityBlocked: (props.sample.molarity_value || 0) <= 0,
     };
 
     this.handleFieldChanged = this.handleFieldChanged.bind(this);
   }
 
   handleAmountChanged(amount) {
-    let sample = this.state.sample;
-    sample.setAmountAndNormalizeToGram(amount);
+    const sample = this.state.sample;
+    // sample.setAmountAndNormalizeToGram(amount);
+    sample.setAmount(amount);
 
-    this.setState({
-      sample: sample,
-    });
+    this.setState({ sample });
   }
 
   handleMolarityChanged(molarity) {
-    let sample = this.state.sample;
-    this.setState({
-      sample: sample,
-    });
+    const sample = this.state.sample;
+    sample.setMolarity(molarity);
+
+    this.setState({ sample, molarityBlocked: false });
+  }
+
+  handleDensityChanged(density) {
+    const sample = this.state.sample;
+    sample.setDensity(density);
+
+    this.setState({ sample, molarityBlocked: true });
   }
 
   showStructureEditor() {
     this.props.parent.setState({
-      showStructureEditor: true
-    })
+      showStructureEditor: true,
+    });
   }
 
   structureEditorButton(isDisabled) {
@@ -45,28 +52,37 @@ export default class SampleForm extends React.Component {
         onClick={this.showStructureEditor}
         disabled={isDisabled}
       >
-        <Glyphicon glyph='pencil'/>
+        <Glyphicon glyph="pencil" />
       </Button>
-    )
+    );
   }
 
-  // Input components of sample details should be disabled if detail level does not allow to read their content
+  // Input components of sample details should be disabled if detail level
+  // does not allow to read their content
   topSecretCheckbox(sample) {
-    if(sample.can_update) {
+    if (sample.can_update) {
       return (
-        <Checkbox ref="topSecretInput" checked={sample.is_top_secret}
+        <Checkbox
+          ref="topSecretInput"
+          checked={sample.is_top_secret}
           onChange={(e) => this.handleFieldChanged(sample, 'is_top_secret', e.target.checked)}
-        >Top secret</Checkbox>
-      )
+        >
+          Top secret
+        </Checkbox>
+      );
     }
+
+    return (<span />);
   }
 
   moleculeInput(sample) {
     return (
-      <FormGroup style={{width: "100%"}}>
+      <FormGroup style={{ width: '100%' }}>
         <ControlLabel>Molecule</ControlLabel>
         <InputGroup>
-          <FormControl type="text" ref="moleculeInput"
+          <FormControl
+            type="text"
+            ref="moleculeInput"
             value={sample.molecule_name}
             disabled={!sample.can_update}
             readOnly={!sample.can_update}
@@ -77,7 +93,7 @@ export default class SampleForm extends React.Component {
           </InputGroup.Button>
         </InputGroup>
       </FormGroup>
-    )
+    );
   }
 
   handleFieldChanged(sample, field, e) {
@@ -85,6 +101,8 @@ export default class SampleForm extends React.Component {
       this.handleAmountChanged(e);
     } else if (/molarity/.test(field)) {
       this.handleMolarityChanged(e);
+    } else if (/density/.test(field)) {
+      this.handleDensityChanged(e);
     } else if (e && e.value) {
       // for numeric inputs
       sample[field] = e.value;
@@ -92,45 +110,50 @@ export default class SampleForm extends React.Component {
       sample[field] = e;
     }
 
-    this.props.parent.setState({
-      sample: sample
-    });
+    this.props.parent.setState({ sample });
   }
 
   textInput(sample, field, label, disabled = false) {
     return (
       <FormGroup>
         <ControlLabel>{label}</ControlLabel>
-        <FormControl type="text"
+        <FormControl
+          type="text"
           value={sample[field] || ''}
           onChange={(e) => {this.handleFieldChanged(sample, field, e.target.value)}}
           disabled={disabled || !sample.can_update}
           readOnly={disabled || !sample.can_update}
         />
       </FormGroup>
-    )
+    );
   }
 
   sampleSolvent(sample) {
     return (
-      <Select ref='solventInput' name='solvents' style={{marginBottom: "15px"}}
-              multi={false} options={solventOptions}
-              onChange={(e) => this.handleFieldChanged(sample, 'solvent', e)}
-              value={sample.solvent} disabled={!sample.can_update}
+      <Select
+        ref="solventInput"
+        id="solventInput"
+        name="solvents"
+        style={{ marginBottom: '15px' }}
+        multi={false}
+        options={solventOptions}
+        value={sample.solvent}
+        disabled={!sample.can_update}
+        onChange={(e) => this.handleFieldChanged(sample, 'solvent', e)}
       />
-    )
+    );
   }
 
   attachedAmountInput(sample, size) {
-    if(!sample.contains_residues)
-      return false;
+    if (!sample.contains_residues) return false;
 
     return this.numInput(sample, 'defined_part_amount', 'g',
-      ['milli', 'none'], 4, 'Attached', 'attachedAmountMg', true, "Weight of the defined part", size)
+      ['milli', 'none'], 4, 'Attached', 'attachedAmountMg',
+      true, 'Weight of the defined part');
   }
 
   numInput(sample, field, unit, prefixes, precision, label, ref = '',
-    disabled = false, title = '', size = 2, notApplicable = false) {
+    disabled = false, title = '', block = false, notApplicable = false) {
     if (sample.contains_residues && unit === 'l') return false;
     const value = !isNaN(sample[field]) ? sample[field] : null;
 
@@ -142,128 +165,147 @@ export default class SampleForm extends React.Component {
           label={label}
           ref={ref}
           metricPrefix={prefixes[0]}
-          metricPrefixes = {prefixes}
+          metricPrefixes={prefixes}
           precision={precision}
           title={title}
           disabled={disabled}
+          block={block}
           onChange={(e) => this.handleFieldChanged(sample, field, e)}
         />
       </td>
-    )
+    );
   }
 
   sampleAmount(sample) {
-    let content = [];
+    const content = [];
     const isDisabled = !sample.can_update;
+    const {molarityBlocked} = this.state;
+
     if (sample.isMethodDisabled('amount_value') === false) {
-      if (sample.isMethodRestricted('molecule') === true) {
-        content.push(
-          this.numInput(sample, 'amount_g', 'g', ['milli', 'none'], 4, 'Amount', 'massMgInput', isDisabled, '')
-        )
-      } else {
-        content.push(
-          this.numInput(sample, 'amount_g', 'g', ['milli', 'none'], 4, 'Amount', 'massMgInput', isDisabled, '')
-        )
+      // if (sample.isMethodRestricted('molecule') === true) {
+      //   content.push(this.numInput(sample, 'amount_g', 'g', ['milli', 'none'],
+      //     4, 'Amount', 'massMgInput', isDisabled, ''));
+      // } else {
+      content.push(this.numInput(sample, 'amount_g', 'g', ['milli', 'none'],
+        4, 'Amount', 'massMgInput', isDisabled, ''));
 
-        if(!sample.contains_residues)
-          content.push(
-            this.numInput(sample, 'amount_l', 'l', ['milli', 'micro', 'none'], 5, '\u202F', 'l', isDisabled, '')
-          )
-
-        content.push(
-          this.numInput(sample, 'amount_mol', 'mol', ['milli', 'none'], 4, '\u202F', 'amountInput', isDisabled, '')
-        )
-
-        if (sample.contains_residues) {
-          content.push(
-            this.attachedAmountInput(sample)
-          )
-        }
+      if (!sample.contains_residues) {
+        content.push(this.numInput(sample, 'amount_l', 'l',
+          ['milli', 'micro', 'none'], 5, '\u202F', 'l', isDisabled, ''));
       }
-     return content;
-    } else {
-      return (
-        <FormGroup>
-          <ControlLabel>Amount</ControlLabel>
-          <FormControl type="text" disabled defaultValue="***" readOnly/>
-        </FormGroup>
-      )
+
+      content.push(this.numInput(sample, 'amount_mol', 'mol',
+        ['milli', 'none'], 4, '\u202F', 'amountInput', isDisabled, ''));
+
+      if (sample.contains_residues) {
+        content.push(this.attachedAmountInput(sample));
+      }
+
+      return content;
     }
+
+    return (
+      <FormGroup>
+        <ControlLabel>Amount</ControlLabel>
+        <FormControl type="text" disabled defaultValue="***" readOnly />
+      </FormGroup>
+    );
   }
 
   sampleDescription(sample) {
     return (
       <FormGroup>
         <ControlLabel>Description</ControlLabel>
-        <FormControl componentClass="textarea"  ref="descriptionInput"
-             placeholder={sample.description}
-             value={sample.description || ''}
-             onChange={(e) => this.handleFieldChanged(sample, 'description', e.target.value)}
-             rows={2}
-             disabled={!sample.can_update}
+        <FormControl
+          componentClass="textarea"
+          ref="descriptionInput"
+          placeholder={sample.description}
+          value={sample.description || ''}
+          onChange={(e) => this.handleFieldChanged(sample, 'description', e.target.value)}
+          rows={2}
+          disabled={!sample.can_update}
         />
       </FormGroup>
-    )
+    );
   }
 
   render() {
-    let sample = this.state.sample || {}
-    let isPolymer = sample.molfile.indexOf(" R# ") !== -1
+    const sample = this.state.sample || {};
+    const isPolymer = sample.molfile.indexOf(' R# ') !== -1;
     const isDisabled = !sample.can_update;
     const polyDisabled = isPolymer || isDisabled;
+    const molarityBlocked = isDisabled ? true : this.state.molarityBlocked;
+    const densityBlocked = isDisabled ? true : !molarityBlocked;
 
     return (
       <Table responsive className="sample-form">
-        <tr>
-          <td colSpan="4">
-            <div style={{display: "flex", justifyContent: "space-between"}}>
-              <div style={{width: "82%"}}>
-                {this.moleculeInput(sample)}
+        <tbody>
+          <tr>
+            <td colSpan="4">
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ width: '82%' }}>
+                  {this.moleculeInput(sample)}
+                </div>
+                <div style={{ width: '15%' }} className="top-secret-checkbox">
+                  {this.topSecretCheckbox(sample)}
+                </div>
               </div>
-              <div style={{width: "15%"}} className="top-secret-checkbox">
-                {this.topSecretCheckbox(sample)}
-              </div>
-            </div>
-          </td>
-        </tr>
+            </td>
+          </tr>
 
-        <tr>
-          <td colSpan="4">
-            <div className="name-form">
-              <div style={{width: "12%"}}>
-                {this.textInput(sample, 'name', 'Name')}
+          <tr>
+            <td colSpan="4">
+              <div className="name-form">
+                <div style={{ width: '20%' }}>
+                  {this.textInput(sample, 'name', 'Name')}
+                </div>
+                <div style={{ width: '20%' }}>
+                  {this.textInput(sample, 'external_label', 'External label')}
+                </div>
+                <div style={{ width: '19%' }}>
+                  {this.textInput(sample, 'location', 'Location')}
+                </div>
+                <div style={{ width: '40%' }}>
+                  <label htmlFor="solventInput">Solvent</label>
+                  {this.sampleSolvent(sample)}
+                </div>
               </div>
-              <div style={{width: "20%"}}>
-                {this.textInput(sample, 'external_label', 'External label')}
-              </div>
-              <div style={{width: "14%"}}>
-                {this.textInput(sample, 'location', 'Location')}
-              </div>
-              <div  style={{width: "50%"}}>
-                <label>Solvent</label>
-                {this.sampleSolvent(sample)}
-              </div>
-            </div>
-          </td>
-        </tr>
+            </td>
+          </tr>
 
-        <tr className="visible-hd">
-          {this.sampleAmount(sample)}
-          {this.numInput(sample, 'boiling_point', '°C', ['none'], 5, 'Boiling point', '', polyDisabled, '', 2, isPolymer)}
-        </tr>
+          <tr className="visible-hd">
+            {this.sampleAmount(sample)}
+            {
+              this.numInput(sample, 'boiling_point', '°C', ['none'], 5,
+                'Boiling point', '', polyDisabled, '', false, isPolymer)
+            }
+          </tr>
 
-        <tr>
-          {this.numInput(sample, 'density', 'g/ml', ['none'], 5, 'Density', '', polyDisabled, '', 2, isPolymer)}
-          {this.numInput(sample, 'molarity_value', 'M', ['milli', 'none'], 5, 'Molarity', '',  polyDisabled, '', 2, isPolymer)}
-          {this.numInput(sample, 'purity', 'none', ['none'], 5, 'Purity', '', isDisabled)}
-          {this.numInput(sample, 'melting_point', '°C', ['none'], 5, 'Melting point', '',  polyDisabled, '', 2, isPolymer)}
-        </tr>
+          <tr>
+            {
+              this.numInput(sample, 'density', 'g/ml', ['none'], 5,
+                'Density', '', polyDisabled, '', densityBlocked, isPolymer)
+            }
+            {
+              this.numInput(sample, 'molarity_value', 'M', ['milli', 'none'],
+                5, 'Molarity', '', polyDisabled, '', molarityBlocked, isPolymer)
+            }
+            {
+              this.numInput(sample, 'purity', 'none', ['none'], 5,
+                'Purity', '', isDisabled)
+            }
+            {
+              this.numInput(sample, 'melting_point', '°C', ['none'], 5,
+                'Melting point', '', polyDisabled, '', false, isPolymer)
+            }
+          </tr>
 
-        <tr style={{'paddingTop': "15px"}}>
-          <td colSpan="4">{this.sampleDescription(sample)}</td>
-        </tr>
+          <tr style={{ paddingTop: '15px' }}>
+            <td colSpan="4">{this.sampleDescription(sample)}</td>
+          </tr>
+        </tbody>
       </Table>
-    )
+    );
   }
 }
 
