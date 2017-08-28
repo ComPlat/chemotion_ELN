05e81f32071b86e6ed3b743c2021b73b2ada02bf
An Nguyen <qn5043@kit.edu>
2017-08-28 15:44:28 +0200

    Update rspec for molarity value with unit

--- spec/factories/samples.rb
+++ spec/factories/samples.rb
@@ -4,6 +4,9 @@ FactoryGirl.define do
     sequence(:name) { |i| "Sample #{i}" }
     target_amount_value 100
     target_amount_unit "mg"
+    molarity_value 1.0
+    molarity_unit "mM"
+    density 0.0
 
   #  molfile "\n  Ketcher 05301616272D 1   1.00000     0.00000     0\n\n  2  1  0     0  0            999 V2000\n    1.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  1  0     0  0\nM  END\n"
     callback(:before_create) do |sample|
--- spec/features/sample_management_spec.rb
+++ spec/features/sample_management_spec.rb
@@ -47,7 +47,8 @@ feature 'Sample management' do
       amount = sample.target_amount_value.to_s
       expect(find_bs_field('Amount').value).to eq(amount)
 
-      molarity = sample.molarity_value.to_s
+      molarity_float = sample.molarity_value * (sample.molarity_unit === 'M' ? 1 : 1000)
+      molarity = molarity.to_s
       expect(find_bs_field('Molarity').value.to_f.to_s).to eq(molarity)
 
       # test read-only molecule data
