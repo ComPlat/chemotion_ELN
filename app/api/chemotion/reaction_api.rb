4dcae73792aa041fca5fd82f9441681b05e044b3
An Nguyen <qn5043@kit.edu>
2017-09-01 11:32:42 +0200

    Copied reaction products name
    Copied reaction's products should take new reaction short_label as name

--- app/api/chemotion/reaction_api.rb
+++ app/api/chemotion/reaction_api.rb
@@ -420,6 +420,11 @@ module Chemotion
         CollectionsReaction.create(reaction: reaction, collection: Collection.get_all_collection_for_user(current_user.id))
 
         if reaction
+          materials.products&.map! do |prod|
+            prod.name.gsub! attributes['origin'].short_label, reaction.short_label
+            prod
+          end
+
           update_materials_for_reaction(reaction, materials, current_user)
           update_literatures_for_reaction(reaction, literatures)
           reaction.reload
