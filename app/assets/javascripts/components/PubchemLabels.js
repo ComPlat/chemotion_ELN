1d6495eac09706ecf628439f2502c61a2fce89a3
PiTrem <pierre.tremouilhac@gmx.de>
2017-09-06 13:19:19 +0200

    refactor PubchemTag should fix Tag wrongly set to clickable

--- app/assets/javascripts/components/PubchemLabels.js
+++ app/assets/javascripts/components/PubchemLabels.js
@@ -1,49 +1,29 @@
 import React from 'react';
-import SVG from 'react-inlinesvg';
+import PropTypes from 'prop-types';
 
-export default class PubchemLabels extends React.Component {
-  constructor(props) {
-    super(props)
-
-    let {element} = props
-    this.has_pubchem = true
-    if (!element.pubchem_tag || !element.pubchem_tag.pubchem_cid)
-      this.has_pubchem = false
-
-    this.handleOnClick = this.handleOnClick.bind(this)
-  }
-
-  handleOnClick(e) {
-    let {element} = this.props
-
-    if (this.has_pubchem) {
-      let cid = element.pubchem_tag.pubchem_cid
-
+const PubchemLabels = ({element}) =>{
+  let cid = element.pubchem_tag && element.pubchem_tag.pubchem_cid
+  let labelStyle = {
+    display: 'inline-block',
+    marginLeft: "5px",
+    marginRight: "5px"
+  };
+  if (!cid) {labelStyle.WebkitFilter = "grayscale(100%)"}
+  const handleOnClick = (e) => {
+    if (!!cid){
       window.open("https://pubchem.ncbi.nlm.nih.gov/compound/" + cid, '_blank')
     }
-
     e.stopPropagation()
   }
-
-  render() {
-    let {element, color} = this.props
-
-    let pubchemLogo = "/images/wild_card/pubchem.svg"
-    let pubchem = (
-      <img src={pubchemLogo} className="pubchem-logo" />
-    )
-
-    let labelStyle = {
-      display: 'inline-block',
-      marginLeft: "5px",
-      marginRight: "5px"
-    }
-    if (this.has_pubchem == false) labelStyle.WebkitFilter = "grayscale(100%)"
-
-    return (
-      <span style={labelStyle} onClick={this.handleOnClick}>
-        {pubchem}
-      </span>
-    )
-  }
+  return (
+    <span style={labelStyle} onClick={handleOnClick}>
+      <img src="/images/wild_card/pubchem.svg" className="pubchem-logo" />
+    </span>
+  )
 }
+
+PubchemLabels.propTypes = {
+  element: PropTypes.object,
+}
+
+export default PubchemLabels;
