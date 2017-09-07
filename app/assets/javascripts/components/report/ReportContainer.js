d0c84fd51ded08755c551f128416796d0f312a26
jasonych99 <jasonych99@gmail.com>
2017-09-07 14:18:22 +0200

    lint & move functions in ReportContainer / Config / Setting

--- app/assets/javascripts/components/report/ReportContainer.js
+++ app/assets/javascripts/components/report/ReportContainer.js
@@ -1,10 +1,7 @@
-import React, {Component} from 'react'
-import {Panel, Button, Tabs, Tab, Row, Col, FormGroup, ControlLabel,
-        FormControl, Tooltip, OverlayTrigger } from 'react-bootstrap';
-import _ from 'lodash';
+import React, { Component } from 'react';
+import { Panel, Tabs, Tab } from 'react-bootstrap';
 import ReportActions from '../actions/ReportActions';
 import ReportStore from '../stores/ReportStore';
-import UIActions from '../actions/UIActions';
 import UIStore from '../stores/UIStore';
 import Setting from './Setting';
 import Previews from './Previews';
@@ -19,37 +16,33 @@ export default class ReportContainer extends Component {
     super(props);
     this.state = {
       ...ReportStore.getState(),
-    }
+    };
     this.onChange = this.onChange.bind(this);
     this.onChangeUI = this.onChangeUI.bind(this);
     this.panelHeader = this.panelHeader.bind(this);
-    this.fileNameRule = this.fileNameRule.bind(this);
-    this.toggleConfigs = this.toggleConfigs.bind(this);
-    this.toggleConfigsAll = this.toggleConfigsAll.bind(this);
-    this.handleImgFormatChanged = this.handleImgFormatChanged.bind(this);
     this.updateQueue = this.updateQueue.bind(this);
   }
 
   componentDidMount() {
     ReportStore.listen(this.onChange);
     UIStore.listen(this.onChangeUI);
-    let state = UIStore.getState();
+    const state = UIStore.getState();
     this.onChangeUI(state);
     ReportActions.getArchives.defer();
   }
 
   componentWillUnmount() {
-    ReportStore.unlisten(this.onChange)
-    UIStore.unlisten(this.onChangeUI)
+    ReportStore.unlisten(this.onChange);
+    UIStore.unlisten(this.onChangeUI);
   }
 
   onChange(state) {
-    this.setState({...state})
+    this.setState({ ...state });
   }
 
   onChangeUI(state) {
-    const newTags = { sampleIds: state['sample'].checkedIds.toArray(),
-                      reactionIds: state['reaction'].checkedIds.toArray() };
+    const newTags = { sampleIds: state.sample.checkedIds.toArray(),
+      reactionIds: state.reaction.checkedIds.toArray() };
     const oldTags = this.state.selectedObjTags;
     const defaultTags = this.state.defaultObjTags;
     ReportActions.updateCheckedTags.defer(oldTags, newTags, defaultTags);
@@ -70,115 +63,6 @@ export default class ReportContainer extends Component {
     return <PanelHeader title="Report Generation" btns={btns} />;
   }
 
-  render() {
-    const { splSettings, checkedAllSplSettings,
-            rxnSettings, checkedAllRxnSettings,
-            configs, checkedAllConfigs,
-            selectedObjs, archives, activeKey,
-            imgFormat, fileName, template } = this.state;
-    return (
-      <Panel header={this.panelHeader()}
-             bsStyle="default">
-
-        <Tabs activeKey={activeKey}
-              onSelect={this.selectTab}
-              id="report-tabs" >
-          <Tab eventKey={0} title={"Config"}>
-            <Config
-              imgFormat={imgFormat}
-              fileName={fileName}
-              configs={configs}
-              checkedAllConfigs={checkedAllConfigs}
-              template={template}
-              fileNameRule={this.fileNameRule}
-              toggleConfigs={this.toggleConfigs}
-              toggleConfigsAll={this.toggleConfigsAll}
-              handleImgFormatChanged={this.handleImgFormatChanged}
-              handleTemplateChanged={this.handleTemplateChanged}
-            />
-          </Tab>
-
-          <Tab eventKey={1} title={"Setting"}>
-            <Setting
-              template={template}
-              splSettings={splSettings}
-              toggleSplSettings={this.toggleSplSettings}
-              toggleSplSettingsAll={this.toggleSplSettingsAll}
-              checkedAllSplSettings={checkedAllSplSettings}
-              rxnSettings={rxnSettings}
-              toggleRxnSettings={this.toggleRxnSettings}
-              toggleRxnSettingsAll={this.toggleRxnSettingsAll}
-              checkedAllRxnSettings={checkedAllRxnSettings} />
-          </Tab>
-
-          <Tab eventKey={2} title={"Order"}>
-            <div className="panel-fit-screen">
-              <Orders selectedObjs={selectedObjs} template={template} />
-            </div>
-          </Tab>
-
-          <Tab eventKey={3} title={"Preview"}>
-            <div className="panel-fit-screen">
-              <Previews selectedObjs={selectedObjs}
-                         splSettings={splSettings}
-                         rxnSettings={rxnSettings}
-                         configs={configs}
-                         template={template} />
-            </div>
-          </Tab>
-
-          <Tab eventKey={4} title={this.archivesTitle()}>
-            <div className="panel-fit-screen">
-              <Archives archives={archives} />
-            </div>
-          </Tab>
-        </Tabs>
-
-      </Panel>
-    );
-  }
-
-  fileNameRule() {
-    return (
-      <Tooltip id="file-name-rule" >
-        <p>Max 40 characters.</p>
-        <p>allowed: a to z, A to Z, 0 to 9, -, _</p>
-      </Tooltip>
-    );
-  }
-
-  toggleSplSettings(text, checked){
-    ReportActions.updateSplSettings({text, checked})
-  }
-
-  toggleRxnSettings(text, checked){
-    ReportActions.updateRxnSettings({text, checked})
-  }
-
-  toggleConfigs(text, checked){
-    ReportActions.updateConfigs({text, checked})
-  }
-
-  toggleSplSettingsAll() {
-    ReportActions.toggleSplSettingsCheckAll()
-  }
-
-  toggleRxnSettingsAll() {
-    ReportActions.toggleRxnSettingsCheckAll()
-  }
-
-  toggleConfigsAll() {
-    ReportActions.toggleConfigsCheckAll()
-  }
-
-  handleImgFormatChanged(e) {
-    ReportActions.updateImgFormat(e.value);
-  }
-
-  handleTemplateChanged(e) {
-    ReportActions.updateTemplate(e.value);
-  }
-
   selectTab(key) {
     ReportActions.updateActiveKey(key);
   }
@@ -186,18 +70,18 @@ export default class ReportContainer extends Component {
   archivesTitle() {
     const unreadIds = this.unreadIds();
     const unReadBadge = unreadIds.length > 0
-      ? <span className='badge-danger'>{unreadIds.length}</span>
+      ? <span className="badge-danger">{unreadIds.length}</span>
       : null;
 
-    return(
+    return (
       <span>Archive {unReadBadge}</span>
     );
   }
 
   unreadIds() {
     let ids = [];
-    this.state.archives.forEach( a => {
-      if(a.unread) {
+    this.state.archives.forEach((a) => {
+      if (a.unread) {
         ids = [...ids, a.id];
       }
     });
@@ -209,5 +93,65 @@ export default class ReportContainer extends Component {
     if (processings.length > 0) {
       ReportActions.updateProcessQueue.defer(processings);
     }
+  }
+
+  render() {
+    const { splSettings, checkedAllSplSettings, archives, activeKey,
+      rxnSettings, checkedAllRxnSettings, imgFormat, fileName, template,
+      configs, checkedAllConfigs, selectedObjs } = this.state;
+    return (
+      <Panel
+        header={this.panelHeader()}
+        bsStyle="default"
+      >
+        <Tabs
+          activeKey={activeKey}
+          onSelect={this.selectTab}
+          id="report-tabs"
+        >
+          <Tab eventKey={0} title={'Config'}>
+            <Config
+              imgFormat={imgFormat}
+              fileName={fileName}
+              configs={configs}
+              checkedAllConfigs={checkedAllConfigs}
+              template={template}
+              handleTemplateChanged={this.handleTemplateChanged}
+            />
+          </Tab>
+          <Tab eventKey={1} title={'Setting'}>
+            <Setting
+              template={template}
+              splSettings={splSettings}
+              checkedAllSplSettings={checkedAllSplSettings}
+              rxnSettings={rxnSettings}
+              checkedAllRxnSettings={checkedAllRxnSettings}
+            />
+          </Tab>
+
+          <Tab eventKey={2} title={'Order'}>
+            <div className="panel-fit-screen">
+              <Orders selectedObjs={selectedObjs} template={template} />
+            </div>
+          </Tab>
+          <Tab eventKey={3} title={'Preview'}>
+            <div className="panel-fit-screen">
+              <Previews
+                selectedObjs={selectedObjs}
+                splSettings={splSettings}
+                rxnSettings={rxnSettings}
+                configs={configs}
+                template={template}
+              />
+            </div>
+          </Tab>
+          <Tab eventKey={4} title={this.archivesTitle()}>
+            <div className="panel-fit-screen">
+              <Archives archives={archives} />
+            </div>
+          </Tab>
+        </Tabs>
+      </Panel>
+    );
   }
 }
--- app/assets/javascripts/components/report/Setting.js
+++ app/assets/javascripts/components/report/Setting.js
@@ -1,30 +1,54 @@
 import React from 'react';
 import { Panel } from 'react-bootstrap';
+import ReportActions from '../actions/ReportActions';
 import CheckBoxs from '../common/CheckBoxs';
 
-const Setting = ({splSettings, toggleSplSettings, toggleSplSettingsAll,
-                  checkedAllSplSettings, rxnSettings, toggleRxnSettings,
-                  toggleRxnSettingsAll, checkedAllRxnSettings, template}) => {
+const toggleSplSettings = (text, checked) => {
+  ReportActions.updateSplSettings({ text, checked });
+};
+
+const toggleSplSettingsAll = () => {
+  ReportActions.toggleSplSettingsCheckAll();
+};
+
+const toggleRxnSettings = (text, checked) => {
+  ReportActions.updateRxnSettings({ text, checked });
+};
+
+const toggleRxnSettingsAll = () => {
+  ReportActions.toggleRxnSettingsCheckAll();
+};
+
+const Setting = ({splSettings, checkedAllSplSettings, rxnSettings,
+  checkedAllRxnSettings, template}) => {
   return (
-    template === "supporting_information"
+    template === 'supporting_information'
       ? <h5>Supporting Infomation has predefined settings.</h5>
       : <div>
-          <Panel header="Sample"
-                  bsStyle="default">
-            <CheckBoxs  items={splSettings}
-                        toggleCheckbox={toggleSplSettings}
-                        toggleCheckAll={toggleSplSettingsAll}
-                        checkedAll={checkedAllSplSettings} />
-          </Panel>
-          <Panel header="Reaction"
-                  bsStyle="default">
-            <CheckBoxs  items={rxnSettings}
-                        toggleCheckbox={toggleRxnSettings}
-                        toggleCheckAll={toggleRxnSettingsAll}
-                        checkedAll={checkedAllRxnSettings} />
-          </Panel>
-        </div>
+        <Panel
+          header="Sample"
+          bsStyle="default"
+        >
+          <CheckBoxs
+            items={splSettings}
+            toggleCheckbox={toggleSplSettings}
+            toggleCheckAll={toggleSplSettingsAll}
+            checkedAll={checkedAllSplSettings}
+          />
+        </Panel>
+        <Panel
+          header="Reaction"
+          bsStyle="default"
+        >
+          <CheckBoxs
+            items={rxnSettings}
+            toggleCheckbox={toggleRxnSettings}
+            toggleCheckAll={toggleRxnSettingsAll}
+            checkedAll={checkedAllRxnSettings}
+          />
+        </Panel>
+      </div>
   );
-}
+};
 
 export default Setting;
