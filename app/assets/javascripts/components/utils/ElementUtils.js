aab9c67bb308e82af0431322bf9a692b349a8b19
An Nguyen <qn5043@kit.edu>
2017-09-12 13:43:04 +0200

    Sample analyses auto format
    Auto format sample analyses content using Delta

--- app/assets/javascripts/components/utils/ElementUtils.js
+++ app/assets/javascripts/components/utils/ElementUtils.js
@@ -29,4 +29,219 @@ const UrlSilentNavigation = (element) => {
   }
 };
 
-module.exports = { SameEleTypId, UrlSilentNavigation };
+const sampleAnalysesFormatPattern = {
+  _13cnmr: [
+    {
+      pattern: '13C',
+      replace: {
+        ops: [
+          { insert: '13', attributes: { script: 'sup' } },
+          { insert: 'C' },
+        ],
+      },
+    },
+    {
+      pattern: '(\\d+) C',
+      replace: {
+        ops: [
+          { insert: '#{1}C' },
+        ],
+      },
+    },
+    {
+      pattern: 'J=',
+      replace: {
+        ops: [
+          { insert: 'J', attributes: { italic: true } },
+          { insert: '=' },
+        ],
+      },
+    },
+    {
+      pattern: 'J=(\\d+)',
+      replace: {
+        ops: [
+          { insert: 'J', attributes: { italic: true } },
+          { insert: ' =#{1}' },
+        ],
+      },
+    },
+    {
+      pattern: 'J =(\\d+)',
+      replace: {
+        ops: [
+          { insert: 'J', attributes: { italic: true } },
+          { insert: ' = #{1}' },
+        ],
+      },
+    },
+    {
+      pattern: '\\) , ',
+      replace: {
+        ops: [
+          { insert: '),' },
+        ],
+      },
+    },
+    {
+      pattern: '\\) (\\d+)',
+      replace: {
+        ops: [
+          { insert: '), #{1}' },
+        ],
+      },
+    },
+    {
+      pattern: '(\\d+),(\\d+)',
+      replace: {
+        ops: [
+          { insert: '#{1}.#{2}' },
+        ],
+      },
+    },
+  ],
+  _1hnmr: [
+    {
+      pattern: '1H',
+      replace: {
+        ops: [
+          { insert: '1', attributes: { script: 'sup' } },
+          { insert: 'H' },
+        ],
+      },
+    },
+    {
+      pattern: '(\\d+) H',
+      replace: {
+        ops: [
+          { insert: '#{1}H' },
+        ],
+      },
+    },
+    {
+      pattern: 'J=',
+      replace: {
+        ops: [
+          { insert: 'J', attributes: { italic: true } },
+          { insert: '=' },
+        ],
+      },
+    },
+    {
+      pattern: 'J=(\\d+)',
+      replace: {
+        ops: [
+          { insert: 'J', attributes: { italic: true } },
+          { insert: ' =#{1}' },
+        ],
+      },
+    },
+    {
+      pattern: 'J =(\\d+)',
+      replace: {
+        ops: [
+          { insert: 'J', attributes: { italic: true } },
+          { insert: ' = #{1}' },
+        ],
+      },
+    },
+    {
+      pattern: '\\) , ',
+      replace: {
+        ops: [
+          { insert: '),' },
+        ],
+      },
+    },
+    {
+      pattern: '\\) (\\d+)',
+      replace: {
+        ops: [
+          { insert: '), #{1}' },
+        ],
+      },
+    },
+    {
+      pattern: '(\\d+),(\\d+)',
+      replace: {
+        ops: [
+          { insert: '#{1}.#{2}' },
+        ],
+      },
+    },
+  ],
+  _ea: [
+    {
+      pattern: '(\\d),(\\d{2})',
+      replace: {
+        ops: [
+          { insert: '#{1}.#{2}' },
+        ],
+      },
+    },
+    {
+      pattern: 'C (\\d{2})',
+      replace: {
+        ops: [
+          { insert: 'C, #{1}' },
+        ],
+      },
+    },
+    {
+      pattern: 'H (\\d{2})',
+      replace: {
+        ops: [
+          { insert: 'H, #{1}' },
+        ],
+      },
+    },
+    {
+      pattern: 'N (\\d{2})',
+      replace: {
+        ops: [
+          { insert: 'N, #{1}' },
+        ],
+      },
+    },
+    {
+      pattern: 'O (\\d{2})',
+      replace: {
+        ops: [
+          { insert: 'O, #{1}' },
+        ],
+      },
+    },
+    {
+      pattern: 'S (\\d{2})',
+      replace: {
+        ops: [
+          { insert: 'S, #{1}' },
+        ],
+      },
+    },
+    {
+      pattern: '(\\d\\.\\d\\d),',
+      replace: {
+        ops: [
+          { insert: '#{1};' },
+        ],
+      },
+    },
+  ],
+  _mass: [
+    {
+      pattern: 'm/z',
+      replace: {
+        ops: [
+          { insert: 'm/z', attributes: { italic: true } },
+        ],
+      },
+    },
+  ],
+};
+
+module.exports = {
+  SameEleTypId,
+  UrlSilentNavigation,
+  sampleAnalysesFormatPattern,
+};
--- app/assets/javascripts/components/utils/quillFormat.js
+++ app/assets/javascripts/components/utils/quillFormat.js
@@ -1,3 +1,5 @@
+import Delta from 'quill-delta';
+
 const rmRedundantSpaceBreak = (target) => {
   target.replace(/\n/g, '').replace(/\s\s+/g, ' ');
 };
@@ -20,9 +22,64 @@ const frontBreak = (content) => {
   return res;
 };
 
+const mapGroupRegexToDelta = (content, matchedGroup) => {
+  let newContent = content;
+  matchedGroup.forEach((m, idx) => {
+    newContent = newContent.map((d) => {
+      const patt = `#{${idx + 1}}`;
+      const insertString = d.insert;
+      d.insert = insertString.replace(patt, m);
+      return d;
+    });
+  });
+
+  return newContent;
+};
+
+const searchAndReplace = (contents, pattern, regexReplace) => {
+  const contentsDelta = new Delta(contents);
+
+  const replacedContents = contentsDelta.map((content) => {
+    if (typeof content.insert !== 'string') return content;
+
+    const contentDelta = new Delta([content]);
+    const regexMatch = new RegExp(pattern, 'g');
+
+    let replaced = new Delta();
+    let cur = 0;
+
+    let matched = regexMatch.exec(content.insert);
+    while (matched) {
+      const l = matched[0].length;
+      const retain = matched.index - cur;
+      const change = new Delta().retain(retain).delete(l);
+      cur = matched.index + l;
+
+      const groupRegex = new RegExp(pattern, 'g');
+      let mappedReplace = regexReplace.ops;
+
+      let matchedGroup = groupRegex.exec(content.insert);
+      while (matchedGroup) {
+        const group = matchedGroup.slice(1);
+        mappedReplace = mapGroupRegexToDelta(mappedReplace, group);
+        matchedGroup = groupRegex.exec(content.insert);
+      }
+
+      replaced = replaced.concat(change.concat(new Delta(mappedReplace)));
+      matched = regexMatch.exec(content.insert);
+    }
+
+    return contentDelta.compose(replaced).ops;
+  });
+
+  const newDelta = replacedContents.reduce((acc, cur) => acc.concat(cur), []);
+  return new Delta(newDelta);
+};
+
 module.exports = {
   rmDeltaRedundantSpaceBreak,
   rmOpsRedundantSpaceBreak,
   rmRedundantSpaceBreak,
   frontBreak,
+  searchAndReplace,
 };
