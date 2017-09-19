30eb8633f3639342af106968325c8988cf49b3b1
PiTrem <pierre.tremouilhac@gmx.de>
2017-09-19 14:42:21 +0200

    delayed job init changed conditions on config

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -9,12 +9,13 @@
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 
 if Rails.env.production? && Delayed::Job.column_names.include?('cron')
-  if Rails.configuration.datamailcollector
+  if Rails.configuration.respond_to?(:datamailcollector)
     CollectDataFromMailJob.set(cron: '*/15 * * * *').perform_later if Delayed::Job.where("handler like ?", "%CollectDataFromMailJob%").empty?
   else
     Delayed::Job.where("handler like ?", "%CollectDataFromMailJob%").destroy_all
   end
-  if Rails.configuration.datacollector
+
+  if Rails.configuration.respond_to?(:datacollector)
     CollectDataFromDevicesJob.set(cron: '*/5 * * * *').perform_later if Delayed::Job.where("handler like ?", "%CollectDataFromDevicesJob%").empty?
   else
     Delayed::Job.where("handler like ?", "%CollectDataFromDevicesJob%").destroy_all
