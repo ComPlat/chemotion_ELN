7c0f360678d125ee2b077b87a6e011d44ea3c8d5
an.nguyen <an.nguyen@kit.edu>
2017-11-22 14:57:13 +0100

    Update chromedriver for RSpec

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,8 +1,8 @@
 module LoginMacros
   def sign_in(user)
     visit root_path
-    fill_in 'email', with: user.email
-    fill_in 'password', with: user.password
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
     find('button[type="submit"]').click
   end
 end
