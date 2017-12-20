7547b7ff6de700668d37520ee23e9a6be1d621e9
jasonych99 <jasonych99@gmail.com>
2017-12-20 12:09:18 +0100

    lint javascripts/components/report/Setting.js

--- app/assets/javascripts/components/report/Setting.js
+++ app/assets/javascripts/components/report/Setting.js
@@ -19,36 +19,28 @@ const toggleRxnSettingsAll = () => {
   ReportActions.toggleRxnSettingsCheckAll();
 };
 
-const Setting = ({splSettings, checkedAllSplSettings, rxnSettings,
-  checkedAllRxnSettings, template}) => {
-  return (
-    template === 'supporting_information'
-      ? <h5>Supporting Infomation has predefined settings.</h5>
-      : <div>
-        <Panel
-          header="Sample"
-          bsStyle="default"
-        >
-          <CheckBoxs
-            items={splSettings}
-            toggleCheckbox={toggleSplSettings}
-            toggleCheckAll={toggleSplSettingsAll}
-            checkedAll={checkedAllSplSettings}
-          />
-        </Panel>
-        <Panel
-          header="Reaction"
-          bsStyle="default"
-        >
-          <CheckBoxs
-            items={rxnSettings}
-            toggleCheckbox={toggleRxnSettings}
-            toggleCheckAll={toggleRxnSettingsAll}
-            checkedAll={checkedAllRxnSettings}
-          />
-        </Panel>
-      </div>
-  );
-};
+const Setting = ({ splSettings, checkedAllSplSettings, rxnSettings,
+  checkedAllRxnSettings, template }) => (
+  template === 'supporting_information'
+    ? <h5>Supporting Infomation has predefined settings.</h5>
+    : <div>
+      <Panel header="Sample" bsStyle="default">
+        <CheckBoxs
+          items={splSettings}
+          toggleCheckbox={toggleSplSettings}
+          toggleCheckAll={toggleSplSettingsAll}
+          checkedAll={checkedAllSplSettings}
+        />
+      </Panel>
+      <Panel header="Reaction" bsStyle="default">
+        <CheckBoxs
+          items={rxnSettings}
+          toggleCheckbox={toggleRxnSettings}
+          toggleCheckAll={toggleRxnSettingsAll}
+          checkedAll={checkedAllRxnSettings}
+        />
+      </Panel>
+    </div>
+);
 
 export default Setting;
