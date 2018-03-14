67fc036842e4bcabc5121099aff144c04ec720eb
PiTrem <pierre.tremouilhac@gmx.de>
2018-03-14 12:30:37 +0100

    upg App.js: remove jquery selection  (vanilla js) + lint
    upg Home.js: remove jquery selection  (vanilla js) + lint
    
    NumeralInput.js: remove jquery selection + lint
    
    LineChart.js: comment out jquery selection

--- app/assets/javascripts/libHome/Home.js
+++ app/assets/javascripts/libHome/Home.js
@@ -1,47 +1,44 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
-import { Grid, Row} from 'react-bootstrap';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Grid, Row } from 'react-bootstrap';
 
 import Navigation from '../components/Navigation'
 import XHome from '../components/extra/HomeXHome'
 
+const extraHomes = () => {
+  const homes = [];
+  const count = XHome.count || 0;
+  for (let j = 0; j < count; j += 1) {
+    homes.push(XHome[`content${j}`]);
+  }
+  return homes;
+};
 
 class Home extends Component {
   constructor(props) {
     super();
-
   }
-
-  extraHomes(){
-    let homes = []
-    let count = XHome.count || 0
-    for (let j=0;j < count;j++){
-      homes.push(XHome["content"+j])
-    }
-    return homes;
-  }
-
 
   render() {
-    return (<div>
-      { XHome.count && XHome.count > 0
-        ?  this.extraHomes().map((Home,i)=><Home key={'home'+i}/>)
-        :  <Grid fluid>
-          <Row className="card-navigation">
-            <Navigation/>
-          </Row>
-          <Row className="card-content">
-          </Row>
-        </Grid>
-      }
+    return (
+      <div>
+        { XHome.count && XHome.count > 0
+          ? extraHomes().map((Annex, i) => <Annex key={`Annex_${i}`} />)
+          : <Grid fluid>
+            <Row className="card-navigation">
+              <Navigation />
+            </Row>
+            <Row className="card-content">
+            </Row>
+          </Grid>
+        }
       </div>
-    )
+    );
   }
 }
 
-$(document).ready(function() {
-  let domElement = document.getElementById('Home');
-  if (domElement){
-    ReactDOM.render(<Home />, domElement);
-  }
+// $(document).ready(function() {
+document.addEventListener('DOMContentLoaded', () => {
+  const domElement = document.getElementById('Home');
+  if (domElement) { ReactDOM.render(<Home />, domElement); }
 });
