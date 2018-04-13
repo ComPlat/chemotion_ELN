4c2bc5a31e17720ea9f39d6f95d93971716cf6eb
PiTrem <pierre.tremouilhac@gmx.de>
2018-04-13 17:16:01 +0200

    suggestion_api: FE encodeURIComponent of query and BE route params by element type
    fix sugestion search of queries with special characters

--- app/assets/javascripts/components/fetchers/SuggestionsFetcher.js
+++ app/assets/javascripts/components/fetchers/SuggestionsFetcher.js
@@ -1,10 +1,9 @@
 import 'whatwg-fetch';
 
 export default class SuggestionsFetcher {
-  static fetchSuggestionsForCurrentUser(elementType, query, collectId,
-    isSync = false) {
+  static fetchSuggestionsForCurrentUser(elementType, query, collectId, isSync = false) {
     return fetch(
-      `/api/v1/suggestions/${elementType}/${query}.json?collection_id=${collectId}&is_sync=${isSync}`,
+      `/api/v1/suggestions/${elementType}?query=${encodeURIComponent(query)}&collection_id=${collectId}&is_sync=${isSync}`,
       { credentials: 'same-origin' }
     ).then(response => response.json())
       .then(json => json.suggestions)
