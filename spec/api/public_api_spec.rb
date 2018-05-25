7634461c093548f501c51285b086fb89b9f45643
PiTrem <pierre.tremouilhac@gmx.de>
2018-05-25 15:39:08 +0200

    user account soft deletion reset email properly

--- spec/api/public_api_spec.rb
+++ spec/api/public_api_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe Chemotion::PublicAPI do
   describe ' GET /api/v1/public/ping' do
     before { get('/api/v1/public/ping','', 'AUTHORIZATION' => 'Bearer qwerty') }
-    it 'responds 200' do
-      expect(response.status).to eq 200
+    it 'responds 204' do
+      expect(response.status).to eq 204
     end
   end
 end
