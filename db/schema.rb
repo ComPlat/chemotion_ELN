3a4bd965c9c646b8ebe5eb5b31583d626f05c3ac
Paggy <pei-chi.huang@kit.edu>
2018-08-07 15:47:10 +0200

    add function changes

--- db/schema.rb
+++ db/schema.rb
@@ -765,20 +765,6 @@ ActiveRecord::Schema.define(version: 20180709180000) do
 
   add_foreign_key "literals", "literatures"
 
-
- execute "CREATE OR REPLACE FUNCTION collection_shared_names(user_id integer, collection_id integer)
- RETURNS json
- LANGUAGE sql
- AS $$
-   select array_to_json(array_agg(row_to_json(result))) from (
-   SELECT sync_collections_users.id, users.type,users.first_name || chr(32) || users.last_name as name,sync_collections_users.permission_level
-   FROM sync_collections_users
-   INNER JOIN users ON users.id = sync_collections_users.user_id AND users.deleted_at IS NULL
-   WHERE sync_collections_users.shared_by_id = $1 and sync_collections_users.collection_id = $2
-   group by  sync_collections_users.id,users.type,users.name_abbreviation,users.first_name,users.last_name,sync_collections_users.permission_level
-   ) as result
- $$"
-
  execute "CREATE OR REPLACE FUNCTION user_ids(user_id integer)
   RETURNS TABLE(user_ids integer)
   LANGUAGE sql
@@ -881,4 +867,18 @@ ActiveRecord::Schema.define(version: 20180709180000) do
 
            return query select coalesce(i_detail_level_sample,0) detail_level_sample, coalesce(i_detail_level_wellplate,0) detail_level_wellplate;
        end;$function$;"
+
+     execute "CREATE OR REPLACE FUNCTION collection_shared_names(user_id integer, collection_id integer)
+      RETURNS json
+      LANGUAGE sql
+      AS $$
+      select array_to_json(array_agg(row_to_json(result))) from (
+      SELECT sync_collections_users.id, users.type,users.first_name || chr(32) || users.last_name as name,sync_collections_users.permission_level,
+      sync_collections_users.reaction_detail_level,sync_collections_users.sample_detail_level,sync_collections_users.screen_detail_level,sync_collections_users.wellplate_detail_level
+      FROM sync_collections_users
+      INNER JOIN users ON users.id = sync_collections_users.user_id AND users.deleted_at IS NULL
+      WHERE sync_collections_users.shared_by_id = $1 and sync_collections_users.collection_id = $2
+      group by  sync_collections_users.id,users.type,users.name_abbreviation,users.first_name,users.last_name,sync_collections_users.permission_level
+      ) as result
+      $$"
 end
