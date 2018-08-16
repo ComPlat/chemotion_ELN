e6c6d6e73598f3e0e9c0370aefa89bc316cc79c4
Paggy <pei-chi.huang@kit.edu>
2018-08-16 15:15:14 +0200

    change to update profile only

--- db/migrate/20180816161600_migrate_layout_to_profile_int.rb
+++ db/migrate/20180816161600_migrate_layout_to_profile_int.rb
@@ -1,24 +1,10 @@
 class MigrateLayoutToProfileInt < ActiveRecord::Migration
-  def up
-    User.where(type: 'Person').find_each do |user|
-      profile = Profile.find_by(user_id: user.id)
-      next if profile.nil?
-      node = user.layout
-      node.each{|k, v| node[k] = v.to_i }
-      layout = {}
-      layout['layout'] = node
-      profile.data = layout || {} if profile.data.nil?
-      profile.data.merge!(layout) unless profile.data.nil?
-      profile.save!
+  def change
+    Profile.find_each do |pr|
+      d = pr.data || {}
+      l = d['layout']
+      l.each_pair{|k, v| l[k] = v.to_i } if l
+      pr.update!(data: d)
     end
   end
-
-  def down
-    Profile.find_each do |profile|
-      profile.data.delete_if {|key, value| key >= "layout" }  unless (profile.data.nil?)
-      profile.save!
-    end
-  end
-
-
 end
