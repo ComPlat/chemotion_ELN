1cae8520f18e8646e0b87f1b5824987c104163a8
Pierre Tremouilhac <pierre.tremouilhac@kit.edu>
2018-08-21 14:12:49 +0200

    Resolve "collection management tab listing"

--- app/assets/javascripts/components/CollectionManagement.js
+++ app/assets/javascripts/components/CollectionManagement.js
@@ -1,53 +1,38 @@
 import React from 'react';
-import {Tabs, Tab} from 'react-bootstrap';
+import { Tabs, Tab } from 'react-bootstrap';
 
 import MyCollections from './collection_management/MyCollections';
 import MySharedCollections from './collection_management/MySharedCollections';
 import SharedWithMeCollections from './collection_management/SharedWithMeCollections';
 import SyncWithMeCollections from './collection_management/SyncWithMeCollections';
-import XTabs from "./extra/CollectionManagementXTabs";
+import XTabs from './extra/CollectionManagementXTabs';
 
-export default class CollectionManagement extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  extraTab(ind,offset=0){
-    const num = ind + offset + 1 ;
-    let NoName =  XTabs[`content${ind}`];
-    const Title = XTabs[`title${ind}`];
-    return(
-       <Tab eventKey={num} key={num} title={Title} >
-           <NoName />
-       </Tab>
-      )
-  }
-
-  render() {
-    const tabContents = []
-    for (let j=0;j < XTabs.count;j++){
-      if (XTabs['on'+j]()){
-        tabContents.push((i)=>this.extraTab(i,2))
-      }
+const CollectionManagement = () => {
+  const tabContents = [
+    <Tab eventKey={0} key={0} title="My Collections"><MyCollections /></Tab>,
+    <Tab eventKey={1} key={1} title="My Shared Collections"><MySharedCollections /></Tab>,
+    <Tab eventKey={2} key={2} title="Collections shared with me "><SharedWithMeCollections /></Tab>,
+    <Tab eventKey={3} key={3} title="Collections synchronized with me "><SyncWithMeCollections /></Tab>,
+  ];
+  const offset = tabContents.length;
+  for (let j = 0; j < XTabs.count; j += 1) {
+    if (XTabs[`on${j}`]()) {
+      const NoName = XTabs[`content${j}`];
+      tabContents.push((
+        <Tab eventKey={offset + j} key={offset + j} title={XTabs[`title${j}`]} >
+          <NoName />
+        </Tab>
+      ));
     }
-    return (
-      <div id="collection-management">
-        <Tabs defaultActiveKey={1} id="collection-management-tab">
-          <Tab eventKey={1} title="My Collections">
-            <MyCollections />
-          </Tab>
-          <Tab eventKey={2} title="My Shared Collections">
-            <MySharedCollections />
-          </Tab>
-          <Tab eventKey={3} title="Collections shared with me ">
-            <SharedWithMeCollections />
-          </Tab>
-          <Tab eventKey={4} title="Collections synchronized with me ">
-            <SyncWithMeCollections />
-          </Tab>
-          {tabContents.map((e,i)=>e(i))}
-        </Tabs>
-      </div>
-    )
   }
-}
+
+  return (
+    <div id="collection-management">
+      <Tabs defaultActiveKey={0} id="collection-management-tab">
+        {tabContents.map(e => e)}
+      </Tabs>
+    </div>
+  );
+};
+
+export default CollectionManagement;
