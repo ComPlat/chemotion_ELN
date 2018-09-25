c87e5fa1e3fe494ef7f04eefa53829668e53f6ee
PiTrem <pierre.tremouilhac@gmx.de>
2018-09-25 16:43:03 +0200

    upg react-inlinesvg to 0.8.1
    upg react-qr-reader tp 2.1.0x
    
    upg node to 8.12.0 LTS
    
    react-dnd to 2.6.0
    
    upg alt-utils to 2.0.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 set :bundle_jobs, 4 # parallel bundler
 
 set :nvm_type, :user
-set :nvm_node, File.exist?('.nvmrc') && File.read('.nvmrc').strip  || 'v6.10.2'
+set :nvm_node, File.exist?('.nvmrc') && File.read('.nvmrc').strip  || 'v8.12.0'
 set :npm_version, File.exist?('.npm-version') && File.read('.npm-version').strip  || '6.4.1'
 set :nvm_map_bins, fetch(:nvm_map_bins, []).push('rake')
 
