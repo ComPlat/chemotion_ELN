5a71842a9c65cef6b3511d7094a9e7c0e8f65912
PiTrem <pierre.tremouilhac@gmx.de>
2018-10-25 12:19:29 +0200

    webdriver set window size
    double screen width

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,15 @@ require 'rails_helper'
 Capybara.register_driver :selenium do |app|
   http_client = Selenium::WebDriver::Remote::Http::Default.new
   http_client.read_timeout = 200
-  Capybara::Selenium::Driver.new(app, browser: :chrome, :http_client => http_client)
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--window-size=2048,768")
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    http_client: http_client,
+    options: options
+  )
 end
 
 hostname = 'http://pubchem.ncbi.nlm.nih.gov'
