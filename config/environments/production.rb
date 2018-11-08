c998f0434ed46e4e0017fdb6eb9b2e80ac4d6c30
PiTrem <pierre.tremouilhac@gmx.de>
2018-11-08 15:47:01 +0100

    enable sprocket uglifier (np compress, no mangle)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = Uglifier.new(harmony: true, mangle: false, compress: false)
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
