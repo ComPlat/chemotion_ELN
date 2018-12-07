4cc91508d93cff00c00a413964d8c27a1ecbb0d4
jasonych99 <jasonych99@gmail.com>
2018-12-07 16:14:16 +0100

    spectra-viewer: move out edit logics

--- lib/chemotion/jcamp.rb
+++ lib/chemotion/jcamp.rb
@@ -95,71 +95,35 @@ module Chemotion
   # process Jcamp files
   module Jcamp
     # Create module
-    module EditUtil
-      def self.generate_peaks_table(peaks)
-        content = peaks.map.with_index { |p, idx|
-          "(#{p[:x]}, #{p[:y]}, <#{idx + 1}>)"
-        }
-        content.join("\n")
-      end
-
-      def self.block_npoints(peaks)
-        "##NPOINTS=#{peaks.length}\n"
-      end
-
-      def self.edit_jcamp(peaks, orig_att)
-        separator = "$$ --- CHEMSPECTRA EDIT CONTENT ---\n"
-        peak_assign = "##PEAK ASSIGNMENTS=(XYA)\n"
-        peaks_table = generate_peaks_table(peaks)
-        parts = orig_att
-                .read_file
-                .encode('UTF-8', undef: :replace, replace: '')
-                .split(separator)
-        content = [
-          parts[0], separator, peak_assign,
-          peaks_table, "\n", block_npoints(peaks),
-          separator, parts[-1]
-        ].join('')
-        Util.generate_tmp_file(content)
-      end
-    end
-  end
-end
-
-# Chemotion module
-module Chemotion
-  # process Jcamp files
-  module Jcamp
-    # Create module
     module Edit
       include HTTParty
 
-      def self.stub_peak_in_img(path)
+      def self.stub_edit_zip_jcamp_n_img(path, peaks_str)
         response = nil
         url = Rails.configuration.spectra.url
         port = Rails.configuration.spectra.port
         File.open(path, 'r') do |f|
           response = HTTParty.post(
-            "http://#{url}:#{port}/peak_in_image",
+            "http://#{url}:#{port}/edit_zip_jcamp_n_img",
             body: {
               multipart: true,
-              file: f
+              file: f,
+              peaks_str: peaks_str
             }
           )
         end
         response
       end
 
-      def self.edit_img(tmp_jcamp)
-        rsp = stub_peak_in_img(tmp_jcamp.path)
-        rsp_io = StringIO.new(rsp.body.to_s)
-        Util.extract_zip(rsp_io)[1]
+      def self.to_coord_string(peaks)
+        peaks.map { |p| "#{p['x']},#{p['y']}" }.join('#')
       end
 
-      def self.spectrum_peaks_edit(peaks, orig_att)
-        tmp_jcamp = EditUtil.edit_jcamp(peaks, orig_att)
-        tmp_img = edit_img(tmp_jcamp)
-        [tmp_jcamp, tmp_img]
+      def self.spectrum_peaks_edit(path, peaks)
+        peaks_str = to_coord_string(peaks)
+        rsp = stub_edit_zip_jcamp_n_img(path, peaks_str)
+        rsp_io = StringIO.new(rsp.body.to_s)
+        Util.extract_zip(rsp_io)
       end
     end
   end
