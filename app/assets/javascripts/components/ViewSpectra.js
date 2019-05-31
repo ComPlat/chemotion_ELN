8c7c5b93ee0586df5ecdbcd263be053db9fb846d
jasonych99 <jasonych99@gmail.com>
2019-05-31 15:19:31 +0200

    adjust ChemSpectr modal management

--- app/assets/javascripts/components/ViewSpectra.js
+++ app/assets/javascripts/components/ViewSpectra.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SpectraViewer, FN } from 'react-spectra-viewer';
-import { Modal, Well } from 'react-bootstrap';
+import { Modal, Well, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 import LoadingActions from './actions/LoadingActions';
@@ -17,9 +17,11 @@ class ViewSpectra extends React.Component {
     };
 
     this.onChange = this.onChange.bind(this);
-    this.onCloseModal = this.onCloseModal.bind(this);
     this.writeOp = this.writeOp.bind(this);
+    this.writeCloseOp = this.writeCloseOp.bind(this);
     this.saveOp = this.saveOp.bind(this);
+    this.saveCloseOp = this.saveCloseOp.bind(this);
+    this.closeOp = this.closeOp.bind(this);
     this.predictOp = this.predictOp.bind(this);
     this.buildOpsByLayout = this.buildOpsByLayout.bind(this);
     this.renderSpectraViewer = this.renderSpectraViewer.bind(this);
@@ -37,10 +39,6 @@ class ViewSpectra extends React.Component {
   onChange(newState) {
     const origState = this.state;
     this.setState({ ...origState, ...newState });
-  }
-
-  onCloseModal() {
-    SpectraActions.ToggleModal();
   }
 
   opsSolvent(shift) {
@@ -133,7 +131,28 @@ class ViewSpectra extends React.Component {
       predict,
       handleSubmit,
     );
-    // SpectraActions.ToggleModal.defer();
+  }
+
+  closeOp() {
+    SpectraActions.ToggleModal.defer();
+  }
+
+  writeCloseOp({
+    peaks, shift, scan, thres, analysis, layout, isAscend, decimal,
+  }) {
+    this.writeOp({
+      peaks, shift, scan, thres, analysis, layout, isAscend, decimal,
+    });
+    this.closeOp();
+  }
+
+  saveCloseOp({
+    peaks, shift, scan, thres, analysis,
+  }) {
+    this.saveOp({
+      peaks, shift, scan, thres, analysis,
+    });
+    this.closeOp();
   }
 
   predictOp({
@@ -149,28 +168,33 @@ class ViewSpectra extends React.Component {
   }
 
   buildOpsByLayout(et) {
+    const baseOps = [
+      { name: 'write', value: this.writeOp },
+      { name: 'write & close', value: this.writeCloseOp },
+      { name: 'save', value: this.saveOp },
+      { name: 'save & close', value: this.saveCloseOp },
+      { name: 'close without save', value: this.closeOp },
+    ];
     const predictable = ['MS', 'INFRARED'].indexOf(et.spectrum.sTyp) < 0;
     if (predictable) {
       return [
-        { name: 'write', value: this.writeOp },
-        { name: 'save', value: this.saveOp },
+        ...baseOps,
         { name: 'predict', value: this.predictOp },
       ];
     }
-    return [
-      { name: 'write', value: this.writeOp },
-      { name: 'save', value: this.saveOp },
-    ];
+    return baseOps;
   }
 
   renderEmpty() {
     const { fetched } = this.state;
     const content = fetched
       ? (
-        <Well>
+        <Well onClick={this.closeOp}>
           <i className="fa fa-exclamation-triangle fa-3x" />
           <h3>No Spectra Found!</h3>
           <h3>Please refresh the page!</h3>
+          <br />
+          <h5>Click here to close the window...</h5>
         </Well>
       )
       : <i className="fa fa-refresh fa-spin fa-3x fa-fw" />;
@@ -186,10 +210,12 @@ class ViewSpectra extends React.Component {
     const { fetched } = this.state;
     const content = fetched
       ? (
-        <Well>
+        <Well onClick={this.closeOp}>
           <i className="fa fa-chain-broken fa-3x" />
           <h3>Invalid spectrum!</h3>
           <h3>Please delete it and upload a valid file!</h3>
+          <br />
+          <h5>Click here to close the window...</h5>
         </Well>
       )
       : <i className="fa fa-refresh fa-spin fa-3x fa-fw" />;
@@ -237,12 +263,23 @@ class ViewSpectra extends React.Component {
       <div className="spectra-viewer">
         <Modal
           show={showModal}
-          onHide={this.onCloseModal}
           dialogClassName="spectra-viewer-dialog"
           animation
         >
-          <Modal.Header closeButton>
-            <Modal.Title>{ modalTitle }</Modal.Title>
+          <Modal.Header>
+            <Modal.Title>
+              { modalTitle }
+              <Button
+                bsStyle="danger"
+                bsSize="small"
+                className="button-right"
+                onClick={this.closeOp}
+              >
+                <span>
+                  <i className="fa fa-trash" /> Close without Save
+                </span>
+              </Button>
+            </Modal.Title>
           </Modal.Header>
           {
             showModal && jcamp
