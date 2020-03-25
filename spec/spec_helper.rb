ce8f16a85f100440d965748ffd6c828cd75c7cad
PiTrem <pierre.tremouilhac@gmx.de>
2020-03-25 17:31:07 +0100

    headless chrome disable shared memory (skip unitrb)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,11 @@ Capybara.register_driver :selenium do |app|
   )
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--window-size=2048,768')
-  # options.add_argument('--disable-gpu')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+
 
   Capybara::Selenium::Driver.new(
     app,
