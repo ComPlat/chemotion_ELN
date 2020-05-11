873dd776cf188427da9608b6dc5c04ba5570e769
Paggy <pei-chi.huang@kit.edu>
2020-05-11 13:23:01 +0200

    name_abbreviation_reserved_list

---
+++ config/initializers/user_props.rb
@@ -0,0 +1,14 @@
+begin
+  user_props_config = Rails.application.config_for :user_props
+
+  Rails.application.configure do
+    config.user_props = ActiveSupport::OrderedOptions.new
+    config.user_props.name_abbr = user_props_config[:name_abbreviation] if user_props_config
+  end
+rescue StandardError => e
+  Rails.logger.error e.message
+  Rails.application.configure do
+    config.editors = nil
+  end
+end
+
