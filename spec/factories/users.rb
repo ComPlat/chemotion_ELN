e27cfe008620e60701a53600e4aaff6e9f3334d7
pei-chi.huang <pei-chi.huang@kit.edu>
2020-10-21 13:27:45 +0200

    Resolve "rspec feature page load fix"

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -13,5 +13,20 @@ FactoryBot.define do
         wellplates: 0
       }
     end
+
+    callback(:after_create) do |user|
+      profile = user.profile
+      data = profile&.data
+      unless data.nil?
+        data.merge!(layout: {
+          'sample' => 1,
+          'reaction' => 2,
+          'wellplate' => 3,
+          'screen' => 4,
+          'research_plan' => 5
+        })
+        profile.update_columns(data: data)
+      end
+    end
   end
 end
