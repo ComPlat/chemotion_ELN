e91890d9adac17ae8b66b9959876b63e17c3afe8
Maximilian Kerp <kerp@taktsoft.com>
2020-11-25 15:02:52 +0100

    Undo change in production env from the app:update task

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true, mangle: false, compress: false)
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
