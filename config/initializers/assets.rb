e73be64d83a6935a51106994aa09162cbdaf2580
ivan <barisic@taktsoft.com>
2020-11-27 14:16:32 +0100

    run rails app:update #rails-upgrade

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,10 +5,12 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
 Rails.application.config.assets.precompile  = %w( manifest.js )
 Rails.application.config.assets.precompile += %w( grape_swagger_rails/application.css )
 Rails.application.config.assets.precompile += %w( grape_swagger_rails/application.js )
