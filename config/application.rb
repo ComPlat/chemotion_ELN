fcc247d014eafe61293051d6652fa945f72fe177
ivan <barisic@taktsoft.com>
2020-12-02 12:16:04 +0100

    use rails 5.2 deafaults #rails-upgrade

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ Bundler.require(*Rails.groups, :plugins)
 module Chemotion
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
     config.version = File.exist?('VERSION') && YAML.load_file('VERSION') || {
       'version': 'v0', 'base_revision': '0', 'current_revision': '0'
     }
