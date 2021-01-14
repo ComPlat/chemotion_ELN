251c96cd087971138495109fa462d8053716c8fb
PiTrem <pierre.tremouilhac@gmx.de>
2021-01-14 11:00:50 +0100

    Rails seeds for environments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,56 @@
-#desc ketcherails  common-templates, amino-acid, name-abbreviations seeds
-#Ketcherails::Engine.load_seed
-!User.find_by(email: 'template.moderator@eln.edu') && User.create!(
-  email: 'template.moderator@eln.edu', password: '@eln.edu', first_name: 'Template', last_name: 'Moderator',
-  name_abbreviation: 'TMo', confirmed_at: Time.now,
-#  is_templates_moderator: true # Use admin user managment page to set it instead
-)
+# adapted from https://gist.github.com/servel333/47f6cca9e51497aeefab
 
-Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
-  load seed
+## db/seeds.rb
+['all', Rails.env].each do |seed|
+  seed_file = Rails.root.join('db', 'seeds', "#{seed}.rb")
+  if File.exists?(seed_file)
+    puts "*** Loading #{seed} seed data"
+    require seed_file
+  end
+
+  seed_dir = Rails.root.join('db', 'seeds', seed)
+  if File.directory?(seed_dir)
+    Dir[File.join(seed_dir, "*.rb")].sort.each do |file|
+      puts "*** Loading #{seed} seed data from #{file}"
+      require file
+    end
+  end
+
 end
+
+##############################
+## Example of folder structure
+
+## db/seeds/all.rb
+# Seeds for all environments
+
+## db/seeds/development.rb
+# Seeds only for the "development" environment.
+
+## db/seeds/test.rb
+# Seeds only for the "test" environment.
+
+## db/seeds/production.rb
+# Seeds only for the "production" environment.
+
+## db/seeds/development/users.rb
+# Seeds only for the "development" environment.
+# ... seed users ...
+
+## db/seeds/production/users.rb
+# Seeds only for the "production" environment.
+# ... seed users ...
+
+
+#########################################
+## Recommended shared seeds files pattern
+
+## db/seeds/shared/dev_users.rb
+# Seeds for any environment but must be manually required
+# ... create users only for some environments.
+
+## db/seeds/development.rb
+# Seeds only for the "development" environment.
+# require_relative "shared/dev_users.rb"
+
+
