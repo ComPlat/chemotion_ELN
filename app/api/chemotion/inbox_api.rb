f9fb829b9766b538c85c25883a530d47d916f5bd
Lars Gollnow <lg@megorei.com>
2021-02-26 12:25:43 +0100

    addd spec

--- app/api/chemotion/inbox_api.rb
+++ app/api/chemotion/inbox_api.rb
@@ -1,12 +1,17 @@
 module Chemotion
   class InboxAPI < Grape::API
     resource :inbox do
-      desc 'shit'
-      params do
-        requires :search_string, type: String, desc: 'Search String'
-      end
-      get 'samples' do
-        Sample.limit(10)
+      resource :samples do
+        desc 'search samples from user by '
+        params do
+          optional :search_string, type: String, desc: 'Search String'
+        end
+        get do
+          # params[:search_string]
+          # Collection.belongs_to_or_shared_by(current_user.id, current_user.group_ids)
+
+          Sample.limit(10)
+        end
       end
     end
   end
