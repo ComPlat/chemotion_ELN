1cf380ad4942d4495e246200a8adea461fd4beb6
Thomas Watolla <tw@megorei.com>
2021-03-08 14:38:27 +0100

    Add API endpoint

--- app/assets/javascripts/components/fetchers/InboxFetcher.js
+++ app/assets/javascripts/components/fetchers/InboxFetcher.js
@@ -24,4 +24,22 @@ export default class InboxFetcher {
 
     return promise;
   }
+
+  static assignToAnalysis(attachmentId, sampleId) {
+    const promise = fetch(`/api/v1/inbox/samples/${attachmentId}?attachment_id=${sampleId}`, {
+      credentials: 'same-origin',
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(json => json)
+      .catch((errorMessage) => {
+        console.log(errorMessage);
+      });
+
+    return promise;
+  }
 }
