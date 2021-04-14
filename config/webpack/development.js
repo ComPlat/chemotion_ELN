053c9f7ac2a9a8ac1346589ef904e34a2b3af78c
PiTrem <pierre.tremouilhac@gmx.de>
2021-04-14 13:45:39 +0200

    log webpack config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const webpackConfig = require('./base')
 
+console.log(JSON.stringify(webpackConfig, undefined, 2));
+
 module.exports = webpackConfig
