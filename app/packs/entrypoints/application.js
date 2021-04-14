f6e3466ce255747a74b265d86576289947e21ec8
PiTrem <pierre.tremouilhac@gmx.de>
2021-04-14 04:04:53 +0200

    webpack: add entrypoint
    rm duplicate babel config

---
+++ app/packs/entrypoints/application.js
@@ -0,0 +1,12 @@
+// see https://github.com/rackt/react-router/issues/1067
+
+var React = require('react');
+var Home = require('../src/libHome/Home');
+var CnC = require('../src/libCnC/CnC');
+var AdminHome = require('../src/admin/AdminHome');
+var ChemScanner = require('../src/components/chemscanner/ChemScanner');
+var ChemSpectra = require('../src/components/chemspectra/ChemSpectra');
+var ChemSpectraEditor = require('../src/components/chemspectra/ChemSpectraEditor');
+var MoleculeModerator = require('../src/components/MoleculeModerator');
+var App = require('../src/components/App');
+
