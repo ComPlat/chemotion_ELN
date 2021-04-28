48319878de73cb2d12012dea67bcd913e207cc78
chia-lin.lin <chia-lin.lin@kit.edu>
2021-04-28 15:21:10 +0200

    add check exist or not

--- config/initializers/eln_features.rb
+++ config/initializers/eln_features.rb
@@ -3,5 +3,5 @@
 Rails.application.configure do
   # config.eln_features = ActiveSupport::OrderedOptions.new
   # config.eln_features.merge(ActiveRecord::Base.connection.table_exists?('matrices') ? Matrice.pluck(:name, :id).to_h : {})
-  Matrice.gen_matrices_json
+  Matrice.gen_matrices_json if ActiveRecord::Base.connection.table_exists?('matrices')
 end
