b25d54418c4456b4db507ce7b5c5e25e651c38c3
Paggy <pei-chi.huang@kit.edu>
2021-05-19 16:31:48 +0200

    element klass init

--- db/migrate/20210316132800_generic_elements_init.rb
+++ db/migrate/20210316132800_generic_elements_init.rb
@@ -2,6 +2,9 @@
 
 # Create generic elements
 class GenericElementsInit < ActiveRecord::Migration
+  class ElementKlass < ActiveRecord::Base
+  end
+
   def self.up
     Matrice.create(name: 'genericElement', enabled: false, label: 'genericElement', include_ids: [], exclude_ids: []) if Matrice.find_by(name: 'genericElement').nil?
     API::ELEMENTS.reverse.each_with_index do |element, idx|
