aa09ee670288fe6eadfccbdca2f08ef6a8970c18
PiTrem <pierre.tremouilhac@kit.edu>
2021-06-08 20:05:19 +0000

    rebase> fix spec

--- spec/models/research_plan_metadata_spec.rb
+++ spec/models/research_plan_metadata_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ResearchPlanMetadata, type: :model do
   let(:research_plan) { create(:research_plan) }
-  let(:research_plan_metadata) { create(:research_plan_metadata) }
+  let(:research_plan_metadata) { build(:research_plan_metadata) }
 
   before do
     research_plan.research_plan_metadata = research_plan_metadata
