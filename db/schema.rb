29c457a3cc3720e2e3b97c6746a1ddc171d289d0
PiTrem <pierre.tremouilhac@kit.edu>
2021-07-06 19:43:45 +0000

    db/schema upd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_180000) do
+ActiveRecord::Schema.define(version: 2021_06_21_155000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
