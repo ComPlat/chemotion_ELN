6b3c358dbfda0d899ce09789c8ed4b69e5ec6ae5
Danny Truong <allenskywalker92@gmail.com>
2021-07-21 10:29:30 +0200

    replace jquery with js

--- app/assets/javascripts/affiliations.js
+++ app/assets/javascripts/affiliations.js
@@ -1,53 +1,60 @@
-attachAutoComplete = function(type, elementId) {
+function attachAutoComplete(type, elementId) {
   var sourceData = []
-  $.getJSON('/api/v1/public/affiliations/' + type, function(data) {
-    sourceData = data.affiliations
-  })
-
-  var elementSelector = 'input[id="' + elementId + '"]'
-  var country = new autoComplete({
-    selector: elementSelector,
-    minChars: 2,
-    source: function(term, suggest) {
-      term = term.toLowerCase()
-      var matches = [];
-      for (i = 0; i < sourceData.length; i++) {
-        if (~sourceData[i].toLowerCase().indexOf(term)) {
-          matches.push(sourceData[i])
+  fetch(
+    '/api/v1/public/affiliations/' + type
+  )
+    .then(response => response.json())
+    .then(data => {
+      sourceData = data.affiliations
+      var elementSelector = 'input[id="' + elementId + '"]'
+      var country = new autoComplete({
+        selector: elementSelector,
+        minChars: 2,
+        source: function (term, suggest) {
+          term = term.toLowerCase()
+          var matches = [];
+          for (i = 0; i < sourceData.length; i++) {
+            if (~sourceData[i].toLowerCase().indexOf(term)) {
+              matches.push(sourceData[i])
+            }
+          }
+          suggest(matches)
         }
-      }
-      suggest(matches)
-    }
-  })
+      })
+    }).catch(error => {
+      // Handle error
+      console.log(error)
+    });;
 }
 
-$(function() {
-  $("form").on("keypress", function (e) {
+(function () {
+  document.querySelector("form").addEventListener("keypress", (e) => function (e) {
     if (e.keyCode == 13) {
       return false;
     }
   })
-
 
   attachAutoComplete("countries", "country-select")
   attachAutoComplete("organizations", "organization-select")
   attachAutoComplete("departments", "department-select")
   attachAutoComplete("groups", "group-select")
 
-});
-$("input#user_email").focusout(
-  function(){
-    var email = $(this).val(), domain_match, domain;
-    if (!email || !(domain_match = email.match(/\@(.*)/))){return}
-    domain = domain_match[1]
-    $.getJSON(
-      '/api/v1/public/affiliations/swot?domain='+ encodeURIComponent(domain)
-    ).done(
-      function(organization){
-        if(organization) {
-          $("input#organization-select").val(organization)
-        }
+  let userEmailInput = document.querySelector("input#user_email")
+  if (userEmailInput) {
+    userEmailInput.addEventListener("focusout", (e) => {
+        var email = e.target.value, domain_match, domain;
+        if (!email || !(domain_match = email.match(/\@(.*)/))) { return }
+        domain = domain_match[1]
+        fetch(
+          '/api/v1/public/affiliations/swot?domain=' + encodeURIComponent(domain)
+        )
+        .then(response => response.json())
+        .then(organization => {
+          if (organization) {
+            document.querySelector("input#organization-select").value = organization;
+          }
+        });
       }
     );
   }
-);
+})();
