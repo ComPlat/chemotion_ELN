1d4b869dca5de137cd025fa8d344ddb1ee5d08d5
Danny Truong <allenskywalker92@gmail.com>
2021-08-02 09:30:54 +0200

    Upload large files as chunk and add loading progress bar

--- app/packs/src/components/actions/LoadingActions.js
+++ app/packs/src/components/actions/LoadingActions.js
@@ -8,6 +8,18 @@ class LoadingActions {
   stop() {
     return null;
   }
+
+  startLoadingWithProgress(filename) {
+    return filename;
+  }
+
+  stopLoadingWithProgress(filename) {
+    return filename;
+  }
+
+  updateLoadingProgress(filename, progress) {
+    return { filename: filename, progress: progress };
+  }
 }
 
 export default alt.createActions(LoadingActions);
