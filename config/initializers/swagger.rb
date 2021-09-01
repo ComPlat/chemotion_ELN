75ccb7d823da0d0b920a0607907ae8fbf26aedc9
Memood <in1767@kit.edu>
2021-09-01 18:10:19 +0200

    customized swagger documentation added
    swagger doc updated
    
    minor changes

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -2,5 +2,6 @@ GrapeSwaggerRails.options.before_action do
   GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
 end
 
-GrapeSwaggerRails.options.url      = '/api/v1/swagger_doc'
+GrapeSwaggerRails.options.url = Rails.env.development? ? '/api/v1/swagger_doc' : '/swagger_doc.json'
 GrapeSwaggerRails.options.app_url  = "#{ENV['APPLICATION_URL']}"
+
