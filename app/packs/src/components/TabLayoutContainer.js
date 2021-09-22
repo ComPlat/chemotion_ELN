8da6fc01900b520e9c05c55abf7a3174cf22222e
Paggy Huang <pei-chi.huang@kit.edu>
2021-09-22 10:04:13 +0200

    add hover over information to generic element symbols

--- app/packs/src/components/TabLayoutContainer.js
+++ app/packs/src/components/TabLayoutContainer.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
 
 import ArrayUtils from './utils/ArrayUtils';
-import TabLayoutCell from './TabLayoutCell'
+import TabLayoutCell from './TabLayoutCell';
 
 export default class TabLayoutContainer extends React.Component {
   constructor(props) {
@@ -18,52 +18,54 @@ export default class TabLayoutContainer extends React.Component {
   }
 
   moveLayout(dragItem, hoverItem) {
-    let {visible, hidden} = this.state
+    let { visible, hidden } = this.state;
 
-    if (!dragItem.isHidden && hoverItem.isHidden && visible.size == 1) return
+    if (!dragItem.isHidden && hoverItem.isHidden && visible.size === 1) return;
 
-    if (dragItem.isHidden)
-      hidden = hidden.splice(dragItem.index, 1)
-    else if (visible.size > 1)
-      visible = visible.splice(dragItem.index, 1)
-
-    if (hoverItem.isHidden)
-      hidden = hidden.splice(hoverItem.index, 0, dragItem.cell)
-    else
-      visible = visible.splice(hoverItem.index, 0, dragItem.cell)
-
-    if (hidden.size == 0) {
-      hidden = ArrayUtils.pushUniq(hidden, "hidden")
-    } else if (hidden.size > 1) {
-      hidden = ArrayUtils.removeFromListByValue(hidden, "hidden")
+    if (dragItem.isHidden) {
+      hidden = hidden.splice(dragItem.index, 1);
+    } else if (visible.size > 1) {
+      visible = visible.splice(dragItem.index, 1);
     }
 
-    this.setState({visible: visible, hidden: hidden})
+    if (hoverItem.isHidden) {
+      hidden = hidden.splice(hoverItem.index, 0, dragItem.cell)
+    } else {
+      visible = visible.splice(hoverItem.index, 0, dragItem.cell)
+    }
+
+    if (hidden.size === 0) {
+      hidden = ArrayUtils.pushUniq(hidden, 'hidden');
+    } else if (hidden.size > 1) {
+      hidden = ArrayUtils.removeFromListByValue(hidden, 'hidden');
+    }
+
+    this.setState({ visible, hidden });
   }
 
   render() {
-    const { visible, hidden } = this.state
-    const { isElementDetails, tabTitles } = this.props
-    let moveLayout = this.moveLayout
+    const { visible, hidden } = this.state;
+    const { isElementDetails, tabTitles } = this.props;
+    let moveLayout = this.moveLayout;
 
     return (
       <table className="layout-container">
-      <tbody><tr>
-        {visible.map(function(e, index) {
-          return (<TabLayoutCell key={index + "_visible"} cell={e}
-                                 isHidden={false} index={index}
-                                 moveLayout={moveLayout}
-                                 title={tabTitles[e] || e}
-                                 isElementDetails={isElementDetails}/>)
-        })}
-        {hidden.map(function(e, index) {
-          return (<TabLayoutCell key={index + "_hidden"} cell={e}
-                                 isHidden={true} index={index}
-                                 moveLayout={moveLayout}
-                                 title={tabTitles[e] || e}	 
-                                 isElementDetails={isElementDetails}/>)
-        })}
-      </tr></tbody>
+        <tbody><tr>
+          {visible.map(function(e, index) {
+            return (<TabLayoutCell key={index + "_visible"} cell={e}
+                                  isHidden={false} index={index}
+                                  moveLayout={moveLayout}
+                                  title={tabTitles[e] || e}
+                                  isElementDetails={isElementDetails}/>)
+          })}
+          {hidden.map(function(e, index) {
+            return (<TabLayoutCell key={index + "_hidden"} cell={e}
+                                  isHidden={true} index={index}
+                                  moveLayout={moveLayout}
+                                  title={tabTitles[e] || e}
+                                  isElementDetails={isElementDetails}/>)
+          })}
+        </tr></tbody>
       </table>
     )
   }
@@ -71,9 +73,9 @@ export default class TabLayoutContainer extends React.Component {
 
 TabLayoutContainer.propTypes = {
   tabTitles: PropTypes.object,
-}
+};
 
 TabLayoutContainer.defaultProps = {
   tabTitles: {},
-}
+};
 
