bfe5aa24b7105c26c3d301302024ef5c6ac64040
PiTrem <pierre.tremouilhac@kit.edu>
2021-10-18 23:19:36 +0000

    db migration: fix typo to prevent: > TypeError: can't cast Array

--- db/migrate/20210527172347_add_solvent_to_samples.rb
+++ db/migrate/20210527172347_add_solvent_to_samples.rb
@@ -5,7 +5,7 @@ class AddSolventToSamples < ActiveRecord::Migration[4.2]
 
     Sample.where(solvent: [nil, '']).where.not(deprecated_solvent: [nil, '']).find_each do |item|
       solvent = Chemotion::SampleConst.solvents_smiles_options.find { |s| s[:label].include?(item.deprecated_solvent) }
-      item.update_columns(solvent: [{ label: solvent[:value][:external_label], smiles: solvent[:value][:smiles], ratio: '1' }.to_json])
+      item.update_columns(solvent: [{ label: solvent[:value][:external_label], smiles: solvent[:value][:smiles], ratio: '1' }].to_json)
     end
   end
 end
