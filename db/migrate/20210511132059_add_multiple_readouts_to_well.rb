fee5da76b2e67c817ade82394d9acfde20fc0b6a
Thomas Watolla <tw@megorei.com>
2021-11-19 12:24:24 +0100

    Change migration

--- db/migrate/20210511132059_add_multiple_readouts_to_well.rb
+++ db/migrate/20210511132059_add_multiple_readouts_to_well.rb
@@ -1,11 +1,12 @@
 class AddMultipleReadoutsToWell < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :wells, :readouts, :jsonb, default: [{"value": "", "unit": ""}]
 
     add_column :wellplates, :readout_titles, :jsonb, default: ["Readout"]
 
-    Well.where.not(readout: nil).each do |w|
-      w.update_attributes(
+    Well.reset_column_information
+    Well.where.not(readout: nil).find_each do |w|
+      w.update_columns(
         readouts: [
           {
             description: 'Readout',
@@ -15,5 +16,6 @@ class AddMultipleReadoutsToWell < ActiveRecord::Migration[5.2]
         ]
       )
     end
+    remove_column :wells, :readout
   end
 end
