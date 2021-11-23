0a7976843651f6202ee122e46c922661f155ef38
PiTrem <pierre.tremouilhac@kit.edu>
2021-11-23 15:22:27 +0000

    simplify asset version check on UI
    (fixes  persistent UI  notification to reload pages after updates)

--- config/initializers/eln_features.rb
+++ config/initializers/eln_features.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# pack version
+pack_path = Webpacker.manifest.send(:data)&.fetch('application.js', nil)
+ENV['VERSION_ASSETS'] = pack_path && File.basename(pack_path)
+
 Rails.application.configure do
   # config.eln_features = ActiveSupport::OrderedOptions.new
   # config.eln_features.merge(ActiveRecord::Base.connection.table_exists?('matrices') ? Matrice.pluck(:name, :id).to_h : {})
