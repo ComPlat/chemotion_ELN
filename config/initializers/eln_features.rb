fb818bcf9fb1b22327fa9a260f20db6c9c5d7aeb
PiTrem <pierre.tremouilhac@kit.edu>
2021-11-30 13:52:06 +0000

    fix db:create : rescue failed db connection from initializers
    also:  CI upd runner image

--- config/initializers/eln_features.rb
+++ config/initializers/eln_features.rb
@@ -7,6 +7,10 @@ ENV['VERSION_ASSETS'] = pack_path && File.basename(pack_path)
 Rails.application.configure do
   # config.eln_features = ActiveSupport::OrderedOptions.new
   # config.eln_features.merge(ActiveRecord::Base.connection.table_exists?('matrices') ? Matrice.pluck(:name, :id).to_h : {})
+  break unless defined?(ActiveRecord::Base)
+
   Matrice.gen_matrices_json if ActiveRecord::Base.connection.table_exists?('matrices')
   ElementKlass.gen_klasses_json if ActiveRecord::Base.connection.table_exists?('element_klasses')
+rescue PG::ConnectionBad, ActiveRecord::NoDatabaseError => e
+  puts e.message
 end
