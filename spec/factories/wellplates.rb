5113e238548fcf2b91927e9a08601318614e42fb
Thomas Watolla <tw@megorei.com>
2021-12-10 14:18:36 +0100

    Add template fixture and expand wellplate factory

--- spec/factories/wellplates.rb
+++ spec/factories/wellplates.rb
@@ -9,5 +9,15 @@ FactoryBot.define do
         'Compound Concentration'
       ]
     end
+
+    trait :with_wells do
+      after(:create) do |wellplate|
+        (1..8).each do |pos_x|
+          (1..12).each do |pos_y|
+            FactoryBot.create(:well, wellplate: wellplate, position_x: pos_x, position_y: pos_y)
+          end
+        end
+      end
+    end
   end
 end
