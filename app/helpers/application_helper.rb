e9af375497c1edd3266ae4c3a13110693e0ef98b
Mehreen <mehreen.mansur>
2021-12-18 10:28:43 +0100

    About page to render current CHANGELOG.md Upgrade notification to present link to /about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,24 @@ module ApplicationHelper
       !Dir[File.join(Gem.loaded_specs[plugin].full_gem_path,"app","views",plugin,"home","_account.*")].empty?
     end
   end
+
+  def markdown(text)
+    options = {
+      filter_html: true,
+      hard_wrap: true,
+      link_attributes: { rel: 'nofollow', target: '_blank' },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+
+    extensions = {
+      autolink: true,
+      superscript: true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    @markdown ||= Redcarpet::Markdown.new(renderer, extensions)
+    @markdown.render(text).html_safe
+  end
 end
