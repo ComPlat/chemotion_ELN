e35993938b64123c48503966d6ed9d25735e2427
Johannes Haubold <jh@megorei.com>
2022-01-11 14:34:10 +0100

    Add initial files for docker dev setup
    There are still some issues with the setup.
    Mainly those are the updated initializers trying to contact the database (which breaks rake db:setup)
    and the conflicting JS package versions (npm breaks, yarn just ignores them)

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -8,51 +8,53 @@
 # Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 
-begin
-  if defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.data_source_exists?('delayed_jobs') && Delayed::Job.column_names.include?('cron')
-    Delayed::Job.where("handler like ?", "%CollectDataFrom%").destroy_all
-    Delayed::Job.where("handler like ?", "%CollectFileFrom%").destroy_all
-    if Rails.configuration.datacollectors && Rails.configuration.datacollectors.services
-      for service in Rails.configuration.datacollectors.services do
-        cron_config = nil
-        config = service[:every].to_s.strip
-        cron_config = '*/' + config + ' * * * *' if config.split(/\s/).size == 1
-        config = service[:cron].to_s.strip
-        cron_config = config if config.split(/\s/).size == 5
-        case service[:name].to_s
-        when 'mailcollector'
-          CollectDataFromMailJob
-        when 'folderwatchersftp'
-          CollectDataFromSftpJob
-        when 'folderwatcherlocal'
-          CollectDataFromLocalJob
-        when 'filewatcherlocal'
-          CollectFileFromLocalJob
-        when 'filewatchersftp'
-          CollectFileFromSftpJob
-        else
-          nil
-        end&.set(cron: cron_config).perform_later if cron_config
+ActiveSupport.on_load(:active_record) do
+  begin
+    if ActiveRecord::Base.connection.data_source_exists?('delayed_jobs') && Delayed::Job.column_names.include?('cron')
+      Delayed::Job.where("handler like ?", "%CollectDataFrom%").destroy_all
+      Delayed::Job.where("handler like ?", "%CollectFileFrom%").destroy_all
+      if Rails.configuration.datacollectors && Rails.configuration.datacollectors.services
+        for service in Rails.configuration.datacollectors.services do
+          cron_config = nil
+          config = service[:every].to_s.strip
+          cron_config = '*/' + config + ' * * * *' if config.split(/\s/).size == 1
+          config = service[:cron].to_s.strip
+          cron_config = config if config.split(/\s/).size == 5
+          case service[:name].to_s
+          when 'mailcollector'
+            CollectDataFromMailJob
+          when 'folderwatchersftp'
+            CollectDataFromSftpJob
+          when 'folderwatcherlocal'
+            CollectDataFromLocalJob
+          when 'filewatcherlocal'
+            CollectFileFromLocalJob
+          when 'filewatchersftp'
+            CollectFileFromSftpJob
+          else
+            nil
+          end&.set(cron: cron_config).perform_later if cron_config
+        end
       end
-    end
-    Delayed::Job.where("handler like ?", "%PubchemCidJob%").destroy_all
-    cron_config = ENV['CRON_CONFIG_PC_CID'].presence
-    cron_config ||= "#{rand(0..59)} #{rand(0..23)} * * #{rand(6..7)}"
-    PubchemCidJob.set(cron: cron_config ).perform_later
-    Delayed::Job.where("handler like ?", "%PubchemLcssJob%").destroy_all
-    cron_config = ENV['CRON_CONFIG_PC_LCSS'].presence
-    cron_config ||= "#{rand(0..59)} #{rand(0..23)} * * #{rand(6..7)}"
-    PubchemLcssJob.set(cron: cron_config).perform_later
-    Delayed::Job.where("handler like ?", "%RefreshElementTagJob%").destroy_all
-    cron_config = ENV['CRON_CONFIG_ELEMENT_TAG'].presence
-    cron_config ||= "#{rand(0..59)} #{rand(20..23)} * * #{rand(6..7)}"
-    RefreshElementTagJob.set(cron: cron_config).perform_later
-    Delayed::Job.where("handler like ?", "%ChemrepoIdJob%").destroy_all
-    cron_config = ENV['CRON_CONFIG_CHEM_REPO_ID'].presence
-    cron_config ||= "#{rand(0..59)} #{rand(17..19)} * * #{rand(6..7)}"
-    ChemrepoIdJob.set(cron: cron_config ).perform_later
+      Delayed::Job.where("handler like ?", "%PubchemCidJob%").destroy_all
+      cron_config = ENV['CRON_CONFIG_PC_CID'].presence
+      cron_config ||= "#{rand(0..59)} #{rand(0..23)} * * #{rand(6..7)}"
+      PubchemCidJob.set(cron: cron_config ).perform_later
+      Delayed::Job.where("handler like ?", "%PubchemLcssJob%").destroy_all
+      cron_config = ENV['CRON_CONFIG_PC_LCSS'].presence
+      cron_config ||= "#{rand(0..59)} #{rand(0..23)} * * #{rand(6..7)}"
+      PubchemLcssJob.set(cron: cron_config).perform_later
+      Delayed::Job.where("handler like ?", "%RefreshElementTagJob%").destroy_all
+      cron_config = ENV['CRON_CONFIG_ELEMENT_TAG'].presence
+      cron_config ||= "#{rand(0..59)} #{rand(20..23)} * * #{rand(6..7)}"
+      RefreshElementTagJob.set(cron: cron_config).perform_later
+      Delayed::Job.where("handler like ?", "%ChemrepoIdJob%").destroy_all
+      cron_config = ENV['CRON_CONFIG_CHEM_REPO_ID'].presence
+      cron_config ||= "#{rand(0..59)} #{rand(17..19)} * * #{rand(6..7)}"
+      ChemrepoIdJob.set(cron: cron_config ).perform_later
 
+    end
+  rescue PG::ConnectionBad, ActiveRecord::NoDatabaseError => e
+    puts e.message
   end
-rescue PG::ConnectionBad, ActiveRecord::NoDatabaseError => e
-  puts e.message
 end
--- config/initializers/inference.rb
+++ config/initializers/inference.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
+ActiveSupport.on_load(:active_record) do
+  Rails.application.configure do
+    inference_config = {}
 
-inference_config = {}
+    if File.exist? Rails.root.join('config', 'inference.yml')
+      inference_config = Rails.application.config_for :inference
+    else
+      begin
+        inference_config = ActiveRecord::Base.connection.table_exists?('matrices') ? (Matrice.find_by(name: 'reactionPrediction')&.configs&.symbolize_keys || {}) : {}
+      rescue ActiveRecord::StatementInvalid, PG::ConnectionBad, PG::UndefinedTable
+      end
+    end
 
-if File.exist? Rails.root.join('config', 'inference.yml')
-  inference_config = Rails.application.config_for :inference
-else
-  begin
-    inference_config = ActiveRecord::Base.connection.table_exists?('matrices') ? (Matrice.find_by(name: 'reactionPrediction')&.configs&.symbolize_keys || {}) : {}
-  rescue ActiveRecord::StatementInvalid, PG::ConnectionBad, PG::UndefinedTable
+    config.inference = ActiveSupport::OrderedOptions.new
+    config.inference.url = inference_config[:url]
+    config.inference.port = inference_config[:port]
   end
-end
-
-Rails.application.configure do
-  config.inference = ActiveSupport::OrderedOptions.new
-  config.inference.url = inference_config[:url]
-  config.inference.port = inference_config[:port]
 end
