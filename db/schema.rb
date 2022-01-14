8ef260a433b641c257fd97ebd8cc815f535215ec
PiTrem <pierre.tremouilhac@kit.edu>
2022-01-14 09:55:11 +0000

    Fix wrong format seeding of the jsonb channels.msg_template that used to work with rails 4
    (see github.com/rails/rails/issues/25594)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_144812) do
+ActiveRecord::Schema.define(version: 2022_01_14_085300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
