7718eda4053ab32f37ba27190b7514049c41040e
Paggy <pei-chi.huang@kit.edu>
2022-03-09 18:33:22 +0100

    fix jsonb default for rails 5

--- db/migrate/20210217164124_change_data_in_profile.rb
+++ db/migrate/20210217164124_change_data_in_profile.rb
@@ -1,6 +1,6 @@
 class ChangeDataInProfile < ActiveRecord::Migration[4.2]
   def change
-    change_column_default :profiles, :data, '{}'
-    change_column_null :profiles, :data, false, '{}'
+    change_column_default :profiles, :data, {}
+    change_column_null :profiles, :data, false, {}
   end
 end
