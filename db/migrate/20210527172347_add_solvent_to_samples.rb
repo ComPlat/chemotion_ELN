2788ba9e6c2735e66221e7592418a396bcfbe74f
PiTrem <pierre.tremouilhac@kit.edu>
2022-04-01 11:07:44 +0200

    fx migration typo

--- db/migrate/20210527172347_add_solvent_to_samples.rb
+++ db/migrate/20210527172347_add_solvent_to_samples.rb
@@ -6,7 +6,7 @@ class AddSolventToSamples < ActiveRecord::Migration[4.2]
     Sample.where(solvent: [nil, '']).where.not(deprecated_solvent: [nil, '']).find_each do |item|
       solvent = Chemotion::SampleConst.solvents_smiles_options.find { |s| item.deprecated_solvent&.include?(s[:label]) }
       new_sol = solvent ? [{ label: solvent[:value][:external_label], smiles: solvent[:value][:smiles], ratio: '1' }].to_json : nil
-      sample.update_column(solvent: new_sol)
+      sample.update_columns(solvent: new_sol)
     end
   end
 end
