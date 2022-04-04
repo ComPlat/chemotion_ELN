a1d31881aeab0b0a525fd7c55da592811e70d614
FabianMauz <fmauz@ipb-halle.de>
2022-04-04 09:25:39 +0200

    REmoved adding of annotation at uploading images

--- app/packs/src/components/fetchers/ResearchPlansFetcher.js
+++ app/packs/src/components/fetchers/ResearchPlansFetcher.js
@@ -92,10 +92,7 @@ export default class ResearchPlansFetcher {
 
   static updateImageFile(image_file, replace) {
     var data = new FormData();
-    data.append('file', image_file);
-    if(image_file.annotation){
-      data.append('annotation',image_file.annotation);
-    }
+    data.append('file', image_file);    
     if (replace) {
       data.append('replace', replace);
     }
