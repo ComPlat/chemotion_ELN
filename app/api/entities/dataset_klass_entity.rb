5cf238a34d3b6f8699bf835ab2a389165ffebcf4
Johannes Haubold <jh@megorei.com>
2022-05-25 17:40:35 +0200

    Fix missing commas in entities

--- app/api/entities/dataset_klass_entity.rb
+++ app/api/entities/dataset_klass_entity.rb
@@ -3,15 +3,15 @@
 module Entities
   class DatasetKlassEntity < ApplicationEntity
     expose(
-      :desc
-      :id
-      :is_active
-      :label
-      :ols_term_id
-      :place
-      :properties_release
-      :properties_template
-      :uuid
+      :desc,
+      :id,
+      :is_active,
+      :label,
+      :ols_term_id,
+      :place,
+      :properties_release,
+      :properties_template,
+      :uuid,
     )
     expose_timestamps(timestamp_fields: [:released_at])
   end
