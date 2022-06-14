dd9c06edd7591876f14f27c3d712155f284860a3
Tasnim Mehzabin <tasnim.mehzabin@kit.edu>
2022-06-14 15:34:11 +0200

    [751] - Collection label is added in screen so that user can see the collections where the research plan belongs to

--- app/api/chemotion/screen_api.rb
+++ app/api/chemotion/screen_api.rb
@@ -148,19 +148,19 @@ module Chemotion
         recent_ols_term_update('chmo', kinds) if kinds&.length&.positive?
 
         collection = current_user.collections.where(id: params[:collection_id]).take
-        CollectionsScreen.create(screen: screen, collection: collection) if collection.present?
+        screen.collections << collection if collection.present?
 
         is_shared_collection = false
         unless collection.present?
           sync_collection = current_user.all_sync_in_collections_users.where(id: params[:collection_id]).take
           if sync_collection.present?
             is_shared_collection = true
-            CollectionsScreen.create(screen: screen, collection: Collection.find(sync_collection['collection_id']))
-            CollectionsScreen.create(screen: screen, collection: Collection.get_all_collection_for_user(sync_collection['shared_by_id']))
+            screen.collections << Collection.find(sync_collection['collection_id'])
+            screen.collections << Collection.get_all_collection_for_user(sync_collection['shared_by_id'])
           end
         end
 
-        CollectionsScreen.create(screen: screen, collection: Collection.get_all_collection_for_user(current_user.id)) unless is_shared_collection
+        screen.collections << Collection.get_all_collection_for_user(current_user.id) unless is_shared_collection
 
         params[:wellplate_ids].each do |id|
           ScreensWellplate.find_or_create_by(wellplate_id: id, screen_id: screen.id)
