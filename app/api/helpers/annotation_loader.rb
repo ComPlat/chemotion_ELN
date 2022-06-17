bb128518f6aed88655751806ca3e73040f661761
FabianMauz <fmauz@ipb-halle.de>
2022-06-17 09:42:10 +0200

    Add loading of annotation

---
+++ app/api/helpers/annotation_loader.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Class for loading an annotation of an attachment with the shrine infrastructure
+class AnnotationLoader
+
+    def getAnnotationOfAttachment(attachment_id)       
+        att = Attachment.find(attachment_id);        
+        raise "could not find annotation of attachment" if isAnnotationJsonAbsent(att.attachment_data);         
+        locationOfAnnotation=att.attachment_data['derivatives']['annotation']['id'];
+        back=File.open(locationOfAnnotation, 'rb') if File.exist?(locationOfAnnotation);
+        raise "could not find annotation of attachment (file not found)" if !back;      
+        annotationSvg=back.read;
+        annotationSvg      
+    end
+
+
+
+
+    def isAnnotationJsonAbsent(attachment_data)
+        !attachment_data||
+        !attachment_data['derivatives']||
+        !attachment_data['derivatives']['annotation']||
+        !attachment_data['derivatives']['annotation']['id']
+    end     
+end
---
+++ spec/api/helpers/annotation_loader_spec.rb
@@ -0,0 +1,104 @@
+#frozen_string_literal: true
+
+require 'rails_helper'
+
+describe AnnotationLoader do
+    context '-> annotations' do
+      
+        describe '-> get' do            
+            it '-> of non existing attachment' do   
+                expect {                
+                    helper=AnnotationLoader.new;
+                    helper.getAnnotationOfAttachment(1);
+                }.to raise_error  ("Couldn't find Attachment with 'id'=1")             
+            end
+
+            it '-> of attachment without annotation' do   
+                expect {   
+                    attachment=createAttachmentWithoutAnnotation();    
+                    attachment.save;         
+                    helper=AnnotationLoader.new;
+                    helper.getAnnotationOfAttachment(attachment.id);
+                }.to raise_error  ("could not find annotation of attachment")             
+            end
+
+            it '-> of attachment with annotation but without file' do   
+             
+                expect {   
+                    attachment=createAttachmentWithAnnotation('/nothingHere/xxx.svg');                    
+                    attachment.save;           
+                    helper=AnnotationLoader.new;
+                    svgData=helper.getAnnotationOfAttachment(attachment.id);
+                }.to raise_error  ("could not find annotation of attachment (file not found)")             
+            end
+
+            it '-> of attachment with annotation and file' do   
+                    exampleSvgAnnotation="<svg>example</svg>";
+                    tempfile = Tempfile.new('annotationFile.svg')
+                    tempfile.write(exampleSvgAnnotation);
+                    tempfile.rewind;
+                    tempfile.close;
+                    attachment=createAttachmentWithAnnotation(tempfile.path);                    
+                    attachment.save;           
+                    helper=AnnotationLoader.new;
+                    svgData=helper.getAnnotationOfAttachment(attachment.id);                     
+                    assert_equal(exampleSvgAnnotation,svgData) ;
+                    tempfile.unlink;
+
+                    
+
+            end
+        end
+    end
+
+
+    def createAttachmentWithoutAnnotation()
+        attachment = Attachment.new(
+            bucket: 1,
+            filename: 'test',
+            file_path: 'tmp',
+            created_by: 1,
+            created_for: 1,
+            content_type: 'svg',
+            attachable_type: 1,
+            attachable_id: 1
+          )        
+        attachment
+    end
+
+    def createAttachmentWithAnnotation(location)
+        attachment=createAttachmentWithoutAnnotation();
+        attachment.attachment_data=createAnnotationJson(location);      
+        attachment
+
+    end
+
+
+
+    def createAnnotationJson(location)
+        tempfile = Tempfile.new('example.png')
+        str='{'\
+            ' "id": "'+tempfile.path+'",'\
+            '"storage": "store",'\
+            '"metadata": {'\
+            '    "size": 29111,'\
+            '   "filename": "example.png",'\
+            '    "mime_type": null'\
+            '},'\
+            '"derivatives": {'\
+            '    "annotation": {'\
+            '        "id": "'+location+'",'\
+            '        "storage": "store",'\
+            '        "metadata": {'\
+            '            "size": 480,'\
+            '            "filename": "example_annotation.svg",'\
+            '            "mime_type": null'\
+            '        }}}}';
+      JSON.parse(str);
+    end
+end
+
+
+
+
+
