87cf14789ac7788fb00d208a0fdd436bdb903fbf
PiTrem <pierre.tremouilhac@kit.edu>
2022-07-05 01:47:19 +0200

    attachment jobs: skip if not a valid attachment (#781)

--- app/jobs/transfer_thumbnail_to_public_job.rb
+++ app/jobs/transfer_thumbnail_to_public_job.rb
@@ -5,7 +5,9 @@ class TransferThumbnailToPublicJob < ApplicationJob
   queue_as :transfer_thumbnail_to_public
   def perform(attach_ary)
     attach_ary.each do |attach_id|
-      a = Attachment.find(attach_id)
+      a = Attachment.find_by(id: attach_id)
+      next unless a
+
       file_path = Rails.public_path.join('images', 'thumbnail', a.identifier)
       File.write(file_path, a.read_thumbnail.force_encoding('UTF-8')) if a.read_thumbnail
     end
