656685072697780c69693ba6fdbdc9c497c64471
Blake Truong <allenskywalker92@gmail.com>
2022-07-05 01:47:53 +0200

    Execute LCSS job when have new molecule (#770)
    * Execute LCSS job when have new molecule
    
    * Execute LCSS job when have new molecule
    
    create a job to get single lcss data
    
    refactor to use active record callback
    
    Co-authored-by: Danny Truong <vu.truong@kit.edu>

---
+++ app/jobs/pubchem_single_lcss_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Job to update molecule info for molecules with no LCSS
+# associated LCSS (molecule tag) is updated if cid found in PC db
+class PubchemSingleLcssJob < ApplicationJob
+  queue_as :single_pubchem_lcss
+
+  def perform(molecule)
+    molecule.pubchem_lcss
+  end
+end
