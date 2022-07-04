bb26a774f3dcb9fd303557181188302912b4f423
Devang Patel <67055123+dp19982@users.noreply.github.com>
2022-07-04 16:41:49 +0200

    show rxn image only if samples are present in rxn (#703)

--- app/packs/src/components/ReactionDetails.js
+++ app/packs/src/components/ReactionDetails.js
@@ -250,12 +250,13 @@ export default class ReactionDetails extends Component {
       return false;
     } else {
       const svgProps = reaction.svgPath.substr(reaction.svgPath.length - 4) === '.svg' ? { svgPath: reaction.svgPath } : { svg: reaction.reaction_svg_file }
-      return (
+      if(reaction.hasMaterials()) {
+        return (
         <SvgFileZoomPan
           duration={300}
           resize={true}
           {...svgProps}
-        />)
+        />)}
     }
   }
 
