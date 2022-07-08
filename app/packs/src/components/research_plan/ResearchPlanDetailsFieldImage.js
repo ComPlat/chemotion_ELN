7f928da510b2c0f6b46f6c4e4130b2daa065a671
FabianMauz <fmauz@ipb-halle.de>
2022-07-08 10:19:13 +0200

    removed unused method

--- app/packs/src/components/research_plan/ResearchPlanDetailsFieldImage.js
+++ app/packs/src/components/research_plan/ResearchPlanDetailsFieldImage.js
@@ -10,41 +10,19 @@ import SVG from 'react-inlinesvg';
 
 export default class ResearchPlanDetailsFieldImage extends Component {
 
-  constructor(props){   
+  constructor(props){
     super(props);
     const { field, onChange } = this.props;
     this.state={imageEditModalShown:false};
     this.state={annotation:{version:0}};
-    
+
   }
 
   componentDidMount(){
     this.actualizeAnnotationVersion();
   }
 
-  actualizeAnnotationVersion(){
-    const { field, onChange } = this.props;
-    if(field.value.public_name){
-      let restOfAnno=field.value.public_name.split(".")[0]+"_annotation.svg";  
-      const imageId=field.value.public_name.split(".")[0];
-      const src = `/images/research_plans/${restOfAnno}`;
-      const encodedValue = encodeURIComponent(imageId);
-      
-      fetch('/api/v1/annotation?imageId='+encodedValue, {
-        credentials: 'same-origin',
-        method: 'get',
-      })
-      .then(res =>{
-      if(res.status==200){
-        return res.json().then(json => {        
-            this.setState({'annotation':json});            
-          })
 
-      }else{
-        console.log("An error occured");
-      }});   
-    }          
-  }
 
   handleDrop(files) {
     const { field, onChange } = this.props;
@@ -54,7 +32,7 @@ export default class ResearchPlanDetailsFieldImage extends Component {
     const img = document.createElement('img');
     const blob = URL.createObjectURL(imageFile);
     img.src = blob;
-    img.onload = function() {    
+    img.onload = function() {
       imageFile.dimension=[img.width,img.height];
       ResearchPlansFetcher.updateImageFile(imageFile, replace).then((value) => {
         // update research plan
@@ -64,7 +42,7 @@ export default class ResearchPlanDetailsFieldImage extends Component {
 
     }
     // upload new image
-   
+
   }
 
   handleResizeChange(event) {
@@ -81,21 +59,21 @@ export default class ResearchPlanDetailsFieldImage extends Component {
 
   renderEdit() {
     const { field,onChange} = this.props;
-    
+
     let content;
     let versionOfAnno;
-    if (field.value.public_name) {     
+    if (field.value.public_name) {
       versionOfAnno=this.state.annotation.version;
-      let restOfAnno=field.value.public_name.split(".")[0]+"_annotation_v"+versionOfAnno+".svg";  
+      let restOfAnno=field.value.public_name.split(".")[0]+"_annotation_v"+versionOfAnno+".svg";
       const src = `/images/research_plans/${restOfAnno}`;
-      //const src = `/images/research_plans/${field.value.public_name}`;      
+      //const src = `/images/research_plans/${field.value.public_name}`;
       const zoomStyle=this.calculateZoomStyle(field);
-       
-      
+
+
       content = (
         <div className="image-container">
            <SVG style={zoomStyle} src={src} cacheRequests={false} />
-        
+
         </div>
       );
     } else {
@@ -106,8 +84,8 @@ export default class ResearchPlanDetailsFieldImage extends Component {
        <ImageAnnotationModalSVG
           imageElementId={"researchPlanImageID"+field.value.public_name}
           imageName={field.value.public_name}
-          file={field}     
-          versionOfAnnotation={versionOfAnno}    
+          file={field}
+          versionOfAnnotation={versionOfAnno}
           dataSrc={"/images/research_plans/"+field.value.public_name}
           isShow={this.state.imageEditModalShown}
           handleSave={()=>{
@@ -115,14 +93,14 @@ export default class ResearchPlanDetailsFieldImage extends Component {
             this.actualizeAnnotationVersion();
             onChange(field.value, field.id);}}
           handleOnClose={()=>{this.setState({imageEditModalShown:false})}}
-       />       
-     
-       <div style={ 
+       />
+
+       <div style={
          {display: 'flex',
          direction:'row'}}
         >
 
-       
+
         <FormGroup style={{ width: '30%' }}>
           <InputGroup>
             <InputGroup.Addon>Zoom</InputGroup.Addon>
@@ -135,12 +113,12 @@ export default class ResearchPlanDetailsFieldImage extends Component {
               onChange={event => this.handleResizeChange(event)}
             />
             <InputGroup.Addon>%</InputGroup.Addon>
-            
-          </InputGroup>        
-        </FormGroup>       
+
+          </InputGroup>
+        </FormGroup>
         {this.renderAnnotateButton()}
         </div>
-      
+
         <Dropzone
           accept="image/*"
           multiple={false}
@@ -175,7 +153,7 @@ export default class ResearchPlanDetailsFieldImage extends Component {
       );
     }
     let versionOfAnno=this.state.annotation.version;
-    let restOfAnno=field.value.public_name.split(".")[0]+"_annotation_v"+versionOfAnno+".svg";  
+    let restOfAnno=field.value.public_name.split(".")[0]+"_annotation_v"+versionOfAnno+".svg";
     const src = `/images/research_plans/${restOfAnno}`;
     const style = (field.value.zoom == null || typeof field.value.zoom === 'undefined'
     || field.value.width === '') ? { width: 'unset' } : { width: `${field.value.zoom}%` };
