f1df86d5a622e2c99abeb40643402f43922d4750
Johannes Haubold <jh@megorei.com>
2022-08-15 18:35:29 +0200

    Rename omniauth_credential folder to omniauthCredential

--- app/packs/src/apps/omniauthCredential/index.js
+++ app/packs/src/apps/omniauthCredential/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import OmniauthCredential from 'src/apps/omniauth_credential/OmniauthCredential';
+import OmniauthCredential from 'src/apps/omniauthCredential/OmniauthCredential';
 
 document.addEventListener('DOMContentLoaded', () => {
   const domElement = document.getElementById('OmniauthCredential');
