6d0a13457e85f075ec9d495ef26955f7880723c9
Matthias Döring <md@megorei.com>
2022-08-16 18:46:14 +0200

    Cleanup collection management spec

--- spec/features/collection_management_spec.rb
+++ spec/features/collection_management_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 describe 'Collection management' do
   let!(:user) { create(:person) }
-  let!(:collection) { create(:collection, user_id: user.id) }
 
   before do
     user.update!(confirmed_at: Time.now, account_active: true)
@@ -12,23 +11,22 @@ describe 'Collection management' do
   end
 
   describe 'Create an unshared collection' do
+    let(:factory_collection_name) { 'Hello Collection' }
+
     it 'create an unshared collection', js: true do
       # press Collections button (on the left-side tree view)
       find('div.take-ownership-btn').click
-      # press Add(plus) button to add collection
       sleep 2
 
+      # press Add(plus) button to add collection
       find('div.root-actions').find(:xpath, '..').all('button')[0].click
-      # input collection name
-      factory_collection_name = 'Hello Collection'
 
+      # input collection name
       new_collection = all('input.collection-label.form-control').last
       new_collection.click
       new_collection.set(factory_collection_name)
 
-      #find('div.root-actions').find(:xpath, '..').all('button')[0].click
-
-      # find update button to save changes 
+      # find update button to save changes
       find('button#my-collections-update-btn.btn.btn-xs.btn-warning').click
 
       expect(page).to have_content(factory_collection_name)
@@ -38,11 +36,12 @@ describe 'Collection management' do
   end
 
   describe 'Delete an unshared collection' do
+    let!(:collection) { create(:collection, user_id: user.id) }
+    let(:factory_collection_name) { collection.label }
+
     it 'delete an unshared collection', js: true do
       # press Collections button (on the left-side tree view)
       find('div.take-ownership-btn').click
-
-      factory_collection_name = collection.label
 
       # expect before deletion to have content
       expect(page).to have_content(factory_collection_name)
@@ -50,8 +49,6 @@ describe 'Collection management' do
       # press Delete button to delete the collection
       first("div.actions.btn-group [class='btn btn-xs btn-danger']:last-of-type").click
 
-      #find('div.root-actions').find(:xpath, '..').all('button')[0].click
-      
       # press Update button to save
       find('button#my-collections-update-btn.btn.btn-xs.btn-warning').click
 
