43f06542d8969d116eb99a7f35f1961515aba4b2
FabianMauz <fmauz@ipb-halle.de>
2022-08-18 15:11:57 +0200

    Fix merge conflicts

--- db/migrate/20210921114428_update_attachments_with_shrine.rb
+++ db/migrate/20210921114428_update_attachments_with_shrine.rb
@@ -1,26 +1,7 @@
 class UpdateAttachmentsWithShrine < ActiveRecord::Migration[5.2]
   def change
-<<<<<<< HEAD
-<<<<<<< HEAD
-    Attachment.reset_column_information
-    
     Attachment.where(attachment_data: [nil]).find_each do |item|
-=======
-    Attachment.where(attachment_data: [nil]).find_each do |item|
-      next if item.nil? 
->>>>>>> 1277-using-gemshrine-file-service
-      file_path = item.store.path
-      next unless File.exist? file_path
-      item.attachment_attacher.attach(File.open(file_path, binmode: true))
-      if item.valid?
-        item.attachment_attacher.create_derivatives
-        item.save!
-      else
-        File.write('failed_attachement_migrate.log', "#{item.id}: File_path: #{file_path}  Message: #{item.errors.to_h[:attachment]}\n", mode: 'a')
-      end
-=======
-    Attachment.where(attachment_data: [nil]).find_each do |item|
-      next if item.nil? 
+      next if item.nil?
       file_path = item.store.path
       next unless File.exist? file_path
 
@@ -32,7 +13,6 @@ class UpdateAttachmentsWithShrine < ActiveRecord::Migration[5.2]
       end
       item[:attachment_data] = attachment
       item.save
->>>>>>> 1277-using-gemshrine-file-service
     end
   end
 end
--- db/migrate/20220210074909_change_version_type_from_attachment.rb
+++ db/migrate/20220210074909_change_version_type_from_attachment.rb
@@ -1,16 +1,6 @@
 class ChangeVersionTypeFromAttachment < ActiveRecord::Migration[5.2]
   def up
-<<<<<<< HEAD
-<<<<<<< HEAD
-    type = Attachment.column_for_attribute(:version).type
-    return if(type == :string)
-
-=======
     return if Attachment.column_for_attribute('version').type == :string
->>>>>>> 1277-using-gemshrine-file-service
-=======
-    return if Attachment.column_for_attribute('version').type == :string
->>>>>>> 1277-using-gemshrine-file-service
     change_column :attachments, :version, :string, null: true, default: nil
     Attachment.find_each do |attach|
       attach.update_column(:version, nil)
