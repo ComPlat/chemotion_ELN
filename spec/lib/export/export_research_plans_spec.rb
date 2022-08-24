587f1ce51e9f9693e4ee076121715a3e738fe61c
FabianMauz <fmauz@ipb-halle.de>
2022-08-24 14:20:09 +0200

    Use factory bot for creating test data

--- spec/lib/export/export_research_plans_spec.rb
+++ spec/lib/export/export_research_plans_spec.rb
@@ -4,23 +4,24 @@ require 'rails_helper'
 require 'helpers/thumbnail/thumbnail_creator'
 
 describe 'Export::ExportResearchPlan' do
-  context '-> research_plan' do
+  context 'with research_plan' do
     describe '-> export' do
-      it '-> with image in body' do
-        user = create_and_save_user
-        rp = create_and_save_reseachplan(user)
-
+      it 'export with image in body' do
+        rp = create(:research_plan)
         example_svg_annotation = '<svg>example</svg>'
         tempfile = Tempfile.new('annotationFile.svg')
         tempfile.write(example_svg_annotation)
         tempfile.rewind
         tempfile.close
 
-        attachment = create_attachment_without_annotation(rp.id, tempfile)
-        attachment.save!
+        attachment = create(:attachment,
+                           bucket: 1,
+                           filename: 'test',
+                           created_by: 1,
+                           attachable_id: rp.id,
+                           attachment_data: create_annotation_json(tempfile.path))
         update_body_of_researchplan(rp, attachment.identifier)
-
-        exporter = Export::ExportResearchPlan.new(user, rp, 'not relevant')
+        exporter = Export::ExportResearchPlan.new(User.find(rp.created_by), rp, 'not relevant')
 
         file_location = extract_file_location(exporter.to_relative_html)
         assert_equal(file_location, attachment.attachment_data['id'])
@@ -28,34 +29,13 @@ describe 'Export::ExportResearchPlan' do
     end
   end
 
-  def create_and_save_user
-    user = User.new
-    user.email = 'FakeEmail@mail.de'
-    user.password = '123456789'
-    user.first_name = 'First'
-    user.last_name = 'Last'
-    user.name = 'Name'
-    user.name_abbreviation = 'FMA'
-    user.save!
-    user
-  end
-
   def extract_file_location(html)
     left_part = html.split("src='")[1]
     left_part.split("'>")[0]
   end
 
-  def create_and_save_reseachplan(user)
-    rp = ResearchPlan.new
-    rp.creator = user
-    rp.name = 'ABC'
-    rp.body = {}
-    rp.save!
-    rp
-  end
-
-  def update_body_of_researchplan(rp, identifier_of_attachment)
-    rp.body = [
+  def update_body_of_researchplan(research_plan, identifier_of_attachment)
+    research_plan.body = [
       {
         "id": 'entry-003',
         "type": 'image',
@@ -65,26 +45,11 @@ describe 'Export::ExportResearchPlan' do
         }
       }
     ]
-    rp.save!
-    rp
+    research_plan.save!
+    research_plan
   end
 
-  def create_attachment_without_annotation(researchplan_id, tempfile)
-    attachment = Attachment.new(
-      bucket: 1,
-      filename: 'test',
-      file_path: 'tmp',
-      created_by: 1,
-      created_for: 1,
-      content_type: 'svg',
-      attachable_type: 1,
-      attachable_id: researchplan_id
-    )
-    attachment.attachment_data = createAnnotationJson(tempfile.path)
-    attachment
-  end
-
-  def createAnnotationJson(location)
+  def create_annotation_json(location)
     tempfile = Tempfile.new('example.png')
     str = '{'\
         ' "id": "' + tempfile.path + '",'\
@@ -104,5 +69,5 @@ describe 'Export::ExportResearchPlan' do
         '            "mime_type": null'\
         '        }}}}'
     JSON.parse(str)
-      end
+  end
 end
