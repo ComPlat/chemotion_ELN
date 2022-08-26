4ad3fc4986d1f00549f838ea46c2cb323491a60b
Matthias Döring <md@megorei.com>
2022-08-26 14:06:31 +0200

    Prepare new spec file
    - file location for app/api/chemotion/example_api.rb is
      spec/api/chemotion/example_api_spec.rb
    - spec contains one describe block for every defined endpoint (e.g.
      POST /api/v1/attachments/files)
    - describe blocks in spec are in the same order like the endpoint
      definitions

---
+++ spec/api/chemotion/attachment_api_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Chemotion::AttachmentAPI do
+  describe 'DELETE /api/v1/attachments/{attachment_id}' do
+    pending 'work in progress'
+  end
+
+  describe 'DELETE /api/v1/attachments/link/{attachment_id}' do
+    pending 'work in progress'
+  end
+
+  describe 'POST /api/v1/attachments/upload_dataset_attachments' do
+    pending 'work in progress'
+  end
+
+  describe 'POST /api/v1/attachments/upload_chunk' do
+    pending 'work in progress'
+  end
+
+  describe 'POST /api/v1/attachments/upload_chunk_complete' do
+    pending 'work in progress'
+  end
+
+  describe 'POST /api/v1/attachments/upload_to_inbox' do
+    pending 'work in progress'
+  end
+
+  describe 'GET /api/v1/attachments/{attachment_id}' do
+    pending 'work in progress'
+  end
+
+  describe 'GET /api/v1/attachments/zip/{container_id}' do
+    pending 'work in progress'
+  end
+
+  describe 'GET /api/v1/attachments/sample_analyses/{sample_id}' do
+    pending 'work in progress'
+  end
+
+  describe 'GET /api/v1/attachments/image/{attachment_id}' do
+    pending 'work in progress'
+  end
+
+  describe 'GET /api/v1/attachments/thumbnail/{attachment_id}' do
+    pending 'work in progress'
+  end
+
+  describe 'POST /api/v1/attachments/thumbnails' do
+    pending 'work in progress'
+  end
+
+  describe 'POST /api/v1/attachments/files' do
+    pending 'work in progress'
+  end
+
+  describe 'POST /api/v1/attachments/regenerate_spectrum' do
+    pending 'work in progress'
+  end
+
+  describe 'POST /api/v1/attachments/save_spectrum' do
+    pending 'work in progress'
+  end
+
+  describe 'POST /api/v1/attachments/infer' do
+    pending 'work in progress'
+  end
+
+  describe 'GET /api/v1/attachments/svgs' do
+    pending 'work in progress'
+  end
+end
