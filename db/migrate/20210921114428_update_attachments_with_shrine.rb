db116b399c7982a670d7cd820d316a087fd8816b
Danny Truong <allenskywalker92@gmail.com>
2022-09-07 12:06:06 +0200

    refactor code

--- db/migrate/20210921114428_update_attachments_with_shrine.rb
+++ db/migrate/20210921114428_update_attachments_with_shrine.rb
@@ -1,18 +1,17 @@
 class UpdateAttachmentsWithShrine < ActiveRecord::Migration[5.2]
   def change
-    Attachment.where(attachment_data: [nil]).find_each do |item|
-      next if item.nil?
-      file_path = item.store.path
+    Attachment.where(attachment_data: [nil]).find_each do |att|
+      file_path = att.store.path
       next unless File.exist? file_path
 
 
-      attachment = { id: file_path, storage: "store", metadata: { size: item.filesize, filename: item.identifier }}
-      if File.exist? item.store.thumb_path
-        thumbnail = { id: file_path, storage: "store", metadata: { size: item.filesize, filename: item.identifier + 'thumb.jpg' }}
-        attachment = { id: file_path, storage: "store", metadata: { size: item.filesize, filename: item.identifier }, derivatives: { thumbnail: thumbnail }}
+      attachment = { id: file_path, storage: "store", metadata: { size: att.filesize, filename: att.identifier }}
+      if File.exist? att.store.thumb_path
+        thumbnail = { id: file_path, storage: "store", metadata: { size: att.filesize, filename: att.identifier + 'thumb.jpg' }}
+        attachment = { id: file_path, storage: "store", metadata: { size: att.filesize, filename: att.identifier }, derivatives: { thumbnail: thumbnail }}
       end
-      item[:attachment_data] = attachment
-      item.save
+      att[:attachment_data] = attachment
+      att.save
     end
   end
 end
