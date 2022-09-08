ea43f16d7e04d6ce89ee02d1a87ff5d0ffbd8007
Matthias Döring <md@megorei.com>
2022-09-08 13:21:10 +0200

    Fix github action timeout issue
    - Increase timeout
    - Fix the blinking delayed job api spec

--- spec/api/delayed_job_api_spec.rb
+++ spec/api/delayed_job_api_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe Chemotion::AdminAPI do
@@ -8,8 +10,10 @@ describe Chemotion::AdminAPI do
       allow_any_instance_of(WardenAuthentication).to receive(:current_user).and_return(admin1)
     end
 
+
+    # TODO: This spec should be moved to the admin_api_spec
     describe 'DelayedJob' do
-      before(:all) do
+      before do
         RefreshElementTagJob.perform_later
       end
 
@@ -21,16 +25,15 @@ describe Chemotion::AdminAPI do
         expect(json_response['jobs'].size).to eq 1
       end
 
-      # in test Delayed Jobs will not automatically recover from errors -> rescue
-      xit 'let job fail' do
+      it 'let job fail' do
         TestFailureJob.perform_later
         success, failure = Delayed::Worker.new.work_off
         expect([success, failure]).to eq [1, 1]
       end
 
       it 'restart (failed) job' do
-        put '/api/v1/admin/jobs/restart', params: { id: Delayed::Job.select(:id).where(queue: 'test') }
-      rescue RuntimeError => e
+        TestFailureJob.perform_later
+        put '/api/v1/admin/jobs/restart', params: { id: Delayed::Job.select(:id).where(queue: 'test').first.id }
         expect(response.status).to eq 200
       end
     end
