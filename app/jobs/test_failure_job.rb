590196a7c0f476f4a0bdd9bed92bd8572c132bc4
Matthias Döring <md@megorei.com>
2022-09-14 05:53:35 +0200

    Remove github retry workflow (#914)
    * Remove retry workflow for ruby unit test
    
    * Move delayjob spec to the right place
    
    * Remove fake job

--- app/jobs/test_failure_job.rb
+++
@@ -1,9 +0,0 @@
-# for api test in spec/api/delayed_job_api_spec.rb
-class TestFailureJob < ActiveJob::Base
-  queue_as :test
-
-  def perform
-    puts 'failed ' + TestFailureJob.name
-    raise "error"
-  end
-end
--- spec/api/chemotion/admin_api_spec.rb
+++ spec/api/chemotion/admin_api_spec.rb
@@ -1,3 +1,32 @@
-RSpec.describe Chemotion::AdminAPI do
+# frozen_string_literal: true
 
+RSpec.describe Chemotion::AdminAPI do
+  let!(:admin1) { create(:admin) }
+
+  before do
+    allow_any_instance_of(WardenAuthentication).to receive(:current_user).and_return(admin1)
+  end
+
+  describe 'GET /api/v1/admin/jobs' do
+    before do
+      Delayed::Job.create(handler: 'Do something')
+      get '/api/v1/admin/jobs'
+    end
+
+    it 'returns the right http status' do
+      expect(response.status).to eq 200
+    end
+
+    it 'returns a response with jobs' do
+      expect(parsed_json_response['jobs'].size).to eq 1
+    end
+  end
+
+  describe 'PUT /api/v1/admin/jobs/restart' do
+    it 'returns the right http status' do
+      failed_job = Delayed::Job.create(failed_at: DateTime.now, handler: 'Do something')
+      put '/api/v1/admin/jobs/restart', params: { id: failed_job.id }
+      expect(response.status).to eq 200
+    end
+  end
 end
