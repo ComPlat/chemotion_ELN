6f56ad1777f79f1aec69940f6233d7cc548d823f
fmauz <fmauz@ipb-halle.de>
2022-09-19 10:40:33 +0200

    Refactor ResearchPlan.js -> addAttachments()
     * use streaming instead of iterating
     * add tests

--- spec/javascripts/packs/src/models/ResearchPlan.spec.js
+++ spec/javascripts/packs/src/models/ResearchPlan.spec.js
@@ -1,9 +1,27 @@
+import Attachment from 'src/models/Attachment';
+import ResearchPlan from 'src/models/ResearchPlan';
+import expect from 'expect';
 
 describe('ResearchPlan', () => {
-    describe('.addAttachments', () => {
-        it('with two attachments, one already present in researchplan', () => {
+  describe('.addAttachments', () => {
+    const attachmentNotInResearchPlan = new Attachment();
+    const attachmentInResearchPlan = new Attachment();
+    const researchPlan = ResearchPlan.buildEmpty();
 
+    it('with empty list', () => {
+       researchPlan.attachments = [attachmentInResearchPlan];
+       researchPlan.addAttachments([]);
+       expect(researchPlan.attachments[0].id).toEqual(attachmentInResearchPlan.id);
+    });
 
-        });
+    it('with two attachments, one already present in researchplan', () => {
+      researchPlan.attachments = [attachmentInResearchPlan];
+
+      researchPlan.addAttachments([attachmentNotInResearchPlan, attachmentInResearchPlan]);
+
+      expect(researchPlan.attachments.length).toEqual(2);
+      expect(researchPlan.attachments[0].id).toEqual(attachmentInResearchPlan.id);
+      expect(researchPlan.attachments[1].id).toEqual(attachmentNotInResearchPlan.id);
     });
   });
+});
