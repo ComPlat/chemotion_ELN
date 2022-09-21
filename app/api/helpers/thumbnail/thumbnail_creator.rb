48b436b28a7fe07b59d376c92b649dfa7e9a7531
Danny Truong <allenskywalker92@gmail.com>
2022-09-21 11:22:16 +0200

    add rescue for thumbnailcreator

--- app/api/helpers/thumbnail/thumbnail_creator.rb
+++ app/api/helpers/thumbnail/thumbnail_creator.rb
@@ -6,12 +6,13 @@
 # https://github.com/merlin-p/thumbnailer.git
 
 class ThumbnailCreator
-    def initialize(thumbnailer = nil)
-      @thumbnailer = thumbnailer || ThumbnailerWrapper.new
-    end
-  
-    def create_derivative(tmp_path, original_file, db_id, result, record)
+  def initialize(thumbnailer = nil)
+    @thumbnailer = thumbnailer || ThumbnailerWrapper.new
+  end
+
+  def create_derivative(tmp_path, original_file, db_id, result, record)
       thumbnail = @thumbnailer.create_thumbnail(tmp_path)
+
       if thumbnail.present?
         dir = File.dirname(thumbnail)
         thumb_path = "#{dir}/#{record.identifier}.thumb.jpg"
@@ -20,13 +21,17 @@ class ThumbnailCreator
         result[:thumbnail] = File.open(thumb_path, 'rb')
         record[:thumb] = true
       end
-  
+
       result
-    end
-  
-    class ThumbnailerWrapper
-      def create_thumbnail(tmp_path)
-        Thumbnailer.create(tmp_path)
-      end
+    rescue StandardError => e
+      Rails.logger.info('**** ThumbnailCreator failed ***')
+      Rails.logger.error e
     end
   end
+
+  class ThumbnailerWrapper
+    def create_thumbnail(tmp_path)
+      Thumbnailer.create(tmp_path)
+    end
+  end
+end
