05880e347a8195699fb41256aaae93e8180cec93
fmauz <fmauz@ipb-halle.de>
2022-09-21 16:08:58 +0200

    Refactor of test
     * add test for throwing an exception inside thumbnailer

--- spec/api/helpers/thumbnail/thumbnail_creator_spec.rb
+++ spec/api/helpers/thumbnail/thumbnail_creator_spec.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 require 'helpers/thumbnail/thumbnail_creator'
-require 'base64'
-require 'fileutils'
-require 'rails_helper'
 
 describe ThumbnailCreator do
   let(:attachment) { create(:attachment) }
-  let(:temp_file) do
-    Tempfile.new('example.png')
-  end
-
-  before do
+  let(:temp_file) { Tempfile.new('example.png') }
+  let(:creator) { described_class.new }
+  let(:expected_path) do
+    File.join(
+      File.dirname(temp_file.path),
+      attachment.identifier + '.thumb.jpg')
   end
 
   describe '.create_derivative' do
@@ -20,12 +18,23 @@ describe ThumbnailCreator do
         .to receive(:create)
         .and_return(temp_file)
 
+      result = creator.create_derivative(temp_file.path, nil, nil, {}, attachment)
+
+      expect(attachment.thumb).to eq true
+      assert_equal(result[:thumbnail].path, expected_path)
+    end
+
+    it 'error at creating thumbnail' do
+      allow_any_instance_of(Thumbnailer)
+        .to receive(:create)
+        .and_raise('An error occurred')
+
       creator = described_class.new
       result = creator.create_derivative(temp_file.path, nil, nil, {}, attachment)
 
-      assert(attachment.thumb)
-      expected_path = File.dirname(temp_file.path) + '/' + attachment.identifier + '.thumb.jpg'
-      assert_equal(result[:thumbnail].path, expected_path)
+      expect(attachment.thumb).to eq false
+      expect(attachment.attachment_data).to eq nil
+      expect(result).to eq ({})
     end
   end
 end
