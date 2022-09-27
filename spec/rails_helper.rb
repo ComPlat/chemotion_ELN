f6cdb3a5def7291418a7e348dc2df8042a1a2008
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-09-27 18:22:11 +0200

    Report code coverage on CI.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 
 SimpleCov.start 'rails' do
   add_group 'GraphQL', 'app/graphql'
