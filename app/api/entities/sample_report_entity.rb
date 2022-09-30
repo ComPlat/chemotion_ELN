11e6d43fc54eed4fb2a884b9bb2bcc76f27992dd
Johannes Haubold <jh@megorei.com>
2022-09-30 17:33:25 +0200

    Add anonymization to SampleReportEntity

--- app/api/entities/sample_report_entity.rb
+++ app/api/entities/sample_report_entity.rb
@@ -1,20 +1,28 @@
 # frozen_string_literal: true
 
+# rubocop:disable Layout/ExtraSpacing
 module Entities
   class SampleReportEntity < SampleEntity
-    expose(
-      :reactions,
-      :molecule_iupac_name,
-      :get_svg_path,
-      :literatures
-    )
+    with_options(anonymize_below: 0) do
+      expose :collections,                              using: 'Entities::CollectionEntity'
+    end
+
+    with_options(anonymize_below: 2) do
+      expose! :analyses,            anonymize_with: [], using: 'Entities::ContainerEntity'
+    end
+
+    with_options(anonymize_below: 10) do
+      expose! :reactions,           anonymize_with: [], using: 'Entities::ReationReportEntity'
+      expose! :molecule_iupac_name, anonymize_with: nil
+      expose! :get_svg_path,        anonymize_with: nil
+      expose! :literatures,         anonymize_with: [], using: 'Entities::LiteratureEntity',   with_user_info: true
+    end
 
     expose_timestamps
-
-    expose :collections, using: 'Entities::CollectionEntity'
 
     def literatures
       Literature.by_element_attributes_and_cat(object.id, 'Sample', 'detail').with_user_info
     end
   end
 end
+# rubocop:enable Layout/ExtraSpacing
