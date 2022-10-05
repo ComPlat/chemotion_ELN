a9ab91133f993714fa40d7ddf3ea7c0d23aeaf8e
Mehreen <mehreen.mansur>
2022-10-05 11:23:03 +0200

    add entity and collection actions for collection_acl

--- app/packs/src/fetchers/CollectionsFetcher.js
+++ app/packs/src/fetchers/CollectionsFetcher.js
@@ -79,6 +79,36 @@ export default class CollectionsFetcher {
     return promise;
   }
 
+  static fetchMyRoots() {
+    let promise = fetch('/api/v1/temp_collections/all', {
+      credentials: 'same-origin'
+    })
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        return json;
+      }).catch((errorMessage) => {
+        console.log(errorMessage);
+      });
+
+    return promise;
+  }
+
+  static fetchSharedWithMeRoots() {
+    let promise = fetch('/api/v1/temp_collections/shared', {
+      credentials: 'same-origin'
+    })
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        return json;
+      }).catch((errorMessage) => {
+        console.log(errorMessage);
+      });
+
+    return promise;
+  }
+
   static fetchSharedRoots() {
     let promise = fetch('/api/v1/collections/shared_roots.json', {
       credentials: 'same-origin'
