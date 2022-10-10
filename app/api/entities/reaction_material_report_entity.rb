22ad6db7c54b899ec3a7e1d5d829c4977442a675
Johannes Haubold <jh@megorei.com>
2022-10-10 10:19:22 +0200

    Fix reaction material Entity to display analyses properly

--- app/api/entities/reaction_material_report_entity.rb
+++ app/api/entities/reaction_material_report_entity.rb
@@ -4,28 +4,33 @@ module Entities
   # wraps a ReactionsSample object like Entities::ReactionMaterialEntity
   # adds some additional fields for reports
   class ReactionMaterialReportEntity < ReactionMaterialEntity
-    with_options(anonymize_below: 0) do
-      expose! :amount_g
-      expose! :amount_ml
-      expose! :amount_mmol
-      expose! :get_svg_path
-      expose! :preferred_label
-      expose! :preferred_tag
-      expose! :real_amount_g
-      expose! :real_amount_ml
-      expose! :real_amount_mmol
-    end
+    expose! :amount_g
+    expose! :amount_ml
+    expose! :amount_mmol
+    expose! :get_svg_path
+    expose! :preferred_label
+    expose! :preferred_tag
+    expose! :real_amount_g
+    expose! :real_amount_ml
+    expose! :real_amount_mmol
 
-    with_options(anonymize_below: 2, anonymize_with: []) do
-      expose! :analyses, using: 'Entities::ContainerEntity'
-    end
+    # This entity does not wrap a sample but a ReactionsSample instance
+    # Therefore we cannot use anonymize_below (it would check detail_levels[ReactionsSample] or one of its
+    # polymorphic subclasses). Instead we do the check manually in the #analyses method below
+    expose! :analyses, using: 'Entities::ContainerEntity'
 
     private
 
     delegate(
-      :amount_g, :amount_ml, :amount_mmol, :analyses, :get_svg_path, :preferred_label, :preferred_tag,
+      :amount_g, :amount_ml, :amount_mmol, :get_svg_path, :preferred_label, :preferred_tag,
       to: :"object.sample"
     )
+
+    def analyses
+      return [] if detail_levels[Sample] < 2
+
+      object.sample.analyses
+    end
 
     def real_amount_g
       object.sample.amount_g(:real)
