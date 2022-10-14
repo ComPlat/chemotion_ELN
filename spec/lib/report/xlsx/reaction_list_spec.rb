71ad04747fd1a74c8c70828e81a13bd655ac9429
Matthias Döring <md@megorei.com>
2022-10-14 13:21:15 +0200

    Disable spec with missing dependency

--- spec/lib/report/xlsx/reaction_list_spec.rb
+++ spec/lib/report/xlsx/reaction_list_spec.rb
@@ -4,9 +4,8 @@ require 'rails_helper'
 
 describe Reporter::Xlsx::ReactionList do
   let(:file_extension) { '.xlsx' }
-  include_context 'Report shared declarations'
-  pending "Requires Roo 2.9.0 to work properly" do
-    raise 'Missing Roo 2.9.0'
-    # it_behaves_like 'Rinchi Xlsx/Csv formats'
-  end
+
+  pending 'Requires Roo 2.9.0 to work properly'
+  # include_context 'Report shared declarations'
+  # it_behaves_like 'Rinchi Xlsx/Csv formats'
 end
