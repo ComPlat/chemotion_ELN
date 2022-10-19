6b831cc2e177526e4a804825e9d7747341cc5ea1
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-10-19 13:28:45 +0200

    Refactored CI. (#952)

--- .github/workflows/config.sh
+++
@@ -1,6 +0,0 @@
-cd chemotion_ELN/config
-sudo cp database.yml.gitlab database.yml
-sudo cp -f storage.yml.example storage.yml
-sudo touch datacollectors.yml
-sudo cp -f profile_default.yml.example profile_default.yml
-sudo touch klasses.json
