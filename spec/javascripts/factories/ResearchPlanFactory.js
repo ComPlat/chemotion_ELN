ee08a46856317f26e3f66b8756862034a8bd76f1
fmauz <fmauz@ipb-halle.de>
2022-10-25 13:05:30 +0200

    Refactor of ResearchPlanFactory
     * now uses pseudo uuids and pseudo checksums for reproducability

--- spec/javascripts/factories/ResearchPlanFactory.js
+++ spec/javascripts/factories/ResearchPlanFactory.js
@@ -1,7 +1,8 @@
-import { factory } from "factory-bot";
-import ResearchPlan from "src/models/ResearchPlan";
-import Container from "src/models/Container";
-import AttachmentFactory from "factories/AttachmentFactory";
+import { factory } from 'factory-bot';
+import ResearchPlan from 'src/models/ResearchPlan';
+import AttachmentFactory from 'factories/AttachmentFactory';
+import PseudoRandomUUIDGenerator from 'factories/PseudoRandomUUIDGenerator';
+import sha256 from 'sha256';
 
 export default class ResearchPlanFactory {
   static instance = undefined;
@@ -17,45 +18,68 @@ export default class ResearchPlanFactory {
   constructor() {
     this.factory = factory;
 
-    this.factory.define("empty", ResearchPlan, {
-      collection_id: 0,
-      type: "research_plan",
-      name: "New Research Plan",
-      body: [],
-      mode: "edit",
-      container: Container.init(),
-      changed: true,
-      can_update: true,
-      attachments: [],
-      wellplates: [],
-      segments: [],
-    });
+    this.factory.define(
+      'empty',
+      ResearchPlan,
+      () => {
+        const researchPlan = ResearchPlan.buildEmpty();
+        return researchPlan;
+      },
+      {
+        afterBuild: (model, attrs, buildOptions) => {
+          this.setPseudoIdsAndChecksums(model, buildOptions);
+          return model;
+        }
+      }
+    );
 
-    this.factory.extend("empty", "with_not_image_body_field",{
+    this.factory.extend('empty', 'with_not_image_body_field', {
       body: [{
-        id: "entry-002",
-        type: "no-image",
+        id: 'entry-002',
+        type: 'no-image',
         value: {}
       }],
       changed: false,
-      attachments : [ AttachmentFactory.build("new")]
+      attachments: [AttachmentFactory.build('new')]
 
     });
 
-    this.factory.define("with_image_body_field", ResearchPlan, async () => {
-      const attachment = await AttachmentFactory.build("new");
-      const researchPlan = await ResearchPlanFactory.build("empty");
-      researchPlan.attachments.push(attachment);
-      researchPlan.changed = false;
-      researchPlan.body = [{
-        id: 'entry-001',
-        type: 'image',
-        value: {
-          file_name: 'xyz.png',
-          public_name: attachment.identifier,
+    this.factory.define(
+      'with_image_body_field',
+      ResearchPlan,
+      async () => {
+        const attachment = await AttachmentFactory.build('new');
+        const researchPlan = await ResearchPlanFactory.build('empty');
+        researchPlan.attachments.push(attachment);
+        researchPlan.changed = false;
+        researchPlan.body = [{
+          id: 'entry-001',
+          type: 'image',
+          value: {
+            file_name: 'xyz.png',
+            public_name: attachment.identifier,
+          }
+        }];
+        return researchPlan;
+      },
+      {
+        afterBuild: (model, attrs, buildOptions) => {
+          this.setPseudoIdsAndChecksums(model, buildOptions);
+          return model;
         }
-      }];
-      return researchPlan;
-    });
+      }
+    );
+  }
+
+  setPseudoIdsAndChecksums(model, buildOptions) {
+    if (buildOptions.reset) {
+      PseudoRandomUUIDGenerator.reset();
+    }
+    model.id = PseudoRandomUUIDGenerator.createNextUUID();
+    model.updateChecksum(sha256(PseudoRandomUUIDGenerator.createNextUUID()));
+    model.container.updateChecksum(sha256(PseudoRandomUUIDGenerator.createNextUUID()));
+    model.container.id = PseudoRandomUUIDGenerator.createNextUUID();
+    model.container.children[0].id = PseudoRandomUUIDGenerator.createNextUUID();
+    model.container.children[0].updateChecksum(sha256(PseudoRandomUUIDGenerator.createNextUUID()));
   }
 }
