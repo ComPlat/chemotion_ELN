59d316d6cacd0dfa1c877290d537c457853d7747
fmauz <fmauz@ipb-halle.de>
2022-10-25 13:23:35 +0200

    Formatting

--- spec/javascripts/packs/src/apps/mydb/elements/details/researchPlans/ImageAttachmentFilter.spec.js
+++ spec/javascripts/packs/src/apps/mydb/elements/details/researchPlans/ImageAttachmentFilter.spec.js
@@ -13,19 +13,35 @@ describe('ImageAttachmentFilter', () => {
   const attachmentInBodyButNoImage = new Attachment();
   describe('.filterAttachmentsWhichAreInBody', () => {
     it('filterAttachmentsWhichAreInBody (no attachments)', () => {
-      const rp = createRpWithBody(attachmentInBody.identifier, attachmentInBodyButNoImage.identifier);
+      const rp = createRpWithBody(
+        attachmentInBody.identifier,
+        attachmentInBodyButNoImage.identifier
+      );
 
       const filter = new ImageAttachmentFilter();
-      const filteredAttachments = filter.filterAttachmentsWhichAreInBody(rp.body, rp.attachments);
+      const filteredAttachments = filter.filterAttachmentsWhichAreInBody(
+        rp.body,
+        rp.attachments
+      );
       expect(filteredAttachments.length).toEqual(0);
     });
 
     it('filterAttachmentsWhichAreInBody (no body)', () => {
-      const rp = createRpWithBody(attachmentInBody.identifier, attachmentInBodyButNoImage.identifier);
-      rp.attachments = [attachmentNotInBody1, attachmentInBody, attachmentInBodyButNoImage];
+      const rp = createRpWithBody(
+        attachmentInBody.identifier,
+        attachmentInBodyButNoImage.identifier
+      );
+      rp.attachments = [
+        attachmentNotInBody1,
+        attachmentInBody,
+        attachmentInBodyButNoImage,
+      ];
 
       const filter = new ImageAttachmentFilter();
-      const filteredAttachments = filter.filterAttachmentsWhichAreInBody([], rp.attachments);
+      const filteredAttachments = filter.filterAttachmentsWhichAreInBody(
+        [],
+        rp.attachments
+      );
       expect(filteredAttachments.length).toEqual(3);
     });
 
@@ -33,11 +49,21 @@ describe('ImageAttachmentFilter', () => {
       const attachmentNotInBody1 = new Attachment();
       const attachmentInBody = new Attachment();
       const attachmentInBodyButNoImage = new Attachment();
-      const rp = createRpWithBody(attachmentInBody.identifier, attachmentInBodyButNoImage.identifier);
-      rp.attachments = [attachmentNotInBody1, attachmentInBody, attachmentInBodyButNoImage];
+      const rp = createRpWithBody(
+        attachmentInBody.identifier,
+        attachmentInBodyButNoImage.identifier
+      );
+      rp.attachments = [
+        attachmentNotInBody1,
+        attachmentInBody,
+        attachmentInBodyButNoImage,
+      ];
 
       const filter = new ImageAttachmentFilter();
-      const filteredAttachments = filter.filterAttachmentsWhichAreInBody(rp.body, rp.attachments);
+      const filteredAttachments = filter.filterAttachmentsWhichAreInBody(
+        rp.body,
+        rp.attachments
+      );
       expect(filteredAttachments.length).toEqual(2);
     });
   });
@@ -45,21 +71,23 @@ describe('ImageAttachmentFilter', () => {
 
 function createRpWithBody(identifierImage, identifierNoImage) {
   const rp = ResearchPlan.buildEmpty();
-  rp.body = [{
-    id: 'entry-001',
-    type: 'image',
-    value: {
-      file_name: 'xyz.png',
-      public_name: identifierImage,
-    }
-  },
-  {
-    id: 'entry-003',
-    type: 'no-image',
-    value: {
-      file_name: 'xyz.png',
-      public_name: identifierNoImage
-    }
-  }];
+  rp.body = [
+    {
+      id: 'entry-001',
+      type: 'image',
+      value: {
+        file_name: 'xyz.png',
+        public_name: identifierImage,
+      },
+    },
+    {
+      id: 'entry-003',
+      type: 'no-image',
+      value: {
+        file_name: 'xyz.png',
+        public_name: identifierNoImage,
+      },
+    },
+  ];
   return rp;
 }
--- spec/javascripts/packs/src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails.spec.js
+++ spec/javascripts/packs/src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails.spec.js
@@ -1,33 +1,34 @@
-import React from "react";
-import expect from "expect";
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import React from 'react';
+import expect from 'expect';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { describe, it } from 'mocha';
 
-import ResearchPlanFactory from "factories/ResearchPlanFactory";
-import AttachmentFactory from "factories/AttachmentFactory";
-import uuid from "uuid";
+import ResearchPlanFactory from 'factories/ResearchPlanFactory';
+import AttachmentFactory from 'factories/AttachmentFactory';
 // although not used import of ElementStore needed to initialize
 // the ResearchPlanDetails.js component. It must be executed before
 // the import of ResearchPlanDetails. Beware of the linter which
 // may put it at the end of the imports !!!
 
 // eslint-disable-next-line no-unused-vars
-import ElementStore from "src/stores/alt/stores/ElementStore";
+import ElementStore from 'src/stores/alt/stores/ElementStore';
 
-import ResearchPlanDetails from "src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails";
+import ResearchPlanDetails from 'src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails';
 
 Enzyme.configure({
   adapter: new Adapter(),
 });
 
-describe("ResearchPlanDetails", async () => {
-  const FIELD_ID_IMAGE = "entry-001";
-  const FIELD_ID_NO_IMAGE = "entry-002";
-  describe(".handleBodyChange", async () => {
-    context("on non existing field", async () => {
-      it(" expecting nothing was changed", async () => {
+describe('ResearchPlanDetails', async () => {
+  const FIELD_ID_IMAGE = 'entry-001';
+  const FIELD_ID_NO_IMAGE = 'entry-002';
+
+  describe('.handleBodyChange', async () => {
+    describe('on non existing field', async () => {
+      it(' expecting nothing was changed', async () => {
         const researchPlanWithImage = await ResearchPlanFactory.build(
-          "with_image_body_field"
+          'with_image_body_field'
         );
         const wrapper = shallow(
           <ResearchPlanDetails
@@ -35,16 +36,16 @@ describe("ResearchPlanDetails", async () => {
             toggleFullScreen={() => {}}
           />
         );
-        wrapper.instance().handleBodyChange({}, "nonExistingFieldId", []);
+        wrapper.instance().handleBodyChange({}, 'nonExistingFieldId', []);
 
         expect(researchPlanWithImage.changed).toEqual(false);
       });
     });
 
-    context("on non image field", async () => {
-      it(" expected to be changed", async () => {
+    describe('on non image field', async () => {
+      it(' expected to be changed', async () => {
         const researchPlanWithoutImage = await ResearchPlanFactory.build(
-          "with_not_image_body_field"
+          'with_not_image_body_field'
         );
 
         const wrapper = shallow(
@@ -56,7 +57,7 @@ describe("ResearchPlanDetails", async () => {
 
         wrapper.instance().handleBodyChange(
           {
-            test: "fakeValue",
+            test: 'fakeValue',
           },
           FIELD_ID_NO_IMAGE,
           []
@@ -67,28 +68,26 @@ describe("ResearchPlanDetails", async () => {
         expect(researchPlanWithoutImage.body).toEqual([
           {
             id: FIELD_ID_NO_IMAGE,
-            type: "no-image",
+            type: 'no-image',
             value: {
-              test: "fakeValue",
+              test: 'fakeValue',
             },
           },
         ]);
       });
     });
-    context("replacing an image field for the first time", async () => {
-      it("expecting to be replaced", async () => {
+    describe('replacing an image field for the first time', async () => {
+      it('expecting to be replaced', async () => {
         const researchPlanWithImage = await ResearchPlanFactory.build(
-          "with_image_body_field"
+          'with_image_body_field'
         );
-        const newImageAttachment = await AttachmentFactory.build("new", {
-          id: uuid.v1(),
-        });
+        const newImageAttachment = await AttachmentFactory.build('new');
 
         const expectedField = {
           id: FIELD_ID_IMAGE,
-          type: "image",
+          type: 'image',
           value: {
-            file_name: "abc.png",
+            file_name: 'abc.png',
             public_name: newImageAttachment.identifier,
           },
         };
@@ -114,26 +113,24 @@ describe("ResearchPlanDetails", async () => {
         expect(researchPlanWithImage.body).toEqual([expectedField]);
       });
     });
-    context(
-      "replacing an image field for the second time - replacing an temporary image",
+    describe(
+      'replacing an image field for the second time - replacing an temporary image',
       async () => {
-        it("expecting to be replaced with old value in memory", async () => {
-          const attachmentToAdd = await AttachmentFactory.build("new", {
-            id: uuid.v1(),
-          });
+        it('expecting to be replaced with old value in memory', async () => {
+          const attachmentToAdd = await AttachmentFactory.build('new');
           const researchPlanWithImage = await ResearchPlanFactory.build(
-            "with_image_body_field"
+            'with_image_body_field'
           );
 
           const newValue = {
-            file_name: "abc.png",
+            file_name: 'abc.png',
             public_name: attachmentToAdd.identifier,
             old_value: researchPlanWithImage.attachments[0].identifier,
           };
 
           const expectedField = {
             id: FIELD_ID_IMAGE,
-            type: "image",
+            type: 'image',
             value: newValue,
           };
 
@@ -154,29 +151,25 @@ describe("ResearchPlanDetails", async () => {
         });
       }
     );
-    context(
-      "replacing an image field for the second time - replacing an already persisted image",
+    describe(
+      'replacing an image field for the second time - replacing an already persisted image',
       async () => {
-        it("expecting to be replaced with old value in memory", async () => {
-          const attachmentToAdd = await AttachmentFactory.build("new", {
-            id: uuid.v1(),
-          });
-          const researchPlanWithImage = await ResearchPlanFactory.build(
-            "with_image_body_field"
-          );
+        it('expecting to be replaced with old value in memory', async () => {
+          const researchPlanWithImage = await ResearchPlanFactory.build('with_image_body_field', {}, { reset: true });
+          const attachmentToAdd = await AttachmentFactory.build('new');
+
           researchPlanWithImage.attachments[0].file = [];
-          researchPlanWithImage.attachments[0].file.preview =
-            researchPlanWithImage.attachments[0].identifier;
-          researchPlanWithImage.attachments[0].identifier = "none";
+          researchPlanWithImage.attachments[0].file.preview = researchPlanWithImage.attachments[0].identifier;
+          researchPlanWithImage.attachments[0].identifier = 'none';
 
           const newValue = {
-            file_name: "abc.png",
+            file_name: 'abc.png',
             public_name: attachmentToAdd.identifier,
             old_value: researchPlanWithImage.attachments[0].file.preview,
           };
           const expectedField = {
             id: FIELD_ID_IMAGE,
-            type: "image",
+            type: 'image',
             value: newValue,
           };
 
