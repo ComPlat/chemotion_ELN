7fee6d0d6896ec10a53c8e1130b8783c91c25aa6
Matthias Döring <md@megorei.com>
2022-10-26 12:46:51 +0200

    Add sentry to mydb frontend app (#994)

--- app/packs/src/apps/mydb/index.js
+++ app/packs/src/apps/mydb/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import Aviator from 'aviator';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
@@ -9,6 +11,17 @@ import initRoutes from 'src/apps/mydb/routes';
 import { RootStore, StoreContext } from 'src/stores/mobx/RootStore';
 
 const AppWithDnD = DragDropContext(HTML5Backend)(App);
+
+Sentry.init({
+  sendClientReports: false,
+  dsn: process.env.SENTRY_FRONTENT_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: process.env.SENTRY_FRONTENT_SAMPLE_RATE,
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   const domElement = document.getElementById('app');
--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -11,7 +11,9 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.version': JSON.stringify(process.version)
+      'process.version': JSON.stringify(process.version),
+      'process.env.SENTRY_FRONTENT_DSN': JSON.stringify(process.env.SENTRY_FRONTENT_DSN),
+      'process.env.SENTRY_FRONTENT_SAMPLE_RATE': JSON.stringify(process.env.SENTRY_FRONTENT_SAMPLE_RATE)
     }),
     new webpack.ProvidePlugin({
       process: 'process/browser',
