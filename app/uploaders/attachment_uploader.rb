ddc62ea0eb87833f6f76329bc0fc49038b749424
fmauz <fmauz@ipb-halle.de>
2022-11-07 15:00:45 +0100

    resolve rubocop comments

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -7,6 +7,7 @@ class AttachmentUploader < Shrine
   require 'helpers/derivative_builder_factory'
 
   MAX_SIZE = Rails.configuration.shrine_storage.maximum_size * 1024 * 1024 # 10 MB
+  MAX_FILES_IN_FOLDER = 10_000
 
   plugin :derivatives
   plugin :keep_files, replaced: true
@@ -14,21 +15,17 @@ class AttachmentUploader < Shrine
   plugin :pretty_location
   plugin :remove_attachment
   Attacher.validate do
-    validate_max_size MAX_SIZE, message: "File #{record.filename} cannot be uploaded. File size must be less than #{Rails.configuration.shrine_storage.maximum_size} MB"
+    validate_max_size MAX_SIZE,
+                      message: "File #{record.filename} cannot be uploaded." \
+                               'File size must be less than ' \
+                               "#{Rails.configuration.shrine_storage.maximum_size} MB"
   end
 
   def generate_location(io, context = {})
     if context[:record]
-      file_name = if io.path.include? 'thumb.jpg'
-                    "#{context[:record][:key]}.thumb.jpg"
-                  elsif io.path.include? 'annotation.svg'
-                    "#{context[:record][:key]}.annotation.svg"
-                  else
-                    "#{context[:record][:key]}#{File.extname(context[:record][:filename])}"
-                  end
-
+      file_name = generate_file_name(io, context)
       bucket = 1
-      bucket = (context[:record][:id] / 10_000).floor + 1 if context[:record][:id].present?
+      bucket = (context[:record][:id] / MAX_FILES_IN_FOLDER).floor + 1 if context[:record][:id].present?
       "#{storage.directory}/#{bucket}/#{file_name}"
     else
       super
@@ -43,8 +40,7 @@ class AttachmentUploader < Shrine
 
     file_path = AttachmentUploader.create_tmp_file(file_basename, file_extension, file)
 
-
-    result=AttachmentUploader.create_derivatives(file_extension, file_path, original, @context[:record].id, record)
+    result = AttachmentUploader.create_derivatives(file_extension, file_path, original, @context[:record].id, record)
 
     result
   end
@@ -65,18 +61,25 @@ class AttachmentUploader < Shrine
 
   def self.create_derivatives(file_extension, file_path, original, attachment_id, record)
     result = {}
-    factory = DerivativeBuilderFactory.new
 
+    factory = DerivativeBuilderFactory.new
     builders = factory.create_derivative_builders(file_extension)
     builders.each do |builder|
       builder.create_derivative(
-        file_path.to_s,
-        original,
-        attachment_id,
-        result, record
+        file_path.to_s, original, attachment_id, result, record
       )
     end
 
     result
+  end
+
+  def generate_file_name(file_stream, context)
+    attachment_key = context[:record][:key]
+
+    file_ending = File.extname(context[:record][:filename])
+    file_ending = "#{attachment_key}.thumb.jpg" if file_stream.path.end_with? 'thumb.jpg'
+    file_ending = "#{attachment_key}.annotation.svg" if file_stream.path.end_with? 'annotation.svg'
+
+    "#{attachment_key}#{file_ending}"
   end
 end
