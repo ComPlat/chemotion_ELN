414aaa26f3b5af0f6987a037ceb028b7011c2411
PiTrem <pierre.tremouilhac@kit.edu>
2022-11-17 16:53:43 +0100

    Ketcher1 generated sample svg service (#1028)
    * Ketcher service (#912) use ketcher render service
    
    * ketcherservice to return nil when error raise so that svg rendering fallback to openbabel
    
    Co-authored-by: PiTrem <pierre.tremouilhac@kit.edu>
    
    * render_svg does now support HTTPs URLs (#975)
    
    Co-authored-by: ptrxyz <peter.krauss@kit.edu>

--- app/api/chemotion/molecule_api.rb
+++ app/api/chemotion/molecule_api.rb
@@ -71,9 +71,9 @@ module Chemotion
             svg_file_src = Rails.public_path.join('images', 'molecules', molecule.molecule_svg_file)
             if File.exist?(svg_file_src)
               mol = molecule.molfile.lines[0..1]
-              if mol[1]&.strip&.match?('OpenBabel')
-                svg = File.read(svg_file_src)
-                svg_process = SVG::Processor.new.structure_svg('openbabel', svg, svg_digest)
+              if svg.nil? || svg&.include?('Open Babel')
+                svg = Molecule.svg_reprocess(svg, molecule.molfile)
+                svg_process = SVG::Processor.new.structure_svg('ketcher', svg, svg_digest, true)
               else
                 FileUtils.cp(svg_file_src, svg_process[:svg_file_path])
               end
