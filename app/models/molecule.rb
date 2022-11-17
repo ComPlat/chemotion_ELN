1d381462087dea31758e3b53c2ff09258ec79ed4
PiTrem <pierre.tremouilhac@kit.edu>
2022-11-17 16:53:43 +0100

    Ketcher1 generated sample svg service (#1028)
    * Ketcher service (#912) use ketcher render service
    
    * ketcherservice to return nil when error raise so that svg rendering fallback to openbabel
    
    Co-authored-by: PiTrem <pierre.tremouilhac@kit.edu>
    
    * render_svg does now support HTTPs URLs (#975)
    
    Co-authored-by: ptrxyz <peter.krauss@kit.edu>

--- app/models/molecule.rb
+++ app/models/molecule.rb
@@ -95,7 +95,7 @@ class Molecule < ApplicationRecord
       molecule.molfile = is_partial && partial_molfile || molfile
       molecule.assign_molecule_data(babel_info, pubchem_info)
     end
- 
+
     molecule.ob_log = babel_info[:ob_log]
     molecule
   end
@@ -136,11 +136,10 @@ class Molecule < ApplicationRecord
     self.iupac_name = pubchem_info[:iupac_name]
     self.names = pubchem_info[:names]
     self.pcid = pubchem_info[:cid]
-    self.check_sum_formular # correct exact and average MW for resins
 
-    #self.attach_svg babel_info[:svg]
-    svg = Chemotion::OpenBabelService.svg_from_molfile(self.molfile)
-    self.attach_svg svg
+    check_sum_formular
+    svg = Molecule.svg_reprocess(babel_info[:svg], molfile)
+    attach_svg svg
 
     self.cano_smiles = babel_info[:cano_smiles]
     self.molfile_version = babel_info[:molfile_version]
@@ -153,12 +152,12 @@ class Molecule < ApplicationRecord
     mol_tag = self.tag
     mol_tag_data = mol_tag.taggable_data || {}
     if mol_tag_data['pubchem_lcss'] && mol_tag_data['pubchem_lcss'].length > 0
-      mol_tag_data['pubchem_lcss'];
+      mol_tag_data['pubchem_lcss']
     else
       mol_tag_data['pubchem_lcss'] = Chemotion::PubchemService.lcss_from_cid(cid)
       # updated_at of element_tags(not molecule) is updated
       mol_tag.update_attributes taggable_data: mol_tag_data
-      mol_tag_data['pubchem_lcss'];
+      mol_tag_data['pubchem_lcss']
     end
   end
 
@@ -244,6 +243,20 @@ class Molecule < ApplicationRecord
   def unique_molecule_name(new_name)
     mns = molecule_names.map(&:name)
     !mns.include?(new_name)
+  end
+
+  def self.svg_reprocess(svg, molfile)
+    return svg unless Rails.configuration.try(:ketcher_service).try(:url).present?
+    return svg if svg.present? && !svg&.include?('Open Babel')
+
+    svg = KetcherService::RenderSvg.svg(molfile)
+
+    if svg&.present?
+      svg = Ketcherails::SVGProcessor.new(svg)
+      svg.centered_and_scaled_svg
+    else
+      Chemotion::OpenBabelService.svg_from_molfile(molfile)
+    end
   end
 
 private
