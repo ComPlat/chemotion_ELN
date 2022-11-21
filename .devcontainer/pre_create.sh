68ee906c24dc5a46b73f3cdb0159d252fc6c2d55
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-11-21 11:33:18 +0100

    Initialize repository before running devcontainer (#1048)

---
+++ .devcontainer/pre_create.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# enable configuration files
+cp .env.development .env
+cp public/welcome-message-sample.md public/welcome-message.md
+cp config/datacollectors.yml.example config/datacollectors.yml
+cp config/storage.yml.example config/storage.yml
+cp config/database.yml.example config/database.yml
+cp config/shrine_config.yml.example config/shrine_config.yml
