659dea005ac2f203de70efba2e681c35de79e3e2
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-11-21 09:42:19 +0000

    Initialize repository before running devcontainer

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -5,12 +5,5 @@ bundle update --bundler # update the bundler version in Gemfile.lock to the inst
 bundle install
 yarn install
 
-# enable configuration files
-cp public/welcome-message-sample.md public/welcome-message.md
-cp config/datacollectors.yml.example config/datacollectors.yml
-cp config/storage.yml.example config/storage.yml
-cp config/database.yml.example config/database.yml
-cp config/shrine_config.yml.example config/shrine_config.yml
-
 # set up database
 bundle exec rake db:setup
