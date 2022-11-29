24b414b23624e5d55a05b9950214f266e22aced4
Matthias Döring <md@megorei.com>
2022-11-29 19:30:14 +0100

    Fix production mode (#1061)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# TODO: Remove this workaround after update autoload configuration to current rails standards
+require_relative '../../lib/chemotion/oneline_log_formatter'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -69,7 +74,7 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :subdomain, :uuid ]
+  config.log_tags = %i[subdomain uuid]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -98,8 +103,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger           = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
