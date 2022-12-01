64bab46ba33714750a71c34d1b6f74ef356f4968
Johannes Haubold <jh@megorei.com>
2022-12-01 17:01:31 +0100

    Fix DND integration in Admin App

--- app/packs/src/apps/admin/index.js
+++ app/packs/src/apps/admin/index.js
@@ -4,9 +4,14 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import AdminHome from 'src/apps/admin/AdminHome';
 
-const AdminHomeWithDnD = (<DndProvider backend={HTML5Backend}><AdminHome /></DndProvider>);
-
 document.addEventListener('DOMContentLoaded', () => {
   const domElement = document.getElementById('AdminHome');
-  if (domElement) { ReactDOM.render(<AdminHomeWithDnD />, domElement); }
+  if (domElement) {
+    ReactDOM.render(
+      <DndProvider backend={HTML5Backend}>
+        <AdminHome />
+      </DndProvider>,
+      domElement
+    );
+  }
 });
