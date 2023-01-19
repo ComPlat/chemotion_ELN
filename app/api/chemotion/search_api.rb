70c479eff07bc7a3d05467b846bbab58d7af6ade
Beate Quednau <beate@firma-pflaum.de>
2023-01-19 13:11:21 +0100

    Fix rubocop errors

--- app/api/chemotion/search_api.rb
+++ app/api/chemotion/search_api.rb
@@ -207,15 +207,15 @@ module Chemotion
         serialized = Entities::SampleEntity.represent(
           sample,
           detail_levels: detail_levels,
-          displayed_in_list: true
+          displayed_in_list: true,
         ).serializable_hash
         serialized_scope.push(serialized)
       end
 
       def serialized_result_by_id(element, serialized_scope)
+        entities = "Entities::#{id_params['model_name'].capitalize}Entity".constantize
         serialized =
-          "Entities::#{id_params['model_name'].capitalize}Entity".constantize
-            .represent(s, displayed_in_list: true).serializable_hash
+          entities.represent(element, displayed_in_list: true).serializable_hash
         serialized_scope.push(serialized)
       end
 
@@ -537,7 +537,7 @@ module Chemotion
       end
 
       namespace :by_ids do
-        desc "Return elements by ids"
+        desc 'Return elements by ids'
         params do
           use :search_params
         end
@@ -549,9 +549,10 @@ module Chemotion
         post do
           scope = search_by_ids(@c_id)
           return unless scope
+
           serialize_result_by_ids(
             scope,
-            params[:page]
+            params[:page],
           )
         end
       end
