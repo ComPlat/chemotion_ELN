35df5122174e4c78adb28a4399d9254f7166e03b
FabianMauz <fmauz@ipb-halle.de>
2023-01-31 13:44:06 +0000

    fixed a display bug

--- app/packs/src/apps/mydb/elements/details/researchPlans/ImageAnnotationModalSVG.js
+++ app/packs/src/apps/mydb/elements/details/researchPlans/ImageAnnotationModalSVG.js
@@ -33,15 +33,15 @@ export default class ImageAnnotationModalSVG extends Component {
               fetch(`/api/v1/attachments/${attachment.id}/annotation`)
                 .then((res) => res.text())
                 .then((text) => {
-                  const svgString = decodeURIComponent(JSON.parse(text));
-                  svgEditor.svgCanvas.setSvgString(svgString);
-                })
-                .then(() => {
                   if (attachment.updatedAnnotation) {
                     svgEditor.svgCanvas.setSvgString(
                       attachment.updatedAnnotation
                     );
-                  }
+                  }else{
+                  const svgString = decodeURIComponent(JSON.parse(text));
+                  svgEditor.svgCanvas.setSvgString(svgString);
+                  } 
+                  svgEditor.updateCanvas(false,false)
                 });
             }}
           />
