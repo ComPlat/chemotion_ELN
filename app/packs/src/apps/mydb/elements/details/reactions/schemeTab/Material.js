47b98456dd46907fde3a70b90fdfab648eeaeafb
Mehreen Mansur <mehreen.mansur@gmail.com>
2023-02-14 08:53:15 +0100

    added check if input value is updated in materail details (#788)
    Co-authored-by: Mehreen <mehreen.mansur>

--- app/packs/src/apps/mydb/elements/details/reactions/schemeTab/Material.js
+++ app/packs/src/apps/mydb/elements/details/reactions/schemeTab/Material.js
@@ -145,7 +145,7 @@ class Material extends Component {
               metricPrefixes={metricPrefixes}
               precision={3}
               disabled={!permitOn(this.props.reaction) || ((this.props.materialGroup !== 'products') && !material.reference && this.props.lockEquivColumn)}
-              onChange={this.handleAmountUnitChange}
+              onChange={e => this.handleAmountUnitChange(e, material.amount_l)}
               onMetricsChange={this.handleMetricsChange}
               bsStyle={material.amount_unit === 'l' ? 'success' : 'default'}
             />
@@ -339,7 +339,8 @@ class Material extends Component {
     }
   }
 
-  handleAmountUnitChange(e) {
+  handleAmountUnitChange(e, value) {
+    if (e.value === value) return;
     if (this.props.onChange && e) {
       const event = {
         amount: e,
@@ -383,7 +384,7 @@ class Material extends Component {
 
   handleEquivalentChange(e) {
     const equivalent = e.value;
-    if (this.props.onChange) {
+    if (this.props.onChange && e) {
       const event = {
         type: 'equivalentChanged',
         materialGroup: this.props.materialGroup,
@@ -514,7 +515,7 @@ class Material extends Component {
                 metricPrefixes={metricPrefixes}
                 precision={4}
                 disabled={!permitOn(reaction) || (this.props.materialGroup !== 'products' && !material.reference && this.props.lockEquivColumn)}
-                onChange={this.handleAmountUnitChange}
+                onChange={e => this.handleAmountUnitChange(e, material.amount_g)}
                 onMetricsChange={this.handleMetricsChange}
                 bsStyle={material.error_mass ? 'error' : massBsStyle}
                 name="molecular-weight"
@@ -534,7 +535,7 @@ class Material extends Component {
             metricPrefixes={metricPrefixesMol}
             precision={4}
             disabled={!permitOn(reaction) || (this.props.materialGroup === 'products' || (!material.reference && this.props.lockEquivColumn))}
-            onChange={this.handleAmountUnitChange}
+            onChange={e => this.handleAmountUnitChange(e, material.amount_mol)}
             onMetricsChange={this.handleMetricsChange}
             bsStyle={material.amount_unit === 'mol' ? 'success' : 'default'}
           />
@@ -551,7 +552,7 @@ class Material extends Component {
             metricPrefixes={metricPrefixesMolConc}
             precision={4}
             disabled
-            onChange={this.handleAmountUnitChange}
+            onChange={e => this.handleAmountUnitChange(e, material.concn)}
             onMetricsChange={this.handleMetricsChange}
           />
         </td>
