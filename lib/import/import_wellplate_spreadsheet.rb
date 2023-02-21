3612c7693dad914ce57705884ef5032db6347552
Johannes Haubold <jh@megorei.com>
2023-02-21 08:40:48 +0100

    Biologie nacharbeiten researchplan table (#1115)
    * Prevent empty rows from being imported into Research Plan Body Tables and fix position sorting
    
    * Enable Wellplate Import from Spreadsheet both with Well position format A1 and A01
    
    * Fix syntax error causing JS errors
    
    * Persist table sorting also for view mode (by saving the reordered data)
    
    * Enable temporary and persistent collapsing of tables in ResearchPlan Body
    
    * Fix rubocop issues
    
    * Fix more rubocop issues

--- lib/import/import_wellplate_spreadsheet.rb
+++ lib/import/import_wellplate_spreadsheet.rb
@@ -16,117 +16,101 @@ module Import
       @letters = [*'A'..'Z']
       @position_index = 0
       @sample_index = 1
-      @readout_index = 4
+      @column_of_first_readout = 4
     end
 
     def process!
-      begin
-        read_file
-      rescue Zip::Error
-        error_messages << "Can not process this type of file, must be '.xlsx'."
-        raise StandardError, error_messages.join("\n")
-      end
-
-      begin
-        check_headers
-      rescue StandardError
-        raise StandardError, error_messages.join("\n")
-      end
-
-      begin
-        check_prefixes
-      rescue StandardError
-        raise StandardError, error_messages.join("\n")
-      end
-
-      begin
-        check_wells
-      rescue StandardError
-        raise StandardError, error_messages.join("\n")
-      end
-
-      begin
+      read_file
+      check_headers
+      check_prefixes
+      check_wells
+      ActiveRecord::Base.transaction do
         import_data
-      rescue StandardError
-        raise StandardError, error_messages.join("\n")
       end
     end
 
     private
 
+    def fail!
+      raise StandardError, error_messages.join("\n")
+    end
+
     def read_file
-      @xlsx = Roo::Spreadsheet.open(@file_path, extension: :xlsx)
+      begin
+        @xlsx = Roo::Spreadsheet.open(@file_path, extension: :xlsx)
+      rescue Zip::Error
+        error_messages << "Can not process this type of file, must be '.xlsx'."
+        fail!
+      end
       @sheet = xlsx.sheet(0)
       @header = sheet.row(1).map(&:to_s).map(&:strip)
       @rows = sheet.parse
     end
 
     def check_headers
-      ['Position', 'Sample', 'External Compound Label/ID', 'Smiles', '.+_Value', '.+_Unit'].each_with_index do |check, index|
-        error_messages << "'#{check}' must be in cell #{@letters[index]}1." if (@header[index] =~ /^#{check}/i).nil?
-      end
+      ['Position', 'Sample', 'External Compound Label/ID', 'Smiles', '.+_Value', '.+_Unit']
+        .each_with_index do |column_header, column_index|
+          next if (@header[column_index] =~ /^#{column_header}/i).present?
 
-      raise StandardError if error_messages.present?
+          error_messages << "'#{column_header}' must be in cell #{@letters[column_index]}1."
+        end
+
+      fail! if error_messages.any?
+    end
+
+    def value_headers
+      @value_headers ||= @header.grep(/^.+_Value/)
+    end
+
+    def value_prefixes
+      @value_prefixes ||= value_headers.map { |e| e.delete_suffix('_Value') }
+    end
+
+    def unit_headers
+      @unit_headers ||= @header.grep(/^.+_Unit/)
+    end
+
+    def unit_prefixes
+      @unit_prefixes ||= unit_headers.map { |e| e.delete_suffix('_Unit') }
     end
 
     def check_prefixes
-      value_headers = @header.select { |e| /^.+_Value/ =~e }
-      value_prefixes = value_headers.map { |e| e.delete_suffix('_Value') }
-      unit_headers = @header.select { |e| /^.+_Unit/ =~e }
-      unit_prefixes = unit_headers.map { |e| e.delete_suffix('_Unit') }
+      prefixes_match = value_prefixes == unit_prefixes
+      prefixes_unique = value_headers.uniq == value_headers && unit_headers.uniq == unit_headers
+      error_messages << "'_Value 'and '_Unit' prefixes don't match up." unless prefixes_match
+      error_messages << 'Prefixes must be unique.' unless prefixes_unique
 
-      error_messages << "'_Value 'and '_Unit' prefixes don't match up." unless value_prefixes == unit_prefixes
-      error_messages << 'Prefixes must be unique.' if value_headers.uniq != value_headers || unit_headers.uniq != unit_headers
-
-      @header[@readout_index..(4 + value_headers.count * 2)].each_with_index do |vh, index|
-        column = index + 4
-        comparison_index = index / 2
-        value_to_compare = column.even? ? value_headers[comparison_index] : unit_headers[comparison_index]
-        error_messages << "#{vh} should be in column #{@letters[column]}" if vh != value_to_compare
-      end
-
-      raise StandardError if error_messages.present?
-
-      @prefixes = value_prefixes
+      fail! if error_messages.any?
     end
 
     def check_wells
-      positions_check = ('A'..'H').map { |lttr| (1..12).map { |nmbr| ["#{lttr}#{nmbr}"] } }.flatten
+      expected_positions = WellPosition.all
       wells = xlsx.column(1).drop(1)
 
-      positions_check.each_with_index do |position, index|
-        error_messages << "Well #{position} is missing or at wrong position." if position != wells[index]
+      expected_positions.each_with_index do |expected_position, index|
+        actual_position_for_index = WellPosition.from_string(wells[index])
+        error = "Well #{expected_position} is missing or at wrong position."
+        error_messages << error if actual_position_for_index != expected_position
       end
 
-      raise StandardError if error_messages.present?
+      fail! if error_messages.any?
     end
 
     def import_data
-      ActiveRecord::Base.transaction do
-        @rows.map.with_index do |row, index|
-          position_x = index % 12 + 1
-          position_y = index / 12 + 1
-          expected_position = "#{@letters[position_y - 1]}#{position_x}"
+      @rows.map do |row|
+        position = WellPosition.from_string(row[@position_index])
+        well = Well.find_or_create_by!(wellplate_id: @wellplate.id, position_x: position.x, position_y: position.y)
 
-          @error_messages << "Error. Position #{row[@position_index]} is faulty." if row[@position_index] != expected_position
-          raise StandardError if error_messages.present?
-
-          tuples = row[@readout_index..@readout_index + @prefixes.count * 2 - 1].each_slice(2).to_a
-          readouts = tuples.map { |tuple| Hash[[%w[value unit], tuple].transpose].symbolize_keys }
-
-          well = Well.find_or_create_by!(
-            wellplate_id: @wellplate.id,
-            position_x: position_x,
-            position_y: position_y
-          )
-
-          well.update!(
-            readouts: readouts
-          )
-        end
-
-        @wellplate.update!(readout_titles: @prefixes)
+        well.update!(readouts: build_readouts_from_row_data(row))
       end
+
+      @wellplate.update!(readout_titles: value_prefixes)
+    end
+
+    def build_readouts_from_row_data(row_data)
+      row_data[@column_of_first_readout..]
+        .each_slice(2)
+        .map { |value, unit| { value: value, unit: unit } }
     end
   end
 end
