fae5633faed7b7b5578f10d68cafbdb5939d99c3
ptrxyz <peter.krauss@kit.edu>
2023-02-22 12:18:51 +0100

    Updated initializer to be more talkative.
    Tried to add some informative log messages.

--- config/initializers/spectra.rb
+++ config/initializers/spectra.rb
@@ -4,29 +4,45 @@ if File.exist? Rails.root.join('config', 'spectra.yml')
   spectra_config = Rails.application.config_for :spectra
 
   Rails.application.configure do
+    # let's build the data structure
     config.spectra = ActiveSupport::OrderedOptions.new
-
     config.spectra.chemspectra = ActiveSupport::OrderedOptions.new
+    config.spectra.nmriumwrapper = ActiveSupport::OrderedOptions.new
 
-    if !spectra_config[:url].nil? && !spectra_config[:port].nil?
+    # check if old config is present
+    if spectra_config[:url].present? || spectra_config[:port].present?
       Rails.logger.info(
-        "Chemspectra configuration that use 'url' and 'port' will deprecated soon" \
-        "Please update 'spectra.yml' file to use Rails.configuration.spectra.chemspectra.url instead."
+        "Chemspectra configurations that use 'url' and 'port' will deprecated soon. " \
+        "Please update your 'spectra.yml' file to use 'chemspectra.url' instead."
       )
-      url = spectra_config[:url]
-      port = spectra_config[:port]
-      config.spectra.chemspectra.url = "http://#{url}:#{port}"
+      if spectra_config[:url].present? && spectra_config[:port].present?
+        url  = spectra_config[:url]
+        port = spectra_config[:port]
+        config.spectra.chemspectra.url = "http://#{url}:#{port}"
+      end
     end
     
-    unless spectra_config[:chemspectra].nil?
-      chemspectra_object = spectra_config[:chemspectra]
-      config.spectra.chemspectra.url = chemspectra_object[:url]
+    # if new config is present, it takes precendence
+    if spectra_config.dig(:chemspectra, :url).present?
+      config.spectra.chemspectra.url = spectra_config.dig(:chemspectra, :url)
     end
-
-    unless spectra_config[:nmriumwrapper].nil?
-      config.spectra.nmriumwrapper = ActiveSupport::OrderedOptions.new
-      nmrium_wrapper_object = spectra_config[:nmriumwrapper]
-      config.spectra.nmriumwrapper.url = nmrium_wrapper_object[:url]
+    
+    # add the config for NMRiumWrapper
+    if spectra_config.dig(:nmriumwrapper, :url).present?
+      config.spectra.nmriumwrapper.url = spectra_config.dig(:nmriumwrapper, :url)
+    end
+    
+    # let's get loud!
+    if config.spectra.chemspectra.url.nil?
+      Rails.logger.warn("No endpoint url set for Chemspectra, it will not be available.")
+    else
+      Rails.logger.info("Chemspectra endpoint is configured to be '#{config.spectra.chemspectra.url}'.")
+    end
+    
+    if config.spectra.nmriumwrapper.url.nil?
+      Rails.logger.warn("No endpoint url set for NMRiumWrapper, it will not be available.")
+    else
+      Rails.logger.info("NMRiumWrapper endpoint configured to be '#{config.spectra.nmriumwrapper.url}'.")        
     end
   end
 end
