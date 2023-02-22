62635693a4d58e535b4f2dd7c166ee04c62aa134
ptrxyz <peter.krauss@kit.edu>
2023-02-22 12:31:19 +0100

    Simplified ui_api.rb to fit new config struct.

--- app/api/chemotion/ui_api.rb
+++ app/api/chemotion/ui_api.rb
@@ -8,20 +8,13 @@ module Chemotion
       desc 'Initialize UI'
       params do
       end
-      get 'initialize' do
-        chem_spectra_config = Rails.configuration.try(:spectra).try(:url)
-        has_chem_spectra = chem_spectra_config.present?
-        has_nmrium_wrapper = false
-        unless Rails.configuration.spectra.chemspectra.nil?
-          chem_spectra = Rails.configuration.spectra.chemspectra
-          has_chem_spectra = !chem_spectra[:url].nil?
-        end
-        unless Rails.configuration.spectra.nmriumwrapper.nil?
-          nmrium_wrapper = Rails.configuration.spectra.nmriumwrapper
-          has_nmrium_wrapper = !nmrium_wrapper.url.nil?
-        end
+      get 'initialize' do        
+        has_chem_spectra = Rails.configuration.spectra.chemspectra.url.present?
+        has_nmrium_wrapper = Rails.configuration.spectra.nmriumwrapper.url.present?
+                
         m_config = Rails.root.join('config', 'matrices.json')
         sfn_config = Rails.configuration.try(:sfn_config).try(:provider)
+        
         {
           has_chem_spectra: has_chem_spectra,
           has_nmrium_wrapper: has_nmrium_wrapper,
