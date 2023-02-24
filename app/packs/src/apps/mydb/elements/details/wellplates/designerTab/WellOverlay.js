66b19cb9546a3b4693d305f46b8fc6508229f80f
Mehreen Mansur <mehreen.mansur@gmail.com>
2023-02-24 09:29:07 +0100

    wellplate trash button issue (#1148)
    * fix wellplate trash button issue
    
    * updated welloverlay css
    
    
    ---------
    
    Co-authored-by: Mehreen <mehreen.mansur>

--- app/packs/src/apps/mydb/elements/details/wellplates/designerTab/WellOverlay.js
+++ app/packs/src/apps/mydb/elements/details/wellplates/designerTab/WellOverlay.js
@@ -46,11 +46,9 @@ const renderWellContent = (well, removeSampleFromWell) => {
     svg = <SVG key={sample.id} className="molecule-mid" src={sample.svgPath} />;
     moleculeName = sample.molecule.iupac_name;
     removeButton = (
-      <div className="pull-right">
-        <Button bsSize="xsmall" bsStyle="danger" onClick={() => removeSampleFromWell(well)}>
-          <span className="fa fa-trash-o"></span>
-        </Button>
-      </div>
+      <Button className="pull-right" bsSize="xsmall" bsStyle="danger" onClick={() => removeSampleFromWell(well)}>
+        <i className="fa fa-trash-o"/>
+      </Button>
     );
   }
   return (
@@ -97,7 +95,7 @@ const content = (
   }];
 
   return (
-    <div style={{ width: 220, height: 650 }}>
+    <div style={{ width: 220, height: 550 }}>
       {renderWellContent(well, removeSampleFromWell)}
       <div>
         <Select
@@ -136,7 +134,7 @@ const content = (
           />
         </FormGroup>
         <FormGroup>
-          <Col style={{ marginLeft: '-15px' }} class="row row-no-gutters" componentClass={ControlLabel} sm={3}>
+          <Col style={{ marginTop: '7px', marginLeft: '-15px' }} class="row row-no-gutters" componentClass={ControlLabel} sm={3}>
             Select&nbsp;Color
           </Col>
           <Col sm={9} style={{ marginLeft: '35px', width: '65%' }}>
