437cd684874e2b9b1241ab56386072835ec8708e
PiTrem <pierre.tremouilhac@kit.edu>
2023-03-14 20:41:39 +0100

    github ci: update image for testing
    - image based on production v1.5.0
    
    - introduce rdkit and openbabel gems compatible with ubuntu 22.04
    
    - also fix datacollector initializer issue introduced with fc624fc23a688f46e55fb39a5eea729e6c056665
    
    - spec datacollector: sftp setting upd
    
    - set .tool-versions
    
    - skip capybara test for now
    
    - update-rb-node-version in  config/docker files

--- config/initializers/datacollectors.rb
+++ config/initializers/datacollectors.rb
@@ -5,12 +5,14 @@ Rails.application.configure { config.datacollectors = nil }
 
 if File.file?(Rails.root.join('config/datacollectors.yml'))
   datacollectors_config = Rails.application.config_for :datacollectors
-  Rails.application.configure do
-    config.datacollectors = ActiveSupport::OrderedOptions.new
-    config.datacollectors.services = datacollectors_config[:services]
-    config.datacollectors.mailcollector = datacollectors_config[:mailcollector]
-    config.datacollectors.sftpusers = datacollectors_config[:sftpusers]
-    config.datacollectors.localcollectors = datacollectors_config[:localcollectors]
-    config.datacollectors.keydir = datacollectors_config[:keydir]
+  if datacollectors_config
+    Rails.application.configure do
+      config.datacollectors = ActiveSupport::OrderedOptions.new
+      config.datacollectors.services = datacollectors_config[:services]
+      config.datacollectors.mailcollector = datacollectors_config[:mailcollector]
+      config.datacollectors.sftpusers = datacollectors_config[:sftpusers]
+      config.datacollectors.localcollectors = datacollectors_config[:localcollectors]
+      config.datacollectors.keydir = datacollectors_config[:keydir]
+    end
   end
 end
