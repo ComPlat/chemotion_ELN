ComPlat/chemotion_ELN

commit fc624fc23a688f46e55fb39a5eea729e6c056665

skip datacollector config if no yml file present (#1167)

config/initializers/datacollectors.rb
@@ -3,9 +3,8 @@
 # set default value
 Rails.application.configure { config.datacollectors = nil }
 
-datacollectors_config = Rails.application.config_for :datacollectors
-
-if datacollectors_config
+if File.file?(Rails.root.join('config/datacollectors.yml'))
+  datacollectors_config = Rails.application.config_for :datacollectors
   Rails.application.configure do
     config.datacollectors = ActiveSupport::OrderedOptions.new
     config.datacollectors.services = datacollectors_config[:services]

commit 434398fd1b79735c185df1df0af5e0732fffb3b2

github ci: update image for testing
- image based on production v1.5.0

- introduce rdkit and openbabel gems compatible with ubuntu 22.04

- also fix datacollector initializer issue introduced with fc624fc23a688f46e55fb39a5eea729e6c056665

- spec datacollector: sftp setting upd

- set .tool-versions

- skip capybara test for now

- update-rb-node-version in  config/docker files

.github/workflows/ci.yml
@@ -21,7 +21,7 @@ jobs:
 
       - uses: ruby/setup-ruby@v1
         with:
-          ruby-version: '2.7.6'
+          ruby-version: '2.7.7'
 
       - name: install linting dependencies
         # IMPORTANT: install rubocop first for pinning to take effect (`gem` doesn't resolve dependencies but simply installs gems in order)
@@ -49,7 +49,7 @@ jobs:
       run:
         shell: bash
 
-    container: complat/chemotion_eln_runner:ruby-2.7.6
+    container: complat/chemotion_eln_runner:v1.5.0
 
     services:
       postgres:
@@ -67,37 +67,24 @@ jobs:
       - name: checkout repository
         uses: actions/checkout@v3
 
-      - name: update package repository
-        run: sudo apt update
-
-      - name: install SSH service
-        run: sudo apt install -y openssh-server
-
-      - name: restart SSH server
-        run: sudo service ssh restart
-
-      - name: check SSH service
-        run: sudo service ssh status
-
-      - name: generate SSH key
+      - name: create user for datacollectors testing
         run: |
+          useradd testuser -m -s /bin/bash
+          mkdir /home/testuser/.ssh
+          chown testuser:testuser /home/testuser/.ssh
+          chmod 700 /home/testuser/.ssh
+
+      - name: configure ssh for datacollectors testing 
+        run: |
+          service ssh restart
           mkdir -p $HOME/.ssh
-          ssh-keygen -t rsa -b 4096 -C "CI Deploy Key" -f $HOME/.ssh/id_rsa -N ""
-          echo "$(<$HOME/.ssh/id_rsa.pub)" >> $HOME/.ssh/authorized_keys
-
-      - name: create user
-        run: |
-          sudo useradd testuser -m -s /bin/bash
-          sudo mkdir /home/testuser/.ssh
-          sudo chown testuser:testuser /home/testuser/.ssh
-          sudo chmod 700 /home/testuser/.ssh
-
-      - name: add SSH key to authorized_keys
-        run: |
-          echo "$(<$HOME/.ssh/id_rsa.pub)" | sudo tee -a /home/testuser/.ssh/authorized_keys
-          cat /home/testuser/.ssh/authorized_keys
-          sudo chown testuser:testuser /home/testuser/.ssh/authorized_keys
-          sudo chmod 600 /home/testuser/.ssh/authorized_keys
+          chmod 700 $HOME/.ssh
+          ssh-keygen -t ed25519 -f $HOME/.ssh/id_test -N ""
+          # echo "$(<$HOME/.ssh/id_test.pub)" >> $HOME/.ssh/authorized_keys
+          echo "$(<$HOME/.ssh/id_test.pub)" | sudo tee -a /home/testuser/.ssh/authorized_keys
+          eval `ssh-agent`
+          ssh-add $HOME/.ssh/id_test
+          ssh-keyscan -H 127.0.0.1  >> $HOME/.ssh/known_hosts
 
       - name: configure repository
         run: |
@@ -110,6 +97,7 @@ jobs:
           cp -f storage.yml.example storage.yml
           cp -f radar.yml.example radar.yml
           touch klasses.json
+          bundle config set without ''
 
       - name: configure postgres
         env:
@@ -126,7 +114,7 @@ jobs:
         run: bundle install
 
       - name: yarn install
-        run: yarn install
+        run:  yarn install --production=false
 
       - name: prepare postgres
         run: |
@@ -134,18 +122,14 @@ jobs:
           RAILS_ENV=test bundle exec rake db:test:prepare
           RAILS_ENV=test bundle exec rake db:seed
 
-      - name: precompile
-        run: RAILS_ENV=test bundle exec rake webpacker:compile
-
       - name: npm test
         run: npm test
 
-      - name: rspec acceptance
-        continue-on-error: true    # don't fail job because this step; TODO: fix flaky acceptance tests...
-        run: RAILS_ENV=test bundle exec rspec spec/features
-
       - name: rspec unit
-        run: RAILS_ENV=test bundle exec rspec --exclude-pattern spec/{features}/**/*_spec.rb
+        run: |
+          eval `ssh-agent`
+          ssh-add $HOME/.ssh/id_test
+          RAILS_ENV=test bundle exec rspec --exclude-pattern spec/{features}/**/*_spec.rb spec
 
       - name: coverage rspec unit
         if: github.event.pull_request && (github.event.pull_request.head.repo.full_name == github.event.pull_request.base.repo.full_name) && (github.triggering_actor != 'dependabot[bot]')  # don't run on PRs from forks because of missing write permission: https://github.com/orgs/community/discussions/26829
@@ -155,3 +139,11 @@ jobs:
           minimum-coverage: 57
           artifact-name: code-coverage-report
           github-token: ${{ secrets.GITHUB_TOKEN }}
+
+#      - name: precompile
+#        run: RAILS_ENV=test bundle exec rake webpacker:compile
+
+#      - name: rspec acceptance
+#        continue-on-error: true    # don't fail job because this step; TODO: fix flaky acceptance tests...
+#        run: RAILS_ENV=test bundle exec rspec spec/features
+

.nvmrc
@@ -1,1 +1,1 @@
-v14.20.0
+v14.21.3

.ruby-version.example
@@ -1,1 +1,1 @@
-2.7.6
+2.7.7

.tool-versions
@@ -1,0 +1,2 @@
+nodejs 14.21.3
+ruby 2.7.7

Dockerfile.chemotion-dev
@@ -6,10 +6,10 @@
 FROM --platform=linux/amd64 ubuntu:focal
 
 ARG DEBIAN_FRONTEND=noninteractive
-ARG VRUBY=2.7.6
-ARG VNODE=14.20.0
+ARG VRUBY=2.7.7
+ARG VNODE=14.21.3
 ARG VNODENEXT=16.16.0
-ARG ASDF_BRANCH=v0.8.1
+ARG ASDF_BRANCH=v0.11.3
 
 RUN set -xe  && apt-get update -yqqq --fix-missing && apt-get upgrade -y
 RUN apt update && apt-get install -yqq --fix-missing bash ca-certificates wget apt-transport-https git gpg\

Dockerfile.focal.gitlab-ci
@@ -1,8 +1,8 @@
 FROM ubuntu:focal
 
 ARG DEBIAN_FRONTEND=noninteractive
-ARG VRUBY=2.7.6
-ARG VNODE=14.20.0
+ARG VRUBY=2.7.7
+ARG VNODE=14.21.3
 ARG VNODENEXT=16.16.0
 
 RUN set -xe  && apt-get update -yqqq --fix-missing && apt-get upgrade -y

Dockerfile.github-ci
@@ -1,6 +1,6 @@
-FROM ruby:2.7.6
+FROM ruby:2.7.7
 
-ARG NODE_VERSION=14.20.0
+ARG NODE_VERSION=14.21.3
 ARG BUNDLER_VERSION=2.1.4
 
 RUN apt-get update && apt-get upgrade -y && apt-get install -y \

Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 
 gem 'aasm'
 gem 'activejob-status'
+gem 'activerecord-nulldb-adapter'
 gem 'ancestry'
 gem 'api-pagination'
 gem 'caxlsx'
@@ -73,8 +74,7 @@ gem 'omniauth-github', '~> 1.4.0'
 gem 'omniauth-oauth2', '~> 1.7', '>= 1.7.2'
 gem 'omniauth_openid_connect'
 gem 'omniauth-orcid', git: 'https://github.com/datacite/omniauth-orcid'
-
-gem 'openbabel', '2.4.90.3', git: 'https://github.com/ComPlat/openbabel-gem.git', branch: 'hot-fix-svg'
+gem 'openbabel', '2.4.90.3', git: 'https://github.com/ptrxyz/openbabel-gem.git', branch: 'ptrxyz-ctime-fix'
 
 gem 'pandoc-ruby'
 gem 'paranoia'
@@ -88,7 +88,7 @@ gem 'pundit'
 gem 'rack'
 gem 'rack-cors', require: 'rack/cors'
 gem 'rails', '6.1.7'
-gem 'rdkit_chem', git: 'https://github.com/CamAnNguyen/rdkit_chem'
+gem 'rdkit_chem', git: 'https://github.com/ptrxyz/rdkit_chem.git', branch: 'pk01'
 gem 'rinchi-gem', '1.0.1', git: 'https://git.scc.kit.edu/ComPlat/rinchi-gem.git'
 gem 'rmagick'
 gem 'roo'
@@ -111,6 +111,7 @@ gem 'sys-filesystem'
 gem 'thor'
 gem 'thumbnailer', git: 'https://github.com/merlin-p/thumbnailer.git'
 gem 'turbo-sprockets-rails4'
+gem 'tzinfo-data'
 
 gem 'webpacker', git: 'https://github.com/rails/webpacker', branch: 'master'
 gem 'whenever', require: false

config/initializers/datacollectors.rb
@@ -5,12 +5,14 @@ Rails.application.configure { config.datacollectors = nil }
 
 if File.file?(Rails.root.join('config/datacollectors.yml'))
   datacollectors_config = Rails.application.config_for :datacollectors
-  Rails.application.configure do
-    config.datacollectors = ActiveSupport::OrderedOptions.new
-    config.datacollectors.services = datacollectors_config[:services]
-    config.datacollectors.mailcollector = datacollectors_config[:mailcollector]
-    config.datacollectors.sftpusers = datacollectors_config[:sftpusers]
-    config.datacollectors.localcollectors = datacollectors_config[:localcollectors]
-    config.datacollectors.keydir = datacollectors_config[:keydir]
+  if datacollectors_config
+    Rails.application.configure do
+      config.datacollectors = ActiveSupport::OrderedOptions.new
+      config.datacollectors.services = datacollectors_config[:services]
+      config.datacollectors.mailcollector = datacollectors_config[:mailcollector]
+      config.datacollectors.sftpusers = datacollectors_config[:sftpusers]
+      config.datacollectors.localcollectors = datacollectors_config[:localcollectors]
+      config.datacollectors.keydir = datacollectors_config[:keydir]
+    end
   end
 end

lib/datacollector/fcollector.rb
@@ -20,7 +20,6 @@ class Fcollector
       @current_collector = nil
       method_params = device.profile.data['method_params']
       host = method_params['host']
-
       case method_params['authen']
       when 'keyfile'
         user = method_params['user']
@@ -33,6 +32,8 @@ class Fcollector
         args = {
           key_data: [],
           keys: kp,
+          auth_methods: %w[publickey],
+          verbose: :error,
           keys_only: true,
         }
       when 'password', nil

spec/factories/devices.rb
@@ -36,10 +36,10 @@ FactoryBot.define do
             'method' => 'filewatchersftp',
             'method_params' => {
               'dir' => "#{Rails.root}/tmp/datacollector/#{name_abbreviation}",
-              'user' => 'testuser',
+              'user' => ENV['DATACOLLECTOR_TEST_USER'].presence || 'testuser',
               'host' => '127.0.0.1',
               'authen' => 'keyfile',
-              'key_name' => "#{Dir.home}/.ssh/id_rsa",
+              'key_name' => "#{Dir.home}/.ssh/id_test",
               'number_of_files' => 1,
             },
           },
@@ -57,7 +57,7 @@ FactoryBot.define do
               'user' => 'dummy',
               'host' => '127.0.0.1',
               'authen' => 'keyfile',
-              'key_name' => "#{Dir.home}/.ssh/id_rsa",
+              'key_name' => "#{Dir.home}/.ssh/id_test",
               'number_of_files' => 1,
             },
           },
@@ -87,10 +87,10 @@ FactoryBot.define do
             'method' => 'folderwatchersftp',
             'method_params' => {
               'dir' => "#{Rails.root}/tmp/datacollector/#{name_abbreviation}",
-              'user' => 'testuser',
+              'user' => ENV['DATACOLLECTOR_TEST_USER'].presence || 'testuser',
               'host' => '127.0.0.1',
               'authen' => 'keyfile',
-              'key_name' => "#{Dir.home}/.ssh/id_rsa",
+              'key_name' => "#{Dir.home}/.ssh/id_test",
               'number_of_files' => 1,
             },
           },
@@ -108,7 +108,7 @@ FactoryBot.define do
               'user' => 'dummy',
               'host' => '127.0.0.1',
               'authen' => 'keyfile',
-              'key_name' => "#{Dir.home}/.ssh/id_rsa",
+              'key_name' => "#{Dir.home}/.ssh/id_test",
               'number_of_files' => 1,
             },
           },

spec/lib/datacollector/filecollector_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Filecollector, type: :model do
         device1
         device2
 
-        expect { described_class.new.execute(false) }.to change(Attachment, :count).by(Device.count)
+        expect { described_class.new.execute(false) }.to change(Attachment, :count).by(2)
       end
     end
 
@@ -25,7 +25,7 @@ RSpec.describe Filecollector, type: :model do
         device_sftp1
         device_sftp3
 
-        expect { described_class.new.execute(true) }.to change(Attachment, :count).by(Device.count)
+        expect { described_class.new.execute(true) }.to change(Attachment, :count).by(2)
       end
     end
 
@@ -34,7 +34,7 @@ RSpec.describe Filecollector, type: :model do
         device_sftp1
         device_sftp3
 
-        expect { described_class.new.execute(true) }.to change(Attachment, :count).by(Device.count)
+        expect { described_class.new.execute(true) }.to change(Attachment, :count).by(2)
       end
     end
 