2ac45c101be042b5e19aaaab864094946fe990b6
Beate Quednau <beate@firma-pflaum.de>
2023-03-23 12:04:42 +0100

    Add basics for generic element search

---
+++ app/packs/src/components/searchModal/forms/SearchModalFunctions.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { Alert } from 'react-bootstrap';
+
+const togglePanel = (store) => () => {
+  if (store.searchResultsCount > 0) {
+    store.toggleSearch();
+    store.toggleSearchResults();
+    store.clearTabCurrentPage();
+  }
+}
+
+const showErrorMessage = (store) => {
+  if (store.error_message) {
+    return <Alert bsStyle="danger">{store.error_message}</Alert>;
+  }
+}
+
+const panelVariables = (store) => {
+  let variables = [
+    {
+      defaultClassName: 'collapsible-search-result',
+      invisibleClassName: (store.search_result_panel_visible ? '' : ' inactive'),
+      inactiveSearchClass: (!store.searchVisible ? 'inactive' : ''),
+      inactiveResultClass: (!store.searchResultVisible? 'inactive' : ''),
+      searchIcon: `fa fa-chevron-${store.search_icon} icon-right`,
+      resultIcon: `fa fa-chevron-${store.result_icon} icon-right`,
+      searchTitle: (store.searchVisible ? 'Search' : 'Refine search'),
+      resultTitle: (store.searchResultVisible ? 'Result' : 'Back to result'),
+    }
+  ];
+  return variables[0];
+}
+
+export { togglePanel, showErrorMessage, panelVariables }
