c8c56608f7ddf45d2209faa407148f5f182a9593
fathia <f.idiris1@gmail.com>
2023-04-17 10:57:42 +0000

    fix payload url

--- app/api/chemotion/editor_api.rb
+++ app/api/chemotion/editor_api.rb
@@ -19,7 +19,7 @@ module Chemotion
         end
         before do
           @attachment = Attachment.find_by(id: params[:attachment_id])
-          error!('401 Unauthorized', 401) unless ElementPolicy.new(current_user, ResearchPlan.find_by(id: @attachment[:attachable_id])).update?
+           error!('401 Unauthorized', 401) unless ElementPolicy.new(current_user, ResearchPlan.find_by(id: @attachment[:attachable_id])).update?
           # error!('401 Unauthorized', 401) if @attachment.oo_editing?
         end
         post do
@@ -39,7 +39,7 @@ module Chemotion
               fileType: 'docx',
               key: token,
               title: @attachment.filename,
-              url: "http://#{request.host_with_port}api/v1/public/download?token=#{token}",
+              url: "http://#{request.host_with_port}/api/v1/public/download?token=#{token}",
               permissions: {
                 download: true,
                 edit: true,
