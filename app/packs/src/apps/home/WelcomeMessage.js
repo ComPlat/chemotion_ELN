a48e9505a6f25c73baef46a2855ea4c99d0f1f28
PiTrem <pierre.tremouilhac@kit.edu>
2023-04-20 19:42:06 +0200

    1285 do no cache the welcome message (#1286)
    * avoid caching of welcome-message
    
    * remove changelog from the about page to avoid confusion on releases  vs deployment dates

--- app/packs/src/apps/home/WelcomeMessage.js
+++ app/packs/src/apps/home/WelcomeMessage.js
@@ -3,29 +3,36 @@ import { Jumbotron } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 
 function Message(props) {
-  const input = props.post;
-  if (input != null && input !== undefined) {
-    return (<Jumbotron>
-      <ReactMarkdown children={input} />
-    </Jumbotron>);
+  const { post } = props;
+  if (post != null && post !== undefined) {
+    return (
+      <Jumbotron>
+        <ReactMarkdown children={post} />
+      </Jumbotron>
+    );
   }
   return null;
 }
 
 class WelcomeMessage extends Component {
-  state = {
-    post: null
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: null
+    };
   }
+
   componentDidMount() {
-    fetch('welcome-message.md')
+    fetch(`welcome-message.md?${Date.now()}`) // Prevent caching
       .then((res) => {
         if (res.ok) {
           return res.text();
         }
         return null;
-      }).then(post => this.setState(state => ({ ...state, post })))
+      }).then((post) => this.setState((state) => ({ ...state, post })))
       .catch((errorMessage) => { console.error(errorMessage); });
   }
+
   render() {
     const { post } = this.state;
     return (
