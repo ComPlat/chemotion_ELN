9962ee12b756f649b5e3990c9d235ee8fbd0dab8
f-idiris <112618970+f-idiris@users.noreply.github.com>
2023-04-26 16:49:21 +0200

    1254 eln to work with onlyoffice 7 (#1278)
    * update only office version to 7.2
    
    * add only office token
    
    * fix payload url
    
    * Add public API tests
    
    * rubocop fixes
    
    * rubocop fix  public_api
    
    * Update public api spec
    
    * deleted unused AttachmentEditButton.js file
    
    * update public api affliations and callback
    
    ---------
    
    Co-authored-by: Danny Truong <allenskywalker92@gmail.com>

--- app/packs/src/apps/mydb/elements/details/researchPlans/attachmentsTab/ResearchPlanDetailsAttachments.js
+++ app/packs/src/apps/mydb/elements/details/researchPlans/attachmentsTab/ResearchPlanDetailsAttachments.js
@@ -97,21 +97,22 @@ export default class ResearchPlanDetailsAttachments extends Component {
     const fileType = last(attachment.filename.split('.'));
     const docType = this.documentType(attachment.filename);
 
-    EditorFetcher.startEditing({ attachment_id: attachment.id }).then(
-      (result) => {
-        if (result.token) {
-          const url = `/editor?id=${attachment.id}&docType=${docType}&fileType=${fileType}&title=${attachment.filename}&key=${result.token}`;
-          window.open(url, '_blank');
+    EditorFetcher.startEditing({ attachment_id: attachment.id })
+    .then((result) => {
+      if (result.token) {
+        const url = `/editor?id=${attachment.id}&docType=${docType}
+        &fileType=${fileType}&title=${attachment.filename}&key=${result.token}
+        &only_office_token=${result.only_office_token}`;
+        window.open(url, '_blank');
 
-          attachment.aasm_state = 'oo_editing';
-          attachment.updated_at = new Date();
+        attachment.aasm_state = 'oo_editing';
+        attachment.updated_at = new Date();
 
-          this.props.onEdit(attachment);
-        } else {
-          alert('Unauthorized to edit this file.');
-        }
+        this.props.onEdit(attachment);
+      } else {
+        alert('Unauthorized to edit this file.');
       }
-    );
+    });
   }
 
   createAttachmentPreviews() {
