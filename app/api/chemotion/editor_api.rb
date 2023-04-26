bae279f8124e84c84a93e244573140fcc9df561d
f-idiris <112618970+f-idiris@users.noreply.github.com>
2023-04-26 16:49:21 +0200

    1254 eln to work with onlyoffice 7 (#1278)
    * update only office version to 7.2
    
    * add only office token
    
    * fix payload url
    
    * Add public API tests
    
    * rubocop fixes
    
    * rubocop fix  public_api
    
    * Update public api spec
    
    * deleted unused AttachmentEditButton.js file
    
    * update public api affliations and callback
    
    ---------
    
    Co-authored-by: Danny Truong <allenskywalker92@gmail.com>

--- app/api/chemotion/editor_api.rb
+++ app/api/chemotion/editor_api.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
+# rubocop:disable Rails/DurationArithmetic
+
 module Chemotion
   # Editor API
-  # rubocop:disable ClassLength
   class EditorAPI < Grape::API
     # rubocop:disable Metrics/BlockLength
     namespace :editor do
@@ -20,20 +21,74 @@ module Chemotion
         end
         before do
           @attachment = Attachment.find_by(id: params[:attachment_id])
-          error!('401 Unauthorized', 401) unless ElementPolicy.new(current_user, ResearchPlan.find_by(id: @attachment[:attachable_id])).update?
+          unless ElementPolicy.new(current_user, ResearchPlan.find_by(id: @attachment[:attachable_id])).update?
+            error!('401 Unauthorized', 401)
+          end
           # error!('401 Unauthorized', 401) if @attachment.oo_editing?
         end
         post do
           payload = {
             att_id: @attachment.id,
             user_id: current_user.id,
-            exp: (Time.now + 15.minutes).to_i
+            exp: (Time.zone.now + 15.minutes).to_i,
           }
           @attachment.oo_editing_start!
           token = JWT.encode payload, Rails.application.secrets.secret_key_base
-          {token: token}
+          only_office_payload = {
+            width: '100%',
+            height: '950px',
+            type: 'desktop',
+            document: {
+              att_id: @attachment.id,
+              fileType: 'docx',
+              key: token,
+              title: @attachment.filename,
+              url: "http://#{request.host_with_port}/api/v1/public/download?token=#{token}",
+              permissions: {
+                download: true,
+                edit: true,
+                fillForms: false,
+                review: false,
+              },
+            },
+            editorConfig: {
+              callbackUrl: "http://#{request.host_with_port}/api/v1/public/callback",
+              mode: 'edit',
+              lang: 'en',
+              customization: {
+                chat: false,
+                compactToolbar: false,
+                customer: {
+                  address: Rails.configuration.editors.info[:address],
+                  info: Rails.configuration.editors.info[:title],
+                  logo: Rails.configuration.editors.info[:logo],
+                  mail: Rails.configuration.editors.info[:mail],
+                  name: Rails.configuration.editors.info[:name],
+                  www: Rails.configuration.editors.info[:website],
+                },
+                feedback: {
+                  url: Rails.configuration.editors.info[:feedbackurl],
+                  visible: false,
+                },
+                forcesave: false,
+                help: false,
+                logo: {
+                  image: Rails.configuration.editors.info[:logo],
+                  imageEmbedded: Rails.configuration.editors.info[:logo],
+                  url: Rails.configuration.editors.info[:website],
+                },
+                showReviewChanges: false,
+                zoom: 100,
+              },
+            },
+          }
+          only_office_token = JWT.encode only_office_payload, Rails.application.secrets.only_office_secret_key_base
+
+          { token: token, only_office_token: only_office_token }
         end
       end
     end
   end
 end
+
+# rubocop:enable Rails/DurationArithmetic
