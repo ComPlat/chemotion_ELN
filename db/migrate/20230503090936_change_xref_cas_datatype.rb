efed796b2e6f043fb4e15be29ee1a6dbeec4b598
PiTrem <pierre.tremouilhac@kit.edu>
2023-05-08 15:53:51 +0200

    Migration corrections: (#1307)
    - Sample cas mig: case for empty string
    - sample svg file migration if no file found
    - fix broken attachment derivative for tiff images

--- db/migrate/20230503090936_change_xref_cas_datatype.rb
+++ db/migrate/20230503090936_change_xref_cas_datatype.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
 # rubocop:disable Rails::SkipsModelValidations
-
+# desc: change the data strucure of cas in sample.xref column
 class ChangeXrefCasDatatype < ActiveRecord::Migration[6.1]
   def change
     Sample.where(xref: nil).find_each do |sample|
       sample.update_columns(xref: {})
     end
 
-    Sample.where("xref ? 'cas'").find_each do |s|
-      if s.xref['cas'].nil?
-        s.update_columns(xref: s.xref.compact!)
-      elsif s.xref['cas'].key?('value')
-        s.update_columns(xref: { cas: (s.xref['cas']['value']).to_s })
-      end
+    Sample.where("xref ? 'cas'").find_each do |sample|
+      xref = sample.xref
+      cas = xref.delete('cas')
+      next if cas.present? && cas.is_a?(String)
+
+      xref['cas'] = cas['value'] if cas.is_a?(Hash) && cas['value'].present?
+
+      sample.update_columns(xref: xref)
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_06_114227) do
+ActiveRecord::Schema.define(version: 2023_05_03_090936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
