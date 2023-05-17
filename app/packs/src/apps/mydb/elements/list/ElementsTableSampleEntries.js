eb2fa8b6f7c235dea1a313ad9b520eef59561182
PiTrem <pierre.tremouilhac@kit.edu>
2023-05-17 11:45:26 +0200

    replace more Aviator.navigate by AviatorNavigation

--- app/packs/src/apps/mydb/elements/list/ElementsTableSampleEntries.js
+++ app/packs/src/apps/mydb/elements/list/ElementsTableSampleEntries.js
@@ -21,7 +21,7 @@ import KeyboardStore from 'src/stores/alt/stores/KeyboardStore';
 
 import { DragDropItemTypes } from 'src/utilities/DndConst';
 import SampleName from 'src/components/common/SampleName';
-import { sampleShowOrNew } from 'src/utilities/routesUtils';
+import { sampleShowOrNew, AviatorNavigation } from 'src/utilities/routesUtils';
 import SvgWithPopover from 'src/components/common/SvgWithPopover';
 import { ShowUserLabels } from 'src/components/UserLabels';
 import CommentIcon from 'src/components/comments/CommentIcon';
@@ -42,9 +42,8 @@ const buildFlattenSampleIds = (displayedMoleculeGroup) => {
 };
 
 const showDetails = (id) => {
-  const { currentCollection, isShared } = UIStore.getState();
-  const uri = `/${isShared ? 's' : ''}collection/${currentCollection.id}/sample/${id}`;
-  Aviator.navigate(uri, { silent: true });
+  const { currentCollection } = UIStore.getState();
+  AviatorNavigation({ element: { id, type: 'sample' }, silent: true });
   sampleShowOrNew({ params: { sampleID: id, collectionID: currentCollection.id } });
 };
 
