0495e3c577e4cb96e2cee5389187153f3474bee5
PiTrem <pierre.tremouilhac@kit.edu>
2023-05-17 16:24:25 +0200

    WIP deprecate is_shared

--- app/packs/src/fetchers/SuggestionsFetcher.js
+++ app/packs/src/fetchers/SuggestionsFetcher.js
@@ -1,4 +1,5 @@
 export default class SuggestionsFetcher {
+<<<<<<< HEAD
   static fetchSuggestionsForCurrentUser(elementType, query, collectId, isShared = false) {
     const urlParams=new URLSearchParams({
       query: encodeURIComponent(query),
@@ -9,6 +10,11 @@ export default class SuggestionsFetcher {
 
     return fetch(
       `/api/v1/suggestions/${elementType}?`+urlParams,
+=======
+  static fetchSuggestionsForCurrentUser(elementType, query, collectId) {
+    return fetch(
+      `/api/v1/suggestions/${elementType}?query=${encodeURIComponent(query)}&collection_id=${collectId}`,
+>>>>>>> WIP deprecate is_shared
       { credentials: 'same-origin' }
     ).then(response => response.json())
       .then(json => json.suggestions)
