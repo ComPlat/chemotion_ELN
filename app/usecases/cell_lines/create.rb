294bfa660bb98f5e2b0e702f1166aaf748683839
FabianMauz <fmauz@ipb-halle.de>
2023-05-22 15:14:55 +0000

    WIP : implement 'create' usecase

--- app/usecases/cell_lines/create.rb
+++ app/usecases/cell_lines/create.rb
@@ -8,7 +8,33 @@ module Usecases
         @params = params
       end
 
-      def execute!; end
+      def execute! 
+        check_parameter
+        cell_line_material = find_cellline_material || create_cellline_material
+      end
+
+      def find_cellline_material 
+
+      end
+
+      def create_cellline_material 
+
+      end
+
+      def check_parameter
+        check_ontology(@params[:organism])
+        check_ontology(@params[:tissue])
+        raise 'amount not valid' unless @params[:amount] && @params[:amount].class == Integer && @params[:amount] >=0
+        raise 'passage not valid' unless @params[:passage] && @params[:passage].class == Integer && @params[:passage] >=0
+        raise 'disease not valid' unless @params[:disease] && @params[:disease].class == String && !@params[:disease].empty?
+        raise 'material name not valid' unless @params[:material_names] && @params[:material_names].length >=0 && !@params[:material_names][0].empty?
+      end
+
+      def check_ontology(field) 
+          return if field.class == String && !field.empty?
+
+          raise 'error' 
+      end
     end
   end
 end
--- spec/usecases/cell_lines/create_spec.rb
+++ spec/usecases/cell_lines/create_spec.rb
@@ -2,10 +2,40 @@
 
 require 'spec_helper'
 
-RSpec.describe Usecases::CellLines::Create do
+RSpec.describe Usecases::CellLines::Create do  
+  let(:user){create(:user)}
+ 
+  let(:params) do
+    {
+      amount: 100,
+      passage: 42,
+      contamination: '',
+      source:'IPB',
+      growth_medium:'water',
+      name:'probe-123',
+      description:'none',
+      material_names:['name-001,name-002'],
+      type:'primary cells',
+      organism:'mouse',
+      tissue:'leg',
+      disease:'cancer',
+      biosafety_level:'S0',
+      variant:'v1',
+      optimal_growth_temp:36.3,
+      cryo_pres_medium:'nitrogen',
+      gender:'male',
+      material_description:'a cell'
+    }
+  end
   describe 'execute!' do
     context 'when data is not valid' do
+      let(:use_case){Usecases::CellLines::Create.new(params,user)}
+      before do
+        use_case.execute!
+      end
+
       it 'error message delivered' do
+        
       end
 
       it 'no cell line object was saved' do
