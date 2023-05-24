3053eb9cd0c1503a06afaf238e4ed4e332bee103
fathia <f.idiris1@gmail.com>
2023-05-24 09:12:06 +0000

    update editor api use PUBLIC_URL

--- app/api/chemotion/editor_api.rb
+++ app/api/chemotion/editor_api.rb
@@ -43,7 +43,7 @@ module Chemotion
               fileType: 'docx',
               key: token,
               title: @attachment.filename,
-              url: "http://#{request.host_with_port}/api/v1/public/download?token=#{token}",
+              url: "#{ENV.fetch('PUBLIC_URL')}/api/v1/public/download?token=#{token}",
               permissions: {
                 download: true,
                 edit: true,
@@ -52,7 +52,7 @@ module Chemotion
               },
             },
             editorConfig: {
-              callbackUrl: "http://#{request.host_with_port}/api/v1/public/callback",
+              callbackUrl: "#{ENV.fetch('PUBLIC_URL')}/api/v1/public/callback",
               mode: 'edit',
               lang: 'en',
               customization: {
