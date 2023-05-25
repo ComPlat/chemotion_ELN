8dda4a194025b848a70c76b2f32d9116966ece67
FabianMauz <fmauz@ipb-halle.de>
2023-05-25 14:23:41 +0000

    implement apo for update celllines

--- app/api/chemotion/cell_line_api.rb
+++ app/api/chemotion/cell_line_api.rb
@@ -3,10 +3,10 @@
 module Chemotion
   class CellLineAPI < Grape::API
     rescue_from ActiveRecord::RecordNotFound do
-      error!('Collection not found', 401)
+      error!('Ressource not found', 401)
     end   
     resource :cell_lines do
-      desc 'Get a cell line by id'      
+      desc 'Get a cell line by id'
       params do
         requires :id, type: Integer, desc: 'id of cell line sample to load'
       end
@@ -39,15 +39,40 @@ module Chemotion
         optional :description, type: String, desc: 'description of a cell line sample'
       end
       post do
+        
         error!('401 Unauthorized', 401) unless current_user.collections.find(params[:collection_id])
-        # sanitize input
         useCase = Usecases::CellLines::Create.new(params, current_user)
         cell_line_sample = useCase.execute!
         return present cell_line_sample, with: Entities::CellLineSampleEntity
       end
-
+      desc 'Update a Cell line sample'
+      params do
+        requires :cell_line_sample_id, type: String, desc: 'id of the cell line to update'
+        optional :organism, type: String, desc: 'name of the donor organism of the cell'
+        optional :tissue, type: String, desc: 'tissue from which the cell originates'
+        optional :amount, type: Integer, desc: 'amount of cells'
+        optional :passage, type: Integer, desc: 'passage of cells'
+        optional :disease, type: String, desc: 'deasease of cells'
+        optional :material_names, type: String, desc: 'names of cell line e.g. [name1,name2]'
+        optional :collection_id, type: Integer, desc: 'Collection of the cell line sample'
+        optional :cell_type, type: String, desc: 'type of cells'
+        optional :biosafety_level, type: String, desc: 'biosafety_level of cells'
+        optional :variant, type: String, desc: 'variant of cells'
+        optional :optimal_growth_temp, type: Float, desc: 'optimal_growth_temp of cells'
+        optional :cryo_pres_medium, type: String, desc: 'cryo preservation medium of cells'
+        optional :gender, type: String, desc: 'gender of donor organism'
+        optional :material_description, type: String, desc: 'description of cell line concept'
+        optional :contamination, type: String, desc: 'contamination of a cell line sample'
+        optional :source, type: String, desc: 'source of a cell line sample'
+        optional :name, type: String, desc: 'name of a cell line sample'
+        optional :description, type: String, desc: 'description of a cell line sample'
+      end
       put do
+        useCase = Usecases::CellLines::Update.new(params, current_user)
+        cell_line_sample = useCase.execute!
+        return present cell_line_sample, with: Entities::CellLineSampleEntity
       end
     end
+    
   end
 end
