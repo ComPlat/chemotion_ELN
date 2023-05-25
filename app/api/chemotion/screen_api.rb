242a45779a2d6ae45f1c4e66cf7e9249b2812ad1
PiTrem <pierre.tremouilhac@kit.edu>
2023-05-25 20:42:18 +0200

    define collection scope owned_by(user_ids) and shared_with(userids)

--- app/api/chemotion/screen_api.rb
+++ app/api/chemotion/screen_api.rb
@@ -24,7 +24,7 @@ module Chemotion
       get do
         scope = if params[:collection_id]
                   begin
-                    Collection.belongs_to_current_user(current_user.id, current_user.group_ids)
+                    Collection.owned_by(user_ids)
                               .find(params[:collection_id]).screens
                   rescue ActiveRecord::RecordNotFound
                     Screen.none
