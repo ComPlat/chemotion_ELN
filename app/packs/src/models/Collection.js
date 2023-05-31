3c6a165158b5a13d5b0b34c43ff6f554aa21034c
PiTrem <pierre.tremouilhac@kit.edu>
2023-05-31 17:37:43 +0200

    ContextAction Btn: move checks on collection to collection model

--- app/packs/src/models/Collection.js
+++ app/packs/src/models/Collection.js
@@ -1,4 +1,5 @@
 import uuid from 'uuid';
+import { PermissionConst } from 'src/utilities/PermissionConst';
 
 export default class Collection {
   constructor(args) {
@@ -32,8 +33,30 @@ export default class Collection {
     return this.user_id === this.currentUser?.id;
   }
 
+  ownedByMeAndNotAll() {
+    return this.ownedByMe() && !this.allCollection();
+  }
+
   //TODO find proper name
   defCol() {
     return this.ownedByMe() && !this.allCollection() ? this.id : null;
   }
+
+  get acl() {
+    return this.collection_acls || [];
+  }
+
+  // check if the collection is shared with the current user
+  // and permission level is at least write
+  hasSharedWrite() {
+    return !!this.acl.find((acl) => (
+      acl.permission_level >= PermissionConst.Write
+    ));
+  }
+
+  // can create elements in the collection
+  canCreateElement() {
+    return this.ownedByMeAndNotAll() || this.hasSharedWrite();
+  }
+
 }
