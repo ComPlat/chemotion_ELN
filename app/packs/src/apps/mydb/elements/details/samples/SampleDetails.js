5b67bfe57d89a2205162b2aadbd2e3617fe93e86
PiTrem <pierre.tremouilhac@kit.edu>
2023-05-31 15:46:39 +0200

    mv collection properties to collection model

--- app/packs/src/apps/mydb/elements/details/samples/SampleDetails.js
+++ app/packs/src/apps/mydb/elements/details/samples/SampleDetails.js
@@ -495,13 +495,11 @@ export default class SampleDetails extends React.Component {
     const titleTooltip = `Created at: ${sample.created_at} \n Updated at: ${sample.updated_at}`;
 
     const { currentCollection } = UIStore.getState();
-    const defCol = currentCollection && currentCollection.is_shared === false &&
-      currentCollection.is_locked === false && currentCollection.label !== 'All' ? currentCollection.id : null;
 
     const copyBtn = (sample.can_copy && !sample.isNew) ? (
       <CopyElementModal
         element={sample}
-        defCol={defCol}
+        defCol={currentCollection?.defCol()}
       />
     ) : null;
 
