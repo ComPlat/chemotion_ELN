aa44b9f8ea071ad8cd77d0ce6449381cf639f5e6
PiTrem <pierre.tremouilhac@kit.edu>
2023-05-31 14:00:45 +0200

    add level details to entities

--- app/api/entities/collection_acl_entity.rb
+++ app/api/entities/collection_acl_entity.rb
@@ -8,6 +8,11 @@ module Entities
     expose :permission_level, documentation: { type: 'string', desc: 'Permission Level' }
     expose :created_at, documentation: { type: 'string', desc: 'Created At' }
     expose :updated_at, documentation: { type: 'string', desc: 'Updated At' }
+    expose :sample_detail_level, documentation: { type: 'string', desc: 'Sample Detail Level' }
+    expose :reaction_detail_level, documentation: { type: 'string', desc: 'Reaction Detail Level' }
+    expose :researchplan_detail_level, documentation: { type: 'string', desc: 'Researchplan Detail Level' }
+    expose :wellplate_detail_level, documentation: { type: 'string', desc: 'Wellplate Detail Level' }
+    expose :screen_detail_level, documentation: { type: 'string', desc: 'Screen Detail Level' }
 
     expose :user, using: Entities::UserBasicEntity, documentation: { type: 'Entities::UserEntity', desc: 'User Info' }
   end
