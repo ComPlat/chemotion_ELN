1a01a27a8afa0e2e50818778eae8b95c49c734eb
PiTrem <pierre.tremouilhac@kit.edu>
2023-05-31 17:35:05 +0200

    CollectionEntity: return acl only for current user

--- app/api/entities/collection_shared_entity.rb
+++ app/api/entities/collection_shared_entity.rb
@@ -3,6 +3,11 @@
 module Entities
   class CollectionSharedEntity < CollectionEntity
     expose :collection_acls, using: Entities::CollectionAclEntity
-    expose :user, using: Entities::UserBasicEntity
+    expose :user, using: Entities::UserSimpleEntity
+
+    # only return acl for current user
+    def collection_acls
+      object.collection_acls.shared_with(options[:shared_with])
+    end
   end
 end
