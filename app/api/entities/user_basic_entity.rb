20b9ae9a7cbbf192192ab4f9daed51343498ab64
PiTrem <pierre.tremouilhac@kit.edu>
2023-05-31 16:52:29 +0200

    rm duplicate user entity

--- app/api/entities/user_basic_entity.rb
+++
@@ -1,8 +0,0 @@
-module Entities
-  class UserBasicEntity < Grape::Entity
-    expose :id, documentation: { type: "Integer", desc: "User's unique id"}
-    expose :name, documentation: { type: "String", desc: "User's name" }
-    expose :initials, documentation: { type: "String", desc: "initials" }
-    expose :type, documentation: { type: "String", desc: "User's type" }
-  end
-end
