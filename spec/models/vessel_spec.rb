62560ddffb15330043ea5e2171abbb62f3a7bf3e
Devang Patel <67055123+endevor-music@users.noreply.github.com>
2023-05-31 14:12:00 +0200

    wip vessel model tests

--- spec/models/vessel_spec.rb
+++ spec/models/vessel_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Vessel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is possible to create a valid collection' do
+    user = create(:user)
+    collection = create(:collection)
+
+    vessel_template = VesselTemplate.create()
+    vessel = Vessel.create(vessel_template: vessel_template, creator: user)
+
+    collectionVessels = CollectionsVessel.create(collection:  collection, vessel: vessel)
+  end
 end
