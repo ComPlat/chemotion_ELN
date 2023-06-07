44726673ad65c57797a6b7089753bd6332c104f3
FabianMauz <fmauz@ipb-halle.de>
2023-06-07 12:43:03 +0000

    add test for creating cell line in mobx store

--- spec/javascripts/factories/CellLineFactory.js
+++ spec/javascripts/factories/CellLineFactory.js
@@ -32,5 +32,30 @@ export default class CellLineFactory {
       cellLine.cellLineId = 2;
       return cellLine;
     });
+    this.factory.define('heLa', CellLine, async () => {
+      const cellLine = CellLine.buildEmpty(0,'');
+      cellLine.cellLineName = 'HeLa';
+      cellLine.cellLineId = 2;
+      cellLine.id = 1;
+      cellLine.organism = 'Human';
+      cellLine.tissue = 'Cervix';
+      cellLine.cellType = 'Epithelium';
+      cellLine.mutation = 'None';
+      cellLine.disease = 'cervical cancer';
+      cellLine.bioSafetyLevel = 'S1';
+      cellLine.variant = '';
+      cellLine.optimalGrowthTemperature = 36;
+      cellLine.cryopreservationMedium = 'DMSO Serum free media, contains 8.7% DMSO in MEM supplemented with methyl cellulose';
+      cellLine.materialDescription = 'Test data for cell line material';
+      cellLine.gender = 'female';
+      cellLine.amount = 1_000_000;
+      cellLine.passage = 1;
+      cellLine.contamination = 'Myococci';
+      cellLine.source = 'The Random Company';
+      cellLine.growthMedium = 'DMEM (High Glucose) + 10% FBS';
+      cellLine.itemDescription = 'Test data for cell line sample';
+      cellLine.short_label = 'SHA-001';
+      return cellLine;
+    });
   }
 }
--- spec/javascripts/stores/mobx/CellLineDetailsStore.spec.js
+++ spec/javascripts/stores/mobx/CellLineDetailsStore.spec.js
@@ -1,13 +1,45 @@
 import expect from 'expect';
 import { CellLineDetailsStore } from 'src/stores/mobx/CellLineDetailsStore';
+import CellLineFactory from 'factories/CellLineFactory';
 
 
-describe('CellLineDetailsStore', () => {
+describe('CellLineDetailsStore', async () => {  
     const store = CellLineDetailsStore.create({})
-    describe('.setMaterialProperties', () => {
-        it('when object not available', () => {
-            expect(() => store.setMaterialProperties(-1,{})).toThrowError('no cellline with id found: -1');
+    describe('.setMaterialProperties', async() => {
+        describe('when object not available', async() => {
+            it('raises an error', async() => {
+                expect(() => store.setMaterialProperties(-1,{})).toThrowError('no cellline with id found: -1');
+            });
+        });
+    });
+    describe('.convertCellLineToModel', async() => {
+        describe('when object not available', async() => {
+            it('creates a valid entry in the store', async() => {
+                const cellLineSample = await CellLineFactory.build('heLa');
+                store.convertCellLineToModel(cellLineSample);
+                const storeSample =store.cellLines(cellLineSample.id);
+
+                expect(storeSample.cellLineName).toBe('HeLa');
+                expect(storeSample.id).toBe("1");
+                expect(storeSample.cellLineId).toBe(2);
+                expect(storeSample.organism).toBe('Human');
+                expect(storeSample.tissue).toBe('Cervix');
+                expect(storeSample.cellType).toBe('Epithelium');
+                expect(storeSample.mutation).toBe('None');
+                expect(storeSample.disease).toBe('cervical cancer');
+                expect(storeSample.bioSafetyLevel).toBe('S1');
+                expect(storeSample.optimalGrowthTemperature).toBe(36);
+                expect(storeSample.cryopreservationMedium).toBe('DMSO Serum free media, contains 8.7% DMSO in MEM supplemented with methyl cellulose');
+                expect(storeSample.materialDescription).toBe('Test data for cell line material');
+                expect(storeSample.gender).toBe('female');
+                expect(storeSample.amount).toBe(1_000_000);
+                expect(storeSample.passage).toBe(1);
+                expect(storeSample.contamination).toBe('Myococci');
+                expect(storeSample.source).toBe('The Random Company');
+                expect(storeSample.growthMedium).toBe('DMEM (High Glucose) + 10% FBS');
+                expect(storeSample.itemDescription).toBe('Test data for cell line sample');
+                expect(storeSample.shortLabel).toBe('SHA-001');
+            });
         });
     });
 });
-
