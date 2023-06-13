787b96841570f03f5e4a8fe2678e338203cf6041
FabianMauz <fmauz@ipb-halle.de>
2023-06-13 09:29:54 +0000

    WIP - create sub cell line item

--- app/packs/src/apps/mydb/elements/list/cellLine/CellLineEntry.js
+++ app/packs/src/apps/mydb/elements/list/cellLine/CellLineEntry.js
@@ -3,6 +3,9 @@ import UIStore from 'src/stores/alt/stores/UIStore';
 import CellLineItemEntry from 'src/apps/mydb/elements/list/cellLine/CellLineItemEntry';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
+import { elementShowOrNew } from 'src/utilities/routesUtils';
+import ElementStore from 'src/stores/alt/stores/ElementStore';
+
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 
@@ -37,6 +40,7 @@ export default class CellLineEntry extends Component {
     return [
       this.renderArrow(),
       this.renderDetailedInfoButton(),
+      this.renderCreateSubSampleButton(),
       <div
         key={firstCellLineItem.cellLineName}
         className="cell-line-group-header-name"
@@ -89,7 +93,34 @@ export default class CellLineEntry extends Component {
           <i className="fa fa-info-circle" aria-hidden="true" />
         </Button>
       );
-     
+  }
+  renderCreateSubSampleButton(){
+    return (
+        <Button
+          className={"button-right "}
+          bsSize="xsmall"
+          onClick={(event) => {
+            event.stopPropagation();
+
+            const { currentCollection, isSync } = UIStore.getState();
+            const ui_state=UIStore.getState();
+            const uri = isSync
+              ? `/scollection/${currentCollection.id}/cell_line/new`
+              : `/collection/${currentCollection.id}/cell_line/new`;
+            Aviator.navigate(uri, { silent: true });
+
+            const e = { 
+              type:"cell_line", params: 
+              { collectionID: currentCollection.id,
+                cell_lineID: "new"
+              }
+            };
+            elementShowOrNew(e);
+          }}
+        >
+          <i className="fa fa-plus" aria-hidden="true" />
+        </Button>
+      );
   }
 
   // eslint-disable-next-line class-methods-use-this
