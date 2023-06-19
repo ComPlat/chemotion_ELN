00213dd85343bc6daa6d4bba02fc84033d3bccb8
Jan C. Brammer <jan.c.brammer@gmail.com>
2023-06-19 08:46:01 +0000

    Update style

--- app/packs/src/apps/mydb/elements/details/reactions/variationsTab/ReactionVariations.js
+++ app/packs/src/apps/mydb/elements/details/reactions/variationsTab/ReactionVariations.js
@@ -6,7 +6,7 @@ import React, {
 import { v4 as uuidv4 } from 'uuid';
 import {
   ToggleButtonGroup, ToggleButton, Button, FormGroup, Radio, ControlLabel, ButtonGroup,
-  OverlayTrigger
+  OverlayTrigger, Tooltip, Form
 } from 'react-bootstrap';
 import _ from 'lodash';
 import { iupacNameTooltip } from 'src/apps/mydb/elements/details/reactions/schemeTab/Material';
@@ -25,26 +25,36 @@ function MaterialHeader({ material }) {
 function RowToolsCellRenderer({ data, copyRow, removeRow }) {
   return (
     <ButtonGroup>
-      <Button onClick={() => copyRow(data)}>Copy</Button>
-      <Button onClick={() => removeRow(data)}>Remove</Button>
+      <Button onClick={() => copyRow(data)}><i className="fa fa-copy"></i></Button>
+      <Button onClick={() => removeRow(data)}><i className="fa fa-trash"></i></Button>
     </ButtonGroup>
   );
 }
 
-const CellRenderer = (props) => {
-  const { value = '', unit = 'None', aux = {} } = props.value ?? {};
-  let cellContent = `${Number(value) ? Number(value).toFixed(3) : 'NaN'} [${unit}]`;
-  if (aux.coefficient) {
-    cellContent += `; Coeff: ${aux.coefficient}`;
+function CellRenderer({ value: cellData }) {
+  const { value = '', unit = 'None', aux = {} } = cellData ?? {};
+  const cellContent = `${Number(value) ? Number(value).toFixed(3) : 'NaN'} [${unit}]`;
+
+  let overlayContent = aux.coefficient ? `Coeff: ${aux.coefficient}` : '';
+  overlayContent += aux.isReference ? '; Ref' : '';
+  overlayContent += aux.yield ? `; Yield: ${aux.yield}%` : '';
+  if (!overlayContent) {
+    return cellContent;
   }
-  if (aux.isReference) {
-    cellContent += '; Ref';
-  }
-  if (aux.yield) {
-    cellContent += `; Yield: ${aux.yield}`;
-  }
-  return cellContent;
-};
+
+  const overlay = (
+    <Tooltip>
+      {overlayContent}
+    </Tooltip>
+  );
+  return (
+    <OverlayTrigger placement="bottom" overlay={overlay}>
+      <div>
+        {cellContent}
+      </div>
+    </OverlayTrigger>
+  );
+}
 
 const CellEditor = forwardRef((props, ref) => {
   const { value = '', unit = 'None', aux = {} } = props.value ?? {};
@@ -193,26 +203,33 @@ export default function ReactionVariations({ reaction, onEditVariations }) {
 
   return (
     <div>
-      <Button onClick={() => addRow()}>+</Button>
+      <Form inline>
+        <Button onClick={() => addRow()}>Add row</Button>
+        {' '}
+        <FormGroup>
+          <ControlLabel>Material Unit</ControlLabel>
+          {' '}
+          {['Equiv', 'Amount'].map(
+            (unit) => (
+              <Radio
+                title="Material Unit"
+                key={unit}
+                checked={materialUnit === unit}
+                onChange={() => setMaterialUnit(unit)}
+                inline
+              >
+                {unit}
+              </Radio>
+            )
+          )}
+        </FormGroup>
+      </Form>
       <ToggleButtonGroup type="checkbox" defaultValue={togglableColumnGroups} onChange={(columnGroupsToDisplay) => toggleColumnGroupVisibility(columnGroupsToDisplay)}>
         {togglableColumnGroups.map(
           (column) => <ToggleButton key={column} value={column}>{column}</ToggleButton>
         )}
       </ToggleButtonGroup>
-      <FormGroup>
-        <ControlLabel>Material Unit</ControlLabel>
-        {['Equiv', 'Amount'].map(
-          (unit) => (
-            <Radio
-              key={unit}
-              checked={materialUnit === unit}
-              onChange={() => setMaterialUnit(unit)}
-            >
-              {unit}
-            </Radio>
-          )
-        )}
-      </FormGroup>
+
       <div style={{ height: '50vh' }} className="ag-theme-alpine">
         <AgGridReact
           ref={gridRef}
@@ -224,8 +241,6 @@ export default function ReactionVariations({ reaction, onEditVariations }) {
             cellRenderer: CellRenderer,
             wrapHeaderText: true,
             autoHeaderHeight: true,
-            wrapText: true,
-            autoHeight: true,
           }}
         />
       </div>
