fed2b863f2a308b2854d4874398e22f09dd21700
TasnimMehzabin <TasnimMehzabin@users.noreply.github.com>
2023-06-20 19:36:38 +0200

    Add inbox pagination (#1108)
    * Inbox pagination is added
    - API is added to fetch the datasets individually per container
    - API is integrated in the inbox section
    - Attachment count functionality is modified based on the current implementation
    
    * Refactor the inbox pagination with individual API calls
    
    * implement the initial pagination on the inbox
    
    * display 20 inbox items at a time
    sort inbox items by created_at desc
    modify and refactor DeviceBoxEntity
    will take to inbox modal if inbox items are clicked in the collectionTree section
    will display ellipsis in collectionTree inbox section if there are more inbox items so that user is taken to inbox modal page
    add badge to the inbox icon in the navigation section
    revert countAttachments to proper logic
    
    * limit the number of datasets and the number of attachments per dataset
    
    * open particular deviceBox in the inbox modal that was clicked in the collectionTree inbox section
    keep only one deviceBox open at a time in the inbox modal
    
    * add pagination option with next and previous buttons within inbox datasets
    close other sections when the inbox section is open in the collection tree for better visibility
    clean up code removing the device_box_entity.rb and refactor within inbox_entity.rb
    handle the deletion of container or attachment from inbox
    refactor the code to fix the number of attachments in the inbox upon container or attachment deletion
    open the inbox modal when the unsorted section is clicked from the inbox of collection tree
    optimize the datasets' sorting by name within inbox container
    fix the research plan API issue
    
    * reduce datasets per page to 35 for each DeviceBox
    
    * modify variable names to avoid issues with variables with the same name in the store
    refactor the implementation of container children sorting by name
    make containers accessible with authorized users only
    refactor the query to load 50 attachments per dataset
    persist the page number within the dataset
    fix the issue with inbox refresh
    add page count within dataset pagination
    add loader when unsorted attachments are being uploaded
    
    
    * fix the issue of opening the list of Unsorted attachments when the upload button is clicked
    
    
    ---------
    
    Co-authored-by: PiTrem <pierre.tremouilhac@kit.edu>

--- app/packs/src/apps/mydb/inbox/InboxModal.js
+++ app/packs/src/apps/mydb/inbox/InboxModal.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Draggable from 'react-draggable';
-import { Badge, Button, Panel, Glyphicon } from 'react-bootstrap';
+import {
+  Badge, Button, Panel, Glyphicon, Pagination
+} from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import InboxStore from 'src/stores/alt/stores/InboxStore';
 import InboxActions from 'src/stores/alt/actions/InboxActions';
@@ -17,9 +19,14 @@ export default class InboxModal extends React.Component {
 
     this.state = {
       inbox: inboxState.inbox,
-      inboxVisible: false,
+      inboxVisible: inboxState.inboxVisible,
       numberOfAttachments: inboxState.numberOfAttachments,
-      visible: inboxState.inboxModalVisible
+      visible: inboxState.inboxModalVisible,
+
+      currentPage: inboxState.currentPage,
+      itemsPerPage: inboxState.itemsPerPage,
+      totalPages: inboxState.totalPages,
+      activeDeviceBoxId: inboxState.activeDeviceBoxId,
     };
 
     this.onChange = this.onChange.bind(this);
@@ -35,23 +42,43 @@ export default class InboxModal extends React.Component {
     InboxStore.unlisten(this.onChange);
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const { currentPage, itemsPerPage } = this.state;
+    if (prevState.currentPage !== currentPage
+        || prevState.itemsPerPage !== itemsPerPage) {
+      InboxActions.fetchInbox({ currentPage, itemsPerPage });
+    }
+  }
+
   onChange(state) {
     this.setState(state);
     this.setState({ visible: state.inboxModalVisible });
   }
 
   onClickInbox() {
-    const { inboxVisible, inbox } = this.state;
-    this.setState({ inboxVisible: !inboxVisible });
+    const {
+      inboxVisible, inbox, currentPage, itemsPerPage
+    } = this.state;
+    InboxActions.setInboxVisible({ inboxVisible: !inboxVisible });
     if (!inbox.children) {
       LoadingActions.start();
-      InboxActions.fetchInbox();
+      InboxActions.fetchInbox({ currentPage, itemsPerPage });
+    }
+  }
+
+  handlePageChange(pageNumber) {
+    const { totalPages } = this.state;
+    if (pageNumber > 0 && pageNumber <= totalPages) {
+      this.setState({
+        currentPage: pageNumber
+      }, () => InboxActions.setInboxPagination({ currentPage: this.state.currentPage }));
     }
   }
 
   refreshInbox() {
+    const { currentPage, itemsPerPage } = this.state;
     LoadingActions.start();
-    InboxActions.fetchInbox();
+    InboxActions.fetchInbox({ currentPage, itemsPerPage });
   }
 
   lockedSubtrees() {
@@ -60,8 +87,48 @@ export default class InboxModal extends React.Component {
     return this.subtrees(roots, null, false);
   }
 
+  renderPagination = () => {
+    const { currentPage, totalPages } = this.state;
+
+    if (totalPages <= 1) {
+      return;
+    }
+
+    const pageNumbers = [];
+    const minPage = Math.max(currentPage - 4, 1);
+    const maxPage = Math.min(minPage + 4, totalPages);
+
+    for (let i = minPage; i <= maxPage; i += 1) {
+      pageNumbers.push(
+        <Pagination.Item
+          key={i}
+          active={i === currentPage}
+          onClick={() => this.handlePageChange(i)}
+        >
+          {i}
+        </Pagination.Item>
+      );
+    }
+
+    if (totalPages > maxPage) {
+      pageNumbers.push(<Pagination.Ellipsis key="Ell" />);
+    }
+
+    return (
+      <div className="list-pagination">
+        <Pagination>
+          <Pagination.First disabled={currentPage === 1} key="First" onClick={() => this.handlePageChange(1)} />
+          <Pagination.Prev disabled={currentPage === 1} key="Prev" onClick={() => this.handlePageChange(currentPage - 1)} />
+          {pageNumbers}
+          <Pagination.Next disabled={currentPage === totalPages} key="Next" onClick={() => this.handlePageChange(currentPage + 1)} />
+          <Pagination.Last disabled={currentPage === totalPages} key="Last" onClick={() => this.handlePageChange(totalPages)} />
+        </Pagination>
+      </div>
+    );
+  };
+
   inboxSubtrees() {
-    const { inbox } = this.state;
+    const { inbox, activeDeviceBoxId } = this.state;
 
     let boxes = '';
     if (inbox.children) {
@@ -69,25 +136,34 @@ export default class InboxModal extends React.Component {
         if (a.name > b.name) { return 1; } if (a.name < b.name) { return -1; } return 0;
       });
       boxes = inbox.children.map(deviceBox => (
-        <DeviceBox key={`box_${deviceBox.id}`} device_box={deviceBox} largerInbox />
+        <DeviceBox
+          key={`box_${deviceBox.id}`}
+          device_box={deviceBox}
+          largerInbox
+          deviceBoxVisible={deviceBox.id === activeDeviceBoxId}
+        />
       ));
     }
 
     return (
       <div className="tree-view">
         {boxes}
-        {inbox.unlinked_attachments
-          ? <UnsortedBox key="unsorted_box" unsorted_box={inbox.unlinked_attachments} largerInbox />
-          : ''
-        }
+        {this.renderPagination()}
+        {inbox.unlinked_attachments ? (
+          <UnsortedBox
+            key="unsorted_box"
+            unsorted_box={inbox.unlinked_attachments}
+            unsortedVisible={activeDeviceBoxId === -1}
+            largerInbox
+          />
+        ) : ''}
       </div>
     );
   }
 
   render() {
     const { showCollectionTree } = this.props;
-    const { visible, inboxVisible } = this.state;
-
+    const { visible, inboxVisible, numberOfAttachments } = this.state;
 
     const panelClass = showCollectionTree ? 'small-col col-md-6' : 'small-col col-md-5';
     const inboxDisplay = inboxVisible ? '' : 'none';
@@ -113,7 +189,13 @@ export default class InboxModal extends React.Component {
                   <span style={{ marginLeft: '10px', marginRight: '5px' }}>Inbox</span>
                 </button>
                 {
-                  this.state.numberOfAttachments > 0 ? <Badge> {this.state.numberOfAttachments} </Badge> : ''
+                  numberOfAttachments > 0 ? (
+                    <Badge>
+                      {' '}
+                      {numberOfAttachments}
+                      {' '}
+                    </Badge>
+                  ) : ''
                 }
                 <Button
                   bsStyle="danger"
