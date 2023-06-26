2810378f8dac169537178d414de7b1a58e2a476e
mekkyz <mos.mekky@gmail.com>
2023-06-26 13:56:34 +0000

    using timezone info from backend

--- spec/javascripts/utils/timezoneHelper.spec.js
+++ spec/javascripts/utils/timezoneHelper.spec.js
@@ -3,18 +3,11 @@ import moment from 'moment';
 import formatDate from 'src/utilities/timezoneHelper';
 
 describe('formatDate', () => {
-  it('should correctly parse and format the date string', () => {
-    let input = '23.06.2023, 11:00';
+  it('should correctly format the date', () => {
+    const testDate = moment().format('DD.MM.YYYY, HH:mm Z');
+    const expectedOutput = moment(testDate, 'DD.MM.YYYY, HH:mm Z').local().format('LLLL');
+    const actualOutput = formatDate(testDate);
 
-    // subtracting 2 hours from the input time to match the testing environment UTC+2 timezone
-    const inputDate = moment.utc(input, 'DD.MM.YYYY, HH:mm').subtract(2, 'hours');
-
-    input = inputDate.format('DD.MM.YYYY, HH:mm');
-
-    const expectedOutput = inputDate.local().format('LLLL');
-
-    const result = formatDate(input);
-
-    expect(result).toEqual(expectedOutput);
+    expect(actualOutput).toEqual(expectedOutput);
   });
 });
