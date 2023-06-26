5017cf633721f6c45db8a6db0ba26b31afab8d75
mekkyz <mos.mekky@gmail.com>
2023-06-26 08:45:20 +0000

    matching the test env timezone

--- spec/javascripts/utils/timezoneHelper.spec.js
+++ spec/javascripts/utils/timezoneHelper.spec.js
@@ -4,9 +4,14 @@ import formatDate from 'src/utilities/timezoneHelper';
 
 describe('formatDate', () => {
   it('should correctly parse and format the date string', () => {
-    const input = '23.06.2023, 11:00';
+    let input = '23.06.2023, 11:00';
 
-    const expectedOutput = moment(input, 'DD.MM.YYYY, HH:mm').local().format('LLLL');
+    // subtracting 2 hours from the input time to match the testing environment UTC+2 timezone
+    const inputDate = moment.utc(input, 'DD.MM.YYYY, HH:mm').subtract(2, 'hours');
+
+    input = inputDate.format('DD.MM.YYYY, HH:mm');
+
+    const expectedOutput = inputDate.local().format('LLLL');
 
     const result = formatDate(input);
 
