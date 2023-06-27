71df85399c020b6f528004f6f72c3f8e87d8ebc3
Devang Patel <67055123+endevor-music@users.noreply.github.com>
2023-06-27 13:44:48 +0200

    add vessel template entity

---
+++ app/api/entities/vessel_template_entity.rb
@@ -0,0 +1,12 @@
+module Entities
+  class VesselTemplateEntity < Grape::Entity
+    expose :id
+    expose :name
+    expose :details
+    expose :vessel_type
+    expose :volume_unit
+    expose :volume_amount
+    expose :material_type
+    expose :material_details
+  end
+end
