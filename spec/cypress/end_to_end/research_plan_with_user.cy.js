5da99959c693a2e2970a06a866626ee656b0528e
Mehmood Ghaffar <engr_mehmood86@hotmail.com>
2023-06-28 12:57:13 +0000

    create user test  and researchplan extended test are added

--- spec/cypress/end_to_end/research_plan_with_user.cy.js
+++ spec/cypress/end_to_end/research_plan_with_user.cy.js
@@ -13,4 +13,36 @@ describe('Research Plan with User', () => {
     cy.contains('My Research Plan 2');
     cy.get('#tabList-tab-4 > span').contains('1(0)');
   });
+
+  it('rename research plan', () => {
+    cy.visit('users/sign_in');
+    cy.createUserWithResearchPlan();
+    cy.login('cu1', 'user_password');
+    cy.get('#tree-id-Col1').click();
+    cy.visit('/mydb/collection/3');
+    cy.get('#tabList-tab-4 > span').click();
+    cy.get('[width="280px"] > :nth-child(1) > .preview-table').click();
+    cy.get('[style="margin: 5px 0px 5px 5px;"] > .btn').click();
+    cy.get('*[class="ql-editor"]').clear().type("ResearchPlan description");
+
+    cy.get('[data-cy="researchplan-item-delete"]').click();
+
+    cy.get('[data-cy="btn_richtext"]').click();
+    cy.get('[data-cy="researchplan-item-delete"]').click();
+
+    cy.get('[data-cy="btn_table"]').click();
+    cy.get('[data-cy="researchplan-item-delete"]').click();
+
+    cy.get('[data-cy="btn_ketcher"]').click();
+    cy.get('[data-cy="researchplan-item-delete"]').click();
+
+    cy.get('[data-cy="btn_image"]').click();
+    cy.get('[data-cy="researchplan-item-delete"]').click();
+
+    cy.get('[data-cy="btn_sample"]').click();
+    cy.get('[data-cy="researchplan-item-delete"]').click();
+
+    cy.get('[data-cy="btn_reaction"]').click();
+    cy.get('[data-cy="researchplan-item-delete"]').click();
+  });
 });
