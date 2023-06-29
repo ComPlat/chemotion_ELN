aa6d4f0b8e49e68e8a7617215561c9e49b99d0da
Devang Patel <67055123+endevor-music@users.noreply.github.com>
2023-06-29 16:14:29 +0200

    add model tests

--- spec/models/vessel_spec.rb
+++ spec/models/vessel_spec.rb
@@ -1,13 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Vessel, type: :model do
-  it 'is possible to create a valid collection' do
-    user = create(:user)
-    collection = create(:collection)
+  let(:user) { create(:user) }
+  let(:collection) { create(:collection) }
+  let(:template) { create(:vessel_template) }
+  let(:vessel) { create(:vessel, vessel_template: template, creator: user, collections: [collection]) }
 
-    vessel_template = VesselTemplate.create()
-    vessel = Vessel.create(vessel_template: vessel_template, creator: user)
+  describe 'creation' do
+    it 'is possible to create a valid vessel' do
+      expect(vessel.valid?).to be(true)
+    end
+  end
 
-    collectionVessels = CollectionsVessel.create(collection:  collection, vessel: vessel)
+  describe 'after creation' do
+    it 'has associations' do
+      expect(CollectionsVessel.find_by(vessel_id: vessel.id)).not_to be_nil
+    end
+  end
+
+  describe 'deletion' do
+    before { vessel.destroy! }
+    it 'destroys associations properly' do
+      expect(CollectionsVessel.find_by(vessel_id: vessel.id)).to be_nil
+    end
+
+    it 'soft deletes vessel' do
+      expect(vessel.deleted_at).not_to be_nil
+    end
   end
 end
--- spec/models/vessel_template_spec.rb
+++ spec/models/vessel_template_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe VesselTemplate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:collection) { create(:collection) }
+  let(:template) { create(:vessel_template) }
+  let(:vessel) { create(:vessel, vessel_template: template, creator: user, collections: [collection]) }
+
+  describe 'creation' do
+    it 'is possible to create a valid vessel template' do
+      expect(template.valid?).to be(true)
+    end
+  end
+
+  describe 'deletion' do
+    before { template.destroy! }
+    
+    it 'soft deletes template' do
+      expect(template.deleted_at).not_to be_nil
+    end
+  end
 end
