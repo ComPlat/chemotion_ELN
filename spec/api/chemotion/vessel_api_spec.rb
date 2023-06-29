f3e9f1eda331b043d09f6878f7bbe4f17d329de4
Devang Patel <67055123+endevor-music@users.noreply.github.com>
2023-06-29 14:53:26 +0200

    correct urls & add post spec

--- spec/api/chemotion/vessel_api_spec.rb
+++ spec/api/chemotion/vessel_api_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe Chemotion::VesselAPI do
   include_context 'api request authorization context'
 
-  describe 'GET /api/v1/vessel/' do
+  describe 'GET /api/v1/vessels/' do
     let(:vessel) { create(:vessel) }
     let(:user) { create(:user) }
     let(:collection) { create(:collection) }
@@ -16,7 +16,7 @@ describe Chemotion::VesselAPI do
         user.collections << collection
         user.save
 
-        get "/api/v1/vessel/#{vessel.id}"
+        get "/api/v1/vessels/#{vessel.id}"
       end
       
       it 'return correct status (http 200)' do
@@ -31,11 +31,64 @@ describe Chemotion::VesselAPI do
 
     context 'vessel does not exist' do
       before do
-        get "/api/v1/vessel/-1"
+        get "/api/v1/vessels/-1"
       end
 
       it 'returns correct status (http 401)' do
         expect(response).to have_http_status :unauthorized
+      end
+    end
+  end
+
+  describe 'POST /api/v1/vessels/' do
+    context 'with correct parameters' do
+      let(:collection) { Collection.first }
+      let(:params) do {
+        collection_id: collection.id,
+        template_name: 'Vessel Template 1',
+        details: 'multi-neck',
+        vessel_type: 'round bottom flask',
+        volume_unit: 'ml',
+        volume_amount: '500',
+        material_type: 'glass',
+        material_details: 'other material details',
+      }
+      end 
+
+      before do
+        post '/api/v1/vessels/', params: params, as: :json
+      end
+
+      it 'returns correct status code 201' do
+        expect(response).to have_http_status :created
+      end
+
+      it 'returns correct representation of saved vessel' do
+        expect(parsed_json_response['name']).to be_nil
+        expect(parsed_json_response['description']).to be_nil
+        expect(parsed_json_response['vessel_template']['name']).to eq 'Vessel Template 1'
+        expect(parsed_json_response['vessel_template']['details']).to eq 'multi-neck'
+        expect(parsed_json_response['vessel_template']['vessel_type']).to eq 'round bottom flask'
+        expect(parsed_json_response['vessel_template']['volume_unit']).to eq 'ml'
+        expect(parsed_json_response['vessel_template']['volume_amount']).to eq '500'
+        expect(parsed_json_response['vessel_template']['material_type']).to eq 'glass'
+        expect(parsed_json_response['vessel_template']['material_details']).to eq 'other material details'
+      end
+    end
+
+    context 'with incorrect parameters' do
+      let(:collection) { Collection.first }
+      let(:params) do {
+
+      }
+      end 
+
+      before do
+        post '/api/v1/vessels/', params: params, as: :json
+      end
+
+      it 'returns correct status code 400' do
+        expect(response).to have_http_status :bad_request
       end
     end
   end
