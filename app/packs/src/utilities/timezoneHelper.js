72df3bf56398863c7f222f771e3f48189be60747
PiTrem <pierre.tremouilhac@kit.edu>
2023-07-03 16:41:01 +0200

    timezoneHelper: default to iso format if parsed value invalid.
    Also use short format date llll, not LLLL.

--- app/packs/src/utilities/timezoneHelper.js
+++ app/packs/src/utilities/timezoneHelper.js
@@ -1,11 +1,27 @@
 import moment from 'moment';
 
+// see config/locale/en.yml for the format
+const elnTimestampFormat = 'DD.MM.YYYY, HH:mm:ss Z';
+
+const regExpTimestamp = /^\d{2}\.\d{2}\.\d{4}, \d{2}:\d{2}:\d{2}/;
+
+const parseDate = (dateString) => {
+  if (regExpTimestamp.test(dateString)) {
+    const date = moment(dateString, elnTimestampFormat);
+    if (date.isValid()) {
+      return date;
+    }
+  }
+  // assume ISO 8601 format
+  return moment(dateString);
+};
+
 const formatDate = (dateString) => {
-  const date = moment(dateString, 'DD.MM.YYYY, HH:mm:ss Z');
+  const date = parseDate(dateString);
 
   const localDate = date.local();
 
-  const formattedDate = localDate.format('LLLL');
+  const formattedDate = localDate.format('llll');
 
   return formattedDate;
 };
@@ -19,6 +35,8 @@ const formatTimeStampsOfElement = (element) => {
 };
 
 export {
+  elnTimestampFormat,
+  parseDate,
   formatDate,
   formatTimeStampsOfElement,
 };
