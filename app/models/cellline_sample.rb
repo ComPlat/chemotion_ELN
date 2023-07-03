8f2d3dff3025cf8915af95ed1eb73de9d2a25c34
FabianMauz <fmauz@ipb-halle.de>
2023-07-03 07:53:22 +0000

    add foreign key for literature/literal

--- app/models/cellline_sample.rb
+++ app/models/cellline_sample.rb
@@ -9,6 +9,8 @@ class CelllineSample < ApplicationRecord
   belongs_to :cell_line_sample, optional: true
   belongs_to :cellline_material
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  has_many :literals, as: :element, dependent: :destroy
+  has_many :literatures, through: :literals
 
   def create_root_container
     self.container = Container.create_root_container if container.nil?
