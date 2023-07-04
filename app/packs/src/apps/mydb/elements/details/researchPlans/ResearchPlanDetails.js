688660758d736c5de6e85b36858f0e990ecd9bfe
PiTrem <pierre.tremouilhac@kit.edu>
2023-07-04 20:52:08 +0200

    Unify datetime format (#1389)
    * use formatDate utility to format displayed timestamps
    (reduce the number of 'import moment.js' )
    
    * fix Intl time format
    
    * Entities: use default timestamp formatter
    
    * timezoneHelper: default to iso format if parsed value invalid.
    
    Also use short format date llll, not LLLL.
    
    TODO use locale ?

--- app/packs/src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails.js
+++ app/packs/src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails.js
@@ -30,6 +30,7 @@ import HeaderCommentSection from 'src/components/comments/HeaderCommentSection';
 import CommentSection from 'src/components/comments/CommentSection';
 import CommentActions from 'src/stores/alt/actions/CommentActions';
 import CommentModal from 'src/components/common/CommentModal';
+import { formatTimeStampsOfElement } from 'src/utilities/timezoneHelper';
 
 export default class ResearchPlanDetails extends Component {
   constructor(props) {
@@ -454,7 +455,7 @@ export default class ResearchPlanDetails extends Component {
   } /* eslint-enable */
 
   renderPanelHeading(researchPlan) {
-    const titleTooltip = `Created at: ${researchPlan.created_at} \n Updated at: ${researchPlan.updated_at}`;
+    const titleTooltip = formatTimeStampsOfElement(researchPlan || {});
 
     return (
       <Panel.Heading>
--- app/packs/src/utilities/timezoneHelper.js
+++ app/packs/src/utilities/timezoneHelper.js
@@ -1,13 +1,42 @@
 import moment from 'moment';
 
+// see config/locale/en.yml for the format
+const elnTimestampFormat = 'DD.MM.YYYY, HH:mm:ss Z';
+
+const regExpTimestamp = /^\d{2}\.\d{2}\.\d{4}, \d{2}:\d{2}:\d{2}/;
+
+const parseDate = (dateString) => {
+  if (regExpTimestamp.test(dateString)) {
+    const date = moment(dateString, elnTimestampFormat);
+    if (date.isValid()) {
+      return date;
+    }
+  }
+  // assume ISO 8601 format
+  return moment(dateString);
+};
+
 const formatDate = (dateString) => {
-  const date = moment(dateString, 'DD.MM.YYYY, HH:mm Z');
+  const date = parseDate(dateString);
 
   const localDate = date.local();
 
-  const formattedDate = localDate.format('LLLL');
+  const formattedDate = localDate.format('llll');
 
   return formattedDate;
 };
 
-export default formatDate;
+// return `Created at: ${formattedCreatedAt} | Updated at: ${formattedUpdatedAt}`;
+const formatTimeStampsOfElement = (element) => {
+  const { created_at, updated_at } = element;
+  const formattedCreatedAt = formatDate(created_at);
+  const formattedUpdatedAt = formatDate(updated_at);
+  return `Created ${formattedCreatedAt} - Updated ${formattedUpdatedAt}`;
+};
+
+export {
+  elnTimestampFormat,
+  parseDate,
+  formatDate,
+  formatTimeStampsOfElement,
+};
