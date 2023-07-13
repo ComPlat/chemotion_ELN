3358e6cfe094dff61d636a9d4929fc9db7883764
fathia <f.idiris1@gmail.com>
2023-07-13 13:56:51 +0000

    add permissions for move / assign to collection

--- app/packs/src/stores/alt/stores/PermissionStore.js
+++ app/packs/src/stores/alt/stores/PermissionStore.js
@@ -5,6 +5,8 @@ class PermissionStore {
   constructor() {
     this.state = {
       is_top_secret: false,
+      move_allowed: false,
+      assign_allowed: false,
       sharing_allowed: false,
       deletion_allowed: false,
       remove_allowed: false,
@@ -21,6 +23,8 @@ class PermissionStore {
     if (result.sharing_allowed !=null) {this.state.sharing_allowed = result.sharing_allowed}
     if (result.deletion_allowed !=null) {this.state.deletion_allowed = result.deletion_allowed}
     if (result.remove_allowed !=null) {this.state.remove_allowed = result.remove_allowed}
+    if (result.move_allowed !=null) {this.state.move_allowed = result.move_allowed}
+    if (result.assign_allowed !=null) {this.state.assign_allowed = result.assign_allowed}
   }
 }
 
