67e7524f8a362cc2b19cb1b6ffa8d30b50d93590
Mehreen <mehreen.mansur>
2023-07-21 10:57:10 +0200

    update send messages method for comment function

--- app/api/helpers/comment_helpers.rb
+++ app/api/helpers/comment_helpers.rb
@@ -20,17 +20,8 @@ module CommentHelpers
     end
   end
 
-  def notify_synchronized_collection_users(collections, current_user, element)
-    sync_collections = collections.synchronized
-    sync_collection_users = SyncCollectionsUser.includes(:user)
-                                               .where(shared_by_id: sync_collections.pluck(:user_id),
-                                                      collection_id: sync_collections.ids)
-
-    message_to = sync_collection_users&.flat_map do |sync_collection_user|
-      sync_collection_user.user&.send(:user_ids)
-    end
-
-    message_to = (message_to + collections.unshared.pluck(:user_id) - [current_user.id]).uniq
+  def notify_users(collections, current_user, element)
+    message_to = (collections.pluck(:user_id) - [current_user.id]).uniq
     return if message_to.blank?
 
     data_args = {
@@ -48,30 +39,30 @@ module CommentHelpers
     )
   end
 
-  def notify_shared_collection_users(shared_collections, current_user, element)
-    shared_collections&.each do |collection|
-      message_to = collection.user.send(:user_ids) - [current_user.id]
-      next if message_to.blank?
+  def notify_acl_collection_users(collections, current_user, element)
+    user_ids = CollectionAcl.where(collection: collections).pluck(:user_id)
+    message_to = (user_ids - [current_user.id]).uniq
 
-      data_args = {
-        commented_by: current_user.name,
-        element_type: element.class.to_s,
-        element_name: element_name(element),
-      }
+    return if message_to.blank?
 
-      Message.create_msg_notification(
-        channel_subject: Channel::COMMENT_ON_MY_COLLECTION,
-        message_from: current_user.id,
-        message_to: collection.user.send(:user_ids) - [current_user.id],
-        data_args: data_args,
-        level: 'info',
-      )
-    end
+    data_args = {
+      commented_by: current_user.name,
+      element_type: element.class.to_s,
+      element_name: element_name(element),
+    }
+
+    Message.create_msg_notification(
+      channel_subject: Channel::COMMENT_ON_MY_COLLECTION,
+      message_from: current_user.id,
+      message_to: collection.user.send(:user_ids) - [current_user.id],
+      data_args: data_args,
+      level: 'info',
+    )
   end
 
   def create_message_notification(collections, current_user, element)
-    notify_synchronized_collection_users(collections, current_user, element)
-    notify_shared_collection_users(collections.where(is_shared: true), current_user, element)
+    notify_users(collections, current_user, element)
+    notify_acl_collection_users(collections, current_user, element)
   end
 
   def notify_comment_resolved(comment, current_user)
