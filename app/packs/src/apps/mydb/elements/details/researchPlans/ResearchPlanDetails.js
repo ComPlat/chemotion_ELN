cd9b7132d1d28b862a6da3dbba434f5c18a376a0
Mehreen Mansur <mehreen.mansur@gmail.com>
2023-07-26 11:50:41 +0200

    fix(UI): available tab options
    * - include tab option from profile data
    - filter null values in hidden tab list
    
    Refs: #1427
    
    ---------
    
    Co-authored-by: Mehreen <mehreen.mansur>
    Co-authored-by: PiTrem <pierre.tremouilhac@kit.edu>

--- app/packs/src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails.js
+++ app/packs/src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails.js
@@ -546,7 +546,7 @@ export default class ResearchPlanDetails extends Component {
         </Tab>
       ),
       metadata: (
-        <Tab eventKey={4} title="Metadata" disabled={researchPlan.isNew} key={`metadata_${researchPlan.id}`}>
+        <Tab eventKey="metadata" title="Metadata" disabled={researchPlan.isNew} key={`metadata_${researchPlan.id}`}>
           {
             !researchPlan.isNew && <CommentSection section="research_plan_metadata" element={researchPlan} />
           }
@@ -562,7 +562,6 @@ export default class ResearchPlanDetails extends Component {
       research_plan: 'Research Plan',
       analyses: 'Analyses',
       attachments: 'Attachments',
-      literature: 'Literature',
       wellplates: 'Wellplates',
       references: 'References',
       metadata: 'Metadata',
