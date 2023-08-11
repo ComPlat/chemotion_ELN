d4ef53b70b4dbe11a3c93640f4f3c15dfa917cd8
Devang Patel <67055123+endevor-music@users.noreply.github.com>
2023-08-11 10:43:56 +0200

    test: load usecase specs
    modified usecase to raise errors for tests

--- spec/usecases/vessels/load_spec.rb
+++ spec/usecases/vessels/load_spec.rb
@@ -3,54 +3,51 @@
 require 'spec_helper'
 
 RSpec.describe Usecases::Vessels::Load do
+  let(:user) { create(:user) }
+  let(:collection) { create(:collection) }
+  let(:vessel) { create(:vessel) }
+  let(:vessel2) { create(:vessel) }
+  let(:id) { vessel.id }
+
+  let(:loaded_vessel) { described_class.new(id, user).execute! }
+
   describe 'execute!' do
-    context 'when loading parameter is by sample id' do
-      context 'when data is not valid' do
-        it 'error message delivered' do
-        end
+    before do
+      CollectionsVessel.create(
+        collection: collection,
+        vessel: vessel,
+      )
+      user.collections << collection
+      user.save
+    end
 
-        it 'returned value is empty' do
-        end
-      end
+    context 'when data is not valid' do
+      let(:loaded_vessel) { described_class.new(id, nil).execute! }
 
-      context 'when vessel does not exist' do
-        it 'returned value is empty' do
-        end
-      end
-
-      context 'when vessel does exist but user has no access' do
-        it 'returned value is empty' do
-        end
-      end
-
-      context 'when vessel does exist and user has access' do
-        it 'returned value is vessel' do
-        end
+      it 'error message delivered' do
+        expect { loaded_vessel }.to raise_error(RuntimeError, 'user is not valid')
       end
     end
 
-    context 'when loading parameter is "by collection id"' do
-      context 'when data is not valid' do
-        it 'error message delivered' do
-        end
+    context 'when vessel does not exist' do
+      let(:id) { -1 }
 
-        it 'returned value is empty' do
-        end
+      it 'returned value is empty' do
+        expect { loaded_vessel }.to raise_error(RuntimeError, 'id not valid')
       end
+    end
 
-      context 'when vessel does not exist' do
-        it 'returned value is empty' do
-        end
+    context 'when cell line does exist but user has no access' do
+      let(:id) { vessel2.id }
+
+      it 'returned value is empty' do
+        expect { loaded_vessel }.to raise_error(RuntimeError, 'user has no access to object')
       end
+    end
 
-      context 'when vessel does exist but user has no access' do
-        it 'returned value is empty' do
-        end
-      end
-
-      context 'when vessel exists and user has access' do
-        it 'returned value is vessel' do
-        end
+    context 'when cell line does exist and user has access' do
+      it 'returned value is the vessel' do
+        expect(loaded_vessel).to eq(vessel)
       end
     end
   end
