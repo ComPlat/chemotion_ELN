c16fbc2c1ec4f0e41817cb7f0f8447cb50e8c586
FabianMauz <fmauz@ipb-halle.de>
2023-08-24 12:19:37 +0000

    style: apply eslint rules

--- app/packs/src/fetchers/LiteraturesFetcher.js
+++ app/packs/src/fetchers/LiteraturesFetcher.js
@@ -5,15 +5,15 @@ import Literature from 'src/models/Literature';
 export default class LiteraturesFetcher {
   static fetchElementReferences(element) {
     if (!element || element.isNew) {
-      return Promise.resolve(Immutable.List())
+      return Promise.resolve(Immutable.List());
     }
     const { type, id } = element;
     return fetch(`/api/v1/literatures?element_type=${type}&element_id=${id}`, {
       credentials: 'same-origin'
-    }).then(response => response.json())
-      .then(json => json.literatures)
-      .then(literatures => literatures.map(literature => new Literature(literature)))
-      .then(lits => lits.reduce((acc, l) => acc.set(l.literal_id, l), new Immutable.Map()))
+    }).then((response) => response.json())
+      .then((json) => json.literatures)
+      .then((literatures) => literatures.map((literature) => new Literature(literature)))
+      .then((lits) => lits.reduce((acc, l) => acc.set(l.literal_id, l), new Immutable.Map()))
       .catch((errorMessage) => { console.log(errorMessage); });
   }
 
@@ -21,7 +21,7 @@ export default class LiteraturesFetcher {
     const { element, literature } = params;
     const { type, id } = element;
     if (!element || element.isNew) {
-      return Promise.resolve(Immutable.List())
+      return Promise.resolve(Immutable.List());
     }
     return fetch('/api/v1/literatures', {
       credentials: 'same-origin',
@@ -31,10 +31,10 @@ export default class LiteraturesFetcher {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ element_type: type, element_id: id, ref: literature })
-    }).then(response => response.json())
+    }).then((response) => response.json())
       .then((json) => { if (json.error) { throw json; } return json.literatures; })
-      .then(literatures => literatures.map(lits => new Literature(lits)))
-      .then(lits => lits.reduce((acc, l) => acc.set(l.literal_id, l), new Immutable.Map()))
+      .then((literatures) => literatures.map((lits) => new Literature(lits)))
+      .then((lits) => lits.reduce((acc, l) => acc.set(l.literal_id, l), new Immutable.Map()))
       .catch((errorMessage) => { console.log(errorMessage); throw errorMessage; });
   }
 
@@ -48,13 +48,12 @@ export default class LiteraturesFetcher {
       headers: {
         Accept: 'application/json',
       },
-    }).then(response => {
-      if (response.status==200){
-        return {}
+    }).then((response) => {
+      if (response.status === 200) {
+        return {};
       }
-      else{
-        return response.json()}
-      })
+      return response.json();
+    })
       .then((json) => { if (json.error) { throw json; } })
       .catch((errorMessage) => { throw errorMessage; });
   }
@@ -68,24 +67,24 @@ export default class LiteraturesFetcher {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(params)
-    }).then(response => response.json())
+    }).then((response) => response.json())
       .then((json) => { if (json.error) { throw json; } return json.literatures; })
-      .then(literatures => literatures.map(lits => new Literature(lits)))
-      .then(lits => lits.reduce((acc, l) => acc.set(l.literal_id, l), new Immutable.Map()))
+      .then((literatures) => literatures.map((lits) => new Literature(lits)))
+      .then((lits) => lits.reduce((acc, l) => acc.set(l.literal_id, l), new Immutable.Map()))
       .catch((errorMessage) => { console.log(errorMessage); throw errorMessage; });
   }
 
   static fetchDOIMetadata(doi) {
     return fetch(`/api/v1/literatures/doi/metadata?doi=${encodeURIComponent(doi)}`, {
       credentials: 'same-origin',
-    }).then(response => response.json())
+    }).then((response) => response.json())
       .catch((errorMessage) => { console.log(errorMessage); });
   }
 
   static fetchReferencesByCollection(params) {
     return fetch(`/api/v1/literatures/collection?id=${params.id}&is_sync_to_me=${params.is_sync_to_me || false}`, {
       credentials: 'same-origin',
-    }).then(response => response.json())
+    }).then((response) => response.json())
       .then((json) => {
         const {
           collectionRefs,
@@ -94,27 +93,27 @@ export default class LiteraturesFetcher {
           researchPlanRefs,
         } = json;
         return {
-          collectionRefs: Immutable.List(collectionRefs.map(lit => new Literature(lit))),
-          sampleRefs: Immutable.List(sampleRefs.map(lit => new Literature(lit))),
-          reactionRefs: Immutable.List(reactionRefs.map(lit => new Literature(lit))),
-          researchPlanRefs: Immutable.List(researchPlanRefs.map(lit => new Literature(lit))),
+          collectionRefs: Immutable.List(collectionRefs.map((lit) => new Literature(lit))),
+          sampleRefs: Immutable.List(sampleRefs.map((lit) => new Literature(lit))),
+          reactionRefs: Immutable.List(reactionRefs.map((lit) => new Literature(lit))),
+          researchPlanRefs: Immutable.List(researchPlanRefs.map((lit) => new Literature(lit))),
         };
       })
       .catch((errorMessage) => { console.log(errorMessage); });
   }
 
   static postReferencesByUIState(params, method = 'post') {
-    return fetch(`/api/v1/literatures/ui_state`, {
+    return fetch('/api/v1/literatures/ui_state', {
       credentials: 'same-origin',
       method,
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(params)
-    }).then(response => response.json())
-      .then(json => json.selectedRefs.map(lit => new Literature(lit)))
-      .then(lits => lits.reduce((acc, l) => acc.set(l.literal_id, l), new Immutable.Map()))
+    }).then((response) => response.json())
+      .then((json) => json.selectedRefs.map((lit) => new Literature(lit)))
+      .then((lits) => lits.reduce((acc, l) => acc.set(l.literal_id, l), new Immutable.Map()))
       .catch((errorMessage) => { console.log(errorMessage); });
   }
 }
