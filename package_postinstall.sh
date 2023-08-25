beff50e6a34eca4caa451976bd3b8bf0de510221
Fabian Mauz <fmauz@ipb-halle.de>
2023-08-25 10:46:35 +0200

    fix: patch citationjs to process doi-sici
    * build(deps): bump citation-js to 0.6.8
    
    * build: remove library patch in post_install script
    
    * feat: patch citation-js library to accept also SICIs
    
    Refs: #1486

--- package_postinstall.sh
+++ package_postinstall.sh
@@ -4,30 +4,13 @@
 
 set -euo pipefail
 
-src1=$(node -e 'console.log(require.resolve("@citation-js/core/lib-mjs/util/fetchFile.js"))')
-src2=$(node -e 'console.log(require.resolve("@citation-js/core/lib-mjs/index.js"))')
-src3=$(node -e 'console.log(require.resolve("@citation-js/plugin-bibtex/lib-mjs/input/constants.js"))')
-src4=$(node -e 'console.log(require.resolve("@citation-js/plugin-wikidata/lib-mjs/entity.js"))')
-
-[[ -e "$src1" && -e "$src2" && -e "$src3" && -e "$src4" ]] || exit 1
-
 YEL='\033[0;33m'
 NOC='\033[0m'
 yellow() {
   printf "${YEL}${1:-}${NOC}\n"
 }
 
-yellow "rewrite import for citation.js in:"
 
-yellow "$src1"
-sed -i "s~import { version } from '../../package.json';~import pkg from '../../package.json';const version = pkg.version;~" $src1
-yellow "$src2"
-sed -i "s~import { version } from '../package.json';~import pkg from '../package.json';const version = pkg.version;~" $src2
-yellow "$src3"
-sed -i "s~export { diacritics, commands } from './unicode.json';~import unicode from './unicode.json';export const diacritics = unicode.diacritics;export const commands = unicode.commands;~" $src3
-yellow "$src4"
-sed -i "s~import { props, ignoredProps } from './props';~import wikiprops from './props';const { props, ignoredProps } = wikiprops  ;~" $src4
-yellow "Done fixing import."
 
 # move svgedit to public folder
 yellow "Adding symbolic link to svg editor in public folder"
