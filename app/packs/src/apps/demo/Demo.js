aa16bce79b5ef77c53e9b9ea5a6db8382484531c
mekkyz <mos.mekky@gmail.com>
2023-09-04 19:46:55 +0000

    Style: cleaning Demo.js

--- app/packs/src/apps/demo/Demo.js
+++ app/packs/src/apps/demo/Demo.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Button from 'ui/Button';
+import ButtonGroup from 'ui/ButtonGroup';
+
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 class Demo extends Component {
   constructor(props) {
@@ -7,31 +10,31 @@ class Demo extends Component {
     this.state = {
       buttons: [
         {
-          variant: 'primary', option: 'main', square: false, title: 'Primary Main', withIcon: false
+          variant: 'primary', size: 'large', square: false, title: 'Primary Large', withIcon: false
         },
         {
-          variant: 'primary', option: 'main', square: true, title: 'Primary Main Square', withIcon: false
+          variant: 'primary', size: 'large', square: true, title: 'Primary Large Square', withIcon: false
         },
         {
-          variant: 'primary', option: 'main', square: false, title: 'Primary Main with Icon', withIcon: true
+          variant: 'primary', size: 'large', square: false, title: 'Primary Large with Icon', withIcon: true
         },
         {
-          variant: 'secondary', option: 'main', square: false, title: 'Secondary Main', withIcon: false
+          variant: 'secondary', size: 'large', square: false, title: 'Secondary Large', withIcon: false
         },
         {
-          variant: 'secondary', option: 'main', square: true, title: 'Secondary Main Square', withIcon: false
+          variant: 'secondary', size: 'large', square: true, title: 'Secondary Large Square', withIcon: false
         },
         {
-          variant: 'secondary', option: 'main', square: false, title: 'Secondary Main with Icon', withIcon: true
+          variant: 'secondary', size: 'large', square: false, title: 'Secondary Large with Icon', withIcon: true
         },
         {
-          variant: 'tertiary', option: 'main', square: false, title: 'Tertiary Main', withIcon: false
+          variant: 'tertiary', size: 'large', square: false, title: 'Tertiary Large', withIcon: false
         },
         {
-          variant: 'tertiary', option: 'main', square: true, title: 'Tertiary Main Square', withIcon: false
+          variant: 'tertiary', size: 'large', square: true, title: 'Tertiary Large Square', withIcon: false
         },
         {
-          variant: 'tertiary', option: 'main', square: false, title: 'Tertiary Main with Icon', withIcon: true
+          variant: 'tertiary', size: 'large', square: false, title: 'Tertiary Large with Icon', withIcon: true
         },
       ]
     };
@@ -52,102 +55,22 @@ class Demo extends Component {
     return 'Button';
   }
 
-  renderButton(btn, index) {
-    return (
-      <div
-        key={index}
-        style={{
-          position: 'relative',
-          display: 'flex',
-          alignItems: 'center',
-          marginBottom: '10px',
-          justifyContent: 'space-between'
-        }}
-      >
-        <h3
-          style={{
-            fontSize: '16px',
-            color: '#777',
-            right: '0',
-            display: 'flex',
-            justifyContent: 'flex-start',
-            alignItems: 'center'
-          }}
-        >
-          {btn.title}
-        </h3>
-
-        <div
-          style={{
-            position: 'absolute',
-            left: '50%',
-            transform: 'translateX(-50%)',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-        >
-          <Button variant={btn.variant} option={btn.option} square={btn.square} style={{ margin: '0 20px' }}>
-            {this.renderButtonContent(btn)}
-          </Button>
-        </div>
-
-        <pre
-          style={{
-            background: '#e9e9e9',
-            padding: '12px',
-            borderRadius: '4px',
-            fontSize: '14px',
-            left: '0',
-            display: 'flex',
-            justifyContent: 'flex-start'
-          }}
-        >
-          {`<Button variant="${btn.variant}" option="${btn.option}" square={${btn.square}}>${this.renderButtonContent(btn)}</Button>`}
-        </pre>
-      </div>
-    );
-  }
-
-
   render() {
     return (
-      <div
-        style={{
-          padding: '50px',
-          backgroundColor: '#f9f9f9',
-          minHeight: '100vh',
-          fontFamily: 'Arial, sans-serif'
+      <div style={{ padding: '50px' }}>
+        <h1 style={{
+          textAlign: 'center', marginBottom: '40px', fontSize: '32px', color: '#333', fontWeight: 'bold'
         }}
-      >
-        <h1
-          style={{
-            textAlign: 'center',
-            marginBottom: '40px',
-            fontSize: '32px',
-            color: '#333',
-            fontWeight: 'bold'
-          }}
         >
           Component Demo
         </h1>
-        <h2
-          style={{
-            textAlign: 'center',
-            marginBottom: '40px',
-            fontSize: '28px',
-            color: '#555'
-          }}
+        <h2 style={{
+          textAlign: 'center', marginBottom: '40px', fontSize: '28px', color: '#555'
+        }}
         >
           Buttons
         </h2>
-        <div
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center'
-          }}
-        >
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
           {this.state.buttons.map((btn, index) => (
             <div
               key={index}
@@ -160,23 +83,11 @@ class Demo extends Component {
                 textAlign: 'center'
               }}
             >
-              <h3
-                style={{
-                  fontSize: '16px',
-                  color: '#777',
-                  marginBottom: '10px'
-                }}
-              >
-                {btn.title}
-              </h3>
-              <div
-                style={{
-                  position: 'relative'
-                }}
-              >
+              <h3 style={{ fontSize: '16px', color: '#777', marginBottom: '10px' }}>{btn.title}</h3>
+              <div>
                 <Button
                   variant={btn.variant}
-                  option={btn.option}
+                  size="xLarge"
                   square={btn.square}
                   style={{
                     margin: '0 20px'
@@ -197,10 +108,30 @@ class Demo extends Component {
                   marginTop: '10px'
                 }}
               >
-                {`<Button variant="${btn.variant}" option="${btn.option}" square={${btn.square}}>${this.renderButtonContent(btn)}</Button>`}
+                {`<Button variant="${btn.variant}"`
+                + ` size="${btn.size}" square={${btn.square}}>${this.renderButtonContent(btn)}</Button>`}
               </pre>
             </div>
           ))}
+        </div>
+        <div>
+          <ButtonGroup>
+            <Button
+              variant="secondary"
+              size="xLarge"
+            >
+              Button1
+            </Button>
+            <Button
+              variant="primary"
+              size="xLarge"
+              square
+              tooltip="Hi!"
+              tooltipPosition="top"
+            >
+              Button2
+            </Button>
+          </ButtonGroup>
         </div>
       </div>
     );
@@ -208,4 +139,3 @@ class Demo extends Component {
 }
 
 export default Demo;
-
