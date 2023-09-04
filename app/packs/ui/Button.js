e4eeb7f03f1c202da84bd583f5beb7cdf0775cbe
mekkyz <mos.mekky@gmail.com>
2023-09-04 19:46:30 +0000

    Improve css styles and introducing own tooltips

--- app/packs/ui/Button.js
+++ app/packs/ui/Button.js
@@ -3,45 +3,54 @@ import PropTypes from 'prop-types';
 
 function Button(props) {
   const {
-    variant, children, option, square, type, onClick, ...otherProps
+    variant, children, size, square, type, onClick, tooltip, tooltipPosition, ...otherProps
   } = props;
 
   const btnClassNames = [
     'btn-common',
     `btn-${variant}`,
-    `btn-${option}`,
+    `btn-${size}`,
     square ? 'btn-square' : ''
   ].join(' ');
 
   return (
-    <button
+    <div className="btn-wrapper">
+
+      <button
       // eslint-disable-next-line react/button-has-type
-      type={type}
-      className={btnClassNames}
-      onClick={onClick}
-      {...otherProps}
-    >
-      {children}
-    </button>
+        type={type}
+        className={btnClassNames}
+        onClick={onClick}
+        {...otherProps}
+      >
+        {children}
+      </button>
+      {tooltip && <span className={`tooltip tooltip-${tooltipPosition}`}>{tooltip}</span>}
+    </div>
+
   );
 }
 
 Button.propTypes = {
   variant: PropTypes.oneOf(['primary', 'secondary', 'tertiary']),
-  option: PropTypes.oneOf(['main', 'panel', 'modal', 'popover']),
+  size: PropTypes.oneOf(['xLarge', 'large', 'medium', 'small']),
   children: PropTypes.node,
   onClick: PropTypes.func,
   square: PropTypes.bool, // this will make the width=height for buttons that only have icons
   type: PropTypes.oneOf(['submit', 'reset', 'button']),
+  tooltip: PropTypes.string,
+  tooltipPosition: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
 };
 
 Button.defaultProps = {
   variant: 'primary',
-  option: 'main',
+  size: 'medium',
   onClick: () => {},
   children: null,
   square: false,
   type: 'button',
+  tooltip: null,
+  tooltipPosition: 'top',
 };
 
 export default Button;
