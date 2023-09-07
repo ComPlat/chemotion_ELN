518890c08be987bc496a2245a49ee963edce244f
nh9378 <basha.adam@kit.edu>
2023-09-07 10:22:53 +0000

    disable sdf format option for chemicals export and adjust exportModal height

--- app/packs/src/components/contextActions/ModalExport.js
+++ app/packs/src/components/contextActions/ModalExport.js
@@ -213,6 +213,8 @@ export default class ModalExport extends React.Component {
 
   buttonBar() {
     const { onHide } = this.props;
+    const chemicalColumns = this.filteredColumns();
+    const sdfChemicalExport = chemicalColumns.chemicals.length !== 0;
     return (
       <ButtonToolbar>
         <div className="pull-right">
@@ -221,7 +223,7 @@ export default class ModalExport extends React.Component {
             <DropdownButton dropup bsStyle="warning" id="md-export-dropdown"
               title="XLSX/SD Export" onSelect={this.handleClick}>
               <MenuItem eventKey="1">XLSX Export</MenuItem>
-              <MenuItem eventKey="2">SDF Export</MenuItem>
+              <MenuItem eventKey="2" disabled={sdfChemicalExport}>SDF Export</MenuItem>
             </DropdownButton>
           </ButtonToolbar>
         </div>
