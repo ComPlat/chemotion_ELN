8477f4e4728cbce830fd825c04c9f58eee1fc320
mekkyz <mos.mekky@gmail.com>
2023-09-11 09:18:25 +0000

    fix: disabled lifecycle update for testing

--- spec/javascripts/packs/src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails.spec.js
+++ spec/javascripts/packs/src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails.spec.js
@@ -1,62 +1,66 @@
-import React from "react";
-import expect from "expect";
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+/* global describe, context, it */
 
-import ResearchPlanFactory from "factories/ResearchPlanFactory";
-import AttachmentFactory from "factories/AttachmentFactory";
-import uuid from "uuid";
+import React from 'react';
+import expect from 'expect';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import ResearchPlanFactory from 'factories/ResearchPlanFactory';
+import AttachmentFactory from 'factories/AttachmentFactory';
+import uuid from 'uuid';
 // although not used import of ElementStore needed to initialize
 // the ResearchPlanDetails.js component. It must be executed before
 // the import of ResearchPlanDetails. Beware of the linter which
 // may put it at the end of the imports !!!
 
 // eslint-disable-next-line no-unused-vars
-import ElementStore from "src/stores/alt/stores/ElementStore";
+import ElementStore from 'src/stores/alt/stores/ElementStore';
 
-import ResearchPlanDetails from "src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails";
+import ResearchPlanDetails from 'src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails';
 
 Enzyme.configure({
   adapter: new Adapter(),
 });
 
-describe("ResearchPlanDetails", async () => {
-  const FIELD_ID_IMAGE = "entry-001";
-  const FIELD_ID_NO_IMAGE = "entry-002";
-  describe(".handleBodyChange", async () => {
-    context("on non existing field", async () => {
-      it(" expecting nothing was changed", async () => {
+describe('ResearchPlanDetails', async () => {
+  const FIELD_ID_IMAGE = 'entry-001';
+  const FIELD_ID_NO_IMAGE = 'entry-002';
+  describe('.handleBodyChange', async () => {
+    context('on non existing field', async () => {
+      it(' expecting nothing was changed', async () => {
         const researchPlanWithImage = await ResearchPlanFactory.build(
-          "with_image_body_field"
+          'with_image_body_field'
         );
         const wrapper = shallow(
           <ResearchPlanDetails
             researchPlan={researchPlanWithImage}
             toggleFullScreen={() => {}}
-          />
+          />,
+          { disableLifecycleMethods: true }
         );
-        wrapper.instance().handleBodyChange({}, "nonExistingFieldId", []);
+        wrapper.instance().handleBodyChange({}, 'nonExistingFieldId', []);
 
         expect(researchPlanWithImage.changed).toEqual(false);
       });
     });
 
-    context("on non image field", async () => {
-      it(" expected to be changed", async () => {
+    context('on non image field', async () => {
+      it(' expected to be changed', async () => {
         const researchPlanWithoutImage = await ResearchPlanFactory.build(
-          "with_not_image_body_field"
+          'with_not_image_body_field'
         );
 
         const wrapper = shallow(
           <ResearchPlanDetails
             researchPlan={researchPlanWithoutImage}
             toggleFullScreen={() => {}}
-          />
+          />,
+          { disableLifecycleMethods: true }
         );
 
         wrapper.instance().handleBodyChange(
           {
-            test: "fakeValue",
+            test: 'fakeValue',
           },
           FIELD_ID_NO_IMAGE,
           []
@@ -67,28 +71,28 @@ describe("ResearchPlanDetails", async () => {
         expect(researchPlanWithoutImage.body).toEqual([
           {
             id: FIELD_ID_NO_IMAGE,
-            type: "no-image",
+            type: 'no-image',
             value: {
-              test: "fakeValue",
+              test: 'fakeValue',
             },
           },
         ]);
       });
     });
-    context("replacing an image field for the first time", async () => {
-      it("expecting to be replaced", async () => {
+    context('replacing an image field for the first time', async () => {
+      it('expecting to be replaced', async () => {
         const researchPlanWithImage = await ResearchPlanFactory.build(
-          "with_image_body_field"
+          'with_image_body_field'
         );
-        const newImageAttachment = await AttachmentFactory.build("new", {
+        const newImageAttachment = await AttachmentFactory.build('new', {
           id: uuid.v1(),
         });
 
         const expectedField = {
           id: FIELD_ID_IMAGE,
-          type: "image",
+          type: 'image',
           value: {
-            file_name: "abc.png",
+            file_name: 'abc.png',
             public_name: newImageAttachment.identifier,
           },
         };
@@ -97,7 +101,8 @@ describe("ResearchPlanDetails", async () => {
           <ResearchPlanDetails
             researchPlan={researchPlanWithImage}
             toggleFullScreen={() => {}}
-          />
+          />,
+          { disableLifecycleMethods: true }
         );
 
         wrapper
@@ -115,25 +120,25 @@ describe("ResearchPlanDetails", async () => {
       });
     });
     context(
-      "replacing an image field for the second time - replacing an temporary image",
+      'replacing an image field for the second time - replacing an temporary image',
       async () => {
-        it("expecting to be replaced with old value in memory", async () => {
-          const attachmentToAdd = await AttachmentFactory.build("new", {
+        it('expecting to be replaced with old value in memory', async () => {
+          const attachmentToAdd = await AttachmentFactory.build('new', {
             id: uuid.v1(),
           });
           const researchPlanWithImage = await ResearchPlanFactory.build(
-            "with_image_body_field"
+            'with_image_body_field'
           );
 
           const newValue = {
-            file_name: "abc.png",
+            file_name: 'abc.png',
             public_name: attachmentToAdd.identifier,
             old_value: researchPlanWithImage.attachments[0].identifier,
           };
 
           const expectedField = {
             id: FIELD_ID_IMAGE,
-            type: "image",
+            type: 'image',
             value: newValue,
           };
 
@@ -141,7 +146,8 @@ describe("ResearchPlanDetails", async () => {
             <ResearchPlanDetails
               researchPlan={researchPlanWithImage}
               toggleFullScreen={() => {}}
-            />
+            />,
+            { disableLifecycleMethods: true }
           );
 
           wrapper
@@ -155,28 +161,25 @@ describe("ResearchPlanDetails", async () => {
       }
     );
     context(
-      "replacing an image field for the second time - replacing an already persisted image",
+      'replacing an image field for the second time - replacing an temporary image',
       async () => {
-        it("expecting to be replaced with old value in memory", async () => {
-          const attachmentToAdd = await AttachmentFactory.build("new", {
+        it('expecting to be replaced with old value in memory', async () => {
+          const attachmentToAdd = await AttachmentFactory.build('new', {
             id: uuid.v1(),
           });
           const researchPlanWithImage = await ResearchPlanFactory.build(
-            "with_image_body_field"
+            'with_image_body_field'
           );
-          researchPlanWithImage.attachments[0].file = [];
-          researchPlanWithImage.attachments[0].file.preview =
-            researchPlanWithImage.attachments[0].identifier;
-          researchPlanWithImage.attachments[0].identifier = "none";
 
           const newValue = {
-            file_name: "abc.png",
+            file_name: 'abc.png',
             public_name: attachmentToAdd.identifier,
-            old_value: researchPlanWithImage.attachments[0].file.preview,
+            old_value: researchPlanWithImage.attachments[0].identifier,
           };
+
           const expectedField = {
             id: FIELD_ID_IMAGE,
-            type: "image",
+            type: 'image',
             value: newValue,
           };
 
@@ -184,7 +187,8 @@ describe("ResearchPlanDetails", async () => {
             <ResearchPlanDetails
               researchPlan={researchPlanWithImage}
               toggleFullScreen={() => {}}
-            />
+            />,
+            { disableLifecycleMethods: true }
           );
 
           wrapper
