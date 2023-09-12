ac079d3a0d4e9165f0f7e396ad76b0fbea82026c
Devang Patel <67055123+endevor-music@users.noreply.github.com>
2023-09-12 15:03:51 +0200

    test: add fetcher spec scaffold

---
+++ spec/javascripts/packs/src/fetchers/VesselsFetcher.spec.js
@@ -0,0 +1,46 @@
+import expect from "expect";
+import Vessel from "../../../../../app/packs/src/models/Vessel"
+import VesselsFetcher from "../../../../../app/packs/src/fetchers/VesselsFetcher"
+
+describe('VesselsFetcher', () => {
+  describe('.fetchByCollectionId()', () => {
+    it('when no vessels in collection', async () => {
+
+    });  
+
+    it('when 2 vessels are in collection', async () => {
+
+    });
+});
+  describe('.fetchById()', () => {
+      it('when no vessel matches the id', async () => {
+
+      });  
+
+      it('when vessel matches the id', async () => {
+
+      });
+  });
+  describe('.update()', () => {
+      it('when params are not valid', async () => {
+
+      });  
+
+      it('when params are not valid', async () => {
+          // const params = {
+
+          // };
+        // const vessel = Vessel.buildEmpty(0, "shortLabel");
+        // VesselsFetcher.create(vessel);
+      });
+  });
+  describe('.create()', () => {
+      it('when params are not valid', async () => {
+
+      });  
+
+      it('when params are not valid', async () => {
+
+      });
+  });
+})
