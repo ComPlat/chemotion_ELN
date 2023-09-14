31f847bd22f263c3303ab85d88cc7381c55609d4
FabianMauz <fmauz@ipb-halle.de>
2023-09-14 14:14:45 +0000

    feat: handle moving into collection where cell line already in

--- app/models/collections_cellline.rb
+++ app/models/collections_cellline.rb
@@ -11,34 +11,46 @@ class CollectionsCellline < ApplicationRecord
   # Remove from collection and process associated elements (and update collection info tag)
   def self.remove_in_collection(cellline_ids, collection_ids)
     CollectionsCellline.find_by(
-      collection_id:collection_ids,
-      cellline_sample_id:cellline_ids,
-      deleted_at: nil
+      collection_id: collection_ids,
+      cellline_sample_id: cellline_ids,
+      deleted_at: nil,
     ).destroy
   end
 
-  def self.move_to_collection(cellline_ids, from_col_ids, to_col_ids)
-    Array(cellline_ids).each{ |cell_line_id|
-     
-      CollectionsCellline.new(
-        cellline_sample_id:cell_line_id,
-        collection_id: to_col_ids,
-      ).save;
-      
+  def self.move_to_collection(cellline_ids, from_col_ids, to_col_id)
+    Array(cellline_ids).each do |cell_line_id|
+      CollectionsCellline.save_to_collection(cell_line_id, to_col_id)
+      CollectionsCellline.delete_in_collection(cell_line_id, from_col_ids)
+    end
 
-      old_entry = CollectionsCellline.find_by(
-        cellline_sample_id:cell_line_id,
-        collection_id: from_col_ids,
-        deleted_at: nil
-      )
-      old_entry.destroy if old_entry
-    }
-    
     CollectionsCellline.update_collection_tag(cellline_ids)
   end
 
+  def self.delete_in_collection(cell_line_id, coll_id)
+    old_entry = CollectionsCellline.find_by(
+      cellline_sample_id: cell_line_id,
+      collection_id: coll_id,
+      deleted_at: nil,
+    )
+    old_entry&.destroy
+  end
+
+  def self.save_to_collection(cell_line_id, to_col_id)
+    entry_already_there = CollectionsCellline.find_by(
+      cellline_sample_id: cell_line_id,
+      collection_id: to_col_id,
+    )
+
+    return if entry_already_there
+
+    CollectionsCellline.new(
+      cellline_sample_id: cell_line_id,
+      collection_id: to_col_id,
+    ).save
+  end
+
   def self.update_collection_tag(element_ids)
-     CelllineSample.includes(:tag).where(id: element_ids).select(:id)
-           .each { |el| el.update_tag!(collection_tag: true) }
+    CelllineSample.includes(:tag).where(id: element_ids).select(:id)
+                  .each { |el| el.update_tag!(collection_tag: true) }
   end
 end
--- spec/models/collections_cellline_spec.rb
+++ spec/models/collections_cellline_spec.rb
@@ -5,20 +5,19 @@ require 'rails_helper'
 RSpec.describe CollectionsCellline do
   let(:c1) { create(:collection) }
   let(:c2) { create(:collection) }
+  let(:collection_src_id) { c1.id }
+  let(:collection_target_id) { c2.id }
   let(:sample) { create(:cellline_sample, collections: [c1]) }
 
+  subject!(:execute) do
+    described_class.move_to_collection(
+      sample.id,
+      collection_src_id,
+      collection_target_id,
+    )
+  end
+
   context 'when target collection is valid and has not yet the cell line' do
-    subject!(:execute) do
-      described_class.move_to_collection(
-        sample.id,
-        collection_src_id,
-        collection_target_id,
-      )
-    end
-
-    let(:collection_src_id) { c1.id }
-    let(:collection_target_id) { c2.id }
-
     it 'cell line is now connected to new collection' do
       expect(described_class.find_by(
                collection_id: collection_target_id,
@@ -38,7 +37,22 @@ RSpec.describe CollectionsCellline do
     xit 'not yet implemented' do end
   end
 
-  context 'when target collection is valid and has not yet the cell line' do
-    xit 'not yet implemented' do end
+  context 'when target collection is valid and but cell line is already in it' do
+    let(:c2) { create(:collection) }
+    let(:sample) { create(:cellline_sample, collections: [c1,c2]) }
+
+    it 'cell line exists once in the target collection' do 
+      expect(described_class.where(
+        collection_id: collection_target_id,
+        cellline_sample_id: sample.id,
+      ).count).to be 1
+    end
+
+    it 'cell line was removed from the old collection' do 
+      expect(described_class.find_by(
+        collection_id: collection_src_id,
+        cellline_sample_id: sample.id,
+      )).to be_nil
+    end
   end
 end
