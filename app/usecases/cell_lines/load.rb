63825dab82c1b19e7c23ade880120bb05d2cb04f
FabianMauz <fmauz@ipb-halle.de>
2023-09-25 14:28:48 +0000

    feat: add access to sync/shared collection when loading a cell line

--- app/usecases/cell_lines/load.rb
+++ app/usecases/cell_lines/load.rb
@@ -9,16 +9,23 @@ module Usecases
       end
 
       def execute!
-        raise 'user is not valid' unless @current_user&.cellline_samples
+        raise 'user is not valid' unless @current_user
         raise 'id not valid' unless @id.is_a?(Numeric) && @id.positive?
 
-        begin
-          cell_line_sample = @current_user.cellline_samples.find(@id)
-        rescue StandardError
-          raise 'user has no access to object'
-        end
+        cell_line_sample = @current_user.cellline_samples.find_by(id: @id)
+
+        cell_line_sample = fetch_cell_lines_from_shared_sync if cell_line_sample.nil?
+
+        raise 'user has no access to object' unless cell_line_sample
 
         cell_line_sample
+      end
+
+      def fetch_cell_lines_from_shared_sync
+        shared_or_synced_celllines = @current_user.shared_collections.map do |col|
+          col.cellline_samples.find_by(id: @id)
+        end
+        shared_or_synced_celllines.first 
       end
     end
   end
