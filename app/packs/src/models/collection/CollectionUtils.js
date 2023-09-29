04802c3ab784701fb9aab015bc989fcc145632c2
FabianMauz <fmauz@ipb-halle.de>
2023-09-29 09:41:11 +0000

    refactor: refactored checking of read only at collections

--- app/packs/src/models/collection/CollectionUtils.js
+++ app/packs/src/models/collection/CollectionUtils.js
@@ -1,9 +1,13 @@
-
 export default class CollectionUtils {
-    static isReadOnly(currentCollection,userId,isSync) {
+  static isReadOnly(currentCollection, userId, isSync) {
+    if (currentCollection.permission_level > 0) { return false; }
 
-        let isShared=currentCollection.is_shared &&
-            currentCollection.shared_by_id!=userId;
-        return currentCollection.permission_level === 0 && (isSync || isShared);
-    }
+    const isShared = currentCollection.is_shared
+            && currentCollection.shared_by_id !== userId;
+
+    const isSynced = isSync
+            && currentCollection.shared_by_id !== userId;
+
+    return isSynced || isShared;
+  }
 }
