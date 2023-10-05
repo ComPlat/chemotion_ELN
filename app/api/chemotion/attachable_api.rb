a46027a52222db4f3ff0a4091945e884bfded1bf
FabianMauz <fmauz@ipb-halle.de>
2023-10-05 09:43:40 +0000

    refac: use loop with index instead of own index variable

--- app/api/chemotion/attachable_api.rb
+++ app/api/chemotion/attachable_api.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Rails/SkipsModelValidations
+
 module Chemotion
   class AttachableAPI < Grape::API
     resource :attachable do
@@ -13,7 +15,10 @@ module Chemotion
       after_validation do
         case params[:attachable_type]
         when 'ResearchPlan'
-          error!('401 Unauthorized', 401) unless ElementPolicy.new(current_user, ResearchPlan.find_by(id: params[:attachable_id])).update?
+          error!('401 Unauthorized', 401) unless ElementPolicy.new(
+            current_user,
+            ResearchPlan.find_by(id: params[:attachable_id]),
+          ).update?
         end
       end
 
@@ -21,13 +26,13 @@ module Chemotion
       post 'update_attachments_attachable' do
         attachable_type = params[:attachable_type]
         attachable_id = params[:attachable_id]
-       
+
         if params.fetch(:files, []).any?
           attach_ary = []
           rp_attach_ary = []
-          index=0;
-          params[:files].each do |file|
+          params[:files].each_with_index do |file, index|
             next unless (tempfile = file[:tempfile])
+
             a = Attachment.new(
               identifier: params[:attfilesIdentifier][index],
               bucket: file[:container_id],
@@ -37,9 +42,9 @@ module Chemotion
               created_for: current_user.id,
               content_type: file[:type],
               attachable_type: attachable_type,
-              attachable_id: attachable_id
+              attachable_id: attachable_id,
             )
-            index=index +1
+
             begin
               a.save!
               attach_ary.push(a.id)
@@ -50,9 +55,13 @@ module Chemotion
             end
           end
         end
-        Attachment.where('id IN (?) AND attachable_type = (?)', params[:del_files].map!(&:to_i), attachable_type).update_all(attachable_id: nil) if params[:del_files].any?
+        if params[:del_files].any?
+          Attachment.where('id IN (?) AND attachable_type = (?)', params[:del_files].map!(&:to_i),
+                           attachable_type).update_all(attachable_id: nil)
+        end
         true
       end
     end
   end
 end
+# rubocop:enable Rails/SkipsModelValidations
