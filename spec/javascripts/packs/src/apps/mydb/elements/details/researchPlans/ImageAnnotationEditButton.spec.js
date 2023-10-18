94731ba788adaae493f29c207e5d08cfd8171be2
Matthias Döring <md@megorei.com>
2023-10-18 15:07:22 +0200

    Fix spec for ImageAnnotationEditButton

--- spec/javascripts/packs/src/apps/mydb/elements/details/researchPlans/ImageAnnotationEditButton.spec.js
+++ spec/javascripts/packs/src/apps/mydb/elements/details/researchPlans/ImageAnnotationEditButton.spec.js
@@ -12,12 +12,12 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('ImageAnnotationEditButton', () => {
   const pngAttachment = new Attachment({ filename: 'example.png' });
   const gifAttachment = new Attachment({ filename: 'example.gif' });
-  const parent = {};
+  const onSelectAttachment = () => {};
 
   describe('.render()', () => {
     context('with not persisted attachment(png)', () => {
       pngAttachment.isNew = true;
-      const wrapper = shallow(<ImageAnnotationEditButton attachment={pngAttachment} parent={parent} />);
+      const wrapper = shallow(<ImageAnnotationEditButton attachment={pngAttachment} onSelectAttachment={onSelectAttachment} />);
 
       it('button is rendered but disabled', () => {
         expect(wrapper.html())
@@ -28,7 +28,7 @@ describe('ImageAnnotationEditButton', () => {
 
     context('with persisted attachment(png)', () => {
       pngAttachment.isNew = false;
-      const wrapper = shallow(<ImageAnnotationEditButton attachment={pngAttachment} parent={parent} />);
+      const wrapper = shallow(<ImageAnnotationEditButton attachment={pngAttachment} onSelectAttachment={onSelectAttachment} />);
 
       it('button is rendered and not disabled', () => {
         expect(wrapper.html())
@@ -38,7 +38,7 @@ describe('ImageAnnotationEditButton', () => {
     });
 
     context('with no attachment', () => {
-      const wrapper = shallow(<ImageAnnotationEditButton attachment={null} parent={parent} />);
+      const wrapper = shallow(<ImageAnnotationEditButton attachment={null} onSelectAttachment={onSelectAttachment} />);
 
       it('button is not rendered', () => {
         expect(wrapper.html()).toEqual(null);
@@ -46,7 +46,7 @@ describe('ImageAnnotationEditButton', () => {
     });
 
     context('with not supported image type(gif)', () => {
-      const wrapper = shallow(<ImageAnnotationEditButton attachment={gifAttachment} parent={parent} />);
+      const wrapper = shallow(<ImageAnnotationEditButton attachment={gifAttachment} onSelectAttachment={onSelectAttachment} />);
 
       it('button is not rendered', () => {
         expect(wrapper.html()).toEqual(null);
