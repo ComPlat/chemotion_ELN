b97606e05dbf86e53f4b089b08b239114ac00bac
FabianMauz <fmauz@ipb-halle.de>
2023-10-18 12:59:20 +0000

    fix: fixed switch case for cell lines, seems to be overwritten by main sync

--- app/api/chemotion/literature_api.rb
+++ app/api/chemotion/literature_api.rb
@@ -19,9 +19,18 @@ module Chemotion
     resource :literatures do
       after_validation do
         unless %r{doi/metadata|ui_state|collection}.match?(request.url)
-          @element_klass = params[:element_type].classify
-          @element = @element_klass.constantize.find_by(id: params[:element_id])
-          @element_policy = ElementPolicy.new(current_user, @element)
+          if params[:element_type] == "cell_line" then
+            cell_line_sample = CelllineSample.find(params[:element_id])
+            @element = CelllineMaterial.find(cell_line_sample.cellline_material_id)
+            @element_policy = ElementPolicy.new(current_user, cell_line_sample)
+            @element_klass="CelllineMaterial"
+            params[:element_id] = @element.id
+          else
+            @element_klass = params[:element_type].classify
+            @element = @element_klass.constantize.find_by(id: params[:element_id])
+            @element_policy = ElementPolicy.new(current_user, @element)
+          end
+          
           allowed = if /get/i.match?(request.env['REQUEST_METHOD'])
                       @element_policy.read?
                     else
