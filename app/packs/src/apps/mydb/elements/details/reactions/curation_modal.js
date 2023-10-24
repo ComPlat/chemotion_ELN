362bdd70011c07d72211bb636e875c779a0361bc
dean.harvey <dean.harvey@kit.edu>
2023-10-24 13:39:35 +0200

    adding state to curation_modal

--- app/packs/src/apps/mydb/elements/details/reactions/curation_modal.js
+++ app/packs/src/apps/mydb/elements/details/reactions/curation_modal.js
@@ -2,21 +2,49 @@ import React, { Component } from 'react';
 import { Button, ButtonToolbar, FormControl, Glyphicon, Modal, Table, Popover,Tooltip,OverlayTrigger} from 'react-bootstrap';
 
 export default class Curation_modal extends Component {
+   
     constructor(props) {
       super(props);
       this.handleShow = this.handleShow.bind(this);
       this.handleClose = this.handleClose.bind(this);
       this.state = {
-        show: false
+        show: false,
+        cleanData : this.clean_data(props.description),
+        typo: "  " 
       };
     }
-
     handleClose() {
       this.setState({ show: false });
     }
   
     handleShow() {
       this.setState({ show: true });
+    }
+
+
+    highlight(desc_text, typo){ 
+    
+
+    // if (typo_array.length > 0 ){
+    //   console.log(typo_array)
+    // desc_text = {desc_text.split(typo)[0]} <b style={{ backgroundColor: '#e8bb49' }}> {typo} </b> {desc_text.split(typo)[1]}
+    //   used_typo.push(typo_array[count_var])
+    //   typo_array = this.spell_check(desc_text)
+    //   typo_array.shift()
+      // used_typo.forEach((used_typo_pair) => {
+      //   // console.log(used_typo_pair)
+      //   console.log("array " + typo_array[count_var] +" used " + used_typo_pair )
+      //   if (typo_array[count_var] === used_typo_pair){
+      //     typo_array = typo_array.splice(count_var,1)
+      //     console.log("splice done " + count_var)
+      //   } 
+      //   else console.log("no splice "+ count_var)
+      // })
+    //   count_var = count_var +1
+      
+    //   };
+      // console.log(typo_array)
+    return ;
     }
 
     clean_data(description){
@@ -36,15 +64,37 @@ export default class Curation_modal extends Component {
         let typo_index_array = [];
         for (const match of matches) {
           typo_index_array.push([match.index,match.index + match[0].length])
+          
         // console.log(
         // `Found ${match[0]} start=${match.index} end=${
         //  match.index + match[0].length}.`,)
         }
+        // console.log(typo_index_array)
          return (typo_index_array);
-      }
-    spell_check_underline(input){
-      
     }
+
+    underline_typo(description){
+      let typo_index_array = this.spell_check(description)
+      let length = typo_index_array.length
+      let count = 0
+      let new_text =""
+      new_text = this.highlight(description,"  ")
+        
+      
+      console.log(new_text)
+      return new_text
+
+    }
+    
+    Highlighted_text(props){
+    let clean_desc = clean_data(props.description)
+    let typo = "  "
+    let hl_desc = this.highlight(props.description, typo)
+    return (
+       <b style={{ backgroundColor: '#e8bb49' }}> {typo} </b> 
+    )
+
+  }
 
     render() {
       return (
@@ -66,10 +116,10 @@ export default class Curation_modal extends Component {
                 
                 </div>
                 <div>
-                {this.spell_check(this.clean_data(this.props.description))}
+                <this.Highlighted_text/>
                     suggestion
                 </div>
-                <div class="row">
+                <div className="row">
                     <Button>Change</Button>
                     <Button>Skip</Button>
                 </div>
