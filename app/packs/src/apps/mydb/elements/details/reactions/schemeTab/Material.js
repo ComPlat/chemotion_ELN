ComPlat/chemotion_ELN

commit 40bc206fb0aeff7c6aad6bdec75259af53fad571

fix: yield percentage error for reactions with decoupled products and disable error message for decoupled samples
Refs: #1531

app/packs/src/apps/mydb/elements/details/reactions/schemeTab/Material.js
@@ -212,31 +212,29 @@ class Material extends Component {
   }
 
   equivalentOrYield(material) {
-    if (this.props.materialGroup === 'products') {
-      if (this.props.reaction.hasPolymers()) {
-        return (
+    const { reaction, materialGroup } = this.props;
+    if (materialGroup === 'products') {
+      const refMaterial = reaction.getReferenceMaterial();
+      let calculateYield = material.equivalent;
+      if (reaction.hasPolymers()) {
+        calculateYield = `${((material.equivalent || 0) * 100).toFixed(0)}%`;
+      } else if (refMaterial.decoupled || material.decoupled) {
+        calculateYield = 'n.a.';
+      } else {
+        calculateYield = `${((material.equivalent <= 1 ? material.equivalent || 0 : 1) * 100).toFixed(0)}%`;
+      }
+      return (
+        <div>
           <FormControl
+            name="yield"
             type="text"
             bsClass="bs-form--compact form-control"
             bsSize="small"
-            value={`${((material.equivalent || 0) * 100).toFixed(0)}%`}
+            value={calculateYield}
             disabled
           />
-        );
-      } else {
-        return (         
-            <div>
-              <FormControl
-                name='yield'
-                type="text"
-                bsClass="bs-form--compact form-control"
-                bsSize="small"
-                value={`${((material.equivalent <= 1 ? material.equivalent || 0 : 1) * 100).toFixed(0)}%`}
-                disabled
-              />
-            </div>
-        );
-      }
+        </div>
+      );
     }
     return (
       <NumeralInputWithUnitsCompo

app/packs/src/apps/mydb/elements/details/reactions/schemeTab/ReactionDetailsScheme.js
@@ -492,7 +492,7 @@ export default class ReactionDetailsScheme extends Component {
 
     updatedS.maxAmount = mFull;
 
-    if (errorMsg) {
+    if (errorMsg && !updatedS.decoupled) {
       updatedS.error_mass = true;
       NotificationActions.add({
         message: errorMsg,
@@ -526,6 +526,18 @@ export default class ReactionDetailsScheme extends Component {
     const newLoading = (newAmountMol / updatedS.amount_g) * 1000.0;
 
     updatedS.residues[0].custom_info.loading = newLoading;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  triggerNotification(isDecoupled) {
+    if (!isDecoupled) {
+      const errorMsg = 'Experimental mass value is larger than possible\n'
+      + 'by 100% conversion! Please check your data.';
+      NotificationActions.add({
+        message: errorMsg,
+        level: 'error',
+      });
+    }
   }
 
   updatedSamplesForAmountChange(samples, updatedSample, materialGroup) {
@@ -602,12 +614,7 @@ export default class ReactionDetailsScheme extends Component {
         } else if (materialGroup === 'products' && sample.amount_g > sample.maxAmount) {
           // eslint-disable-next-line no-param-reassign
           sample.equivalent = 1;
-          const errorMsg = 'Experimental mass value is larger than possible\n' +
-          'by 100% conversion! Please check your data.';
-          NotificationActions.add({
-            message: errorMsg,
-            level: 'error',
-          });
+          this.triggerNotification(sample.decoupled);
         }
       }
 
@@ -644,12 +651,7 @@ export default class ReactionDetailsScheme extends Component {
           sample.equivalent = sample.maxAmount !== 0 ? (sample.amount_g / sample.maxAmount) : 0;
           if (sample.amount_g > sample.maxAmount) {
             sample.equivalent = 1;
-            const errorMsg = 'Experimental mass value is larger than possible\n' +
-            'by 100% conversion! Please check your data.';
-            NotificationActions.add({
-              message: errorMsg,
-              level: 'error',
-            });
+            this.triggerNotification(sample.decoupled);
           }
         } else {
           // NB: sample equivalent independant of coeff

app/packs/src/apps/mydb/elements/details/samples/SampleDetails.js
@@ -84,7 +84,7 @@ const decoupleCheck = (sample) => {
     LoadingActions.stop();
     return false;
   }
-  if (sample.decoupled && sample.sum_formula.trim() === '') { sample.sum_formula = 'undefined structure'; }
+  if (sample.decoupled && sample.sum_formula?.trim() === '') { sample.sum_formula = 'undefined structure'; }
   if (!sample.decoupled) { sample.sum_formula = ''; }
   return true;
 };
@@ -299,7 +299,7 @@ export default class SampleDetails extends React.Component {
     if (!sample.decoupled) {
       sample.sum_formula = '';
     } else {
-      if (sample.sum_formula.trim() === '') sample.sum_formula = 'undefined structure';
+      if (sample.sum_formula?.trim() === '') sample.sum_formula = 'undefined structure';
       if (sample.residues && sample.residues[0] && sample.residues[0].custom_info) {
         sample.residues[0].custom_info.polymer_type = 'self_defined';
         delete sample.residues[0].custom_info.surface_type;

app/packs/src/models/Reaction.js
@@ -748,6 +748,10 @@ export default class Reaction extends Element {
     });
   }
 
+  getReferenceMaterial() {
+    return this.referenceMaterial;
+  }
+
   updateMaterial(material, refreshCoefficient) {
     const cats = ['starting_materials', 'reactants', 'solvents', 'products'];
     let i = 0;

commit 3ef1129914dfb73b32d54b1a52cbd7b65009d7e7

fix: reaction calculation when no reference material present
fix break introduced with 40bc206fb0

Ref: #1589

app/packs/src/apps/mydb/elements/details/reactions/schemeTab/Material.js
@@ -226,7 +226,7 @@ class Material extends Component {
       let calculateYield = material.equivalent;
       if (reaction.hasPolymers()) {
         calculateYield = `${((material.equivalent || 0) * 100).toFixed(0)}%`;
-      } else if (refMaterial.decoupled || material.decoupled) {
+      } else if (refMaterial && (refMaterial.decoupled || material.decoupled)) {
         calculateYield = 'n.a.';
       } else {
         calculateYield = `${((material.equivalent <= 1 ? material.equivalent || 0 : 1) * 100).toFixed(0)}%`;