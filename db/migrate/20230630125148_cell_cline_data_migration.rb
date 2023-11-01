65c183564ba0b104cb401622a0f7467cc93e3f58
FabianMauz <fmauz@ipb-halle.de>
2023-11-01 09:13:05 +0000

    fix: add cell line ui visibility property to existing profiles

--- db/migrate/20230630125148_cell_cline_data_migration.rb
+++ db/migrate/20230630125148_cell_cline_data_migration.rb
@@ -4,5 +4,10 @@ class CellClineDataMigration < ActiveRecord::Migration[6.1]
       user.counters['celllines']="0"
       user.update_column(:counters, user.counters)
     end
+
+    Profile.all.each do |profile|
+      profile.data['layout']['cell_line']=-1000
+      profile.save
+    end
   end
 end
