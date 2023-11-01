816eae18d07b08875b51f3f40ac6bbf9036f3835
FabianMauz <fmauz@ipb-halle.de>
2023-11-01 12:35:56 +0000

    test: add tests for adding literature to a cell line

--- spec/api/literature_api_spec.rb
+++ spec/api/literature_api_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # rubocop:disable RSpec/LetSetup
-# rubocop:disable RSpec/FilePath
+# rubocop:disable RSpec/FilePath,RSpec/MultipleMemoizedHelpers, RSpec/NestedGroups
 
 require 'rails_helper'
 
@@ -14,14 +14,22 @@ describe Chemotion::LiteratureAPI do
   let!(:l2) { create(:literature) }
   let!(:lt1) { create(:literal, literature: l1, element: r1, user: user) }
   let!(:lt2) { create(:literal, literature: l2, element: r1, user: user) }
+  let!(:params_element_id) { r1.id }
+  let!(:element_type) { 'reaction' }
+  let!(:is_new) { true }
+  let!(:doi) { nil }
+  let!(:url) { 'test' }
+  let!(:title) { 'testpost' }
   let!(:params) do
     {
-      element_id: r1.id,
-      element_type: 'reaction',
+      element_id: params_element_id,
+      element_type: element_type,
       ref: {
-        is_new: true,
-        title: 'testpost',
-        url: 'test',
+        is_new: is_new,
+        title: title,
+        url: url,
+        doi: doi,
+        id: l1.id,
       },
     }
   end
@@ -77,6 +85,74 @@ describe Chemotion::LiteratureAPI do
   end
 
   describe 'POST /api/v1/literatures' do
+    context 'when adding literature to a cell line' do
+      let!(:cell_line) { create(:cellline_sample, cellline_material: cellline_material, collections: [collection]) }
+      let!(:params_element_id) { cell_line.id }
+      let!(:element_type) { 'cell_line' }
+
+      context 'when literature is not in database' do
+        let!(:cellline_material) { create(:cellline_material, source: 'XYZ') }
+
+        before do
+          post '/api/v1/literatures', params: params
+        end
+
+        it 'responds 201' do
+          expect(response).to have_http_status :created
+        end
+
+        it 'literal was attached to cell line' do
+          expect(cell_line.reload.cellline_material.literatures.count).to be 1
+        end
+      end
+
+      context 'when literature is new but already in database' do
+        let!(:cellline_material) { create(:cellline_material, source: 'XYZ') }
+        let!(:literatures_count) { Literature.count }
+        let!(:url) { l1.url }
+        let!(:title) { l1.title }
+        let!(:doi) { l1.doi }
+
+        before do
+          post '/api/v1/literatures', params: params
+        end
+
+        it 'responds 201' do
+          expect(response).to have_http_status :created
+        end
+
+        it 'literature was not created again' do
+          expect(Literature.count).to be literatures_count
+        end
+
+        it 'literal again attached to cell line' do
+          expect(cell_line.reload.cellline_material.literatures.count).to eq 1
+        end
+      end
+
+      context 'when literature is not new' do
+        let!(:literatures_count) { Literature.count }
+        let!(:cellline_material) { create(:cellline_material, source: 'ABC') }
+        let!(:is_new) { false }
+
+        before do
+          post '/api/v1/literatures', params: params
+        end
+
+        it 'responds 201' do
+          expect(response).to have_http_status :created
+        end
+
+        it 'literature was not created again' do
+          expect(Literature.count).to be literatures_count
+        end
+
+        it 'literal again attached to cell line' do
+          expect(cell_line.reload.cellline_material.literatures.count).to eq 1
+        end
+      end
+    end
+
     context 'with valid parameters' do
       before { post '/api/v1/literatures', params: params }
 
@@ -106,7 +182,7 @@ describe Chemotion::LiteratureAPI do
     end
 
     context 'with valid parameter' do
-      let!(:literal)    { create(:literal, literature: l1, element: r1, user: user) }
+      let!(:literal) { create(:literal, literature: l1, element: r1, user: user) }
       let!(:element_id) { r1.id }
 
       before do
@@ -124,4 +200,4 @@ describe Chemotion::LiteratureAPI do
   end
 end
 # rubocop:enable RSpec/LetSetup
-# rubocop:enable RSpec/FilePath
+# rubocop:enable RSpec/FilePath,RSpec/MultipleMemoizedHelpers, RSpec/NestedGroups
