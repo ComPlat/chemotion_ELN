5775123fa24a5fb0e28ea4b8c80dbbe8ecffca6f
FabianMauz <fmauz@ipb-halle.de>
2023-11-06 11:28:00 +0000

    fix: fixed place of cell line element

--- db/migrate/20231106000000_cellline_element_init.rb
+++ db/migrate/20231106000000_cellline_element_init.rb
@@ -4,7 +4,7 @@ class CelllineElementInit < ActiveRecord::Migration[6.1]
 
   def self.up
     klass = ElementKlass.find_or_create_by(name: "cell_line")
-    attributes = { label: "cell_line".titleize, desc: "ELN #{"cell_line".titleize}", icon_name: "icon-#{"cell_line"}", klass_prefix: '', properties_template: {}, is_generic: false, place: -1000 }
+    attributes = { label: "cell_line".titleize, desc: "ELN #{"cell_line".titleize}", icon_name: "icon-#{"cell_line"}", klass_prefix: '', properties_template: {}, is_generic: false, place: 5 }
     klass&.update(attributes)
   end
 end
