c9e069b5301693f2a0a879f87ade3bbbb750200a
FabianMauz <fmauz@ipb-halle.de>
2023-12-11 07:43:34 +0000

    refactor: removed dead code

--- app/packs/src/components/contextActions/CreateButton.js
+++ app/packs/src/components/contextActions/CreateButton.js
@@ -265,20 +265,7 @@ export default class CreateButton extends React.Component {
     const { layout } = this.state;
     const type = UserStore.getState().currentType;
     const { elements, genericEls, itemTables } = elementList();
-
-    // let genericEls = [];
-    // const currentUser = (UserStore.getState() && UserStore.getState().currentUser) || {};
-    // if (MatrixCheck(currentUser.matrix, 'genericElement')) {
-    //   genericEls = UserStore.getState().genericEls || [];
-    // }
-    // const itemTables = [];
-    // // eslint-disable-next-line max-len
-    // const sortedLayout = filter(Object.entries(layout), (o) => o[1] && o[1] > 0).sort((a, b) => a[1] - b[1]);
-
-    // sortedLayout?.forEach(([k]) => {
-    //   const el = elements.concat(genericEls).find((ael) => ael.name === k);
-    //   if (el) itemTables.push(<MenuItem id={`create-${el.name}-button`} key={el.name} onSelect={() => this.createElementOfType(`${el.name}`)}>Create {el.label}</MenuItem>);
-    // });
+    
     elements.concat(genericEls).forEach((el) => {
       itemTables.push(<MenuItem id={`create-${el.name}-button`} key={el.name} onSelect={() => this.createElementOfType(`${el.name}`)}>Create {el.label}</MenuItem>);
     });
