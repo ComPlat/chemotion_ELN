df1a75b92b4f2ad3786a63810720dc81f152d080
Johannes Haubold <jh@megorei.com>
2023-12-13 12:36:05 +0100

    chore: Improve Dev Setup by autorecognizing the installed tool versions (#1665)

--- run-ruby-dev.sh
+++ run-ruby-dev.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
+export ASDF_BRANCH=v0.13.1
 
-gem install bundler -v 2.1.4 && bundle install
+# if asdf is not installed -> install
+if command -v asdf ; then
+    echo '>>> asdf is already installed -> continue'
+else
+    echo '>>> Missing asdf. Installing...'
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch $ASDF_BRANCH
+fi
+
+# if asdf plugins are not installed -> install
+if asdf plugin list | grep -Fxq 'ruby'; then
+    echo '>>> Ruby plugin for asdf is installed -> continue'
+else
+    echo '>>> Missing ruby plugin for asdf. Installing...'
+    asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+fi
+
+if asdf plugin list | grep -Fxq 'nodejs'; then
+    echo '>>> Nodejs plugin for asdf is installed -> continue'
+else
+    echo '>>> Missing nodejs plugin for asdf. Installing...'
+    asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+fi
+
+# install all tools as defined by .tool-versions
+echo '>>> Installing tool versions'
+asdf install
+
+# install bundler if not present and then install all gems as defined by GEMFILE
+echo '>>> checking bundler installation'
+BUNDLER_VERSION=$(sed -n '/BUNDLED WITH/{n;p;}' "Gemfile.lock" | tr -d '[:space:]')
+gem install bundler -v $BUNDLER_VERSION
+
+echo '>>> Installing gems'
+bundle install
 
 rm -f tmp/pids/server.pid
 
@@ -15,6 +49,5 @@ else
     echo "================================================"
     bundle exec rake db:setup
 fi
-
 
 bundle exec rails s -p 3000 -b 0.0.0.0
