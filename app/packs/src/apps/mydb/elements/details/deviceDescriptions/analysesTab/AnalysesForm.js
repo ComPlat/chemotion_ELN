304c95d47f32c04ab9f2c444b2f89eee293f20be
Beate Quednau <beate@firma-pflaum.de>
2023-12-18 15:56:08 +0100

    Add basic form with tabs for device description

---
+++ app/packs/src/apps/mydb/elements/details/deviceDescriptions/analysesTab/AnalysesForm.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const AnalysesForm = () => {
+
+  return (
+    <>
+      Analyses Tab
+    </>
+  );
+}
+
+export default AnalysesForm;
---
+++ app/packs/src/apps/mydb/elements/details/deviceDescriptions/detailsTab/DetailsForm.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const DetailsForm = () => {
+
+  return (
+    <>
+      Details Tab
+    </>
+  );
+}
+
+export default DetailsForm;
