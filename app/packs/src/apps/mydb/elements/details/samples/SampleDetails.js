1f313bbc1ee68a803e1d5b5962c5d21a85b54c1e
mekkyz <mos.mekky@gmail.com>
2024-01-09 21:38:11 +0000

    Fix: remove duplicate user label and center the share button

--- app/packs/src/apps/mydb/elements/details/samples/SampleDetails.js
+++ app/packs/src/apps/mydb/elements/details/samples/SampleDetails.js
@@ -972,7 +972,7 @@ export default class SampleDetails extends React.Component {
           </OverlayTrigger>
           <ShowUserLabels element={sample} />
           <ElementAnalysesLabels element={sample} key={`${sample.id}_analyses`} />
-          {colLabel}
+          <div style={{ marginLeft: '10px', marginTop: '-5px' }}>{colLabel}</div>
           <ElementReactionLabels element={sample} key={`${sample.id}_reactions`} />
           <PubchemLabels element={sample} />
           <HeaderCommentSection element={sample} />
@@ -1034,7 +1034,6 @@ export default class SampleDetails extends React.Component {
           {inventorySample}
           {decoupleCb}
         </div>
-        <ShowUserLabels element={sample} />
       </div>
     );
   }
