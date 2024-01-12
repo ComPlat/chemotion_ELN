0ad4c54a9c38615a896084479a3e772c510cba72
Lan Le <lan.le@kit.edu>
2024-01-12 09:54:03 +0100

    feat: display multi spectra for NMR, HPLC UIVIS

--- app/packs/src/apps/mydb/elements/details/ViewSpectra.js
+++ app/packs/src/apps/mydb/elements/details/ViewSpectra.js
@@ -23,7 +23,11 @@ const rmRefreshed = (analysis) => {
 const layoutsWillShowMulti = [
   FN.LIST_LAYOUT.CYCLIC_VOLTAMMETRY,
   FN.LIST_LAYOUT.SEC,
-  FN.LIST_LAYOUT.AIF
+  FN.LIST_LAYOUT.AIF,
+  FN.LIST_LAYOUT.H1,
+  FN.LIST_LAYOUT.C13,
+  FN.LIST_LAYOUT.UVVIS,
+  FN.LIST_LAYOUT.HPLC_UVVIS,
 ];
 
 class ViewSpectra extends React.Component {
@@ -580,13 +584,14 @@ class ViewSpectra extends React.Component {
   }
 
   buildOpsByLayout(et) {
-    if (this.props.sample && this.props.sample instanceof ResearchPlan) {
+    const { sample } = this.props;
+    if (sample && sample instanceof ResearchPlan) {
       return [
         { name: 'write & save', value: this.saveOp },
         { name: 'write, save & close', value: this.saveCloseOp },
       ];
     }
-    const updatable = this.props.sample && this.props.sample.can_update;
+    const updatable = sample && sample.can_update;
     let baseOps = updatable ? [
       { name: 'write peak & save', value: this.writePeakOp },
       { name: 'write peak, save & close', value: this.writeClosePeakOp },
@@ -606,12 +611,11 @@ class ViewSpectra extends React.Component {
           { name: 'save', value: this.writeCommon },
           { name: 'save & close', value: this.writeCloseCommon },
         ];
-      } else {
-        return [
-          { name: 'save', value: this.saveOp },
-          { name: 'save & close', value: this.saveCloseOp },
-        ];
       }
+      return [
+        { name: 'save', value: this.saveOp },
+        { name: 'save & close', value: this.saveCloseOp },
+      ];
     }
     const saveable = updatable;
     if (saveable) {
@@ -688,16 +692,16 @@ class ViewSpectra extends React.Component {
     let entityFileNames = false;
     if (!isExist) {
       if (!listMuliSpcs || listMuliSpcs.length === 0) return this.renderInvalid();
-      listMuliSpcs = listMuliSpcs.filter(((x) => x !== undefined));
-      listEntityFiles = listEntityFiles.filter(((x) => x !== undefined));
-      multiEntities = listMuliSpcs.map((spc) => {
+      const filteredListMuliSpcs = listMuliSpcs.filter(((x) => x !== undefined));
+      const filteredListEntityFiles = listEntityFiles.filter(((x) => x !== undefined));
+      multiEntities = filteredListMuliSpcs.map((spc) => {
         const {
           entity
         } = FN.buildData(spc.jcamp);
         currEntity = entity;
         return entity;
       });
-      entityFileNames = listEntityFiles.map((x) => x.label);
+      entityFileNames = filteredListEntityFiles.map((x) => x.label);
     }
 
     const others = this.buildOthers();
@@ -715,7 +719,8 @@ class ViewSpectra extends React.Component {
         {
           !isExist && multiEntities.length === 0
             ? this.renderInvalid()
-            : <SpectraEditor
+            : (
+              <SpectraEditor
                 entity={currEntity}
                 multiEntities={multiEntities}
                 entityFileNames={entityFileNames}
@@ -728,7 +733,8 @@ class ViewSpectra extends React.Component {
                 canChangeDescription
                 onDescriptionChanged={this.onSpectraDescriptionChanged}
                 userManualLink={{ cv: 'https://www.chemotion.net/docs/services/chemspectra/cv' }}
-            />
+              />
+            )
         }
       </Modal.Body>
     );
