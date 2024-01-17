41e8bb31cdef88201f2524432e3e10be02605464
mekkyz <mos.mekky@gmail.com>
2024-01-17 19:51:47 +0000

    Fix: eslint

--- app/packs/src/apps/mydb/elements/list/ElementsTableSampleEntries.js
+++ app/packs/src/apps/mydb/elements/list/ElementsTableSampleEntries.js
@@ -63,7 +63,7 @@ const isCurrEleDropType = (sourceType, targetType) => {
 const dragColumn = (element, showDragColumn, sourceType, targetType) => {
   if (showDragColumn) {
     return (
-      <td style={{ verticalAlign: 'middle', textAlign: 'center' }} >
+      <td style={{ verticalAlign: 'middle', textAlign: 'center' }}>
         <ElementContainer
           key={element.id}
           sourceType={isCurrEleDropType(sourceType, targetType) ? sourceType : ''}
@@ -75,7 +75,7 @@ const dragColumn = (element, showDragColumn, sourceType, targetType) => {
   return null;
 };
 
-const TopSecretIcon = ({ element }) => {
+function TopSecretIcon({ element }) {
   if (element.type === 'sample' && element.is_top_secret === true) {
     const tooltip = (<Tooltip id="top_secret_icon">Top secret</Tooltip>);
     return (
@@ -85,20 +85,20 @@ const TopSecretIcon = ({ element }) => {
     );
   }
   return null;
-};
+}
 
 TopSecretIcon.propTypes = {
   element: PropTypes.object,
 };
 
-const XvialIcon = ({ label }) => {
+function XvialIcon({ label }) {
   return (label || '').match(/^X\d+.*/) ? (
     <i
       className="icon-xvial"
       style={{ marginRight: '5px', fontSize: '20px' }}
     />
   ) : null;
-};
+}
 
 XvialIcon.propTypes = {
   label: PropTypes.string,
@@ -122,26 +122,30 @@ const svgPreview = (showPreviews, sample) => (
   <div style={{ float: 'left' }}>
     {
       showPreviews
-        ? <SvgWithPopover
-          hasPop
-          previewObject={{
-            txtOnly: '',
-            isSVG: true,
-            src: sample.svgPath
-          }}
-          popObject={{
-            title: sample.molecule_iupac_name,
-            src: sample.svgPath,
-            height: '26vh',
-            width: '52vw',
-          }}
-        />
+        ? (
+          <SvgWithPopover
+            hasPop
+            previewObject={{
+              txtOnly: '',
+              isSVG: true,
+              src: sample.svgPath
+            }}
+            popObject={{
+              title: sample.molecule_iupac_name,
+              src: sample.svgPath,
+              height: '26vh',
+              width: '52vw',
+            }}
+          />
+        )
         : null
     }
   </div>
 );
 
-const MoleculeHeader = ({ sample, show, showDragColumn, onClick, targetType }) => {
+function MoleculeHeader({
+  sample, show, showDragColumn, onClick, targetType
+}) {
   const showIndicator = (show) ? 'glyphicon-chevron-down' : 'glyphicon-chevron-right';
 
   const { collId, showPreviews } = UIStore.getState();
@@ -150,33 +154,38 @@ const MoleculeHeader = ({ sample, show, showDragColumn, onClick, targetType }) =
       style={{ backgroundColor: '#F5F5F5', cursor: 'pointer' }}
       onClick={onClick}
     >
-      {sample.molecule?.inchikey === 'DUMMY' && sample.molfile == null ?
-        (<td colSpan="3" style={{ position: 'relative ' }} ><div><h4>(No-structure sample)</h4></div></td>) :
-        (
-          <td colSpan="2" style={{ position: 'relative ' }} >
+      {sample.molecule?.inchikey === 'DUMMY' && sample.molfile == null
+        ? (<td colSpan="3" style={{ position: 'relative ' }}><div><h4>(No-structure sample)</h4></div></td>)
+        : (
+          <td colSpan="2" style={{ position: 'relative ' }}>
             {svgPreview(showPreviews, sample)}
-            <div style={{ position: 'absolute', right: '3px', top: '14px' }} >
-              <OverlayTrigger placement="bottom" overlay={overlayToggle} >
-                <span style={{ fontSize: 15, color: '#337ab7', lineHeight: '10px' }} >
+            <div style={{ position: 'absolute', right: '3px', top: '14px' }}>
+              <OverlayTrigger placement="bottom" overlay={overlayToggle}>
+                <span style={{ fontSize: 15, color: '#337ab7', lineHeight: '10px' }}>
                   <i className={`glyphicon ${showIndicator}`} />
                 </span>
               </OverlayTrigger>
             </div>
-            <div style={{ paddingLeft: 5, wordWrap: 'break-word' }} >
+            <div style={{ paddingLeft: 5, wordWrap: 'break-word' }}>
               <h4><SampleName sample={sample} /></h4>
             </div>
-            <div style={{ position: 'absolute', top: '10px', right: '25px', float: 'right' }} >
+            <div style={{
+              position: 'absolute', top: '10px', right: '25px', float: 'right'
+            }}
+            >
               <ChemrepoLabels chemrepoId={sample.molecule.chem_repo && sample.molecule.chem_repo.id} />
               <PubchemLabels element={sample} />
             </div>
-            <div style={{ position: 'absolute', bottom: '10px', right: '25px', float: 'right' }} >
+            <div style={{
+              position: 'absolute', bottom: '10px', right: '25px', float: 'right'
+            }}
+            >
               <ComputedPropLabel cprops={sample.molecule_computed_props} />
             </div>
           </td>
-        )
-      }
-      {sample.molecule?.inchikey === 'DUMMY' && sample.molfile == null ?
-        null : dragColumn(sample, showDragColumn, DragDropItemTypes.MOLECULE, targetType)}
+        )}
+      {sample.molecule?.inchikey === 'DUMMY' && sample.molfile == null
+        ? null : dragColumn(sample, showDragColumn, DragDropItemTypes.MOLECULE, targetType)}
     </tr>
   );
 }
@@ -226,7 +235,7 @@ export default class ElementsTableSampleEntries extends Component {
       let numSamples = moleculelist[moleculeId].length;
       if (nextProps.moleculeSort && numSamples > 3) { numSamples = 3; }
       displayedMoleculeGroup[idx].numSamples = numSamples;
-    })
+    });
     this.setState({
       displayedMoleculeGroup,
       targetType: currentElement && currentElement.type,
@@ -235,7 +244,9 @@ export default class ElementsTableSampleEntries extends Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    const { collapseAll, showDragColumn, moleculeSort, currentElement, elements, ui } = this.props;
+    const {
+      collapseAll, showDragColumn, moleculeSort, currentElement, elements, ui
+    } = this.props;
     const { checkedAll, checkedIds, uncheckedIds } = ui;
     const nextUi = nextProps.ui;
     return collapseAll !== nextProps.collapseAll // Bool
@@ -254,11 +265,22 @@ export default class ElementsTableSampleEntries extends Component {
     KeyboardStore.unlisten(this.sampleOnKeyDown);
   }
 
+  handleMoleculeToggle(moleculeName) {
+    let { moleculeGroupsShown } = this.state;
+    if (!moleculeGroupsShown.includes(moleculeName)) {
+      moleculeGroupsShown = moleculeGroupsShown.concat(moleculeName);
+    } else {
+      moleculeGroupsShown = moleculeGroupsShown.filter((item) => item !== moleculeName);
+    }
+    this.setState({ moleculeGroupsShown }, this.forceUpdate());
+    this.props.onChangeCollapse(false);
+  }
+
   sampleOnKeyDown(state) {
-    const context = state.context;
+    const { context } = state;
     if (context != 'sample') { return false; }
 
-    const documentKeyDownCode = state.documentKeyDownCode;
+    const { documentKeyDownCode } = state;
     let { keyboardIndex, keyboardSeletectedElementId, flattenSamplesId } = this.state;
 
     switch (documentKeyDownCode) {
@@ -305,18 +327,6 @@ export default class ElementsTableSampleEntries extends Component {
     }, this.forceUpdate());
   }
 
-
-  handleMoleculeToggle(moleculeName) {
-    let { moleculeGroupsShown } = this.state;
-    if (!moleculeGroupsShown.includes(moleculeName)) {
-      moleculeGroupsShown = moleculeGroupsShown.concat(moleculeName);
-    } else {
-      moleculeGroupsShown = moleculeGroupsShown.filter(item => item !== moleculeName);
-    }
-    this.setState({ moleculeGroupsShown }, this.forceUpdate());
-    this.props.onChangeCollapse(false);
-  }
-
   isElementChecked(element) {
     const { checkedIds, uncheckedIds, checkedAll } = this.props.ui;
     return (checkedAll && ArrayUtils.isValNotInArray(uncheckedIds || [], element.id))
@@ -328,12 +338,11 @@ export default class ElementsTableSampleEntries extends Component {
     return (currentElement && currentElement.id === element.id);
   }
 
-
   renderSamples(samples, index) {
     const { keyboardSeletectedElementId, displayedMoleculeGroup } = this.state;
     const { showDragColumn } = this.props;
-    const length = samples.length;
-    const numSamples = displayedMoleculeGroup[index].numSamples;
+    const { length } = samples;
+    const { numSamples } = displayedMoleculeGroup[index];
 
     const sampleRows = samples.slice(0, numSamples).map((sample, ind) => {
       const selected = this.isElementSelected(sample);
@@ -375,13 +384,20 @@ export default class ElementsTableSampleEntries extends Component {
 
     if (numSamples < length) {
       const showMoreSamples = (
-        <tr key={`${index}_showMore`}><td colSpan="3" style={{ padding: 0 }} >
-          <Button
-            bsStyle="info"
-            onClick={() => this.showMoreSamples(index)}
-            style={{ fontSize: '14px', width: '100%', float: 'left', borderRadius: '0px' }}
-          >Show more samples</Button>
-        </td></tr>
+        <tr key={`${index}_showMore`}>
+          <td colSpan="3" style={{ padding: 0 }}>
+            <Button
+              bsStyle="info"
+              onClick={() => this.showMoreSamples(index)}
+              style={{
+                fontSize: '14px', width: '100%', float: 'left', borderRadius: '0px'
+              }}
+            >
+              Show more samples
+            </Button>
+          </td>
+
+        </tr>
       );
       sampleRows.push(showMoreSamples);
     }
@@ -392,7 +408,7 @@ export default class ElementsTableSampleEntries extends Component {
   renderMoleculeGroup(moleculeGroup, index) {
     const { showDragColumn, collapseAll } = this.props;
     const { moleculeGroupsShown, targetType } = this.state;
-    const molecule = moleculeGroup[0].molecule;
+    const { molecule } = moleculeGroup[0];
     const moleculeName = molecule.iupac_name || molecule.inchistring;
     const showGroup = !moleculeGroupsShown.includes(moleculeName) && !collapseAll;
 
@@ -413,10 +429,10 @@ export default class ElementsTableSampleEntries extends Component {
   render() {
     const { displayedMoleculeGroup } = this.state;
     return (
-      <Table className="sample-entries" >
+      <Table className="sample-entries">
         {Object.keys(displayedMoleculeGroup).map((group, index) => {
           const moleculeGroup = displayedMoleculeGroup[group];
-          const numSamples = displayedMoleculeGroup[group].numSamples;
+          const { numSamples } = displayedMoleculeGroup[group];
           return this.renderMoleculeGroup(moleculeGroup, index, numSamples);
         })}
       </Table>
