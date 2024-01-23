f9bb99c166b1afdc114da5752365547ad087fc27
Adam Basha <55552142+adambasha0@users.noreply.github.com>
2024-01-23 08:56:54 +0100

    feat: add sample inventory label counter
    add inventory model and inventory label feature 
    
    Refs: #1581

---
+++ spec/javascripts/packs/src/fetchers/InventoryFetcher.spec.js
@@ -0,0 +1,107 @@
+import InventoryFetcher from 'src/fetchers/InventoryFetcher';
+import expect from 'expect';
+import sinon from 'sinon';
+import {
+  describe, it, beforeEach, afterEach
+} from 'mocha';
+
+describe('InventoryFetcher methods', () => {
+  let fetchStub;
+
+  beforeEach(() => {
+    fetchStub = sinon.stub(global, 'fetch');
+  });
+
+  afterEach(() => {
+    fetchStub.restore();
+  });
+
+  describe('updateInventoryLabel', () => {
+    const params = {
+      prefix: 'BCN',
+      name: 'Bräse North Camp',
+      counter: 1,
+      collection_ids: [10]
+    };
+    const expectedResponse = [
+      {
+        collections: [
+          {
+            id: 10,
+            label: 'chemotion-repository.net'
+          },
+          {
+            id: 16,
+            label: 'project CU1-lavender'
+          },
+        ],
+        inventory: {
+          id: 3,
+          prefix: 'BCN',
+          name: 'Bräse North Camp',
+          counter: 1,
+        }
+      },
+    ];
+
+    it('should update sample inventory label for a user', async () => {
+      fetchStub.resolves(new Response(JSON.stringify(expectedResponse)));
+
+      const result = await InventoryFetcher.updateInventoryLabel(params);
+
+      sinon.assert.calledOnce(fetchStub);
+      sinon.assert.calledWithExactly(fetchStub, '/api/v1/inventory/update_inventory_label', {
+        credentials: 'same-origin',
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(params)
+      });
+      expect(result).not.toBeNull();
+      expect(result).toEqual(expectedResponse);
+    });
+  });
+
+  describe('fetchLabelsAndCollections', () => {
+    const expectedResponse = [
+      {
+        collections: [
+          {
+            id: 10,
+            label: 'chemotion-repository.net'
+          },
+          {
+            id: 16,
+            label: 'project CU1-lavender'
+          },
+        ],
+        inventory: {
+          id: 3,
+          prefix: 'BCN',
+          name: 'Bräse North Camp',
+          counter: 1,
+        }
+      },
+    ];
+
+    it('should fetch user inventory labels and collections', async () => {
+      fetchStub.resolves(new Response(JSON.stringify(expectedResponse)));
+
+      const result = await InventoryFetcher.fetchLabelsAndCollections();
+
+      sinon.assert.calledOnce(fetchStub);
+      sinon.assert.calledWithExactly(fetchStub, '/api/v1/inventory/user_inventory_collections', {
+        credentials: 'same-origin',
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+
+      expect(result).toEqual(expectedResponse);
+    });
+  });
+});
