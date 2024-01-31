7c7511a3b27ff7c132a3105059ff145717457209
Mehreen <mehreen.mansur@gmail.com>
2024-01-31 15:20:09 +0100

    add rubocop fixes

--- spec/features/copy_research_plan_spec.rb
+++ spec/features/copy_research_plan_spec.rb
@@ -3,34 +3,34 @@
 require 'rails_helper'
 
 describe 'Copy research plans' do
-  let!(:user1) { create(:user, first_name: 'Hello', last_name: 'Complat', account_active: true) }
-  let!(:user2) { create(:user, first_name: 'User2', last_name: 'Complat', account_active: true) }
-  let(:rp1) { create(:research_plan, creator: user1, name: 'RP 1', body: []) }
-  let!(:col1) { create(:collection, user_id: user1.id, label: 'Col1') }
+  let!(:user_one) { create(:user, first_name: 'Hello', last_name: 'Complat', account_active: true) }
+  let!(:user_two) { create(:user, first_name: 'User2', last_name: 'Complat', account_active: true) }
+  let(:rp_one) { create(:research_plan, creator: user_one, name: 'RP 1', body: []) }
+  let!(:col_one) { create(:collection, user_id: user_one.id, label: 'Col1') }
 
-  let(:rp2) { create(:research_plan, creator: user1, name: 'RP 2', body: []) }
-  let!(:root_share) { create(:collection, user: user1, shared_by_id: user2.id, is_shared: true, is_locked: true) }
+  let(:rp_two) { create(:research_plan, creator: user_one, name: 'RP 2', body: []) }
+  let!(:root_share) { create(:collection, user: user_one, shared_by_id: user_two.id, is_shared: true, is_locked: true) }
   let!(:col_share) do
-    create(:collection, user: user1, label: 'share-col', permission_level: 10, shared_by_id: user2.id,
+    create(:collection, user: user_one, label: 'share-col', permission_level: 10, shared_by_id: user_two.id,
                         is_shared: true, ancestry: root_share.id.to_s)
   end
 
   before do
-    sign_in(user1)
+    sign_in(user_one)
     fp = Rails.public_path.join('images', 'molecules', 'molecule.svg')
     svg_path = Rails.root.join('spec/fixtures/images/molecule.svg')
     `ln -s #{svg_path} #{fp} ` unless File.exist?(fp)
-    CollectionsResearchPlan.find_or_create_by!(research_plan: rp1, collection: col1)
-    CollectionsResearchPlan.find_or_create_by!(research_plan: rp2, collection: col_share)
+    CollectionsResearchPlan.find_or_create_by!(research_plan: rp_one, collection: col_one)
+    CollectionsResearchPlan.find_or_create_by!(research_plan: rp_two, collection: col_share)
   end
 
-  it 'new research plan', js: true do
+  it 'new research plan', :js do
     find_by_id('tree-id-Col1').click
     first('i.icon-research_plan').click
-    expect(page).not_to have_button('copy-element-btn', wait: 5)
+    expect(page).to have_no_button('copy-element-btn', wait: 5)
   end
 
-  it 'to same collection', js: true do
+  it 'to same collection', :js do
     find_by_id('tree-id-Col1').click
     first('i.icon-research_plan').click
     find_by_id('tree-id-Col1').click
@@ -43,7 +43,7 @@ describe 'Copy research plans' do
     expect(page).to have_content('RP copy')
   end
 
-  it 'to diff collection', js: true do
+  it 'to diff collection', :js do
     find_by_id('tree-id-Col1').click
     first('i.icon-research_plan').click
     find_by_id('tree-id-Col1').click
@@ -57,7 +57,7 @@ describe 'Copy research plans' do
     expect(page).to have_content('RP copy', wait: 5)
   end
 
-  it 'to shared collection with permission', js: true do
+  it 'to shared collection with permission', :js do
     find_by_id('shared-home-link').click
     find_all('span.glyphicon-plus')[0].click
     find_by_id('tree-id-share-col').click
