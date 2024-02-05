7b76965da0b62f7266cfbd0289864b8727996b1e
Beate Quednau <beate@firma-pflaum.de>
2024-02-05 14:12:46 +0100

    Add encryption / decription for device novnc password

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -2,9 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe Device, type: :model do
+RSpec.describe Device do
   let(:user) { create(:person) }
   let(:device) { create(:device) }
+  let(:device_with_novnc) { create(:device, :novnc_settings) }
 
   before do
     device.people << user
@@ -13,5 +14,25 @@ RSpec.describe Device, type: :model do
 
   it 'handles user device relationship correctly' do
     expect(device.users).to eq Person.where id: user.id
+  end
+
+  describe '.encrypt_novnc_password' do
+    before do
+      device_with_novnc.update!({ novnc_password: 'testen' })
+    end
+
+    it 'returns an encryted novnc password' do
+      expect(device_with_novnc.novnc_password).not_to eq('testen')
+    end
+  end
+
+  describe '.decrypted_novnc_password' do
+    before do
+      device_with_novnc.update!({ novnc_password: 'testen' })
+    end
+
+    it 'returns a decrypted novnc password' do
+      expect(device_with_novnc.decrypted_novnc_password).to eq('testen')
+    end
   end
 end
