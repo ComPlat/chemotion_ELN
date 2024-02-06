b2ba5c7bb6a25be03cc566a134b789ffb726e2fb
Beate Quednau <beate@firma-pflaum.de>
2024-02-06 17:56:13 +0100

    Add annotation to device model

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,39 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: devices
+#
+#  id                            :bigint           not null, primary key
+#  name                          :string
+#  name_abbreviation             :string
+#  first_name                    :string
+#  last_name                     :string
+#  email                         :string
+#  serial_number                 :string
+#  verification_status           :string           default("none")
+#  account_active                :boolean          default(FALSE)
+#  visibility                    :boolean          default(FALSE)
+#  deleted_at                    :datetime
+#  created_at                    :datetime         not null
+#  updated_at                    :datetime         not null
+#  datacollector_method          :string
+#  datacollector_dir             :string
+#  datacollector_host            :string
+#  datacollector_user            :string
+#  datacollector_authentication  :string
+#  datacollector_number_of_files :string
+#  datacollector_key_name        :string
+#  novnc_token                   :string
+#  novnc_target                  :string
+#  novnc_password                :string
+#
+# Indexes
+#
+#  index_devices_on_deleted_at         (deleted_at)
+#  index_devices_on_email              (email) UNIQUE
+#  index_devices_on_name_abbreviation  (name_abbreviation) UNIQUE WHERE (name_abbreviation IS NOT NULL)
+#
 class Device < ApplicationRecord
   attr_accessor :datacollector_fields
 
