34ffaeab5cb8c0d9594ac705aaa3924be5aa9301
fathia <f.idiris1@gmail.com>
2024-02-13 09:48:37 +0000

    remove outdated migration files

--- db/migrate/20240118121856_add_sampleable_to_sample.rb
+++
@@ -1,5 +0,0 @@
-class AddSampleableToSample < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :samples, :sampleable, polymorphic: true, index: true
-  end
-end
--- db/migrate/20240126140201_remove_polymorphic_association_from_samples.rb
+++
@@ -1,5 +0,0 @@
-class RemovePolymorphicAssociationFromSamples < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :samples, :sampleable, polymorphic: true, index: true
-  end
-end
