cf2b2b160233384431866e4cc710d9284d9e8fcf
PiTrem <pierre.tremouilhac@kit.edu>
2024-02-19 18:10:34 +0100

    refactor: rb quill delta converter (#1757)
    - quill convert as class method
    
    - prevent spawning node process if ops is blank
    
    - option to filter image insert from ops
    
    - pass input as file if too large (`Errno::E2BIG (Argument list too long)`)
    
    - filter empty inserts from string input to avoid conversion to fail
    
    - force gc on migrations that uses the quill-converter to avoid accumulation of node processes
    
    chore: revert nokogiri upd ( ruby version requirement )

--- spec/lib/chemotion/quill_to_plain_text_spec.rb
+++ spec/lib/chemotion/quill_to_plain_text_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'QuillToPlainText' do
-  subject(:lib) { Chemotion::QuillToPlainText.new }
+  subject(:lib) { Chemotion::QuillToPlainText }
 
   describe 'convert' do
     let(:delta_ops) do
@@ -46,6 +46,9 @@ RSpec.describe 'QuillToPlainText' do
     let(:delta_ops) do
       "{\"ops\":[{\"insert\":\"\"}]}"
     end
+    let(:delta_ops_multi) do
+      "{\"ops\":[{\"insert\":\"\"},{\"insert\":\"\"}]}"
+    end
     let(:plain_text) do
       ''
     end
@@ -56,6 +59,23 @@ RSpec.describe 'QuillToPlainText' do
 
     it 'converts a quill delta ops as ruby json string to plain text' do
       expect(lib.convert(JSON.parse(delta_ops))).to match(plain_text)
+    end
+
+    it 'converts a quill delata with multiple empty inserts' do
+      expect(lib.convert(JSON.parse(delta_ops_multi))).to match(plain_text)
+    end
+  end
+
+  describe 'convert long delta' do
+    let(:delta_ops) do
+      "{\"ops\":[{\"insert\":\"#{'a' * 10_000}\"}]}"
+    end
+    let(:plain_text) do
+      'a' * 10_000
+    end
+
+    it 'converts a long quill delta ops to plain text' do
+      expect(lib.convert(delta_ops)).to match(plain_text)
     end
   end
 end
