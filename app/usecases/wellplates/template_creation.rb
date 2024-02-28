6f8d89cce8ffb5c501e0d9241e73c9686fe302cc
FabianMauz <fmauz@ipb-halle.de>
2024-02-28 13:37:02 +0000

    refactor: refactored template creation

--- app/usecases/wellplates/template_creation.rb
+++ app/usecases/wellplates/template_creation.rb
@@ -10,24 +10,42 @@ module Usecases
       end
 
       def execute!
-        raise "Width of wellplate to high" if @wellplate.width >100 
-        raise "Height of wellplate to high" if @wellplate.height >100
+        raise 'Width of wellplate to high' if @wellplate.width > 100
+        raise 'Height of wellplate to high' if @wellplate.height > 100
 
+        xfile = create_excel_object
+        create_data_content(xfile.workbook.worksheets.first)
+
+        xfile
+      end
+
+      private
+
+      def create_excel_object
         xfile = Axlsx::Package.new
-        file_extension = 'xlsx'
         xfile.workbook.styles.fonts.first.name = 'Calibri'
+
+        xfile.workbook.styles do |s|
+          @data_style = s.add_style alignment: { horizontal: :center }, sz: 10
+          @header_style = s.add_style alignment: { horizontal: :center }, sz: 11
+        end
+
         sheet = xfile.workbook.add_worksheet(name: 'Wellplate import template')
         sheet.add_row(@header)
 
-        start = 0.1
-        x = 1
-        @wellplate.wells.each do |well|
-          well_data = [well.sortable_alphanumeric_position, '', '', '', start, 'mg', x, 'GW']
-          x += 1
-          start += 0.1
-          sheet.add_row(well_data)
-        end
         xfile
+      end
+
+      def create_data_content(sheet)
+        mock_value_mg = 0.1
+        mock_value_gw = 1
+        @wellplate.wells.each do |well|
+          well_data = [well.sortable_alphanumeric_position, '', '', '',
+                       mock_value_mg.round(2), 'mg', mock_value_gw, 'GW']
+          mock_value_gw += 1
+          mock_value_mg += 0.01
+          sheet.add_row(well_data, style: @data_style)
+        end
       end
     end
   end
