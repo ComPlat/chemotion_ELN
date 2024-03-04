625ca9f2a677a7c21a0865f78d2efc876e0b7b09
Mehreen <mehreen.mansur@gmail.com>
2024-03-04 12:29:40 +0100

    delete elements from all collections on transfer of ownership

--- app/usecases/sharing/take_ownership.rb
+++ app/usecases/sharing/take_ownership.rb
@@ -17,12 +17,11 @@ module Usecases
         # if user already owns the (unshared) collection, there is nothing to do here
         return if acl.user_id == previous_owner_id
 
+        delete_from_all_collection(previous_owner_id, collection)
         cols = Collection.where([' id = ? or ancestry = ?  or ancestry like ? or ancestry like ? or ancestry like ? ',
                                  col_id, col_id.to_s, "%/#{col_id}", "#{col_id}/%", "%/#{col_id}/%"])
         cols.update_all(user_id: new_owner_id)
-
-        acl.update(user_id: previous_owner_id, permission_level: 2)
-        collection.update(user_id: previous_owner_id)
+        acl.destroy
 
         user = User.find_by(id: new_owner_id)
         Message.create_msg_notification(
@@ -30,6 +29,15 @@ module Usecases
           data_args: { new_owner: user.name, collection_name: collection.label },
           message_from: new_owner_id, message_to: [previous_owner_id]
         )
+      end
+
+      def delete_from_all_collection(previous_owner_id, collection)
+        all_col = Collection.find_by(user_id: previous_owner_id, label: 'All')
+        %w[sample reaction wellplate screen research_plan].each do |element|
+          element_ids = collection.send(element + 's').pluck(:id)
+          collection_element = 'Collections' + (element.split('_').map(&:capitalize).join(''))
+          collection_element.constantize.where(collection_id: all_col.id, "#{element}_id": [element_ids]).destroy_all
+        end
       end
     end
   end
