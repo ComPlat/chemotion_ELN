ea5af85af76fffefeb9f0da1494a193cc5f87977
FabianMauz <fmauz@ipb-halle.de>
2024-03-13 13:11:22 +0000

    refactor: refactored condition for duration in advanced search

--- app/usecases/search/conditions_for_advanced_search.rb
+++ app/usecases/search/conditions_for_advanced_search.rb
@@ -190,14 +190,13 @@ module Usecases
           @conditions[:additional_condition] = "AND (#{@table}.temperature ->> 'valueUnit')::TEXT = '#{filter['unit']}'"
           @conditions[:condition_table] = ''
         when 'duration'
-          time_divisor= duration_interval_by_unit(filter['unit'])
-          is_data_valid = "reactions.duration similar to ('\\d+%')"
+          time_divisor = duration_interval_by_unit(filter['unit'])
+          is_data_valid = "#{@table}.duration similar to ('\\d+%')"
           time_from_duration_column = "EXTRACT(epoch FROM #{@table}.duration::interval)/#{time_divisor}::INT"
 
           @conditions[:field] =
             "CASE WHEN #{is_data_valid} THEN #{time_from_duration_column} ELSE 0 END"
 
-          @conditions[:first_condition] = "#{@table}.duration IS NOT NULL AND #{@table}.duration != '' AND "
           @conditions[:condition_table] = ''
         when 'target_amount_value'
           @conditions[:additional_condition] = "AND #{@table}.target_amount_unit = '#{filter['unit']}'"
