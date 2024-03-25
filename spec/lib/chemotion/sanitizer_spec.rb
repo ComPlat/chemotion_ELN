ea4605e3616a98e019732099dff31c38fdffce68
PiTrem <pierre.tremouilhac@kit.edu>
2024-03-25 10:52:40 +0100

    rubocop

--- spec/lib/chemotion/sanitizer_spec.rb
+++ spec/lib/chemotion/sanitizer_spec.rb
@@ -5,15 +5,15 @@ require 'rails_helper'
 describe Chemotion::Sanitizer do
   subject(:sanitizer) { described_class }
 
-  let(:svg_file_1) { File.read('spec/fixtures/svg/cyclo-C2H4I.cdjs.in.svg') }
-  let(:svg_file_2) { File.read('spec/fixtures/svg/cyclo-C2H4I.k215.in.svg') }
-  let(:svg_file_3) { File.read('spec/fixtures/svg/oxirane-resine.k1.in.svg') }
-  let(:svg_file_4) { File.read('spec/fixtures/svg/oxirane.k218.in.svg') }
+  let(:svg_file1) { File.read('spec/fixtures/svg/cyclo-C2H4I.cdjs.in.svg') }
+  let(:svg_file2) { File.read('spec/fixtures/svg/cyclo-C2H4I.k215.in.svg') }
+  let(:svg_file3) { File.read('spec/fixtures/svg/oxirane-resine.k1.in.svg') }
+  let(:svg_file4) { File.read('spec/fixtures/svg/oxirane.k218.in.svg') }
 
-  let(:svg_file_1_sanitized) { File.read('spec/fixtures/svg/cyclo-C2H4I.cdjs.out.svg') }
-  let(:svg_file_2_sanitized) { File.read('spec/fixtures/svg/cyclo-C2H4I.k215.out.svg') }
-  let(:svg_file_3_sanitized) { File.read('spec/fixtures/svg/oxirane-resine.k1.out.svg') }
-  let(:svg_file_4_sanitized) { File.read('spec/fixtures/svg/oxirane.k218.out.svg') }
+  let(:svg_file1_sanitized) { File.read('spec/fixtures/svg/cyclo-C2H4I.cdjs.out.svg') }
+  let(:svg_file2_sanitized) { File.read('spec/fixtures/svg/cyclo-C2H4I.k215.out.svg') }
+  let(:svg_file3_sanitized) { File.read('spec/fixtures/svg/oxirane-resine.k1.out.svg') }
+  let(:svg_file4_sanitized) { File.read('spec/fixtures/svg/oxirane.k218.out.svg') }
 
   describe 'scrub_xml' do
     let(:xml) { '<xml><script>alert("xss")</script></xml>' }
@@ -46,13 +46,15 @@ describe Chemotion::Sanitizer do
       expect(sanitizer.scrub_xml(xml)).to eq(xml)
     end
 
+    # rubocop:disable RSpec/MultipleExpectations
     it 'processes SVG files' do
-      expect(sanitizer.scrub_svg(svg_file_1)).to eq(svg_file_1_sanitized)
+      expect(sanitizer.scrub_svg(svg_file1)).to eq(svg_file1_sanitized)
       # NB stroke-miterlimit as style value is removed by the sanitizer but should be kept
-      expect(sanitizer.scrub_svg(svg_file_2)).to eq(svg_file_2_sanitized)
+      expect(sanitizer.scrub_svg(svg_file2)).to eq(svg_file2_sanitized)
       # NB rgba() as style value is removed by the sanitizer.
-      expect(sanitizer.scrub_svg(svg_file_3)).to eq(svg_file_3_sanitized)
-      expect(sanitizer.scrub_svg(svg_file_4)).to eq(svg_file_4_sanitized)
+      expect(sanitizer.scrub_svg(svg_file3)).to eq(svg_file3_sanitized)
+      expect(sanitizer.scrub_svg(svg_file4)).to eq(svg_file4_sanitized)
     end
+    # rubocop:enable RSpec/MultipleExpectations
   end
 end
