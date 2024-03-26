b2e1efff0e79254948e56a4a1d06879441be31f3
PiTrem <pierre.tremouilhac@kit.edu>
2024-03-26 16:38:33 +0100

    refactor: handle quill to plain text asynchronously in Reaction and Container for better performance on save
    TODO: use a better converter

--- app/models/container.rb
+++ app/models/container.rb
@@ -28,7 +28,8 @@ class Container < ApplicationRecord
   belongs_to :containable, polymorphic: true, optional: true
   has_many :attachments, as: :attachable
 
-  before_save :content_to_plain_text
+  around_save :content_to_plain_text,
+              if: -> { extended_metadata_changed? && extended_metadata && extended_metadata['content'].present? }
   # TODO: dependent destroy for attachments should be implemented when attachment get paranoidized instead of this DJ
   before_destroy :delete_attachment
   before_destroy :destroy_datasetable
@@ -71,13 +72,22 @@ class Container < ApplicationRecord
   # rubocop:disable Style/StringLiterals
 
   def content_to_plain_text
-    return unless extended_metadata_changed?
-    return if extended_metadata.blank? || (extended_metadata.present? && extended_metadata['content'].blank?)
+    yield
+    update_content_to_plain_text
+  end
 
+  # rubocop:disable Rails/SkipsModelValidations
+  def update_content_to_plain_text
     plain_text = Chemotion::QuillToPlainText.convert(extended_metadata['content'])
     return if plain_text.blank?
 
-    self.plain_text_content = plain_text
+    update_columns(plain_text_content: plain_text)
+  # we dont want to raise any error if the plain text content is not updated
+  rescue StandardError => e
+    Rails.logger.error("Error while converting content to plain text: #{e.message}")
   end
+  # rubocop:enable Rails/SkipsModelValidations
+
+  handle_asynchronously :update_content_to_plain_text, queue: 'plain_text_container_content'
   # rubocop:enable Style/StringLiterals
 end
