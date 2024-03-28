bc6ded0ed9bd8596e70225f0fd0a218e5fdab631
FabianMauz <fmauz@ipb-halle.de>
2024-03-28 12:15:06 +0000

    test: fixed test

--- spec/javascripts/packs/src/apps/mydb/elements/details/wellplates/propertiesTab/WellplateProperties.spec.js
+++ spec/javascripts/packs/src/apps/mydb/elements/details/wellplates/propertiesTab/WellplateProperties.spec.js
@@ -1,21 +1,13 @@
-/* global describe, context, it, beforeEach, afterEach */
+/* global describe, context, it */
 
 import React from 'react';
 import expect from 'expect';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-import _ from 'lodash';
-import {
-  ButtonToolbar, DropdownButton, MenuItem, OverlayTrigger, Popover, Button
-} from 'react-bootstrap';
-
-import QuillEditor from 'src/components/QuillEditor';
-
 import Wellplate from 'src/models/Wellplate';
 
 import WellplateProperties from 'src/apps/mydb/elements/details/wellplates/propertiesTab/WellplateProperties';
-import sinon from 'sinon';
 
 Enzyme.configure({
   adapter: new Adapter(),
@@ -24,18 +16,18 @@ Enzyme.configure({
 describe('WellplateProperties', async () => {
   describe('constructor()', async () => {
     context('when wellplate is of size 3x5', async () => {
+      const wellplate = Wellplate.buildEmpty(0, 2, 2);
+
       const wrapper = shallow(<WellplateProperties
-        wellplate={Wellplate.createEmpty()}
+        wellplate={wellplate}
         changeProperties={() => {}}
         handleAddReadout={() => {}}
         handleRemoveReadout={() => {}}
-        // name="MyWellPlate"
-        // description="MyDescription"
         readoutTitles={[]}
       />);
 
       it('component is correct initialized', async () => {
-        // console.log(wrapper.html());
+        expect(wrapper).not.toEqual(null);
       });
     });
   });
