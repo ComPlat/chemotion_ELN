cd2456f181c9dcfb2efaeba318996bf51e2401f1
PiTrem <pierre.tremouilhac@kit.edu>
2024-04-08 14:57:42 +0200

    fix svg scrubber

--- spec/lib/chemotion/sanitizer_spec.rb
+++ spec/lib/chemotion/sanitizer_spec.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+'spec/fixtures/svg/cyclo-C2H4I.cdjs.out.svg'# frozen_string_literal: true
 
 require 'rails_helper'
 
@@ -9,11 +9,15 @@ describe Chemotion::Sanitizer do
   let(:svg_file2) { File.read('spec/fixtures/svg/cyclo-C2H4I.k215.in.svg') }
   let(:svg_file3) { File.read('spec/fixtures/svg/oxirane-resine.k1.in.svg') }
   let(:svg_file4) { File.read('spec/fixtures/svg/oxirane.k218.in.svg') }
+  let(:svg_file5) { File.read('spec/fixtures/svg/cyclopropane.cdjs.in.svg') }
+  let(:svg_file6) { File.read('spec/fixtures/svg/cyclopropane.k215.in.svg') }
 
   let(:svg_file1_sanitized) { File.read('spec/fixtures/svg/cyclo-C2H4I.cdjs.out.svg') }
   let(:svg_file2_sanitized) { File.read('spec/fixtures/svg/cyclo-C2H4I.k215.out.svg') }
   let(:svg_file3_sanitized) { File.read('spec/fixtures/svg/oxirane-resine.k1.out.svg') }
   let(:svg_file4_sanitized) { File.read('spec/fixtures/svg/oxirane.k218.out.svg') }
+  let(:svg_file5_sanitized) { File.read('spec/fixtures/svg/cyclopropane.cdjs.out.svg') }
+  let(:svg_file6_sanitized) { File.read('spec/fixtures/svg/cyclopropane.k215.out.svg') }
 
   describe 'scrub_xml' do
     let(:xml) { '<xml><script>alert("xss")</script></xml>' }
@@ -46,15 +50,30 @@ describe Chemotion::Sanitizer do
       expect(sanitizer.scrub_xml(xml)).to eq(xml)
     end
 
-    # rubocop:disable RSpec/MultipleExpectations
-    it 'processes SVG files' do
+    it 'processes SVG files from cdjs' do
       expect(sanitizer.scrub_svg(svg_file1)).to eq(svg_file1_sanitized)
+    end
+
+    it 'processes SVG files from ketch 2.15' do
       # NB stroke-miterlimit as style value is removed by the sanitizer but should be kept
       expect(sanitizer.scrub_svg(svg_file2)).to eq(svg_file2_sanitized)
+    end
+
+    it 'processes SVG files ketch 1 with resine' do
       # NB rgba() as style value is removed by the sanitizer.
       expect(sanitizer.scrub_svg(svg_file3)).to eq(svg_file3_sanitized)
+    end
+
+    it 'processes SVG files from ketch 2.18' do
       expect(sanitizer.scrub_svg(svg_file4)).to eq(svg_file4_sanitized)
     end
-    # rubocop:enable RSpec/MultipleExpectations
+
+    it 'processes SVG files cdjs 2' do
+      expect(sanitizer.scrub_svg(svg_file5)).to eq(svg_file5_sanitized)
+    end
+
+    it 'processes SVG files ketch 2.15 2' do
+      expect(sanitizer.scrub_svg(svg_file6)).to eq(svg_file6_sanitized)
+    end
   end
 end
