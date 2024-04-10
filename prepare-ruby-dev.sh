06fcefeaddf0797fa0e58854d61adc157179316c
Sören Peters <dev.peters@icloud.com>
2024-04-10 15:02:50 +0000

    refactor: remove unused part as this is obsolet due to .tool-versions file

--- prepare-ruby-dev.sh
+++ prepare-ruby-dev.sh
@@ -9,21 +9,6 @@ else
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch $ASDF_BRANCH
 fi
 
-# if asdf plugins are not installed -> install
-if asdf plugin list | grep -Fxq 'ruby'; then
-    echo '>>> Ruby plugin for asdf is installed -> continue'
-else
-    echo '>>> Missing ruby plugin for asdf. Installing...'
-    asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-fi
-
-if asdf plugin list | grep -Fxq 'nodejs'; then
-    echo '>>> Nodejs plugin for asdf is installed -> continue'
-else
-    echo '>>> Missing nodejs plugin for asdf. Installing...'
-    asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-fi
-
 # install all tools as defined by .tool-versions
 echo '>>> Installing tool versions'
 asdf install
