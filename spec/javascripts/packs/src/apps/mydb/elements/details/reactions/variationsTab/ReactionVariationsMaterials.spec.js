61af77ea9ded0c4261f36230e5eca3dca3b61156
Jan C. Brammer <jan.c.brammer@gmail.com>
2024-04-18 13:58:59 +0000

    Test `ReactionVariationsMaterials`

--- spec/javascripts/packs/src/apps/mydb/elements/details/reactions/variationsTab/ReactionVariationsMaterials.spec.js
+++ spec/javascripts/packs/src/apps/mydb/elements/details/reactions/variationsTab/ReactionVariationsMaterials.spec.js
@@ -2,6 +2,7 @@ import expect from 'expect';
 import {
   getReactionMaterials, updateYields, updateEquivalents,
   removeObsoleteMaterialsFromVariations, addMissingMaterialsToVariations,
+  updateNonReferenceMaterialOnMassChange, EquivalentParser
 } from 'src/apps/mydb/elements/details/reactions/variationsTab/ReactionVariationsMaterials';
 import { setUpMaterial, setUpReaction } from 'helper/reactionVariationsHelpers';
 
@@ -47,7 +48,7 @@ describe('ReactionVariationsMaterials', () => {
     const updatedVariationsRow = updateYields(reaction.variations[0], reaction.hasPolymers());
     expect(updatedVariationsRow.products[productID].aux.yield).toBe(5);
   });
-  it("updates non-reference materials' equivalents when reference material's amount changes ", async () => {
+  it("updates non-reference materials' equivalents when reference material's amount changes", async () => {
     const reaction = await setUpReaction();
     const reactantID = reaction.reactants[0].id;
     expect(reaction.variations[0].reactants[reactantID].aux.equivalent).toBe(1);
@@ -58,5 +59,45 @@ describe('ReactionVariationsMaterials', () => {
     });
     const updatedVariationsRow = updateEquivalents(reaction.variations[0]);
     expect(updatedVariationsRow.reactants[reactantID].aux.equivalent).toBeCloseTo(50, 0.01);
+  });
+  it("updates non-reference materials' equivalents when own amount changes", async () => {
+    const reaction = await setUpReaction();
+    const reactant = Object.values(reaction.variations[0].reactants)[0];
+    reactant.value *= 0.42;
+    const updatedReactant = updateNonReferenceMaterialOnMassChange(
+      reaction.variations[0],
+      reactant,
+      'reactants',
+      false
+    );
+    expect(reactant.aux.equivalent).toBeGreaterThan(updatedReactant.aux.equivalent);
+  });
+  it("updates non-reference materials' yields when own amount changes", async () => {
+    const reaction = await setUpReaction();
+    const product = Object.values(reaction.variations[0].products)[0];
+    product.value *= 0.042;
+    const updatedProduct = updateNonReferenceMaterialOnMassChange(
+      reaction.variations[0],
+      product,
+      'products',
+      true
+    );
+    expect(product.aux.yield).toBeGreaterThan(updatedProduct.aux.yield);
+  });
+  it("updates materials' amount when equivalent changes", async () => {
+    const reaction = await setUpReaction();
+    const variationsRow = reaction.variations[0];
+    const reactant = variationsRow.reactants[Object.keys(variationsRow.reactants)[0]];
+    const updatedReactant = EquivalentParser({
+      data: variationsRow,
+      oldValue: reactant,
+      newValue: reactant.aux.equivalent * 0.42
+    });
+    expect(reactant.value).toBeGreaterThan(updatedReactant.value);
+    expect(EquivalentParser({
+      data: variationsRow,
+      oldValue: reactant,
+      newValue: -42
+    }).value).toBe(0);
   });
 });
