697a169e81e988f7149f55ba30364a476550d129
Chia-Lin Lin <chia-lin.lin@kit.edu>
2024-05-07 12:23:32 +0200

    feat: cdjs v23 compatibility (#1894)
    also add better notification on editor loading error
    
    Co-authored-by: Pei Chi Huang <pei-chi.huang@kit.edu>

--- app/packs/src/components/structureEditor/KetcherEditor.js
+++ app/packs/src/components/structureEditor/KetcherEditor.js
@@ -3,26 +3,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const KetcherEditor = (props) => {
+function KetcherEditor(props) {
   const {
-    editor, fh, fs, molfile
+    editor, iH, iS, molfile
   } = props;
-  const initMol = molfile || '\n  noname\n\n  0  0  0  0  0  0  0  0  0  0999 V2000\nM  END\n';
+  const initMol = molfile
+    || '\n  noname\n\n  0  0  0  0  0  0  0  0  0  0999 V2000\nM  END\n';
   if (editor && editor.structureDef && editor.structureDef.editor) {
     editor.structureDef.editor.setMolecule(initMol);
   }
   return (
     <div>
-      <iframe id={editor.id} src={editor.extSrc} title={editor.label} height={fh} width="100%" style={fs} />
+      <iframe id={editor.id} src={editor.extSrc} title={editor.label} height={iH} width="100%" style={iS} />
     </div>
   );
-};
+}
 
 KetcherEditor.propTypes = {
   molfile: PropTypes.string,
   editor: PropTypes.object.isRequired,
-  fh: PropTypes.string.isRequired,
-  fs: PropTypes.object.isRequired
+  iH: PropTypes.string.isRequired,
+  iS: PropTypes.object.isRequired
 };
 
 export default KetcherEditor;
