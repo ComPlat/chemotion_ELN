e2d34805f7eb8dcaf33ce27373f444ccc5e34f04
adambasha0 <adambasha1992@gmail.com>
2024-05-08 10:41:01 +0000

    feat: add gas phase to reactions and reactions_samples tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2024_06_25_105013) do
+=======
+ActiveRecord::Schema.define(version: 2024_04_25_144934) do
+>>>>>>> 782a86dfa (feat: add gas phase to reactions and reactions_samples tables)
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -956,6 +960,7 @@ ActiveRecord::Schema.define(version: 2024_06_25_105013) do
     t.text "plain_text_description"
     t.text "plain_text_observation"
     t.jsonb "variations", default: []
+    t.boolean "gaseous", default: false
     t.index ["deleted_at"], name: "index_reactions_on_deleted_at"
     t.index ["rinchi_short_key"], name: "index_reactions_on_rinchi_short_key", order: :desc
     t.index ["rinchi_web_key"], name: "index_reactions_on_rinchi_web_key"
@@ -974,6 +979,9 @@ ActiveRecord::Schema.define(version: 2024_06_25_105013) do
     t.boolean "waste", default: false
     t.float "coefficient", default: 1.0
     t.boolean "show_label", default: false, null: false
+    t.boolean "feedstock_gas_reference", default: false
+    t.boolean "gas", default: false
+    t.jsonb "gas_phase_data", default: {"time"=>{"unit"=>"h", "value"=>nil}, "temperature"=>{"unit"=>"°C", "value"=>nil}, "turnover_number"=>nil, "part_per_million"=>nil, "turnover_frequency"=>{"unit"=>"per hour", "value"=>nil}}
     t.index ["reaction_id"], name: "index_reactions_samples_on_reaction_id"
     t.index ["sample_id"], name: "index_reactions_samples_on_sample_id"
   end
@@ -1715,8 +1723,8 @@ ActiveRecord::Schema.define(version: 2024_06_25_105013) do
        RETURNS TABLE(literatures text)
        LANGUAGE sql
       AS $function$
-         select string_agg(l2.id::text, ',') as literatures from literals l , literatures l2 
-         where l.literature_id = l2.id 
+         select string_agg(l2.id::text, ',') as literatures from literals l , literatures l2
+         where l.literature_id = l2.id
          and l.element_type = $1 and l.element_id = $2
        $function$
   SQL
