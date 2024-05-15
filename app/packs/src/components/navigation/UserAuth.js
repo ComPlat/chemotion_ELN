551b372587d682f9da358943819d7f9c997af456
Ashwini <amurthy.ashwini@gmail.com>
2024-05-15 12:18:51 +0000

    feat: frontend implementation of affiliations

--- app/packs/src/components/navigation/UserAuth.js
+++ app/packs/src/components/navigation/UserAuth.js
@@ -29,6 +29,7 @@ import NotificationActions from 'src/stores/alt/actions/NotificationActions';
 import { UserLabelModal } from 'src/components/UserLabels';
 import GroupElement from 'src/components/navigation/GroupElement';
 import { formatDate } from 'src/utilities/timezoneHelper';
+import Affiliations from '../../apps/userSettings/Affiliations';
 
 export default class UserAuth extends Component {
   constructor(props) {
@@ -41,6 +42,7 @@ export default class UserAuth extends Component {
       currentDevices: [],
       selectedUsers: null,
       showSubscription: false,
+      showAffiliations: false,
       currentSubscriptions: [],
       showDeviceMetadataModal: false,
       device: {},
@@ -58,6 +60,9 @@ export default class UserAuth extends Component {
     this.handleSubscriptionClose = this.handleSubscriptionClose.bind(this);
     this.handleDeviceMetadataModalShow = this.handleDeviceMetadataModalShow.bind(this);
     this.handleDeviceMetadataModalClose = this.handleDeviceMetadataModalClose.bind(this);
+    this.handleAffiliationsShow = this.handleAffiliationsShow.bind(this);
+    this.handleAffiliationsHide = this.handleAffiliationsHide.bind(this);
+    this.renderAffiliations = this.renderAffiliations.bind(this);
 
     this.promptTextCreator = this.promptTextCreator.bind(this);
 
@@ -299,6 +304,22 @@ export default class UserAuth extends Component {
       this.setState({ currentGroups });
     });
   };
+
+  handleAffiliationsShow() {
+    this.setState({ showAffiliations: true });
+  }
+  handleAffiliationsHide = () => {
+    this.setState({ showAffiliations: false });
+  };
+
+  renderAffiliations() {
+    return (
+      <Affiliations
+        show={this.state.showAffiliations}
+        onHide={this.handleAffiliationsHide} />
+    );
+
+  }
 
   // render modal
   renderModal() {
@@ -669,6 +690,10 @@ export default class UserAuth extends Component {
             <MenuItem eventKey="5" href="/pages/affiliations">
               My Affiliations
             </MenuItem>
+            <MenuItem
+              onClick={this.handleAffiliationsShow}>
+              New Affiliations
+            </MenuItem>
             <MenuItem onClick={this.handleShow}>My Groups & Devices</MenuItem>
             <MenuItem onClick={this.handleLabelShow}>My Labels</MenuItem>
             {/* <MenuItem onClick={this.handleSubscriptionShow}>My Subscriptions</MenuItem>
@@ -692,6 +717,7 @@ export default class UserAuth extends Component {
           </NavItem>
         </Nav>
         {this.renderModal()}
+        {this.renderAffiliations()}
         <UserLabelModal
           showLabelModal={this.state.showLabelModal}
           onHide={() => this.handleLabelClose()}
