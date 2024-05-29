efbbd0466c8e0487241059d17395f591251ee43c
Mehreen <mehreen.mansur@gmail.com>
2024-05-29 10:56:32 +0200

    update for remove from collection issue [1033]

--- app/models/collections_sample.rb
+++ app/models/collections_sample.rb
@@ -25,7 +25,7 @@ class CollectionsSample < ApplicationRecord
 
   def self.remove_in_collection(element_ids, collection_ids)
     # Remove from collections
-    error_message = delete_in_collection_with_filter(element_ids, collection_ids)
+    error_message = delete_in_collection_without_association(element_ids, collection_ids)
     return error_message unless error_message.nil?
 
     # update sample tag with collection info
@@ -33,17 +33,19 @@ class CollectionsSample < ApplicationRecord
   end
 
   # prevent removing sample from a collection if associated wellplate or reaction is present
-  def self.delete_in_collection_with_filter(sample_ids, collection_ids)
+  def self.delete_in_collection_without_association(sample_ids, collection_ids)
     [collection_ids].flatten.each do |cid|
       next unless cid.is_a?(Integer)
       # TODO: sql function
       # from a collection, select sample_ids with neither wellplate nor reaction associated
       sample_reaction_ids = fetch_sample_ids('reaction', 'reactions_samples', cid, sample_ids)
-      message = generate_error_message('reaction') if sample_reaction_ids.empty?
+      associated_sample_ids = sample_ids - sample_reaction_ids
+      message = generate_error_message(associated_sample_ids, 'reaction') if associated_sample_ids.present?
       return message unless message.nil?
 
       sample_wellplate_ids = fetch_sample_ids('wellplate', 'wells', cid, sample_ids)
-      message = generate_error_message('wellplate') if sample_wellplate_ids.empty?
+      associated_sample_ids = sample_ids - sample_wellplate_ids
+      message = generate_error_message(associated_sample_ids, 'wellplate') if sample_wellplate_ids.present?
       return message unless message.nil?
 
       ids = sample_wellplate_ids + sample_reaction_ids
@@ -51,13 +53,13 @@ class CollectionsSample < ApplicationRecord
     end
   end
 
-  def self.fetch_sample_ids(elementKlass, element, cid, sample_ids)
+  def self.fetch_sample_ids(element_class, element, cid, sample_ids)
     CollectionsSample.joins(
       <<~SQL
         left join #{element} el
         on el.sample_id = collections_samples.sample_id and el.deleted_at is null
-        left join collections_#{elementKlass}s c
-        on c.collection_id = #{cid} and c.#{elementKlass}_id = el.#{elementKlass}_id and c.deleted_at is null
+        left join collections_#{element_class}s c
+        on c.collection_id = #{cid} and c.#{element_class}_id = el.#{element_class}_id and c.deleted_at is null
       SQL
       ).where(
         "collections_samples.collection_id = #{cid} and collections_samples.sample_id in (?) and c.id is null",
@@ -65,8 +67,16 @@ class CollectionsSample < ApplicationRecord
       ).pluck(:sample_id)
   end
 
-  def self.generate_error_message element
-    { error: "Sample cannot be deleted due to associated #{element}." }
+  def self.generate_error_message(sample_ids, element)
+    labels = Sample.where(id: sample_ids).pluck(:short_label)
+    if labels.count > 3
+      labels = labels.first(3)
+      labels << '...'
+    end
+
+    { error: "Some samples #{labels} cannot be removed because they are part of a #{element}.\n
+              Delete the associated #{element}s or remove the samples from the #{element}s."
+    }
   end
 
   def self.create_in_collection(element_ids, collection_ids)
@@ -77,7 +87,7 @@ class CollectionsSample < ApplicationRecord
 
   def self.move_to_collection(element_ids, from_col_ids, to_col_ids)
     # Delete in collection
-    delete_in_collection_with_filter(element_ids, from_col_ids)
+    delete_in_collection_without_association(element_ids, from_col_ids)
     # Upsert in target collection
     insert_in_collection(element_ids, to_col_ids)
     # Update element tag with collection info
