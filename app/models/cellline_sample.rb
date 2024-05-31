aef1d292f3217d402cb36ab0efc843295592abbc
FabianMauz <fmauz@ipb-halle.de>
2024-05-31 09:19:17 +0000

    feat: add root container if not present after creation

--- app/models/cellline_sample.rb
+++ app/models/cellline_sample.rb
@@ -37,6 +37,8 @@ class CelllineSample < ApplicationRecord
 
   has_many :sync_collections_users, through: :collections
 
+  after_create :create_root_container
+
   scope :by_sample_name, lambda { |query, collection_id|
                            joins(:collections).where(collections: { id: collection_id })
                                               .where('name ILIKE ?', "%#{sanitize_sql_like(query)}%")
@@ -48,5 +50,9 @@ class CelllineSample < ApplicationRecord
       .where('collections.id=?', collection_id)
       .where('cellline_materials.name ILIKE ?', "%#{sanitize_sql_like(query)}%")
   }
+
+  def create_root_container
+    self.container = Container.create_root_container if container.nil?
+  end
 end
 # rubocop:enable Rails/InverseOf, Rails/HasManyOrHasOneDependent
