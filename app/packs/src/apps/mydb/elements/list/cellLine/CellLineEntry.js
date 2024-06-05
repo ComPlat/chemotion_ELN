543734daeef38db8769204b571ad89134fa97fff
Joachim Tillessen <tillessen@emjot.de>
2024-06-05 19:27:32 +0200

    replace glyphicon chevrons with font-awesome based component

--- app/packs/src/apps/mydb/elements/list/cellLine/CellLineEntry.js
+++ app/packs/src/apps/mydb/elements/list/cellLine/CellLineEntry.js
@@ -6,6 +6,7 @@ import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { elementShowOrNew } from 'src/utilities/routesUtils';
 import { CellLinePropTypeTableEntry } from 'src/models/cellLine/CellLinePropTypes';
 import Aviator from 'aviator';
+import ChevronIcon from 'src/components/common/ChevronIcon';
 
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
@@ -56,10 +57,9 @@ export default class CellLineEntry extends Component {
 
   renderArrow() {
     const { showEntries } = this.state;
-    const arrowType = showEntries ? 'glyphicon-chevron-right' : 'glyphicon-chevron-down';
     return (
       <div key="arrow" className="cell-line-group-arrow floating-right">
-        <i className={`glyphicon ${arrowType}`} />
+        <ChevronIcon direction={showEntries ? 'down' : 'right'} />
       </div>
 
     );
