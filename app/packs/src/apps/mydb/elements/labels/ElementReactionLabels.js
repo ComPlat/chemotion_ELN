9f88278ba969f83ce27e3879b1673386d6c6ed57
Joachim Tillessen <tillessen@emjot.de>
2024-06-07 16:07:05 +0200

    Format icons in ElementTableSampleEntries

--- app/packs/src/apps/mydb/elements/labels/ElementReactionLabels.js
+++ app/packs/src/apps/mydb/elements/labels/ElementReactionLabels.js
@@ -3,7 +3,6 @@ import { Modal, Button } from 'react-bootstrap';
 
 import ElementActions from 'src/stores/alt/actions/ElementActions';
 import ElementStore from 'src/stores/alt/stores/ElementStore';
-import Label from 'src/components/legacyBootstrap/Label'
 
 export default class ElementReactionLabels extends React.Component {
   constructor(props) {
@@ -54,35 +53,25 @@ export default class ElementReactionLabels extends React.Component {
 
     if (!element.tag || !element.tag.taggable_data ||
         !element.tag.taggable_data.reaction_id)
-      return (<span />);
+      return null;
 
     const { showWarning, clicked } = this.state;
 
-    const reaction = <i className="icon-reaction" />;
-
-
     return (
-      <div style={{ display: 'inline-block', marginTop: '-5px' }}>
-        <div onClick={this.handleOnClick}>
-          <span className="collection-label" key={element.id}>
-            <Label>{reaction}</Label>
-          </span>
-        </div>
-        {/* <div style={{clear: 'both'}} /> */}
-        <div className="center">
-          <Modal show={showWarning && clicked} onHide={this.closeWarning}>
-            <Modal.Header closeButton>
-              <Modal.Title>No Access to Element</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              Sorry, you cannot access this Reaction.
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={this.closeWarning}>Close</Button>
-            </Modal.Footer>
-          </Modal>
-        </div>
-      </div>
+      <>
+        <i className="icon-reaction" onClick={this.handleOnClick} key={element.id}/>
+        <Modal show={clicked} onHide={this.closeWarning}>
+          <Modal.Header closeButton>
+            <Modal.Title>No Access to Element</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Sorry, you cannot access this Reaction.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeWarning}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     )
   }
 }
