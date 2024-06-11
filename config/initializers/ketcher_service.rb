03482579a70bda00aaaf3554dcc4279879943437
PiTrem <pierre.tremouilhac@kit.edu>
2024-06-11 11:14:33 +0200

    refactor: structure_editors initializers
      - ensure  Rails.application.structure_editors is defined and an instance
        of ActiveSupport::OrderedOptions, when config/structure_editors.yml:
            -- is missing or
            -- is present but nothing defined for the current environment
    
    refactor: ketcher service initializers
    
        - ensure  Rails.application.ketcher_service is defined and an instance
        of ActiveSupport::OrderedOptions, when config/ketcher_service.yml:
        -- is missing
        -- is present but nothing defined for the current environment
    
        - warn if config/ketcher_service.yml present but no :url is defined
        (would suggest the config file is obsolete)

--- config/initializers/ketcher_service.rb
+++ config/initializers/ketcher_service.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
-if File.exist? Rails.root.join('config', 'ketcher_service.yml')
-  ketcher_svc_config = Rails.application.config_for(:ketcher_service)
-  url = ketcher_svc_config[:url]
+# This load the optional configuration for the backend ketcher rendering service
 
-  Rails.application.configure do
-    config.ketcher_service = ActiveSupport::OrderedOptions.new
-    config.ketcher_service.url = url
+ref = "Initializing #{File.basename(__FILE__, '.rb')}:"
+
+Rails.application.configure do
+  config.ketcher_service = nil                           # Create service config
+  config.ketcher_service = config_for :ketcher_service   # Load config/.yml
+rescue RuntimeError                                      # Rescue: RuntimeError is raised if the file is not found
+  Rails.logger.info "#{ref} yml configuration not found"
+ensure
+  # Load default missing configuration if ketcher_service.yml not found or no config is defined for the environment
+  config.ketcher_service ||= config_for :default_missing
+
+  if (info = config.ketcher_service.url || config.ketcher_service.desc)
+    Rails.logger.info "#{ref} Ketcher-render service: #{info}"
+  else
+    Rails.logger.warn "#{ref} Ketcher render service: configuration found but no url defined"
   end
-
-  Rails.logger.info("Render service configured at: #{Rails.configuration.try(:ketcher_service).try(:url)}")
 end
--- config/initializers/structure_editors.rb
+++ config/initializers/structure_editors.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
-structure_editors_config = {}
 
-if File.exist? Rails.root.join('config', 'structure_editors.yml')
-  structure_editors_config = Rails.application.config_for :structure_editors
-end
+# This load the optional configuration for additional structure editors.
+
+ref = "Initializing #{File.basename(__FILE__, '.rb')}:"
 
 Rails.application.configure do
-  config.structure_editors = ActiveSupport::OrderedOptions.new
-  config.structure_editors.editors = structure_editors_config[:editors]
+  config.structure_editors = nil                            # Create service
+  config.structure_editors = config_for :structure_editors  # Load config/.yml
+rescue RuntimeError                                         # Rescue if the yml file is not found
+  Rails.logger.info "#{ref} yml configuration not found"
+ensure
+  # Load default missing configuration if ketcher_service.yml not found or no config is defined for the environment
+  config.structure_editors ||= config_for :default_missing
+
+  info = config.structure_editors.editors&.size || config.structure_editors.desc
+  Rails.logger.info "#{ref} optional editors: #{info}"
 end
