8f81b315ae0dfc2354cde09045aa82d48b48c061
Jan C. Brammer <jan.c.brammer@gmail.com>
2024-06-25 15:12:11 +0200

    refactor: reaction variations table db migration (#1987)
    squash migrations
    
    - #1966: Add missing `notes` and `analyses` entries.
    - #1980: Also, for each material,  restructure `mass` entry and add missing `amount` and `volume` entries.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_24_120634) do
+ActiveRecord::Schema.define(version: 2024_06_25_105013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
