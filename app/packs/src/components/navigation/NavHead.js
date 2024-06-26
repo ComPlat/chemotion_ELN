9ce88c2eb865fff07d502ad0293300b447aa67b7
Luciano Maiwald <mail@luciano-maiwald.de>
2024-06-26 11:04:21 +0200

    Add NavHead back to navigation, update MenuItem references

--- app/packs/src/components/navigation/NavHead.js
+++ app/packs/src/components/navigation/NavHead.js
@@ -7,40 +7,39 @@ function NavHead() {
   return (
     <Navbar.Brand>
       <NavDropdown title="Chemotion" className="navig-brand" id="bg-nested-dropdown-brand">
-        <MenuItem eventKey="11" href="https://chemotion.net/docs/eln/ui" target="_blank">
+        <NavDropdown.Item eventKey="11" href="https://chemotion.net/docs/eln/ui" target="_blank">
           Documentation
           <i className="fa fa-external-link" style={{ float: 'right' }} aria-hidden />
-        </MenuItem>
-        <MenuItem eventKey="14" href="https://chemotion.net/search" target="_blank">
+        </NavDropdown.Item>
+        <NavDropdown.Item eventKey="14" href="https://chemotion.net/search" target="_blank">
           Search documentation
           <i className="fa fa-external-link" style={{ float: 'right' }} aria-hidden />
-        </MenuItem>
-        <MenuItem eventKey="11" href="https://chemotion.net/helpdesk" target="_blank">
+        </NavDropdown.Item>
+        <NavDropdown.Item eventKey="11" href="https://chemotion.net/helpdesk" target="_blank">
           Helpdesk - Contact Us
           <i className="fa fa-external-link" style={{ float: 'right' }} aria-hidden />
-        </MenuItem>
-        <MenuItem eventKey="14" href="https://github.com/ComPlat/chemotion_ELN/issues" target="_blank">
+        </NavDropdown.Item>
+        <NavDropdown.Item eventKey="14" href="https://github.com/ComPlat/chemotion_ELN/issues" target="_blank">
           Report an issue on Github
           <i className="fa fa-external-link" style={{ float: 'right' }} aria-hidden />
-        </MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey="15" href="https://www.chemotion.net" target="_blank">
+        </NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item eventKey="15" href="https://www.chemotion.net" target="_blank">
           Chemotion.net
           <i className="fa fa-external-link " style={{ float: 'right' }} aria-hidden />
-        </MenuItem>
-        <MenuItem eventKey="15" href="https://www.chemotion-repository.net" target="_blank">
+        </NavDropdown.Item>
+        <NavDropdown.Item eventKey="15" href="https://www.chemotion-repository.net" target="_blank">
           Chemotion-Repository.net
           {' '}
-          {' '}
           <i className="fa fa-external-link " aria-hidden />
-        </MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey="16" href={isOnMydb ? '/home' : '/mydb'} target="_self">
+        </NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item eventKey="16" href={isOnMydb ? '/home' : '/mydb'} target="_self">
           {isOnMydb ? 'Home' : 'ELN'}
-        </MenuItem>
-        <MenuItem eventKey="17" href="/about" target="_self">
+        </NavDropdown.Item>
+        <NavDropdown.Item eventKey="17" href="/about" target="_self">
           About
-        </MenuItem>
+        </NavDropdown.Item>
       </NavDropdown>
     </Navbar.Brand>
   );
