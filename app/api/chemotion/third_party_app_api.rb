c2e6bf25e55d1218c62246fe7f4041856bd54322
Hadi Tariq <hadi-tariq@live.co.uk>
2024-07-10 14:22:18 +0200

    Tpa create new migration for file_types (#2011)
    * fixup: migration for TPA file_types
    
     dev: recreate the table rake db:migrate:redo VERSION=20230213102539
    
     dev: keep filtypes optional for now
    
    ---------
    
    Co-authored-by: PiTrem <pierre.tremouilhac@kit.edu>

--- app/api/chemotion/third_party_app_api.rb
+++ app/api/chemotion/third_party_app_api.rb
@@ -125,15 +125,15 @@ module Chemotion
         after_validation do
           params[:name]&.strip!
           params[:url]&.strip!
-          params[:file_types]&.each(&:strip!) if params[:file_types].present?
+          params[:file_types]&.strip!
         end
 
         desc 'create new third party app entry'
         params do
-          requires :file_types, type: Array[String], desc: 'Array of file types which can be handled by the app.'
           requires :url, type: String, allow_blank: false, desc: 'The url in order to redirect to the app.'
           requires :name, type: String, allow_blank: false,
                           desc: 'name of third party app. User will chose correct app based on names.'
+          optional :file_types, type: String, desc: 'comma separated mime-types'
         end
 
         rescue_from ActiveRecord::RecordInvalid do |e|
@@ -148,9 +148,9 @@ module Chemotion
         route_param :id, type: Integer, desc: '3rd party app id' do
           desc 'update a third party app entry'
           params do
-            requires :file_types, type: Array[String], desc: 'Array of file types which can be handled by the app.'
             optional :url, type: String, allow_blank: false, desc: 'The url where the 3rd party app lives.'
             optional :name, type: String, allow_blank: false, desc: 'Name of third party app.'
+            optional :file_types, type: String, desc: 'comma separated mime-types'
           end
 
           put do
