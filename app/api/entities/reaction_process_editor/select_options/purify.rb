341e4480cbe8536c42666bd65e3e7ec666481360
Christian Buggle <christian@buggle.net>
2024-07-15 14:32:28 +0200

    Rewrite SelectOptions as Singleton
    * Include Singleton in SelectOptions::Base.
    * Change to instance methods (was: class methods) in all SelectOptions classes.
    * Add SelectOptions::Remove.

--- app/api/entities/reaction_process_editor/select_options/purify.rb
+++ app/api/entities/reaction_process_editor/select_options/purify.rb
@@ -4,62 +4,70 @@ module Entities
   module ReactionProcessEditor
     module SelectOptions
       class Purify < Base
-        def self.select_options(reaction_process)
+        def select_options(reaction_process)
           { CRYSTALLIZATION: {
               modes: crystallization_modes,
-              automation_modes: SelectOptions::Custom.automation_modes,
+              automation_modes: SelectOptions::Custom.instance.automation_modes,
               solvent_options: material_options(reaction_process)['CRYSTALLIZATION'],
             },
             EXTRACTION: {
-              automation_modes: SelectOptions::Custom.automation_modes,
+              automation_modes: SelectOptions::Custom.instance.automation_modes,
               phases: extraction_phases,
               solvent_options: material_options(reaction_process)['EXTRACTION'],
             },
             FILTRATION: {
-              automation_modes: SelectOptions::Custom.automation_modes,
+              automation_modes: SelectOptions::Custom.instance.automation_modes,
               modes: filtration_modes,
               solvent_options: material_options(reaction_process)['FILTRATION'],
             },
-            CHROMATOGRAPHY: SelectOptions::Chromatography.select_options.merge(
-              automation_modes: SelectOptions::Chromatography.automation_modes,
+            CHROMATOGRAPHY: SelectOptions::Chromatography.instance.select_options.merge(
               solvent_options: material_options(reaction_process)['CHROMATOGRAPHY'],
             ) }
         end
 
-        def self.material_options(reaction_process)
-          reaction = reaction_process.reaction
-
-          solvents = (reaction.solvents + reaction.purification_solvents).uniq
-          diverse_solvents = Medium::DiverseSolvent.all
-
-          solvent_options = sample_purify_options(solvents,
-                                                  'SOLVENT') + sample_purify_options(diverse_solvents,
-                                                                                     'DIVERSE_SOLVENT')
-          chromatography_options = sample_purify_options(Medium::Modifier.all, 'MODIFIER') +
-                                   sample_purify_options(diverse_solvents, 'DIVERSE_SOLVENT')
-
+        def material_options(reaction_process)
           {
-            CRYSTALLIZATION: sample_purify_options(Medium::Additive.all, 'ADDITIVE'),
+            CRYSTALLIZATION: crystallization_options,
             CHROMATOGRAPHY: chromatography_options,
-            EXTRACTION: solvent_options,
-            FILTRATION: solvent_options,
+            EXTRACTION: solvent_options(reaction_process),
+            FILTRATION: solvent_options(reaction_process),
           }.deep_stringify_keys
         end
 
-        def self.crystallization_modes
+        def crystallization_modes
           [{ value: 'NONE', label: 'None' },
            { value: 'COLD', label: 'Cold' },
            { value: 'HOT', label: 'Hot' }]
         end
 
-        def self.extraction_phases
+        def extraction_phases
           [{ value: 'AQUEOUS', label: 'Aqueous' },
            { value: 'ORGANIC', label: 'Organic' }]
         end
 
-        def self.filtration_modes
+        def filtration_modes
           [{ value: 'KEEP_SUPERNATANT', label: 'Supernatant' },
            { value: 'KEEP_PRECIPITATE', label: 'Precipitate' }]
+        end
+
+        private
+
+        def solvent_options(reaction_process)
+          reaction = reaction_process.reaction
+
+          solvents = (reaction.solvents + reaction.purification_solvents).uniq
+
+          sample_minimal_options(solvents,
+                                 'SOLVENT') + sample_minimal_options(Medium::DiverseSolvent.all, 'DIVERSE_SOLVENT')
+        end
+
+        def chromatography_options
+          sample_minimal_options(Medium::Modifier.all,
+                                 'MODIFIER') + sample_minimal_options(Medium::DiverseSolvent.all, 'DIVERSE_SOLVENT')
+        end
+
+        def crystallization_options
+          sample_minimal_options(Medium::Additive.all, 'ADDITIVE')
         end
       end
     end
--- app/api/entities/reaction_process_editor/select_options/reaction_process.rb
+++ app/api/entities/reaction_process_editor/select_options/reaction_process.rb
@@ -4,36 +4,37 @@ module Entities
   module ReactionProcessEditor
     module SelectOptions
       class ReactionProcess < Base
-        def self.all(reaction_process)
+        def all(reaction_process)
           {
             samples_preparations: sample_preparation_options(reaction_process),
-            vessel_preparations: SelectOptions::Vessels.preparations,
+            vessel_preparations: SelectOptions::Vessels.instance.preparations,
             step_name_suggestions: step_name_suggestions(reaction_process),
-            activity_type_equipment: SelectOptions::Equipment.per_activity_type,
-            purify: SelectOptions::Purify.select_options(reaction_process),
-            condition_additional_information: SelectOptions::Conditions.additional_information,
-            addition_speed_types: SelectOptions::Custom.addition_speed_types,
-            materials: SelectOptions::Materials.all_for(reaction_process),
-            equipment: SelectOptions::Equipment.all,
-            automation_modes: SelectOptions::Custom.automation_modes,
-            motion_types: SelectOptions::Custom.motion_types,
-            remove_sample_types: SelectOptions::Samples.remove_sample_types,
-            save_sample_types: SelectOptions::Samples.save_sample_types,
-            save_sample_origin_types: SelectOptions::Samples.save_sample_origin_types,
-            analysis_types: SelectOptions::Custom.analysis_types,
+            activity_type_equipment: SelectOptions::Equipment.instance.per_activity_type,
+            purify: SelectOptions::Purify.instance.select_options(reaction_process),
+            condition_additional_information: SelectOptions::Conditions.instance.additional_information,
+            addition_speed_types: SelectOptions::Custom.instance.addition_speed_types,
+            materials: SelectOptions::Materials.instance.all_for(reaction_process),
+            equipment: SelectOptions::Equipment.instance.all,
+            automation_modes: SelectOptions::Custom.instance.automation_modes,
+            motion_types: SelectOptions::Custom.instance.motion_types,
+            remove_sample_types: SelectOptions::Samples.instance.remove_sample_types,
+            remove_origin_types: SelectOptions::Remove.instance.origin_types,
+            save_sample_types: SelectOptions::Samples.instance.save_sample_types,
+            save_sample_origin_types: SelectOptions::Samples.instance.save_sample_origin_types,
+            analysis_types: SelectOptions::Custom.instance.analysis_types,
           }
         end
 
-        def self.sample_preparation_options(reaction_process)
+        def sample_preparation_options(reaction_process)
           {
             prepared_samples: samples_info_options(prepared_samples(reaction_process), 'SAMPLE'),
             unprepared_samples: samples_info_options(unprepared_samples(reaction_process), 'SAMPLE'),
-            equipment: SelectOptions::Equipment.all,
-            preparation_types: SelectOptions::Samples.preparation_types,
+            equipment: SelectOptions::Equipment.instance.all,
+            preparation_types: SelectOptions::Samples.instance.preparation_types,
           }
         end
 
-        def self.preparable_samples(reaction_process)
+        def preparable_samples(reaction_process)
           reaction = reaction_process.reaction
 
           (reaction.reactions_starting_material_samples +
@@ -44,15 +45,15 @@ module Entities
           reaction.reactions_intermediate_samples).map(&:sample).uniq
         end
 
-        def self.prepared_samples(reaction_process)
+        def prepared_samples(reaction_process)
           reaction_process.samples_preparations.order(:created_at).includes(sample: [:molecule]).map(&:sample)
         end
 
-        def self.unprepared_samples(reaction_process)
+        def unprepared_samples(reaction_process)
           preparable_samples(reaction_process) - prepared_samples(reaction_process)
         end
 
-        def self.step_name_suggestions(reaction_process)
+        def step_name_suggestions(reaction_process)
           reaction_ids = Reaction.where(creator: reaction_process.creator).ids
 
           procedure_ids = ::ReactionProcessEditor::ReactionProcess.where(reaction_id: reaction_ids).ids
