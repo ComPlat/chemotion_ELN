0a81626184cc9bc7e810fd54aa043d9b6a1f77b2
Beate Quednau <bq@megorei.com>
2024-07-25 14:16:16 +0200

    Fix admin device for new bootstrap (#2038)
    * Fix styles for Admin device list
    
    * Fix basic styles for admin device modal
    
    * Fix device modal
    
    * Fix scrollable content height

--- app/packs/src/apps/admin/devices/DeviceModal.js
+++ app/packs/src/apps/admin/devices/DeviceModal.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { Modal, Button, ButtonToolbar, Alert, Tabs, Tab } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Modal, Button, ButtonToolbar, Alert, Tabs, Tab, Stack } from 'react-bootstrap';
 import Draggable from "react-draggable";
 import DevicePropertiesTab from './DevicePropertiesTab';
 import DeviceUserGroupsTab from './DeviceUserGroupsTab';
@@ -16,11 +16,11 @@ const DeviceModal = () => {
   const deviceMetadataStore = useContext(StoreContext).deviceMetadata;
   let device = devicesStore.device;
   const disableTab = devicesStore.create_or_update == 'update' ? false : true;
+  const [deltaPosition, setDeltaPosition] = useState({ x: 0, y: 0 });
 
   const errorMessage = devicesStore.error_message + deviceMetadataStore.error_message;
   const successMessage = devicesStore.success_message + deviceMetadataStore.success_message;
   const withAlertClass = errorMessage || successMessage ? 'with-alert' : '';
-  const buttonsWithAlertClass = errorMessage ? 'with-alert' : '';
 
   const minimizedClass = devicesStore.modalMinimized ? ' minimized' : '';
   let deviceParams = {};
@@ -66,36 +66,36 @@ const DeviceModal = () => {
   const handleValidationState = () => {
     let errorMessages = ['Please fill out all needed fields'];
 
-    const nameValue = device.name.trim() === '' ? 'error' : null;
+    const nameValue = device.name.trim() === '' ? 'is-invalid' : '';
     if (nameValue) { errorMessages.push('Please enter a name'); }
     devicesStore.changeDevice('valid_name', nameValue);
 
-    const nameAbbreviationValue = device.name_abbreviation.trim() === '' ? 'error' : null;
+    const nameAbbreviationValue = device.name_abbreviation.trim() === '' ? 'is-invalid' : '';
     if (nameAbbreviationValue) { errorMessages.push('Please enter a name abbreviation'); }
     devicesStore.changeDevice('valid_name_abbreviation', nameAbbreviationValue);
 
     if (anyDatacollectorFields().length >= 1) {
       devicesStore.changeDevice('datacollector_fields', true);
     
-      const methodValue = !device.datacollector_method ? 'error' : null;
+      const methodValue = !device.datacollector_method ? 'is-invalid' : '';
       if (methodValue) { errorMessages.push('Please select watch method'); }
       devicesStore.changeDevice('valid_datacollector_method', methodValue);
     
-      const dirValue = !device.datacollector_dir ? 'error' : null;
+      const dirValue = !device.datacollector_dir ? 'is-invalid' : '';
       if (dirValue) { errorMessages.push('Please enter a watch directory'); }
       devicesStore.changeDevice('valid_datacollector_dir', dirValue);
     
       if (endsWith(device.datacollector_method, 'sftp')) {
-        const userValue = !device.datacollector_user ? 'error' : null;
+        const userValue = !device.datacollector_user ? 'is-invalid' : '';
         if (userValue) { errorMessages.push('Please enter a user'); }
         devicesStore.changeDevice('valid_datacollector_user', userValue);
     
-        const hostValue = !device.datacollector_host ? 'error' : null;
+        const hostValue = !device.datacollector_host ? 'is-invalid' : '';
         if (hostValue) { errorMessages.push('Please enter a host'); }
         devicesStore.changeDevice('valid_datacollector_host', hostValue);
     
         const keyNameValue =
-          device.datacollector_authentication === 'keyfile' && !device.datacollector_key_name ? 'error' : null;
+          device.datacollector_authentication === 'keyfile' && !device.datacollector_key_name ? 'is-invalid' : '';
         if (keyNameValue) { errorMessages.push('Use key file, Please enter a key path'); }
         devicesStore.changeDevice('valid_datacollector_key_name', keyNameValue);
       }
@@ -104,7 +104,7 @@ const DeviceModal = () => {
     }
 
     if (devicesStore.active_tab_key == 4) {
-      const novncTarget = !device.novnc_target ? 'error' : null;
+      const novncTarget = !device.novnc_target ? 'is-invalid' : '';
       if (novncTarget) { errorMessages.push('Please type a Target for the device'); }
       devicesStore.changeDevice('valid_novnc_target', novncTarget);
     }
@@ -179,93 +179,107 @@ const DeviceModal = () => {
     }
   }
 
+  const handleDrag = (e, ui) => {
+    const { x, y } = deltaPosition;
+    setDeltaPosition({
+      x: x + ui.deltaX,
+      y: y + ui.deltaY,
+    });
+  }
+
   return (
-    <Draggable handle=".handle">
-      <Modal
-        centered
-        show={devicesStore.deviceModalVisible}
-        onHide={() => handleCancel()}
-        backdrop={false}
-        dialogas="draggable-modal"
-      >
-        <Modal.Header className="handle" closeButton>
-          <div className="col-md-11 col-sm-11">
-            <Modal.Title>
-              <i className="fa fa-arrows move" />
-              {modalTitle()}
-            </Modal.Title>
-          </div>
-          <div className="col-md-1 col-sm-1">
-            <i
-              className="fa fa-window-minimize window-minimize"
-              onClick={() => devicesStore.toggleModalMinimized()} />
-          </div>
-        </Modal.Header>
-        <Modal.Body>
-          <div className={`draggable-modal-form-container${minimizedClass}`}>
-            <div className="draggable-modal-form-fields">
-              {showMessage()}
-              <div className={`draggable-modal-scrollable-content ${withAlertClass}`}>
-                <Tabs
-                  activeKey={devicesStore.active_tab_key}
-                  animation={false}
-                  onSelect={handleSelectTab}
-                  id="device-form-tabs"
-                  key="form-tabs"
-                >
-                  <Tab
-                    eventKey={1}
-                    title="Properties"
-                    key="tab-properties-1"
+    <Draggable handle=".modal-header" onDrag={handleDrag}>
+      <div>
+        <Modal
+          show={devicesStore.deviceModalVisible}
+          onHide={() => handleCancel()}
+          backdrop={false}
+          keyboard={false}
+          className={`draggable-modal-dialog${minimizedClass}`}
+          size="xxxl"
+          dialogClassName="draggable-modal"
+          contentClassName={`draggable-modal-content${minimizedClass}`}
+          style={{
+            transform: `translate(${deltaPosition.x}px, ${deltaPosition.y}px)`,
+          }}
+        >
+          <Modal.Header closeButton>
+            <Stack direction="horizontal" className="draggable-modal-stack justify-content-between" gap={3}>
+              <Modal.Title className="draggable-modal-stack-title">
+                <i className="fa fa-arrows move" />
+                {modalTitle()}
+              </Modal.Title>
+              <Button className="ms-auto ms-lg-5 pt-2 align-self-start bg-transparent border-0">
+                <i
+                  className="fa fa-window-minimize window-minimize"
+                  onClick={() => devicesStore.toggleModalMinimized()} />
+              </Button>
+            </Stack>
+          </Modal.Header>
+          <Modal.Body>
+            <div className={`draggable-modal-form-container${minimizedClass}`}>
+              <div className="draggable-modal-form-fields">
+                {showMessage()}
+                <div className={`draggable-modal-scrollable-content ${withAlertClass}`}>
+                  <Tabs
+                    activeKey={devicesStore.active_tab_key}
+                    onSelect={handleSelectTab}
+                    key="form-tabs"
                   >
-                    <DevicePropertiesTab />
-                  </Tab>
-                  <Tab
-                    eventKey={2}
-                    title="Users & Groups"
-                    key="tab-user-group-2"
-                    disabled={disableTab}
-                  >
-                    <DeviceUserGroupsTab />
-                  </Tab>
-                  <Tab
-                    eventKey={3}
-                    title="Data Collector"
-                    key="tab-data-collector-3"
-                    disabled={disableTab}
-                  >
-                    <DeviceDataCollectorTab />
-                  </Tab>
-                  <Tab
-                    eventKey={4}
-                    title="NoVNC Settings"
-                    key="tab-novnc-settings-4"
-                    disabled={disableTab}
-                  >
-                    <DeviceNovncTab />
-                  </Tab>
-                  <Tab
-                    eventKey={5}
-                    title="Metadata"
-                    key="tab-metadata-5"
-                    disabled={disableTab}
-                  >
-                    <DeviceMetadataTab />
-                  </Tab>
-                </Tabs>
+                    <Tab
+                      eventKey={1}
+                      title="Properties"
+                      key="tab-properties-1"
+                    >
+                      <DevicePropertiesTab />
+                    </Tab>
+                    <Tab
+                      eventKey={2}
+                      title="Users & Groups"
+                      key="tab-user-group-2"
+                      disabled={disableTab}
+                    >
+                      <DeviceUserGroupsTab />
+                    </Tab>
+                    <Tab
+                      eventKey={3}
+                      title="Data Collector"
+                      key="tab-data-collector-3"
+                      disabled={disableTab}
+                    >
+                      <DeviceDataCollectorTab />
+                    </Tab>
+                    <Tab
+                      eventKey={4}
+                      title="NoVNC Settings"
+                      key="tab-novnc-settings-4"
+                      disabled={disableTab}
+                    >
+                      <DeviceNovncTab />
+                    </Tab>
+                    <Tab
+                      eventKey={5}
+                      title="Metadata"
+                      key="tab-metadata-5"
+                      disabled={disableTab}
+                    >
+                      <DeviceMetadataTab />
+                    </Tab>
+                  </Tabs>
+                </div>
+                <ButtonToolbar className="draggable-modal-form-buttons">
+                  <Button variant="warning" onClick={() => handleCancel()}>
+                    Cancel
+                  </Button>
+                  <Button variant="primary" onClick={saveDeviceOrRelation} >
+                    Save
+                  </Button>
+                </ButtonToolbar>
               </div>
-              <ButtonToolbar className={`draggable-modal-form-buttons ${buttonsWithAlertClass}`}>
-                <Button variant="warning" onClick={() => handleCancel()}>
-                  Cancel
-                </Button>
-                <Button variant="primary" onClick={saveDeviceOrRelation} >
-                  Save
-                </Button>
-              </ButtonToolbar>
             </div>
-          </div>
-        </Modal.Body>
-      </Modal>
+          </Modal.Body>
+        </Modal>
+      </div>
     </Draggable>
   );
 }
