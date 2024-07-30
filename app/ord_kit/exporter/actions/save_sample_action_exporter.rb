fbafa51c0cc1d8c37836105500c440b27e03304d
Christian Buggle <christian@buggle.net>
2024-07-30 23:43:13 +0200

    Add new fields to SaveSample ORD export
    * Add new fields to SaveSampleActionExporter (vessel, molecular_entities, origin_type, …)
    * Enhance ORD accordingly.
    * Move Exporter classes for Action specific Samples attributes to module OrdKit::Exporter::Actions.
    * Add OrdKit::Exporter::Samples::SamplesExporter.

--- app/ord_kit/exporter/actions/save_sample_action_exporter.rb
+++ app/ord_kit/exporter/actions/save_sample_action_exporter.rb
@@ -10,8 +10,28 @@ module OrdKit
           {
             save_sample: ReactionProcessAction::ActionSaveSample.new(
               sample: OrdKit::Exporter::Compounds::SaveCompoundExporter.new(action).to_ord,
+              molecular_entities: molecular_entities,
+              vessel: Vessels::ReactionProcessVesselExporter.new(action.workup['reaction_process_vessel']).to_ord,
+              origin_type: origin_type,
+              purify_origin: purify_origin,
             ),
           }
+        end
+
+        def molecular_entities
+          Array(action.workup['samples']).map do |sample|
+            OrdKit::Exporter::Samples::SampleExporter.new(sample).to_ord
+          end
+        end
+
+        def origin_type
+          OrdKit::ReactionProcessAction::ActionSaveSample::OriginType.const_get(workup['origin_type'].to_s)
+        rescue NameError
+          OrdKit::ReactionProcessAction::ActionSaveSample::OriginType::UNSPECIFIED
+        end
+
+        def purify_origin
+          SaveSample::PurifyOriginExporter.new(action).to_ord if action.workup['origin_type'] == 'PURIFCATION'
         end
       end
     end
