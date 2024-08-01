d8355ee2e6dda80218e943e10b71853309711b57
Beate Quednau <beate@firma-pflaum.de>
2024-08-01 19:35:22 +0200

    Fix screen analyses tab and rounded card header

--- app/packs/src/apps/mydb/elements/details/screens/analysesTab/ScreenDetailsContainers.js
+++ app/packs/src/apps/mydb/elements/details/screens/analysesTab/ScreenDetailsContainers.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'react-bootstrap';
+import { Button, Accordion, Card } from 'react-bootstrap';
 
 import Container from 'src/models/Container';
 import ContainerComponent from 'src/components/container/ContainerComponent';
 import PrintCodeButton from 'src/components/common/PrintCodeButton';
+import AccordionHeaderWithButtons from 'src/apps/mydb/elements/details/AccordionHeaderWithButtons';
 
 import TextTemplateActions from 'src/stores/alt/actions/TextTemplateActions';
-import PanelGroup from 'src/components/legacyBootstrap/PanelGroup'
 
 export default class ScreenDetailsContainers extends Component {
   constructor(props) {
     super();
     const { screen } = props;
     this.state = {
-      screen,
-      activeContainer: 0
+      screen
     };
+    this.analysesContainer = screen.container.children.filter(element => ~element.container_type.indexOf('analyses'));
   }
 
   componentDidMount() {
@@ -29,9 +29,9 @@ export default class ScreenDetailsContainers extends Component {
     })
   }
 
-  handleChange(container) {
-    const { screen } = this.state
-    this.props.parent.handleScreenChanged(screen)
+  handleChange() {
+    const { screen } = this.state;
+    this.props.parent.handleScreenChanged(screen);
   }
 
   handleAdd() {
@@ -39,9 +39,8 @@ export default class ScreenDetailsContainers extends Component {
     let container = Container.buildEmpty();
     container.container_type = "analysis";
 
-    screen.container.children.filter(element => ~element.container_type.indexOf('analyses'))[0].children.push(container);
-
-    const newKey = screen.container.children.filter(element => ~element.container_type.indexOf('analyses'))[0].children.length - 1;
+    this.analysesContainer[0].children.push(container);
+    const newKey = this.analysesContainer[0].children.length - 1;
 
     this.handleAccordionOpen(newKey);
 
@@ -55,6 +54,12 @@ export default class ScreenDetailsContainers extends Component {
     this.props.parent.setState({ screen: screen })
   }
 
+  handleClickToRemoveContainer(container) {
+    if (confirm('Delete the container?')) {
+      this.handleRemove(container);
+    }
+  }
+
   handleUndo(container) {
     let { screen } = this.state;
     container.is_deleted = false;
@@ -62,111 +67,149 @@ export default class ScreenDetailsContainers extends Component {
     this.props.parent.setState({ screen: screen })
   }
 
-  handleAccordionOpen(key) {
-    this.setState({ activeContainer: key });
-  }
-
   addButton() {
     const { readOnly } = this.props;
-    if (!readOnly) {
-      return (
-        <Button size="sm" variant="success" onClick={() => this.handleAdd()}>
+    if (readOnly) { return null; }
+
+    return (
+      <div className="my-2">
+        <Button
+          size="xxsm"
+          variant="success"
+          onClick={() => this.handleAdd()}
+        >
+          <i className="fa fa-plus me-1" />
           Add analysis
         </Button>
-      )
-    }
+      </div>
+    );
+  }
+
+  headerValues(container) {
+    const kind = container.extended_metadata['kind'];
+    const typeValue = kind && kind !== '' ? (<div className="mt-2">Type: {kind.split('|')[1] || kind}</div>) : '';
+    const status = container.extended_metadata['status'];
+    const statusValue = status && status !== '' ? (<div className="mt-2">Status: {status}</div>) : '';
+    const containerNameClass = container.is_deleted ? '' : 'fw-bold text-decoration-underline';
+
+    return (
+      <>
+        <div className={containerNameClass}>{container.name}</div>
+        {typeValue}
+        {statusValue}
+      </>
+    );
+  }
+
+  containerHeader(container) {
+    const { readOnly } = this.props;
+
+    return (
+      <div className="d-flex justify-content-between w-100 mb-0">
+        <div>
+          {this.headerValues(container)}
+        </div>
+        <div>
+          <PrintCodeButton element={screen} analyses={[container]} ident={container.id} />
+          <Button
+            size="xxsm"
+            variant="danger"
+            className="ms-2"
+            disabled={readOnly}
+            onClick={() => { this.handleClickToRemoveContainer(container) }}
+          >
+            <i className="fa fa-trash"></i>
+          </Button>
+        </div>
+      </div>
+    );
+  }
+    
+  containerHeaderDeleted(container) {
+    return (
+      <div className="d-flex justify-content-between w-100 mb-0">
+        <div className="text-decoration-line-through">
+          {this.headerValues(container)}
+        </div>
+        <div>
+          <Button
+            size="xxsm"
+            variant="danger"
+            onClick={() => this.handleUndo(container)}
+          >
+            <i className="fa fa-undo"></i>
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
+  collapsableBody(container, key) {
+    if (container.is_deleted) { return null; }
+    const { readOnly } = this.props;
+
+    return (
+      <Accordion.Collapse eventKey={key}>
+        <Card.Body>
+          <ContainerComponent
+            disabled={readOnly}
+            readOnly={readOnly}
+            templateType="screen"
+            container={container}
+            onChange={() => this.handleChange()}
+          />
+        </Card.Body>
+      </Accordion.Collapse>
+    )
   }
 
   render() {
-    const { screen, activeContainer } = this.state;
-    const { readOnly } = this.props;
-
-    let containerHeader = (container) =>
-      <p style={{ width: '100%' }}>{container.name}
-        {(container.extended_metadata['kind'] && container.extended_metadata['kind'] != '') ?
-          (` - Type: ${container.extended_metadata['kind'].split('|')[1] || container.extended_metadata['kind']}`) : ''}
-        {(container.extended_metadata['status'] && container.extended_metadata['status'] != '') ? (' - Status: ' + container.extended_metadata['status']) : ''}
-        <Button size="sm" variant="danger"
-          disabled={readOnly}
-          onClick={() => { if (confirm('Delete the container?')) { this.handleRemove(container) } }}>
-          <i className="fa fa-trash"></i>
-        </Button>
-        <PrintCodeButton element={screen} analyses={[container]} ident={container.id} />
-      </p>
-
-    let containerHeaderDeleted = (container) => <p style={{ width: '100%' }}><strike>{container.name}
-      {(container.extended_metadata['kind'] && container.extended_metadata['kind'] != '') ?
-        (` - Type: ${container.extended_metadata['kind'].split('|')[1] || container.extended_metadata['kind']}`) : ''}
-      {(container.extended_metadata['status'] && container.extended_metadata['status'] != '') ? (' - Status: ' + container.extended_metadata['status']) : ''}
-    </strike>
-      <Button className="pull-right" size="sm" variant="danger" onClick={() => this.handleUndo(container)}>
-        <i className="fa fa-undo"></i>
-      </Button>
-    </p>
+    const { screen } = this.state;
 
     if (screen.container != null) {
-
-      var analyses_container = screen.container.children.filter(element => ~element.container_type.indexOf('analyses'));
-
-      if (analyses_container.length == 1 && analyses_container[0].children.length > 0) {
+      if (this.analysesContainer.length == 1 && this.analysesContainer[0].children.length > 0) {
         return (
           <div>
-            <p>&nbsp;{this.addButton()}</p>
-            <PanelGroup defaultActiveKey={0} activeKey={activeContainer} accordion>
-              {analyses_container[0].children.map((container, key) => {
-                if (container.is_deleted) {
-                  return (
-                    <Panel eventKey={key}
-                      key={key} >
-                      <Panel.Heading>{containerHeaderDeleted(container)}</Panel.Heading>
-                    </Panel>
-                  );
-                } else {
-                  return (
-                    <Panel eventKey={key}
-                      key={key} onClick={() => this.handleAccordionOpen(key)}>
-                      <Panel.Heading>{containerHeader(container)}</Panel.Heading>
-                      <Panel.Body collapsible="true">
-                        <ContainerComponent
-                          templateType="screen"
-                          readOnly={readOnly}
-                          container={container}
-                          onChange={container => this.handleChange(container)}
-                        />
-                      </Panel.Body>
-                    </Panel>
-                  );
-                }
-
-              }
-              )}
-            </PanelGroup>
+            <div className="mb-2 me-1 d-flex flex-row-reverse">
+              {this.addButton()}
+            </div>
+            <Accordion>
+              {this.analysesContainer[0].children.map((container, key) => {
+                return (
+                  <Card
+                    key={`screen_container_${container.id}`}
+                    className="rounded-0"
+                  >
+                    <Card.Header className="bg-gray-200 py-3 rounded-0">
+                      <AccordionHeaderWithButtons eventKey={key}>
+                        {container.is_deleted
+                          ? this.containerHeaderDeleted(container)
+                          : this.containerHeader(container)}
+                      </AccordionHeaderWithButtons> 
+                    </Card.Header>
+                    {this.collapsableBody(container, key)}
+                  </Card>
+                );
+              })}
+            </Accordion>
           </div>
-        )
+        );
       } else {
         return (
-          <div>
-            <p className='noAnalyses-warning'>
-              There are currently no Analyses.
-              {this.addButton()}
-            </p>
+          <div className="d-flex align-items-center justify-content-between my-2">
+            <span> There are currently no Analyses.</span>
+            {this.addButton()}
           </div>
         )
       }
-
     } else {
-
       return (
-        <div>
-          <p className='noAnalyses-warning'>
-            There are currently no Analyses.
-          </p>
+        <div className="m-4">
+          There are currently no Analyses.
         </div>
       )
     }
-
   }
-
 }
 
 ScreenDetailsContainers.propTypes = {
