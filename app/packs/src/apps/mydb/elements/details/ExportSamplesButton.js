aa395f3756c8c9e1622e28a5ef516b98231172f0
Luciano Maiwald <mail@luciano-maiwald.de>
2024-08-05 11:43:39 +0200

    Update ExportSamplesButton

--- app/packs/src/apps/mydb/elements/details/ExportSamplesButton.js
+++ app/packs/src/apps/mydb/elements/details/ExportSamplesButton.js
@@ -7,21 +7,29 @@ export default class ExportSamplesButton extends Component {
   constructor(props) {
     super(props);
     this.state = { startExport: false };
+    this.handleExport = this.handleExport.bind(this);
   }
 
-  handleExport(type, id) {
+  handleExport() {
     this.setState({ startExport: true });
+
+    const { type, id } = this.props;
     CollectionsFetcher.expotSamples(type, id)
       .then(() => { this.setState({ startExport: false }); })
       .catch((errorMessage) => { console.log(errorMessage); });
   }
 
   render() {
-    const { type, id } = this.props;
     const { startExport } = this.state;
     return (
-      <Button variant="info" onClick={() => this.handleExport(type, id)}>
-        Export samples{startExport ? <span>&nbsp;<i className="fa fa-spin fa-spinner" /></span> : null}
+      <Button
+        variant="info"
+        onClick={this.handleExport}
+      >
+        Export samples
+        {startExport && (
+          <i className="fa fa-spin fa-spinner ms-2" />
+        )}
       </Button>
     );
   }
