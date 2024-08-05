c98bf0f455f1bb2ac45857d8bcf3a7edec384d28
adambasha0 <adambasha1992@gmail.com>
2024-08-05 09:46:39 +0000

    revert-merge:reaction-vessel-size

--- db/migrate/20240711120833_add_vessel_size_to_reaction.rb
+++
@@ -1,5 +0,0 @@
-class AddVesselSizeToReaction < ActiveRecord::Migration[6.1]
-  def change
-    add_column :reactions, :vessel_size, :jsonb, null: true, default: { 'unit' => 'ml', 'amount' => nil }
-  end
-end
--- spec/api/entities/reaction_entity_spec.rb
+++ spec/api/entities/reaction_entity_spec.rb
@@ -218,7 +218,6 @@ describe Entities::ReactionEntity do
           tlc_solvents: reaction.tlc_solvents,
           vessel_size: reaction.vessel_size,
           gaseous: reaction.gaseous,
-          vessel_size: reaction.vessel_size,
         )
       end
 
@@ -306,7 +305,6 @@ describe Entities::ReactionEntity do
           variations: [],
           vessel_size: '***',
           gaseous: '***',
-          vessel_size: '***',
         )
       end
 
