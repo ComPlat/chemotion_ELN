5d3da1701ff0e189c1616e78c66ca585c172cce2
Luciano Maiwald <mail@luciano-maiwald.de>
2024-08-08 09:13:25 +0200

    Update Bootstrap: CellLine Details (#2059)
    * Remove unused method in CellLineDetails
    
    * Update CellLineDetails to new bootstrap version
    
    * Change cell line GeneralProperties PanelGroup to Accordion
    
    * Translate Form in GeneralProperties and CellLineName
    
    * Use utility classes in InvalidPropertyWarning
    
    * Use utility classes for Amount, separate logic and markup
    
    * Update AnalysesContainer button placement to utility classes
    
    * Update AnalysesContainer to new bootstrap version
    
    Also consolidate EditModeHeader and OrderModeHeader into a single Header
    component.
    
    * Remove unused css rule
    
    * Translate custom css to utility class
    
    * Style analysesTab Header using utility classes

--- app/packs/src/apps/mydb/elements/details/cellLines/propertiesTab/CellLineName.js
+++ app/packs/src/apps/mydb/elements/details/cellLines/propertiesTab/CellLineName.js
@@ -2,11 +2,8 @@ import React from 'react';
 import CellLinesFetcher from 'src/fetchers/CellLinesFetcher';
 import PropTypes from 'prop-types';
 import { StoreContext } from 'src/stores/mobx/RootStore';
-import {
-  Col, Row, FormControl
-} from 'react-bootstrap';
+import { Col, Row, Form } from 'react-bootstrap';
 import Creatable from 'react-select3/creatable';
-import ControlLabel from 'src/components/legacyBootstrap/ControlLabel'
 
 export default class CellLineName extends React.Component {
   // eslint-disable-next-line react/static-property-placement
@@ -43,59 +40,56 @@ export default class CellLineName extends React.Component {
     const { cellLineDetailsStore } = this.context;
     const { nameSuggestions } = this.state;
     const { id, name, readOnly } = this.props;
-    const className = cellLineDetailsStore.cellLines(id).cellLineName
-      ? 'cell-line-name-autocomplete'
-      : 'cell-line-name-autocomplete invalid';
+
     if (readOnly) {
       return (
-        <Row>
-          <Col componentClass={ControlLabel} sm={3}>Cell line name *</Col>
+        <Form.Group as={Row}>
+          <Form.Label column sm={3}>Cell line name *</Form.Label>
           <Col sm={9}>
-            <FormControl
+            <Form.Control
               disabled
-              className=""
               type="text"
               value={cellLineDetailsStore.cellLines(id).cellLineName}
-              onChange={() => {}}
             />
           </Col>
-        </Row>
+        </Form.Group>
       );
     }
 
+    const className = cellLineDetailsStore.cellLines(id).cellLineName
+      ? 'cell-line-name-autocomplete'
+      : 'cell-line-name-autocomplete invalid';
     return (
-      <div className="cell-line-name">
-        <Row>
-          <Col componentClass={ControlLabel} sm={3}>Cell line name *</Col>
-          <Col sm={9}>
-            <Creatable
-              className={className}
-              onChange={(e) => {
-                if (typeof e.value === 'number') {
-                  const currentEntry = nameSuggestions.filter((x) => x.value === e.value);
-                  if (currentEntry.length > 0) {
-                    cellLineDetailsStore.changeCellLineName(id, currentEntry[0].name);
-                    CellLinesFetcher.getCellLineMaterialById(e.value)
-                      .then((result) => {
-                        cellLineDetailsStore.setMaterialProperties(id, result);
-                      });
-                  }
-                } else {
-                  cellLineDetailsStore.changeCellLineName(id, e.value);
+      <Form.Group as={Row} className="cell-line-name">
+        <Form.Label column sm={3}>Cell line name *</Form.Label>
+        <Col sm={9}>
+          <Creatable
+            className={className}
+            onChange={(e) => {
+              if (typeof e.value === 'number') {
+                const currentEntry = nameSuggestions.filter((x) => x.value === e.value);
+                if (currentEntry.length > 0) {
+                  cellLineDetailsStore.changeCellLineName(id, currentEntry[0].name);
+                  CellLinesFetcher.getCellLineMaterialById(e.value)
+                    .then((result) => {
+                      cellLineDetailsStore.setMaterialProperties(id, result);
+                    });
                 }
-              }}
-              onInputChange={(e, action) => {
-                if (action.action === 'input-change') {
-                  cellLineDetailsStore.changeCellLineName(id, e);
-                }
-              }}
-              options={nameSuggestions}
-              placeholder="enter new cell line name or choose from existing ones "
-              defaultInputValue={name}
-            />
-          </Col>
-        </Row>
-      </div>
+              } else {
+                cellLineDetailsStore.changeCellLineName(id, e.value);
+              }
+            }}
+            onInputChange={(e, action) => {
+              if (action.action === 'input-change') {
+                cellLineDetailsStore.changeCellLineName(id, e);
+              }
+            }}
+            options={nameSuggestions}
+            placeholder="enter new cell line name or choose from existing ones "
+            defaultInputValue={name}
+          />
+        </Col>
+      </Form.Group>
     );
   }
 }
