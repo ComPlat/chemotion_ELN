d66816780a9fbf32cf5f51b4d88fc2a9c8ab8177
Luciano Maiwald <mail@luciano-maiwald.de>
2024-08-14 14:59:01 +0200

    Update MetadataCreator(s) to new bootstrap version

--- app/packs/src/components/metadata/MetadataCreator.js
+++ app/packs/src/components/metadata/MetadataCreator.js
@@ -1,88 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Col, FormControl, FormGroup, Row } from 'react-bootstrap';
-import ControlLabel from 'src/components/legacyBootstrap/ControlLabel'
+import {
+  Button, Col, Form, Row
+} from 'react-bootstrap';
 
-const MetadataCreator = ({ creator, index, onAdd, onChange, onRemove }) => (
+const MetadataCreator = ({
+  creator, index, onAdd, onChange, onRemove
+}) => (
   <div>
     <Row>
-      <Col sm={4}>
-        <FormGroup>
-          <ControlLabel>
-            Given name
-          </ControlLabel>
-          <FormControl
-            type="text"
-            value={creator.givenName}
-            onChange={event => onChange(event.target.value, 'creators', index, 'givenName')}
-          />
-        </FormGroup>
-      </Col>
-      <Col sm={4}>
-        <FormGroup>
-          <ControlLabel>
-            Family name
-          </ControlLabel>
-          <FormControl
-            type="text"
-            value={creator.familyName}
-            onChange={event => onChange(event.target.value, 'creators', index, 'familyName')}
-          />
-        </FormGroup>
-      </Col>
-      <Col sm={4}>
-        <FormGroup>
-          <ControlLabel>
-            ORCID iD
-          </ControlLabel>
-          <FormControl
-            type="text"
-            value={creator.orcid}
-            onChange={event => onChange(event.target.value, 'creators', index, 'orcid')}
-          />
-        </FormGroup>
-      </Col>
+      <Form.Group className="mb-3" as={Col} xs={4}>
+        <Form.Label>
+          Given name
+        </Form.Label>
+        <Form.Control
+          type="text"
+          value={creator.givenName}
+          onChange={(event) => onChange(event.target.value, 'creators', index, 'givenName')}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" as={Col} xs={4}>
+        <Form.Label>
+          Family name
+        </Form.Label>
+        <Form.Control
+          type="text"
+          value={creator.familyName}
+          onChange={(event) => onChange(event.target.value, 'creators', index, 'familyName')}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" as={Col} xs={4}>
+        <Form.Label>
+          ORCID iD
+        </Form.Label>
+        <Form.Control
+          type="text"
+          value={creator.orcid}
+          onChange={(event) => onChange(event.target.value, 'creators', index, 'orcid')}
+        />
+      </Form.Group>
     </Row>
-    <Row>
-      <Col sm={12}>
-        <FormGroup>
-          <ControlLabel>
-            Affiliations
-          </ControlLabel>
-          {
-            creator.affiliations && creator.affiliations.map((affliation, affiliationIndex) => (
-              <Row key={affiliationIndex} className="align-items-center metadata-affiliation">
-                <Col xs={8}>
-                  <FormControl
-                    type="text"
-                    value={affliation.affiliation}
-                    onChange={event => onChange(event.target.value, 'creators', index, 'affiliations', affiliationIndex, 'affiliation')}
-                  />
-                </Col>
-                <Col xs={4}>
-                  <Button variant="danger" onClick={() => onRemove('creators', index, 'affiliations', affiliationIndex)}>
-                    <i className="fa fa-trash-o" />
-                  </Button>
-                  {
-                    (affiliationIndex === creator.affiliations.length - 1) &&
-                    <Button
-                      className="metadata-add-affiliation" variant="success"
-                      onClick={() => onAdd('creators', index, 'affiliations')}>
-                      <i className="fa fa-plus" />
-                    </Button>
-                  }
-                </Col>
-              </Row>
-            ))
-          }
-        </FormGroup>
-      </Col>
-      <Col sm={12}>
-        <Button variant="danger" size="sm" onClick={() => onRemove('creators', index)}>
-          Remove creator
-        </Button>
-      </Col>
-    </Row>
+    <Form.Group className="mb-3">
+      <Form.Label>
+        Affiliations
+      </Form.Label>
+      {creator.affiliations && creator.affiliations.map((affliation, affiliationIndex) => (
+        <Row key={affiliationIndex} className="align-items-center metadata-affiliation">
+          <Col xs={8}>
+            <Form.Control
+              type="text"
+              value={affliation.affiliation}
+              onChange={(event) => onChange(event.target.value, 'creators', index, 'affiliations', affiliationIndex, 'affiliation')}
+            />
+          </Col>
+          <Col xs={4}>
+            <Button
+              variant="danger"
+              onClick={() => onRemove('creators', index, 'affiliations', affiliationIndex)}
+            >
+              <i className="fa fa-trash-o" />
+            </Button>
+            {(affiliationIndex === creator.affiliations.length - 1) && (
+              <Button
+                className="metadata-add-affiliation"
+                variant="success"
+                onClick={() => onAdd('creators', index, 'affiliations')}
+              >
+                <i className="fa fa-plus" />
+              </Button>
+            )}
+          </Col>
+        </Row>
+      ))}
+    </Form.Group>
+    <Button variant="danger" size="sm" onClick={() => onRemove('creators', index)}>
+      Remove creator
+    </Button>
     <hr />
   </div>
 );
