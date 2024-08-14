6ebd0c613049552b8e4ffdc502e937876265d4cd
Luciano Maiwald <mail@luciano-maiwald.de>
2024-08-14 10:43:39 +0200

    Update NavNewSession to new bootstrap version
    Also do not render emtpy <Col>s

--- app/packs/src/components/navigation/NavNewSession.js
+++ app/packs/src/components/navigation/NavNewSession.js
@@ -5,17 +5,11 @@ import uuid from 'uuid';
 import {
   Button,
   Form,
-  FormGroup,
-  FormControl,
-  Navbar,
-  NavItem,
-  Nav,
   OverlayTrigger,
   Tooltip,
   Row,
   Col
 } from 'react-bootstrap';
-
 
 function omniauthLabel(icon, name) {
   if (icon) {
@@ -26,46 +20,86 @@ function omniauthLabel(icon, name) {
   return name;
 }
 
-function NewSession({ authenticityToken, omniauthProviders, extraRules={} }) {
-  const signUp = extraRules?.disable_signup === true ? null : (<NavItem href="/users/sign_up"> or Sign Up </NavItem>);
-  const items = omniauthProviders && Object.keys(omniauthProviders).map((key) => (
-    <Button key={uuid.v4()} className="omniauth-btn" style={{ textAlign: 'left', marginRight: '10px' }} href={`/users/auth/${key}`}>
-      Login with &nbsp;
-      {omniauthLabel(omniauthProviders[key].icon, omniauthProviders[key].label || key)}
-    </Button>
-  ));
-
-  const signIn = extraRules && extraRules?.disable_db_login === true ? null : (
-    <Form inline id="new_user" className="new_user" action="/users/sign_in" acceptCharset="UTF-8" method="post">
+function SignInForm({ authenticityToken }) {
+  return (
+    <Form id="new_user" className="new_user" action="/users/sign_in" acceptCharset="UTF-8" method="post">
       <input name="utf8" value="✓" type="hidden" />
       <input name="authenticity_token" value={authenticityToken} type="hidden" />
-      <Row className='g-0'>
+
+      <Row className="g-1 align-items-center">
         <Col xs="auto">
-          <OverlayTrigger placement="left" overlay={<Tooltip id="login_tooltip">Log in with email or name abbreviation(case-senstive)</Tooltip>}>
-            <FormGroup>
-              <FormControl id="user_login" type="text" placeholder="Email or name abbreviation" name="user[login]" className=" mr-sm-2"/>
-            </FormGroup>
+          <OverlayTrigger
+            placement="left"
+            overlay={(
+              <Tooltip id="login_tooltip">
+                Log in with email or name abbreviation (case-senstive)
+              </Tooltip>
+            )}
+          >
+            <Form.Group>
+              <Form.Control
+                id="user_login"
+                type="text"
+                placeholder="Email or name abbreviation"
+                name="user[login]"
+                className=" mr-sm-2"
+              />
+            </Form.Group>
           </OverlayTrigger>
         </Col>
         <Col xs="auto">
-          <FormGroup>
-            <FormControl id="user_password" type="password" name="user[password]" placeholder="password" className=" mr-sm-2"/>
-          </FormGroup>
+          <Form.Group>
+            <Form.Control
+              id="user_password"
+              type="password"
+              name="user[password]"
+              placeholder="password"
+              className=" mr-sm-2"
+            />
+          </Form.Group>
         </Col>
         <Col xs="auto">
-          <Button type="submit" variant="primary">
-            <i className="fa fa-sign-in"></i>
+          <Button type="submit" size="sm" variant="primary">
+            <i className="fa fa-sign-in" />
           </Button>
         </Col>
       </Row>
     </Form>
   );
+}
+
+function NewSession({ authenticityToken, omniauthProviders, extraRules = {} }) {
+  const items = omniauthProviders && Object.keys(omniauthProviders).map((key) => (
+    <Button
+      key={uuid.v4()}
+      className="omniauth-btn"
+      style={{ textAlign: 'left', marginRight: '10px' }}
+      href={`/users/auth/${key}`}
+    >
+      Login with
+      {' '}
+      {omniauthLabel(omniauthProviders[key].icon, omniauthProviders[key].label || key)}
+    </Button>
+  ));
+
+  const showSignIn = !extraRules.disable_db_login === true;
+  const showSignUp = !extraRules.disable_signup === true;
 
   return (
-    <Row className='g-3 align-items-center'>
-      <Col xs="auto">{items}</Col>
-      <Col xs="auto">{signIn}</Col>
-      <Col xs="auto">{signUp}</Col>
+    <Row className="g-3 align-items-center">
+      {items.length !== 0 && <Col xs="auto">{items}</Col>}
+      {showSignIn && (
+        <Col xs="auto">
+          <SignInForm authenticityToken={authenticityToken} />
+        </Col>
+      )}
+      {showSignUp && (
+        <Col xs="auto">
+          <a variant="link" href="/users/sign_up">
+            or Sign Up
+          </a>
+        </Col>
+      )}
     </Row>
   );
 }
