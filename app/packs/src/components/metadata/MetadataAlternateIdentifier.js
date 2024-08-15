586bd2ef11ac38d4524a508af6a0189ab506f794
Luciano Maiwald <mail@luciano-maiwald.de>
2024-08-15 09:31:44 +0200

    Update Metadata AlternateIdentifier(s), RelatedIdentifier(s) to new bootstrap version

--- app/packs/src/components/metadata/MetadataAlternateIdentifier.js
+++ app/packs/src/components/metadata/MetadataAlternateIdentifier.js
@@ -1,56 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Col, FormControl, FormGroup, Row } from 'react-bootstrap';
+import {
+  Button, Col, Form, Row
+} from 'react-bootstrap';
 import Select from 'react-select3';
-import ControlLabel from 'src/components/legacyBootstrap/ControlLabel'
 
-import { alternateIdentifierTypes } from 'src/components/staticDropdownOptions/radar/alternateIdentifierTypes'
+import { alternateIdentifierTypes } from 'src/components/staticDropdownOptions/radar/alternateIdentifierTypes';
 
-const MetadataAlternateIdentifier = ({ alternateIdentifier, index, onChange, onRemove }) => {
-  const alternateIdentifierType = alternateIdentifierTypes.find(el => el.value == alternateIdentifier.alternateIdentifierType)
+const MetadataAlternateIdentifier = ({
+  alternateIdentifier, index, onChange, onRemove
+}) => {
+  const alternateIdentifierType = alternateIdentifierTypes
+    .find((el) => el.value === alternateIdentifier.alternateIdentifierType);
 
   return (
-    <div>
-      <Row>
-        <Col sm={8}>
-          <FormGroup>
-            <ControlLabel>
-              Identifier
-            </ControlLabel>
-            <FormControl
-              type="text"
-              value={alternateIdentifier.value}
-              onChange={event => onChange(event.target.value, 'alternateIdentifiers', index, 'value')}
-            />
-          </FormGroup>
-        </Col>
-        <Col sm={3}>
-          <FormGroup>
-            <ControlLabel>
-              Identifier type
-            </ControlLabel>
-            <Select
-              name="alternateIdentifierType"
-              options={alternateIdentifierTypes}
-              onChange={option => onChange(option.value, 'alternateIdentifiers', index, 'alternateIdentifierType')}
-              value={alternateIdentifierType}
-              menuPortalTarget={document.body}
-              styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }}
-            />
-          </FormGroup>
-        </Col>
-        <Col sm={1}>
-          <ControlLabel>
-            &nbsp;
-          </ControlLabel>
-          <Button variant="danger" onClick={() => onRemove('alternateIdentifiers', index)}>
-            <i className="fa fa-trash-o" />
-          </Button>
-        </Col>
-      </Row>
-    </div>
-  )
-}
+    <Row className="mb-3">
+      <Form.Group as={Col} xs={8}>
+        <Form.Label>
+          Identifier
+        </Form.Label>
+        <Form.Control
+          type="text"
+          value={alternateIdentifier.value}
+          onChange={(event) => onChange(event.target.value, 'alternateIdentifiers', index, 'value')}
+        />
+      </Form.Group>
+      <Form.Group as={Col} xs={3}>
+        <Form.Label>
+          Identifier type
+        </Form.Label>
+        <Select
+          name="alternateIdentifierType"
+          options={alternateIdentifierTypes}
+          onChange={(option) => onChange(option.value, 'alternateIdentifiers', index, 'alternateIdentifierType')}
+          value={alternateIdentifierType}
+          menuPortalTarget={document.body}
+          styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
+        />
+      </Form.Group>
+      <Col xs={1} className="d-flex align-items-center">
+        <Button variant="danger" onClick={() => onRemove('alternateIdentifiers', index)}>
+          <i className="fa fa-trash-o" />
+        </Button>
+      </Col>
+    </Row>
+  );
+};
 
 MetadataAlternateIdentifier.propTypes = {
   alternateIdentifier: PropTypes.object.isRequired,
