ea8c350d4571c545007eedfe4f762eaa1ffe38da
Luciano Maiwald <mail@luciano-maiwald.de>
2024-08-15 15:18:30 +0200

    Update ScifinderCredential to new bootstrap version

--- app/packs/src/apps/scifinderCredential/ScifinderCredential.js
+++ app/packs/src/apps/scifinderCredential/ScifinderCredential.js
@@ -1,39 +1,58 @@
-/* eslint-disable react/forbid-prop-types */
 import React, { useState, useEffect } from 'react';
-import { Button, Row, Col } from 'react-bootstrap';
+import {
+  Button, Card, Row, Col
+} from 'react-bootstrap';
 import moment from 'moment';
 import uuid from 'uuid';
 import UsersFetcher from 'src/fetchers/UsersFetcher';
 import NotificationActions from 'src/stores/alt/actions/NotificationActions';
-import Panel from 'src/components/legacyBootstrap/Panel'
+
+const notify = ({ title, msg, lvl }) => {
+  NotificationActions.add({
+    title,
+    message: msg,
+    level: lvl,
+    position: 'tc',
+    dismissible: 'button',
+    uid: uuid.v4()
+  });
+};
 
 const ScifinderCredential = () => {
   const [credential, setCredential] = useState({});
-  const notify = (_params) => {
-    NotificationActions.add({
-      title: _params.title, message: _params.msg, level: _params.lvl, position: 'tc', dismissible: 'button', uid: uuid.v4()
-    });
-  };
 
   useEffect(() => {
     UsersFetcher.scifinderCredential().then((json) => {
       setCredential(json);
-    }).catch(err => notify({ title: 'SciFinder-n Error', lvl: 'error', msg: err }));
+    }).catch((err) => notify({
+      title: 'SciFinder-n Error',
+      lvl: 'error',
+      msg: err
+    }));
   }, [credential.access_token]);
 
   return (
-    <Panel>
-      <Panel.Heading><Panel.Title>SciFinder-n Credential</Panel.Title></Panel.Heading>
-      <Panel.Body>
+    <Card className="mb-3">
+      <Card.Header>SciFinder-n Credential</Card.Header>
+      <Card.Body>
         <Row>
-          <Col sm={4}><b style={{ float: 'right' }}>Current token will expire at</b></Col>
-          <Col sm={4}>
-            {credential.expires_at ? `${moment(credential.expires_at).format('YYYY-MM-DD HH:mm:ss')} UTC` : ''}
+          <Col xs={{ span: 3, offset: 3 }} className="fw-bold">Current token will expire at</Col>
+          <Col xs={2}>
+            {credential.expires_at
+              ? `${moment(credential.expires_at).format('YYYY-MM-DD HH:mm:ss')} UTC`
+              : '-'}
           </Col>
-          <Col sm={4}><Button variant="primary" href={`${window.location.origin}/users/auth/oauth2`}>Get token</Button></Col>
+          <Col xs={3}>
+            <Button
+              variant="primary"
+              href={`${window.location.origin}/users/auth/oauth2`}
+            >
+              Get token
+            </Button>
+          </Col>
         </Row>
-      </Panel.Body>
-    </Panel>
+      </Card.Body>
+    </Card>
   );
 };
 
