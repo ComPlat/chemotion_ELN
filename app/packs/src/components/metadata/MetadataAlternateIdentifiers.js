b5d066a5c4890d5682551d223799b7795dc8edb6
Luciano Maiwald <mail@luciano-maiwald.de>
2024-08-15 11:10:23 +0200

    Update Bootstrap: Metadata (#2079)

--- app/packs/src/components/metadata/MetadataAlternateIdentifiers.js
+++ app/packs/src/components/metadata/MetadataAlternateIdentifiers.js
@@ -2,31 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
-import MetadataAlternateIdentifier from './MetadataAlternateIdentifier'
+import MetadataAlternateIdentifier from 'src/components/metadata/MetadataAlternateIdentifier';
 
-const MetadataAlternateIdentifiers = ({ metadata, onAdd, onChange, onRemove }) => {
-  return (
-    <div>
-      <h4>Alternate identifiers</h4>
-      <p>
-        An identifier other than the DOI applied to the collection. The alternate identifier
-        should be an additional identifier for the same instance of the resource (i.e., same location, same file).
-        For other places where the same content can be found, please use a related identifier.
-      </p>
-      {
-        metadata.alternateIdentifiers && metadata.alternateIdentifiers.map((alternateIdentifier, index) => (
-          <MetadataAlternateIdentifier
-            alternateIdentifier={alternateIdentifier} index={index} key={index}
-            onChange={onChange} onRemove={onRemove}
-          />
-        ))
-      }
-      <Button variant="success" size="sm" onClick={event => onAdd('alternateIdentifiers')}>
-        Add new alternate identifier
-      </Button>
-    </div>
-  )
-}
+const MetadataAlternateIdentifiers = ({
+  metadata, onAdd, onChange, onRemove
+}) => (
+  <div>
+    <h4>Alternate identifiers</h4>
+    <p>
+      An identifier other than the DOI applied to the collection. The alternate identifier
+      should be an additional identifier for the same instance of the resource (i.e., same
+      location, same file). For other places where the same content can be found, please
+      use a related identifier.
+    </p>
+    {metadata.alternateIdentifiers && metadata.alternateIdentifiers.map((alternateIdentifier, index) => (
+      <MetadataAlternateIdentifier
+        alternateIdentifier={alternateIdentifier}
+        index={index}
+        key={index}
+        onChange={onChange}
+        onRemove={onRemove}
+      />
+    ))}
+    <Button variant="success" size="sm" onClick={() => onAdd('alternateIdentifiers')}>
+      Add new alternate identifier
+    </Button>
+  </div>
+);
 
 MetadataAlternateIdentifiers.propTypes = {
   metadata: PropTypes.object.isRequired,
