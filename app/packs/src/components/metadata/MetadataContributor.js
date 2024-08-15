884d95a3909f8c51e242f085604de4139636f292
Luciano Maiwald <mail@luciano-maiwald.de>
2024-08-15 09:41:54 +0200

    Update Metadata Contributor(s) to new bootstrap version

--- app/packs/src/components/metadata/MetadataContributor.js
+++ app/packs/src/components/metadata/MetadataContributor.js
@@ -1,117 +1,107 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Col, FormControl, FormGroup, Row } from 'react-bootstrap';
+import {
+  Button, Col, Form, Row
+} from 'react-bootstrap';
 import Select from 'react-select3';
-import ControlLabel from 'src/components/legacyBootstrap/ControlLabel'
 
-import { contributorTypes } from 'src/components/staticDropdownOptions/radar/contributorTypes'
+import { contributorTypes } from 'src/components/staticDropdownOptions/radar/contributorTypes';
 
-const MetadataContributor = ({ contributor, index, onAdd, onChange, onRemove }) => {
-  const contributorType = contributorTypes.find(el => el.value == contributor.contributorType)
+const MetadataContributor = ({
+  contributor, index, onAdd, onChange, onRemove
+}) => {
+  const contributorType = contributorTypes.find((el) => el.value == contributor.contributorType);
 
   return (
     <div>
-      <Row>
-        <Col sm={4}>
-          <FormGroup>
-            <ControlLabel>
-              Given name
-            </ControlLabel>
-            <FormControl
-              type="text"
-              value={contributor.givenName}
-              onChange={event => onChange(event.target.value, 'contributors', index, 'givenName')}
-            />
-          </FormGroup>
-        </Col>
-        <Col sm={4}>
-          <FormGroup>
-            <ControlLabel>
-              Family name
-            </ControlLabel>
-            <FormControl
-              type="text"
-              value={contributor.familyName}
-              onChange={event => onChange(event.target.value, 'contributors', index, 'familyName')}
-            />
-          </FormGroup>
-        </Col>
-        <Col sm={4}>
-          <FormGroup>
-            <ControlLabel>
-              ORCID iD
-            </ControlLabel>
-            <FormControl
-              type="text"
-              value={contributor.orcid}
-              onChange={event => onChange(event.target.value, 'contributors', index, 'orcid')}
-            />
-          </FormGroup>
-        </Col>
+      <Row className="mb-3">
+        <Form.Group as={Col} xs={4}>
+          <Form.Label>
+            Given name
+          </Form.Label>
+          <Form.Control
+            type="text"
+            value={contributor.givenName}
+            onChange={(event) => onChange(event.target.value, 'contributors', index, 'givenName')}
+          />
+        </Form.Group>
+        <Form.Group as={Col} xs={4}>
+          <Form.Label>
+            Family name
+          </Form.Label>
+          <Form.Control
+            type="text"
+            value={contributor.familyName}
+            onChange={(event) => onChange(event.target.value, 'contributors', index, 'familyName')}
+          />
+        </Form.Group>
+        <Form.Group as={Col} xs={4}>
+          <Form.Label>
+            ORCID iD
+          </Form.Label>
+          <Form.Control
+            type="text"
+            value={contributor.orcid}
+            onChange={(event) => onChange(event.target.value, 'contributors', index, 'orcid')}
+          />
+        </Form.Group>
       </Row>
-      <Row>
-        <Col sm={8}>
-          <FormGroup>
-            <ControlLabel>
-              Contributor type
-            </ControlLabel>
-            <Select
-              name="contributorType"
-              options={contributorTypes}
-              onChange={option => onChange(option.value, 'contributors', index, 'contributorType')}
-              value={contributorType}
-              menuPortalTarget={document.body}
-              styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }}
-            />
-          </FormGroup>
-        </Col>
+      <Row className="mb-3">
+        <Form.Group as={Col} xs={8}>
+          <Form.Label>
+            Contributor type
+          </Form.Label>
+          <Select
+            name="contributorType"
+            options={contributorTypes}
+            onChange={(option) => onChange(option.value, 'contributors', index, 'contributorType')}
+            value={contributorType}
+            menuPortalTarget={document.body}
+            styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
+          />
+        </Form.Group>
       </Row>
-      <Row>
-        <Col sm={12}>
-          <FormGroup>
-            <ControlLabel>
-              Affiliations
-            </ControlLabel>
-            {
-              contributor.affiliations && contributor.affiliations.map((affliation, affiliationIndex) => (
-                <Row key={affiliationIndex} className="align-items-center metadata-affiliation">
-                  <Col xs={8}>
-                    <FormControl
-                      type="text"
-                      value={affliation.affiliation}
-                      onChange={event => onChange(event.target.value, 'contributors', index, 'affiliations', affiliationIndex, 'affiliation')}
-                    />
-                  </Col>
-                  <Col xs={4}>
-                    <Button
-                      className="metadata-remove-affiliation" variant="danger"
-                      onClick={() => onRemove('contributors', index, 'affiliations', affiliationIndex)}>
-                      <i className="fa fa-trash-o" />
-                    </Button>
-                    {
-                      (affiliationIndex === contributor.affiliations.length - 1) &&
-                      <Button
-                        className="metadata-add-affiliation" variant="success"
-                        onClick={() => onAdd('contributors', index, 'affiliations')}>
-                        <i className="fa fa-plus" />
-                      </Button>
-                    }
-                  </Col>
-                </Row>
-              ))
-            }
-          </FormGroup>
-        </Col>
-        <Col sm={12}>
-          <Button variant="danger" size="sm" onClick={() => onRemove('contributors', index)}>
-            Remove contributor
-          </Button>
-        </Col>
-      </Row>
+      <Form.Group className="mb-3">
+        <Form.Label>
+          Affiliations
+        </Form.Label>
+        {contributor.affiliations && contributor.affiliations.map((affliation, affiliationIndex) => (
+          <Row key={affiliationIndex} className="align-items-center metadata-affiliation">
+            <Col xs={8}>
+              <Form.Control
+                type="text"
+                value={affliation.affiliation}
+                onChange={(event) => onChange(event.target.value, 'contributors', index, 'affiliations', affiliationIndex, 'affiliation')}
+              />
+            </Col>
+            <Col xs={4}>
+              <Button
+                className="metadata-remove-affiliation"
+                variant="danger"
+                onClick={() => onRemove('contributors', index, 'affiliations', affiliationIndex)}
+              >
+                <i className="fa fa-trash-o" />
+              </Button>
+              {(affiliationIndex === contributor.affiliations.length - 1) && (
+                <Button
+                  className="metadata-add-affiliation"
+                  variant="success"
+                  onClick={() => onAdd('contributors', index, 'affiliations')}
+                >
+                  <i className="fa fa-plus" />
+                </Button>
+              )}
+            </Col>
+          </Row>
+        ))}
+      </Form.Group>
+      <Button variant="danger" size="sm" onClick={() => onRemove('contributors', index)}>
+        Remove contributor
+      </Button>
       <hr />
     </div>
-  )
-}
+  );
+};
 
 MetadataContributor.propTypes = {
   contributor: PropTypes.object.isRequired,
--- app/packs/src/components/metadata/MetadataContributors.js
+++ app/packs/src/components/metadata/MetadataContributors.js
@@ -2,28 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
-import MetadataContributor from './MetadataContributor';
+import MetadataContributor from 'src/components/metadata/MetadataContributor';
 
-const MetadataContributors = ({ metadata, onAdd, onChange, onRemove }) => (
+const MetadataContributors = ({
+  metadata, onAdd, onChange, onRemove
+}) => (
   <div>
     <h4>Contributors</h4>
     <p>
       The persons or institutions responsible for collecting, managing, distributing, or otherwise contributing to the
       development of the resource.
     </p>
-    {
-      metadata.contributors && metadata.contributors.map((creator, index) => (
-        <MetadataContributor
-          contributor={creator} index={index} key={index}
-          onAdd={onAdd} onChange={onChange} onRemove={onRemove}
-        />
-      ))
-    }
+    {metadata.contributors && metadata.contributors.map((creator, index) => (
+      <MetadataContributor
+        contributor={creator}
+        index={index}
+        key={index}
+        onAdd={onAdd}
+        onChange={onChange}
+        onRemove={onRemove}
+      />
+    ))}
     <Button variant="success" size="sm" onClick={() => onAdd('contributors')}>
       Add new contributor
     </Button>
   </div>
-)
+);
 
 MetadataContributors.propTypes = {
   metadata: PropTypes.object.isRequired,
