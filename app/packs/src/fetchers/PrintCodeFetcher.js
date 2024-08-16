f9751df49c5f745bbe545322d86cbb1c256bb160
Nicolass67 <99028040+Nicolass67@users.noreply.github.com>
2024-08-16 13:10:02 +0200

    feat: Printing configuration: customizable code type, layout, label options, and molecule display for PDF output (#2018)
    * Change UI :
    - Add Modal
    - Add Preview of PDF
    
    * Change PDF options :
    - User can now select options for PDF (QR code, Bar code or Data matrix)
    
    * ADD Sample Image to pdfs
    
    * Fix display sample image on pdf
    
    * Fix Display sample on pdf
    
    * Fix Print QR code feature and clean code
    
    * Changes to the printing code feature
    
     - Printing code are now defined by a
        defaultConfig.json
    
     - Printing code can be sized by a width param
        in the defaultConfig(previous was small/large)
    
     - More printing options
    
    * Change in Printing feature :
    - Add error message if some value aren't correct
    - Add new possible labels
    - Add new printing options
    
    * Printing feature :
    - Import/Change printing configuration in admin page (UI only)
    
    * FIX :
    - Rubocop formatting
    - use of hash arguments in initialize method
    - Replace global variables with constants
    
    * FIX : code cleaner
    
    * Fix : clean code
    
    * Fix : clean branches
    
    * Add defaultConfig.json
    
    * Fix :
    - Change Print code button tooltip
    - Move fetcher in a PrintCodeFetcher file
    
    * Fix : rubocop on code_log_api.rb

---
+++ app/packs/src/fetchers/PrintCodeFetcher.js
@@ -0,0 +1,19 @@
+import 'whatwg-fetch';
+
+export default class PrintCodeFetcher {
+  static fetchPrintCodes(url) {
+    // Request options for fetching the PDF.
+    const requestOptions = {
+      credentials: 'same-origin',
+      method: 'GET',
+    };
+    // Fetch the PDF and set the preview.
+    return fetch(url, requestOptions)
+      .then((response) => response.blob())
+      .then((blob) => ({ type: blob.type, data: URL.createObjectURL(blob) }))
+      .then((result) => result.data)
+      .catch((errorMessage) => {
+        console.log(errorMessage);
+      });
+  }
+}
