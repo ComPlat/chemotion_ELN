5ec509fb9d11297d37e10e72bb1b47481986fc79
Ashwini <amurthy.ashwini@gmail.com>
2024-08-22 10:18:34 +0200

    refactor: affiliations page (#1957)
    * react implementation of affiliations as modal in main mydb app
    
    ---------
    
    Co-authored-by: PiTrem <pierre.tremouilhac@kit.edu>

---
+++ app/packs/src/fetchers/UserSettingsFetcher.js
@@ -0,0 +1,67 @@
+import 'whatwg-fetch';
+
+export default class UserSettingsFetcher {
+  static getAutoCompleteSuggestions(type) {
+    return fetch(
+      `/api/v1/public/affiliations/${type}`
+    ).then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  static getAllAffiliations() {
+    return fetch(
+      '/api/v1/affiliations/'
+    ).then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  static createAffiliation(params) {
+    return fetch('/api/v1/affiliations/', {
+      credentials: 'same-origin',
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    }).then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  static updateAffiliation(params) {
+    return fetch('/api/v1/affiliations/', {
+      credentials: 'same-origin',
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    })
+      .then((response) => response.json())
+      .then((json) => json)
+      .catch((errorMessage) => {
+        console.log(errorMessage);
+      });
+  }
+
+  static deleteAffiliation(id) {
+    return fetch(`/api/v1/affiliations/${id}`, {
+      credentials: 'same-origin',
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
