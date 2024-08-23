8a7639ca26bef9e6478dedd4959a533a899c7bf1
Pei Chi Huang <pei-chi.huang@kit.edu>
2024-08-23 13:13:49 +0200

    feat: structure viewer (#2098)
    Co-authored-by: Chia-Lin Lin <chia-lin.lin@kit.edu>

---
+++ app/packs/src/components/viewer/MolViewerBtn.js
@@ -0,0 +1,72 @@
+/* eslint-disable react/forbid-prop-types */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import MolViewerModal from 'src/components/viewer/MolViewerModal';
+import UIStore from 'src/stores/alt/stores/UIStore';
+
+export default class MolViewerBtn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+    this.handleModalOpen = this.handleModalOpen.bind(this);
+  }
+
+  handleModalOpen(e) {
+    if (e) {
+      e.stopPropagation();
+    }
+    const { show } = this.state;
+    this.setState({ show: !show });
+  }
+
+  render() {
+    const { disabled, fileContent, isPublic, viewType, className } = this.props;
+    const { show } = this.state;
+    const config = UIStore.getState().moleculeViewer;
+    if (isPublic && !config?.featureEnabled) return null;
+    if (!config.chembox) return null;
+
+    return (
+      <>
+        <OverlayTrigger
+          placement="top"
+          overlay={
+            <Tooltip id="tooltip_molviewer" style={{ pointerEvents: 'none' }}>
+              Click to see structure in Viewer
+            </Tooltip>
+          }
+        >
+          <Button
+            className={className}
+            bsSize="xsmall"
+            bsStyle="info"
+            disabled={disabled}
+            onClick={e => this.handleModalOpen(e)}
+          >
+            <i className="fa fa-cube" aria-hidden="true" /> View in 3D
+          </Button>
+        </OverlayTrigger>
+        {show ? (
+          <MolViewerModal
+            fileContent={fileContent}
+            handleModalOpen={e => this.handleModalOpen(e)}
+            isPublic={isPublic}
+            show={show}
+            viewType={viewType}
+          />
+        ) : null}
+      </>
+    );
+  }
+}
+
+MolViewerBtn.propTypes = {
+  disabled: PropTypes.bool.isRequired,
+  fileContent: PropTypes.string.isRequired,
+  isPublic: PropTypes.bool.isRequired,
+  viewType: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+MolViewerBtn.defaultProps = { className: 'button-right' };
---
+++ app/packs/src/components/viewer/MolViewerSet.js
@@ -0,0 +1,5 @@
+export default Object.freeze({
+  PK: 'moleculeViewer',
+  JSMOL: 'jsmol',
+  NGL: 'ngl',
+});
