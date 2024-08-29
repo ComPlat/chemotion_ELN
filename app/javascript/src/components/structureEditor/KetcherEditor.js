1ea6924d36c31218529d5f019beb1244dd32c18d
Hadi <hadi-tariq@live.co.uk>
2024-08-29 12:35:31 +0200

    draft: ketcher editor subscription

--- app/javascript/src/components/structureEditor/KetcherEditor.js
+++ app/javascript/src/components/structureEditor/KetcherEditor.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/forbid-prop-types */
-import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import React, { useEffect, useRef } from 'react';
 
 
 function KetcherEditor(props) {
@@ -16,6 +16,30 @@ function KetcherEditor(props) {
   const loadContent = (event) => {
     if (event.data.eventType === 'init') {
       editor.structureDef.editor.setMolecule(initMol);
+      // editor._structureDef.editor.editor.subscribe("change", (r) => console.log(r));
+      const list = [
+        "message", "elementEdit", "bondEdit", "zoomIn", "zoomOut", "rgroupEdit",
+        "sgroupEdit", "sdataEdit", "quickEdit", "attachEdit", "removeFG", "change",
+        "selectionChange", "aromatizeStruct", "dearomatizeStruct", "enhancedStereoEdit",
+        "confirm", "cursor", "showInfo", "apiSettings", "updateFloatingTools",
+        "click"
+        // , "dblclick", "mousedown", "mousemove", "mouseup", "mouseleave", "mouseover"
+      ];
+
+      // Function to handle each event
+      function handleEvent(eventName, data) {
+        console.log(`${eventName} event triggered with data:`, data);
+      }
+
+      // Subscribe to each event in the list
+      list.forEach(eventName => {
+        editor._structureDef.editor.editor.subscribe(eventName, async (eventData) => {
+          const result = await eventData;
+          handleEvent(eventName, result);
+        });
+      });
+
+      console.log(editor._structureDef.editor.editor);
     }
   };
 
