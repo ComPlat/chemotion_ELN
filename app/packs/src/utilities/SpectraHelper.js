fb32dc8f3041fd0d9a1d6ded1a8b16d0c8b3e268
Pei Chi Huang <pei-chi.huang@kit.edu>
2024-08-30 15:40:00 +0200

    feat: other solvent for cv (#2121)

--- app/packs/src/utilities/SpectraHelper.js
+++ app/packs/src/utilities/SpectraHelper.js
@@ -235,7 +235,7 @@ const inlineNotation = (layout, data, metadata) => {
     scanRate, voltaData, sampleName
   } = data;
   const {
-    cvConc, cvSolvent, cvRef, cvRefOthers, cvScanRate,
+    cvConc, cvSolvent, cvSolventOthers, cvRef, cvRefOthers, cvScanRate,
   } = metadata;
 
   switch (layout) {
@@ -259,7 +259,7 @@ const inlineNotation = (layout, data, metadata) => {
         if (isRef) {
           const posNegString = x[0] > x[1] ? 'neg.' : 'pos.';
           const concentrationStr = cvConc || '<conc. of sample>';
-          const solventStr = cvSolvent || '<solvent>';
+          const solventStr = (cvSolvent === 'others' ? cvSolventOthers : cvSolvent) || '<solvent>';
           let internalRefStr = "(Fc+/Fc)";
           refOps = [
             { insert: `CV (${concentrationStr} in ${solventStr} vs. Ref ` },
