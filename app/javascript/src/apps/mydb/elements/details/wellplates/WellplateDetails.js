fcefff8f5d553fe7d2ae6476dd15ee0067be93ec
Adrian Herrmann <adrian.herrmann@kit.edu>
2024-09-02 09:06:13 +0000

    feat: disable print button for size 0

--- app/javascript/src/apps/mydb/elements/details/wellplates/WellplateDetails.js
+++ app/javascript/src/apps/mydb/elements/details/wellplates/WellplateDetails.js
@@ -268,7 +268,7 @@ export default class WellplateDetails extends Component {
           iconClass="fa fa-print"
           header={false}
           onClick={() => this.handlePrint()}
-          disabled={wellplate.width > 12}
+          disabled={wellplate.width > 12 || wellplate.size === 0}
         />
       </>
     );
