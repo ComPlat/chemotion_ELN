79a56d4618092ced898120ed4a70b0193dcf0702
Christian Buggle <christian@buggle.net>
2024-09-04 11:20:57 +0200

    Fix issues with ELN javascript (#RPE5)
    * Add option PURE to Intermediate materials
    
    * Add reaction_step to SampleEntity

--- app/api/entities/sample_entity.rb
+++ app/api/entities/sample_entity.rb
@@ -76,6 +76,7 @@ module Entities
       expose! :sample_type
       expose! :sample_details
       expose! :components,              unless: :displayed_in_list, anonymize_with: [],   using: 'Entities::ComponentEntity'
+      expose! :reaction_step
     end
     # rubocop:enable Layout/ExtraSpacing, Metrics/BlockLength
 
@@ -145,6 +146,16 @@ module Entities
 
     def gas_phase_data
       object.reactions_samples.pick(:gas_phase_data)
+    end
+
+    def reaction_step
+      # Enhancement for IntermediateSamples as saved by the ReactionProcessEditor
+      # We want to present the position of the ReactionProcessStep in which the object (Sample) was saved.
+      intermediate = ReactionsIntermediateSample.find_by(sample_id: object.id)
+
+      return if intermediate.nil?
+
+      ::ReactionProcessEditor::ReactionProcessStep.find_by(id: intermediate.reaction_process_step_id)&.step_number
     end
   end
 end
