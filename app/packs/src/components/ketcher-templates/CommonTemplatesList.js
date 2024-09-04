6ac58b51a0871fff69b4069a5b1c9ef1088118fd
Hadi Tariq <hadi-tariq@live.co.uk>
2024-09-04 16:07:04 +0200

    feat: search bar for common templates (#2127)

--- app/packs/src/components/ketcher-templates/CommonTemplatesList.js
+++ app/packs/src/components/ketcher-templates/CommonTemplatesList.js
@@ -1,27 +1,52 @@
 import PropTypes from 'prop-types';
-import React, {memo, useState} from 'react';
+import React, { memo, useCallback, useEffect, useState } from 'react';
 import {
-  Modal,
-  Panel,
   ControlLabel,
+  Modal,
   OverlayTrigger,
+  Panel,
   Tooltip,
 } from 'react-bootstrap';
 import CommonTemplateItem from 'src/components/ketcher-templates/CommonTemplateItem';
 
-const CommonTemplatesList = memo(({options, onClickHandle, selectedItem}) => {
+const debounce = (func, delay) => {
+  let timeoutId;
+  return function(...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
+  };
+};
+
+const CommonTemplatesList = memo(({ options, onClickHandle, selectedItem }) => {
   const [commonTemplateModal, setCommonTemplateModal] = useState(false);
-  const toolTip = 'Select a template and Press CTRL + v inside the canvas.';
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredOptions, setFilteredOptions] = useState(options);
+  const toolTip = 'Select a template. Clicking inside the canvas and Press CTRL + v.';
 
   const onSelectItem = (item) => {
     onClickHandle(item);
     setCommonTemplateModal(false);
   };
 
+  const handleSearch = useCallback(
+    debounce((query) => {
+      const lowerCaseQuery = query.toLowerCase();
+      const filtered = options.filter((item) =>
+        item.name.toLowerCase().includes(lowerCaseQuery)
+      );
+      setFilteredOptions(filtered);
+    }, 300),
+    [options]
+  );
+
+  useEffect(() => {
+    handleSearch(searchQuery);
+  }, [searchQuery, handleSearch]);
+
   return (
     <div>
       <div className="common-template-header">
-        <div style={{width: '95%'}}>
+        <div style={{ width: '95%' }}>
           <ControlLabel>Common Templates:</ControlLabel>
         </div>
         <OverlayTrigger placement="top" overlay={<Tooltip id="commontemplates">{toolTip}</Tooltip>}>
@@ -39,25 +64,38 @@ const CommonTemplatesList = memo(({options, onClickHandle, selectedItem}) => {
       </div>
 
       <Modal show={commonTemplateModal} onHide={() => setCommonTemplateModal(false)}>
-        <Modal.Header closeButton />
+        <Modal.Header closeButton >
+          {toolTip}
+        </Modal.Header>
         <Modal.Body>
-          <Panel style={{height: 300}}>
+          <Panel style={{ height: 300 }}>
             <Panel.Heading>
               <Panel.Title>
                 Common Template list:
               </Panel.Title>
             </Panel.Heading>
-            <Panel.Body style={{height: '80%', overflow: 'auto'}}>
+            <Panel.Body style={{ height: '80%', overflow: 'auto' }}>
               <div>
-                {options.map((item, idx) =>
+                <input
+                  type="text"
+                  placeholder="Search templates by Name..."
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  className='common-template-search'
+                />
+                {filteredOptions.length != options.length &&
+                  <div style={{ marginVertical: '10px' }}>
+                    {filteredOptions.length} out of {options.length} templates found
+                  </div>
+                }
+                {filteredOptions.map((item, idx) => (
                   <CommonTemplateItem key={idx} item={item} onClickItem={(value) => onSelectItem(value)} />
-                )}
+                ))}
               </div>
             </Panel.Body>
           </Panel>
         </Modal.Body>
       </Modal>
-
     </div>
   );
 });
@@ -65,8 +103,7 @@ const CommonTemplatesList = memo(({options, onClickHandle, selectedItem}) => {
 export default CommonTemplatesList;
 
 CommonTemplatesList.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
   options: PropTypes.arrayOf(PropTypes.object).isRequired,
   onClickHandle: PropTypes.func.isRequired,
-  selectedItem: PropTypes.string.isRequired
+  selectedItem: PropTypes.string.isRequired,
 };
