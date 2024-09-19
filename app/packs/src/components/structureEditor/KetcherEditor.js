bcc1fc63fadfc20e807d09da7c1d6db2d664c955
Hadi <hadi-tariq@live.co.uk>
2024-09-19 15:09:03 +0200

    chore: atom, image binding with global store

--- app/packs/src/components/structureEditor/KetcherEditor.js
+++ app/packs/src/components/structureEditor/KetcherEditor.js
@@ -1,7 +1,13 @@
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/forbid-prop-types */
-import PropTypes, { node } from 'prop-types';
+import PropTypes from 'prop-types';
 import React, { useEffect, useRef, useState } from 'react';
+
+let connectionDS = {};
+function generateRandomNumber() {
+  console.log("called?");
+  return parseInt(Math.floor(Math.random() * 10000)); // 0 to 9999
+}
 
 function KetcherEditor(props) {
   const iframeRef = useRef();
@@ -24,47 +30,81 @@ function KetcherEditor(props) {
 
       // Function to handle each event
       async function handleEvent(eventName, data) {
-        console.log(`${eventName} event triggered with data:`, data);
+        // console.log(`${eventName} event triggered with data:`, data);
+        let ketFormat = await editorS.structureDef.editor.getKet();
+        ketFormat = JSON.parse(ketFormat);
+        let allNodes = [...ketFormat.root.nodes];
+
+        const mols = [];
+        const images = [];
+        allNodes.forEach((item, idx) => {
+          if (item?.type === 'image') {
+            images.push(allNodes[idx]);
+          } else {
+            mols.push(allNodes[idx]);
+          }
+        });
+
         data.forEach(async (item) => {
           switch (item?.operation) {
+            case "Upsert image":
+              connectionDS = {
+                [`mol_${mols.at(-1).$ref}`]: `img_${images.length - 1}`
+              };
+              break;
             case "Move image":
+              Object.keys(connectionDS).forEach((_) => {
+                const images_list = ketFormat.root.nodes.slice(mols.length, allNodes.length);
+                images_list.forEach((item, idx) => {
+                  const location = {
+                    x: item.boundingBox.x,
+                    y: item.boundingBox.y,
+                    z: -1
+                  };
+                  ketFormat[`mol${idx}`].atoms[0].location = Object.values(location);
+                  ketFormat[`mol${idx}`].atoms[1].location = Object.values(location);
+                  ketFormat[`mol${idx}`].stereoFlagPosition = location;
+                });
+                editorS.structureDef.editor.setMolecule(JSON.stringify(ketFormat));
+              });
+              break;
+            case "Move image!":
               const moleculePattern = /^mol\d+/;
-              let ketFormat = await editorS.structureDef.editor.getKet();
-              ketFormat = JSON.parse(ketFormat);
               let moleculeKeys = Object.keys(ketFormat);
               moleculeKeys = moleculeKeys?.filter(str => moleculePattern.test(str));
               const imageWithPositions = {};
               let imageNodes = [...ketFormat.root.nodes];
-              // imageNodes = moleculeKeys.slice(moleculeKeys.length);
               imageNodes.forEach((item, idx) => {
-                if (item?.type === 'image') {
-                  imageWithPositions[`my_alias`] = imageNodes[idx];
-                }
+                imageWithPositions[`temp_0${idx}`] = item?.type === 'image' ? imageNodes[idx] : "not_an_image";
               });
 
-              console.log({ imageWithPositions });
-              moleculeKeys.forEach((item, _) => {
-                const molecule = ketFormat[item];
-                const molecule_alias = molecule.atoms[0].alias;
-                const location = {
-                  x: imageWithPositions[molecule_alias].boundingBox.x,
-                  y: imageWithPositions[molecule_alias].boundingBox.y,
-                  z: -1
-                };
-                molecule.atoms[0].location = Object.values(location);
-                molecule.atoms[1].location = Object.values(location);
-                molecule.stereoFlagPosition = location;
-                ketFormat[item] = molecule;
-              });
-              editorS.structureDef.editor.setMolecule(JSON.stringify(ketFormat));
-              // const R_temp = R_elements_template({ x: "-1.2990", y: "-1.2500" });
-              // const updatedContent = modifyMolString(molfile, "elementEdit", R_temp);
-              // editorS._structureDef.editor.setMolecule(updatedContent);
-              // setEditor(editorS);
+              console.log(imageWithPositions);
+              if (Object.keys(imageWithPositions).length) {
+                moleculeKeys.forEach((item, _) => {
+                  const molecule = ketFormat[item];
+                  const molecule_alias = molecule.atoms[0].alias;
+                  console.log(molecule_alias);
+                  if (imageWithPositions[molecule_alias]) {
+                    const location = {
+                      x: imageWithPositions[molecule_alias].boundingBox.x,
+                      y: imageWithPositions[molecule_alias].boundingBox.y,
+                      z: -1
+                    };
+                    molecule.atoms[0].location = Object.values(location);
+                    molecule.atoms[1].location = Object.values(location);
+                    molecule.stereoFlagPosition = location;
+                    ketFormat[item] = molecule;
+                  }
+                });
+                // for later use
+                // window.surface_chem = {
+                //   "alias_00111": "love is the new alias"
+                // };
+                editorS.structureDef.editor.setMolecule(JSON.stringify(ketFormat));
+              }
+
               break;
-            case "Upsert image":
-              console.log("upsert");
-              break;
+
             case "Calculate implicit hydrogen":
               // console.log("R: LAST CURSOR POSITION", editorS._structureDef.editor.editor.lastCursorPosition);
               // console.log("R:__SELCTION", editorS._structureDef.editor.editor._selection);
@@ -74,8 +114,12 @@ function KetcherEditor(props) {
               // console.log("ADD ATOM:__SELCTION", editorS._structureDef.editor.editor._selection);
               break;
             default:
+              break;
             // console.log("ELEMENT DETAILS:__NEW", item);
+
           }
+          // editorS.structureDef.editor.setMolecule(JSON.stringify(ketFormat));
+
         });
       }
 
