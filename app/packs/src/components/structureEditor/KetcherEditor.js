86b79ae638abeb21ca88550c74f7b58dce60bce9
Hadi <hadi-tariq@live.co.uk>
2024-09-19 09:09:29 +0200

    chore: static combo drag ratom with image/template

--- app/packs/src/components/structureEditor/KetcherEditor.js
+++ app/packs/src/components/structureEditor/KetcherEditor.js
@@ -1,65 +1,7 @@
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/forbid-prop-types */
-import PropTypes from 'prop-types';
+import PropTypes, { node } from 'prop-types';
 import React, { useEffect, useRef, useState } from 'react';
-
-
-
-
-function addElementAtIndex(array, index, newElement) {
-  // Check if the index is within bounds
-  if (index >= 0 && index <= array.length) {
-    // Use splice to insert the new element at the specified index
-    array.splice(index, 0, newElement);
-  } else {
-    console.error("Index out of bounds");
-  }
-
-  return array;
-}
-
-function modifyMolString(molfile, type, newElement) {
-  // list of lines in molfile
-  const line = molfile.split("\n");
-  const atomStart = 3;
-  // console.log(line);
-  // file header type i.e INDIGO
-  const fileType = line[1];
-
-  // file atoms & bonds info
-  const fileInfo = line[3].trim().split(" ");
-
-  // number of atoms in a file
-  const numberOfAtoms = parseInt(fileInfo[0].trim());
-  const positionToAddNewAtom = 3 + numberOfAtoms + 1;
-  // number of bonds in a file
-  const numberOfBonds = parseInt(fileInfo[1].trim());
-
-  // adding numberOfAtoms, numberOfBonds you get a line number to place extra content
-  const rowNumToStartAdding = numberOfAtoms + numberOfBonds + 1;
-  let newLines = [];
-  if (type === 'elementEdit') {
-    // temp
-    newLines = addElementAtIndex(line, positionToAddNewAtom, newElement);
-    if (newLines[line.length - 2].indexOf("RGP") !== -1) newLines[line.length - 2] += `1  ${numberOfAtoms}  1`;
-    else {
-      newLines = addElementAtIndex(line, line.length - 2, `M  RGP  1  ${numberOfAtoms}  1`);
-    }
-    // increment atoms numbers
-    fileInfo[0] = numberOfAtoms + 1;
-    line[3] = " " + fileInfo.join(" ");
-
-    // add alias
-    newLines = addElementAtIndex(line, numberOfAtoms + numberOfBonds + atomStart + 2, `A   ${numberOfAtoms + 1}`);
-    newLines = addElementAtIndex(line, numberOfAtoms + numberOfBonds + atomStart + 2 + 1, `My alias`);
-  }
-  console.log({ newLines });
-
-  return newLines.join("\n");
-}
-
-// const R_elements_template = ({ x, y }) => "    " + x + "   " + y + "   0.0000 R#  0  0  0  0  0  0  0  0  0  0  0  0";
-const R_elements_template = ({ }) => "    4.4750   -4.4750    0.0000 R#  0  0  0  0  0  0  0  0  0  0  0  0";
 
 function KetcherEditor(props) {
   const iframeRef = useRef();
@@ -73,64 +15,69 @@ function KetcherEditor(props) {
 
 
 
-  const loadContent = (event) => {
+  const loadContent = async (event) => {
     if (event.data.eventType === 'init') {
-      window.editor = editorS;
       editorS.structureDef.editor.setMolecule(initMol);
-
-      // editor.structureDef.editor.editor.renderAndRecoordinateStruct();
-      // editor.structureDef.editor.setMolecule(initMol);
       const list = [
-        "elementEdit",
-        "bondEdit",
-        "rgroupEdit",
-        "sgroupEdit",
-        "sdataEdit",
-        "quickEdit",
-        "attachEdit",
-        "removeFG",
         "change",
-        "quickEdit",
-        // "click"
-        // "dblclick",
-        // "mousedown",
-        // "mousemove",
-        // "mouseup",
-        // "mouseleave",
-        // "mouseover",
-        // "showInfo",
-        // "message",
       ];
 
       // Function to handle each event
       async function handleEvent(eventName, data) {
-        // console.log(`${eventName} event triggered with data:`, data, molfile);
-        console.log(await editor.structureDef.editor.getKet(), "editor?");
-        return;
+        console.log(`${eventName} event triggered with data:`, data);
         data.forEach(async (item) => {
           switch (item?.operation) {
             case "Move image":
-            case "Upsert image":
-              const R_temp = R_elements_template({ x: "-1.2990", y: "-1.2500" });
-              const updatedContent = modifyMolString(molfile, "elementEdit", R_temp);
-              // console.log(await editor.structureDef.editor.getMolfile(), "editor?");
-              editorS._structureDef.editor.setMolecule(updatedContent);
+              const moleculePattern = /^mol\d+/;
+              let ketFormat = await editorS.structureDef.editor.getKet();
+              ketFormat = JSON.parse(ketFormat);
+              let moleculeKeys = Object.keys(ketFormat);
+              moleculeKeys = moleculeKeys?.filter(str => moleculePattern.test(str));
+              const imageWithPositions = {};
+              let imageNodes = [...ketFormat.root.nodes];
+              // imageNodes = moleculeKeys.slice(moleculeKeys.length);
+              imageNodes.forEach((item, idx) => {
+                if (item?.type === 'image') {
+                  imageWithPositions[`my_alias`] = imageNodes[idx];
+                }
+              });
+
+              console.log({ imageWithPositions });
+              moleculeKeys.forEach((item, _) => {
+                const molecule = ketFormat[item];
+                const molecule_alias = molecule.atoms[0].alias;
+                const location = {
+                  x: imageWithPositions[molecule_alias].boundingBox.x,
+                  y: imageWithPositions[molecule_alias].boundingBox.y,
+                  z: -1
+                };
+                molecule.atoms[0].location = Object.values(location);
+                molecule.atoms[1].location = Object.values(location);
+                molecule.stereoFlagPosition = location;
+                ketFormat[item] = molecule;
+              });
+              editorS.structureDef.editor.setMolecule(JSON.stringify(ketFormat));
+              // const R_temp = R_elements_template({ x: "-1.2990", y: "-1.2500" });
+              // const updatedContent = modifyMolString(molfile, "elementEdit", R_temp);
+              // editorS._structureDef.editor.setMolecule(updatedContent);
               // setEditor(editorS);
               break;
+            case "Upsert image":
+              console.log("upsert");
+              break;
             case "Calculate implicit hydrogen":
-              console.log("R: LAST CURSOR POSITION", editorS._structureDef.editor.editor.lastCursorPosition);
-              console.log("R:__SELCTION", editorS._structureDef.editor.editor._selection);
+              // console.log("R: LAST CURSOR POSITION", editorS._structureDef.editor.editor.lastCursorPosition);
+              // console.log("R:__SELCTION", editorS._structureDef.editor.editor._selection);
               break;
             case "Add atom":
-              console.log("ADD ATOM: LAST CURSOR POSITION", editorS._structureDef.editor.editor.lastCursorPosition);
-              console.log("ADD ATOM:__SELCTION", editorS._structureDef.editor.editor._selection);
+              // console.log("ADD ATOM: LAST CURSOR POSITION", editorS._structureDef.editor.editor.lastCursorPosition);
+              // console.log("ADD ATOM:__SELCTION", editorS._structureDef.editor.editor._selection);
               break;
             default:
-              console.log("ELEMENT DETAILS:__NEW", item);
+            // console.log("ELEMENT DETAILS:__NEW", item);
           }
         });
       }
-
 
       // Subscribe to each event in the list
       list.forEach(eventName => {
@@ -139,7 +86,7 @@ function KetcherEditor(props) {
           handleEvent(eventName, result);
         });
       });
-    }
+    };
   };
 
   useEffect(() => {
