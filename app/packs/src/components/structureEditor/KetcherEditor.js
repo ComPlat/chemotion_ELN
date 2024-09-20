986b5631f8150e838677e589ddce789ec092e25d
Hadi <hadi-tariq@live.co.uk>
2024-09-20 11:11:30 +0200

    chore: on add image remove bonds to avoid alias modal issue

--- app/packs/src/components/structureEditor/KetcherEditor.js
+++ app/packs/src/components/structureEditor/KetcherEditor.js
@@ -5,7 +5,7 @@ import React, { useEffect, useRef, useState } from 'react';
 
 function KetcherEditor(props) {
   const iframeRef = useRef();
-  const [editorS, setEditor] = useState(props.editor);
+  const [editorS] = useState(props.editor);
   let {
     iH, iS, molfile
   } = props;
@@ -35,7 +35,6 @@ function KetcherEditor(props) {
       }
     });
 
-
     const mols = [];
     Object.keys(ketFormat)?.forEach((item) => {
       if (ketFormat[item]?.atoms?.length && ketFormat[item]?.atoms[0]?.alias) mols.push(item);
@@ -44,64 +43,30 @@ function KetcherEditor(props) {
     data.forEach(async (item) => {
       switch (item?.operation) {
         case "Upsert image":
-          console.log("upsert image!!!");
+          console.log(ketFormat[`mol${mols.length - 1}`]);
+          delete ketFormat[`mol${mols.length - 1}`].sgroups;
+          delete ketFormat[`mol${mols.length - 1}`].bonds;
+          ketFormat[`mol${mols.length - 1}`].atoms.splice(1, 1);
+          editorS.structureDef.editor.setMolecule(JSON.stringify(ketFormat));
           break;
         case "Move image":
           const images_list = ketFormat.root.nodes.slice(allNodes.length - mols.length, allNodes.length);
-          console.log(mols.length - allNodes.length, allNodes.length);
           images_list.forEach((item, idx) => {
             const location = {
               x: item.boundingBox.x + item.boundingBox.width / 2,
               y: item.boundingBox.y - item.boundingBox.height / 2,
-              z: -1
+              z: 0
             };
             if (ketFormat[mols[idx]].atoms[0].alias) {
-              ketFormat[mols[idx]].atoms[0].location = Object.values(location);
-              ketFormat[mols[idx]].atoms[1].location = Object.values(location);
+              ketFormat[mols[idx]].atoms[0].location = [...Object.values(location)];
+              console.log(ketFormat[mols[idx]].atoms[0]);
+              if (ketFormat[mols[idx]].atoms[1]) {
+                ketFormat[mols[idx]].atoms[1].location = Object.values(location);
+              }
               ketFormat[mols[idx]].stereoFlagPosition = location;
             }
           });
           editorS.structureDef.editor.setMolecule(JSON.stringify(ketFormat));
-          break;
-        case "Move image!":
-          const moleculePattern = /^mol\d+/;
-          let moleculeKeys = Object.keys(ketFormat);
-          moleculeKeys = moleculeKeys?.filter(str => moleculePattern.test(str));
-          const imageWithPositions = {};
-          let imageNodes = [...ketFormat.root.nodes];
-          imageNodes.forEach((item, idx) => {
-            imageWithPositions[`temp_0${idx}`] = item?.type === 'image' ? imageNodes[idx] : "not_an_image";
-          });
-
-          if (Object.keys(imageWithPositions).length) {
-            moleculeKeys.forEach((item, _) => {
-              const molecule = ketFormat[item];
-              const molecule_alias = molecule.atoms[0].alias;
-              console.log(molecule_alias);
-              if (imageWithPositions[molecule_alias]) {
-                const location = {
-                  x: imageWithPositions[molecule_alias].boundingBox.x,
-                  y: imageWithPositions[molecule_alias].boundingBox.y,
-                  z: -1
-                };
-                molecule.atoms[0].location = Object.values(location);
-                molecule.atoms[1].location = Object.values(location);
-                molecule.stereoFlagPosition = location;
-                ketFormat[item] = molecule;
-              }
-            });
-            // for later use
-            // window.surface_chem = {
-            //   "alias_00111": "love is the new alias"
-            // };
-            editorS.structureDef.editor.setMolecule(JSON.stringify(ketFormat));
-          }
-
-          break;
-
-        case "Calculate implicit hydrogen":
-          // console.log("R: LAST CURSOR POSITION", editorS._structureDef.editor.editor.lastCursorPosition);
-          // console.log("R:__SELCTION", editorS._structureDef.editor.editor._selection);
           break;
         case "Add atom":
           // console.log("ADD ATOM: LAST CURSOR POSITION", editorS._structureDef.editor.editor.lastCursorPosition);
@@ -109,7 +74,6 @@ function KetcherEditor(props) {
           break;
         default:
           break;
-        // console.log("ELEMENT DETAILS:__NEW", item);
 
       }
       // editorS.structureDef.editor.setMolecule(JSON.stringify(ketFormat));
