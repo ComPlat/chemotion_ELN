caedd88d7fe873d68ac897867ed78c658a00b5d9
Pei Chi Huang <pei-chi.huang@kit.edu>
2024-09-30 19:57:37 +0800

    fix: oidc params issue (#2190)
    where `params` was expected to have accessor methods, but was actually a hash
    
    also resolved #2183

--- app/packs/src/apps/omniauthCredential/LoginOptions.js
+++ app/packs/src/apps/omniauthCredential/LoginOptions.js
@@ -10,6 +10,15 @@ import uuid from 'uuid';
 import UserStore from 'src/stores/alt/stores/UserStore';
 import UserActions from 'src/stores/alt/actions/UserActions';
 
+function omniauthLabel(icon, name) {
+  if (icon) {
+    return (
+      <img src={`/images/providers/${icon}`} alt={name} title={name} />
+    );
+  }
+  return name;
+}
+
 export default class LoginOptions extends Component {
   constructor(props) {
     super(props);
@@ -41,7 +50,7 @@ export default class LoginOptions extends Component {
       <Col key={uuid.v1()} md={12 / keys.length} className="login-options">
         <Button href={`/users/auth/${key}`}>
           Login with &nbsp;
-          <img src={`/images/providers/${omniauthProviders[key].icon}`} className="kit-logo" alt={key} title={key} />
+          {omniauthLabel(omniauthProviders[key].icon, omniauthProviders[key].label || key)}
         </Button>
       </Col>
     ));
