3088380619da5626dc8a294025cb013d158e8f76
Luciano Maiwald <mail@luciano-maiwald.de>
2024-10-08 11:40:06 +0200

    Add styleguide stub

---
+++ app/packs/src/apps/styleguide/Styleguide.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Styleguide = () => {
+  return (
+    <Container className="my-3">
+      <h1>Chemotion Styleguide</h1>
+    </Container>
+  )
+}
+
+export default Styleguide;
