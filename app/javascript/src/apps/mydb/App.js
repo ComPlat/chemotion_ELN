86af786eda220f4f62d32c2aafde67b43109ffd3
Luciano Maiwald <mail@luciano-maiwald.de>
2024-10-10 15:08:47 +0200

    fix: Do not size inbox modal based on whether the collection tree is shown

--- app/javascript/src/apps/mydb/App.js
+++ app/javascript/src/apps/mydb/App.js
@@ -146,7 +146,7 @@ class App extends Component {
           <LoadingModal />
           <ProgressModal />
         </Row>
-        <InboxModal showCollectionTree={showCollectionTree} />
+        <InboxModal />
         <Calendar />
       </Container>
     );
