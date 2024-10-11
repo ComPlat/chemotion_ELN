13cefccedbb91a09be0bec26b0d75be1670ce720
Luciano Maiwald <mail@luciano-maiwald.de>
2024-10-11 11:59:59 +0200

    Restructure App layout
    This patch introduces the app layout structure proposed by Shapefield'
    redesign document.

--- app/javascript/src/apps/mydb/elements/Elements.js
+++ app/javascript/src/apps/mydb/elements/Elements.js
@@ -20,6 +20,7 @@ export default class Elements extends Component {
 
   componentDidMount() {
     ElementStore.listen(this.handleOnChange);
+    this.handleOnChange(ElementStore.getState());
   }
 
   componentWillUnmount() {
@@ -41,7 +42,7 @@ export default class Elements extends Component {
     const listWidth = hasCurrentElement ? 5 : 12;
 
     return (
-      <Row className='w-100'>
+      <Row className="flex-grow-1">
         <Col xs={listWidth}>
           <ElementsList
             overview={!hasCurrentElement}
