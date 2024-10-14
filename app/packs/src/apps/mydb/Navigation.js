40de13467cdd48deef45c2d4b3c62321d4d62abd
Luciano Maiwald <mail@luciano-maiwald.de>
2024-10-14 17:38:41 +0200

    Separate Home and mydb Navigation components
    With the new layout, the two components will look and function
    differently.

--- app/packs/src/apps/mydb/Navigation.js
+++ app/packs/src/apps/mydb/Navigation.js
@@ -14,7 +14,6 @@ import NavNewSession from 'src/components/navigation/NavNewSession';
 import NavHead from 'src/components/navigation/NavHead';
 import DocumentHelper from 'src/utilities/DocumentHelper';
 import NavigationModal from 'src/components/navigation/NavigationModal';
-import PropTypes from 'prop-types';
 import OpenCalendarButton from 'src/components/calendar/OpenCalendarButton';
 
 const colMenuTooltip = <Tooltip id="col_menu_tooltip">Toggle sidebar</Tooltip>;
@@ -93,21 +92,17 @@ export default class Navigation extends React.Component {
   }
 
   navHeader() {
-    const { isHidden } = this.props;
-
     return (
       <div className="d-flex gap-4">
-        {!isHidden && (
-          <Navbar.Text>
-            <OverlayTrigger placement="right" delayShow={1000} overlay={colMenuTooltip}>
-              <i
-                className="fa fa-list"
-                onClick={this.toggleCollectionTree}
-                role="button"
-              />
-            </OverlayTrigger>
-          </Navbar.Text>
-        )}
+        <Navbar.Text>
+          <OverlayTrigger placement="right" delayShow={1000} overlay={colMenuTooltip}>
+            <i
+              className="fa fa-list"
+              onClick={this.toggleCollectionTree}
+              role="button"
+            />
+          </OverlayTrigger>
+        </Navbar.Text>
         <NavHead />
       </div>
     );
@@ -137,42 +132,31 @@ export default class Navigation extends React.Component {
     const {
       currentUser, modalProps, omniauthProviders, extraRules
     } = this.state;
-    const { isHidden } = this.props;
     const { profile } = UserStore.getState();
     const { customClass } = (profile && profile.data) || {};
     return (
       <Navbar className="bg-gray-200 justify-content-between px-4">
         {this.navHeader()}
-        {!isHidden && (
-          <Nav navbar className="navbar-form gap-2 mx-auto d-flex flex-nowrap">
-            <div className="d-flex align-items-center flex-nowrap">
-              <Search noSubmit={!!currentUser} className="w-auto" />
-            </div>
-            {currentUser && (
-              <>
-                <ManagingActions
-                  updateModalProps={this.updateModalProps}
-                  customClass={customClass}
-                />
-                <ContextActions
-                  updateModalProps={this.updateModalProps}
-                  customClass={customClass}
-                />
-                <NavigationModal {...modalProps} />
-              </>
-            )}
-          </Nav>
-        )}
+        <Nav navbar className="navbar-form gap-2 mx-auto d-flex flex-nowrap">
+          <div className="d-flex align-items-center flex-nowrap">
+            <Search noSubmit={!!currentUser} className="w-auto" />
+          </div>
+          {currentUser && (
+            <>
+              <ManagingActions
+                updateModalProps={this.updateModalProps}
+                customClass={customClass}
+              />
+              <ContextActions
+                updateModalProps={this.updateModalProps}
+                customClass={customClass}
+              />
+              <NavigationModal {...modalProps} />
+            </>
+          )}
+        </Nav>
         {this.userSession(omniauthProviders, extraRules)}
       </Navbar>
     );
   }
 }
-
-Navigation.propTypes = {
-  isHidden: PropTypes.bool,
-};
-
-Navigation.defaultProps = {
-  isHidden: false,
-};
