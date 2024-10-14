c6ef7a7c23403317180a36a9e2efbb1efd12f07c
Hadi <hadi-tariq@live.co.uk>
2024-10-14 16:59:48 +0200

    feat: molfile compatibility within editors
    refactor: mofile import/export compatibility
    
    feat: shapes synced with ketcher rails
    
    chore: standard size defined for shapes
    
    draft: adding polymer images to svg
    
    chore: svg svg with editor save
    
    fix: loofah image tag allowed for sanitization, shapes display rails-ketcher vice-versa
    
    refactor: molfile pre-processors, flags, helper function for handleAddAtom process
    
    refactor: templates source
    
    refactor: delete template and restore image-counters
    
    refactor: DOM > images update flags setter & getters
    
    refactor: tags polymer identifiers
    
    refactor: event-handler and event-fire for ketcher2 changes
    
    refactor: ketcher button events
    
    molecule file reset
    
    refactor: template list for local storage
    
    fix: file with no polyer main cb
    
    fix: clean canvas use-cases
    
    draft: counter issue with canvas is new
    
    fix: addHandleAtom with seen_third_parts, avoiding duplicates for image_used_counter increament
    
    chore: template force alias modification removed
    
    code clean
    
    surface shape size changed && image selection action overide
    
    chore: hide extra attributes for user templates list
    
    fix: image delete overide, t_02 template size changed
    
    draft: removal of linked atom with image on image delete
    
    fix: on delete image re-sync counters and delete atom
    
    fix: contant image size on layout and on all actions
    
    feat: redo and undo
    
    fix: multiple move atoms makes add atom disable
    
    fix: multi template delete, remove atom with image delete and vice versa
    
    update: delete working with multi images, single atom with alias, or single image
    
    fix:event: delete image
    
    fix: delete with image or delete with selection or delete with atom
    
    fix: image movement after delete
    
    draft:refactor
    
    code clean
    
    refac: savemolfile
    
    draft: sb/schema.rb rebase
    
    refac: scrubber, svf-processor
    
    Test+Refactoring: reading molfile with/without polymers; test for hasKetcherData
    
    test: setKetcherdata
    
    test: hasKetcherdata, setKetcherdata
    
    refactoring: onTemplateMove
    
    refactoring+test: movetemplate & placeImageOnAtom
    
    Refac+test: on add atom
    
    test: on atom add image counter checker
    
    refactor+tests: on image delete, mockup files for test
    
    test: on delete image delete atom with connected alias; with single or multi images|
    
    test+refac: on delete atom
    
    clean+refac: add atom logic divied
    
    chore: code doc, template-list shape alignment
    
    fix: delete atom when a mol is dropped on other mol; delete mol1 and update aliases for mol0
    
    specs fix ketcher Editor
    
    refactoring: delete image, read mofile, delete atom(single/group)
    
    refactoring+fixes: delete atom by image, delete image by atom, savingMofile
    
    fix: delete image when single atom behind is deleted
    
    fix: delete atom case when dragged index is smaller then others
    
    chore: template names updated bead, surface
    
    cleanup + minor fixes
    
    test: delete atom to reset aliases, molfile save
    
    Refactoring, comments, templates-source parsing
    
    Ketcher2 Surface Chemistry feature documentation:v1
    
    fix: reading molfile for saving
    
    fix: polymers index
    
    chore: image tag filter applied
    
    Chore: Ket2 Surface Chemistry feature docs
    
    test: template list processing
    
    fix: delete atom with select, behind atom, template, calculate pi event, minor feature-docs changes
    
    fix: delete template on click atom
    
    fix: utils moved and delete atom minor fix
    
    fix: image placement index with refactoring
    
    fix: on save molfile
    
    fix: resolve ESLint warnings and errors
    
    chore: linting, correct template matching while saving
    
    clean: Identifying polymers list
    
    chore: packs moved, docker configs

--- app/javascript/src/utilities/UserTemplatesHelpers.js
+++ app/javascript/src/utilities/UserTemplatesHelpers.js
@@ -1,13 +1,14 @@
 import ProfilesFetcher from 'src/fetchers/ProfilesFetcher';
 import UsersFetcher from 'src/fetchers/UsersFetcher';
+
 const key = 'ketcher-tmpls';
 
 const createAddAttachmentidToNewUserTemplate = async (newValue, newItem, deleteIdx) => {
   const res = await ProfilesFetcher.uploadUserTemplates({
     content: JSON.stringify(newItem),
-  }).catch(err => console.log("err in create"));
+  }).catch((err) => console.log('err in create'));
   const attachment_id = res?.template_details?.filename;
-  newItem['props']['path'] = attachment_id;
+  newItem.props.path = attachment_id;
   newValue[newValue.length - 1] = newItem;
   if (deleteIdx) newValue.splice(deleteIdx, 1);
   window.removeEventListener('storage', null);
@@ -39,9 +40,7 @@ const updateUserTemplateDetails = async (oldValue, newValue) => {
     if (!exists) {
       await ProfilesFetcher.deleteUserTemplate({
         path: oldValue[i].props.path,
-      }).catch(() =>
-        console.log('ISSUE WITH DELETE', localItem?.props?.path)
-      );
+      }).catch(() => console.log('ISSUE WITH DELETE', localItem?.props?.path));
       createAddAttachmentidToNewUserTemplate(newValue, newValue[i], i);
       break;
     }
