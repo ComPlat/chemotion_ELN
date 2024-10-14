50fff4e67d38ef1c2e766e037f7ddb2134472f94
Luciano Maiwald <mail@luciano-maiwald.de>
2024-10-14 17:38:41 +0200

    Separate Home and mydb Navigation components
    With the new layout, the two components will look and function
    differently.

--- app/javascript/src/apps/home/index.js
+++ app/javascript/src/apps/home/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from 'src/apps/home/Home';
+import { RootStore, StoreContext } from 'src/stores/mobx/RootStore';
 
 document.addEventListener('DOMContentLoaded', () => {
   const domElement = document.getElementById('Home');
-  if (domElement) { ReactDOM.render(<Home />, domElement); }
+  if (domElement) {
+    ReactDOM.render(
+      <StoreContext.Provider value={RootStore.create({})}>
+        <Home />
+      </StoreContext.Provider>,
+      domElement
+    );
+  }
 });
