efc9d1e1a0a2a99ac4f75e037cb2b942e5453a07
Joachim Tillessen <tillessen@emjot.de>
2024-10-16 23:37:04 +0200

    Make content readable with new bg color

--- app/packs/src/apps/mydb/elements/list/ElementsTableSampleEntries.js
+++ app/packs/src/apps/mydb/elements/list/ElementsTableSampleEntries.js
@@ -145,7 +145,6 @@ function MoleculeHeader({ sample, show, showPreviews, showDragColumn, onClick, t
 
   return (
     <tr
-      className="bg-gray-100"
       role="button"
       onClick={onClick}
     >
@@ -428,7 +427,7 @@ export default class ElementsTableSampleEntries extends Component {
     const showGroup = !moleculeGroupsShown.includes(moleculeName) && !collapseAll;
 
     return (
-      <tbody key={index}>
+      <tbody key={index} className="sheet">
         <MoleculeHeader
           sample={moleculeGroup[0]}
           show={showGroup}
