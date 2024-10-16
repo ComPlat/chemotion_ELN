1740b821613acf191deb7d0911e482545cb8dea1
Beate Quednau <beate@firma-pflaum.de>
2024-10-16 15:25:34 +0200

    Remove not necessary ddQuillId

--- app/packs/src/components/reactQuill/ToolbarDropdown.js
+++ app/packs/src/components/reactQuill/ToolbarDropdown.js
@@ -19,9 +19,6 @@ export default class ToolbarDropdown extends React.Component {
     this.pickerSpan = React.createRef();
     this.pickerLabel = React.createRef();
 
-    const genId = Math.random().toString().substr(2, 10);
-    this.ddQuillId = `ql-dropdown-${genId}`;
-
     const sheets = document.styleSheets;
     this.sheet = sheets[sheets.length - 1];
 
@@ -79,7 +76,7 @@ export default class ToolbarDropdown extends React.Component {
 
     const { expanded } = this.state;
     const expandedClass = expanded ? 'ql-expanded' : '';
-    const className = `${this.ddQuillId} ql-picker ${expandedClass}`;
+    const className = `ql-picker ${expandedClass}`;
 
     return (
       <span className="ql-formats">
