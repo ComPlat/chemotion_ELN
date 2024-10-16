55c154ec14a7b2838a70fe41a63b9608a1ae80a6
Luciano Maiwald <mail@luciano-maiwald.de>
2024-10-16 12:31:54 +0200

    Add basic Sidebar toggling

--- app/javascript/src/apps/mydb/layout/Sidebar.js
+++ app/javascript/src/apps/mydb/layout/Sidebar.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
+import classNames from 'classnames';
 
 import NavHead from "src/components/navigation/NavHead";
 import CollectionTree from "src/apps/mydb/collections/CollectionTree";
@@ -10,17 +12,28 @@ import SampleTaskNavigationElement from "src/components/sampleTaskInbox/SampleTa
 import OpenCalendarButton from "src/components/calendar/OpenCalendarButton";
 import NoticeButton from "src/components/contextActions/NoticeButton";
 
-export default function Sidebar() {
+export default function Sidebar({ isCollapsed, toggleCollapse }) {
   return (
     <div className="h-100 d-flex flex-column">
       <div className="d-flex flex-column flex-grow-1 overflow-y-auto h-0">
         <div className="d-flex justify-content-center py-3">
           <NavHead />
         </div>
-        <CollectionTree />
+        <Button
+          onClick={toggleCollapse}
+          variant="light"
+        >
+          <i className="fa fa-exchange" />
+        </Button>
+        {!isCollapsed && (
+          <CollectionTree />
+        )}
         <CollectionManagementButton />
       </div>
-      <div className="d-flex justify-content-center gap-3 py-4">
+      <div className={classNames(
+        'd-flex justify-content-center gap-3 py-4',
+        { 'flex-column align-items-center': isCollapsed }
+      )}>
         <InboxButton />
         <SampleTaskNavigationElement />
         <OpenCalendarButton />
@@ -36,3 +49,8 @@ export default function Sidebar() {
     </div>
   );
 }
+
+Sidebar.propTypes = {
+  isCollapsed: PropTypes.bool.isRequired,
+  toggleCollapse: PropTypes.func.isRequired,
+};
