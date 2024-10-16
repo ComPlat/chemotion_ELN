8f9ace18bfff8b79c814b620afbafb7e20a33d1e
Luciano Maiwald <mail@luciano-maiwald.de>
2024-10-16 12:14:36 +0200

    Update CollectionManagement to new layout, move button to sidebar

--- app/javascript/src/apps/mydb/collections/CollectionManagement.js
+++ app/javascript/src/apps/mydb/collections/CollectionManagement.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
 
+import ScrollContainer from 'src/components/common/ScrollContainer';
 import MyCollections from 'src/apps/mydb/collections/MyCollections';
 import MySharedCollections from 'src/apps/mydb/collections/MySharedCollections';
 import SharedWithMeCollections from 'src/apps/mydb/collections/SharedWithMeCollections';
@@ -9,13 +10,33 @@ import CollectionTabs from 'src/apps/mydb/collections/CollectionTabs';
 
 export default function CollectionManagement() {
   return (
-    <div className="tabs-container--with-borders">
+    <div className="tabs-container--with-borders tabs-container--with-full-height">
       <Tabs defaultActiveKey={0} id="collection-management-tab">
-        <Tab eventKey={0} title="My Collections"><MyCollections /></Tab>
-        <Tab eventKey={1} title="My Shared Collections"><MySharedCollections /></Tab>
-        <Tab eventKey={2} title="Collections shared with me "><SharedWithMeCollections /></Tab>
-        <Tab eventKey={3} title="Collections synchronized with me "><SyncWithMeCollections /></Tab>
-        <Tab eventKey={4} title="Collection Tabs"><CollectionTabs /></Tab>
+        <Tab eventKey={0} title="My Collections">
+          <ScrollContainer>
+            <MyCollections />
+          </ScrollContainer>
+        </Tab>
+        <Tab eventKey={1} title="My Shared Collections">
+          <ScrollContainer>
+            <MySharedCollections />
+          </ScrollContainer>
+        </Tab>
+        <Tab eventKey={2} title="Collections shared with me ">
+          <ScrollContainer>
+            <SharedWithMeCollections />
+          </ScrollContainer>
+        </Tab>
+        <Tab eventKey={3} title="Collections synchronized with me ">
+          <ScrollContainer>
+            <SyncWithMeCollections />
+          </ScrollContainer>
+        </Tab>
+        <Tab eventKey={4} title="Collection Tabs">
+          <ScrollContainer>
+            <CollectionTabs />
+          </ScrollContainer>
+        </Tab>
       </Tabs>
     </div>
   );
