b16ecacfe943b76267c27905312c59093386b864
Luciano Maiwald <mail@luciano-maiwald.de>
2024-10-16 08:41:59 +0200

    chore: Remove use of `user.profile.data.customClass`
    There does not seem to be a way to modify this value and there is no
    personally customizable theming in the new design.

--- app/javascript/src/components/contextActions/ExportImportButton.js
+++ app/javascript/src/components/contextActions/ExportImportButton.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import { Dropdown, ButtonGroup } from 'react-bootstrap';
 
 import UIStore from 'src/stores/alt/stores/UIStore';
@@ -26,7 +25,7 @@ const editMetadataFunction = () => {
   });
 };
 
-function ExportImportButton({ customClass }) {
+function ExportImportButton() {
   const [modal, showModal] = useState(null);
   const hideModal = () => showModal(null);
 
@@ -76,7 +75,7 @@ function ExportImportButton({ customClass }) {
   return (
     <>
       <Dropdown as={ButtonGroup} id="export-dropdown">
-        <Dropdown.Toggle variant="light" className={customClass}>
+        <Dropdown.Toggle variant="light">
           <i className="fa fa-download" />
           <i className="fa fa-upload ms-1" />
         </Dropdown.Toggle>
@@ -141,13 +140,5 @@ function ExportImportButton({ customClass }) {
     </>
   );
 }
-
-ExportImportButton.propTypes = {
-  customClass: PropTypes.string,
-};
-
-ExportImportButton.defaultProps = {
-  customClass: null,
-};
 
 export default ExportImportButton;
