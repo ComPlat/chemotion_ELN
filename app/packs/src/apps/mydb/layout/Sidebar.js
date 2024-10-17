f1df010fe758f11dc76416918650718d1eaaae11
Joachim Tillessen <tillessen@emjot.de>
2024-10-17 15:21:40 +0200

    rudimentary placement of sidebar toggle button

--- app/packs/src/apps/mydb/layout/Sidebar.js
+++ app/packs/src/apps/mydb/layout/Sidebar.js
@@ -14,32 +14,37 @@ import NoticeButton from 'src/components/contextActions/NoticeButton';
 
 export default function Sidebar({ isCollapsed, toggleCollapse }) {
   return (
-    <div className="h-100 d-flex flex-column">
-      <div className="d-flex flex-column flex-grow-1 overflow-y-auto h-0">
-        <div className="d-flex justify-content-center py-3">
-          <NavHead />
-        </div>
+    <>
+      <div className="sidebar-collapse-button-container">
         <Button
           onClick={toggleCollapse}
           variant="light"
+          className="sidebar-collapse-button"
         >
           <i className="fa fa-exchange" />
         </Button>
-        {!isCollapsed && (
-          <CollectionTree />
-        )}
-        <CollectionManagementButton />
       </div>
-      <div className={classNames(
-        'd-flex justify-content-center gap-3 py-4',
-        { 'flex-column align-items-center': isCollapsed }
-      )}>
-        <InboxButton />
-        <SampleTaskNavigationElement />
-        <OpenCalendarButton />
-        <NoticeButton />
+      <div className="h-100 d-flex flex-column">
+        <div className="d-flex flex-column flex-grow-1 overflow-y-auto h-0">
+          <div className="d-flex justify-content-center py-3">
+            <NavHead />
+          </div>
+          {!isCollapsed && (
+            <CollectionTree />
+          )}
+          <CollectionManagementButton />
+        </div>
+        <div className={classNames(
+          'd-flex justify-content-center gap-3 py-4',
+          { 'flex-column align-items-center': isCollapsed }
+        )}>
+          <InboxButton />
+          <SampleTaskNavigationElement />
+          <OpenCalendarButton />
+          <NoticeButton />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
