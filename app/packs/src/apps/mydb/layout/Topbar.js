e7dd4d48fed5b6eb4cee1ec2651a47ac35af4c13
Luciano Maiwald <mail@luciano-maiwald.de>
2024-10-17 16:02:08 +0200

    Add Support dropdown

--- app/packs/src/apps/mydb/layout/Topbar.js
+++ app/packs/src/apps/mydb/layout/Topbar.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { ButtonGroup } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { ButtonGroup, Dropdown, DropdownButton } from 'react-bootstrap';
 
+import UIStore from 'src/stores/alt/stores/UIStore';
 import Search from 'src/components/navigation/search/Search';
 import CreateButton from 'src/components/contextActions/CreateButton';
 import SplitElementButton from 'src/components/contextActions/SplitElementButton';
@@ -9,7 +10,29 @@ import ExportImportButton from 'src/components/contextActions/ExportImportButton
 import ScanCodeButton from 'src/components/contextActions/ScanCodeButton';
 import UserAuth from 'src/components/navigation/UserAuth';
 
+function ExternalItem({ title, href }) {
+  return (
+    <Dropdown.Item
+      className="d-flex gap-3 align-items-baseline justify-content-between"
+      href={href}
+      target="_blank"
+    >
+      {title}
+      <i className="fa fa-external-link" />
+    </Dropdown.Item>
+  );
+}
+
 export default function Topbar() {
+  const [version, setVersion] = useState({});
+  const onUiStoreChange = ({ version }) => setVersion(version);
+  useEffect(() => {
+    UIStore.listen(onUiStoreChange);
+    onUiStoreChange(UIStore.getState());
+    return () => UIStore.unlisten(onUiStoreChange);
+  }, []);
+  const hasVersions = version && Object.keys(version).length > 1;
+
   return (
     <div className="d-flex justify-content-between py-3">
       <Search />
@@ -23,6 +46,31 @@ export default function Topbar() {
         <ExportImportButton />
         <ReportUtilButton />
         <ScanCodeButton />
+
+
+        <DropdownButton title="Info & Support" variant="light">
+          <ExternalItem title="Chemotion.net" href="https://www.chemotion.net" />
+          <ExternalItem title="Chemotion-Repository.net" href="https://www.chemotion-repository.net" />
+          <Dropdown.Divider />
+          <ExternalItem title="Search documentation" href="https://chemotion.net/search" />
+          <ExternalItem title="Helpdesk - Contact Us" href="https://chemotion.net/helpdesk" />
+          <ExternalItem title="Report an issue on Github" href="https://github.com/ComPlat/chemotion_ELN/issues" />
+          {hasVersions && (
+            <>
+              <Dropdown.Divider />
+              <Dropdown.ItemText className="d-flex flex-column text-muted">
+                {Object.entries(version).map(([k, v]) => (
+                  <span key={k} className="d-flex justify-content-between">
+                    <span>{k}:</span>
+                    <span style={{userSelect: 'text'}}>
+                      {k == 'version' ? v : v.substring(0, 8)}
+                    </span>
+                  </span>
+                ))}
+              </Dropdown.ItemText>
+            </>
+          )}
+        </DropdownButton>
 
         <UserAuth />
       </div>
