8aa650b7f0b27e42c9c4c5e67976d522534c5e1a
Ashwini <amurthy.ashwini@gmail.com>
2024-10-23 10:05:10 +0000

    handle invalid strings on fast sample create

--- app/packs/src/apps/mydb/elements/details/samples/SampleDetails.js
+++ app/packs/src/apps/mydb/elements/details/samples/SampleDetails.js
@@ -1262,7 +1262,7 @@ export default class SampleDetails extends React.Component {
 
   renderMolfileModal() {
     const { molfile } = this.state;
-    const molfileText = molfile.replace(/\r?\n/g, '<br />');
+    const molfileText = molfile ? molfile.replace(/\r?\n/g, '<br />') : '';
 
     return (
       <Modal
