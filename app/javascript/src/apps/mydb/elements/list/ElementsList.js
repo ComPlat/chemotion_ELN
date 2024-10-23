c475452502187834192035f375923997cdcb02e4
Ashwini <amurthy.ashwini@gmail.com>
2024-10-23 14:02:50 +0000

    feat: add update endpoint for vessels

--- app/javascript/src/apps/mydb/elements/list/ElementsList.js
+++ app/javascript/src/apps/mydb/elements/list/ElementsList.js
@@ -112,7 +112,7 @@ export default class ElementsList extends React.Component {
       'sample', 'reaction', 'screen',
       'wellplate', 'research_plan',
       'cell_line', 'device_description'
-    ].concat(genericKlasses);
+    , 'vessel'].concat(genericKlasses);
 
     const newTotalCheckedElements = {};
     let needsUpdate = false;
@@ -169,7 +169,8 @@ export default class ElementsList extends React.Component {
       'wellplate',
       'research_plan',
       'cell_line',
-      'device_description'
+      'device_description',
+      'vessel'
     ]);
     const tabItems = visible.map((value, i) => {
       let iconClass = `icon-${value}`;
