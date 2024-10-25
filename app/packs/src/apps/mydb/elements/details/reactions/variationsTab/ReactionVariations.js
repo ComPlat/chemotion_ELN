6be3f02bd88c62f5326b2b69ce3e8caafc16b104
Jan C. Brammer <jan.c.brammer@gmail.com>
2024-10-25 06:51:23 +0000

    Change editability of duration according to gas mode

--- app/packs/src/apps/mydb/elements/details/reactions/variationsTab/ReactionVariations.js
+++ app/packs/src/apps/mydb/elements/details/reactions/variationsTab/ReactionVariations.js
@@ -35,6 +35,7 @@ import {
 export default function ReactionVariations({ reaction, onReactionChange }) {
   const gridRef = useRef(null);
   const reactionVariations = reaction.variations;
+  const [gasMode, setGasMode] = useState(reaction.gaseous);
   const [allReactionAnalyses, setAllReactionAnalyses] = useState(getReactionAnalyses(reaction));
   const [reactionMaterials, setReactionMaterials] = useState(getReactionMaterials(reaction));
   const [columnDefinitions, setColumnDefinitions] = useReducer(columnDefinitionsReducer, [
@@ -85,6 +86,7 @@ export default function ReactionVariations({ reaction, onReactionChange }) {
         {
           field: 'properties.duration',
           cellDataType: getCellDataType('duration'),
+          editable: !gasMode,
           entryDefs: {
             currentEntry: 'duration',
             displayUnit: getStandardUnit('duration'),
@@ -145,6 +147,9 @@ export default function ReactionVariations({ reaction, onReactionChange }) {
   // https://react.dev/learn/you-might-not-need-an-effect#adjusting-some-state-when-a-prop-changes
   // https://react.dev/reference/react/useState#storing-information-from-previous-renders
   const updatedReactionMaterials = getReactionMaterials(reaction);
+  const updatedGasMode = reaction.gaseous;
+  const updatedAllReactionAnalyses = getReactionAnalyses(reaction);
+
   if (
     !isEqual(
       getReactionMaterialsIDs(reactionMaterials),
@@ -169,7 +174,16 @@ export default function ReactionVariations({ reaction, onReactionChange }) {
     setReactionMaterials(updatedReactionMaterials);
   }
 
-  const updatedAllReactionAnalyses = getReactionAnalyses(reaction);
+  if (gasMode !== updatedGasMode) {
+    setColumnDefinitions(
+      {
+        type: 'toggle_gas_mode',
+        gasMode: updatedGasMode,
+      }
+    );
+    setGasMode(updatedGasMode);
+  }
+
   if (!isEqual(allReactionAnalyses, updatedAllReactionAnalyses)) {
     /*
     The "Variations" tab holds references to analyses in the "Analyses" tab.
