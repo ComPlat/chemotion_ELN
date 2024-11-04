09069618e00decf771bf83f068034a1ddacf3cd6
Luciano Maiwald <mail@luciano-maiwald.de>
2024-11-04 14:30:26 +0100

    Consolidate Admin, CnC & Home Navigation components

--- app/javascript/src/apps/admin/AdminHome.js
+++ app/javascript/src/apps/admin/AdminHome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Col, Nav, NavItem, Container } from 'react-bootstrap';
-import AdminNavigation from 'src/apps/admin/AdminNavigation';
+import BaseNavigation from 'src/components/navigation/BaseNavigation';
 import Notifications from 'src/components/Notifications';
 import AdminDashboard from 'src/apps/admin/AdminDashboard';
 import UserManagement from 'src/apps/admin/UserManagement';
@@ -19,18 +19,9 @@ class AdminHome extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      showTree: true,
       pageIndex: 0,
     };
-    this.toggleTree = this.toggleTree.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
-  }
-
-  toggleTree() {
-    const { showTree } = this.state;
-    this.setState({
-      showTree: !showTree,
-    });
   }
 
   handleSelect(pageIndex) {
@@ -41,39 +32,25 @@ class AdminHome extends React.Component {
 
   mainContent() {
     const { pageIndex } = this.state;
-    if (pageIndex === 0) {
-      return this.renderContent(<AdminDashboard />);
-    } else if (pageIndex === 1) {
-      return this.renderContent(<UserManagement />);
-    } else if (pageIndex === 2) {
-      return this.renderContent(<MessagePublish />);
-    } else if (pageIndex === 4) {
-      return this.renderContent(<GroupsDevices />);
-    } else if (pageIndex === 5) {
-      return this.renderContent(<OlsTerms />);
-    } else if (pageIndex === 7) {
-      return this.renderContent(<MatrixManagement />);
-    } else if (pageIndex === 8) {
-      return this.renderContent(<TextTemplateContainer />);
-    } else if (pageIndex === 9) {
-      return this.renderContent(<DevicesList />);
-    //} else if (pageIndex === 12) {
-    //  return this.renderContent(<TemplateManagement />);
-    } else if (pageIndex === 13) {
-      return this.renderContent(<DelayedJobs />);
-    } else if (pageIndex === 14) {
-      return this.renderContent(<ChemSpectraLayouts />);
-    } else if (pageIndex === 15) {
-      return this.renderContent(<ThirdPartyApp />);
+    switch (pageIndex) {
+      case 0: return <AdminDashboard />;
+      case 1: return <UserManagement />;
+      case 2: return <MessagePublish />;
+      case 4: return <GroupsDevices />;
+      case 5: return <OlsTerms />;
+      case 7: return <MatrixManagement />;
+      case 8: return <TextTemplateContainer />;
+      case 9: return <DevicesList />;
+      // case 12: return <TemplateManagement />;
+      case 13: return <DelayedJobs />;
+      case 14: return <ChemSpectraLayouts />;
+      case 15: return <ThirdPartyApp />;
+      default: return null;
     }
-    return (null);
   }
 
   renderTree() {
-    const { showTree, pageIndex } = this.state;
-    if (!showTree) {
-      return null;
-    }
+    const { pageIndex } = this.state;
 
     return (
       <Nav className="flex-column fs-5 gap-3 mt-2" variant="pills" activeKey={pageIndex} onSelect={this.handleSelect}>
@@ -117,25 +94,15 @@ class AdminHome extends React.Component {
     );
   }
 
-  renderContent(component) {
-    return (
-      <Col >
-        {component}
-      </Col>
-    );
-  }
-
   render() {
     return (
       <div>
         <Container fluid>
           <Row className="mb-3">
-            <AdminNavigation toggleTree={this.toggleTree} />
+            <BaseNavigation />
           </Row>
           <div className="d-flex gap-4">
-            {this.state.showTree && 
-              this.renderTree()
-            }
+            {this.renderTree()}
             <div className="flex-grow-1">
               {this.mainContent()}
             </div>
