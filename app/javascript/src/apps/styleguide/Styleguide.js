22ffbcb552b0b90a1d3ca325f1ca23c324af829c
Joachim Tillessen <tillessen@emjot.de>
2024-11-06 11:49:55 +0100

    Change input bg color, update styleguide

--- app/javascript/src/apps/styleguide/Styleguide.js
+++ app/javascript/src/apps/styleguide/Styleguide.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Tabs, Tab, Button } from 'react-bootstrap';
+import { Container, Tabs, Tab, Button, Form } from 'react-bootstrap';
 import Sheet from 'src/components/common/Sheet';
 
 const Styleguide = () => {
@@ -67,9 +67,29 @@ const Styleguide = () => {
 
   const renderForms = () => {
     return (
-      <>
+      <Sheet>
         <h2>Forms</h2>
-      </>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Sheet>
     )
   }
 
