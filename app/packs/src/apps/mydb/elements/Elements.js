d56f43edda245a01bbadc9ef64e4ee4bc78ba1ee
Luciano Maiwald <mail@luciano-maiwald.de>
2024-11-07 14:57:11 +0100

    Add element list collapse button functionality

--- app/packs/src/apps/mydb/elements/Elements.js
+++ app/packs/src/apps/mydb/elements/Elements.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Button } from 'react-bootstrap';
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 
@@ -6,8 +6,14 @@ import ElementsList from 'src/apps/mydb/elements/list/ElementsList';
 import ElementDetails from 'src/apps/mydb/elements/details/ElementDetails';
 import ElementStore from 'src/stores/alt/stores/ElementStore';
 
+const defaultLayout = [40, 60];
+const isLayoutCollapsed = (layout) => layout[0] === 0;
+
 export default function Elements() {
   const [showDetailView, setShowDetailView] = useState(false);
+  const [isCollapsed, setCollapsed] = useState(true);
+  const [returnLayout, setReturnLayout] = useState(null);
+  const panelRef = useRef(null);
 
   useEffect(() => {
     const onElementStoreChange = ({ currentElement }) => {
@@ -19,11 +25,38 @@ export default function Elements() {
     return () => ElementStore.unlisten(onElementStoreChange);
   }, []);
 
+  const toggleListView = useCallback(() => {
+    if (!panelRef.current) return;
+    const panel = panelRef.current;
+
+    const layout = panel.getLayout();
+    if (isLayoutCollapsed(layout)) {
+      if (returnLayout) {
+        panel.setLayout(returnLayout);
+        setReturnLayout(null);
+      } else {
+        panel.setLayout(defaultLayout);
+      }
+    } else {
+      setReturnLayout(layout);
+      panel.setLayout([0, 100]);
+    }
+  }, [returnLayout]);
+
+  const onLayout = useCallback((layout) => {
+    setCollapsed(isLayoutCollapsed(layout));
+  }, []);
+
   return (
     <div className="flex-grow-1">
-      <PanelGroup direction="horizontal">
-        <Panel collapsible defaultSize={40} className="overflow-x-auto pt-3 px-3">
-          <div className="h-100" style={{ minWidth: '600px' }}>
+      <PanelGroup
+        autoSaveId="elements-panel"
+        direction="horizontal"
+        ref={panelRef}
+        onLayout={onLayout}
+      >
+        <Panel collapsible defaultSize={defaultLayout[0]} className="overflow-x-auto">
+          <div className="h-100 mt-3 mx-3" style={{ minWidth: '600px' }}>
             <ElementsList overview={!showDetailView} />
           </div>
         </Panel>
@@ -35,11 +68,11 @@ export default function Elements() {
                 className="panel-collapse-button"
                 onClick={toggleListView}
               >
-                <i className="fa fa-angle-double-left" />
+                <i className={`fa fa-angle-double-${isCollapsed ? 'right' : 'left'}`} />
               </Button>
             </PanelResizeHandle>
-            <Panel defaultSize={60} className="overflow-x-auto pt-3 px-3">
-              <div className="h-100" style={{ minWidth: '680px' }}>
+            <Panel defaultSize={defaultLayout[1]} className="overflow-x-auto">
+              <div className="h-100 mt-3 mx-3" style={{ minWidth: '680px' }}>
                 <ElementDetails />
               </div>
             </Panel>
