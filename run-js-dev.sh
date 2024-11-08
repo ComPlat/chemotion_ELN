f06fd76d1234e1589542ec722804b51eabe21e41
Johannes Haubold <jh@megorei.com>
2024-11-08 20:59:33 +0100

    Change runner to shakapacker instead of webpacker

--- run-js-dev.sh
+++ run-js-dev.sh
@@ -13,4 +13,4 @@ yarn install
 echo "=========================================================================================================="
 echo "THIS WILL FAIL UNTIL THE RUBY GEMS ARE INSTALLED BY run-ruby-dev.sh. JUST TRY AGAIN AFTER INSTALLING THEM."
 echo "=========================================================================================================="
-./bin/webpacker-dev-server
+./bin/shakapacker-dev-server
