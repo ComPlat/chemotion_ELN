1fc4d86486fa817b803bcbce2350f8a0975ddc96
Hadi <hadi-tariq@live.co.uk>
2024-11-12 20:01:19 +0100

    Test+Refactoring: reading molfile with/without polymers; test for hasKetcherData
    test: setKetcherdata
    
    test: hasKetcherdata, setKetcherdata
    
    refactoring: onTemplateMove
    
    refactoring+test: movetemplate & placeImageOnAtom
    
    Refac+test: on add atom
    
    test: on atom add image counter checker
    
    refactor+tests: on image delete, mockup files for test
    
    test: on delete image delete atom with connected alias; with single or multi images|
    
    test+refac: on delete atom
    
    clean+refac: add atom logic divied
    
    chore: code doc, template-list shape alignment
    
    fix: delete atom when a mol is dropped on other mol; delete mol1 and update aliases for mol0
    
    specs fix ketcher Editor
    
    refactoring: delete image, read mofile, delete atom(single/group)
    
    refactoring+fixes: delete atom by image, delete image by atom, savingMofile
    
    fix: delete image when single atom behind is deleted
    
    fix: delete atom case when dragged index is smaller then others
    
    chore: template names updated bead, surface
    
    cleanup + minor fixes
    
    test: delete atom to reset aliases, molfile save
    
    Refactoring, comments, templates-source parsing

---
+++ app/packs/src/surfaceChemistryImages.js
@@ -0,0 +1,26 @@
+// standard sizes for the shapes
+export const [standard_height_cirlce, standard_width_circle] = [1.0250000000000006, 1.0250000000000006];
+export const [standard_height_square, standard_width_square] = [0.50, 2.0];
+
+// list of data is required when alias is t_##, and image is not available in canvas
+export const template_list_data = [
+  null, // templates starting with 1 
+  {
+    "type": "image",
+    "format": "image/svg+xml",
+    "boundingBox": {
+      "width": standard_width_circle,
+      "height": standard_height_cirlce
+    },
+    "data": "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDAgMTAwIj4NCiAgPGRlZnM+DQogICAgPHJhZGlhbEdyYWRpZW50IGlkPSJncmFkMSIgY3g9IjUwJSIgY3k9IjUwJSIgcj0iNTAlIiBmeD0iNTAlIiBmeT0iNTAlIj4NCiAgICAgIDxzdG9wIG9mZnNldD0iMCUiIHN0eWxlPSJzdG9wLWNvbG9yOnJnYigyNTUsMjU1LDI1NSk7c3RvcC1vcGFjaXR5OjEiIC8+DQogICAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0eWxlPSJzdG9wLWNvbG9yOnJnYigwLDAsMCk7c3RvcC1vcGFjaXR5OjEiIC8+DQogICAgPC9yYWRpYWxHcmFkaWVudD4NCiAgPC9kZWZzPg0KICA8Y2lyY2xlIGN4PSI1MCIgY3k9IjUwIiByPSI0MCIgZmlsbD0idXJsKCNncmFkMSkiIC8+DQo8L3N2Zz4NCg=="
+  },
+  {
+    "type": "image",
+    "format": "image/svg+xml",
+    "boundingBox": {
+      "width": standard_width_square,
+      "height": standard_height_square
+    },
+    "data": "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMDAiIGhlaWdodD0iMjAwIiB2aWV3Qm94PSIwIDAgMzAwIDMwMCI+CiAgPHJlY3Qgd2lkdGg9IjMwMCIgaGVpZ2h0PSIxNTAiIHk9IjgwIiByeD0iMjAiIHJ5PSIyMCIKICBzdHJva2U9IiMwMDAiIHN0cm9rZS13aWR0aD0iMTAiIGZpbGw9Ijc1NzA3MCIKICAvPgo8L3N2Zz4="
+  }
+];
