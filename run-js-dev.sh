e470917779b55af97c58f81c0541fee2c4a32ae0
Hadi <hadi-tariq@live.co.uk>
2024-11-25 14:00:48 +0100

    Ketcher2 Surface Chemistry feature documentation:v1
    fix: reading molfile for saving
    
    fix: polymers index
    
    chore: image tag filter applied
    
    Chore: Ket2 Surface Chemistry feature docs
    
    test: template list processing
    
    fix: delete atom with select, behind atom, template, calculate pi event, minor feature-docs changes
    
    fix: delete template on click atom
    
    fix: utils moved and delete atom minor fix
    
    fix: image placement index with refactoring
    
    fix: on save molfile
    
    fix: resolve ESLint warnings and errors
    
    chore: linting, correct template matching while saving
    
    clean: Identifying polymers list
    
    chore: packs moved, docker configs

--- run-js-dev.sh
+++ run-js-dev.sh
@@ -4,6 +4,18 @@
 ## assume nodejs is installed (through ./run-ruby-dev.sh)
 ./prepare-nodejspkg.sh
 
+# if NODE_PATH is set, add --modules-folder option to .yarnrc and create a symlink to the node_modules folder
+if [ -z "$NODE_PATH" ]; then
+    echo ">>> NODE_PATH is not set"
+else
+    echo ">>> NODE_PATH is set to $NODE_PATH"
+#    echo ">>> Adding --modules-folder option to .yarnrc"
+#    echo -e "--modules-folder $NODE_PATH" > .yarnrc
+# create a symlink unless it already exists
+    [ -L "${HOME}/app/node_modules" ] || ln -s  "${NODE_PATH}/" "${HOME}/app/node_modules" && bash package_postinstall.sh
+fi
+
+
 echo "=========================================================================================================="
 echo "THIS WILL FAIL UNTIL THE RUBY GEMS ARE INSTALLED BY run-ruby-dev.sh. JUST TRY AGAIN AFTER INSTALLING THEM."
 echo "=========================================================================================================="
