6106f9db38a39e2276717cd14ea0a9f26db4b55d
Jan C. Brammer <jan.c.brammer@gmail.com>
2024-11-28 12:07:46 +0000

    Adapt entities to gas types

--- app/api/entities/reaction_variation_entity.rb
+++ app/api/entities/reaction_variation_entity.rb
@@ -20,28 +20,28 @@ module Entities
       end
     end
 
-    def materials(material_type)
+    def materials(material_type, entity)
       {}.tap do |materials|
         object[material_type]&.each do |k, v|
-          materials[k] = ReactionVariationMaterialEntity.represent(v)
+          materials[k] = entity.represent(v)
         end
       end
     end
 
     def starting_materials
-      materials(:startingMaterials)
+      materials(:startingMaterials, StartingMaterialEntity)
     end
 
     def reactants
-      materials(:reactants)
+      materials(:reactants, StartingMaterialEntity)
     end
 
     def products
-      materials(:products)
+      materials(:products, ProductMaterialEntity)
     end
 
     def solvents
-      materials(:solvents)
+      materials(:solvents, SolventMaterialEntity)
     end
   end
 
@@ -52,10 +52,33 @@ module Entities
     )
   end
 
-  class ReactionVariationMaterialEntity < ApplicationEntity
+  class SolventMaterialEntity < ApplicationEntity
+    expose :volume, using: 'Entities::ReactionVariationMaterialEntryEntity'
+
+    expose :aux, using: 'Entities::ReactionVariationMaterialAuxEntity'
+  end
+
+  class ProductMaterialEntity < ApplicationEntity
     expose :mass, using: 'Entities::ReactionVariationMaterialEntryEntity'
     expose :amount, using: 'Entities::ReactionVariationMaterialEntryEntity'
-    expose :volume, using: 'Entities::ReactionVariationMaterialEntryEntity'
+    expose :yield, using: 'Entities::ReactionVariationMaterialEntryEntity'
+
+    expose :duration, if: lambda { |object, _| object[:aux][:gasType] == 'gas' }, using: 'Entities::ReactionVariationMaterialEntryEntity'
+    expose :temperature, if: lambda { |object, _| object[:aux][:gasType] == 'gas' }, using: 'Entities::ReactionVariationMaterialEntryEntity'
+    expose :concentration, if: lambda { |object, _| object[:aux][:gasType] == 'gas' }, using: 'Entities::ReactionVariationMaterialEntryEntity'
+    expose :turnoverNumber, if: lambda { |object, _| object[:aux][:gasType] == 'gas' }, using: 'Entities::ReactionVariationMaterialEntryEntity'
+    expose :turnoverFrequency, if: lambda { |object, _| object[:aux][:gasType] == 'gas' }, using: 'Entities::ReactionVariationMaterialEntryEntity'
+
+    expose :aux, using: 'Entities::ReactionVariationMaterialAuxEntity'
+  end
+
+  class StartingMaterialEntity < ApplicationEntity
+    expose :mass, using: 'Entities::ReactionVariationMaterialEntryEntity'
+    expose :amount, using: 'Entities::ReactionVariationMaterialEntryEntity'
+    expose :equivalent, using: 'Entities::ReactionVariationMaterialEntryEntity'
+
+    expose :volume, if: lambda { |object, _| (object[:aux][:gasType] == 'feedstock') }, using: 'Entities::ReactionVariationMaterialEntryEntity'
+
     expose :aux, using: 'Entities::ReactionVariationMaterialAuxEntity'
   end
 
@@ -68,8 +91,9 @@ module Entities
       :molarity,
       :molecularWeight,
       :sumFormula,
-      :yield,
-      :equivalent,
+      :gasType,
+      :vesselVolume,
+      :materialType,
     )
   end
 
