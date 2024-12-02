40e12af9d39cfa191597067546d4168b423041cd
Ashwini <amurthy.ashwini@gmail.com>
2024-12-02 07:49:04 +0000

    feat: add vesselUtilities to extract params

---
+++ app/javascript/src/utilities/VesselUtilities.js
@@ -0,0 +1,25 @@
+const extractVesselApiParameter = (vessel) => {
+    return {
+        "vessel_id":vessel.id,
+        "collection_id":vessel.collectionId,
+        "vessel_name":vessel.vesselInstanceName,
+        "description":vessel.vesselInstanceDescription,
+        "bar_code":vessel.barCode,
+        "qr_code":vessel.qrCode,
+        "template_name":vessel.vesselName,
+        "material_details":vessel.materialDetails,
+        "material_type":vessel.materialType,
+        "vessel_type":vessel.vesselType,
+        "volume_amount":vessel.volumeAmount,
+        "volume_unit":vessel.volumeUnit,
+        "weight_amount":vessel.weight,
+        "weight_unit":vessel.weightUnit,
+        "details":vessel.details,
+        "short_label":vessel.short_label,
+        "container":vessel.container
+};
+}
+
+export {
+    extractVesselApiParameter
+};
