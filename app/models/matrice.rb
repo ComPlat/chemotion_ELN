9077629505303dfa5af590f3a4e272d04e34708d
PiTrem <pierre.tremouilhac@kit.edu>
2024-12-04 17:21:39 +0100

    fix(migration): manage matrice id so that new matrices does not increment the sequence over 32 as this leads error when recalculating the users.matrix.
    on create
    - reset the sequence to the latest  i used id (no unused id)
    - force delete of matrices that are out of range

--- app/models/matrice.rb
+++ app/models/matrice.rb
@@ -17,18 +17,26 @@
 #
 #  index_matrices_on_name  (name) UNIQUE
 #
+# Configuration settings for some features
+# - set visibility/activation of features globaly or for users and group
+#
+# @note: db/triggers/update_users_matrix_trg on update/insert to update user matrix
 class Matrice < ApplicationRecord
+  include SequenceUtilities
+
   acts_as_paranoid
+  before_create :clean_invalid_ids
+  before_create :reset_sequence
   after_create :gen_json
+  after_destroy :gen_json
 
   def self.gen_matrices_json
     mx = pluck(:name, :id).to_h || {}
   rescue ActiveRecord::StatementInvalid, PG::ConnectionBad, PG::UndefinedTable
     mx = {}
   ensure
-    File.write(
-      Rails.root.join('config', 'matrices.json'),
-      mx.to_json.concat("\n")
+    Rails.root.join('config/matrices.json').write(
+      mx.to_json.concat("\n"),
     )
   end
 
@@ -46,5 +54,11 @@ class Matrice < ApplicationRecord
 
   def gen_json
     Matrice.gen_matrices_json
+  end
+
+  # Remove matrices with id > 31
+  # @note: this is a temporary solution to remove invalid matrices
+  def clean_invalid_ids
+    self.class.where('id > 31').find_each(&:really_destroy!)
   end
 end
