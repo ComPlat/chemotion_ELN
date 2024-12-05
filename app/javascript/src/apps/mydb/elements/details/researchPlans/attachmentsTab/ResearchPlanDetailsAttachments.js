500113a80ca76e9d83dd01fd7cec2b5bc0817629
Adrian Herrmann <adrian.herrmann@kit.edu>
2024-12-05 08:43:57 +0000

    fix: attachment space calculation

--- app/javascript/src/apps/mydb/elements/details/researchPlans/attachmentsTab/ResearchPlanDetailsAttachments.js
+++ app/javascript/src/apps/mydb/elements/details/researchPlans/attachmentsTab/ResearchPlanDetailsAttachments.js
@@ -222,7 +222,7 @@ class ResearchPlanDetailsAttachments extends Component {
 
   checkUserQuota() {
     const { filteredAttachments } = this.state;
-    const totalSize = filteredAttachments.filter((attachment) => !attachment.is_deleted)
+    const totalSize = filteredAttachments.filter((attachment) => attachment.is_new && !attachment.is_deleted)
       .reduce((acc, attachment) => acc + attachment.filesize, 0);
     const { currentUser } = UserStore.getState();
     return currentUser.available_space !== 0 && totalSize > (currentUser.available_space - currentUser.used_space);
