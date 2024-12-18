9fb09656f0ce079ef067765520247beed2542e6f
Johannes Haubold <jh@megorei.com>
2024-12-18 15:37:56 +0100

    refactor: migrate to shakapacker (#2236)
    upgrade from retired rails/webpacker to shakacode/shakapacker
    
    * Changes from Shakapacker installation
    
    Steps to reproduce:
    docker-compose -f docker-compose.dev.yml run app bash
    bundle install
    asdf reshim # no idea why but didn't work without it
    ./bin/rails shakapacker:install
    
    
    * Move JS file hierarchy to location defined in shakapacker.yml
    
    * Add nano to docker image
    
    * Change runner to shakapacker instead of webpacker
    
    * Update shakapacker config to enable HMR
    
    * Fix alt.js/babel issue
    
    * Remove obsolete .babelrc.bak
    
    *  migrate all references to app/packs to app/javascript
    
    * migrate all references to webpacker to shakapacker (notable exclusion: docker-compose.dev.yml so users don't have to rebuild the whole container)
    
    * remove all references to app/packs/klasses.json and config/klasses.json. Those are no longer existing. The only place where a klasses.json is residing is public/klasses.json, as written by the Labimotion gem
    
    * upd gitignore
    
    * Fix babel config
    
    * chore: bump obsolet plugin-proposal to plugin-transform
    
    
    ---------
    
    Co-authored-by: Matthias Döring <md@megorei.com>

--- config/initializers/eln_features.rb
+++ config/initializers/eln_features.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-# pack version
-pack_path = Webpacker.manifest.send(:data)&.fetch('application.js', nil)
+pack_path = Shakapacker.manifest.send(:data)&.fetch('application.js', nil)
 ENV['VERSION_ASSETS'] = pack_path && File.basename(pack_path)
 
 ActiveSupport.on_load(:active_record) do
