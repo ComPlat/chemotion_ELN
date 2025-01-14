a4347afb502b68e992597762e6ee5d459d6d16ed
Ashwini <amurthy.ashwini@gmail.com>
2025-01-14 08:45:54 +0000

    feat: render container datasetfield in attachmentscontainer

--- app/packs/src/apps/mydb/elements/details/vessels/attachmentsTab/AttachmentsContainer.js
+++ app/packs/src/apps/mydb/elements/details/vessels/attachmentsTab/AttachmentsContainer.js
@@ -6,8 +6,9 @@ import Container from 'src/models/Container';
 import ElementStore from 'src/stores/alt/stores/ElementStore';
 import AttachmentDropzone from 'src/components/container/AttachmentDropzone';
 import AttachmentsModal from 'src/apps/mydb/elements/details/vessels/attachmentsTab/AttachmentsModal';
+import ContainerDatasetField from 'src/components/container/ContainerDatasetField';
 
-function AttachmentsContainer({ item  }) {
+function AttachmentsContainer({ item }) {
   const { currentElement } = ElementStore.getState();
   const context = useContext(StoreContext);
   const [container, setContainer] = useState({
@@ -35,8 +36,9 @@ function AttachmentsContainer({ item  }) {
   };
 
   const handleChange = (updatedDatasetContainer) => {
-    const updatedChildren = container.children
-      .map((child) => child.id === updatedDatasetContainer.id ? updatedDatasetContainer : child);
+    const updatedChildren = container.children.map((child) =>
+      child.id === updatedDatasetContainer.id ? updatedDatasetContainer : child
+    );
     const updatedContainer = {
       ...container,
       children: updatedChildren,
@@ -55,7 +57,6 @@ function AttachmentsContainer({ item  }) {
 
     setContainer(updatedContainer);
     handleModalOpen(datasetContainer);
-    handleChange(updatedContainer);
   };
 
   const handleAddWithAttachments = (attachments) => {
@@ -73,8 +74,76 @@ function AttachmentsContainer({ item  }) {
 
     setContainer(updatedContainer);
     handleModalOpen(datasetContainer);
-    handleChange(updatedContainer);
   };
+
+  const handleRemove = (datasetContainer) => {
+    const updatedChildren = container.children.map((child) =>
+      child.id === datasetContainer.id ? { ...child, is_deleted: true } : child
+    );
+    setContainer({
+      ...container,
+      children: updatedChildren,
+    });
+  };
+
+  const handleUndo = (datasetContainer) => {
+    const updatedChildren = container.children.map((child) =>
+      child.id === datasetContainer.id ? { ...child, is_deleted: false } : child
+    );
+    setContainer({
+      ...container,
+      children: updatedChildren,
+    });
+  };
+
+  const addButton = () => (
+    <div className="d-flex justify-content-end mt-2 mb-0">
+      <Button size="sm" variant="success" onClick={handleAdd}>
+        <i className="fa fa-plus" />
+      </Button>
+    </div>
+  );
+
+  if (container.children.length > 0) {
+    return (
+      <div>
+        <div className="border rounded p-2 mb-2">
+          <div className="list-group">
+            {container.children.map((datasetContainer, key) => (
+              <div key={key} className="list-group-item">
+                <ContainerDatasetField
+                  datasetContainer={datasetContainer}
+                  onChange={() => handleChange(datasetContainer)}
+                  handleRemove={() => handleRemove(datasetContainer)}
+                  handleUndo={() => handleUndo(datasetContainer)}
+                  handleModalOpen={() => handleModalOpen(datasetContainer)}
+                  disabled={false}
+                  readOnly={false}
+                />
+              </div>
+            ))}
+            <div key="attachmentdropzone" className="list-group-item disabled">
+              <AttachmentDropzone
+                handleAddWithAttachments={handleAddWithAttachments}
+              />
+            </div>
+          </div>
+          {addButton()}
+        </div>
+        {modal.show && modal.datasetContainer && (
+          <AttachmentsModal
+            onHide={handleModalHide}
+            onChange={handleChange}
+            show={modal.show}
+            readOnly={false}
+            datasetContainer={modal.datasetContainer}
+            analysisContainer={modal.analysisContainer}
+            disabled={false}
+          />
+        )}
+      </div>
+    );
+  }
 
   return (
     <div className="bg-gray-200">
@@ -87,23 +156,7 @@ function AttachmentsContainer({ item  }) {
             />
           </div>
         </div>
-        <div className="d-flex justify-content-end mt-2 mb-0">
-          <Button size="sm" variant="success" onClick={handleAdd}>
-            <i className="fa fa-plus" />
-          </Button>
-        </div>
-        {modal.show && modal.datasetContainer && (
-        <AttachmentsModal
-          onHide={handleModalHide}
-          onChange={handleChange}
-          kind={null}
-          show={modal.show}
-          readOnly={false}
-          datasetContainer={modal.datasetContainer}
-          analysisContainer={modal.analysisContainer}
-          disabled={false}
-        />
-        )}
+        {addButton()}
       </div>
     </div>
   );
