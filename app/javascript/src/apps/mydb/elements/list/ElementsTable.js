2c991e233f545637799d0145eb7510afce49d637
Ashwini <amurthy.ashwini@gmail.com>
2025-01-22 16:15:53 +0000

    feat: display vessels in groups

--- app/javascript/src/apps/mydb/elements/list/ElementsTable.js
+++ app/javascript/src/apps/mydb/elements/list/ElementsTable.js
@@ -25,6 +25,8 @@ import { Select } from 'src/components/common/Select';
 import PropTypes from 'prop-types';
 import CellLineGroup from 'src/models/cellLine/CellLineGroup';
 import CellLineContainer from 'src/apps/mydb/elements/list/cellLine/CellLineContainer';
+import VesselContainer from 'src/apps/mydb/elements/list/vessel/VesselContainer';
+import VesselGroup from 'src/models/vessel/VesselGroup';
 import ChevronIcon from 'src/components/common/ChevronIcon';
 import DeviceDescriptionList from 'src/apps/mydb/elements/list/deviceDescriptions/DeviceDescriptionList';
 import DeviceDescriptionListHeader from 'src/apps/mydb/elements/list/deviceDescriptions/DeviceDescriptionListHeader';
@@ -691,7 +693,13 @@ export default class ElementsTable extends React.Component {
           ui={ui}
         />
       );
-    } else {
+    } else if (type === 'vessel') {
+      elementsTableEntries = (
+        <VesselContainer
+          vesselGroups={VesselGroup.buildVesselGroups(elements)}
+        />
+      );
+    }  else {
       elementsTableEntries = (
         <ElementsTableEntries
           elements={elements}
