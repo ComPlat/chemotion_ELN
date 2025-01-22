85a1f249fabe028b3934ee1b951ba5ad4e58b475
Ashwini <amurthy.ashwini@gmail.com>
2025-01-22 16:15:53 +0000

    feat: display vessels in groups

---
+++ app/javascript/src/apps/mydb/elements/list/vessel/VesselItemEntry.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import UIStore from 'src/stores/alt/stores/UIStore';
+import { Table } from 'react-bootstrap';
+import ElementCheckbox from 'src/apps/mydb/elements/list/ElementCheckbox';
+import ElementContainer from 'src/apps/mydb/elements/list/ElementContainer';
+import { elementShowOrNew } from 'src/utilities/routesUtils';
+import { DragDropItemTypes } from 'src/utilities/DndConst';
+import Aviator from 'aviator';
+import ElementCollectionLabels from 'src/apps/mydb/elements/labels/ElementCollectionLabels';
+import ElementStore from 'src/stores/alt/stores/ElementStore';
+import PropTypes from 'prop-types';
+
+const VesselItemEntry = ({ vesselItem }) => {
+  const showDetails = () => {
+    const { currentCollection, isSync } = UIStore.getState();
+    const { id, type } = vesselItem;
+
+    const uri = isSync
+      ? `/scollection/${currentCollection.id}/vessel/${id}`
+      : `/collection/${currentCollection.id}/vessel/${id}`;
+    Aviator.navigate(uri, { silent: true });
+
+    const e = {
+      type: 'vessel',
+      params: {
+        collectionID: currentCollection.id,
+        new_vessel: false,
+        vesselID: id,
+      },
+    };
+    e.params[`${type}ID`] = id;
+
+    elementShowOrNew(e);
+  };
+
+  const isElementChecked = (element) => {
+    const { checkedIds = [], uncheckedIds = [], checkedAll } = UIStore.getState().vessel;
+    return (checkedAll && !uncheckedIds.includes(element.id)) || checkedIds.includes(element.id);
+  };
+
+  const { currentElement } = ElementStore.getState();
+  const backgroundColorClass = currentElement?.id === vesselItem.id ? 'text-bg-primary' : '';
+
+  return (
+    <div className="group-entry">
+      <Table className="elements" hover>
+        <tbody>
+          <tr className={`${backgroundColorClass} border-top`}>
+            <td className="select-checkBox">
+              <ElementCheckbox
+                element={vesselItem}
+                checked={isElementChecked(vesselItem)}
+              />
+            </td>
+            <td
+              className="short_label"
+              onClick={showDetails}
+            >
+              {vesselItem.short_label}
+            </td>
+            <td
+              className="item-text"
+              onClick={showDetails}
+            >
+              <div>
+                <div className="item-properties floating">
+                  <div className="starting floating item-property-value">
+                    {vesselItem.itemName}
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <ElementCollectionLabels element={vesselItem} />
+            </td>
+            <td className="arrow">
+              <ElementContainer
+                sourceType={DragDropItemTypes.VESSEL}
+                element={vesselItem}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+VesselItemEntry.propTypes = {
+  vesselItem: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    short_label: PropTypes.string,
+    itemName: PropTypes.string,
+  }).isRequired,
+};
+
+export default VesselItemEntry;
