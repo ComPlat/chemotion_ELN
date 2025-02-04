e07c40f0a1d07eb97a338f55a19edbe66049a6b3
Christian Buggle <christian@buggle.net>
2025-02-04 13:03:20 +0100

    Fix ORD export of mobile_phase, unknown solvents (RPE#24)
    * Fix ORD export of mobile_phases
    * Fix SolventsWithRation export for unknown Solvents
    ** Find Solvents in SolventsWithRationExporter by ontology_id, then by label, else export them 1:1 from their label/value. (Fixes export of solvents injected through DeviceMethod step definitions).

--- app/ord_kit/exporter/samples/ontology_solvents_with_ratio_exporter.rb
+++ app/ord_kit/exporter/samples/ontology_solvents_with_ratio_exporter.rb
@@ -10,16 +10,28 @@ module OrdKit
 
         def to_ord
           Array(@solvents_workup).map do |solvent_workup|
-            # TODO: key 'chmo_id' is obsolete, but still in data. Write transformation script.
-            ontology_id = solvent_workup['ontology_id'] || solvent_workup['chmo_id']
             OrdKit::OntologySolventWithRatio.new(
-              solvent: ontology_ord(ontology_id),
+              solvent: solvent_ord(solvent_workup),
               ratio: solvent_workup['ratio'].to_s,
             )
           end
         end
 
         private
+
+        def solvent_ord(solvent_workup)
+          ontology_id = solvent_workup['ontology_id'] ||
+                        ReactionProcessEditor::Ontology.find_by(label: solvent_workup['label'])&.id
+
+          if ontology_id
+            ontology_ord(ontology_id)
+          else
+            # workaround for Solvents from predefined steps.
+            { id: 'Solvent has no Ontology ID',
+              label: solvent_workup['label'] || 'Error: Empty Solvent',
+              name: solvent_workup['value'] }
+          end
+        end
 
         def ontology_ord(ontology_id)
           OrdKit::Exporter::Models::OntologyExporter.new(ontology_id).to_ord
