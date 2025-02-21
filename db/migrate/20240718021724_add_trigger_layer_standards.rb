74e8693f3b6e177855e0ffc35a284bab1e7f4336
Paggy Huang <pei-chi.huang@kit.edu>
2025-02-21 17:19:43 +0100

    feat: labimotion-2.0.0 update labimotion to v2-rc: - chore:  updated react-boostrap - feat: LabIMotion Vocabulary (Lab-Vocab) - feat: Generic Layers: reuse set of fields across multiple templates. see LabIMotion/labimotion#37 for the features see LabIMotion/labimotion#39 for the features

--- db/migrate/20240718021724_add_trigger_layer_standards.rb
+++ db/migrate/20240718021724_add_trigger_layer_standards.rb
@@ -1,5 +1,7 @@
 class AddTriggerLayerStandards < ActiveRecord::Migration[6.1]
   def change
-    create_trigger :lab_trg_layers_changes, on: :layers
+    if table_exists? :layers
+      create_trigger :lab_trg_layers_changes, on: :layers
+    end
   end
 end
