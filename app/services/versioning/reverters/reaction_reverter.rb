a3e0258bdbc56df8c6e5321619c09a6eac04e686
Adrian Herrmann <adrian.herrmann@kit.edu>
2025-03-06 12:06:27 +0000

    added variations to versions frontend

--- app/services/versioning/reverters/reaction_reverter.rb
+++ app/services/versioning/reverters/reaction_reverter.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
-class Versioning::Reverters::ReactionReverter < Versioning::Reverters::BaseReverter
-  def self.scope
-    Reaction.with_deleted
+module Versioning
+  module Reverters
+    class ReactionReverter < Versioning::Reverters::BaseReverter
+      def self.scope
+        Reaction.with_deleted
+      end
+
+      def field_definitions
+        {
+          variations: handle_json,
+        }.with_indifferent_access
+      end
+
+      private
+
+      def handle_json
+        lambda do |value|
+          return [] if value.blank?
+
+          value.split("\n").map { |variation| JSON.parse(variation.gsub('=>', ':').gsub('nil', 'null')) }
+        end
+      end
+    end
   end
 end
