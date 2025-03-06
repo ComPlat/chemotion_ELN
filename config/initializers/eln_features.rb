345d799f9151f87441f57263d82942a871ec5b6d
PiTrem <pierre.tremouilhac@kit.edu>
2025-03-06 08:56:36 +0100

    fix: enabling rdkit (#2361)
    * fix: enabling rdkit
    
    refactor the setting of the rails configuration pg_cartridge:
    - initialize value to 'none' (feature disabled - empty migration)
    - use default value rdkit@4.4.0 if ENV not set
    - validate cartridge name/ version format -> disable if not
    - check if pg extension is available -> disable if not
    - check if pg extension is installed -> enable and warn if not / expect migration to
      be run
    
    refactor the db/seeds/shared/rdkit.seed.rb:
    - check if pg_cartridge is not 'none' -> settings correct and extension
      available
    - (re)run migrations if schema or mol tables is not there OR extension
      is not installed (restoring db)
    - more verbose log message
    
    This ensures
    - Rails.configuration.pg_cartridge is defined in any cases
    - the app can boot if the db is not created yet and pg_cartridge is not
      'none'
    - ensure migration create the extension if available but not installed:
      20240808125800_init_rdkit_schema drop and recreate the extension
    
    * chore: update schema for extension
    
    - get active record to look for tables in multiple schemas
    
    - ci test to use pg-rdkit image
    
    refs: #2055

--- config/initializers/eln_features.rb
+++ config/initializers/eln_features.rb
@@ -1,30 +1,85 @@
 # frozen_string_literal: true
 
-module Chemotion
-  class Application < Rails::Application
-    pack_path = Shakapacker.manifest.send(:data)&.fetch('application.js', nil)
-    ENV['VERSION_ASSETS'] = pack_path && File.basename(pack_path)
+# This file is used to set the features of the ELN
+#
+# define version of the assets to notifiy the user to reload the page
+pack_path = Shakapacker.manifest.send(:data)&.fetch('application.js', nil)
+ENV['VERSION_ASSETS'] = pack_path && File.basename(pack_path)
 
-    # PG Cartridge for structure search
-    cartridge = ENV.fetch('PG_CARTRIDGE', nil)
-    cartridge_version = ENV.fetch('PG_CARTRIDGE_VERSION', nil)
+# PG Cartridge feature for structure search
+#
+# initialize the default value
+Rails.application.config.pg_cartridge = 'none'
 
-    cartridge_present = false
-    if cartridge.in?(['rdkit']) && cartridge_version =~ /\d+\.\d+\.\d+/
-      cartridge_present = ActiveRecord::Base.connection.exec_query(
-        "select * from pg_available_extensions
-    where name = '#{cartridge}' and installed_version =   '#{cartridge_version}' ;",
-      ).first
-      puts "Warning: PG cartridge #{cartridge} version #{cartridge_version} is not installed" unless cartridge_present
-    end
+# Some functions to help setting the pg_cartridge feature
+# check if the pg extension is available
+# generate sql queries from name and version
+# @param [String] name
+# @param [String] version
+# @return [String] sql query
+extension_available = lambda do |name, version|
+  ActiveRecord::Base.connection.execute(
+    "SELECT * FROM pg_available_extensions WHERE name = '#{name}' AND default_version = '#{version}'",
+  ).count&.positive?
+end
 
-    config.pg_cartridge = cartridge_present ? cartridge : 'none'
+# check if the pg extension is installed
+extension_installed = lambda do |name, version|
+  ActiveRecord::Base.connection.execute(
+    "SELECT * FROM pg_extension WHERE extname = '#{name}' AND extversion = '#{version}'",
+  ).count&.positive?
+end
 
-    ActiveSupport.on_load(:active_record) do
-      Matrice.gen_matrices_json if ActiveRecord::Base.connection.table_exists?('matrices')
-      Labimotion::ElementKlass.gen_klasses_json if ActiveRecord::Base.connection.table_exists?('element_klasses')
-    rescue PG::ConnectionBad, ActiveRecord::NoDatabaseError => e
-      puts e.message
-    end
-  end
+# get the name and version of the extension or use default values
+cartridge_name    = -> { ENV.fetch('PG_CARTRIDGE', 'rdkit') }
+cartridge_version = -> { ENV.fetch('PG_CARTRIDGE_VERSION', '4.4.0') }
+cartridge_setting_valid = ->(name, version) { name.in?(%w[rdkit]) && version.match?(/\d+\.\d+\.\d+/) }
+
+output_message = lambda do |level, message, name = '', version = ''|
+  header = "PG structure search #{name}-#{version}"
+  puts "#{level.upcase} - #{header}: #{message}"
+  Rails.logger.send(level, "#{header}: #{message}")
+end
+messages = {
+  init: 'checking whether to enable the feature',
+  setting_invalid: 'WARN PG cartridge setting invalid - feature disabled',
+  extension_not_available: 'WARN PG cartridge extension not available - feature disabled',
+  extension_not_installed: 'WARN PG cartridge extension available but not installed !!
+                            feature enabled but ensure the extension is installed',
+  extension_installed: 'INFO PG cartridge extension installed - feature enabled',
+}
+
+ActiveSupport.on_load(:active_record) do
+  cartridge = cartridge_name.call
+  version = cartridge_version.call
+  output_message.call(:info, messages[:init], cartridge, version)
+  # Set whether PG Cartridge for structure search can be used
+  valid = cartridge_setting_valid.call(cartridge, version)
+  available = valid     && extension_available.call(cartridge, version)
+  installed = available && extension_installed.call(cartridge, version)
+  Rails.configuration.pg_cartridge = case
+                                     when !valid
+                                       output_message.call(:warn, messages[:setting_invalid])
+                                       'none'
+                                     when valid && !available
+                                       output_message.call(:warn, messages[:extension_not_available], cartridge,
+                                                           version)
+                                       'none'
+                                     when available && !installed
+                                       output_message.call(:warn, messages[:extension_not_installed], cartridge,
+                                                           version)
+                                       cartridge
+                                     when installed
+                                       output_message.call(:info, messages[:extension_installed], cartridge, version)
+                                       cartridge
+                                     end
+rescue PG::ConnectionBad, ActiveRecord::NoDatabaseError => e
+  Rails.logger.warn e.message
+end
+
+ActiveSupport.on_load(:active_record) do
+  Matrice.gen_matrices_json if ActiveRecord::Base.connection.table_exists?('matrices')
+  Labimotion::ElementKlass.gen_klasses_json if ActiveRecord::Base.connection.table_exists?('element_klasses')
+rescue PG::ConnectionBad, ActiveRecord::NoDatabaseError => e
+  Rails.logger.warn e.message
 end
--- db/migrate/20240808125802_init_rdkit_functions.rb
+++ db/migrate/20240808125802_init_rdkit_functions.rb
@@ -2,9 +2,17 @@
 
 class InitRdkitFunctions < ActiveRecord::Migration[6.1]
   def change
-    if Chemotion::Application.config.pg_cartridge == 'rdkit'
-      create_function :set_samples_mol_rdkit
-      create_trigger :set_samples_mol_rdkit_trg, on: :samples
+    reversible do |direction|
+      direction.up do
+        if Chemotion::Application.config.pg_cartridge == 'rdkit'
+          create_function :set_samples_mol_rdkit
+          create_trigger :set_samples_mol_rdkit_trg, on: :samples
+        end
+      end
+      direction.down do
+        execute 'DROP TRIGGER IF EXISTS set_samples_mol_rdkit_trg ON samples'
+        execute 'DROP FUNCTION IF EXISTS set_samples_mol_rdkit()'
+      end
     end
   end
 end
