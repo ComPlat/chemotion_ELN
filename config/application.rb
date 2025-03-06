eab99fd1db704268284fcb03a02888a4cd5f6239
PiTrem <pierre.tremouilhac@kit.edu>
2025-03-06 05:40:26 +0100

    chore: update schema for extension
    - get active record to look for tables in multiple schemas
    
    - ci test to use pg-rdkit image

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,9 @@ module Chemotion
 
     config.active_job.queue_adapter = :delayed_job
 
+    # Get ActiveRecord to look for tables in multiple schemas
+    config.active_record.schema_format = :sql
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
