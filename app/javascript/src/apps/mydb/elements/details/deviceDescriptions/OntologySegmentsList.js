e40a7595e2e7724a8c7f00e10b5deee3f5db8c50
Beate Quednau <bq@megorei.com>
2025-03-14 17:39:43 +0100

    Add editable ontology segment form modal

--- app/javascript/src/apps/mydb/elements/details/deviceDescriptions/OntologySegmentsList.js
+++ app/javascript/src/apps/mydb/elements/details/deviceDescriptions/OntologySegmentsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion } from 'react-bootstrap';
+import { Accordion, Form } from 'react-bootstrap';
 import { cloneDeep } from 'lodash';
 import { Constants, GenInterface, GenToolbar } from 'chem-generic-ui';
 
@@ -7,7 +7,7 @@ import UserStore from 'src/stores/alt/stores/UserStore';
 import Segment from 'src/models/Segment';
 import { observer } from 'mobx-react';
 
-const OntologySegmentsList = ({ store, element, handleSegmentsChange, handleRetrieveRevision }) => {
+const OntologySegmentsList = ({ store, element, isSelection }) => {
   const ontologies = element['ontologies'] || [];
   if (ontologies.length < 1) { return null; }
 
@@ -17,6 +17,34 @@ const OntologySegmentsList = ({ store, element, handleSegmentsChange, handleRetr
 
   const toggleSegment = (segment) => {
     store.toggleSegment(segment);
+  }
+
+  const handleSegmentsChange = (segment) => {
+    let segments = [...element.segments];
+    const sid = segments.findIndex((s) => s.segment_klass_id === segment.segment_klass_id);
+    if (sid >= 0) { segments.splice(sid, 1, segment); } else { segments.push(segment); }
+
+    store.changeDeviceDescription('segments', segments);
+  }
+
+  const handleRetrieveRevision = (revision, cb) => {
+    let segments = [...element.segments];
+    const selectedSegmentId = store.selected_segment_id;
+    const sid = segments.findIndex((s) => s.id === selectedSegmentId);
+
+    if (sid !== -1) {
+      segments[sid].properties = revision;
+      cb();
+      store.changeDeviceDescription('segments', segments);
+      store.setSelectedSegmentId(0);
+    } 
+  }
+
+  const changeFormSelection = (segment, index_ontology, index_segment, event) => {
+    event.stopPropagation();
+    let ontologies = [...element.ontologies];
+    ontologies[index_ontology].segments[index_segment].show = event.target.checked;
+    store.changeDeviceDescription('ontologies', ontologies);
   }
 
   const segmentVersionToolbar = (segment, segmentKlass, index, j) => {
@@ -32,25 +60,59 @@ const OntologySegmentsList = ({ store, element, handleSegmentsChange, handleRetr
     );
   }
 
-  const segmentsOfOntologies = () => {
-    list = [];
+  const genericFormFields = (rows, ontology, index) => {
+    ontology['segments'].forEach((segment, j) => {
+      const segmentKlass = segmentKlasses.find(
+        (s) => s.element_klass && s.element_klass.name === element.type && segment['segment_klass_id'] == s.id
+      );
+      existingSegment = element['segments'].find((s) => {
+        return segment['segment_klass_id'] === s.segment_klass_id;
+      });
 
-    ontologies
-      .sort((a, b) => a.index - b.index)
-      .forEach((ontology, index) => {
-        let rows = [];
-        if (!ontology['segments']) { return null; }
+      const segmentElement = existingSegment ? existingSegment : Segment.buildEmpty(cloneDeep(segmentKlass));
 
-        ontology['segments'].forEach((segment, j) => {
-          const segmentKlass = segmentKlasses.find(
-            (s) => s.element_klass && s.element_klass.name === element.type && segment['segment_klass_id'] == s.id
-          );
-          existingSegment = element['segments'].find((s) => {
-            return segment['segment_klass_id'] === s.segment_klass_id;
-          });
-
-          const segmentElement = existingSegment ? existingSegment : Segment.buildEmpty(cloneDeep(segmentKlass));
-
+      if (isSelection) {
+        const accordionRowIdent = `segment-row-${j}`;
+        const showValue = segment['show'] === undefined ? true : segment['show'];
+        rows.push(
+          <Accordion
+            className="mb-4"
+            key={`ontology-row-segments-list-${j}`}
+          >
+            <Accordion.Item eventKey={accordionRowIdent}>
+              <Accordion.Header>
+                <div className="d-flex gap-2">
+                  <div onClick={(event) => event.stopPropagation()}>
+                    <Form.Check
+                      type="checkbox"
+                      label="Use this form for"
+                      checked={showValue}
+                      onChange={(event) => changeFormSelection(segment, index, j, event)}
+                    />
+                  </div>
+                  <b>{segmentKlass.label}</b>
+                </div>
+              </Accordion.Header>
+              <Accordion.Body>
+                <div className="mb-3">
+                  <GenInterface
+                    generic={segmentElement}
+                    fnChange={handleSegmentsChange}
+                    extLayers={[]}
+                    genId={0}
+                    isPreview={false}
+                    isSearch={false}
+                    isActiveWF={false}
+                    fnNavi={() => {}}
+                    key={`ontology-${index}-${j}`}
+                  />
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        );
+      } else {
+        if (segment['show']) {
           rows.push(segmentVersionToolbar(segmentElement, segmentKlass, index, j));
           rows.push(
             <GenInterface
@@ -65,10 +127,27 @@ const OntologySegmentsList = ({ store, element, handleSegmentsChange, handleRetr
               key={`ontology-${index}-${j}`}
             />
           );
-        });
+        }
+      }
+    });
+    return rows;
+  }
+
+  const segmentsOfOntologies = () => {
+    list = [];
+
+    ontologies
+      .sort((a, b) => a.index - b.index)
+      .forEach((ontology, index) => {
+        let rows = [];
+        if (!ontology['segments']) { return null; }
+        if (store.ontology_index_for_edit !== index && store.ontology_index_for_edit !== -1) { return null; }
+
+        const showSegment = ontology['segments'].filter((s) => s['show'] !== false);
+        if (showSegment.length < 1 && !isSelection) { return null; }
 
         let deletedClass = '';
-        const accordionIdent = `segment-${index}`;
+        const accordionIdent = isSelection ? `segment-selection-${index}` : `segment-${index}`;
         let isActive = store.toggable_segments.findIndex(i => i === accordionIdent) === -1;
 
         if (ontology.data.is_deleted) {
@@ -76,25 +155,32 @@ const OntologySegmentsList = ({ store, element, handleSegmentsChange, handleRetr
           isActive = false;
         }
 
-        list.push(
-          <Accordion
-            className={`mb-4${deletedClass}`}
-            activeKey={isActive && accordionIdent}
-            onSelect={() => toggleSegment(accordionIdent)}
-            key={`ontology-segments-list-${index}`}
-          >
-            <Accordion.Item eventKey={accordionIdent}>
-              <Accordion.Header>
-                {`${ontology.data.label}`}
-              </Accordion.Header>
-              <Accordion.Body>
-                <div className="mb-3">
-                  {rows}
-                </div>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        ); 
+        rows = genericFormFields(rows, ontology, index);
+
+        if (isSelection) {
+          list.push(<h3 className="mb-4">{ontology.data.label}</h3>);
+          list.push(rows);
+        } else {
+          list.push(
+            <Accordion
+              className={`mb-4${deletedClass}`}
+              activeKey={isActive && accordionIdent}
+              onSelect={() => toggleSegment(accordionIdent)}
+              key={`ontology-segments-list-${index}`}
+            >
+              <Accordion.Item eventKey={accordionIdent}>
+                <Accordion.Header>
+                  {`${ontology.data.label}`}
+                </Accordion.Header>
+                <Accordion.Body>
+                  <div className="mb-3">
+                    {rows}
+                  </div>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          );
+        }
       });
     return list;
   }
