dbe87329bde9375a3b483013808ec1a51dcd3964
Johannes Haubold <jh@megorei.com>
2025-03-18 15:01:15 +0100

    Update SBMM-Sample factory

--- spec/factories/sequencebased_macromolecule_samples.rb
+++ spec/factories/sequencebased_macromolecule_samples.rb
@@ -6,9 +6,22 @@ FactoryBot.define do
     sequence(:short_label) { |index| "SBMM-S-#{index}" }
     external_label { nil }
     function_or_application { nil }
-    concentration { nil }
-    molarity { nil }
-    volume_as_used { }
+    concentration_value { nil }
+    concentration_unit { nil }
+    molarity_value { nil }
+    molarity_unit { nil }
+    activity_per_volume_value { nil }
+    activity_per_volumne_unit { nil }
+    activity_per_mass_value { nil }
+    activity_per_mass_unit { nil }
+    volume_as_used_value { nil }
+    volume_as_used_unit { nil }
+    amount_as_used_mol_value { nil }
+    amount_as_used_mol_unit { nil }
+    amount_as_used_mass_value { nil }
+    amount_as_used_mass_unit { nil }
+    activity_value { nil }
+    activity_unit { nil }
 
     sequence_based_macromolecule { nil }
   end
