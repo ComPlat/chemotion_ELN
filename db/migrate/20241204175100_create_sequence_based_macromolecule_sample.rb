2b5aac93df9d53fe3a65c301b160938e594af32f
Johannes Haubold <jh@megorei.com>
2025-03-18 14:59:37 +0100

    Some updates for SBMM-Samples

--- db/migrate/20241204175100_create_sequence_based_macromolecule_sample.rb
+++ db/migrate/20241204175100_create_sequence_based_macromolecule_sample.rb
@@ -7,10 +7,30 @@ class CreateSequenceBasedMacromoleculeSample < ActiveRecord::Migration[6.1]
       t.string :external_label, null: true
       t.string :short_label, null: false, unique: true
       t.string :function_or_application, null: true
-      t.float :concentration, null: true
-      t.float :molarity, null: true
-      t.float :amount_as_used, null: true
-      t.float :volume_as_used, null: true
+
+      t.float :concentration_value, null: true
+      t.string :concentration_unit, null: false, default: 'ng/L'
+
+      t.float :molarity_value, null: true
+      t.string :molarity_unit, null: false, default: 'mol/L'
+
+      t.float :activity_per_volume_value, null: true
+      t.string :activity_per_volume_unit, null: false, default: 'U/L'
+
+      t.float :activity_per_mass_value, null: true
+      t.string :activity_per_mass_unit, null: false, default: 'U/g'
+
+      t.float :volume_as_used_value, null: true
+      t.string :volume_as_used_unit, null: false, default: 'L'
+
+      t.float :amount_as_used_mol_value, null: true
+      t.string :amount_as_used_mol_unit, null: false, default: 'mol'
+
+      t.float :amount_as_used_mass_value, null: true
+      t.string :amount_as_used_mass_unit, null: false, default: 'g'
+
+      t.float :activity_value, null: true
+      t.string :activity_unit, null: false, default: 'U'
 
       t.belongs_to :sequence_based_macromolecule, foreign_key: true, index: { name: "#{index_prefix}_sbmm" }
       t.belongs_to :user, foreign_key: true, index: { name: "#{index_prefix}_user" }
