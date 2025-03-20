aa9964a17f48bd6abd66312ee78648f80ffb45b6
Pei Chi Huang <pei-chi.huang@kit.edu>
2025-03-20 10:01:37 +0100

    refactor: generic device description klass migration (#2397)
    DRY migration that provides the generic device-description klass and revision
    as most attributes are automatically handled by the library
    
    ref: #2281

--- db/migrate/20240207121720_init_device_description_to_element_klasses.rb
+++ db/migrate/20240207121720_init_device_description_to_element_klasses.rb
@@ -1,49 +1,12 @@
 class InitDeviceDescriptionToElementKlasses < ActiveRecord::Migration[6.1]
   def up
-    klass = Labimotion::ElementKlass.where(name: 'device_description').first
-    if klass.nil?
-      klass = Labimotion::ElementKlass.create(name: 'device_description')
-      uuid = SecureRandom.uuid
-      properties = klass.properties_template || { uuid: uuid, layers: {}, select_options: {} }
-      properties['uuid'] = uuid
-      properties['eln'] = Chemotion::Application.config.version
-      properties['klass'] = 'ElementKlass'
-      select_options = properties['select_options']
-      select_options&.map { |k, v| select_options[k] = { desc: k, options: v } }
-      properties['select_options'] = select_options || {}
-
-      attributes = {
-        name: 'device_description',
-        label: 'Device Description',
-        desc: 'ELN Device Description',
-        icon_name: 'icon-device_description',
-        is_active: true,
-        klass_prefix: '',
-        is_generic: false,
-        place: 5,
-        uuid: uuid,
-        released_at: DateTime.now,
-        properties_template: properties,
-        properties_release: properties
-      }
-      klass.update(attributes)
-      klass.reload
-
-      revision_attributes = {
-        element_klass_id: klass.id,
-        uuid: klass.uuid,
-        deleted_at: klass.deleted_at,
-        properties_release: klass.properties_template,
-        released_at: klass.released_at
-      }
-      Labimotion::ElementKlassesRevision.create(revision_attributes)
-    end
+    klass = Labimotion::ElementKlass.find_or_create_by(name: "device_description")
+    el = 'device_description'
+    attributes = { label: el.titleize, desc: "ELN #{el.titleize}", icon_name: "icon-#{el}", klass_prefix: '', properties_template: {}, is_generic: false, place: 6 }
+    klass&.update(attributes)
   end
 
   def down
-    klass = Labimotion::ElementKlass.where(name: 'device_description').first
-    if klass.present?
-      klass.destroy!
-    end
+    Labimotion::ElementKlass.where(name: "device_description").destroy_all
   end
 end
