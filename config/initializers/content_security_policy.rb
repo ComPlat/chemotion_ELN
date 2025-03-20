7d1ef322247670ae6494c801a4d0fe949a569b93
PiTrem <pierre.tremouilhac@kit.edu>
2025-03-20 14:23:54 +0100

    revert: csp cover in #2399

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,15 +5,15 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-  #   policy.default_src :self, :https
-  #   policy.font_src    :self, :https, :data
-  #   policy.img_src     :self, :https, :data
-  #   policy.object_src  :none
-  #   policy.script_src  :self, :https
-  #   policy.style_src   :self, :https
+#   policy.default_src :self, :https
+#   policy.font_src    :self, :https, :data
+#   policy.img_src     :self, :https, :data
+#   policy.object_src  :none
+#   policy.script_src  :self, :https
+#   policy.style_src   :self, :https
 
-  #   # Specify URI for violation reports
-  #   # policy.report_uri "/csp-violation-report-endpoint"
+#   # Specify URI for violation reports
+#   # policy.report_uri "/csp-violation-report-endpoint"
 
   # connect_src for log and webpack-dev-serverwebsockets in development
   if Rails.env.development?
@@ -22,12 +22,12 @@ Rails.application.config.content_security_policy do |policy|
     host = url.host
     port = url.port
 
-    src = [:self, 'blob:', 'http://localhost:3035', 'http://webpacker:3035', 'ws://localhost:3035', 'https://service.tib.eu']
+    src = [:self, 'http://localhost:3035', 'http://webpacker:3035', 'ws://localhost:3035', 'https://service.tib.eu']
     src += ["ws://#{host}:3035", "#{scheme}://#{host}:3035"] if host.present?
     src += ["#{scheme}://#{host}:#{url.port}"] if host.present? && port.present?
     puts "connect_src: #{src}"
 
-    policy.connect_src(*src)
+    policy.connect_src *src
   end
 end
 
