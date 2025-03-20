e9c56fc4145929accfa3ba83d18367a02230d8a3
PiTrem <pierre.tremouilhac@kit.edu>
2025-03-20 15:16:41 +0100

    build(test): use sql schema format for test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory
   # config.active_storage.service = :test
 
+  config.active_record.schema_format = :sql
+
   # config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
