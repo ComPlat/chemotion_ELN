8b15e5bfdc804f80e45f4a86a7497a2d76c600f9
Ashwini <amurthy.ashwini@gmail.com>
2025-03-21 12:15:36 +0000

    feat: frontend changes to render templates and lined vessels

--- app/javascript/src/apps/mydb/elements/details/vessels/VesselTemplateDetails.js
+++ app/javascript/src/apps/mydb/elements/details/vessels/VesselTemplateDetails.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { StoreContext } from 'src/stores/mobx/RootStore';
 import PropTypes from 'prop-types';
-import { Card, Button, Form, Row, Col } from 'react-bootstrap';
+import { Card, Button, Form, Row, Col, Table } from 'react-bootstrap';
 import DetailActions from 'src/stores/alt/actions/DetailActions';
+import VesselsFetcher from 'src/fetchers/VesselsFetcher';
 
 function VesselTemplateDetails({ template, vesselInstances, toggleFullScreen }) {
-  const vesselTemplate = template.vessel_template;
+
+  const { vesselDetailsStore } = useContext(StoreContext);
+  const vesselId = template.id;
+  const vesselItem = vesselDetailsStore.getVessel(vesselId) || template.vessel_template;
+  const [vesselTemplate, setVesselTemplate] = useState(template.vessel_template);
+  const [instances, setInstances] = useState(vesselInstances);
+  const [isTemplateUpdated, setIsTemplateUpdated] = useState(false);
 
   const renderEnlargenButton = () => (
     <Button
@@ -41,78 +49,102 @@ function VesselTemplateDetails({ template, vesselInstances, toggleFullScreen }) 
     </div>
   );
 
+  const formatLabel = (field) => field.replace('_', ' ').toLowerCase().replace(/^\w/, (c) => c.toUpperCase());
+
+  const handleTemplateChange = (field, value) => {
+    setVesselTemplate((prev) => ({ ...prev, [field]: value }));
+    setIsTemplateUpdated(true);
+  };
+
+  const updateTemplate = () => {
+    VesselsFetcher.updateVesselTemplate(vesselTemplate.id, vesselTemplate);
+    setIsTemplateUpdated(false);
+  };
+
+  const handleInstanceChange = (index, field, value) => {
+    setInstances((prevInstances) => {
+      const updatedInstances = [...prevInstances];
+      updatedInstances[index] = { ...updatedInstances[index], [field]: value };
+      return updatedInstances;
+    });
+  };
+
+  const updateInstance = (index) => {
+    VesselsFetcher.updateVesselInstance(instances[index].id, instances[index]);
+  };
+
   return (
-    <Card className="detail-card">
-      <Card.Header>
-        {renderHeaderContent()}
-      </Card.Header>
-      <Card.Body className="bg-gray-100">
-        <h5 className="my-2">Vessel template details:</h5>
-        <Form>
-          <Form.Group as={Row} className="mb-2">
-            <Form.Label column sm={2}><strong>Name:</strong></Form.Label>
-            <Col sm={4}>
-              <Form.Control
-                type="text"
-                readOnly
-                value={vesselTemplate.name}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-2">
-            <Form.Label column sm={2}><strong>Details:</strong></Form.Label>
-            <Col sm={4}>
-              <Form.Control
-                type="text"
-                readOnly
-                value={vesselTemplate.details || ''}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-2">
-            <Form.Label column sm={2}><strong>Type:</strong></Form.Label>
-            <Col sm={4}>
-              <Form.Control
-                type="text"
-                readOnly
-                value={vesselTemplate.vessel_type || ''}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-2">
-            <Form.Label column sm={2}><strong>Material:</strong></Form.Label>
-            <Col sm={4}>
-              <Form.Control
-                type="text"
-                readOnly
-                value={vesselTemplate.material_type || ''}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-2">
-            <Form.Label column sm={2}><strong>Volume:</strong></Form.Label>
-            <Col sm={4}>
-              <Form.Control
-                type="text"
-                readOnly
-                value={`${vesselTemplate.volume_amount || ''} ${vesselTemplate.volume_unit || ''}`}
-              />
-            </Col>
-          </Form.Group>
-        </Form>
-        <h5 className="mb-4">Vessel instances:</h5>
-        <Form>
-          {vesselInstances.map((item) => (
-            <Form.Group as={Row} className="mb-2" key={item.id}>
-              <Form.Label column sm={2} />
-              <Col sm={4}>
-                <Form.Control type="text" readOnly value={item.name || 'Unnamed instance'} />
-              </Col>
-            </Form.Group>
-          ))}
-        </Form>
-      </Card.Body>
-    </Card>
+    <>
+      <Card className="detail-card shadow-sm">
+        <Card.Header>
+          {renderHeaderContent()}
+        </Card.Header>
+        <Card.Body className="bg-light">
+          <Card.Title className="mb-3">Vessel template details</Card.Title>
+          <Form>
+            {['name', 'details', 'vessel_type', 'material_type', 'material_details', 'volume_amount', 'volume_unit']
+              .map((field) => (
+                <Form.Group as={Row} key={field} className="mb-2 align-items-center">
+                  <Form.Label column sm={3}>
+                    {formatLabel(field)}
+                    :
+                  </Form.Label>
+                  <Col sm={5}>
+                    <Form.Control
+                      type="text"
+                      value={vesselTemplate[field] || ''}
+                      onChange={(e) => handleTemplateChange(field, e.target.value)}
+                    />
+                  </Col>
+                </Form.Group>
+              ))}
+            <div>
+              <Button variant="primary" size="sm" className="mt-2" onClick={updateTemplate} disabled={!isTemplateUpdated}>
+                Update Template
+              </Button>
+            </div>
+          </Form>
+        </Card.Body>
+      </Card>
+      <Card className="detail-card shadow-sm">
+        <Card.Body>
+          <Card.Title classNME="mb-3">Vessel Instances</Card.Title>
+          <Table bordered hover responsive className="table-sm border-rounded">
+            <thead className="table-light">
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Barcode</th>
+                <th>QR Code</th>
+                <th>Weight amount</th>
+                <th>Weight unit</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {instances.map((instance, index) => (
+                <tr key={instance.id}>
+                  {['name', 'description', 'bar_code', 'qr_code', 'weight_amount', 'weight_unit']
+                    .map((field) => (
+                      <td key={field} className="p-1">
+                        <Form.Control
+                          type="text"
+                          className="border-0 bg-transparent"
+                          value={instance[field] || ''}
+                          onChange={(e) => handleInstanceChange(index, field, e.target.value)}
+                        />
+                      </td>
+                    ))}
+                  <td>
+                    <Button variant="primary" size="xsm" onClick={() => updateInstance(index)}>Update</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
 
