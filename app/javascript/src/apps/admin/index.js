6469875a092a957ea485315180da5e895b13cad1
Ali Zaib <alizaib1217@gmail.com>
2025-03-24 09:59:47 +0000

    refactor: removed react dnd on admin page.

--- app/javascript/src/apps/admin/index.js
+++ app/javascript/src/apps/admin/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import AdminHome from 'src/apps/admin/AdminHome';
 
 document.addEventListener('DOMContentLoaded', () => {
   const domElement = document.getElementById('AdminHome');
   if (domElement) {
-    ReactDOM.render(
-      <DndProvider backend={HTML5Backend}>
-        <AdminHome />
-      </DndProvider>,
-      domElement
-    );
+    ReactDOM.render(<AdminHome />, domElement);
   }
 });
