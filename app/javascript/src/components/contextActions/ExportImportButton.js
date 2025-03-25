d627153232e53936a41fdd86603945b2b2e2ade0
Adam Basha <55552142+adambasha0@users.noreply.github.com>
2025-03-25 14:57:09 +0100

    fix(UI): import chemicals modal (#2414)
    remove dependency on  the modal title to set the import action (standard samples or chemicals)
    
    removed Import Chemicals to Collection option from header-import and introduced a checkbox in the import module to select between importing as a chemical or a sample.
    
    ref: #2394

--- app/javascript/src/components/contextActions/ExportImportButton.js
+++ app/javascript/src/components/contextActions/ExportImportButton.js
@@ -14,7 +14,7 @@ import ModalImportCollection from 'src/components/contextActions/ModalImportColl
 import { elementShowOrNew } from 'src/utilities/routesUtils';
 
 const importSampleFunction = (updateModalProps, importAsChemical) => {
-  const title = importAsChemical ? 'Import chemicals from file' : 'Import samples from file';
+  const title = 'Import samples from file';
   const component = ModalImport;
   const action = ElementActions.importSamplesFromFile;
   const listSharedCollections = false;
@@ -143,13 +143,6 @@ function ExportImportButton({ isDisabled, updateModalProps, customClass }) {
           title="Import from spreadsheet or sdf"
         >
           Import samples to collection
-        </Dropdown.Item>
-        <Dropdown.Item
-          onClick={() => importSampleFunction(updateModalProps, true)}
-          disabled={isDisabled}
-          title="Import chemicals from spreadsheet"
-        >
-          Import chemicals to collection
         </Dropdown.Item>
         <Dropdown.Divider />
         <Dropdown.Item
