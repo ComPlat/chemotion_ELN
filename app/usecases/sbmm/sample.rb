25a727b5a5f13247ad5e3562a042614fe3127b8c
Johannes Haubold <jh@megorei.com>
2025-03-25 15:53:26 +0100

    Fix some parameter issues

--- app/usecases/sbmm/sample.rb
+++ app/usecases/sbmm/sample.rb
@@ -11,7 +11,7 @@ module Usecases
 
       def create(params)
         sbmm = ::Usecases::Sbmm::Create.new.find_or_create_by(params[:sequence_based_macromolecule_attributes])
-        sample = SequenceBasedMacromoleculeSample.new(params.except(:sequence_based_macromolecule_attributes))
+        sample = SequenceBasedMacromoleculeSample.new(params.except(:sequence_based_macromolecule_attributes, :collection_id))
         sample.user = current_user
         sample.sequence_based_macromolecule = sbmm
         target_collections(params).each do |collection|
