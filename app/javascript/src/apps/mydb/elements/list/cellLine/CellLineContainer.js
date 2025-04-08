83e3166c5e179146038f905a66ef480dd80f174f
Luciano Maiwald <mail@luciano-maiwald.de>
2025-04-08 08:22:24 +0200

    Use ElementGroupsRenderer in CellLineContainer

--- app/javascript/src/apps/mydb/elements/list/cellLine/CellLineContainer.js
+++ app/javascript/src/apps/mydb/elements/list/cellLine/CellLineContainer.js
@@ -1,31 +1,45 @@
-import React from 'react';
-import CellLineEntry from 'src/apps/mydb/elements/list/cellLine/CellLineEntry';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { CellLinePropTypeTableEntry } from 'src/models/cellLine/CellLinePropTypes';
 
-export default function CellLineContainer({ cellLineGroups, isElementSelected, showDetails }) {
+import CellLineGroupItem from 'src/apps/mydb/elements/list/cellLine/CellLineGroupItem';
+import CellLineGroupHeader from 'src/apps/mydb/elements/list/cellLine/CellLineGroupHeader';
+import CellLine from 'src/models/cellLine/CellLine';
+
+import ElementGroupsRenderer from 'src/apps/mydb/elements/list/renderers/ElementGroupsRenderer';
+
+function CellLineContainer({
+  elements,
+  isGroupCollapsed,
+  toggleGroupCollapse,
+}) {
+  const getGroupKey = useCallback(
+    (element) => `${element.cellLineName}:${element.source}`,
+    []
+  );
+
   return (
-    <div className="list-container">
-      {cellLineGroups.map(
-        (group) => (
-          <CellLineEntry
-            key={group.cellLineItems[0].id}
-            cellLineItems={group.cellLineItems}
-            isElementSelected={isElementSelected}
-            showDetails={showDetails}
-          />
-        )
+    <ElementGroupsRenderer
+      elements={elements}
+      getGroupKey={getGroupKey}
+      isGroupCollapsed={isGroupCollapsed}
+      toggleGroupCollapse={toggleGroupCollapse}
+      renderGroupHeader={(group) => (
+        <CellLineGroupHeader cellLineItems={group} />
       )}
-    </div>
+      renderGroupItem={(item, showDetails) => (
+        <CellLineGroupItem
+          cellLineItem={item}
+          showDetails={showDetails}
+        />
+      )}
+    />
   );
 }
 
 CellLineContainer.propTypes = {
-  cellLineGroups: PropTypes.arrayOf(
-    PropTypes.shape({
-      cellLineItems: PropTypes.arrayOf(CellLinePropTypeTableEntry),
-    })
-  ).isRequired,
-  isElementSelected: PropTypes.func.isRequired,
-  showDetails: PropTypes.func.isRequired,
+  elements: PropTypes.arrayOf(PropTypes.instanceOf(CellLine)).isRequired,
+  isGroupCollapsed: PropTypes.func.isRequired,
+  toggleGroupCollapse: PropTypes.func.isRequired,
 };
+
+export default CellLineContainer;
