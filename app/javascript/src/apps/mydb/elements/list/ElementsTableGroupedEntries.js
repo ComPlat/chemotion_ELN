801af7fdc1978b7e2d942ad77298e0ab71d99e05
Luciano Maiwald <mail@luciano-maiwald.de>
2025-04-09 16:26:55 +0200

    Extract ReactionGroupHeader, ReactionGroupItem components

--- app/javascript/src/apps/mydb/elements/list/ElementsTableGroupedEntries.js
+++ app/javascript/src/apps/mydb/elements/list/ElementsTableGroupedEntries.js
@@ -1,53 +1,13 @@
 /* eslint-disable react/forbid-prop-types */
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ElementCollectionLabels from 'src/apps/mydb/elements/labels/ElementCollectionLabels';
 import ElementGroupsRenderer from 'src/apps/mydb/elements/list/renderers/ElementGroupsRenderer';
 
-import UIStore from 'src/stores/alt/stores/UIStore';
-
-import ReactionStatus from 'src/apps/mydb/elements/list/reaction/ReactionStatus';
-import ReactionRole from 'src/apps/mydb/elements/list/reaction/ReactionRole';
-import CommentIcon from 'src/components/comments/CommentIcon';
-import SvgWithPopover from 'src/components/common/SvgWithPopover';
 import { ShowUserLabels } from 'src/components/UserLabels';
 
-function ReactionsHeader({
-  group, element, onClick
-}) {
-  const { showPreviews } = UIStore.getState();
-
-  return (
-    <div
-      onClick={onClick}
-      role="button"
-    >
-      {showPreviews && (
-        <SvgWithPopover
-          hasPop
-          previewObject={{
-            txtOnly: '',
-            isSVG: true,
-            className: 'reaction-header',
-            src: element.svgPath
-          }}
-          popObject={{
-            title: group,
-            src: element.svgPath,
-            height: '26vh',
-            width: '52vw',
-          }}
-        />
-      )}
-    </div>
-  );
-}
-
-ReactionsHeader.propTypes = {
-  group: PropTypes.string.isRequired,
-  element: PropTypes.object.isRequired,
-  onClick: PropTypes.func.isRequired,
-};
+import ReactionGroupHeader from 'src/apps/mydb/elements/list/reaction/ReactionGroupHeader';
+import ReactionGroupItem from 'src/apps/mydb/elements/list/reaction/ReactionGroupItem';
 
 function GenericElementsHeader({
   group, onClick
@@ -68,114 +28,84 @@ GenericElementsHeader.propTypes = {
   onClick: PropTypes.func.isRequired,
 };
 
-export default class ElementsTableGroupedEntries extends Component {
-  getReactionGroupKey(element) {
-    const { elementsGroup } = this.props;
-    return element[elementsGroup];
+function GenericElementItem({ element, showDetails }) {
+  return (
+    <div
+      role="button"
+      onClick={showDetails}
+      className="d-flex gap-2"
+    >
+      <div className="preview-table">
+        {element.title()}
+      </div>
+      <ShowUserLabels element={element} />
+      <ElementCollectionLabels element={element} />
+    </div>
+  );
+}
+
+GenericElementItem.propTypes = {
+  element: PropTypes.object.isRequired,
+  showDetails: PropTypes.func.isRequired,
+};
+
+export default function ElementsTableGroupedEntries({
+  type,
+  elements,
+  elementsGroup,
+}) {
+  if (type === 'reaction') {
+    const getGroupKey = (element) => element[elementsGroup];
+    return (
+      <ElementGroupsRenderer
+        type="reaction"
+        elements={elements}
+        getGroupKey={(element) => getGroupKey(element)}
+        renderGroupHeader={(group, toggleGroupCollapse) => {
+          const groupKey = getGroupKey(group[0]);
+          return (
+            <ReactionGroupHeader
+              group={groupKey}
+              element={group[0]}
+              onClick={() => toggleGroupCollapse()}
+            />
+          );
+        }}
+        renderGroupItem={(item, showDetails) => (
+          <ReactionGroupItem element={item} showDetails={showDetails} />
+        )}
+      />
+    );
   }
 
-  getGenericGroupKey(element) {
-    const { elementsGroup } = this.props;
+  const getGroupKey = (element) => {
     const groupElements = elementsGroup.split('.');
     const layer = groupElements[0];
     const field = groupElements[1];
 
     const { fields } = (element.properties.layers[layer] || { fields: [{ field, value: '' }] });
     return fields.find((f) => f.field === field)?.value || '[empty]';
-  }
+  };
 
-  renderReactionElement(element, showDetails) {
-    return (
-      <div
-        onClick={showDetails}
-        role="button"
-        className="d-flex gap-2"
-      >
-        <div className="d-flex gap-2">
-          <SvgWithPopover
-            hasPop
-            previewObject={{
-              txtOnly: element.title(),
-              isSVG: true,
-              src: element.svgPath
-            }}
-            popObject={{
-              title: element.short_label,
-              src: element.svgPath,
-              height: '26vh',
-              width: '52vw'
-            }}
+  return (
+    <ElementGroupsRenderer
+      type={type}
+      elements={elements}
+      getGroupKey={(element) => getGroupKey(element)}
+      renderGroupHeader={(group, toggleGroupCollapse) => {
+        const groupKey = getGroupKey(group[0]);
+        return (
+          <GenericElementsHeader
+            group={groupKey}
+            onClick={() => toggleGroupCollapse()}
           />
-          <div className="d-flex gap-1 align-items-center">
-            <ReactionStatus element={element} />
-            <ReactionRole element={element} />
-            <ShowUserLabels element={element} />
-          </div>
-          <CommentIcon commentCount={element.comment_count} />
-          <ElementCollectionLabels element={element} key={element.id} />
-        </div>
-      </div>
-    );
-  }
-
-  renderGenericElement(element, showDetails) {
-    return (
-      <div
-        role="button"
-        onClick={showDetails}
-        className="d-flex gap-2"
-      >
-        <div className="preview-table">
-          {element.title()}
-        </div>
-        <ShowUserLabels element={element} />
-        <ElementCollectionLabels element={element} />
-      </div>
-    );
-  }
-
-  render() {
-    const { elements, type } = this.props;
-
-    if (type === 'reaction') {
-      return (
-        <ElementGroupsRenderer
-          type="reaction"
-          elements={elements}
-          getGroupKey={(element) => this.getReactionGroupKey(element)}
-          renderGroupHeader={(group, toggleGroupCollapse) => {
-            const groupKey = this.getReactionGroupKey(group[0]);
-            return (
-              <ReactionsHeader
-                group={groupKey}
-                element={group[0]}
-                onClick={() => toggleGroupCollapse()}
-              />
-            );
-          }}
-          renderGroupItem={(item, showDetails) => this.renderReactionElement(item, showDetails)}
-        />
-      );
-    }
-
-    return (
-      <ElementGroupsRenderer
-        type={type}
-        elements={elements}
-        getGroupKey={(element) => this.getGenericGroupKey(element)}
-        renderGroupHeader={(group, toggleGroupCollapse) => {
-          const groupKey = this.getGenericGroupKey(group[0]);
-          return (
-            <GenericElementsHeader
-              group={groupKey}
-              onClick={() => toggleGroupCollapse()}
-            />
-          );
-        }}
-        renderGroupItem={(item, showDetails) => this.renderGenericElement(item, showDetails)}
-      />
-    );
-  }
+        );
+      }}
+      renderGroupItem={(item, showDetails) => (
+        <GenericElementItem element={item} showDetails={showDetails} />
+      )}
+    />
+  );
 }
 
 ElementsTableGroupedEntries.propTypes = {
---
+++ app/javascript/src/apps/mydb/elements/list/reaction/ReactionGroupHeader.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import UIStore from 'src/stores/alt/stores/UIStore';
+
+import Reaction from 'src/models/Reaction';
+import SvgWithPopover from 'src/components/common/SvgWithPopover';
+
+function ReactionGroupHeader({ group, element, onClick }) {
+  const [showPreviews, setShowPreviews] = useState(UIStore.getState().showPreviews);
+
+  useEffect(() => {
+    const updateShowPreviews = (state) => {
+      setShowPreviews(state.showPreviews);
+    };
+
+    UIStore.listen(updateShowPreviews);
+    return () => UIStore.unlisten(updateShowPreviews);
+  }, []);
+
+  return (
+    <div
+      onClick={onClick}
+      role="button"
+    >
+      {showPreviews && (
+        <SvgWithPopover
+          hasPop
+          previewObject={{
+            txtOnly: '',
+            isSVG: true,
+            className: 'reaction-header',
+            src: element.svgPath
+          }}
+          popObject={{
+            title: group,
+            src: element.svgPath,
+            height: '26vh',
+            width: '52vw',
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+ReactionGroupHeader.propTypes = {
+  group: PropTypes.string.isRequired,
+  element: PropTypes.instanceOf(Reaction).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default ReactionGroupHeader;
