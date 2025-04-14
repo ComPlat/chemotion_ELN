732ef8922ec8f1de2ed17950ab348ee3fa67b9a8
Johannes Haubold <jh@megorei.com>
2025-04-14 14:21:53 +0200

    Fixes to make specs green again

--- app/api/helpers/container_helpers.rb
+++ app/api/helpers/container_helpers.rb
@@ -1,7 +1,10 @@
 module ContainerHelpers
   extend Grape::API::Helpers
 
-  def update_datamodel(container)
+
+  # NOTE: the second parameter is actually redundant, as the current_user gets injected as a helper in API.rb
+  # Still labimotion passes the parameter and so I had to re-add it to prevent breaking specs
+  def update_datamodel(container, _redundant_current_user_for_labimotion)
     usecase = Usecases::Containers::UpdateDatamodel.new(current_user)
     usecase.update_datamodel(container)
   end
