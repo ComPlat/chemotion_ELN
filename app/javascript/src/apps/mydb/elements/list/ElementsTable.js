fce708f6d125cfdaffaa7a6751472fe2cbe4d82f
Ashwini <amurthy.ashwini@gmail.com>
2025-04-16 13:08:00 +0000

    refactor: group vessel entry based on template id

--- app/javascript/src/apps/mydb/elements/list/ElementsTable.js
+++ app/javascript/src/apps/mydb/elements/list/ElementsTable.js
@@ -26,7 +26,7 @@ import PropTypes from 'prop-types';
 import CellLineGroup from 'src/models/cellLine/CellLineGroup';
 import CellLineContainer from 'src/apps/mydb/elements/list/cellLine/CellLineContainer';
 import VesselContainer from 'src/apps/mydb/elements/list/vessel/VesselContainer';
-import VesselGroup from 'src/models/vessel/VesselGroup';
+import { groupVesselsByTemplateId } from 'src/models/vessel/VesselGroup';
 import ChevronIcon from 'src/components/common/ChevronIcon';
 import DeviceDescriptionList from 'src/apps/mydb/elements/list/deviceDescriptions/DeviceDescriptionList';
 import DeviceDescriptionListHeader from 'src/apps/mydb/elements/list/deviceDescriptions/DeviceDescriptionListHeader';
@@ -696,10 +696,10 @@ export default class ElementsTable extends React.Component {
     } else if (type === 'vessel') {
       elementsTableEntries = (
         <VesselContainer
-          vesselGroups={VesselGroup.buildVesselGroups(elements)}
+          vesselGroups={groupVesselsByTemplateId(elements)}
         />
       );
-    }  else {
+    } else {
       elementsTableEntries = (
         <ElementsTableEntries
           elements={elements}
