203c08a48c8c1522d3771c4ce54160677366cc00
PiTrem <pierre.tremouilhac@kit.edu>
2025-04-22 14:36:16 +0200

    feat: add created_by_type to attachments since it can be generated by devices and users
    TODO implement full polymorphism or define multiple association

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -46,6 +46,7 @@ class Attachment < ApplicationRecord # rubocop:disable Metrics/ClassLength
 
   validate :check_file_size
 
+  after_initialize :set_default_created_by_type
   before_create :generate_key
 
   # reload to get identifier:uuid
@@ -76,6 +77,12 @@ class Attachment < ApplicationRecord # rubocop:disable Metrics/ClassLength
   scope :where_template, lambda {
     where(attachable_type: 'Template')
   }
+
+  def set_default_created_by_type
+    self.created_by_type ||= 'User'
+  rescue NoMethodError
+    # This is a workaround for the case ActiveRecord::Migrator.current_version < 20210921114428
+  end
 
   def extname
     File.extname(filename.to_s)
