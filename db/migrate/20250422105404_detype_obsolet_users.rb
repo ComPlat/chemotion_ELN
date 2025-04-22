99e49ca375c4a3293190bebdbadb467fb352a430
PiTrem <pierre.tremouilhac@kit.edu>
2025-04-22 13:21:35 +0200

    fix(db): de-type deleted user-device
    to avoid history serialization error since Device is not a User anymore
    
    ```
    ActiveRecord::SubclassNotFound
    Invalid single-table inheritance type: Device is not a subclass of User
    ```
    
    refs: #1736, #2068

---
+++ db/migrate/20250422105404_detype_obsolet_users.rb
@@ -0,0 +1,12 @@
+class DetypeObsoletUsers < ActiveRecord::Migration[6.1]
+  class User < ActiveRecord::Base
+    self.inheritance_column = nil
+    self.table_name = 'users'
+  end
+
+  # Operating on soft-deleted records causes issues if those with type `Device`
+  # since Device < User has been deprecated and refactor to its own table.
+  def up
+    User.where(type: 'Device').update_all(type:  'DeviceDeprecated') }
+  end
+end
