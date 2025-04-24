a7572a14ffdf43bccffcf30b2193defed02e7763
PiTrem <pierre.tremouilhac@kit.edu>
2025-04-24 14:56:28 +0200

    fix: arg parsing to query pubchem by cid (#2463)
    refs: #2447

--- spec/lib/pub_chem_spec.rb
+++ spec/lib/pub_chem_spec.rb
@@ -15,7 +15,23 @@ RSpec.describe 'PubChem' do
 
       target = []
       result = PubChem.most_occurance(target)
-      expect(result).to eq nil
+      expect(result).to be_nil
+    end
+  end
+
+  describe '.get_cas_from_cid' do
+    it 'builds the correct URL' do
+      expected_url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/123456/XML?heading=CAS'
+      expected_body = '<Value><StringWithMarkup><String>123-45-6</String></StringWithMarkup></Value>'
+      allow(HTTParty).to(
+        receive(:get).with(expected_url, anything)
+                     .and_return(instance_double(HTTParty::Response, success?: true, body: expected_body)),
+      )
+
+      [123_456, '123456', ' 1234 , 12345 ,123456 ', "1 \n \n 1234 \n 12345 \n 123456"].each do |cid|
+        result = PubChem.get_cas_from_cid(cid)
+        expect(result).to eq ['123-45-6']
+      end
     end
   end
 end
