ComPlat/chemotion_ELN

commit 94903f2392e18711962272268a3d33690a7bba25

feat: change history tracker with UI for auditing record changes (#2068)
Add a change history tracking with gem logidze.

Store a history which keeps track of every change made to certain
entities and add a "History" tab to the Reaction/Sample detail pages.
Related entities are displayed in the version history. E.g. Reaction ->
ReactionsSample.
Save timestamp and author of the version with the changes. 
A change diff to the previous version can be displayed.
Instead of one version per database, update versions are grouped by the
request id (since some requests can lead to multiple updates for the same record). 
These changes are merged to make it more intuitive for the user.

Reverting a record to previous state: 
the user can update selected fields of a record to a former values.
Note that this is not a roll-back and the applied reverting changes are tracked
as well.

Tracked entities:
- Sample
  - Residue
  - ElementalComposition
- Reaction
- ReactionsSample
- ResearchPlan
  - ResearchPlansWellplate
  - ResearchPlanMetadata
- Screen
- Wellplate
  - Well
- Container
- Attachment

(replace #1253)

-----

## Backend
- implement Logidze 
  - catch whodunit and wrap it into each DB update/insert transaction 
- api for fetching/reverting history
- fetchers, reverters, and serializers in app/service/versioning

## DB
- add json column to store the history for each tracked model 
- build specific `gem-fx` versioned sql logging functions
- added triggers on update insert for tracked elements
 a `gem-fx` versioned function is used to generate the default trigger for each model
 instead of using `gem-fx` versioned individual triggers since all triggers are identical
- NB migration squashed and timestamped forward
- ⚠️ migration initiate history object for all records of the tracked models  
- migration rollback confirmed:
NB: back-rolling the logging function also roll-back all triggers since they depends on it
- added soft deletion on container

app/javascript/src/apps/mydb/elements/details/reactions/ReactionDetails.js
@@ -2,12 +2,13 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/require-default-props */
 import React, { Component } from 'react';
+import Aviator from 'aviator';
 import PropTypes from 'prop-types';
 import {
   Button, Tabs, Tab, OverlayTrigger, Tooltip, Card, ButtonToolbar, ButtonGroup
 } from 'react-bootstrap';
 import SvgFileZoomPan from 'react-svg-file-zoom-pan-latest';
-import { findIndex } from 'lodash';
+import { findIndex, isEmpty } from 'lodash';
 import ElementCollectionLabels from 'src/apps/mydb/elements/labels/ElementCollectionLabels';
 import ElementResearchPlanLabels from 'src/apps/mydb/elements/labels/ElementResearchPlanLabels';
 import ElementAnalysesLabels from 'src/apps/mydb/elements/labels/ElementAnalysesLabels';
@@ -53,6 +54,32 @@ import { formatTimeStampsOfElement } from 'src/utilities/timezoneHelper';
 import GasPhaseReactionActions from 'src/stores/alt/actions/GasPhaseReactionActions';
 import { ShowUserLabels } from 'src/components/UserLabels';
 import ButtonGroupToggleButton from 'src/components/common/ButtonGroupToggleButton';
+// eslint-disable-next-line import/no-named-as-default
+import VersionsTable from 'src/apps/mydb/elements/details/VersionsTable';
+
+const handleProductClick = (product) => {
+  const uri = Aviator.getCurrentURI();
+  const uriArray = uri.split(/\//);
+  Aviator.navigate(`/${uriArray[1]}/${uriArray[2]}/sample/${product.id}`, { silent: true });
+  sampleShowOrNew({ params: { sampleID: product.id } });
+};
+
+const productLink = (product) => (
+  <span>
+    Analysis: &nbsp;
+    <span
+      aria-hidden="true"
+      className="pseudo-link"
+      onClick={() => handleProductClick(product)}
+      style={{ cursor: 'pointer' }}
+      title="Open sample window"
+    >
+      <i className="icon-sample" />
+      &nbsp;
+      {product.title()}
+    </span>
+  </span>
+);
 
 export default class ReactionDetails extends Component {
   constructor(props) {
@@ -60,7 +87,7 @@ export default class ReactionDetails extends Component {
 
     const { reaction } = props;
     this.state = {
-      reaction: reaction,
+      reaction,
       literatures: reaction.literatures,
       activeTab: UIStore.getState().reaction.activeTab,
       activeAnalysisTab: UIStore.getState().reaction.activeAnalysisTab,
@@ -116,28 +143,18 @@ export default class ReactionDetails extends Component {
       reaction: reactionFromCurrentState, activeTab, visible, activeAnalysisTab
     } = this.state;
     return (
-      reactionFromNextProps.id !== reactionFromCurrentState.id ||
-      reactionFromNextProps.updated_at !== reactionFromCurrentState.updated_at ||
-      reactionFromNextProps.reaction_svg_file !== reactionFromCurrentState.reaction_svg_file ||
-      !!reactionFromNextProps.changed || !!reactionFromNextProps.editedSample ||
-      nextActiveTab !== activeTab || nextVisible !== visible ||
-      nextActiveAnalysisTab !== activeAnalysisTab
+      reactionFromNextProps.id !== reactionFromCurrentState.id
+      || reactionFromNextProps.updated_at !== reactionFromCurrentState.updated_at
+      || reactionFromNextProps.reaction_svg_file !== reactionFromCurrentState.reaction_svg_file
+      || !!reactionFromNextProps.changed || !!reactionFromNextProps.editedSample
+      || nextActiveTab !== activeTab || nextVisible !== visible
+      || nextActiveAnalysisTab !== activeAnalysisTab
       || reactionFromNextState !== reactionFromCurrentState
     );
   }
 
   componentWillUnmount() {
-    UIStore.unlisten(this.onUIStoreChange)
-  }
-
-  onUIStoreChange(state) {
-    if (state.reaction.activeTab != this.state.activeTab ||
-      state.reaction.activeAnalysisTab !== this.state.activeAnalysisTab) {
-      this.setState({
-        activeTab: state.reaction.activeTab,
-        activeAnalysisTab: state.reaction.activeAnalysisTab
-      });
-    }
+    UIStore.unlisten(this.onUIStoreChange);
   }
 
   handleSubmit(closeView = false) {
@@ -153,11 +170,6 @@ export default class ReactionDetails extends Component {
     if (reaction.is_new || closeView) {
       DetailActions.close(reaction, true);
     }
-  }
-
-  reactionIsValid() {
-    const { reaction } = this.state;
-    return reaction.hasMaterials() && reaction.SMGroupValid();
   }
 
   handleReactionChange(reaction, options = {}) {
@@ -191,42 +203,66 @@ export default class ReactionDetails extends Component {
     this.handleReactionChange(newReaction, options);
   }
 
-  handleProductClick(product) {
-    const uri = Aviator.getCurrentURI();
-    const uriArray = uri.split(/\//);
-    Aviator.navigate(`/${uriArray[1]}/${uriArray[2]}/sample/${product.id}`, { silent: true });
-    sampleShowOrNew({ params: { sampleID: product.id } });
-  }
-
   handleProductChange(product, cb) {
-    let { reaction } = this.state
+    const { reaction } = this.state;
 
-    reaction.updateMaterial(product)
-    reaction.changed = true
+    reaction.updateMaterial(product);
+    reaction.changed = true;
 
-    this.setState({ reaction }, cb)
+    this.setState({ reaction }, cb);
   }
 
-  productLink(product) {
-    return (
-      <span>
-        Analysis:
-        <span className="pseudo-link"
-          onClick={() => this.handleProductClick(product)}
-          role="button"
-          title="Open sample window">
-          <i className="icon-sample" />{product.title()}
-        </span>
-      </span>
-    )
+  handleSelect = (key) => {
+    UIActions.selectTab({ tabKey: key, type: 'reaction' });
+    this.setState({
+      activeTab: key
+    });
+  };
+
+  handleSelectActiveAnalysisTab = (key) => {
+    UIActions.selectActiveAnalysisTab(key);
+    this.setState({
+      activeAnalysisTab: key
+    });
+  };
+
+  handleSegmentsChange(se) {
+    const { reaction } = this.state;
+    const { segments } = reaction;
+    const idx = findIndex(segments, (o) => o.segment_klass_id === se.segment_klass_id);
+    if (idx >= 0) { segments.splice(idx, 1, se); } else { segments.push(se); }
+    reaction.segments = segments;
+    reaction.changed = true;
+    this.setState({ reaction });
+  }
+
+  onUIStoreChange(state) {
+    const { activeTab } = this.state;
+    const { activeAnalysisTab } = this.state;
+    if (state.reaction.activeTab !== activeTab
+      || state.reaction.activeAnalysisTab !== activeAnalysisTab) {
+      this.setState({
+        activeTab: state.reaction.activeTab,
+        activeAnalysisTab: state.reaction.activeAnalysisTab
+      });
+    }
+  }
+
+  onTabPositionChanged(visible) {
+    this.setState({ visible });
+  }
+
+  reactionIsValid() {
+    const { reaction } = this.state;
+    return reaction.hasMaterials() && reaction.SMGroupValid();
   }
 
   productData(reaction) {
-    const { products } = this.state.reaction;
+    const { products } = reaction;
     const { activeAnalysisTab } = this.state;
 
     const tabs = products.map((product, key) => {
-      const title = this.productLink(product);
+      const title = productLink(product);
       const setState = () => this.handleProductChange(product);
       const handleSampleChanged = (_, cb) => this.handleProductChange(product, cb);
 
@@ -313,8 +349,8 @@ export default class ReactionDetails extends Component {
       <ElementCollectionLabels element={reaction} key={reaction.id} placement="right" />
     );
 
-    const rsPlanLabel = !(reaction.isNew || _.isEmpty(reaction.research_plans)) && (
-      <ElementResearchPlanLabels plans={reaction.research_plans} placement="right" />
+    const rsPlanLabel = (reaction.isNew || isEmpty(reaction.research_plans)) ? null : (
+      <ElementResearchPlanLabels plans={reaction.research_plans} key={reaction.id} placement="right" />
     );
 
     return (
@@ -336,8 +372,7 @@ export default class ReactionDetails extends Component {
           <ButtonToolbar className="gap-1 justify-content-end">
             <PrintCodeButton element={reaction} />
             {!reaction.isNew
-              && <OpenCalendarButton isPanelHeader eventableId={reaction.id} eventableType="Reaction" />
-            }
+              && <OpenCalendarButton isPanelHeader eventableId={reaction.id} eventableType="Reaction" />}
             <OverlayTrigger
               placement="bottom"
               overlay={<Tooltip id="generateReport">Generate Report</Tooltip>}
@@ -410,46 +445,6 @@ export default class ReactionDetails extends Component {
     );
   }
 
-  handleSelect(key) {
-    UIActions.selectTab({ tabKey: key, type: 'reaction' });
-    this.setState({
-      activeTab: key
-    });
-  }
-
-  handleSelectActiveAnalysisTab(key) {
-    UIActions.selectActiveAnalysisTab(key);
-    this.setState({
-      activeAnalysisTab: key
-    });
-  }
-
-  onTabPositionChanged(visible) {
-    this.setState({ visible });
-  }
-
-  reactionIsValid() {
-    const { reaction } = this.state;
-    return reaction.hasMaterials() && reaction.SMGroupValid();
-  }
-
-  productLink(product) {
-    return (
-      <span>
-        Analysis:
-        <span
-          className="pseudo-link"
-          onClick={() => this.handleProductClick(product)}
-          role="button"
-          title="Open sample window"
-        >
-          <i className="icon-sample me-1 ms-1" />
-          {product.title()}
-        </span>
-      </span>
-    );
-  }
-
   updateReactionSvg() {
     const { reaction } = this.state;
     const materialsSvgPaths = {
@@ -473,16 +468,6 @@ export default class ReactionDetails extends Component {
         reaction.reaction_svg_file = result.reaction_svg;
         this.setState(reaction);
       });
-  }
-
-  handleSegmentsChange(se) {
-    const { reaction } = this.state;
-    const { segments } = reaction;
-    const idx = findIndex(segments, (o) => o.segment_klass_id === se.segment_klass_id);
-    if (idx >= 0) { segments.splice(idx, 1, se); } else { segments.push(se); }
-    reaction.segments = segments;
-    reaction.changed = true;
-    this.setState({ reaction });
   }
 
   handleGaseousChange() {
@@ -538,7 +523,7 @@ export default class ReactionDetails extends Component {
           }
           <ReactionDetailsScheme
             reaction={reaction}
-            onReactionChange={(reaction, options) => this.handleReactionChange(reaction, options)}
+            onReactionChange={(r, options) => this.handleReactionChange(r, options)}
             onInputChange={(type, event) => this.handleInputChange(type, event)}
           />
         </Tab>
@@ -600,7 +585,22 @@ export default class ReactionDetails extends Component {
             isActive={activeTab === REACTION_VARIATIONS_TAB_KEY}
           />
         </Tab>
-      )
+      ),
+      versions: (
+        <Tab
+          eventKey="versioning"
+          title="Versions"
+          key={`Versions_Reaction_${reaction.id.toString()}`}
+        >
+          <VersionsTable
+            type="reactions"
+            id={reaction.id}
+            element={reaction}
+            parent={this}
+            isEdited={reaction.changed}
+          />
+        </Tab>
+      ),
     };
 
     const tabTitlesMap = {
@@ -618,10 +618,10 @@ export default class ReactionDetails extends Component {
     const submitLabel = (reaction && reaction.isNew) ? 'Create' : 'Save';
     const exportButton = (reaction && reaction.isNew) ? null : <ExportSamplesButton type="reaction" id={reaction.id} />;
 
-    const currentTab = (activeTab !== 0 && activeTab) || visible[0];
+    const currentActiveTab = (activeTab !== 0 && activeTab) || visible[0];
 
     return (
-      <Card className={"detail-card" + (reaction.isPendingToSave ? " detail-card--unsaved" : "")}>
+      <Card className={`detail-card${reaction.isPendingToSave ? ' detail-card--unsaved' : ''}`}>
         <Card.Header>
           {this.reactionHeader(reaction)}
         </Card.Header>
@@ -635,7 +635,13 @@ export default class ReactionDetails extends Component {
           />
           {this.state.sfn && <ScifinderSearch el={reaction} />}
           <div className="tabs-container--with-borders">
-            <Tabs activeKey={currentTab} onSelect={this.handleSelect.bind(this)} id="reaction-detail-tab" unmountOnExit={true}>
+            <Tabs
+              mountOnEnter
+              activeKey={currentActiveTab}
+              onSelect={this.handleSelect}
+              id="reaction-detail-tab"
+              unmountOnExit
+            >
               {tabContents}
             </Tabs>
             <CommentModal element={reaction} />

app/javascript/src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails.js
@@ -42,9 +42,13 @@ import { formatTimeStampsOfElement } from 'src/utilities/timezoneHelper';
 import UserStore from 'src/stores/alt/stores/UserStore';
 import MatrixCheck from 'src/components/common/MatrixCheck';
 import { commentActivation } from 'src/utilities/CommentHelper';
+// eslint-disable-next-line import/no-named-as-default
+import VersionsTable from 'src/apps/mydb/elements/details/VersionsTable';
 
 export default class ResearchPlanDetails extends Component {
+  // eslint-disable-next-line react/static-property-placement
   static contextType = StoreContext;
+
   constructor(props) {
     super(props);
     const { researchPlan } = props;
@@ -206,6 +210,7 @@ export default class ResearchPlanDetails extends Component {
     // update only this attachment
     researchPlan.attachments.map((currentAttachment) => {
       if (currentAttachment.id === attachment.id) return attachment;
+      return null;
     });
     this.setState({ researchPlan });
     this.forceUpdate();
@@ -246,6 +251,7 @@ export default class ResearchPlanDetails extends Component {
 
     ResearchPlansFetcher.postResearchPlanMetadata(args).then((result) => {
       if (result.error) {
+        // eslint-disable-next-line no-alert
         alert(result.error);
       }
     });
@@ -257,6 +263,13 @@ export default class ResearchPlanDetails extends Component {
 
   onTabPositionChanged(visible) {
     this.setState({ visible });
+  }
+
+  toggleFullScreen() {
+    this.props.toggleFullScreen();
+
+    // toogle update prop to notify react data grid for view change
+    this.setState((prevState) => ({ update: !prevState.update }));
   }
 
   dropWellplate(wellplate) {
@@ -443,12 +456,12 @@ export default class ResearchPlanDetails extends Component {
 
   renderAnalysesTab(researchPlan) {
     return (
-          <ResearchPlanDetailsContainers
-            handleSubmit={this.handleSubmit}
-            handleResearchPlanChange={this.handleResearchPlanChange}
-            researchPlan={researchPlan}
-            readOnly={false}
-          />
+      <ResearchPlanDetailsContainers
+        handleSubmit={this.handleSubmit}
+        handleResearchPlanChange={this.handleResearchPlanChange}
+        researchPlan={researchPlan}
+        readOnly={false}
+      />
     );
   }
 
@@ -574,6 +587,21 @@ export default class ResearchPlanDetails extends Component {
           <ResearchPlanMetadata researchPlan={researchPlan} />
         </Tab>
       ),
+      versions: (
+        <Tab
+          eventKey="versioning"
+          title="Versions"
+          key={`Versions_ResearchPlan_${researchPlan.id.toString()}`}
+        >
+          <VersionsTable
+            type="research_plans"
+            id={researchPlan.id}
+            element={researchPlan}
+            parent={this}
+            isEdited={researchPlan.changed}
+          />
+        </Tab>
+      ),
     };
 
     const tabTitlesMap = {
@@ -605,7 +633,13 @@ export default class ResearchPlanDetails extends Component {
             onTabPositionChanged={this.onTabPositionChanged}
           />
           <div className="tabs-container--with-borders">
-            <Tabs activeKey={activeTab} onSelect={(key) => this.handleSelect(key)} id="screen-detail-tab">
+            <Tabs
+              mountOnEnter
+              unmountOnExit
+              activeKey={activeTab}
+              onSelect={(key) => this.handleSelect(key)}
+              id="screen-detail-tab"
+            >
               {tabContents}
             </Tabs>
           </div>

app/javascript/src/apps/mydb/elements/details/samples/propertiesTab/SampleForm.js
@@ -13,6 +13,7 @@ import DetailActions from 'src/stores/alt/actions/DetailActions';
 import NumeralInputWithUnitsCompo from 'src/apps/mydb/elements/details/NumeralInputWithUnitsCompo';
 import NumericInputUnit from 'src/apps/mydb/elements/details/NumericInputUnit';
 import TextRangeWithAddon from 'src/apps/mydb/elements/details/samples/propertiesTab/TextRangeWithAddon';
+import { solventOptions } from 'src/components/staticDropdownOptions/options';
 import SampleDetailsSolvents from 'src/apps/mydb/elements/details/samples/propertiesTab/SampleDetailsSolvents';
 import NotificationActions from 'src/stores/alt/actions/NotificationActions';
 import InventoryFetcher from 'src/fetchers/InventoryFetcher';
@@ -106,6 +107,31 @@ export default class SampleForm extends React.Component {
     );
   }
 
+  infoMessage = () => (
+    <Tooltip id="assignButton">
+      Information mirrored to the reaction table describing the content of pure
+      compound or amount of pure compound in a given solution
+    </Tooltip>
+  );
+
+  // Input components of sample details should be disabled if detail level
+  // does not allow to read their content
+  topSecretCheckbox(sample) {
+    if (sample.can_update) {
+      return (
+        <Checkbox
+          inputRef={(ref) => { this.topSecretInput = ref; }}
+          checked={sample.is_top_secret}
+          onChange={(e) => this.handleFieldChanged('is_top_secret', e.target.checked)}
+        >
+          Top secret
+        </Checkbox>
+      );
+    }
+
+    return (<span />);
+  }
+
   drySolventCheckbox(sample) {
     if (sample.can_update) {
       return (
@@ -145,12 +171,12 @@ export default class SampleForm extends React.Component {
 
   addMolName(moleculeName) {
     this.setState({ isMolNameLoading: true });
-    DetailActions.updateMoleculeNames(this.props.sample, moleculeName);
+    DetailActions.updateMoleculeNames(this.props.sample, moleculeName.label);
   }
 
   updateMolName(e) {
     const { sample } = this.props;
-    sample.molecule_name = e.value;
+    sample.molecule_name = e;
     this.props.handleSampleChanged(sample);
   }
 
@@ -255,9 +281,9 @@ export default class SampleForm extends React.Component {
             onMenuOpen={() => this.openMolName(sample)}
             onChange={this.updateMolName}
             isLoading={this.state.isMolNameLoading}
-            value={moleculeNames.find(({ value }) => value == mno?.value)}
-            onCreateOption={this.addMolName}
-            className="flex-grow-1"
+            value={!newMolecule && mno && mno.value}
+            onNewOptionClick={this.addMolName}
+            clearable={false}
           />
           {this.structureEditorButton(!sample.can_update)}
         </InputGroup>
@@ -504,6 +530,22 @@ export default class SampleForm extends React.Component {
         unit={unit}
         numericValue={value}
         label={label}
+      />
+    );
+  }
+
+  sampleSolvent(sample) {
+    return (
+      <Select
+        ref={(input) => { this.solventInput = input; }}
+        id="solventInput"
+        name="solvents"
+        style={{ marginBottom: '15px' }}
+        multi={false}
+        options={solventOptions}
+        value={sample.solvent}
+        disabled={!sample.can_update}
+        onChange={(e) => this.handleFieldChanged('solvent', e)}
       />
     );
   }
@@ -850,7 +892,7 @@ export default class SampleForm extends React.Component {
 }
 
 SampleForm.propTypes = {
-  sample: PropTypes.object,
+  sample: PropTypes.object.isRequired,
   handleSampleChanged: PropTypes.func.isRequired,
   showStructureEditor: PropTypes.func.isRequired,
   customizableField: PropTypes.func.isRequired,

commit 60057c10819da04fbac17a832b44c55ad2d5c047

fix: regression introduced by #2068

app/javascript/src/apps/mydb/elements/details/reactions/ReactionDetails.js
@@ -66,16 +66,14 @@ const handleProductClick = (product) => {
 
 const productLink = (product) => (
   <span>
-    Analysis: &nbsp;
+    Analysis:
     <span
       aria-hidden="true"
       className="pseudo-link"
       onClick={() => handleProductClick(product)}
-      style={{ cursor: 'pointer' }}
       title="Open sample window"
     >
-      <i className="icon-sample" />
-      &nbsp;
+      <i className="icon-sample mx-1" />
       {product.title()}
     </span>
   </span>
@@ -618,7 +616,7 @@ export default class ReactionDetails extends Component {
     const submitLabel = (reaction && reaction.isNew) ? 'Create' : 'Save';
     const exportButton = (reaction && reaction.isNew) ? null : <ExportSamplesButton type="reaction" id={reaction.id} />;
 
-    const currentActiveTab = (activeTab !== 0 && activeTab) || visible[0];
+    const currentTab = (activeTab !== 0 && activeTab) || visible[0];
 
     return (
       <Card className={`detail-card${reaction.isPendingToSave ? ' detail-card--unsaved' : ''}`}>
@@ -637,7 +635,7 @@ export default class ReactionDetails extends Component {
           <div className="tabs-container--with-borders">
             <Tabs
               mountOnEnter
-              activeKey={currentActiveTab}
+              activeKey={currentTab}
               onSelect={this.handleSelect}
               id="reaction-detail-tab"
               unmountOnExit

app/javascript/src/apps/mydb/elements/details/researchPlans/ResearchPlanDetails.js
@@ -266,13 +266,6 @@ export default class ResearchPlanDetails extends Component {
     this.setState({ visible });
   }
 
-  toggleFullScreen() {
-    this.props.toggleFullScreen();
-
-    // toogle update prop to notify react data grid for view change
-    this.setState((prevState) => ({ update: !prevState.update }));
-  }
-
   dropWellplate(wellplate) {
     const { researchPlan } = this.state;
     researchPlan.changed = true;

app/javascript/src/apps/mydb/elements/details/samples/propertiesTab/SampleForm.js
@@ -107,31 +107,6 @@ export default class SampleForm extends React.Component {
     );
   }
 
-  infoMessage = () => (
-    <Tooltip id="assignButton">
-      Information mirrored to the reaction table describing the content of pure
-      compound or amount of pure compound in a given solution
-    </Tooltip>
-  );
-
-  // Input components of sample details should be disabled if detail level
-  // does not allow to read their content
-  topSecretCheckbox(sample) {
-    if (sample.can_update) {
-      return (
-        <Checkbox
-          inputRef={(ref) => { this.topSecretInput = ref; }}
-          checked={sample.is_top_secret}
-          onChange={(e) => this.handleFieldChanged('is_top_secret', e.target.checked)}
-        >
-          Top secret
-        </Checkbox>
-      );
-    }
-
-    return (<span />);
-  }
-
   drySolventCheckbox(sample) {
     if (sample.can_update) {
       return (
@@ -171,7 +146,7 @@ export default class SampleForm extends React.Component {
 
   addMolName(moleculeName) {
     this.setState({ isMolNameLoading: true });
-    DetailActions.updateMoleculeNames(this.props.sample, moleculeName.label);
+    DetailActions.updateMoleculeNames(this.props.sample, moleculeName);
   }
 
   updateMolName(e) {
@@ -269,7 +244,6 @@ export default class SampleForm extends React.Component {
     const newMolecule = !mno || sample._molecule.id !== mno.mid;
     let moleculeNames = newMolecule ? [] : [mno];
     if (sample && mnos) { moleculeNames = moleculeNames.concat(mnos); }
-
     return (
       <Form.Group>
         <Form.Label>Molecule name</Form.Label>
@@ -281,9 +255,9 @@ export default class SampleForm extends React.Component {
             onMenuOpen={() => this.openMolName(sample)}
             onChange={this.updateMolName}
             isLoading={this.state.isMolNameLoading}
-            value={!newMolecule && mno}
-            onNewOptionClick={this.addMolName}
-            clearable={false}
+            value={moleculeNames.find(({ value }) => value === mno?.value)}
+            onCreateOption={this.addMolName}
+            className="flex-grow-1"
           />
           {this.structureEditorButton(!sample.can_update)}
         </InputGroup>
@@ -307,7 +281,7 @@ export default class SampleForm extends React.Component {
 
   fetchNextInventoryLabel() {
     const { currentCollection } = UIStore.getState();
-    if (this.matchSelectedCollection(currentCollection)) {
+    if(this.matchSelectedCollection(currentCollection)) {
       InventoryFetcher.fetchInventoryOfCollection(currentCollection.id)
         .then((result) => {
           if (result && result.prefix && result.counter !== undefined) {
@@ -530,22 +504,6 @@ export default class SampleForm extends React.Component {
         unit={unit}
         numericValue={value}
         label={label}
-      />
-    );
-  }
-
-  sampleSolvent(sample) {
-    return (
-      <Select
-        ref={(input) => { this.solventInput = input; }}
-        id="solventInput"
-        name="solvents"
-        style={{ marginBottom: '15px' }}
-        multi={false}
-        options={solventOptions}
-        value={sample.solvent}
-        disabled={!sample.can_update}
-        onChange={(e) => this.handleFieldChanged('solvent', e)}
       />
     );
   }

app/javascript/src/stores/alt/actions/DetailActions.js
@@ -39,8 +39,8 @@ class DetailActions {
       MoleculesFetcher
         .updateNames(id, newMolName)
         .then((result) => {
-          const mn = result.find((r) => r.name === newMolName);
-          if (mn) sample.molecule_name = { label: mn.name, value: mn.id };
+          const mn = result.find((r) => r.label === newMolName);
+          if (mn) sample.molecule_name = mn;
           sample.molecule_names = result;
           dispatch(sample);
         })