fcc4bfb561537c8bd5b31b170608cdf3a456cf0a
Adrian Herrmann <adrian.herrmann@kit.edu>
2025-04-29 13:48:18 +0000

    added log_data to literature

--- db/schema.rb
+++ db/schema.rb
@@ -926,6 +926,7 @@ ActiveRecord::Schema.define(version: 2026_07_09_140001) do
     t.jsonb "refs"
     t.string "doi"
     t.string "isbn"
+    t.jsonb "log_data"
     t.index ["deleted_at"], name: "index_literatures_on_deleted_at"
   end
 
@@ -2777,8 +2778,9 @@ ActiveRecord::Schema.define(version: 2026_07_09_140001) do
       END;
       $function$
   SQL
-
-
+  create_trigger :logidze_on_literatures, sql_definition: <<-SQL
+      CREATE TRIGGER logidze_on_literatures BEFORE INSERT OR UPDATE ON public.literatures FOR EACH ROW WHEN ((COALESCE(current_setting('logidze.disabled'::text, true), ''::text) <> 'on'::text)) EXECUTE FUNCTION logidze_logger('null', 'updated_at')
+  SQL
   create_trigger :logidze_on_attachments, sql_definition: <<-SQL
       CREATE TRIGGER logidze_on_attachments BEFORE INSERT OR UPDATE ON public.attachments FOR EACH ROW WHEN ((COALESCE(current_setting('logidze.disabled'::text, true), ''::text) <> 'on'::text)) EXECUTE FUNCTION logidze_logger('null', 'updated_at')
   SQL
