a8e44f2a0d80d66d5bfc4a516186aac1e380a293
Hadi <hadi-tariq@live.co.uk>
2025-05-07 08:03:50 +0000

    specs: transformSvgIdsAndReferences

--- app/javascript/src/utilities/SvgUtils.js
+++ app/javascript/src/utilities/SvgUtils.js
@@ -1,17 +1,14 @@
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 
-// modify svg id, replace at instances - append a uuid with already existing glyph id eg: Glyph-0-1_####-####-####-####
 const transformSvgIdsAndReferences = async (svgText) => {
-  let svg = svgText;
-  const idMap = {};
   const parser = new DOMParser();
   const xmlDoc = parser.parseFromString(svgText, 'image/svg+xml');
-
   const defs = xmlDoc.querySelector('defs');
   if (!defs) return svgText;
+
+  const idMap = {};
   const elementsWithId = defs.querySelectorAll('[id]');
 
-  // Step 1: Collect all IDs and assign unique suffixes
   elementsWithId.forEach((el) => {
     const originalId = el.getAttribute('id');
     const uniqueId = `${originalId}_${uuid()}`;
@@ -19,14 +16,15 @@ const transformSvgIdsAndReferences = async (svgText) => {
     el.setAttribute('id', uniqueId);
   });
 
-  // Step 2: Replace all IDs and references
+  let svg = svgText;
   Object.entries(idMap).forEach(([originalId, uniqueId]) => {
     const idRegex = new RegExp(`id="${originalId}"`, 'g');
     const hrefRegex = new RegExp(`xlink:href="#${originalId}"`, 'g');
-    svg = svgText
+    svg = svg
       .replace(idRegex, `id="${uniqueId}"`)
       .replace(hrefRegex, `xlink:href="#${uniqueId}"`);
   });
+
   return svg;
 };
 
