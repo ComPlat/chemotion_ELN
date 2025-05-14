52c13496ff6297cdf7beb9a38e16269a992a247d
Christian Buggle <christian@buggle.net>
2025-05-14 18:46:11 +0200

    Fix cors (RPE#32)
    * Fix cors

--- app/javascript/src/apps/mydb/elements/details/reactions/ReactionDetails.js
+++ app/javascript/src/apps/mydb/elements/details/reactions/ReactionDetails.js
@@ -489,9 +489,9 @@ export default class ReactionDetails extends Component {
                       <i className="fa fa-floppy-o " />
                     </Button>
                   </OverlayTrigger>
-                <ReactionEditorLink reaction={reaction} />
                 </>
               )}
+              <ReactionEditorLink reaction={reaction} />
             {copyBtn}
             <ConfirmClose el={reaction} />
           </ButtonToolbar>
