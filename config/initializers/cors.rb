cfecf98896777f0c1fc17dedb1a5c56185e6c0c7
Christian Buggle <christian@buggle.net>
2025-05-14 18:46:11 +0200

    Fix cors (RPE#32)
    * Fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/api/v1/public/*', headers: :any, methods: %i[get post patch put delete options],
+
+    resource '/api/v1/public/*', headers: :any, methods: %i[get post options]
+
+    resource '/api/v1/reaction_process_editor/*', headers: :any, methods: %i[get post patch put delete options],
                        expose: %w[Authorization Content-Disposition Content-Filename]
   end
 
