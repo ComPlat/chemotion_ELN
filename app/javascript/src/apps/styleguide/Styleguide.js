dd5b58ac8c7def66769c9f22935399a9218b0133
Joachim Tillessen <tillessen@emjot.de>
2025-05-14 11:46:55 +0200

    more specific styles for surface tabs header and content

--- app/javascript/src/apps/styleguide/Styleguide.js
+++ app/javascript/src/apps/styleguide/Styleguide.js
@@ -4,51 +4,64 @@ import {
 } from 'react-bootstrap';
 
 function Styleguide() {
-  const renderTypography = () => (
+  const renderStyleguideCard = (title, content) => (
     <div>
-      <h2>Typography</h2>
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <h5>Heading 5</h5>
-      <h6>Heading 6</h6>
-      <p>
-        This is a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non commodo nisi.
-        Vestibulum pretium tellus id fermentum sodales. Pellentesque habitant morbi tristique senectus et netus et
-        malesuada fames ac turpis egestas. Curabitur venenatis libero ut quam aliquam dictum. Nullam at iaculis
-        tellus. Curabitur et erat mollis, maximus sem et, pulvinar felis.
-      </p>
-      <ul>
-        <li>First item in unsorted list.</li>
-        <li>Second item in unsorted list.</li>
-        <li>Third item in unsorted list.</li>
-        <li>Fourth item in unsorted list.</li>
-      </ul>
-      <ol>
-        <li>First item in sorted list.</li>
-        <li>Second item in sorted list.</li>
-        <li>Third item in sorted list.</li>
-        <li>Fourth item in sorted list.</li>
-      </ol>
-      <p>
-        This paragraph contains a
-        <a href="https://chemotion.net/" targert="_blank">link</a>
-        to another page.
-      </p>
-      <p>
-        You can use the mark tag to
-        <mark>highlight</mark>
-        text.
-      </p>
-      <p><del>This line of text is meant to be treated as deleted text.</del></p>
-      <p><s>This line of text is meant to be treated as no longer accurate.</s></p>
-      <p><ins>This line of text is meant to be treated as an addition to the document.</ins></p>
-      <p><u>This line of text will render as underlined.</u></p>
-      <p><small>This line of text is meant to be treated as fine print.</small></p>
-      <p><strong>This line rendered as bold text.</strong></p>
-      <p><em>This line rendered as italicized text.</em></p>
+      <div className="surface-tab__header">
+        <h2>{title}</h2>
+      </div>
+      <div className="surface-tab__text-content">
+        {content}
+      </div>
     </div>
+  );
+
+  const renderTypography = () => (
+    renderStyleguideCard(
+      'Typography',
+      <>
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+        <p>
+          This is a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non commodo nisi.
+          Vestibulum pretium tellus id fermentum sodales. Pellentesque habitant morbi tristique senectus et netus et
+          malesuada fames ac turpis egestas. Curabitur venenatis libero ut quam aliquam dictum. Nullam at iaculis
+          tellus. Curabitur et erat mollis, maximus sem et, pulvinar felis.
+        </p>
+        <ul>
+          <li>First item in unsorted list.</li>
+          <li>Second item in unsorted list.</li>
+          <li>Third item in unsorted list.</li>
+          <li>Fourth item in unsorted list.</li>
+        </ul>
+        <ol>
+          <li>First item in sorted list.</li>
+          <li>Second item in sorted list.</li>
+          <li>Third item in sorted list.</li>
+          <li>Fourth item in sorted list.</li>
+        </ol>
+        <p>
+          This paragraph contains a&nbsp;
+          <a href="https://chemotion.net/" targert="_blank">link</a>
+          &nbsp;to another page.
+        </p>
+        <p>
+          You can use the mark tag to
+          <mark>highlight</mark>
+          text.
+        </p>
+        <p><del>This line of text is meant to be treated as deleted text.</del></p>
+        <p><s>This line of text is meant to be treated as no longer accurate.</s></p>
+        <p><ins>This line of text is meant to be treated as an addition to the document.</ins></p>
+        <p><u>This line of text will render as underlined.</u></p>
+        <p><small>This line of text is meant to be treated as fine print.</small></p>
+        <p><strong>This line rendered as bold text.</strong></p>
+        <p><em>This line rendered as italicized text.</em></p>
+      </>
+    )
   );
 
   const buttonVariants = [
@@ -63,38 +76,40 @@ function Styleguide() {
   ];
 
   const renderButtons = () => (
-    <div>
-      <h2>Buttons</h2>
-      <p className="styleguide-label">Default state</p>
-      <div className="d-flex mb-3 gap-3">
-        {buttonVariants.map((variant) => (
-          <Button key={variant} variant={variant}>
-            {`${variant} Button`}
-          </Button>
-        ))}
-      </div>
-      <p className="styleguide-label">Active state</p>
-      <div className="d-flex mb-3 gap-3">
-        {buttonVariants.map((variant) => (
-          <Button key={variant} variant={variant} active>
-            {`${variant} Button`}
-          </Button>
-        ))}
-      </div>
-      <p className="styleguide-label">Disabled state</p>
-      <div className="d-flex mb-3 gap-3">
-        {buttonVariants.map((variant) => (
-          <Button key={variant} variant={variant} disabled>
-            {`${variant} Button`}
-          </Button>
-        ))}
-      </div>
-    </div>
+    renderStyleguideCard(
+      'Buttons',
+      <>
+        <p className="styleguide-label">Default state</p>
+        <div className="d-flex mb-3 gap-3">
+          {buttonVariants.map((variant) => (
+            <Button key={variant} variant={variant}>
+              {`${variant} Button`}
+            </Button>
+          ))}
+        </div>
+        <p className="styleguide-label">Active state</p>
+        <div className="d-flex mb-3 gap-3">
+          {buttonVariants.map((variant) => (
+            <Button key={variant} variant={variant} active>
+              {`${variant} Button`}
+            </Button>
+          ))}
+        </div>
+        <p className="styleguide-label">Disabled state</p>
+        <div className="d-flex mb-3 gap-3">
+          {buttonVariants.map((variant) => (
+            <Button key={variant} variant={variant} disabled>
+              {`${variant} Button`}
+            </Button>
+          ))}
+        </div>
+      </>
+    )
   );
 
   const renderForms = () => (
-    <div>
-      <h2>Forms</h2>
+    renderStyleguideCard(
+      'Forms',
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
@@ -115,7 +130,7 @@ function Styleguide() {
           Submit
         </Button>
       </Form>
-    </div>
+    )
   );
 
   return (
