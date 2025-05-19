4072ef827354c673d89f1194d6a5ac21d27074da
PiTrem <pierre.tremouilhac@kit.edu>
2025-05-19 13:09:27 +0200

    feat: warning for reaction analysis. #2513

--- app/javascript/src/apps/mydb/elements/details/reactions/analysesTab/ReactionDetailsContainers.js
+++ app/javascript/src/apps/mydb/elements/details/reactions/analysesTab/ReactionDetailsContainers.js
@@ -356,7 +356,11 @@ export default class ReactionDetailsContainers extends Component {
       if (analyses_container.length === 1 && analyses_container[0].children.length > 0) {
         return (
           <div>
-            <div className="d-flex justify-content-end align-items-center mb-3">
+            <div className="d-flex justify-content-between align-items-center mb-3">
+              <span className="text-muted me-3 small" style={{ maxWidth: '60%' }}>
+                This tab can be used for reaction-related data (e.g., process control, in situ).
+                For sample data (e.g., characterization), use the sample analysis tab.
+              </span>
               <ButtonToolbar className="gap-1">
                 <div className="mt-2">
                   <CommentButton toggleCommentBox={this.toggleCommentBox} size="sm" />
