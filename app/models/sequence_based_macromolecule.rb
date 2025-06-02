6fbf03a72f150bb3a440b83b4725911cc4d4ba15
Johannes Haubold <jh@megorei.com>
2025-06-02 12:29:43 +0200

    Add scopes to find duplicate SBMMs

--- app/models/sequence_based_macromolecule.rb
+++ app/models/sequence_based_macromolecule.rb
@@ -77,8 +77,85 @@ class SequenceBasedMacromolecule < ApplicationRecord
                          }
 
   accepts_nested_attributes_for(
-    :sequence_based_macromolecule_samples,
     :protein_sequence_modification,
     :post_translational_modification,
   )
+
+  def self.duplicate_sbmm_exists?(base_sbmm)
+    duplicate_sbmm(base_sbmm).present?
+  end
+
+  def self.duplicate_sbmm(base_sbmm)
+    case base_sbmm.uniprot_derivation
+    when 'uniprot'
+      find_duplicate_uniprot_sbmm(base_sbmm)
+    when 'uniprot_modified'
+      find_duplicate_modified_sbmm(base_sbmm)
+    when 'uniprot_unknown'
+      find_duplicate_uniprot_sbmm(base_sbmm)
+    else
+      nil
+    end
+  end
+
+  def self.find_duplicate_uniprot_sbmm(base_sbmm)
+    return unless base_sbmm.primary_accession # shouldn't occur but better safe than sorry
+
+    scope = uniprot
+    scope = scope.where.not(id: base_sbmm.id) if base_sbmm.id
+
+    scope.find_by(primary_accession: base_sbmm.primary_accession)
+  end
+
+  def self.find_duplicate_modified_sbmm(base_sbmm)
+    scope = modified
+    scope = scope.where.not(id: base_sbmm.id) if base_sbmm.id
+    scope = scope.joins(:protein_sequence_modification, :post_translational_modification)
+    scope.find_by(
+      sbmm_type: base_sbmm.sbmm_type,
+      sequence: base_sbmm.sequence,
+      molecular_weight: base_sbmm.molecular_weight,
+      heterologous_expression: base_sbmm.heterologous_expression,
+      protein_sequence_modification: {
+        modification_n_terminal: base_sbmm.protein_sequence_modification.modification_n_terminal,
+        modification_c_terminal: base_sbmm.protein_sequence_modification.modification_c_terminal,
+        modification_insertion: base_sbmm.protein_sequence_modification.modification_insertion,
+        modification_deletion: base_sbmm.protein_sequence_modification.modification_deletion,
+        modification_mutation: base_sbmm.protein_sequence_modification.modification_mutation,
+        modification_other: base_sbmm.protein_sequence_modification.modification_other
+      },
+      post_translational_modification: {
+        phosphorylation_enabled: base_sbmm.post_translational_modification.phosphorylation_enabled,
+        phosphorylation_ser_enabled: base_sbmm.post_translational_modification.phosphorylation_ser_enabled,
+        phosphorylation_thr_enabled: base_sbmm.post_translational_modification.phosphorylation_thr_enabled,
+        phosphorylation_tyr_enabled: base_sbmm.post_translational_modification.phosphorylation_tyr_enabled,
+        glycosylation_enabled: base_sbmm.post_translational_modification.glycosylation_enabled,
+        glycosylation_n_linked_asn_enabled: base_sbmm.post_translational_modification.glycosylation_n_linked_asn_enabled,
+        glycosylation_o_linked_lys_enabled: base_sbmm.post_translational_modification.glycosylation_o_linked_lys_enabled,
+        glycosylation_o_linked_ser_enabled: base_sbmm.post_translational_modification.glycosylation_o_linked_ser_enabled,
+        glycosylation_o_linked_thr_enabled: base_sbmm.post_translational_modification.glycosylation_o_linked_thr_enabled,
+        acetylation_enabled: base_sbmm.post_translational_modification.acetylation_enabled,
+        acetylation_lysin_number: base_sbmm.post_translational_modification.acetylation_lysin_number,
+        hydroxylation_enabled: base_sbmm.post_translational_modification.hydroxylation_enabled,
+        hydroxylation_lys_enabled: base_sbmm.post_translational_modification.hydroxylation_lys_enabled,
+        hydroxylation_pro_enabled: base_sbmm.post_translational_modification.hydroxylation_pro_enabled,
+        methylation_enabled: base_sbmm.post_translational_modification.methylation_enabled,
+        methylation_arg_enabled: base_sbmm.post_translational_modification.methylation_arg_enabled,
+        methylation_glu_enabled: base_sbmm.post_translational_modification.methylation_glu_enabled,
+        methylation_lys_enabled: base_sbmm.post_translational_modification.methylation_lys_enabled,
+        other_modifications_enabled: base_sbmm.post_translational_modification.other_modifications_enabled
+      }
+    )
+  end
+
+  def self.find_duplicate_unknown_sbmm(base_sbmm)
+    scope = unknown
+    scope = scope.where.not(id: base_sbmm.id) if base_sbmm.id
+    scope.find_by(
+      sbmm_type: base_sbmm.sbmm_type,
+      sequence: base_sbmm.sequence,
+      molecular_weight: base_sbmm.molecular_weight,
+      heterologous_expression: base_sbmm.heterologous_expression
+    )
+  end
 end
