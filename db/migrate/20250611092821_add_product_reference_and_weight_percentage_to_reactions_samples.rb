423387aa75e9dd56ee05f7bc3832f96271584935
adambasha0 <adambasha1992@gmail.com>
2025-06-11 15:55:00 +0000

    feat: Add support for weight_percentage and product_reference
    - Updated API endpoints to handle  and .
    - Modified entities to expose the new fields.
    - Added database migrations for  and  in reactions and reaction samples

---
+++ db/migrate/20250611092821_add_product_reference_and_weight_percentage_to_reactions_samples.rb
@@ -0,0 +1,6 @@
+class AddProductReferenceAndWeightPercentageToReactionsSamples < ActiveRecord::Migration[6.1]
+  def change
+    add_column :reactions_samples, :product_reference, :boolean, null: true, default: false
+    add_column :reactions_samples, :weight_percentage, :float, null: true, default: nil
+  end
+end
