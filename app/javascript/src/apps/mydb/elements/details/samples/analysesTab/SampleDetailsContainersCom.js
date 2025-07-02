b754f2d90fbf0d543eb6794e6e18912fdab59821
Ali Zaib <alizaib1217@gmail.com>
2025-07-02 14:28:10 +0200

    feat: Added Fab prototype for reaction details.

--- app/javascript/src/apps/mydb/elements/details/samples/analysesTab/SampleDetailsContainersCom.js
+++ app/javascript/src/apps/mydb/elements/details/samples/analysesTab/SampleDetailsContainersCom.js
@@ -49,13 +49,13 @@ function ReactionsDisplay({
 
   return (
     <div>
-      <div className="d-flex justify-content-between align-items-center mb-3">
+      {/* <div className="d-flex justify-content-between align-items-center mb-3">
         {AnalysisModeToggle(mode, handleToggleMode, isDisabled)}
         <ButtonToolbar className="gap-2">
           <CommentButton toggleCommentBox={toggleCommentBox} size="xsm" />
           {addButton()}
         </ButtonToolbar>
-      </div>
+      </div> */}
       <CommentBox
         isVisible={commentBoxVisible}
         value={sample.container.description}
