02feb31d45afaf0ebda9cdba32965b945e31e882
Tasnim Mehzabin <tasnim.mehzabin@live.com>
2025-07-11 11:03:39 +0000

    feat: update density calculation logic in Sample model and improve tooltip display in Material component

--- app/javascript/src/apps/mydb/elements/details/reactions/schemeTab/ReactionDetailsScheme.js
+++ app/javascript/src/apps/mydb/elements/details/reactions/schemeTab/ReactionDetailsScheme.js
@@ -98,6 +98,8 @@ export default class ReactionDetailsScheme extends React.Component {
       } else {
         this.handleRegularSample(splitSample, tagGroup, extLabel, reaction, tagMaterial);
       }
+
+      this.addSampleToReaction(splitSample, reaction, tagMaterial, tagGroup);
     } catch (error) {
       console.error('Error in dropSample:', error);
       NotificationActions.add({
@@ -160,7 +162,6 @@ export default class ReactionDetailsScheme extends React.Component {
     splitSample.components = sampleComponents;
 
     this.setTargetAmountFromComponents(splitSample, sampleComponents);
-    this.addSampleToReaction(splitSample, reaction, tagMaterial, tagGroup);
   }
 
   handleMixtureWithApiComponents(splitSample, srcSample, reaction, tagMaterial, tagGroup) {
@@ -170,12 +171,9 @@ export default class ReactionDetailsScheme extends React.Component {
         await splitSample.initialComponents(sampleComponents);
 
         this.setTargetAmountFromComponents(splitSample, sampleComponents);
-        this.addSampleToReaction(splitSample, reaction, tagMaterial, tagGroup);
       })
       .catch((error) => {
         console.error('Failed to fetch components:', error);
-        // Still add the sample even if components fail to load
-        this.addSampleToReaction(splitSample, reaction, tagMaterial, tagGroup);
       });
   }
 
@@ -201,7 +199,6 @@ export default class ReactionDetailsScheme extends React.Component {
 
   handleRegularSample(splitSample, tagGroup, extLabel, reaction, tagMaterial) {
     this.insertSolventExtLabel(splitSample, tagGroup, extLabel);
-    this.addSampleToReaction(splitSample, reaction, tagMaterial, tagGroup);
   }
 
   addSampleToReaction(splitSample, reaction, tagMaterial, tagGroup) {
