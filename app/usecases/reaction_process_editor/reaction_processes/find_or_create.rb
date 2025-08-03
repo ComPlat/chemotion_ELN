02feae940ee2dddc4b5ac61a6e7c3413b8f05a8d
Christian Buggle <christian@buggle.net>
2025-08-03 19:39:41 +0200

    Introduce automation ordinal (RPE#34)
    Introduce `automation ordinal` to provide a unique (in the scope of a reaction) integer id for each Activity. This is required for automation devices in the lab that cannot handle long uuids.

--- app/usecases/reaction_process_editor/reaction_processes/find_or_create.rb
+++ app/usecases/reaction_process_editor/reaction_processes/find_or_create.rb
@@ -5,7 +5,7 @@ module Usecases
     module ReactionProcesses
       class FindOrCreate
         def self.execute!(reaction:)
-          reaction_process = reaction.reaction_process || reaction.create_reaction_process
+          reaction_process = reaction.reaction_process || reaction.create_reaction_process(automation_ordinal: 0)
 
           Usecases::ReactionProcessEditor::Provenances::FindOrCreate.execute!(reaction_process: reaction_process)
           reaction_process
