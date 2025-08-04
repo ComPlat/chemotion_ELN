e16a7750e28d1dc9233cf8b4668e737d5fbc094d
Christian Buggle <christian@buggle.net>
2025-08-04 11:32:11 +0200

    Fix rebase conflicts for RPE
    * Downgrade postgresql container to v14 (was v16). Temporary fix before DB is upgraded.
    * Add gem ‘execjs’.

--- app/models/reaction_process_editor/api_user.rb
+++ app/models/reaction_process_editor/api_user.rb
@@ -5,39 +5,41 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :inet
-#  last_sign_in_ip        :inet
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  name                   :string
-#  first_name             :string           not null
-#  last_name              :string           not null
-#  deleted_at             :datetime
-#  counters               :hstore           not null
-#  name_abbreviation      :string(12)
-#  type                   :string           default("Person")
-#  reaction_name_prefix   :string(3)        default("R")
+#  account_active         :boolean
+#  allocated_space        :bigint           default(0)
+#  confirmation_sent_at   :datetime
 #  confirmation_token     :string
 #  confirmed_at           :datetime
-#  confirmation_sent_at   :datetime
-#  unconfirmed_email      :string
-#  layout                 :hstore           not null
-#  selected_device_id     :integer
+#  counters               :hstore           not null
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
+#  deleted_at             :datetime
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
 #  failed_attempts        :integer          default(0), not null
-#  unlock_token           :string
-#  locked_at              :datetime
-#  account_active         :boolean
-#  matrix                 :integer          default(0)
-#  providers              :jsonb
+#  first_name             :string           not null
 #  jti                    :string
+#  last_name              :string           not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
+#  layout                 :hstore           not null
+#  locked_at              :datetime
+#  matrix                 :integer          default(0)
+#  name                   :string
+#  name_abbreviation      :string(12)
+#  providers              :jsonb
+#  reaction_name_prefix   :string(3)        default("R")
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  type                   :string           default("Person")
+#  unconfirmed_email      :string
+#  unlock_token           :string
+#  used_space             :bigint           default(0)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  selected_device_id     :integer
 #
 # Indexes
 #
