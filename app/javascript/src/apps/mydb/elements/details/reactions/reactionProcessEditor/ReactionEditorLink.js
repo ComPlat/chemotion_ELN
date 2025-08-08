c622f94d1b295aeb3a9d2b751e62418066288877
Christian Buggle <christian@buggle.net>
2025-08-08 14:05:25 +0200

    Fix ReactionEditorLink (RPE#36)
    * Move component ReactionEditorLink to standard javascript directory `mydb/elements` (was: `/packs’).

---
+++ app/javascript/src/apps/mydb/elements/details/reactions/reactionProcessEditor/ReactionEditorLink.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Tooltip, OverlayTrigger, Nav } from 'react-bootstrap';
+
+const ReactionEditorLink = ({ reaction }) => {
+	return (
+		<>
+			<OverlayTrigger
+				placement="bottom"
+				overlay={<Tooltip id="editReaction">Edit Reaction</Tooltip>}
+			>
+				<Nav.Link href={reaction.editor_link_target} target="_blank" className="btn btn-xs btn-success button-right">
+					<i className="fa fa-edit" />
+				</Nav.Link>
+			</OverlayTrigger>
+		</>
+	)
+}
+
+export default ReactionEditorLink
