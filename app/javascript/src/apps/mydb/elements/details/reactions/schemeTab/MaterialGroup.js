9a1b701f2e80f79f46bed50db2395eed015445d8
Adam Basha <55552142+adambasha0@users.noreply.github.com>
2025-08-21 16:29:54 +0200

    style: improve yield/conversion rate toggle button implementation (#2620)
    - Replace custom color properties with Bootstrap variant prop in ToggleButton
    - Update MaterialGroup to use ToggleButton with proper styling classes
    - Add informative tooltip to yield field explaining calculation basis

--- app/javascript/src/apps/mydb/elements/details/reactions/schemeTab/MaterialGroup.js
+++ app/javascript/src/apps/mydb/elements/details/reactions/schemeTab/MaterialGroup.js
@@ -161,28 +161,35 @@ function GeneralMaterialGroup({
   }
 
   const yieldConversionRateFields = () => {
-    const conversionText = 'Click to switch to conversion field.'
-    + ' The conversion will not be displayed as part of the reaction scheme';
-    const yieldText = 'Click to switch to yield field.'
-    + ' The yield will be displayed as part of the reaction scheme';
+    const conversionText = (
+      <>
+        Click to switch to conversion field.
+        <br />
+        The conversion will be displayed as part of the reaction scheme.
+      </>
+    );
+    const yieldText = (
+      <>
+        Click to switch to yield field.
+        <br />
+        The yield will be displayed as part of the reaction scheme.
+      </>
+    );
     let conversionOrYield = displayYieldField;
     if (displayYieldField || displayYieldField === null) {
       conversionOrYield = true;
     }
     return (
-      <div>
-        <ToggleButton
-          isToggledInitial={conversionOrYield}
-          onToggle={switchYield}
-          onLabel="Yield"
-          offLabel="Conv."
-          onColor="transparent"
-          offColor="transparent"
-          tooltipOn={conversionText}
-          tooltipOff={yieldText}
-          additionalClasses="fw-bold text-dark"
-        />
-      </div>
+      <ToggleButton
+        isToggledInitial={conversionOrYield}
+        onToggle={switchYield}
+        onLabel="Yield"
+        offLabel="Conv."
+        variant="primary"
+        tooltipOn={conversionText}
+        tooltipOff={yieldText}
+        buttonClass="px-2 py-1"
+      />
     );
   };
 
