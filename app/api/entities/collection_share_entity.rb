9a14266f576adcd4b70f7b3d1a99b698444a0b24
Johannes Haubold <jh@megorei.com>
2025-08-25 10:21:22 +0200

    Add CollectionShare model

---
+++ app/api/entities/collection_share_entity.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Entities
+  class CollectionShareEntity < ApplicationEntity
+    expose! :id,
+    expose! :shared_with
+    expose! :shared_with_id
+    expose! :permission_level # TODO: wie kommuniziert man sauber die Möglichkeiten die das FE freigeben soll?
+    expose! :permission_level
+    expose! :celllinesample_detail_level
+    expose! :devicedescription_detail_level
+    expose! :element_detail_level
+    expose! :reaction_detail_level
+    expose! :researchplan_detail_level
+    expose! :sample_detail_level
+    expose! :screen_detail_level
+    expose! :sequencebasedmacromoleculesample_detail_level
+    expose! :wellplate_detail_level
+
+    def shared_with
+      "#{shared_with.name} (#{shared_with.name_abbreviation})"
+    end
+  end
+end
