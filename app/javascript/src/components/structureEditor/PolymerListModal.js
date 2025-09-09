e6a4a74817dfee01eeddd187950ce056c03380b3
Hadi Tariq <hadi-tariq@live.co.uk>
2025-09-09 20:30:04 +0200

    feat: Ketcher solid phase support basic (#2655)
    For the feature to work properly, disabled-rescale should be implemented 
    ( see epam/ketcher/pull/6982)
    
    ## 1. Core Ketcher Editor Enhancements
    
    - Major updates to `KetcherEditor.js`
    - Integrated `onChange` event listeners for real-time editor updates. which helps to track images, atoms and textnodes changes in the canvas
    - Types of events
    - Polymer list modal
    - Added a new button in the top toolbar with a complete list of selectable shapes(opens up in a modal).  
    
    ## 2. Surface Chemistry Support
    
    Details of how feature is divided into multiple files.  
    
    ### Atoms & Molecule Manipulation  
    
    File: `AtomsAndMolManipulation.js`  
    Features:
    - Molecule structure editing  
    - Atom property modification  
    - Bond creation and handling  
    - Coordinate system transformations  
     
    ### DOM Handling  
    File: `DomHandeling.js`  
    
    Features:
    - SVG element creation, manipulation, and removal  
    - Event listener management  
    - DOM tree traversal and modifications  
    
    ### Ketcher Initialization & Parsing  
    File: `InitializeAndParseKetcher.js`  
    
    Features:
    - Editor initialization  
    - Molfile parsing and validation  
    - Conversion between structure data formats  
    - Editor state management 
    
    ### Surface Chemistry Utilities  
    File: `Ketcher2SurfaceChemistryUtils.js`  
    
    Features:
    - Surface chemistry–specific operations  
    - Reaction surface handling  
    - Chemical structure validation  
    - Surface property management    
    
    ### Canvas Operations  
    File: `canvasOperations.js`  
    
    Features:
    - Rendering and updating the canvas  
    - Drawing operations  
    - Layer management  
    - Visual element positioning  
    
    ### State Management  
    
    File: `stateManager.js`  
    
    Features: 
    - Editor state tracking  
    - Undo/redo functionality  
    - State persistence  
    - Change history management  
    
      
    
    ### Event Handling  
    
    File: `eventHandler.js`  
    Features:  
    - User interaction handling (mouse & keyboard)  
    - Custom event dispatching  
    - Event propagation control  
      
    
    #### Extra Information on molfile
    Structure for how to shapes are stored in the molfile as extra data:
    All shapes are connected together with a " " and each item is based on seveleral components:
    
    `0s/0.24-2.29`
    0s: index of atom
    0.24-2.29: size of the shape
    
    ```
    
    M  END
    
    > <PolymersList>
    
    0s/0.24-2.29 5/4/0.46-0.46
    
    $$$$
    ```  
    
    ### Additional Modules
    - `constants.js`: Shared constants and configurations for all sort of identifiers including dom elements
    - `iconBaseProvider.js`: Icon and asset management, which helps to convert svg to base64
    - `public/json/surfaceChemistryShapes.json`: Predefined shape definitions and configurations with templates id  
    
    ## 3. Polymer Support
    
    - Added new polymer templates and shapes  
    - Implemented polymer list modal  
    - Extensive new SVG assets for polymer shapes  
    - Added **semantic** and **basic** polymer shape support  
    
    ## 4. Sanitizer Functionality
    
    - Updated sanitizer for improved data validation and security   
    
    ## 5. Asset Additions
    
    - Added numerous **SVG assets** for polymer-related shapes:  
    - **Basic shapes:** support, active phase, etc.  
    - **Semantic shapes:** dopants, mono-/multi-support, etc.

---
+++ app/javascript/src/components/structureEditor/PolymerListModal.js
@@ -0,0 +1,295 @@
+/* eslint-disable max-len */
+import { PropTypes } from 'mobx-react';
+import React, { useState, useEffect } from 'react';
+import {
+  Accordion, Button, Card, Form, Modal, Spinner
+} from 'react-bootstrap';
+
+const PolymerShapes = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <!-- Square with border -->
+  <rect x="4" y="4" width="8" height="8" stroke="black" stroke-width="2" fill="none" />
+  
+  <!-- Circle with border -->
+  <circle cx="18" cy="18" r="4" stroke="black" stroke-width="2" fill="none" />
+</svg>
+`;
+
+const SpecialCharacterPickerIcon = `
+<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12 2C7.03 2 3 6.03 3 11H5C5 7.134 8.134 4 12 4C15.866 4 19 7.134 19 11C19 14.866 15.866 18 12 18C10.673 18 9.402 17.597 8.343 16.828L7 18.172C8.645 19.421 10.735 20 12.999 20C17.97 20 22 15.97 22 11C22 6.03 17.97 2 12 2Z" fill="currentColor"/>
+</svg>
+`;
+
+function InlineSVG({ src }) {
+  const [svgContent, setSvgContent] = useState(null);
+
+  useEffect(() => {
+    fetch(src)
+      .then((res) => res.text())
+      .then((text) => setSvgContent(text))
+      .catch((err) => console.error('SVG load error:', err));
+  }, [src]);
+
+  return (
+    <div
+      className="inline-svg"
+      dangerouslySetInnerHTML={{ __html: svgContent }}
+    />
+  );
+}
+
+export default InlineSVG;
+
+function PolymerListModal({
+  loading, onShapeSelection, title, onCloseClick
+}) {
+  const [shapesList, setShapeList] = useState([]); // Initialize the state as an empty array
+  const [category, setCategory] = useState(() => localStorage.getItem('polymerCategory') || 'basic');
+  const [loadingData, setLoadingData] = useState(false); // Initialize the state as an empty array
+
+  const loadTemplates = () => {
+    fetch('/json/surfaceChemistryShapes.json').then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    }).then((data) => {
+      setShapeList(data);
+    }).catch((error) => {
+      console.error('Error fetching the JSON data:', error);
+    })
+      .finally(() => {
+        setTimeout(() => {
+          setLoadingData(false);
+        }, 200);
+      });
+  };
+
+  useEffect(() => {
+    setLoadingData(true);
+    loadTemplates();
+  }, [category]);
+
+  const onCategoryChange = (categoryAlias) => {
+    setCategory(categoryAlias);
+    setLoadingData(true);
+    localStorage.setItem('polymerCategory', categoryAlias); // ✅ correct value
+  };
+
+  return (
+    <Modal
+      centered
+      className="w-500 h-500 top-50 start-50 translate-middle"
+      style={{ zIndex: '10000' }}
+      contentClassName="border-1"
+      animation
+      show={loading}
+      onHide={onCloseClick}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form.Group className="w-100 d-flex justify-content-end align-items-center mb-3">
+          <div
+            className="btn-group w-100"
+            role="group"
+            aria-label="Category switch"
+            style={{ display: 'flex', gap: 0 }}
+          >
+            {shapesList && Object.keys(shapesList).map((categoryItem, index, arr) => {
+              const isActive = category === categoryItem;
+              const isFirst = index === 0;
+              const isLast = index === arr.length - 1;
+
+              return (
+                <Button
+                  key={categoryItem}
+                  onClick={() => onCategoryChange(categoryItem)}
+                  style={{
+                    flex: 1,
+                    backgroundColor: isActive ? '#167782' : 'transparent',
+                    color: isActive ? '#fff' : '#6c757d',
+                    border: isActive ? 'none' : '1px solid #ced4da',
+                    textTransform: 'capitalize',
+                    borderRadius: isFirst
+                      ? '8px 0 0 8px'
+                      : isLast
+                        ? '0 8px 8px 0'
+                        : '0',
+                    margin: 0
+                  }}
+                >
+                  {categoryItem}
+                </Button>
+              );
+            })}
+          </div>
+        </Form.Group>
+
+        <Accordion>
+          {loadingData ? (
+            <div className="d-flex justify-content-center align-items-center" style={{ height: '180px' }}>
+              <Spinner animation="border" role="status" variant="#167782" style={{ color: '#167782' }}>
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </div>
+          ) : shapesList && shapesList[category]?.map((tab) => (
+            <Card key={tab.id}>
+              <Card.Header>
+                <Accordion.Item eventKey={String(tab.id)}>
+                  <Accordion.Header>{tab.label}</Accordion.Header>
+                </Accordion.Item>
+              </Card.Header>
+              <Accordion.Collapse eventKey={String(tab.id)}>
+                <Card.Body>
+                  <Accordion>
+                    {tab.subTabs.map((subTab) => (
+                      <Card key={subTab.id}>
+                        <Card.Header>
+                          <Accordion.Item eventKey={subTab.id}>
+                            <Accordion.Header>{subTab.label}</Accordion.Header>
+                          </Accordion.Item>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey={subTab.id}>
+                          <Card.Body>
+                            {subTab?.shapes?.map((shape) => (
+                              <Button
+                                key={shape.template_id}
+                                variant="normal"
+                                onClick={async () => {
+                                  if (shape.template_id) {
+                                    onShapeSelection(shape.template_id, true);
+                                  }
+                                }}
+                              >
+                                <div className="flex flex-col items-center gap-2">
+                                  <InlineSVG src={`/polymerShapes/${category}/${shape.iconName}.svg`} />
+                                </div>
+                              </Button>
+                            ))}
+                          </Card.Body>
+                        </Accordion.Collapse>
+                      </Card>
+                    ))}
+                  </Accordion>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          ))}
+        </Accordion>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+const PolymerListIconKetcherToolbarButton = (iframeDocument) => {
+  const parentElement = iframeDocument.querySelector('.App-module_top__SBeSV.css-2yv69u');
+  const container = parentElement?.querySelector('.css-6qnjre');
+  if (container) {
+    const newButton = iframeDocument.createElement('button');
+    newButton.classList.add('css-9c2fhu');
+    newButton.title = 'Polymer List';
+
+    newButton.style.backgroundColor = 'transparent';
+    newButton.style.border = '0';
+
+    newButton.innerHTML = PolymerShapes;
+    container.appendChild(newButton);
+  }
+};
+
+const specialCharButton = (iframeDocument) => {
+  const parentElement = iframeDocument.querySelector('.BottomToolbar-module_group__b-pGt');
+  if (parentElement) {
+    const newButton = iframeDocument.createElement('button');
+    newButton.classList.add('ActionButton-module_button__nfoWQ');
+    newButton.classList.add('textNodeChar');
+    newButton.title = 'Text Node Special Char';
+
+    newButton.style.backgroundColor = 'transparent';
+    newButton.style.border = '0';
+
+    // Set the SVG as the innerHTML of the button
+    newButton.innerHTML = SpecialCharacterPickerIcon;
+    parentElement.appendChild(newButton);
+  }
+};
+
+function SpecialCharModal({
+  loading,
+  title,
+  onCloseClick,
+  dashedSelection,
+  restSelection,
+  onDashedSelection,
+  onRestSelections
+}) {
+  const specialCharacters = [
+    '!', '@', '#', '$',
+    '/', '?', '∆', '★'
+  ];
+
+  return (
+    <Modal
+      centered
+      className="w-500 h-500 top-50 start-50 translate-middle"
+      style={{ zIndex: '10000' }}
+      contentClassName="border-1"
+      animation
+      show={loading}
+      onHide={onCloseClick}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="flex-1 bg-gray-100 p-4 border border-gray-300 rounded-lg shadow-md max-w-xl ">
+            <h4>Standard bonds</h4>
+            <div className="flex-row flex-wrap gap-2" style={{ display: 'flex' }}>
+              {
+                specialCharacters.map((item) => (
+                  <Button
+                    key={item}
+                    className={`w-10 h-10 text-lg font-medium border rounded-md hover:bg-gray-200 text-gray-800 shadow-sm flex items-center justify-center ${restSelection === item ? 'bg-green-200' : 'bg-white'}`}
+                    onClick={() => onRestSelections(item)}
+                  >
+                    {item}
+                  </Button>
+                ))
+              }
+            </div>
+          </div>
+          <div className="flex-1 bg-gray-100  p-4 border border-gray-300 rounded-lg shadow-md max-w-xl">
+            <h4>Dashed-bonds</h4>
+            <div className="flex-row flex-wrap gap-2" style={{ display: 'flex' }}>
+              {
+                specialCharacters.map((item) => (
+                  <Button
+                    key={item}
+                    className={`w-10 h-10 text-lg font-medium border rounded-md hover:bg-gray-200 text-gray-800 shadow-sm flex items-center justify-center ${dashedSelection === item ? 'bg-green-200' : 'bg-white'}`}
+                    onClick={() => onDashedSelection(item)}
+                  >
+                    {item}
+                  </Button>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export {
+  PolymerListModal, PolymerListIconKetcherToolbarButton, specialCharButton, SpecialCharModal
+};
+
+PolymerListModal.propTypes = {
+  loading: PropTypes.bool,
+  title: PropTypes.String,
+  onCloseClick: PropTypes.func,
+  onShapeSelection: PropTypes.func
+};
