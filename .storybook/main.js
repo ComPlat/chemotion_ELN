bdeac7979ccbbb427713ec0949f912b988daf8cc
Luciano Maiwald <mail@luciano-maiwald.de>
2025-09-17 12:03:42 +0200

    Add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-docs"
+  ],
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
+  }
+};
+export default config;
