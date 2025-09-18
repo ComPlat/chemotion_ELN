b9bd119ddc4de5451992a69f9d5f7ab480e4960b
Lenz Fiedler <37868410+RandomDefaultUser@users.noreply.github.com>
2025-09-18 15:22:00 +0200

    build(dev): map local user id to ubuntu user in app image and avoid parallel image building (#2668)
    - map the host current uid to the dev container default user (debian host, when not using devcontainer )
    
    ```
    docker_cmd="docker compose -f docker-compose.dev.yml --env-file .dockerenv "
    gid=$(id -g) uid=$(id -u) ${docker_cmd} build setup
    ```
    
    - Avoid concurrent building of app and webpacker and implement better healthcheck and up sequence
      by using docker compose service `setup` to pre-build the image  common to app and webpacker
    as well as creating the common volumes   and setting up the dev env.   Only then is the app started
    - add script to implement the uid/gid mapping 
    - update .devcontainer/docker-compose.vs to skip this mapping since VS has its own handling for mapping the uid
    - extract the db-preparation step in its own sh file
    - add script for a default config setup (copy config/*.yml.example) to be used both by devcontainer  or directly.
    - run ruby server precheck: only install missing gem dependencies
    - option to customize port mapping for the services

---
+++ prepare-db.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# prepare rails database
+# assume default database configuration
+DATABASE_NAME=${DATABASE_NAME:-chemotion_dev}
+DATABASE_USER=${DATABASE_USER:-postgres}
+DATABASE_HOST=${DATABASE_HOST:-postgres}
+DATABASE_PORT=${DATABASE_PORT:-5432}
+
+# check if yq is installed
+# if yq is installed parse config/database.yml file for the actual values
+# if yq is not installed, then keep the set values default values
+if command -v yq &> /dev/null
+then
+  DATABASE_NAME=$(yq -r .development.database config/database.yml)
+  DATABASE_USER=$(yq -r .development.username config/database.yml)
+  DATABASE_HOST=$(yq -r .development.host config/database.yml)
+  DATABASE_PORT=$(yq -r .development.port config/database.yml)
+fi
+echo "DATABASE_NAME: $DATABASE_NAME"
+echo "DATABASE_USER: $DATABASE_USER"
+echo "DATABASE_HOST: $DATABASE_HOST"
+echo "DATABASE_PORT: $DATABASE_PORT"
+
+# check if the database for the given environment configuration exists
+db_exists=$( psql -h $DATABASE_HOST -U $DATABASE_USER -p $DATABASE_PORT -XtAc "SELECT 1 FROM pg_database WHERE datname='$DATABASE_NAME'" )
+echo "Database exists: $db_exists"
+if [ "$db_exists" = '1' ]
+then
+  echo "==================================================="
+  echo "Database already exists, skipping Database creation"
+  echo "==================================================="
+  if [ "$RAKE_DB_MIGRATE" = "always" ]
+  then
+    echo "================================================"
+    echo "Running 'rake db:migrate'"
+    echo "================================================"
+      bundle exec rake db:migrate
+  fi
+else
+  # if RAKE_DB_MIGRATE is set to always or once, run rake db:setup
+  if [ "$RAKE_DB_MIGRATE" = "always" ] || [ "$RAKE_DB_MIGRATE" = "once" ]
+  then
+    echo "================================================"
+    echo "Database does not exist"
+    echo "running 'rake db:create/migrate/seed'"
+    echo "================================================"
+    bundle exec rake db:create
+    bundle exec rake db:migrate
+    bundle exec rake db:seed
+  else
+    echo "================================================"
+    echo "Database does not exist, running 'rake db:setup'"
+    echo "================================================"
+    bundle exec rake db:setup
+  fi
+fi
+
+
