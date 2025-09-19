1ae7e78b7465387a425fe59022613dfb0a836b83
Joachim Tillessen <tillessen@emjot.de>
2025-09-19 12:39:15 +0200

    remove legacy styleguide

--- app/javascript/src/apps/styleguide/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Styleguide from 'src/apps/styleguide/Styleguide';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const el = document.getElementById('Styleguide');
-  if (el) ReactDOM.render(<Styleguide />, el);
-});
