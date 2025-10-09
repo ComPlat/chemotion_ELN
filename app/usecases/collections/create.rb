0342152ac6d40f692f7c8b822620b3d152d46afa
Johannes Haubold <jh@megorei.com>
2025-10-09 15:40:08 +0200

    Fix position calculation when creating a new root collection

--- app/usecases/collections/create.rb
+++ app/usecases/collections/create.rb
@@ -10,35 +10,40 @@ module Usecases
       end
 
       def perform!(parent_id:, label:, inventory_id:)
-        collection = create_collection(
-          parent: parent(parent_id), 
-          label: label, 
-          inventory: inventory(inventory_id)
+        inventory = find_inventory(inventory_id)
+        if parent_id
+          parent = find_parent(parent_id)
+          position = parent.children.count + 1
+        else
+          parent = nil
+          position = current_user.collections.where(ancestry: '/').count + 1
+        end
+
+        current_user.collections.create!(
+          parent: parent,
+          position: position,
+          label: label,
+          inventory: inventory
         )
       end
 
       private
 
-      def parent(parent_id)
+      def find_parent(parent_id)
         return unless parent_id
 
         current_user.collections.find(parent_id) if parent_id.present? # NOTE: own collections only
       end
 
       # TODO: how are inventories access controlled?
-      def inventory(inventory_id)
+      def find_inventory(inventory_id)
         return unless inventory_id
 
         Inventory.find(inventory_id)
       end
 
       def create_collection(parent:, label:, inventory:)
-        current_user.collections.create!(
-          parent: parent,
-          position: parent.children.count + 1,
-          label: label,
-          inventory: inventory
-        )
+
       end
     end
   end
