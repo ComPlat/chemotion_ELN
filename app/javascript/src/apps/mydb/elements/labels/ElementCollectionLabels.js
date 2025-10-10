5f0a11cbf172e2dc488960bfdefd83936dda1943
Johannes Haubold <jh@megorei.com>
2025-10-10 15:38:46 +0200

    Refactor taggable concern and ElementCollectionLabels

--- app/javascript/src/apps/mydb/elements/labels/ElementCollectionLabels.js
+++ app/javascript/src/apps/mydb/elements/labels/ElementCollectionLabels.js
@@ -1,129 +1,88 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable camelcase */
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import {
   OverlayTrigger, Popover, Button
 } from 'react-bootstrap';
 import UserStore from 'src/stores/alt/stores/UserStore';
 import { aviatorNavigationWithCollectionId } from 'src/utilities/routesUtils';
+import { StoreContext } from 'src/stores/mobx/RootStore';
 
-export default class ElementCollectionLabels extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      element: props.element
-    };
+function ElementCollectionLabels({ element, placement }) {
+  const { currentUser } = UserStore.getState();
+  if (!currentUser) return (<span />);
+  if (!element.tag) return (<span />);
+  if (!element.tag.taggable_data) return (<span />);
+  if (!element.tag.taggable_data.collection_labels) return (<span />);
+  if (element.tag.taggable_data.collection_labels.length == 0) return (<span />);
 
-    this.handleOnClick = this.handleOnClick.bind(this);
-  }
+  const collectionsStore = useContext(StoreContext).collections;
 
-  handleOnClick(label, e) {
+  const handleOnClick = (label, e) => {
     e.stopPropagation();
-    aviatorNavigationWithCollectionId(label.id, this.state.element.type, this.state.element.id);
+    aviatorNavigationWithCollectionId(label.id, element.type, element.id);
   }
 
-  labelStyle(label) {
-    return label.is_shared ? 'warning' : 'info';
-  }
-
-  formatLabels(labels, is_synchronized) {
+  const formatLabels = (labels)  =>{
     return labels.map((label, index) => {
-      if (is_synchronized && label.isOwner) {
-        return (
-          <span className="d-inline-block m-1" key={index}>
-            <Button disabled variant="light" size="sm">
-              {label.name}
-            </Button>
-          </span>
-        );
-      }
+      const collectionFromStore = collectionsStore.find(label.id)
+      if (!collectionFromStore) return (<span />);
+
       return (
         <span className="d-inline-block m-1" key={index}>
-          <Button variant="light" size="sm" onClick={(e) => this.handleOnClick(label, e)}>
-            {label.name}
+          <Button variant="light" size="sm" onClick={(e) => handleOnClick(label, e)}>
+            {collectionFromStore.label}
           </Button>
         </span>
       );
     });
   }
 
-  renderCollectionsLabels(collectionName, labels, is_synchronized = false) {
+  const renderLabels = (title, labels) => {
     if (labels.length === 0) return null;
     return (
       <>
-        <Popover.Header>{collectionName}</Popover.Header>
-        <Popover.Body>{this.formatLabels(labels, is_synchronized)}</Popover.Body>
+        <Popover.Header>{title}</Popover.Header>
+        <Popover.Body>{formatLabels(labels)}</Popover.Body>
       </>
     );
   }
 
-  render() {
-    const { element } = this.state;
+  const ownCollections = labels.filter(label => collectionsStore.isOwnCollection(label.id))
+  const sharedCollections = labels.filter(label => collectionsStore.isSharedCollection(label.id))
 
-    if (!element.tag || !element.tag.taggable_data || !element.tag.taggable_data.collection_labels) {
-      return (<span />);
-    }
+  const collectionOverlay = (
+    <Popover className="scrollable-popover" id="element-collections">
+      {renderLabels('My Collections', ownCollections)}
+      {renderLabels('Shared Collections', sharedCollections)}
+    </Popover>
+  );
 
-    let { currentUser } = UserStore.getState();
-    currentUser = currentUser || {};
-
-    const { collection_labels } = element.tag.taggable_data;
-    const shared_labels = [];
-    const labels = [];
-    const sync_labels = [];
-    collection_labels.forEach((label) => {
-      if (label) {
-        if (!label.is_shared && !label.is_synchronized && label.user_id === currentUser.id) {
-          labels.push(label);
-        } else if (label.is_shared && !label.is_synchronized
-          && (label.user_id === currentUser.id || label.shared_by_id === currentUser.id)) {
-          shared_labels.push(label);
-        } else if (label.is_synchronized && (label.user_id
-          === currentUser.id || label.shared_by_id === currentUser.id)) {
-          let isOwner = false;
-          if (label.shared_by_id === currentUser.id) {
-            isOwner = true;
-          }
-          sync_labels.push({ ...label, isOwner });
-        }
-      }
-    });
-
-    const total_shared_collections = shared_labels.length + sync_labels.length;
-
-    if (labels.length === 0 && total_shared_collections === 0) { return (<span />); }
-
-    const collectionOverlay = (
-      <Popover className="scrollable-popover" id="element-collections">
-        {this.renderCollectionsLabels('My Collections', labels)}
-        {this.renderCollectionsLabels('Shared Collections', shared_labels)}
-      </Popover>
-    );
-
-    return (
-      <OverlayTrigger
-        trigger="click"
-        rootClose
-        placement={this.props.placement}
-        overlay={collectionOverlay}
+  return (
+    <OverlayTrigger
+      trigger="click"
+      rootClose
+      placement={placement}
+      overlay={collectionOverlay}
+    >
+      <Button
+        key={element.id}
+        size="xxsm"
+        variant="light"
+        onClick={(e) => e.stopPropagation()}
       >
-        <Button
-          key={element.id}
-          size="xxsm"
-          variant="light"
-          onClick={(e) => e.stopPropagation()}
-        >
-          <i className="fa fa-list" />
-          {` ${labels.length} `}
-          {' - '}
-          {`${total_shared_collections} `}
-          <i className="fa fa-share-alt" />
-        </Button>
-      </OverlayTrigger>
-    );
-  }
+        <i className="fa fa-list" />
+        {` ${ownCollections.length} `}
+        {' - '}
+        {`${sharedCollections.length} `}
+        <i className="fa fa-share-alt" />
+      </Button>
+    </OverlayTrigger>
+  );
 }
+
+export default observer(ElementCollectionLabels)
 
 ElementCollectionLabels.propTypes = {
   element: PropTypes.shape({
@@ -133,11 +92,6 @@ ElementCollectionLabels.propTypes = {
       taggable_data: PropTypes.shape({
         collection_labels: PropTypes.arrayOf(PropTypes.shape({
           id: PropTypes.number,
-          name: PropTypes.string,
-          is_shared: PropTypes.bool,
-          is_synchronized: PropTypes.bool,
-          user_id: PropTypes.number,
-          shared_by_id: PropTypes.number,
         })),
       }),
     }),
