c3aeddc1e7c85a2932458cf757521e6cfe9b7fc8
Adrian Herrmann <headri@web.de>
2025-10-15 13:25:51 +0000

    refactor models and migration

--- db/migrate/20251014150000_add_short_label_to_research_plans.rb
+++ db/migrate/20251014150000_add_short_label_to_research_plans.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
+# rubocop:disable Rails/SkipsModelValidations
+
 class AddShortLabelToResearchPlans < ActiveRecord::Migration[6.1]
   def up
     add_column :research_plans, :short_label, :string
 
-    ResearchPlan.order(:created_at).group_by(&:created_by).each do |user_id, plans|
-      user = User.find_by(id: user_id)
-      next unless user
+    User.find_each do |u|
+      plans = ResearchPlan.where(created_by: u.id).order(:created_at)
 
       plans.each_with_index do |rp, index|
         counter = index + 1
-        rp.update!(short_label: "#{user.name_abbreviation}-RP#{counter}")
+        rp.update_column('short_label', "#{u.name_abbreviation}-RP#{counter}")
       end
 
-      if user.respond_to?(:counters)
-        user.counters ||= {}
-        user.counters['research_plans'] = plans.size.to_s
-        user.save!
+      if u.respond_to?(:counters)
+        u.counters ||= {}
+        u.counters['research_plans'] = plans.size.to_s
+        u.save!
       end
     end
   end
@@ -30,3 +33,5 @@ class AddShortLabelToResearchPlans < ActiveRecord::Migration[6.1]
     end
   end
 end
+
+# rubocop:enable Rails/SkipsModelValidations
