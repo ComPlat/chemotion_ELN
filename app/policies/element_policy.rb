5013448d08b87f079fa6501daf230aa5288ce7f9
Johannes Haubold <jh@megorei.com>
2025-10-17 16:49:17 +0200

    Rework ElementsPolicy and remove unused Pundit gem

--- app/policies/element_policy.rb
+++ app/policies/element_policy.rb
@@ -14,53 +14,45 @@ class ElementPolicy
   # 1. there exists an unshared collection which he owns and that contains the element or
   # 2. the user has been shared a collection containing the element with an according permission level
   def read?
+    return false unless user_and_record_present?
     record_is_in_own_collection? || record_shared_with_minimum_permission_level?(0)
   end
 
   def update?
+    return false unless user_and_record_present?
     record_is_in_own_collection? || record_shared_with_minimum_permission_level?(1)
   end
 
   def copy?
+    return false unless user_and_record_present?
     record_is_in_own_collection? || record_shared_with_minimum_permission_level?(1)
   end
 
   def share?
-    return true unless record
-
+    return false unless user_and_record_present?
     record_is_in_own_collection? || record_shared_with_minimum_permission_level?(2)
   end
 
   def destroy?
+    return false unless user_and_record_present?
     record_is_in_own_collection? || record_shared_with_minimum_permission_level?(3)
   end
 
   def import?
+    return false unless user_and_record_present?
     record_is_in_own_collection? || record_shared_with_minimum_permission_level?(4)
   end
 
   def pass_ownership?
+    return false unless user_and_record_present?
     record_is_in_own_collection? || record_shared_with_minimum_permission_level?(6)
   end
 
-  def scope
-    Pundit.policy_scope!(user, record.class)
-  end
-
-  class Scope
-    attr_reader :user, :scope
-
-    def initialize(user, scope)
-      @user = user
-      @scope = scope
-    end
-
-    def resolve
-      scope
-    end
-  end
-
   private
+
+  def user_and_record_present?
+    user.present? && record.present?
+  end
 
   def record_is_in_own_collection?
     record.collections.where(user: user).any?
