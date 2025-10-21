cd41cbda335fcda544f042a4c7d3cc0ea7f9c81c
PiTrem <pierre.tremouilhac@kit.edu>
2025-10-21 16:32:26 +0200

    build(dev): track rubocop and pronto gems for ci lint
    - use bundler cache to workflow `linting`

---
+++ Gemfile.cop.rb
@@ -0,0 +1,9 @@
+# Gemfile.lint
+source 'https://rubygems.org'
+
+gem 'rubocop', require: false
+gem 'rubocop-performance', require: false
+gem 'rubocop-rails', require: false
+gem 'rubocop-rspec', require: false
+gem 'pronto', require: false
+gem 'pronto-rubocop', require: false
