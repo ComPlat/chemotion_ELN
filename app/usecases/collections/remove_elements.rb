d24ebd6268deb36b58a611e18976b9fce7132e14
Johannes Haubold <jh@megorei.com>
2025-10-22 14:44:27 +0200

    Add DELETE /api/v1/collection_elements endpoint

--- app/usecases/collections/remove_elements.rb
+++ app/usecases/collections/remove_elements.rb
@@ -1,6 +1,6 @@
 module Usecases
   module Collections
-    class AddElements
+    class RemoveElements
       attr_reader :current_user, :collection
 
       def initialize(current_user)
@@ -10,41 +10,35 @@ module Usecases
 
       def perform!(collection_id:, ui_state:)
         find_collection(collection_id)
-        check_access_to_elements(ui_state)
-        add_elements_to_collection(ui_state)
+        prevent_removal_from_all_collection!
+        remove_elements_from_collection(ui_state)
       end
 
       def find_collection(collection_id)
         @collection = Collection.own_collections_for(current_user).find_by(id: collection_id)
         @collection ||= Collection.shared_with_minimum_permission_level(
           current_user,
-          CollectionShare.permission_level(:import_elements)
+          CollectionShare.permission_level(:delete_elements)
         ).find_by(id: collection_id)
 
-        raise Errors::InsufficientPermissionError.new('You do not have the right to add elements to this collection') unless @collection
+        raise Errors::InsufficientPermissionError.new('You do not have the right to remove elements from this collection') unless @collection
       end
 
-      def check_access_to_elements(ui_state)
+      def prevent_removal_from_all_collection!
+        return unless collection.label == 'All' && collection.is_locked
+
+        raise Errors::InsufficientPermissionError.new('You can not delete from an ALL-Collection')
+      end
+
+      def remove_elements_from_collection(ui_state)
         ui_state.each do |class_string, ui_selections|
-          element_class = API::ELEMENT_CLASS[class_string] || Labimotion::ElementKlass.find(name: class_string)&&.elements
+          element_class = API::ELEMENT_CLASS[class_string] || Labimotion::ElementKlass.find(name: class_string)&.elements
           next unless element_class
 
-          scope = element_class.by_ui_state(ui_selections)
-
-          policy = ElementsPolicy.new(current_user, scope)
-          return if policy.share_all?
-
-          raise Errors::InsufficientPermissionError.new("You do not have the right to add #{class_string} to this collection")
-        end
-      end
-
-      def add_elements_to_collection(ui_state)
-        ui_state.each do |class_string, ui_selections|
-          element_class = API::ELEMENT_CLASS[class_string] || Labimotion::ElementKlass.find(name: class_string).elements
           element_ids = element_class.by_ui_state(ui_selections).ids
           join_table = API::ELEMENT_CLASS[class_string].collections_element_class || Labimotion::CollectionsElement
 
-          join_table.create_in_collection(element_ids, collection.id)
+          join_table.remove_in_collection(element_ids, collection.id)
         end
       end
 
