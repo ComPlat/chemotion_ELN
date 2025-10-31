fd1825c355bbaaf04bffa7f724fa4d47d1411b89
Ali Zaib <alizaib1217@gmail.com>
2025-10-31 09:55:27 +0000

    implemented CORS for collections/import
    fixed cors error
    
    fixed credential cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,5 +4,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '/api/v1/public/*', headers: :any, methods: %i[get post options]
+    resource '/api/v1/collections/imports/*', headers: :any, methods: %i[post options], credentials: false
   end
 end
