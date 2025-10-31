2dff0f82d98c736f7254db4011f84e8b6125105f
Ali Zaib <alizaib1217@gmail.com>
2025-10-31 11:28:45 +0100

    fixed cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,10 +5,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '/api/v1/public/*', headers: :any, methods: %i[get post options]
   end
-  if ENV['CORS_ALLOWED_ORIGINS'].present?
-    allow do
-      origins ENV['CORS_ALLOWED_ORIGINS'].split(',').map(&:strip)
-      resource '/api/v1/collections/imports/*', headers: :any, methods: %i[post options], credentials: true
-    end
+  allow do
+    origins '*'
+    resource '/api/v1/collections/imports*', headers: :any, methods: %i[post options], credentials: false
   end
 end
