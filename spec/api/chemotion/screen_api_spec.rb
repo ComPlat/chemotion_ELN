7097d74cb2bb9ee5a9a7b1eede34ae4a9b6c22b2
Johannes Haubold <jh@megorei.com>
2025-11-03 14:39:26 +0100

    Update ScreenAPI to work with the new collection system

--- spec/api/chemotion/screen_api_spec.rb
+++ spec/api/chemotion/screen_api_spec.rb
@@ -11,30 +11,30 @@ describe Chemotion::ScreenAPI do
     }
   end
   let(:other_user) { create(:person) }
-  let(:collection) do
-    create(
-      :collection,
-      user_id: user.id,
-      sample_detail_level: 10,
-      reaction_detail_level: 10,
-      wellplate_detail_level: 10,
-      screen_detail_level: 10
-    )
-  end
+  let(:collection) { create(:collection, user: user) }
   let(:another_collection) { create(:collection, user_id: user.id) }
   let(:other_user_collection) { create(:collection, user_id: other_user.id) }
-  let(:other_shared_collection) { create(:collection, user_id: other_user.id, is_shared: true, permission_level: 3) }
+  let(:other_shared_collection) do
+    create(:collection, user_id: other_user.id).tap do |collection|
+      create(:collection_share, collection: collection, shared_with: user, permission_level: 3)
+    end
+  end
 
-  let(:screen) { create(:screen, component_graph_data: { some_dummy: 'data', with_nested: { but_cool: 'Stuff' } }) }
-  let(:another_screen) { create(:screen) }
+  let(:screen) do
+    create(
+      :screen,
+      collections: [collection],
+      component_graph_data: { some_dummy: 'data', with_nested: { but_cool: 'Stuff' } }
+    )
+  end
+  let(:another_screen) { create(:screen, collections: [collection]) }
 
   describe 'GET /api/v1/screens' do
-    before do
-      CollectionsScreen.create!(screen: screen, collection: collection)
-      CollectionsScreen.create!(screen: another_screen, collection: collection)
-    end
-
     context 'when no error occurs' do
+      before do
+        screen
+        another_screen
+      end
       it 'returns a list of screens' do
         get '/api/v1/screens', headers: request_headers
         expect(parsed_json_response['screens'].length).to eq(2)
@@ -47,13 +47,18 @@ describe Chemotion::ScreenAPI do
       end
 
       it 'returns screens' do
+        screen
+        another_screen
+
         get '/api/v1/screens', params: params, headers: request_headers
         expect(JSON.parse(response.body)['screens'].size).to eq(2)
       end
 
       context 'when no screens found' do
+        let(:empty_collection) { create(:collection, user: user, label: 'empty collection') }
+        let(:collection) { empty_collection }
+
         it 'returns no screens' do
-          allow(Collection).to receive(:belongs_to_or_shared_by).and_raise(ActiveRecord::RecordNotFound)
           get '/api/v1/screens', params: params, headers: request_headers
           expect(JSON.parse(response.body)['screens'].size).to eq(0)
         end
@@ -63,8 +68,6 @@ describe Chemotion::ScreenAPI do
 
   describe 'GET /api/v1/screens/{id}' do
     before do
-      CollectionsScreen.create!(screen: screen, collection: collection)
-
       get "/api/v1/screens/#{screen.id}", headers: request_headers
     end
 
@@ -101,10 +104,6 @@ describe Chemotion::ScreenAPI do
       Entities::ScreenEntity.represent(screen, root: :screen)
     end
 
-    before do
-      CollectionsScreen.create!(screen: screen, collection: collection)
-    end
-
     it 'adds an empty research plan to the screen' do
       expect do
         post "/api/v1/screens/#{screen.id}/add_research_plan", params: request_body
@@ -124,10 +123,10 @@ describe Chemotion::ScreenAPI do
 
   describe 'PUT /api/v1/screens/{id}' do
     let(:container) { create(:container) }
-    let(:screen) { create(:screen, name: 'Testname', container: container) }
-    let(:wellplate) { create(:wellplate) }
-    let(:other_wellplate) { create(:wellplate) }
-    let(:research_plan) { create(:research_plan) }
+    let(:screen) { create(:screen, name: 'Testname', container: container, collections: [collection]) }
+    let(:wellplate) { create(:wellplate, collections: [collection]) }
+    let(:other_wellplate) { create(:wellplate, collections: [collection]) }
+    let(:research_plan) { create(:research_plan, collections: [collection]) }
     let(:params) do
       {
         id: screen.id,
@@ -143,8 +142,6 @@ describe Chemotion::ScreenAPI do
     end
 
     before do
-      CollectionsScreen.create!(screen: screen, collection: collection)
-      CollectionsResearchPlan.create(research_plan: research_plan, collection: collection)
       ScreensWellplate.create!(wellplate: other_wellplate, screen: screen)
       put "/api/v1/screens/#{screen.id}", params: params.to_json, headers: request_headers
     end
@@ -171,10 +168,8 @@ describe Chemotion::ScreenAPI do
 
   describe 'POST /api/v1/screens' do
     let(:container) { create(:container) }
-    let(:screen) { create(:screen, name: 'Testname', container: container) }
-    let(:wellplate) { create(:wellplate) }
-    let(:other_wellplate) { create(:wellplate) }
-    let(:research_plan) { create(:research_plan) }
+    let(:wellplate) { create(:wellplate, collections: [collection]) }
+    let(:research_plan) { create(:research_plan, collections: [collection]) }
     let(:params) do
       {
         name: 'New Screen Testname',
