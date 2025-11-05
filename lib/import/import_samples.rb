7fbd26bbb5969068abec5145c92715e5878974da
adambasha0 <adambasha1992@gmail.com>
2025-11-05 11:23:47 +0000

    refactor: fix rubocop warning messages

--- lib/import/import_samples.rb
+++ lib/import/import_samples.rb
@@ -257,8 +257,8 @@ module Import
         molfile_smiles = babel_info[:smiles]
         molfile_smiles = Chemotion::OpenBabelService.canon_smiles_to_smiles molfile_smiles if check['smiles']
       end
-      if molfile_smiles.blank? && molfile_smiles != row['cano_smiles'] &&
-         molfile_smiles != row['smiles'] && molfile_smiles != row['canonical_smiles']
+      if molfile_smiles.blank? && (molfile_smiles != row['cano_smiles'] &&
+         molfile_smiles != row['smiles'] && molfile_smiles != row['canonical_smiles'])
         @unprocessable << { row: row, index: i }
         go_to_next = true
       end
@@ -275,7 +275,7 @@ module Import
       [molecule, molfile]
     end
 
-    def assign_molecule_data(molfile_coord, babel_info, inchikey, row, index)
+    def assign_molecule_data(molfile_coord, babel_info, inchikey, _row, _index)
       if inchikey.blank?
         go_to_next = true
       else
