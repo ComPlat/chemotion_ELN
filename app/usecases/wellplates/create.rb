85fb878a353f6876f7cdf02f1d25d23f8c230f55
Johannes Haubold <jh@megorei.com>
2025-11-06 09:10:15 +0100

    Update Wellplate API and specs

--- app/usecases/wellplates/create.rb
+++ app/usecases/wellplates/create.rb
@@ -13,26 +13,21 @@ module Usecases
 
       def execute! # rubocop:disable Metrics/AbcSize
         ActiveRecord::Base.transaction do
-          wellplate = Wellplate.create(params.except(:collection_id, :wells, :segments, :size, :user_labels))
-          wellplate.set_short_label(user: @current_user)
+          wellplate = Wellplate.create(
+            params.except(:collection_id, :wells, :segments, :size, :user_labels).merge(
+              collections: [collection]
+            )
+          )
+          wellplate.set_short_label(user: current_user)
           wellplate.reload
-          wellplate.save_segments(segments: params[:segments], current_user_id: @current_user.id)
-          update_element_labels(wellplate, params[:user_labels], @user_id)
+          wellplate.save_segments(segments: params[:segments], current_user_id: current_user.id)
+          update_element_labels(wellplate, params[:user_labels], current_user.id)
 
-          is_shared_collection = false
-          if user_collection
-            CollectionsWellplate.create(wellplate: wellplate, collection: user_collection)
-          elsif sync_collection_user
-            is_shared_collection = true
-            CollectionsWellplate.create(wellplate: wellplate, collection: sync_collection_user.collection)
-
+          if collection_is_owned_by_user?
+            CollectionsWellplate.create(wellplate: wellplate, collection: all_collection_of_current_user)
+          elsif collection_is_shared_to_user?
             CollectionsWellplate.create(wellplate: wellplate, collection: all_collection_of_sharer)
           end
-
-          CollectionsWellplate.create(
-            wellplate: wellplate,
-            collection: all_collection_of_current_user
-          ) unless is_shared_collection
 
           WellplateUpdater
             .new(wellplate: wellplate, current_user: current_user)
@@ -44,16 +39,20 @@ module Usecases
 
       private
 
-      def user_collection
-        @user_collection ||= current_user.collections.find_by(id: params[:collection_id])
+      def collection
+        @collection ||= Collection.accessible_for(current_user).find(params[:collection_id])
       end
 
-      def sync_collection_user
-        @sync_collection_user ||= current_user.all_sync_in_collections_users.find_by(id: params[:collection_id])
+      def collection_is_owned_by_user?
+        @collection.user == current_user
+      end
+
+      def collection_is_shared_to_user?
+        @collection.user != current_user && CollectionShare.exists?(collection: @collection, shared_with: current_user)
       end
 
       def all_collection_of_sharer
-        Collection.get_all_collection_for_user(sync_collection_user.shared_by_id)
+        Collection.get_all_collection_for_user(collection.user.id)
       end
 
       def all_collection_of_current_user
