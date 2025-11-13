533ff2580877c2f13185b7a716e61cb28725249e
Adrian Herrmann <headri@web.de>
2025-11-13 09:36:45 +0000

    fixed editButton and linting

--- app/javascript/src/apps/mydb/elements/details/sequenceBasedMacromoleculeSamples/attachmentsTab/AttachmentForm.js
+++ app/javascript/src/apps/mydb/elements/details/sequenceBasedMacromoleculeSamples/attachmentsTab/AttachmentForm.js
@@ -1,17 +1,15 @@
 import React, { useContext, useEffect } from 'react';
 import { ButtonToolbar } from 'react-bootstrap';
 
-import EditorFetcher from 'src/fetchers/EditorFetcher';
 import Attachment from 'src/models/Attachment';
 
 import ImageAnnotationModalSVG from 'src/apps/mydb/elements/details/researchPlans/ImageAnnotationModalSVG';
-import { last, findKey } from 'lodash';
 import SaveEditedImageWarning from 'src/apps/mydb/elements/details/researchPlans/SaveEditedImageWarning';
 import {
   undoButton,
   downloadButton,
   removeButton,
-  editButton,
+  EditButton,
   importButton,
   annotateButton,
   customDropzone,
@@ -26,64 +24,18 @@ import UIStore from 'src/stores/alt/stores/UIStore';
 import { observer } from 'mobx-react';
 import { StoreContext } from 'src/stores/mobx/RootStore';
 
-const AttachmentForm = ({ readonly }) => {
+function AttachmentForm({ readonly }) {
   const sbmmStore = useContext(StoreContext).sequenceBasedMacromoleculeSamples;
   const sbmmSample = sbmmStore.sequence_based_macromolecule_sample;
   const { thirdPartyApps } = UIStore.getState() || [];
-
-  useEffect(() => {
-    editorInitial();
-    createAttachmentPreviewImage();
-  }, []);
-
-  useEffect(() => {
-    if (sbmmSample.updated) {
-      createAttachmentPreviewImage();
-    }
-  }, [sbmmSample.attachments]);
-
-  const editorInitial = () => {
-    EditorFetcher.initial().then((result) => {
-      sbmmStore.setAttachmentEditor(result.installed);
-      sbmmStore.setAttachmentExtension(result.ext);
-    });
-  }
-
-  const createAttachmentPreviewImage = () => {
-    const attachments = sbmmSample.attachments.map((attachment) => {
-      if (attachment.preview !== undefined && attachment.preview !== '') { return attachment; }
-
-      attachment.preview = attachment.thumb
-        ? `/api/v1/attachments/${attachment.id}`
-        : '/images/wild_card/not_available.svg';
-      return attachment;
-    });
-    sbmmStore.setFilteredAttachments(attachments);
-  }
-
-  const handleSortChange = (e) => {
-    sbmmStore.setAttachmentSortBy(e.target.value);
-    filterAndSortAttachments();
-  }
-
-  const toggleSortDirection = () => {
-    const sortDirection = sbmmStore.attachment_sort_direction === 'asc' ? 'desc' : 'asc';
-    sbmmStore.setAttachmentSortDirectory(sortDirection);
-    filterAndSortAttachments();
-  }
-
-  const handleFilterChange = (e) => {
-    sbmmStore.setAttachmentFilterText(e.target.value);
-    filterAndSortAttachments();
-  }
 
   const filterAndSortAttachments = () => {
     const filterText = sbmmStore.attachment_filter_text.toLowerCase();
     const sortBy = sbmmStore.attachment_sort_by;
 
-    const filteredAttachments = sbmmSample.attachments.filter((attachment) => {
-      return attachment.filename.toLowerCase().includes(filterText)
-    });
+    const filteredAttachments = sbmmSample.attachments.filter(
+      (attachment) => attachment.filename.toLowerCase().includes(filterText)
+    );
 
     filteredAttachments.sort((a, b) => {
       let comparison = 0;
@@ -107,85 +59,90 @@ const AttachmentForm = ({ readonly }) => {
     });
 
     sbmmStore.setFilteredAttachments(filteredAttachments);
-  }
+  };
 
-  const handleEditAttachment = (attachment) => {
-    const fileType = last(attachment.filename.split('.'));
-    const docType = documentType(attachment.filename);
+  const createAttachmentPreviewImage = () => {
+    const attachments = sbmmSample.attachments.map((attachment) => {
+      if (attachment.preview !== undefined && attachment.preview !== '') { return attachment; }
 
-    EditorFetcher.startEditing({ attachment_id: attachment.id })
-      .then((result) => {
-        if (result.token) {
-          const url = `/editor?id=${attachment.id}&docType=${docType}
-          &fileType=${fileType}&title=${attachment.filename}&key=${result.token}
-          &only_office_token=${result.only_office_token}`;
-          window.open(url, '_blank');
+      attachment.preview = attachment.thumb
+        ? `/api/v1/attachments/${attachment.id}`
+        : '/images/wild_card/not_available.svg';
+      return attachment;
+    });
+    sbmmStore.setFilteredAttachments(attachments);
+  };
 
-          attachment.aasm_state = 'oo_editing';
-          attachment.updated_at = new Date();
+  useEffect(() => {
+    createAttachmentPreviewImage();
+  }, []);
 
-          updateEditedAttachment(attachment);
-        } else {
-          alert('Unauthorized to edit this file.');
-        }
-      });
-  }
+  useEffect(() => {
+    if (sbmmSample.updated) {
+      createAttachmentPreviewImage();
+    }
+  }, [sbmmSample.attachments]);
+
+  const handleSortChange = (e) => {
+    sbmmStore.setAttachmentSortBy(e.target.value);
+    filterAndSortAttachments();
+  };
+
+  const toggleSortDirection = () => {
+    const sortDirection = sbmmStore.attachment_sort_direction === 'asc' ? 'desc' : 'asc';
+    sbmmStore.setAttachmentSortDirectory(sortDirection);
+    filterAndSortAttachments();
+  };
+
+  const handleFilterChange = (e) => {
+    sbmmStore.setAttachmentFilterText(e.target.value);
+    filterAndSortAttachments();
+  };
 
   const onUndoDelete = (attachment) => {
     const index = sbmmSample.attachments.indexOf(attachment);
     sbmmStore.changeAttachment(index, 'is_deleted', false);
-  }
+  };
 
   const onDelete = (attachment) => {
     const index = sbmmSample.attachments.indexOf(attachment);
     sbmmStore.changeAttachment(index, 'is_deleted', true);
-  }
-
-  const documentType = (filename) => {
-    const ext = last(filename.split('.'));
-    const docType = findKey(sbmmStore.attachment_extension, (o) => o.includes(ext));
-
-    if (typeof docType === 'undefined' || !docType) {
-      return null;
-    }
-
-    return docType;
-  }
+  };
 
   const showImportConfirm = (attachmentId) => {
     sbmmStore.attachment_show_import_confirm[attachmentId] = true;
     sbmmStore.setShowImportConfirm(sbmmStore.attachment_show_import_confirm);
-  }
+  };
 
   const hideImportConfirm = (attachmentId) => {
     sbmmStore.attachment_show_import_confirm[attachmentId] = false;
     sbmmStore.setShowImportConfirm(sbmmStore.attachment_show_import_confirm);
-  }
+  };
 
   const confirmAttachmentImport = (attachment) => {
     hideImportConfirm(attachment.id);
-  }
+  };
 
   const openAnnotateModal = (attachment) => {
     sbmmStore.toogleAttachmentModal();
     sbmmStore.setAttachmentSelected(attachment);
-  }
+  };
 
   const updateAttachments = (attachments) => {
     sbmmStore.changeSequenceBasedMacromoleculeSample('attachments', attachments);
     sbmmStore.setFilteredAttachments(
       sbmmStore.sequence_based_macromolecule_sample.attachments
     );
-  }
+  };
 
   const handleAttachmentDrop = (files) => {
     const newAttachments = files.map((file) => Attachment.fromFile(file));
     const updatedAttachments = sbmmSample.attachments.concat(newAttachments);
     updateAttachments(updatedAttachments);
-  }
+  };
 
   const updateEditedAttachment = (attachment) => {
-    let attachments = [];
+    const attachments = [];
     sbmmSample.attachments.map((currentAttachment) => {
       if (currentAttachment.id === attachment.id) {
         attachments.push(attachment);
@@ -194,51 +151,36 @@ const AttachmentForm = ({ readonly }) => {
       }
     });
     updateAttachments(attachments);
-  }
+  };
 
   const handleEditAnnotation = (annotation) => {
-    let selectedAttachment = { ...sbmmStore.attachment_selected };
+    const selectedAttachment = { ...sbmmStore.attachment_selected };
     selectedAttachment.updatedAnnotation = annotation;
     updateEditedAttachment(selectedAttachment);
-  }
+  };
 
-  const attachmentRowActions = (attachment) => {
-    const updatedAt = new Date(attachment.updated_at).getTime() + 15 * 60 * 1000;
-    const isEditing = attachment.aasm_state === 'oo_editing' && new Date().getTime() < updatedAt;
-    const editDisable =
-      !sbmmStore.attachment_editor || attachment.aasm_state === 'oo_editing'
-      || attachment.is_new || documentType(attachment.filename) === null;
-
-    return (
-      <ButtonToolbar className="gap-1">
-        {downloadButton(attachment)}
-        <ThirdPartyAppButton attachment={attachment} options={thirdPartyApps} />
-        {editButton(
-          attachment,
-          sbmmStore.attachment_extension,
-          sbmmStore.attachment_editor,
-          isEditing,
-          editDisable,
-          handleEditAttachment
-        )}
-        {annotateButton(attachment, () => openAnnotateModal(attachment))}
-        {importButton(
-          attachment,
-          sbmmStore.attachment_show_import_confirm,
-          sbmmSample.changed,
-          showImportConfirm,
-          hideImportConfirm,
-          confirmAttachmentImport
-        )}
-        <div className="ms-2">
-          {removeButton(attachment, onDelete, readonly)}
-        </div>
-      </ButtonToolbar>
-    );
-  }
+  const attachmentRowActions = (attachment) => (
+    <ButtonToolbar className="gap-1">
+      {downloadButton(attachment)}
+      <ThirdPartyAppButton attachment={attachment} options={thirdPartyApps} />
+      <EditButton attachment={attachment} onChange={updateEditedAttachment} />
+      {annotateButton(attachment, () => openAnnotateModal(attachment))}
+      {importButton(
+        attachment,
+        sbmmStore.attachment_show_import_confirm,
+        sbmmSample.changed,
+        showImportConfirm,
+        hideImportConfirm,
+        confirmAttachmentImport
+      )}
+      <div className="ms-2">
+        {removeButton(attachment, onDelete, readonly)}
+      </div>
+    </ButtonToolbar>
+  );
 
   const showList = () => {
-    let attachmentList = [];
+    const attachmentList = [];
 
     sbmmStore.filteredAttachments.map((attachment) => {
       const rowTextClass = attachment.is_deleted ? ' text-decoration-line-through' : '';
@@ -279,10 +221,10 @@ const AttachmentForm = ({ readonly }) => {
             </div>
           )}
         </div>
-      )
+      );
     });
     return attachmentList;
-  }
+  };
 
   const showFilter = () => {
     if (sbmmSample.attachments.length === 0) { return null; }
@@ -296,7 +238,7 @@ const AttachmentForm = ({ readonly }) => {
         true
       )
     );
-  }
+  };
 
   const renderImageEditModal = () => {
     if (!sbmmStore.show_attachment_image_edit_modal) { return null; }
@@ -312,10 +254,10 @@ const AttachmentForm = ({ readonly }) => {
             handleEditAnnotation(newAnnotation);
           }
         }
-        handleOnClose={() => { sbmmStore.toogleAttachmentModal() }}
+        handleOnClose={() => { sbmmStore.toogleAttachmentModal(); }}
       />
     );
-  }
+  };
 
   return (
     <div className="p-3">
