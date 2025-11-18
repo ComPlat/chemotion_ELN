89d57f905ac87066e3fd0e1a017cfd939292eaa9
Adrian Herrmann <headri@web.de>
2025-11-18 10:17:33 +0000

    fixed migrations

--- db/migrate/20250101000000_seed_report_templates.rb
+++
@@ -1,9 +0,0 @@
-class SeedReportTemplates < ActiveRecord::Migration[4.2]
-  def change
-    ReportTemplate.reset_column_information
-    Attachment.reset_column_information
-    User.reset_column_information
-    seed_path = Rails.root.join('db', 'seeds', 'shared', 'report_templates.seed.rb')
-    load(seed_path) if File.exist?(seed_path)
-  end
-end
