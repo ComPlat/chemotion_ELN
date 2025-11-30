d1d1336466905455a9aa0e7cebb2b7cffdd0952c
Christian Buggle <christian@buggle.net>
2025-11-30 18:24:52 +0100

    Add Centrifugation to ORD Export (RPE#48)
    * Add Centrifugation to ORD definition.
    * Add CentrifugationExporter.
    * Adapt condition keys in workup / infoLineSampleCondition (also refactor AddSample).

---
+++ app/ord_kit/exporter/actions/purification/centrifugation_exporter.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module OrdKit
+  module Exporter
+    module Actions
+      module Purification
+        class CentrifugationExporter < Actions::Purification::Base
+          def to_ord
+            {
+              centrifugation:
+              ReactionProcessAction::ActionPurificationCentrifugation.new({
+                                                                            pressure: Metrics::PressureExporter.new(workup['PRESSURE']).to_ord,
+                                                                            temperature: Metrics::TemperatureExporter.new(workup['TEMPERATURE']).to_ord,
+                                                                            motion: Metrics::MotionExporter.new(workup['MOTION']).to_ord,
+                                                                          }),
+            }
+          end
+        end
+      end
+    end
+  end
+end
