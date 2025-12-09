c5b1d484253066f278d38139443b0ea948eefe26
Tasnim Mehzabin <tasnim.mehzabin@live.com>
2025-12-09 15:43:01 +0100

    refactor: preserve solvent when creating new samples in reactions
    Remove :solvent from exclusion list in create_new_sample to ensure
    solvent data is preserved when creating new materials via ReactionAPI.

--- app/usecases/reactions/update_materials.rb
+++ app/usecases/reactions/update_materials.rb
@@ -117,7 +117,7 @@ module Usecases
           :type, :molecule, :collection_id, :short_label, :waste, :show_label, :coefficient, :user_labels,
           :boiling_point_lowerbound, :boiling_point_upperbound,
           :melting_point_lowerbound, :melting_point_upperbound, :segments, :gas_type,
-          :gas_phase_data, :conversion_rate, :components, :solvent
+          :gas_phase_data, :conversion_rate, :components
         ).merge(created_by: @current_user.id,
                 boiling_point: rangebound(sample.boiling_point_lowerbound, sample.boiling_point_upperbound),
                 melting_point: rangebound(sample.melting_point_lowerbound, sample.melting_point_upperbound))
