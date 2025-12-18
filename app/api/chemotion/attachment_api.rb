c7060f5b2a01cc9fad678e8169c8dc19c66ba947
Adrian Herrmann <headri@web.de>
2025-12-18 09:54:34 +0000

    added auto assign

--- app/api/chemotion/attachment_api.rb
+++ app/api/chemotion/attachment_api.rb
@@ -40,6 +40,24 @@ module Chemotion
 
         old_att&.destroy
       end
+
+      def find_unique_match_for_filename(filename, user)
+        samples = InboxSearchElements.call(
+          search_string: filename,
+          current_user: user,
+          element: :sample,
+        )
+
+        reactions = InboxSearchElements.call(
+          search_string: filename,
+          current_user: user,
+          element: :reaction,
+        )
+        return reactions.first if samples.empty? && reactions.one?
+        return samples.first if reactions.empty? && samples.one?
+
+        nil
+      end
     end
 
     rescue_from ActiveRecord::RecordNotFound do |_error|
@@ -259,7 +277,29 @@ module Chemotion
           begin
             attach.save!
             attach_ary.push(attach.id)
-          rescue StandardError
+
+            match = find_unique_match_for_filename(file[:filename], current_user)
+
+            if match && match.container&.children&.find_by(container_type: 'analyses') # auto assign to element
+              analyses_container = match.container.children.find_by(container_type: 'analyses')
+              analysis_name = attach.filename.chomp(File.extname(attach.filename))
+              new_analysis_container = analyses_container.children.create(container_type: 'analysis',
+                                                                          name: analysis_name)
+              dataset = new_analysis_container.children.create(parent_id: new_analysis_container.id,
+                                                               container_type: 'dataset', name: analysis_name)
+              attach.update!(attachable: dataset)
+              type = match.model_name.singular
+              @link = "#{Rails.application.config.root_url}/mydb/collection/all/#{type}/#{match.id}"
+              Message.create_msg_notification(
+                channel_subject: Channel::ASSIGN_INBOX_TO_SAMPLE,
+                message_from: current_user.id,
+                data_args: { filename: attach.filename, info: "#{match.short_label} #{match.name}" },
+                url: @link,
+                level: 'success',
+              )
+            end
+          rescue StandardError => e
+            Rails.logger.error(e)
             status 413
           ensure
             tempfile.close
