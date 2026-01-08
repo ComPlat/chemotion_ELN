c83701df7818132e6e3d42bb630cd2342a20edbc
haditariq <hadi-tariq@live.co.uk>
2026-01-08 12:44:22 +0000

    reaction details revered

--- app/javascript/src/apps/mydb/elements/details/reactions/ReactionDetails.js
+++ app/javascript/src/apps/mydb/elements/details/reactions/ReactionDetails.js
@@ -677,24 +677,19 @@ export default class ReactionDetails extends Component {
 
   // eslint-disable-next-line class-methods-use-this
   updateReactionVesselSize(reaction) {
-    if (!reaction) return;
-
     const { catalystMoles, vesselSize } = reaction.findReactionVesselSizeCatalystMaterialValues();
 
-    // Avoid dispatch while another Alt dispatch is in progress.
-    setTimeout(() => {
-      if (vesselSize) {
-        GasPhaseReactionActions.setReactionVesselSize(vesselSize);
-      } else {
-        GasPhaseReactionActions.setReactionVesselSize(null);
-      }
+    if (vesselSize) {
+      GasPhaseReactionActions.setReactionVesselSize(vesselSize);
+    } else {
+      GasPhaseReactionActions.setReactionVesselSize(null);
+    }
 
-      if (catalystMoles) {
-        GasPhaseReactionActions.setCatalystReferenceMole(catalystMoles);
-      } else {
-        GasPhaseReactionActions.setCatalystReferenceMole(null);
-      }
-    }, 0);
+    if (catalystMoles) {
+      GasPhaseReactionActions.setCatalystReferenceMole(catalystMoles);
+    } else {
+      GasPhaseReactionActions.setCatalystReferenceMole(null);
+    }
   }
 
   /**
