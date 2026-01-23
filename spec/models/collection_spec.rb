9c5fa3def2e412e93ac1beb764cc6aba5901910d
PiTrem <pierre.tremouilhac@kit.edu>
2026-01-23 15:26:07 +0100

    chore: update model annotation

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -4,29 +4,30 @@
 #
 # Table name: collections
 #
-#  id                             :integer          not null, primary key
-#  ancestry                       :string           default("/"), not null
-#  celllinesample_detail_level    :integer          default(10)
-#  deleted_at                     :datetime
-#  devicedescription_detail_level :integer          default(10)
-#  element_detail_level           :integer          default(10)
-#  is_locked                      :boolean          default(FALSE)
-#  is_shared                      :boolean          default(FALSE)
-#  is_synchronized                :boolean          default(FALSE), not null
-#  label                          :text             not null
-#  permission_level               :integer          default(0)
-#  position                       :integer
-#  reaction_detail_level          :integer          default(10)
-#  researchplan_detail_level      :integer          default(10)
-#  sample_detail_level            :integer          default(10)
-#  screen_detail_level            :integer          default(10)
-#  tabs_segment                   :jsonb
-#  wellplate_detail_level         :integer          default(10)
-#  created_at                     :datetime         not null
-#  updated_at                     :datetime         not null
-#  inventory_id                   :bigint
-#  shared_by_id                   :integer
-#  user_id                        :integer          not null
+#  id                                            :integer          not null, primary key
+#  ancestry                                      :string           default("/"), not null
+#  celllinesample_detail_level                   :integer          default(10)
+#  deleted_at                                    :datetime
+#  devicedescription_detail_level                :integer          default(10)
+#  element_detail_level                          :integer          default(10)
+#  is_locked                                     :boolean          default(FALSE)
+#  is_shared                                     :boolean          default(FALSE)
+#  is_synchronized                               :boolean          default(FALSE), not null
+#  label                                         :text             not null
+#  permission_level                              :integer          default(0)
+#  position                                      :integer
+#  reaction_detail_level                         :integer          default(10)
+#  researchplan_detail_level                     :integer          default(10)
+#  sample_detail_level                           :integer          default(10)
+#  screen_detail_level                           :integer          default(10)
+#  sequencebasedmacromoleculesample_detail_level :integer          default(10)
+#  tabs_segment                                  :jsonb
+#  wellplate_detail_level                        :integer          default(10)
+#  created_at                                    :datetime         not null
+#  updated_at                                    :datetime         not null
+#  inventory_id                                  :bigint
+#  shared_by_id                                  :integer
+#  user_id                                       :integer          not null
 #
 # Indexes
 #
