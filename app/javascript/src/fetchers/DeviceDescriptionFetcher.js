3f320c3135d9da25e89d27e58037983f050f4588
Beate Quednau <bq@megorei.com>
2026-02-02 13:54:18 +0100

    Remove unused api request, fix api test

--- app/javascript/src/fetchers/DeviceDescriptionFetcher.js
+++ app/javascript/src/fetchers/DeviceDescriptionFetcher.js
@@ -52,24 +52,6 @@ export default class DeviceDescriptionFetcher {
       .catch(errorMessage => console.log(errorMessage));
   }
 
-  static fetchSegmentKlassIdsByNewOntology(deviceDescriptionId, params) {
-    return fetch(
-      `/api/v1/device_descriptions/byontology/${deviceDescriptionId}`,
-      {
-        ...this._httpOptions('PUT'),
-        body: JSON.stringify(params)
-      }
-    ).then(response => response.json())
-      .then((json) => {
-        if (json.error) {
-          return [];
-        } else {
-          return json;
-        }
-      })
-      .catch(errorMessage => console.log(errorMessage));
-  }
-
   static createDeviceDescription(deviceDescription) {
     const newFiles = (deviceDescription.attachments || []).filter((a) => a.is_new && !a.is_deleted);
 
