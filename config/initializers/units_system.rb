4320cca1303cab7f3eedb1468f4bbfd6112a43d4
Adrian Herrmann <headri@web.de>
2026-02-03 16:22:21 +0100

    fix: load actual units system from labimotion (#2937)
    * added initializer to generate an actual public asset units json that the advanced search is using
    
    advanced search is fetching units_system/units_system.json that should be in  sync with the definition of Labimotion::Units::FIELDS 
    
    * Stop tracking generated units_system

---
+++ config/initializers/units_system.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Rails.application.config.after_initialize do
+  path = Rails.public_path.join('units_system', 'units_system.json')
+  File.write(path, JSON.pretty_generate({ fields: Labimotion::Units::FIELDS }))
+rescue StandardError => e
+  Rails.logger.error("Failed to write units_system.json: #{e.message}")
+end
