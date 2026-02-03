04dc7f31c09ce5ef3fb10ca4bbdcaa8128ebe11e
Adrian Herrmann <headri@web.de>
2026-02-03 11:23:51 +0000

    use Rails.public_path

--- config/initializers/units_system.rb
+++ config/initializers/units_system.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.config.after_initialize do
-  path = File.join('public', 'units_system')
-  File.write(
-    File.join(path, 'units_system.json'),
-    JSON.pretty_generate({ fields: Labimotion::Units::FIELDS }),
-  )
+  path = Rails.public_path.join('units_system', 'units_system.json')
+  File.write(path, JSON.pretty_generate({ fields: Labimotion::Units::FIELDS }))
 rescue StandardError => e
   Rails.logger.error("Failed to write fields.json: #{e.message}")
 end
