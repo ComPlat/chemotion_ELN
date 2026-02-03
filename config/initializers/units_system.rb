cb8814dc51d702e096b794c04b0a1d1e15924ce5
Adrian Herrmann <headri@web.de>
2026-02-03 11:25:08 +0000

    fix error message

--- config/initializers/units_system.rb
+++ config/initializers/units_system.rb
@@ -4,5 +4,5 @@ Rails.application.config.after_initialize do
   path = Rails.public_path.join('units_system', 'units_system.json')
   File.write(path, JSON.pretty_generate({ fields: Labimotion::Units::FIELDS }))
 rescue StandardError => e
-  Rails.logger.error("Failed to write fields.json: #{e.message}")
+  Rails.logger.error("Failed to write units_system.json: #{e.message}")
 end
