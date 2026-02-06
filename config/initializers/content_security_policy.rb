a76c60c4020d3b871964d7caea4a1bdedf796e07
PiTrem <pierre.tremouilhac@kit.edu>
2026-02-06 10:28:44 +0100

    build: fix csp for office script
    - ensure external script from configured src can be loaded for the
    document editor page
    
    - rename initializer for a correct loading order as csp initializer now
    depends on the editor one

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -17,6 +17,8 @@ Rails.application.config.content_security_policy do |policy|
   # Allow WebSocket, blob, and specific external connections
   src = %i[self https data wss blob]
   src += ['https://commonchemistry.cas.org', 'https://dx.doi.org', 'https://doi.org', 'https://api.crossref.org', 'https://service.tib.eu']
+  script_src = %i[self]
+  script_src += [Rails.application.config.editors.docserver_api] if Rails.application.config.editors&.docserver_api.present?
 
   # connect_src for log and webpack-dev-serverwebsockets in development
   if Rails.env.development?
@@ -28,14 +30,21 @@ Rails.application.config.content_security_policy do |policy|
     src += ['http://localhost:3035', 'http://webpacker:3035', 'ws://localhost:3035']
     src += ["ws://#{host}:3035", "#{scheme}://#{host}:3035"] if host.present?
     src += ["#{scheme}://#{host}:#{url.port}"] if host.present? && port.present?
-    puts "connect_src: #{src}"
-
+    puts <<~CSPINFO
+      connect_src: #{src}"
+      script_src: #{script_src}"
+    CSPINFO
   end
+
+  policy.script_src_elem(*script_src)
   policy.connect_src(*src)
 
   # Specify URI for violation reports
   policy.report_uri '/csp-violation-report'
 end
+
+Rails.application.config.content_security_policy_nonce_generator = ->(request) { SecureRandom.base64(16) }
+Rails.application.config.content_security_policy_nonce_directives = %w(script-src script-src-elem)
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
