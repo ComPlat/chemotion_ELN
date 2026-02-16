c047678c53d5ab87c65bab66d41b877fcfc66439
Adrian Herrmann <headri@web.de>
2026-02-16 08:20:03 +0100

    fix: checksum calculation (#2957)
    Corrected use of lodash omit:
    omit → remove by name
    omitBy → remove by rule

--- app/javascript/src/models/LiteratureMap.js
+++ app/javascript/src/models/LiteratureMap.js
@@ -36,7 +36,7 @@ export default class LiteratureMap {
   }
 
   checksum(fieldsToOmit = []) {
-    return sha256(JSON.stringify(_.omit(_.omit(this,
+    return sha256(JSON.stringify(_.omitBy(_.omit(this,
       ['_checksum', ...fieldsToOmit],
     ), _.isEmpty)));
   }
@@ -87,8 +87,8 @@ export default class LiteratureMap {
       collection_id: this.collection_id
     };
     _.merge(params, extraParams);
-    const paramsWithoutNullEntries = _.omit(params, _.isNull);
-    const cleanParams = _.omit(paramsWithoutNullEntries, x => (x === '***'));
+    const paramsWithoutNullEntries = _.omitBy(params, _.isNull);
+    const cleanParams = _.omitBy(paramsWithoutNullEntries, x => (x === '***'));
     return cleanParams;
   }
 
