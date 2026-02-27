572a9d8c4e29a9d0f4651df8f3e8609dffeee814
Beate Quednau <bq@megorei.com>
2026-02-27 13:42:37 +0100

    Fix move / assign to collection for device descriptions

--- app/usecases/collections/add_elements.rb
+++ app/usecases/collections/add_elements.rb
@@ -16,7 +16,6 @@ module Usecases
         add_elements_to_collection(ui_state)
       end
 
-      # rubocop:disable Rails/FindByOrAssignmentMemoization
       def find_collection(collection_id)
         @collection = Collection.own_collections_for(current_user).find_by(id: collection_id)
         @collection ||= Collection.shared_with_minimum_permission_level(
@@ -28,7 +27,6 @@ module Usecases
 
         raise Errors::InsufficientPermissionError, 'You do not have the right to add elements to this collection'
       end
-      # rubocop:enable Rails/FindByOrAssignmentMemoization
 
       def check_access_to_elements(ui_state)
         ui_state.each do |class_string, ui_selections|
@@ -52,6 +50,11 @@ module Usecases
         ui_state.each do |class_string, ui_selections|
           element_class = API::ELEMENT_CLASS[class_string] || Labimotion::ElementKlass.find(name: class_string).elements
           element_ids = element_class.by_ui_state(ui_selections).ids
+
+          if element_class == DeviceDescription
+            element_ids = Usecases::DeviceDescriptions::ByUIState.new(element_ids).with_joined_ids
+          end
+
           join_table = API::ELEMENT_CLASS[class_string].collections_element_class || Labimotion::CollectionsElement
 
           join_table.create_in_collection(element_ids, collection.id)
