40fd4139475578f87cc8a6bcc60ca9babc3bf45b
PiTrem <pierre.tremouilhac@kit.edu>
2026-02-28 10:36:58 +0100

    feat: auto transfer inbox item to reaction analyses (#2837)

--- app/api/chemotion/inbox_api.rb
+++ app/api/chemotion/inbox_api.rb
@@ -92,56 +92,109 @@ module Chemotion
           requires :search_string, type: String, desc: 'Search String'
         end
         get do
-          search_string = params[:search_string]
-          search_string.chomp!(File.extname(search_string))
-          search_string.chomp!(' EA')
-          search_string.sub!(/-?[a-zA-Z]$/, '')
-          search_string.sub!(/^[a-zA-Z0-9]+-/, '')
-          collection_ids = Collection.belongs_to_or_shared_by(current_user.id, current_user.group_ids).map(&:id)
-          samples = Sample.by_exact_name(search_string)
-                          .joins(:collections_samples)
-                          .where(collections_samples: { collection_id: collection_ids }).uniq
-          samples.select { |s| ElementPolicy.new(current_user, s).update? }
+          samples = InboxSearchElements.call(
+            search_string: params[:search_string],
+            current_user: current_user,
+            element: :sample,
+          )
 
-          ids = samples.pluck(:id)
-          rs = ReactionsSample.where(sample_id: ids)
           res = samples.map do |s|
             {
               id: s.id,
               name: s.name,
               short_label: s.short_label,
-              type: rs.find { |r| r.sample_id == s.id }&.type&.sub(/^Reactions/, '')&.sub(/Sample/, ''),
+              type: s.reactions_samples.map do |rs|
+                rs.type.sub(/^Reactions/, '').sub('Sample', '')
+              end.first,
             }
           end
+
           { samples: res }
         end
 
         desc 'assign attachment to sample'
         params do
-          optional :attachment_id, type: Integer, desc: 'Sample ID'
+          requires :attachment_id, type: Integer, desc: 'Attachment ID'
         end
         before do
           @sample = Sample.find(params[:sample_id])
           error!('401 Unauthorized', 401) unless ElementPolicy.new(current_user, @sample).update?
-          attachment = Attachment.find(params[:attachment_id])
-          error!('402 Unauthorized', 402) unless attachment.created_for == current_user.id
+          @attachment = Attachment.find(params[:attachment_id])
+          error!('401 Unauthorized', 401) unless @attachment.created_for == current_user.id
         end
         post ':sample_id' do
-          analyses_container = Sample.find(params[:sample_id]).container.children.find_by(container_type: 'analyses')
-          attachment = Attachment.find(params[:attachment_id])
-          analysis_name = attachment.filename.chomp(File.extname(attachment.filename))
+          analysis_name = @attachment.filename.chomp(File.extname(@attachment.filename))
 
-          new_analysis_container = analyses_container.children.create(container_type: 'analysis', name: analysis_name)
-          dataset = new_analysis_container.children.create(parent_id: new_analysis_container.id,
-                                                           container_type: 'dataset', name: analysis_name)
-          attachment.update!(attachable: dataset)
+          dataset = @sample.container.analyses_container.create_analysis_with_dataset!(name: analysis_name)
+
+          @attachment.update!(attachable: dataset)
 
           @link = "#{Rails.application.config.root_url}/mydb/collection/all/sample/#{@sample.id}"
 
           Message.create_msg_notification(
             channel_subject: Channel::ASSIGN_INBOX_TO_SAMPLE,
             message_from: current_user.id,
-            data_args: { filename: attachment.filename, info: "#{@sample.short_label} #{@sample.name}" },
+            data_args: { filename: @attachment.filename, info: "#{@sample.short_label} #{@sample.name}" },
+            url: @link,
+            level: 'success',
+          )
+
+          present dataset, with: Entities::ContainerEntity, root: :container
+        end
+      end
+
+      resource :reactions do
+        desc 'search reactions from user by'
+        params do
+          requires :search_string, type: String, desc: 'Search String'
+        end
+        get do
+          original = params[:search_string].to_s
+
+          variation = original[/-v(\d+)(?=\.[^.]+$|$)/, 1] # Handling for variations with "ReactionName-vi"
+          search_string = original.sub(/-v\d+(?=\.[^.]+$|$)/, '')
+
+          reactions = InboxSearchElements.call(
+            search_string: search_string,
+            current_user: current_user,
+            element: :reaction,
+          )
+
+          {
+            reactions: reactions.map do |r|
+              {
+                id: r.id,
+                name: r.name,
+                short_label: r.short_label,
+                type: '',
+                variation: variation,
+              }
+            end,
+          }
+        end
+
+        desc 'assign attachment to reaction'
+        params do
+          requires :attachment_id, type: Integer, desc: 'Attachment ID'
+          optional :variation, type: Integer, desc: 'Variation Number'
+        end
+        before do
+          @reaction = Reaction.find(params[:reaction_id])
+          error!('401 Unauthorized', 401) unless ElementPolicy.new(current_user, @reaction).update?
+          @attachment = Attachment.find(params[:attachment_id])
+          error!('401 Unauthorized', 401) unless @attachment.created_for == current_user.id
+        end
+        post ':reaction_id' do
+          analysis_name = @attachment.filename.chomp(File.extname(@attachment.filename))
+          dataset = @reaction.container.analyses_container.create_analysis_with_dataset!(name: analysis_name)
+          @attachment.update!(attachable: dataset)
+          @link = "#{Rails.application.config.root_url}/mydb/collection/all/reaction/#{@reaction.id}"
+          @reaction.assign_attachment_to_variation(params[:variation], dataset.parent_id)
+
+          Message.create_msg_notification(
+            channel_subject: Channel::ASSIGN_INBOX_TO_SAMPLE,
+            message_from: current_user.id,
+            data_args: { filename: @attachment.filename, info: "#{@reaction.short_label} #{@reaction.name}" },
             url: @link,
             level: 'success',
           )
