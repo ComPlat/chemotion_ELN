5cd437544c24c22144682c904c736ae6780033e4
Martin Starman <martin.starman@kit.edu>
2026-03-09 13:29:41 +0000

    Added OTP (one time password) Added workflow (page/email) to enable Added workflow (page/email) to disable Extended Login with OTP

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,25 @@
+module Users
+  class SessionsController < Devise::SessionsController
+    def create
+      requested_user = User.where(name_abbreviation: params[:user][:login]).or(User.where(email: params[:user][:login])).take
+      if requested_user && requested_user.valid_password?(params[:user][:password])
+        if requested_user.otp_required_for_login
+          unless params[:user][:otp_attempt].present?
+            render json: { otp_required: true }, status: :unauthorized
+            return
+          end
+
+          unless requested_user.validate_and_consume_otp!(params[:user][:otp_attempt])
+            render json: {  otp_required: true, error: "Invalid OTP" }, status: :unauthorized
+            return
+          end
+        end
+      end
+
+      super do |resource|
+        # custom logic after successful login
+        Rails.logger.info("Login success: #{resource.id}")
+      end
+    end
+  end
+end
---
+++ app/controllers/users/two_factor_auth_controller.rb
@@ -0,0 +1,61 @@
+module Users
+  class TwoFactorAuthController < ApplicationController
+    before_action :decode_jwt, only: [:request_enable, :request_disable, :verify]
+    layout "two_factor_auth"
+
+    def request_enable
+      @qr_code_svg = @user.generate_qr_code
+      render :request_enable
+    end
+
+    def request_disable
+      @user.otp_required_for_login = false
+      @user.save!
+      @header = 'Two-Factor Authentication disabled!'
+      @message = 'Successfully disabled Two-Factor Authentication.'
+      render :request_result
+    end
+
+    def verify
+      if @user.check_otp(params[:code])# replace with real verification
+        @user.otp_required_for_login = true
+        @user.save!
+        @header = 'Two-Factor Authentication activated!'
+        @message = 'Successfully activated Two-Factor Authentication.'
+      else
+        @message = 'Code wrong!'
+        return :request_enable
+      end
+      render :request_result
+    end
+
+    private
+
+    def decode_jwt
+      token = params[:jwt]
+
+      begin
+        @payload = JsonWebToken.decode(token)
+        if @payload[:action] != 'activate_2fa'
+          @header = 'Two-Factor Authentication failed'
+          @message = 'Token is a TFA token!'
+          return render :request_result, status: :unauthorized
+        end
+        @user = User.find(@payload[:user_id])
+        if @user.blank?
+          @header = 'Two-Factor Authentication failed'
+          @message = 'Token is a TFA token!'
+          return render :request_result, status: :unauthorized
+        end
+      rescue Errors::ExpiredSignature
+        @header = 'Two-Factor Authentication failed!'
+        @message = 'Token expired! please request a new token!'
+        render :request_result, status: :unauthorized
+      rescue Errors::DecodeError
+        @header = 'Two-Factor Authentication failed'
+        @message = 'Token is not valid!'
+        render :request_result, status: :unauthorized
+      end
+    end
+  end
+end
