e35d591370b47422e71c23ebcd91dd3995f473ea
Joachim Tillessen <tillessen@emjot.de>
2026-03-10 10:30:37 +0100

    style: rename 'Literature' to 'References' (#2991)

--- app/javascript/src/apps/mydb/collections/CollectionSubtreeFunctions.js
+++ app/javascript/src/apps/mydb/collections/CollectionSubtreeFunctions.js
@@ -72,7 +72,7 @@ export default function CollectionSubtreeFunctions({ collection }) {
         <Dropdown.Menu renderOnMount popperConfig={{ strategy: 'fixed' }}>
           <Dropdown.Item onClick={handleShowLiterature}>
             <i className="fa square-icon fa-book me-1" />
-            Literature Report
+            Reference Report
           </Dropdown.Item>
           <Dropdown.Item onClick={handleImportSamples}>
             <i className="icon-square icon-arrow-down-to-bracket me-1" />
