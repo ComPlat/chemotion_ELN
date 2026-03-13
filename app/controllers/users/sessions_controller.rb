b2a21fc1b2c5fd73b4138ed8f82fed82c9cebc7e
Martin Starman <martin.starman@kit.edu>
2026-03-13 07:48:40 +0000

    Fixed github-actions[bot] comments

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,5 @@
 module Users
   class SessionsController < Devise::SessionsController
-
     def new
       @username = params[:login]
       @invalid = params[:invalid]
@@ -9,17 +8,15 @@ module Users
 
     def create
       requested_user = User.where(name_abbreviation: params[:user][:login]).or(User.where(email: params[:user][:login])).take
-      if requested_user && requested_user.valid_password?(params[:user][:password])
-        if requested_user.otp_required_for_login
-          unless params[:user][:otp_attempt].present?
-            render json: { otp_required: true }, status: :unauthorized
-            return
-          end
+      if requested_user&.valid_password?(params[:user][:password]) && requested_user.otp_required_for_login
+        if params[:user][:otp_attempt].blank?
+          render json: { otp_required: true }, status: :unauthorized
+          return
+        end
 
-          unless requested_user.validate_and_consume_otp!(params[:user][:otp_attempt])
-            render json: {  otp_required: true, error: "Invalid OTP" }, status: :unauthorized
-            return
-          end
+        unless requested_user.validate_and_consume_otp!(params[:user][:otp_attempt])
+          render json: { otp_required: true, error: 'Invalid OTP' }, status: :unauthorized
+          return
         end
       end
 
