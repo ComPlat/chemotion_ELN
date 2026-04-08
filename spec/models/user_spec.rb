de076a7fe3605d2fe24e5163495f037f734ce065
PiTrem <pierre.tremouilhac@kit.edu>
2026-04-08 13:47:59 +0200

    chore: db/schema model-annotations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,7 +38,6 @@
 #  reset_password_sent_at    :datetime
 #  reset_password_token      :string
 #  sign_in_count             :integer          default(0), not null
-#  tokens                    :jsonb
 #  type                      :string           default("Person")
 #  unconfirmed_email         :string
 #  unlock_token              :string
