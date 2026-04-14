4d5e4e83dbe5aa8ac0d08daf63569579cb3ddfc6
Joachim Tillessen <tillessen@emjot.de>
2026-04-14 14:31:09 +0200

    Migrate SelectionShareModal and CollectionSharesEditModal to AppModal

--- app/javascript/src/apps/mydb/elements/list/selectionActions/SelectionShareModal.js
+++ app/javascript/src/apps/mydb/elements/list/selectionActions/SelectionShareModal.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Form, Modal } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import AppModal from 'src/components/common/AppModal';
 import { AsyncSelect } from 'src/components/common/Select';
 
 import SelectionSharingShortcuts from 'src/apps/mydb/elements/list/selectionActions/SelectionSharingShortcuts';
@@ -18,7 +19,7 @@ const defaultProps = {
   title: 'Sharing of Elements',
   collectionId: null,
   collectionShareId: null,
-  shareType: "new",
+  shareType: 'new',
   collectionPermissions: {
     permissionLevel: 0,
     sampleDetailLevel: 0,
@@ -30,9 +31,15 @@ const defaultProps = {
   showUserSelect: true,
 };
 
-const SelectionShareModal = (props) => {
-  const { title, collectionId, collectionShareId, shareType, collectionPermissions, showUserSelect, onHide } = { ...defaultProps, ...props }
-
+function SelectionShareModal({
+  title,
+  collectionId,
+  collectionShareId,
+  shareType,
+  collectionPermissions,
+  showUserSelect,
+  onHide,
+}) {
   const collectionsStore = useContext(StoreContext).collections;
   const [permissions, setPermissions] = useState(collectionPermissions);
   const [selectedUsers, setSelectedUsers] = useState([]);
@@ -42,8 +49,8 @@ const SelectionShareModal = (props) => {
   const currentUser = (UserStore.getState() && UserStore.getState().currentUser) || {};
   const uiState = UIStore.getState();
   const displayWarning = permissions.permissionLevel === '5';
-  const canSubmit = !showUserSelect || selectedUsers != null && selectedUsers.length > 0
-  const submitTitle = shareType == 'edit' ? 'Edit Permissions' : 'Create Shared Collection';
+  const canSubmit = !showUserSelect || (selectedUsers != null && selectedUsers.length > 0);
+  const submitTitle = shareType === 'edit' ? 'Edit Permissions' : 'Create Shared Collection';
 
   const handleSharing = () => {
     const permissionsParams = {
@@ -57,7 +64,7 @@ const SelectionShareModal = (props) => {
       devicedescription_detail_level: 10,
       sequencebasedmacromoleculesample_detail_level: 10,
       researchplan_detail_level: 10,
-    }
+    };
 
     if (shareType === 'new') {
       // create new collection with shared elements
@@ -65,33 +72,32 @@ const SelectionShareModal = (props) => {
         uiState: filterParamsFromUIState(uiState),
         users: selectedUsers,
         permissions: permissionsParams,
-        currentUser: currentUser,
+        currentUser,
       };
-      collectionsStore.collectionShareForElements(params)
-
+      collectionsStore.collectionShareForElements(params);
     } else if (shareType === 'create') {
       // create collection share of collectionId
       const params = {
         collection_id: collectionId,
-        user_ids: selectedUsers.map(u => u.id),
+        user_ids: selectedUsers.map((u) => u.id),
         ...permissionsParams
-      }
+      };
       collectionsStore.addCollectionShare(params, currentUser, false);
     } else if (shareType === 'edit') {
       // edit permissions of collection share
       const params = {
         id: collectionShareId,
         ...permissionsParams
-      }
-      collectionsStore.updateCollectionShare(collectionShareId, params)
+      };
+      collectionsStore.updateCollectionShare(collectionShareId, params);
     }
 
     onHide();
-  }
+  };
 
   const handleShortcutChange = (e) => {
-    let val = e.target.value
-    let permissionsAndDetailLevel = {}
+    const val = e.target.value;
+    let permissionsAndDetailLevel = {};
 
     switch (val) {
       case 'user':
@@ -110,31 +116,31 @@ const SelectionShareModal = (props) => {
         permissionsAndDetailLevel = SelectionSharingShortcuts.supervisor();
         break;
       default:
-        permAndDetLevs = {};
+        permissionsAndDetailLevel = {};
     }
-    permissionsAndDetailLevel['elementDetailLevel'] = permissions.elementDetailLevel;
+    permissionsAndDetailLevel.elementDetailLevel = permissions.elementDetailLevel;
     setPermissions(permissionsAndDetailLevel);
     setRole(val);
-  }
+  };
 
   const handlePermissionLevelChange = (e) => {
-    let val = e.target.value;
+    const val = e.target.value;
     setPermissions({ ...permissions, permissionLevel: val });
     setRole(defaultRole);
-  }
+  };
 
   const handleDetailLevelChange = (e, elementType) => {
-    let val = e.target.value
+    const val = e.target.value;
     setPermissions({ ...permissions, [`${elementType}DetailLevel`]: val });
     setRole(defaultRole);
-  }
+  };
 
   const handleSelectUser = (val) => {
     setSelectedUsers(val);
-  }
+  };
 
   const loadUserByName = (input) => {
-    if (!input) { return Promise.resolve([]) }
+    if (!input) { return Promise.resolve([]); }
 
     return UsersFetcher.fetchUsersByName(input, 'Person,Group')
       .then((res) => selectUserOptionFormater(
@@ -142,7 +148,7 @@ const SelectionShareModal = (props) => {
       )).catch((errorMessage) => {
         console.log(errorMessage);
       });
-  }
+  };
 
   const userSelect = () => {
     if (!showUserSelect) return null;
@@ -160,112 +166,115 @@ const SelectionShareModal = (props) => {
         />
       </Form.Group>
     );
-  }
+  };
 
   return (
-    <Modal show centered onHide={onHide}>
-      <Modal.Header closeButton>
-        <Modal.Title>{title}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Group className="mb-3" controlId="shortcutSelect">
-            <Form.Label>Role</Form.Label>
-            <Form.Select
-              placeholder="Pick a sharing role (optional)"
-              value={role || ''}
-              onChange={(e) => handleShortcutChange(e)}>
-              <option value='Pick a sharing role'>Pick a sharing role (optional)</option>
-              <option value='user'>User</option>
-              <option value='partner'>Partner</option>
-              <option value='collaborator'>Collaborator</option>
-              <option value='reviewer'>Reviewer</option>
-              <option value='supervisor'>Supervisor</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="permissionLevelSelect" id="permissionLevelSelect">
-            <Form.Label>Permission level</Form.Label>
-            <Form.Select
-              onChange={(e) => handlePermissionLevelChange(e)}
-              value={permissions.permissionLevel || ''}>
-              <option value='0'>Read</option>
-              <option value='1'>Write</option>
-              <option value='2'>Share</option>
-              <option value='3'>Delete</option>
-              <option value='4'>Import Elements</option>
-              <option value='5'>Pass ownership</option>
-            </Form.Select>
-            {displayWarning && (
-              <Form.Text>
-                <i className="fa fa-exclamation-circle ms-1" aria-hidden="true" />
-                Transfering ownership applies for all sub collections.
-              </Form.Text>
-            )}
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="sampleDetailLevelSelect">
-            <Form.Label>Sample detail level</Form.Label>
-            <Form.Select
-              onChange={(e) => handleDetailLevelChange(e, 'sample')}
-              value={permissions.sampleDetailLevel || ''}>
-              <option value='0'>Molecular mass of the compound, external label</option>
-              <option value='1'>Molecule, structure</option>
-              <option value='2'>Analysis Result + Description</option>
-              <option value='3'>Analysis Datasets</option>
-              <option value='10'>Everything</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="reactionDetailLevelSelect">
-            <Form.Label>Reaction detail level</Form.Label>
-            <Form.Select
-              onChange={(e) => handleDetailLevelChange(e, 'reaction')}
-              value={permissions.reactionDetailLevel || ''}>
-              <option value='0'>Observation, description, calculation</option>
-              <option value='10'>Everything</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="wellplateDetailLevelSelect">
-            <Form.Label>Wellplate detail level</Form.Label>
-            <Form.Select
-              onChange={(e) => handleDetailLevelChange(e, 'wellplate')}
-              value={permissions.wellplateDetailLevel || ''}>
-              <option value='0'>Wells (Positions)</option>
-              <option value='1'>Readout</option>
-              <option value='10'>Everything</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="screenDetailLevelSelect">
-            <Form.Label>Screen detail level</Form.Label>
-            <Form.Select
-              onChange={(e) => handleDetailLevelChange(e, 'screen')}
-              value={permissions.screenDetailLevel || ''}>
-              <option value='0'>Name, description, condition, requirements</option>
-              <option value='10'>Everything</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="elementDetailLevelSelect">
-            <Form.Label>Element detail level</Form.Label>
-            <Form.Select
-              onChange={(e) => handleDetailLevelChange(e, 'element')}
-              value={permissions.elementDetailLevel || ''}>
-              <option value='10'>Everything</option>
-            </Form.Select>
-          </Form.Group>
-          {userSelect()}
-          <Button
-            id="create-sync-shared-col-btn"
-            variant="warning"
-            disabled={!canSubmit}
-            onClick={handleSharing}
+    <AppModal
+      show
+      onHide={onHide}
+      title={title}
+      primaryActionLabel={submitTitle}
+      onPrimaryAction={handleSharing}
+      primaryActionDisabled={!canSubmit}
+    >
+      <Form>
+        <Form.Group className="mb-3" controlId="shortcutSelect">
+          <Form.Label>Role</Form.Label>
+          <Form.Select
+            placeholder="Pick a sharing role (optional)"
+            value={role || ''}
+            onChange={(e) => handleShortcutChange(e)}
           >
-            {submitTitle}
-          </Button>
-        </Form>
-      </Modal.Body>
-    </Modal>
+            <option value="Pick a sharing role">Pick a sharing role (optional)</option>
+            <option value="user">User</option>
+            <option value="partner">Partner</option>
+            <option value="collaborator">Collaborator</option>
+            <option value="reviewer">Reviewer</option>
+            <option value="supervisor">Supervisor</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="permissionLevelSelect" id="permissionLevelSelect">
+          <Form.Label>Permission level</Form.Label>
+          <Form.Select
+            onChange={(e) => handlePermissionLevelChange(e)}
+            value={permissions.permissionLevel || ''}
+          >
+            <option value="0">Read</option>
+            <option value="1">Write</option>
+            <option value="2">Share</option>
+            <option value="3">Delete</option>
+            <option value="4">Import Elements</option>
+            <option value="5">Pass ownership</option>
+          </Form.Select>
+          {displayWarning && (
+            <Form.Text>
+              <i className="fa fa-exclamation-circle ms-1" aria-hidden="true" />
+              Transfering ownership applies for all sub collections.
+            </Form.Text>
+          )}
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="sampleDetailLevelSelect">
+          <Form.Label>Sample detail level</Form.Label>
+          <Form.Select
+            onChange={(e) => handleDetailLevelChange(e, 'sample')}
+            value={permissions.sampleDetailLevel || ''}
+          >
+            <option value="0">Molecular mass of the compound, external label</option>
+            <option value="1">Molecule, structure</option>
+            <option value="2">Analysis Result + Description</option>
+            <option value="3">Analysis Datasets</option>
+            <option value="10">Everything</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="reactionDetailLevelSelect">
+          <Form.Label>Reaction detail level</Form.Label>
+          <Form.Select
+            onChange={(e) => handleDetailLevelChange(e, 'reaction')}
+            value={permissions.reactionDetailLevel || ''}
+          >
+            <option value="0">Observation, description, calculation</option>
+            <option value="10">Everything</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="wellplateDetailLevelSelect">
+          <Form.Label>Wellplate detail level</Form.Label>
+          <Form.Select
+            onChange={(e) => handleDetailLevelChange(e, 'wellplate')}
+            value={permissions.wellplateDetailLevel || ''}
+          >
+            <option value="0">Wells (Positions)</option>
+            <option value="1">Readout</option>
+            <option value="10">Everything</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="screenDetailLevelSelect">
+          <Form.Label>Screen detail level</Form.Label>
+          <Form.Select
+            onChange={(e) => handleDetailLevelChange(e, 'screen')}
+            value={permissions.screenDetailLevel || ''}
+          >
+            <option value="0">Name, description, condition, requirements</option>
+            <option value="10">Everything</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="elementDetailLevelSelect">
+          <Form.Label>Element detail level</Form.Label>
+          <Form.Select
+            onChange={(e) => handleDetailLevelChange(e, 'element')}
+            value={permissions.elementDetailLevel || ''}
+          >
+            <option value="10">Everything</option>
+          </Form.Select>
+        </Form.Group>
+        {userSelect()}
+      </Form>
+    </AppModal>
   );
 }
 
 export default observer(SelectionShareModal);
+
+SelectionShareModal.defaultProps = defaultProps;
 
 SelectionShareModal.propTypes = {
   title: PropTypes.string,
