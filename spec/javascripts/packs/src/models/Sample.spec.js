1a357322251a797d01598fd95203f465354409fd
Tasnim Mehzabin <tasnim.mehzabin@live.com>
2026-04-23 14:59:01 +0200

    fix: update preserveConcentration handling in ReactionDetailsScheme and Sample
    - Changed `preserveConcentration` from deletion to setting it to false in `ReactionDetailsScheme.js` to ensure proper concentration recalculation.
    - Updated corresponding test in `Sample.spec.js` to reflect the new handling of `preserveConcentration` during solvent updates.

--- spec/javascripts/packs/src/models/Sample.spec.js
+++ spec/javascripts/packs/src/models/Sample.spec.js
@@ -1450,7 +1450,7 @@ describe('Sample', async () => {
       sample.updateConcentrationFromSolvent(reaction);
       expect(sample.concn).toBe(1.23);
 
-      delete sample.preserveConcentration;
+      sample.preserveConcentration = false;
       sample.updateConcentrationFromSolvent(reaction);
 
       expect(sample.concn).toBeCloseTo(0.2, 5); // 0.1 mol / 0.5 L = 0.2 mol/L
