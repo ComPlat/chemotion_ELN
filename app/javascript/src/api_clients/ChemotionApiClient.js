5aec0fa10a3d795cf73d95f7a222c090801a5bed
Johannes Haubold <jh@megorei.com>
2026-04-30 13:18:16 +0200

    Use ESLint compatible code formatting

--- app/javascript/src/api_clients/ChemotionApiClient.js
+++ app/javascript/src/api_clients/ChemotionApiClient.js
@@ -2,24 +2,24 @@
 // import 'whatwg-fetch';
 
 // default functions for 90% of use cases
-const get_json = (api_endpoint, options = {}) => {
+const getJson = (apiEndpoint, options = {}) => {
   const defaults = { method: 'GET' }
   
-  return api_request(api_endpoint, { ...defaults, ...options })
+  return apiRequest(apiEndpoint, { ...defaults, ...options })
 }
 
-const put_json = (api_endpoint, options) => {
+const putJson = (apiEndpoint, options) => {
   const defaults = { method: 'PUT' }
 
   if (typeof options.body != 'string') {
     options.body = JSON.stringify(options.body)
   }
 
-  return api_request(api_endpoint, { ...defaults, ...options })
+  return apiRequest(apiEndpoint, { ...defaults, ...options })
 }
 
 // this function assumes the body is already a form data object and does not try to typecast it
-const put_form_data = (api_endpoint, options) => {
+const putFormData = (apiEndpoint, options) => {
   const defaults = {
     method: 'PUT',
     headers: {
@@ -27,21 +27,21 @@ const put_form_data = (api_endpoint, options) => {
     }
   }
 
-  return api_request(api_endpoint, { ...defaults, ...options })
+  return apiRequest(apiEndpoint, { ...defaults, ...options })
 }
 
-const post_json = (api_endpoint, options) => {
+const postJson = (apiEndpoint, options) => {
   const defaults = { method: 'POST' }
 
   if (typeof options.body != 'string') {
     options.body = JSON.stringify(options.body)
   }
 
-  return api_request(api_endpoint, { ...defaults, ...options })
+  return apiRequest(apiEndpoint, { ...defaults, ...options })
 }
 
 // this function assumes the body is already a form data object and does not try to typecast it
-const post_form_data = (api_endpoint, options) => {
+const postFormData = (apiEndpoint, options) => {
   const defaults = {
     method: 'POST',
     headers: {
@@ -49,39 +49,39 @@ const post_form_data = (api_endpoint, options) => {
     }
   }
 
-  return api_request(api_endpoint, { ...defaults, ...options })
+  return apiRequest(apiEndpoint, { ...defaults, ...options })
 }
 
-const delete_request = (api_endpoint, options = {}) => {
+const deleteRequest = (apiEndpoint, options = {}) => {
   const defaults = { method: 'DELETE' }
-  return api_request(api_endpoint, { ...defaults, ...options })
+  return apiRequest(apiEndpoint, { ...defaults, ...options })
 }
 
 // most low level request, which actually calls fetch
-const api_request = (api_endpoint, options) => {
-  const global_defaults = {
+const apiRequest = (apiEndpoint, options) => {
+  const globalDefaults = {
     credentials: 'same-origin',
-    handle_response_success: (response) => { return response.json() },
-    handle_response_error: (exception) => { console.log(exception) },
+    handleResponseSuccess: (response) => { return response.json() },
+    handleResponseError: (exception) => { console.log(exception) },
     headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }
   }
 
-  options = { ...global_defaults, ...options }
-  const { handle_response_success, handle_response_error } = options
+  options = { ...globalDefaults, ...options }
+  const { handleResponseSuccess, handleResponseError } = options
 
-  return fetch(api_endpoint, options)
-          .then(handle_response_success)
-          .catch(handle_response_error)
+  return fetch(apiEndpoint, options)
+          .then(handleResponseSuccess)
+          .catch(handleResponseError)
 }
 
 const ChemotionApiClient = {
-  api_request,
-  get_json,
-  put_json,
-  put_form_data,
-  post_json,
-  post_form_data,
-  delete_request
+  apiRequest,
+  getJson,
+  putJson,
+  putFormData,
+  postJson,
+  postFormData,
+  deleteRequest
 }
 
 export default ChemotionApiClient
--- app/javascript/src/fetchers/CollectionsFetcher.js
+++ app/javascript/src/fetchers/CollectionsFetcher.js
@@ -2,42 +2,42 @@ import ApiClient from 'src/api_clients/ChemotionApiClient';
 
 export default class CollectionsFetcher {
   static fetchCollections() {
-    return ApiClient.get_json('/api/v1/collections')
+    return ApiClient.getJson('/api/v1/collections')
   }
 
   static fetchByCollectionId(collectionId) {
-    return ApiClient.get_json(`/api/v1/collections/${collectionId}`)
+    return ApiClient.getJson(`/api/v1/collections/${collectionId}`)
                     .then(json => json.collection)
   }
 
   static addCollection(params) {
-    return ApiClient.post_json('/api/v1/collections', { body: params })
+    return ApiClient.postJson('/api/v1/collections', { body: params })
                     .then(json => json.collection)
   }
 
   static buldUpdateForOwnCollections(params) {
-    return ApiClient.post_json('/api/v1/collections/bulk_update_own_collections', { body: params })
+    return ApiClient.postJson('/api/v1/collections/bulk_update_own_collections', { body: params })
                     .then(json => json.collections)
   }
 
   static updateCollection(collectionId, params) {
-    return ApiClient.put_json(`/api/v1/collections/${collectionId}`, { body: params })
+    return ApiClient.putJson(`/api/v1/collections/${collectionId}`, { body: params })
                     .then(json => json.collection)
   }
 
   static deleteCollection(collectionId) {
-    return ApiClient.delete_request(`/api/v1/collections/${collectionId}`)
+    return ApiClient.deleteRequest(`/api/v1/collections/${collectionId}`)
                     .then(json => json.collections)
   }
 
   static exportCollections(collectionIds) {
-    return ApiClient.post_json('/api/v1/collections/export', { body: collectionIds })
+    return ApiClient.postJson('/api/v1/collections/export', { body: collectionIds })
   }
 
   static importCollections(params) {
     const data = new FormData();
     data.append('file', params.file);
 
-    return ApiClient.post_form_data('/api/v1/collections/import/', { body: data })
+    return ApiClient.postFormData('/api/v1/collections/import/', { body: data })
   }
 }
