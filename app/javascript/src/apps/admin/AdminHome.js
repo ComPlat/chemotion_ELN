3ad86b2e3cb701e3f898c8a8fdd5c3982d60d8bf
Adrian Herrmann <headri@web.de>
2026-04-30 07:53:46 +0000

    refactor language handling & switch

--- app/javascript/src/apps/admin/AdminHome.js
+++ app/javascript/src/apps/admin/AdminHome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Row, Col, Nav, NavItem, Container, Button, InputGroup
+  Row, Col, Nav, NavItem, Container, Dropdown
 } from 'react-bootstrap';
 import BaseNavigation from 'src/components/navigation/BaseNavigation';
 import Notifications from 'src/components/Notifications';
@@ -19,16 +19,19 @@ import ThirdPartyApp from 'src/apps/admin/ThirdPartyApp';
 import { IntlProvider, FormattedMessage } from 'react-intl';
 import I18NFetcher from 'src/fetchers/I18NFetcher';
 
+const LOCALES = ['en', 'de'];
+const DEFAULT_LOCALE = 'en';
+
 class AdminHome extends React.Component {
   constructor(props) {
     super();
     this.state = {
       pageIndex: 0,
-      locale: 'en',
-      messagesEN: null,
-      messagesDE: null,
+      locale: DEFAULT_LOCALE,
+      messages: null,
     };
     this.handleSelect = this.handleSelect.bind(this);
+    this.handleLocaleSelect = this.handleLocaleSelect.bind(this);
   }
 
   componentDidMount() {
@@ -41,28 +44,37 @@ class AdminHome extends React.Component {
     });
   }
 
+  handleLocaleSelect(locale) {
+    if (locale) this.setState({ locale });
+  }
+
   getMessages() {
-    const { locale, messagesEN, messagesDE } = this.state;
+    const { locale, messages } = this.state;
+    if (!messages) return null;
+    const fallback = messages[DEFAULT_LOCALE] || {};
+    if (locale === DEFAULT_LOCALE) return fallback;
+    return { ...fallback, ...(messages[locale] || {}) };
+  }
 
-    const locales = {
-      en: messagesEN,
-      de: messagesDE,
-    };
-
-    return { ...messagesEN, ...(locales[locale] || {}) };
+  getLanguageName(code) {
+    const { messages } = this.state;
+    return (messages && messages[code] && messages[code][`language-${code}`]) || code;
   }
 
   fetchMessages() {
-    Promise.all([
-      I18NFetcher.fetchAdminMessages('en'),
-      I18NFetcher.fetchAdminMessages('de')
-    ])
-      .then(([EN, DE]) => {
-        this.setState({
-          messagesEN: EN,
-          messagesDE: DE
-        });
+    const requests = LOCALES.flatMap((locale) => [
+      I18NFetcher.fetchGeneralMessages(locale),
+      I18NFetcher.fetchAdminMessages(locale),
+    ]);
+    Promise.all(requests).then((results) => {
+      const messages = {};
+      LOCALES.forEach((locale, index) => {
+        const general = results[index * 2];
+        const admin = results[index * 2 + 1];
+        messages[locale] = { ...general, ...admin };
       });
+      this.setState({ messages });
+    });
   }
 
   mainContent() {
@@ -84,76 +96,93 @@ class AdminHome extends React.Component {
     }
   }
 
+  renderLocaleSwitch() {
+    const { locale } = this.state;
+    return (
+      <div className="d-grid">
+        <Dropdown onSelect={this.handleLocaleSelect}>
+          <Dropdown.Toggle variant="primary" className="w-100">
+            {this.getLanguageName(locale)}
+          </Dropdown.Toggle>
+          <Dropdown.Menu className="w-100">
+            {LOCALES.map((code) => (
+              <Dropdown.Item key={code} eventKey={code} active={code === locale}>
+                {this.getLanguageName(code)}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    );
+  }
+
   renderTree() {
     const { pageIndex } = this.state;
 
     return (
-      <Nav className="flex-column fs-5 gap-3 mt-2" variant="pills" activeKey={pageIndex} onSelect={this.handleSelect}>
-        <NavItem>
-          <InputGroup>
-            <Button onClick={() => this.setState({ locale: 'en' })}>English</Button>
-            <Button onClick={() => this.setState({ locale: 'de' })}>Deutsch</Button>
-          </InputGroup>
-        </NavItem>
-        <NavItem>
-          <Nav.Link eventKey={0}>
-            <FormattedMessage id="navigation-dashboard" />
-          </Nav.Link>
-        </NavItem>
-        <NavItem>
-          <Nav.Link eventKey={1}>
-            <FormattedMessage id="navigation-user_management" />
-          </Nav.Link>
-        </NavItem>
-        <NavItem>
-          <Nav.Link eventKey={9}>
-            <FormattedMessage id="navigation-devices" />
-          </Nav.Link>
-        </NavItem>
-        <NavItem>
-          <Nav.Link eventKey={4}>
-            <FormattedMessage id="navigation-groups" />
-          </Nav.Link>
-        </NavItem>
-        <NavItem>
-          <Nav.Link eventKey={7}>
-            <FormattedMessage id="navigation-UI_features" />
-          </Nav.Link>
-        </NavItem>
-        <NavItem>
-          <Nav.Link eventKey={8}>
-            <FormattedMessage id="navigation-text_templates" />
-          </Nav.Link>
-        </NavItem>
-        <NavItem>
-          <Nav.Link eventKey={2}>
-            <FormattedMessage id="navigation-message_publish" />
-          </Nav.Link>
-        </NavItem>
-        <NavItem>
-          <Nav.Link eventKey={5}>
-            <FormattedMessage id="navigation-load_OLS_terms" />
-          </Nav.Link>
-        </NavItem>
-        {/* <NavItem>
-      <Nav.Link eventKey={12}>Report-template Management</Nav.Link>
-    </NavItem> */}
-        <NavItem>
-          <Nav.Link eventKey={13}>
-            <FormattedMessage id="navigation-delayed_jobs" />
-          </Nav.Link>
-        </NavItem>
-        <NavItem>
-          <Nav.Link eventKey={14}>
-            <FormattedMessage id="navigation-ChemSpectra_layouts" />
-          </Nav.Link>
-        </NavItem>
-        <NavItem>
-          <Nav.Link eventKey={15}>
-            <FormattedMessage id="navigation-third_party_apps" />
-          </Nav.Link>
-        </NavItem>
-      </Nav>
+      <div className="d-flex flex-column gap-3 mt-2">
+        {this.renderLocaleSwitch()}
+        <Nav className="flex-column fs-5 gap-3" variant="pills" activeKey={pageIndex} onSelect={this.handleSelect}>
+          <NavItem>
+            <Nav.Link eventKey={0}>
+              <FormattedMessage id="navigation-dashboard" />
+            </Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link eventKey={1}>
+              <FormattedMessage id="navigation-user_management" />
+            </Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link eventKey={9}>
+              <FormattedMessage id="navigation-devices" />
+            </Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link eventKey={4}>
+              <FormattedMessage id="navigation-groups" />
+            </Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link eventKey={7}>
+              <FormattedMessage id="navigation-UI_features" />
+            </Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link eventKey={8}>
+              <FormattedMessage id="navigation-text_templates" />
+            </Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link eventKey={2}>
+              <FormattedMessage id="navigation-message_publish" />
+            </Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link eventKey={5}>
+              <FormattedMessage id="navigation-load_OLS_terms" />
+            </Nav.Link>
+          </NavItem>
+          {/* <NavItem>
+            <Nav.Link eventKey={12}>Report-template Management</Nav.Link>
+          </NavItem> */}
+          <NavItem>
+            <Nav.Link eventKey={13}>
+              <FormattedMessage id="navigation-delayed_jobs" />
+            </Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link eventKey={14}>
+              <FormattedMessage id="navigation-ChemSpectra_layouts" />
+            </Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link eventKey={15}>
+              <FormattedMessage id="navigation-third_party_apps" />
+            </Nav.Link>
+          </NavItem>
+        </Nav>
+      </div>
     );
   }
 
@@ -164,7 +193,7 @@ class AdminHome extends React.Component {
       return null;
     }
     return (
-      <IntlProvider messages={messages} locale={locale} defaultLocale="en">
+      <IntlProvider messages={messages} locale={locale} defaultLocale={DEFAULT_LOCALE}>
         <div>
           <Container fluid>
             <Row className="mb-3">
--- app/javascript/src/fetchers/I18NFetcher.js
+++ app/javascript/src/fetchers/I18NFetcher.js
@@ -1,15 +1,21 @@
+const fetchMessages = (path) => fetch(path)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    console.error('Error fetching JSON:', error);
+    return {};
+  });
+
 export default class I18NFetcher {
+  static fetchGeneralMessages(locale) {
+    return fetchMessages(`/i18n/${locale}.json`);
+  }
+
   static fetchAdminMessages(locale) {
-    return fetch(`/i18n/admin/${locale}.json`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        console.error('Error fetching JSON:', error);
-        return {};
-      });
+    return fetchMessages(`/i18n/admin/${locale}.json`);
   }
 }
