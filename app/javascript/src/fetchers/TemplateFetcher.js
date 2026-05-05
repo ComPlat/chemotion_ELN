442198778f6cac0a94568bc998e315278eaa2c56
Beate Quednau <bq@megorei.com>
2026-05-05 14:08:26 +0200

    Rework TemplateFetcher using ChemotionApiClient

--- app/javascript/src/fetchers/TemplateFetcher.js
+++ app/javascript/src/fetchers/TemplateFetcher.js
@@ -1,17 +1,14 @@
-import 'whatwg-fetch';
+import ApiClient from 'src/api_clients/ChemotionApiClient';
 
 export default class TemplateFetcher {
   static commonTemplates() {
-    return fetch('/json/ketcher_common_templates.json', {
-      credentials: 'same-origin',
-    })
-      .then((response) => {
-        if (!response.ok) throw new Error('Failed to load common templates');
+    return ApiClient.getJson('/json/ketcher_common_templates', {
+      handleResponseSuccess: (response) => {
+        if (response.ok === false) {
+          throw new Error('Failed to load common templates');
+        }
         return response.json();
-      })
-      .catch((error) => {
-        console.error('Error loading templates:', error);
-        return [];
-      });
+      }
+    });
   }
 }
