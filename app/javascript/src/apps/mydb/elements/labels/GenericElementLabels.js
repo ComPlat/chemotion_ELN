83dbf58174edaa00bcabac14526c72fa7a2e7b23
Beate Quednau <bq@megorei.com>
2026-07-17 12:32:45 +0200

    Replace UserStore at element labels

--- app/javascript/src/apps/mydb/elements/labels/GenericElementLabels.js
+++ app/javascript/src/apps/mydb/elements/labels/GenericElementLabels.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 import ElementActions from 'src/stores/alt/actions/ElementActions';
-import UserStore from 'src/stores/alt/stores/UserStore';
+import { StoreContext } from 'src/stores/mobx/RootStore';
 import ElementIcon from 'src/components/common/ElementIcon';
 import ElementNoAccessTrigger from 'src/apps/mydb/elements/labels/ElementNoAccessTrigger';
 
-function GenericElementLabels({ element }) {
+const GenericElementLabels = ({ element }) => {
+  const { userStore } = useContext(StoreContext);
   if (!element.tag?.taggable_data?.element) {
     return null;
   }
 
   const elInfo = element.tag.taggable_data.element;
-  const klasses = UserStore.getState().genericEls;
+  const { klasses } = userStore.genericEls;
   const klass = (klasses && klasses.find((currentElement) => currentElement.name === elInfo.type)) || {};
   const iconElement = { ...klass, type: elInfo.type };
 
@@ -31,7 +32,7 @@ function GenericElementLabels({ element }) {
       warningMessage="Sorry, you cannot access this Element."
     />
   );
-}
+};
 
 GenericElementLabels.propTypes = {
   element: PropTypes.shape({
