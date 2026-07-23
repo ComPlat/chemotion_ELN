1b0b5682e851ee66b9dbc97b818d404b7abac843
Beate Quednau <bq@megorei.com>
2026-07-23 13:39:49 +0200

    Replace UserStore at ComputedPropsGraphContainer

--- app/javascript/src/components/computedProps/ComputedPropsGraphContainer.js
+++ app/javascript/src/components/computedProps/ComputedPropsGraphContainer.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {
+  forwardRef, useContext, useImperativeHandle, useState
+} from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 
 import { Select, CreatableSelect } from 'src/components/common/Select';
 import {
   Row, Col, Form, Container
 } from 'react-bootstrap';
 
-import UserStore from 'src/stores/alt/stores/UserStore';
-import UserActions from 'src/stores/alt/actions/UserActions';
+import { StoreContext } from 'src/stores/mobx/RootStore';
 
 import ComputedPropsGraph from 'src/components/computedProps/ComputedPropsGraph';
 import GraphReferenceTable from 'src/components/computedProps/GraphReferenceTable';
@@ -40,118 +40,58 @@ const graphSettings = {
   dipol_debye: { label: 'Dipol', unit: 'debye', range: [0, 14] },
 };
 
-export default class ComputedPropsGraphContainer extends React.Component {
-  constructor(props) {
-    super(props);
+const ComputedPropsGraphContainer = forwardRef(({ show, graphData }, ref) => {
+  const { userStore } = useContext(StoreContext);
+  const computedProps = userStore.profile?.data?.computed_props;
 
-    this.state = {
-      curTemplateIdx: 0,
-      graphTemplates: [],
-      templateInputValue: '',
-    };
+  const [graphTemplates, setGraphTemplates] = useState(
+    () => computedProps?.graph_templates ?? [defaultTemplate]
+  );
+  const [curTemplateIdx, setCurTemplateIdx] = useState(
+    () => computedProps?.cur_template_idx ?? 0
+  );
+  const [templateInputValue, setTemplateInputValue] = useState(
+    () => graphTemplates[curTemplateIdx]?.name || ''
+  );
 
-    this.onUserChange = this.onUserChange.bind(this);
-    this.onTemplateChange = this.onTemplateChange.bind(this);
-    this.onXAxisChange = this.onXAxisChange.bind(this);
-    this.onYAxisChange = this.onYAxisChange.bind(this);
-
-    this.updateReferences = this.updateReferences.bind(this);
-    this.saveTemplate = this.saveTemplate.bind(this);
-    this.deleteTemplate = this.deleteTemplate.bind(this);
-    this.onDescChange = this.onDescChange.bind(this);
+  // Sync templateInputValue when the selected template changes, without
+  // resetting it while the user is typing a new template name.
+  const currentTemplateName = graphTemplates[curTemplateIdx]?.name || '';
+  const [prevTemplateName, setPrevTemplateName] = useState(currentTemplateName);
+  if (currentTemplateName !== prevTemplateName) {
+    setPrevTemplateName(currentTemplateName);
+    setTemplateInputValue(currentTemplateName);
   }
 
-  componentDidMount() {
-    UserStore.listen(this.onUserChange);
-    this.onUserChange(UserStore.getState());
-  }
+  const onXAxisChange = (xAxis) => {
+    setGraphTemplates(graphTemplates.map((templ, idx) => (
+      idx === curTemplateIdx ? { ...templ, xAxisType: xAxis.value } : templ
+    )));
+  };
 
-  componentDidUpdate(prevProps, prevState) {
-  // Sync templateInputValue when template changes
-    const { curTemplateIdx, graphTemplates } = this.state;
-    const currentTemplate = graphTemplates[curTemplateIdx];
-    const prevTemplate = prevState.graphTemplates[prevState.curTemplateIdx];
+  const onYAxisChange = (yAxis) => {
+    setGraphTemplates(graphTemplates.map((templ, idx) => (
+      idx === curTemplateIdx ? { ...templ, yAxisType: yAxis.value } : templ
+    )));
+  };
 
-    if (currentTemplate?.name !== prevTemplate?.name) {
-      this.setState({
-        templateInputValue: currentTemplate?.name || ''
-      });
-    }
-  }
-
-  componentWillUnmount() {
-    UserStore.unlisten(this.onUserChange);
-  }
-
-  onUserChange(userState) {
-    const profileData = _.get(userState, 'profile.data', {});
-    const graphTemplates = _.get(
-      profileData,
-      'computed_props.graph_templates',
-      [defaultTemplate]
-    );
-
-    const curTemplateIdx = _.get(profileData, 'computed_props.cur_template_idx', 0);
-    const currentTemplate = graphTemplates[curTemplateIdx];
-
-    this.setState({ graphTemplates, curTemplateIdx, templateInputValue: currentTemplate?.name || '' });
-  }
-
-  onXAxisChange(xAxis) {
-    const { curTemplateIdx, graphTemplates } = this.state;
-    const newGraphTemplates = graphTemplates.map((templ, idx) => {
-      if (idx === curTemplateIdx) {
-        const newTempl = _.cloneDeep(templ);
-        newTempl.xAxisType = xAxis.value;
-        return newTempl;
-      }
-      return templ;
-    });
-
-    this.setState({ graphTemplates: newGraphTemplates });
-  }
-
-  onYAxisChange(yAxis) {
-    const { curTemplateIdx, graphTemplates } = this.state;
-    const newGraphTemplates = graphTemplates.map((templ, idx) => {
-      if (idx === curTemplateIdx) {
-        const newTempl = _.cloneDeep(templ);
-        newTempl.yAxisType = yAxis.value;
-        return newTempl;
-      }
-      return templ;
-    });
-
-    this.setState({ graphTemplates: newGraphTemplates });
-  }
-
-  onDescChange(e) {
-    const { curTemplateIdx, graphTemplates } = this.state;
+  const onDescChange = (e) => {
     const desc = e.target.value;
-    const newGraphTemplates = graphTemplates.map((templ, idx) => {
-      if (idx === curTemplateIdx) {
-        const newTempl = _.cloneDeep(templ);
-        newTempl.referenceDesc = desc;
-        return newTempl;
-      }
-      return templ;
-    });
+    setGraphTemplates(graphTemplates.map((templ, idx) => (
+      idx === curTemplateIdx ? { ...templ, referenceDesc: desc } : templ
+    )));
+  };
 
-    this.setState({ graphTemplates: newGraphTemplates });
-  }
-
-  onTemplateChange(template) {
+  const onTemplateChange = (template) => {
     if (!template) {
-      this.setState({ templateInputValue: '' });
+      setTemplateInputValue('');
       return;
     }
-    const { graphTemplates } = this.state;
+
     const tIdx = graphTemplates.findIndex((t) => t.name === template.label);
     if (tIdx > -1) {
-      this.setState({
-        curTemplateIdx: tIdx,
-        templateInputValue: template.label
-      });
+      setCurTemplateIdx(tIdx);
+      setTemplateInputValue(template.label);
     } else {
       const newTempl = {
         name: template.label,
@@ -160,170 +100,173 @@ export default class ComputedPropsGraphContainer extends React.Component {
         referenceDesc: '',
         referencePoints: [],
       };
+      const newGraphTemplates = [...graphTemplates, newTempl];
 
-      graphTemplates.push(newTempl);
-      this.setState({
-        graphTemplates,
-        curTemplateIdx: graphTemplates.length - 1,
-        templateInputValue: template.label
-      });
+      setGraphTemplates(newGraphTemplates);
+      setCurTemplateIdx(newGraphTemplates.length - 1);
+      setTemplateInputValue(template.label);
     }
-  }
+  };
 
-  updateReferences(refs) {
-    const { curTemplateIdx, graphTemplates } = this.state;
-    const newGraphTemplates = graphTemplates.map((templ, idx) => {
-      if (idx === curTemplateIdx) {
-        const newTempl = _.cloneDeep(templ);
-        newTempl.referencePoints = refs;
-        return newTempl;
-      }
-      return templ;
+  const updateReferences = (refs) => {
+    setGraphTemplates(graphTemplates.map((templ, idx) => (
+      idx === curTemplateIdx ? { ...templ, referencePoints: refs } : templ
+    )));
+  };
+
+  const saveTemplate = () => {
+    userStore.updateUserProfileValues({
+      ...userStore.profile,
+      data: {
+        computed_props: {
+          graph_templates: graphTemplates,
+          cur_template_idx: curTemplateIdx,
+        },
+      },
     });
+  };
 
-    this.setState({ graphTemplates: newGraphTemplates });
+  const deleteTemplate = () => {
+    const newGraphTemplates = graphTemplates.filter((_templ, idx) => idx !== curTemplateIdx);
+    const newTemplateIdx = curTemplateIdx > 1 ? curTemplateIdx - 1 : 0;
+
+    setGraphTemplates(newGraphTemplates);
+    setCurTemplateIdx(newTemplateIdx);
+
+    userStore.updateUserProfileValues({
+      ...userStore.profile,
+      data: {
+        computed_props: {
+          graph_templates: newGraphTemplates,
+          cur_template_idx: newTemplateIdx,
+        },
+      },
+    });
+  };
+
+  useImperativeHandle(ref, () => ({
+    saveTemplate,
+    deleteTemplate,
+  }));
+
+  if (!show || graphData.length === 0) return <span />;
+
+  const template = graphTemplates.length === 0
+    ? defaultTemplate
+    : graphTemplates[curTemplateIdx];
+
+  const xAxisType = template?.xAxisType || 'lumo';
+  const yAxisType = template?.yAxisType || 'mean_abs_potential';
+  const xAxis = graphSettings[xAxisType] || graphSettings.lumo;
+  const yAxis = graphSettings[yAxisType] || graphSettings.mean_abs_potential;
+
+  const referenceDesc = template?.referenceDesc ?? defaultTemplate.referenceDesc;
+  const referencePoints = template?.referencePoints || etlReferences;
+  if (referencePoints.length === 0) {
+    referencePoints.push({ x: '', y: '', type: 'reference' });
   }
 
-  saveTemplate() {
-    const userProfile = UserStore.getState().profile;
-    const { graphTemplates, curTemplateIdx } = this.state;
-    _.set(userProfile, 'data.computed_props.graph_templates', graphTemplates);
-    _.set(userProfile, 'data.computed_props.cur_template_idx', curTemplateIdx);
-    UserActions.updateUserProfile(userProfile);
-  }
+  const data = graphData.filter((dat) => dat.props).map((dat) => ({
+    name: dat.name,
+    svgPath: dat.svgPath,
+    x: dat.props[xAxisType] ?? dat.props.lumo,
+    y: dat.props[yAxisType] ?? dat.props.mean_abs_potential,
+  }));
 
-  deleteTemplate() {
-    const userProfile = UserStore.getState().profile;
-    const { graphTemplates } = this.state;
-    const { curTemplateIdx } = this.state;
-    const newGraphTemplates = _.cloneDeep(graphTemplates);
-    newGraphTemplates.splice(curTemplateIdx, 1);
-    const newTemplateIdx = curTemplateIdx > 1 ? (curTemplateIdx - 1) : 0;
-    _.set(userProfile, 'data.computed_props.graph_templates', newGraphTemplates);
-    _.set(userProfile, 'data.computed_props.cur_template_idx', newTemplateIdx);
-    UserActions.updateUserProfile(userProfile);
-  }
+  const axisSelectOptions = Object.keys(graphSettings).map((k) => (
+    { label: graphSettings[k].label, value: k }
+  ));
+  const templateOptions = graphTemplates.map((templ, idx) => (
+    { label: templ.name, value: idx }
+  ));
 
-  render() {
-    const { show, graphData } = this.props;
-    if (!show || graphData.length === 0) return <span />;
+  return (
+    <Container>
+      <Row>
+        <Col xs={18} md={12}>
+          <ComputedPropsGraph
+            xAxis={xAxis}
+            yAxis={yAxis}
+            show={show}
+            data={data}
+            referencePoints={referencePoints}
+            referenceDesc={referenceDesc}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={9} md={6}>
+          <GraphReferenceTable
+            xLabel={xAxis.label}
+            yLabel={yAxis.label}
+            data={referencePoints}
+            updateData={updateReferences}
+          />
+        </Col>
+        <Col xs={9} md={6} className="d-flex">
+          <Form horizontal className="flex-grow-1 justify-content-end mt-2">
+            <Form.Group controlId="formInlineTemplate" className="mb-2">
+              <Form.Label column sm={4}>Template</Form.Label>
+              <CreatableSelect
+                isClearable
+                isInputEditable
+                inputValue={templateInputValue}
+                onChange={(selectedOption) => {
+                  const value = selectedOption ? selectedOption.label : '';
+                  setTemplateInputValue(value);
+                  onTemplateChange(selectedOption);
+                }}
+                onInputChange={(inputValue, { action }) => {
+                  if (action === 'input-change') {
+                    setTemplateInputValue(inputValue);
+                  }
+                }}
+                value={templateOptions.find(({ value }) => value === curTemplateIdx)}
+                options={templateOptions}
+                placeholder="Select or create template"
+                allowCreateWhileLoading
+                formatCreateLabel={(label) => `Create new '${label}' template`}
+              />
+            </Form.Group>
+            <Form.Group controlId="formInlineXAxis" className="mb-2">
+              <Form.Label column sm={4}>X Axis</Form.Label>
+              <Select
+                onChange={onXAxisChange}
+                value={axisSelectOptions.find(({ value }) => value === xAxisType)}
+                options={axisSelectOptions}
+              />
+            </Form.Group>
+            <Form.Group controlId="formInlineYAxis" className="mb-2">
+              <Form.Label column sm={4}>Y Axis</Form.Label>
+              <Select
+                onChange={onYAxisChange}
+                value={axisSelectOptions.find(({ value }) => value === yAxisType)}
+                options={axisSelectOptions}
+              />
+            </Form.Group>
+            <Form.Group controlId="formInlineRefDesc" className="mb-2">
+              <Form.Label>References Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                type="description"
+                placeholder="Description"
+                value={referenceDesc}
+                rows={5}
+                onChange={onDescChange}
+              />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+});
 
-    const { curTemplateIdx, graphTemplates, templateInputValue } = this.state;
-    const template = graphTemplates.size === 0
-      ? defaultTemplate
-      : graphTemplates[curTemplateIdx];
-
-    const xAxisType = template?.xAxisType || 'lumo';
-    const yAxisType = template?.yAxisType || 'mean_abs_potential';
-    const xAxis = graphSettings[xAxisType] || graphSettings.lumo;
-    const yAxis = graphSettings[yAxisType] || graphSettings.mean_abs_potential;
-
-    const referenceDesc = template?.referenceDesc ?? defaultTemplate.referenceDesc;
-    const referencePoints = template?.referencePoints || etlReferences;
-    if (referencePoints.length === 0) {
-      referencePoints.push({ x: '', y: '', type: 'reference' });
-    }
-
-    const data = graphData.filter((dat) => dat.props).map((dat) => ({
-      name: dat.name,
-      svgPath: dat.svgPath,
-      x: _.get(dat, `props.${xAxisType}`, dat.props.lumo),
-      y: _.get(dat, `props.${yAxisType}`, dat.props.mean_abs_potential),
-    }));
-
-    const axisSelectOptions = Object.keys(graphSettings).map((k) => (
-      { label: graphSettings[k].label, value: k }
-    ));
-    const templateOptions = graphTemplates.map((templ, idx) => (
-      { label: templ.name, value: idx }
-    ));
-
-    return (
-      <Container>
-        <Row>
-          <Col xs={18} md={12}>
-            <ComputedPropsGraph
-              xAxis={xAxis}
-              yAxis={yAxis}
-              show={show}
-              data={data}
-              referencePoints={referencePoints}
-              referenceDesc={referenceDesc}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={9} md={6}>
-            <GraphReferenceTable
-              xLabel={xAxis.label}
-              yLabel={yAxis.label}
-              data={referencePoints}
-              updateData={this.updateReferences}
-            />
-          </Col>
-          <Col xs={9} md={6} className="d-flex">
-            <Form horizontal className="flex-grow-1 justify-content-end mt-2">
-              <Form.Group controlId="formInlineTemplate" className="mb-2">
-                <Form.Label column sm={4}>Template</Form.Label>
-                <CreatableSelect
-                  isClearable
-                  isInputEditable
-                  inputValue={templateInputValue}
-                  onChange={(selectedOption) => {
-                    const value = selectedOption ? selectedOption.label : '';
-                    this.setState({ templateInputValue: value });
-                    this.onTemplateChange(selectedOption);
-                  }}
-                  onInputChange={(inputValue, { action }) => {
-                    if (action === 'input-change') {
-                      this.setState({ templateInputValue: inputValue });
-                    }
-                  }}
-                  value={templateOptions.find(({ value }) => value === curTemplateIdx)}
-                  options={templateOptions}
-                  placeholder="Select or create template"
-                  allowCreateWhileLoading
-                  formatCreateLabel={(label) => `Create new '${label}' template`}
-                />
-              </Form.Group>
-              <Form.Group controlId="formInlineXAxis" className="mb-2">
-                <Form.Label column sm={4}>X Axis</Form.Label>
-                <Select
-                  onChange={this.onXAxisChange}
-                  value={axisSelectOptions.find(({ value }) => value === xAxisType)}
-                  options={axisSelectOptions}
-                />
-              </Form.Group>
-              <Form.Group controlId="formInlineYAxis" className="mb-2">
-                <Form.Label column sm={4}>Y Axis</Form.Label>
-                <Select
-                  onChange={this.onYAxisChange}
-                  value={axisSelectOptions.find(({ value }) => value === yAxisType)}
-                  options={axisSelectOptions}
-                />
-              </Form.Group>
-              <Form.Group controlId="formInlineRefDesc" className="mb-2">
-                <Form.Label>References Description</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  type="description"
-                  placeholder="Description"
-                  value={referenceDesc}
-                  rows={5}
-                  onChange={(e) => this.onDescChange(e)}
-                />
-              </Form.Group>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+ComputedPropsGraphContainer.displayName = 'ComputedPropsGraphContainer';
 
 ComputedPropsGraphContainer.propTypes = {
   graphData: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   show: PropTypes.bool.isRequired,
 };
 
-ComputedPropsGraphContainer.defaultProps = {};
+export default ComputedPropsGraphContainer;
