a1c5c746d75757137fc444d3e18237452922628b
Beate Quednau <bq@megorei.com>
2026-07-28 17:52:16 +0200

    Remove UserStore from omniauthCredential LoginOptions

--- app/javascript/src/apps/omniauthCredential/LoginOptions.js
+++ app/javascript/src/apps/omniauthCredential/LoginOptions.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import {
   Container,
@@ -7,8 +7,8 @@ import {
   Button
 } from 'react-bootstrap';
 import uuid from 'uuid';
-import UserStore from 'src/stores/alt/stores/UserStore';
-import UserActions from 'src/stores/alt/actions/UserActions';
+import { observer } from 'mobx-react';
+import { StoreContext } from 'src/stores/mobx/RootStore';
 
 function omniauthLabel(icon, name) {
   if (icon) {
@@ -19,53 +19,40 @@ function omniauthLabel(icon, name) {
   return name;
 }
 
-export default class LoginOptions extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      omniauthProviders: []
-    };
-    this.onChange = this.onChange.bind(this);
-  }
+const LoginOptions = () => {
+  const { userStore } = useContext(StoreContext);
+  const { omniauthProviders } = userStore || [];
 
-  componentDidMount() {
-    UserStore.listen(this.onChange);
-    UserActions.fetchOmniauthProviders();
-  }
+  useEffect(() => {
+    userStore.fetchOmniauthProviders();
+  }, [userStore]);
 
-  onChange(state) {
-    const { omniauthProviders } = this.state;
-    if (state?.omniauthProviders !== omniauthProviders) {
-      this.setState({
-        omniauthProviders: state.omniauthProviders
-      });
-    }
-  }
+  const keys = Object.keys(omniauthProviders);
+  if (keys.length === 0) return null;
 
-  render() {
-    const { omniauthProviders } = this.state;
-    const keys = Object.keys(omniauthProviders);
-    if (keys.length === 0) return null;
-    const items = keys.map((key) => (
-      <Col key={uuid.v1()} md={12 / keys.length} className="login-options">
-        <Button href={`/users/auth/${key}`}>
-          Login with &nbsp;
-          {omniauthLabel(omniauthProviders[key].icon, omniauthProviders[key].label || key)}
-        </Button>
-      </Col>
-    ));
+  const items = keys.map((key) => (
+    <Col key={uuid.v1()} md={12 / keys.length} className="login-options">
+      <Button href={`/users/auth/${key}`}>
+        Login with &nbsp;
+        {omniauthLabel(omniauthProviders[key].icon, omniauthProviders[key].label || key)}
+      </Button>
+    </Col>
+  ));
 
-    return (
-      <Container>
-        <Row>
-          {items}
-        </Row>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Row>
+        {items}
+      </Row>
+    </Container>
+  );
+};
+
+const ObservedLoginOptions = observer(LoginOptions);
 
 document.addEventListener('DOMContentLoaded', () => {
   const domElement = document.getElementById('LoginOptions');
-  if (domElement) { ReactDOM.render(<LoginOptions />, domElement); }
+  if (domElement) { ReactDOM.render(<ObservedLoginOptions />, domElement); }
 });
+
+export default ObservedLoginOptions;
