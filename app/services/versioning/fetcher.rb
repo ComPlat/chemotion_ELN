de21872920fc095640a413de7dc59c547faa6a41
Adrian Herrmann <headri@web.de>
2026-08-11 08:00:36 +0000

    refactor fetcher

--- app/services/versioning/fetcher.rb
+++ app/services/versioning/fetcher.rb
@@ -6,6 +6,16 @@ module Versioning
 
     attr_accessor :record
 
+    FETCHERS = {
+      ::Collection => Versioning::Fetchers::CollectionFetcher,
+      ::Sample => Versioning::Fetchers::SampleFetcher,
+      ::Reaction => Versioning::Fetchers::ReactionFetcher,
+      ::ResearchPlan => Versioning::Fetchers::ResearchPlanFetcher,
+      ::Screen => Versioning::Fetchers::ScreenFetcher,
+      ::Wellplate => Versioning::Fetchers::WellplateFetcher,
+      ::DeviceDescription => Versioning::Fetchers::DeviceDescriptionFetcher,
+    }.freeze
+
     def self.call(record)
       new(record: record).call
     end
@@ -17,22 +27,10 @@ module Versioning
     private
 
     def versions
-      case record
-      when ::Collection
-        Versioning::Fetchers::CollectionFetcher.call(collection: record)
-      when ::Sample
-        Versioning::Fetchers::SampleFetcher.call(sample: record)
-      when ::Reaction
-        Versioning::Fetchers::ReactionFetcher.call(reaction: record)
-      when ::ResearchPlan
-        Versioning::Fetchers::ResearchPlanFetcher.call(research_plan: record)
-      when ::Screen
-        Versioning::Fetchers::ScreenFetcher.call(screen: record)
-      when ::Wellplate
-        Versioning::Fetchers::WellplateFetcher.call(wellplate: record)
-      when ::DeviceDescription
-        Versioning::Fetchers::DeviceDescriptionFetcher.call(device_description: record)
-      end
+      fetcher_class = FETCHERS[record.class]
+      return [] unless fetcher_class
+
+      fetcher_class.call(record.class.name.underscore.to_sym => record)
     end
   end
 end
