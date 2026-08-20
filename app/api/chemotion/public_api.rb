0ae44b66cdf9aebfac2e6baea5a3432c03ff51b5
Beate Quednau <bq@megorei.com>
2026-08-20 15:15:45 +0200

    Add about version info to public api, Remove about view

--- app/api/chemotion/public_api.rb
+++ app/api/chemotion/public_api.rb
@@ -90,6 +90,21 @@ module Chemotion
         end
       end
 
+      namespace :about do
+        desc 'get Chemotion ELN - About Infos'
+        get do
+          current_version = []
+          Chemotion::Application.config.version.each do |k, v|
+            next if v.to_s == '0'
+            current_version << { k.humanize => v }
+          end
+          changelog_version = Chemotion::Application.config.version["version"]&.gsub(/\./,'')
+          changelog_link =  "https://github.com/ComPlat/chemotion_ELN/blob/main/CHANGELOG.md##{changelog_version}"
+
+          { version: current_version, changelog_link: changelog_link }
+        end
+      end
+
       namespace :download do
         desc 'download file for editoring'
         before do
